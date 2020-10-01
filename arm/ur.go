fe2cec1ead5b190f56d48a4bf56ca1ae6fca24ff
Alexa Greenberg <alexa.greenberg@viam.com>
2020-10-01 01:03:19 -0400

    little ui to play with

--- arm/ur.go
+++ arm/ur.go
@@ -8,6 +8,25 @@ import (
 	"time"
 )
 
+type URArm struct {
+	conn  net.Conn
+	State RobotState
+	Debug bool
+}
+
+func URArmConnect(host string) (*URArm, error) {
+	conn, err := net.Dial("tcp", host+":30001")
+	if err != nil {
+		return nil, err
+	}
+
+	arm := &URArm{conn: conn, Debug: false}
+
+	go reader(conn, arm) // TODO: how to shutdown
+
+	return arm, nil
+}
+
 func movej(conn net.Conn, base float64, shoulder float64, elbow float64, w1 float64, w2 float64, w3 float64) error {
 	return movejRadians(conn, convertDtoR(base), convertDtoR(shoulder), convertDtoR(elbow), convertDtoR(w1), convertDtoR(w2), convertDtoR(w3))
 }
@@ -21,21 +40,30 @@ func movejRadians(conn net.Conn, base float64, shoulder float64, elbow float64, 
 	return err
 }
 
-var state RobotState
+func (arm *URArm) MoveToPositionC(c CartesianInfo) error {
+	return arm.MoveToPosition(
+		c.X,
+		c.Y,
+		c.Z,
+		c.Rx,
+		c.Ry,
+		c.Rz,
+	)
+}
 
-func moveToPosition(conn net.Conn, x, y, z, rx, ry, rz float64) error {
-	_, err := fmt.Fprintf(conn, "movel(p[%f,%f,%f,%f,%f,%f], a=0.1, v=0.1, r=0)\r\n", x, y, z, rx, ry, rz)
+func (arm *URArm) MoveToPosition(x, y, z, rx, ry, rz float64) error {
+	_, err := fmt.Fprintf(arm.conn, "movel(p[%f,%f,%f,%f,%f,%f], a=0.1, v=0.1, r=0)\r\n", x, y, z, rx, ry, rz)
 	if err != nil {
 		return err
 	}
 
 	for {
-		if math.Round(x*100) == math.Round(state.CartesianInfo.X*100) &&
-			math.Round(y*100) == math.Round(state.CartesianInfo.Y*100) &&
-			math.Round(z*100) == math.Round(state.CartesianInfo.Z*100) &&
-			math.Round(rx*100) == math.Round(state.CartesianInfo.Rx*100) &&
-			math.Round(ry*100) == math.Round(state.CartesianInfo.Ry*100) &&
-			math.Round(rz*100) == math.Round(state.CartesianInfo.Rz*100) {
+		if math.Round(x*100) == math.Round(arm.State.CartesianInfo.X*100) &&
+			math.Round(y*100) == math.Round(arm.State.CartesianInfo.Y*100) &&
+			math.Round(z*100) == math.Round(arm.State.CartesianInfo.Z*100) &&
+			math.Round(rx*100) == math.Round(arm.State.CartesianInfo.Rx*100) &&
+			math.Round(ry*100) == math.Round(arm.State.CartesianInfo.Ry*100) &&
+			math.Round(rz*100) == math.Round(arm.State.CartesianInfo.Rz*100) {
 			return nil
 		}
 		time.Sleep(10 * time.Millisecond)
@@ -43,7 +71,7 @@ func moveToPosition(conn net.Conn, x, y, z, rx, ry, rz float64) error {
 
 }
 
-func reader(conn net.Conn) {
+func reader(conn net.Conn, arm *URArm) {
 	for {
 		buf := make([]byte, 4)
 		n, err := conn.Read(buf)
@@ -68,52 +96,28 @@ func reader(conn net.Conn) {
 		}
 
 		if buf[0] == 16 {
-			state, err = readRobotStateMessage(buf[1:])
+			state, err := readRobotStateMessage(buf[1:])
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("isOn: %v stopped: %v joints: %f %f %f %f %f %f cartesian: %f %f %f %f %f %f\n",
-				state.RobotModeData.IsRobotPowerOn,
-				state.RobotModeData.IsEmergencyStopped || state.RobotModeData.IsProtectiveStopped,
-				state.Joints[0].AngleDegrees(),
-				state.Joints[1].AngleDegrees(),
-				state.Joints[2].AngleDegrees(),
-				state.Joints[3].AngleDegrees(),
-				state.Joints[4].AngleDegrees(),
-				state.Joints[5].AngleDegrees(),
-				state.CartesianInfo.X,
-				state.CartesianInfo.Y,
-				state.CartesianInfo.Z,
-				state.CartesianInfo.Rx,
-				state.CartesianInfo.Ry,
-				state.CartesianInfo.Rz)
-
+			arm.State = state
+			if arm.Debug {
+				fmt.Printf("isOn: %v stopped: %v joints: %f %f %f %f %f %f cartesian: %f %f %f %f %f %f\n",
+					state.RobotModeData.IsRobotPowerOn,
+					state.RobotModeData.IsEmergencyStopped || state.RobotModeData.IsProtectiveStopped,
+					state.Joints[0].AngleDegrees(),
+					state.Joints[1].AngleDegrees(),
+					state.Joints[2].AngleDegrees(),
+					state.Joints[3].AngleDegrees(),
+					state.Joints[4].AngleDegrees(),
+					state.Joints[5].AngleDegrees(),
+					state.CartesianInfo.X,
+					state.CartesianInfo.Y,
+					state.CartesianInfo.Z,
+					state.CartesianInfo.Rx,
+					state.CartesianInfo.Ry,
+					state.CartesianInfo.Rz)
+			}
 		}
 	}
-}
-
-func main() {
-	fmt.Println("hi")
-
-	conn, err := net.Dial("tcp", "192.168.2.155:30001")
-	if err != nil {
-		panic(err)
-	}
-
-	go reader(conn)
-
-	time.Sleep(500 * time.Millisecond)
-
-	rx := -math.Pi
-	ry := 0.0
-	rz := 0.0
-
-	moveToPosition(conn, .3, .4, 0.3, rx, ry, rz) // desk back left
-	moveToPosition(conn, .3, -.15, 0.3, rx, ry, rz) // desk front left
-	moveToPosition(conn, .6, -.15, 0.3, rx, ry, rz) // desck front right
-	moveToPosition(conn, .6, .4, 0.3, rx, ry, rz) // desk back right
-
-	moveToPosition(conn, .45, .1, 0, rx, ry, rz)   // desk middle down
-	moveToPosition(conn, .45, .1, 0.5, rx, ry, rz) // desk middle up
-
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/echolabsinc/robotcore
 go 1.14
 
 require (
+	fyne.io/fyne v1.3.3
 	github.com/stretchr/testify v1.6.1
 	gocv.io/x/gocv v0.24.0
 )
