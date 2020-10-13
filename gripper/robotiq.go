43301df18cacae6fc87767f5217123c81a2dc2ee
Alexa Greenberg <alexa.greenberg@viam.com>
2020-10-12 23:00:03 -0400

    gripper work

--- gripper/robotiq.go
+++ gripper/robotiq.go
@@ -1,7 +1,9 @@
 package gripper
 
 import (
+	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -14,12 +16,90 @@ func NewGripper(host string) (*Gripper, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Gripper{conn}, nil
+	g := &Gripper{conn}
+
+	g.Set("ACT", "1")   // robot activate
+	g.Set("GTO", "1")   // gripper activate
+	g.Set("FOR", "200") // force (0-255)
+	g.Set("SPE", "200") // speed (0-255)
+
+	// wait for init
+	time.Sleep(1500 * time.Millisecond) // TODO: how to make better
+
+	return g, nil
 }
 
-func (g *Gripper) Set(what string, to string) error {
-	_, err := g.conn.Write([]byte("SET " + what + " " + to + "\r\n"))
-	// TODO: rather than sleeping, should ask if we've made it there yet
-	time.Sleep(10 * time.Millisecond)
-	return err
+func (g *Gripper) Send(msg string) (string, error) {
+	_, err := g.conn.Write([]byte(msg))
+	if err != nil {
+		return "", err
+	}
+
+	res, err := g.read()
+	if err != nil {
+		return "", err
+	}
+
+	return res, err
+}
+
+func (g *Gripper) Set(what string, to string) (string, error) {
+	return g.Send(fmt.Sprintf("SET %s %s\r\n", what, to))
+}
+
+func (g *Gripper) Get(what string) (string, error) {
+	return g.Send(fmt.Sprintf("GET %s\r\n", what))
+}
+
+func (g *Gripper) read() (string, error) {
+	buf := make([]byte, 128)
+	x, err := g.conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+	if x > 100 {
+		return "", fmt.Errorf("Gripper::read too much: %d\n", x)
+	}
+	if x == 0 {
+		return "", nil
+	}
+	return strings.TrimSpace(string(buf[0:x])), nil
+}
+
+// --------------
+
+func (g *Gripper) SetPos(pos string) (bool, error) {
+	_, err := g.Set("POS", pos)
+	if err != nil {
+		return false, err
+	}
+
+	prev := ""
+
+	for {
+		x, err := g.Get("POS")
+		if err != nil {
+			return false, err
+		}
+		fmt.Printf("got: [%s]\n", x)
+		if x == "POS "+pos {
+			return true, nil
+		}
+
+		if prev == x {
+			return false, nil
+		}
+		prev = x
+
+		time.Sleep(100 * time.Millisecond)
+	}
+
+}
+
+func (g *Gripper) Open() (bool, error) {
+	return g.SetPos("3")
+}
+
+func (g *Gripper) Close() (bool, error) {
+	return g.SetPos("249")
 }
