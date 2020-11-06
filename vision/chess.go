ced952329d4cc07b2999c79b507681f25b7d34a7
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-05 21:53:40 -0500

    more old cruft removal

--- vision/chess.go
+++ vision/chess.go
@@ -5,7 +5,6 @@ import (
 	"image"
 	"image/color"
 	"math"
-	"time"
 
 	"gocv.io/x/gocv"
 )
@@ -64,42 +63,4 @@ func closeupProcess(img gocv.Mat) {
 	//process(img)
 	fromGithub(img)
 	//process(img)
-}
-
-func webcamDisplaySilly() {
-	// set to use a video capture device 0
-	deviceID := 0
-
-	// open webcam
-	webcam, err := gocv.OpenVideoCapture(deviceID)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer webcam.Close()
-
-	// open display window
-	window := gocv.NewWindow("test1")
-	defer window.Close()
-
-	// prepare image matrix
-	img := gocv.NewMat()
-	defer img.Close()
-
-	fmt.Printf("start reading camera device: %v\n", deviceID)
-	for {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("cannot read device %v\n", deviceID)
-			continue
-		}
-		if img.Empty() {
-			continue
-		}
-
-		process(img)
-
-		window.IMShow(img)
-		window.WaitKey(1)
-		time.Sleep(1000 * time.Millisecond)
-	}
 }
