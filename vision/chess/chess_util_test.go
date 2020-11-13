240b1ea6c3ae518ae988a93d1181f7f6dbe42072
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-13 14:50:40 -0500

    making things work for new camera

--- vision/chess/chess_util_test.go
+++ vision/chess/chess_util_test.go
@@ -69,6 +69,51 @@ func TestWarpColorAndDepthToChess1(t *testing.T) {
 		t.Errorf("height for C1 is wrong %f", x)
 	}
 
-	theBoard.Annotate()
-	gocv.IMWrite("out/board1_annotated.png", theBoard.color)
+	annotated := theBoard.Annotate()
+	gocv.IMWrite("out/board1_annotated.png", annotated)
+}
+
+func TestWarpColorAndDepthToChess2(t *testing.T) {
+	img := gocv.IMRead("data/board2.png", gocv.IMReadUnchanged)
+	dm, err := vision.ParseDepthMap("data/board2.dat.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	debugOut := gocv.NewMat()
+	defer debugOut.Close()
+	corners, err := findChessCorners(img, &debugOut)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gocv.IMWrite("out/board2_corners.png", debugOut)
+
+	a, b, err := warpColorAndDepthToChess(img, dm.ToMat(), corners)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.MkdirAll("out", 0775)
+	gocv.IMWrite("out/board2_warped.png", a)
+
+	theBoard := Board{a, b}
+
+	x := theBoard.PieceHeight("B1")
+	if x < 40 || x > 58 {
+		t.Errorf("height for B1 is wrong %f", x)
+	}
+
+	x = theBoard.PieceHeight("E1")
+	if x < 70 || x > 100 {
+		t.Errorf("height for E1 is wrong %f", x)
+	}
+
+	x = theBoard.PieceHeight("C1")
+	if x < 50 || x > 71 {
+		t.Errorf("height for C1 is wrong %f", x)
+	}
+
+	annotated := theBoard.Annotate()
+	gocv.IMWrite("out/board2_annotated.png", annotated)
 }
