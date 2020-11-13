818cf10d8ccad7037141dbc04f4bbff6f22d3cc6
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-13 13:19:55 -0500

    suffer through bgr

--- vision/colors_test.go
+++ vision/colors_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestWhatColor1(t *testing.T) {
-	data := gocv.Vecb{200, 20, 20}
+	data := gocv.Vecb{20, 20, 200}
 	c, _ := WhatColor(data)
 	if c != "red" {
 		t.Errorf("got %s instead of red", c)
