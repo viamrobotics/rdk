ad976254d5739a04d40b3c49553c99a09f81dac7
Alexa Greenberg <alexa.greenberg@viam.com>
2020-11-24 12:17:00 -0500

    more things can go in config

--- robot/config_test.go
+++ robot/config_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestConfig1(t *testing.T) {
+func TestConfigRobot(t *testing.T) {
 	cfg, err := ReadConfig("data/robot.json")
 	if err != nil {
 		t.Fatal(err)
@@ -12,6 +12,29 @@ func TestConfig1(t *testing.T) {
 
 	if len(cfg.Components) != 3 {
 		t.Errorf("bad config read %v", cfg)
+	}
+
+}
+
+func TestConfig1(t *testing.T) {
+	cfg, err := ReadConfig("data/cfgtest1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r, err := NewRobot(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pic, _, err := r.Cameras[0].NextColorDepthPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pic.Close()
+
+	if pic.Cols() < 100 {
+		t.Errorf("pictures seems wrong %d %d", pic.Cols(), pic.Rows())
 	}
 
 }
