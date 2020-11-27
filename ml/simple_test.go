1001d26b568603709d85ea652f932a2f99ae2502
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-26 22:38:21 -0500

    playing with a ml api

--- ml/simple_test.go
+++ ml/simple_test.go
@@ -7,17 +7,17 @@ import (
 func TestSimple1(t *testing.T) {
 
 	data := [][]float64{
-		[]float64{0, 0},
-		[]float64{1, 0},
-		[]float64{0, 1},
-		[]float64{1, 1},
-		[]float64{0, 0},
-		[]float64{1, 0},
-		[]float64{0, 1},
-		[]float64{1, 1},
+		[]float64{0, 0, 5},
+		[]float64{1, 0, 6},
+		[]float64{0, 1, 1},
+		[]float64{1, 1, 3},
+		[]float64{0, 0, 4},
+		[]float64{1, 0, 1},
+		[]float64{0, 1, 2},
+		[]float64{1, 1, 3},
 	}
 
-	correct := []float64{
+	correct := []int{
 		0,
 		1,
 		0,
@@ -43,4 +43,60 @@ func TestSimple1(t *testing.T) {
 			t.Errorf("wrong result for row %d, data: %v correct: %v got: %v", x, row, correct[x], got)
 		}
 	}
+}
+
+func TestSimpleNN1(t *testing.T) {
+
+	data := [][]float64{
+		[]float64{0, 0, 5},
+		[]float64{1, 0, 6},
+		[]float64{0, 1, 1},
+		[]float64{1, 1, 3},
+		[]float64{0, 0, 4},
+		[]float64{1, 0, 1},
+		[]float64{0, 1, 2},
+		[]float64{1, 1, 3},
+	}
+
+	correct := []int{
+		0,
+		1,
+		0,
+		1,
+		0,
+		1,
+		0,
+		1,
+	}
+
+	c := GoLearnNNClassifier{}
+	err := c.Train(data, correct)
+	if err != nil {
+		panic(err)
+	}
+
+	for x, row := range data {
+		got, err := c.Classify(row)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != correct[x] {
+			t.Errorf("wrong result for row %d, data: %v correct: %v got: %v", x, row, correct[x], got)
+		}
+	}
+
+	for _, r := range data {
+		r[2] = r[2] + 1
+	}
+
+	for x, row := range data {
+		got, err := c.Classify(row)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != correct[x] {
+			t.Errorf("wrong result for row %d, data: %v correct: %v got: %v", x, row, correct[x], got)
+		}
+	}
+
 }
