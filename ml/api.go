4613b70ec45d8b9fa7c6559c198cf0a7d43423d9
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-26 22:38:21 -0500

    playing with a ml api

--- ml/api.go
+++ ml/api.go
@@ -3,6 +3,6 @@ package ml
 // TODO: this is all wrong, I just need a pivot point in the sand
 
 type Classifier interface {
-	Classify(data []float64) (float64, error)
-	Train(data [][]float64, correct []float64) error
+	Classify(data []float64) (int, error)
+	Train(data [][]float64, correct []int) error
 }
