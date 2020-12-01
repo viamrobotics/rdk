c41f3d9e7cedb5ae32aa2dbb63fe096b9e656e25
Eliot Horowitz <eliothorowitz@gmail.com>
2020-11-30 22:07:13 -0500

    math util code for squares that is fast

--- rcutil/math.go
+++ rcutil/math.go
@@ -6,3 +6,13 @@ func AbsInt(n int) int {
 	}
 	return n
 }
+
+// Math.pow( x, 2 ) is slow, this is faster
+func Square(n float64) float64 {
+	return n * n
+}
+
+// Math.pow( x, 2 ) is slow, this is faster
+func SquareInt(n int) int {
+	return n * n
+}
