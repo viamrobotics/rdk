6e797dc3cfc343e2c7ad70032790afb6156088b7
Eliot Horowitz <eliothorowitz@gmail.com>
2020-12-17 15:38:19 -0500

    alternat format for depth data that is a lot faster

--- vision/depth_map_test.go
+++ vision/depth_map_test.go
@@ -10,6 +10,7 @@ func TestDepthMap1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	m.Smooth()
 
 	if m.width != 1280 {
 		t.Errorf("wrong width")
@@ -27,6 +28,7 @@ func TestDepthMap1(t *testing.T) {
 	theMat := m.ToMat()
 
 	m = NewDepthMapFromMat(theMat)
+
 	if m.width != 1280 {
 		t.Errorf("wrong width")
 	}
@@ -60,4 +62,33 @@ func TestDepthMap1(t *testing.T) {
 		t.Errorf("wrong depth")
 	}
 
+}
+
+func TestDepthMapNewFormat(t *testing.T) {
+	m, err := ParseDepthMap("data/depthformat2.dat.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if m.width != 1280 || m.height != 720 {
+		t.Errorf("width and height wrong %v %v", m.width, m.height)
+	}
+
+	numZero := 0
+
+	for x := 0; x < m.width; x++ {
+		d := m.GetDepth(x, m.height-1)
+		if d == 0 {
+			numZero = numZero + 1
+		} else {
+			if d < 100 || d > 5000 {
+				t.Errorf("weird depth %v", d)
+			}
+		}
+
+	}
+
+	if numZero == 0 || numZero >= m.width {
+		t.Errorf("numZero wrong %v", numZero)
+	}
 }
