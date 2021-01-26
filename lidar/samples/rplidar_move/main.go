816bb6b08d61d6243bc23213b99d0a6a842d58ec
Alexa Greenberg <alexa.greenberg@viam.com>
2021-01-26 10:31:01 -0500

    works a little more now

--- lidar/samples/rplidar_move/main.go
+++ lidar/samples/rplidar_move/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"sync"
 	"syscall"
 	"time"
 
@@ -20,7 +19,6 @@ import (
 	"github.com/edaniels/golog"
 	"github.com/edaniels/gostream"
 	"github.com/edaniels/gostream/codec/vpx"
-	"github.com/james-bowman/sparse"
 )
 
 type stringFlags []string
@@ -32,6 +30,36 @@ func (sf *stringFlags) Set(value string) error {
 
 func (sf *stringFlags) String() string {
 	return fmt.Sprint([]string(*sf))
+}
+
+func registerDevices(devicePaths []string) []lidar.Device {
+	golog.Global.Debugw("registering devices")
+	var lidarDevices []lidar.Device
+	for _, devPath := range devicePaths {
+		if devPath == "fake" {
+			lidarDevices = append(lidarDevices, &support.FakeLidar{})
+			continue
+		}
+		lidarDev, err := rplidar.NewRPLidar(devPath)
+		if err != nil {
+			golog.Global.Fatal(err)
+		}
+		lidarDevices = append(lidarDevices, lidarDev)
+	}
+
+	for i, lidarDev := range lidarDevices {
+		if rpl, ok := lidarDev.(*rplidar.RPLidar); ok {
+			golog.Global.Infow("rplidar",
+				"dev_path", devicePaths[i],
+				"model", rpl.Model(),
+				"serial", rpl.SerialNumber(),
+				"firmware_ver", rpl.FirmwareVersion(),
+				"hardware_rev", rpl.HardwareRevision())
+		}
+		lidarDev.Start()
+	}
+
+	return lidarDevices
 }
 
 func main() {
@@ -58,106 +86,78 @@ func main() {
 		port = int(portParsed)
 	}
 
-	golog.Global.Debugw("registering devices")
-	var lidarDevices []lidar.Device
-	for _, devPath := range devicePaths {
-		if devPath == "fake" {
-			lidarDevices = append(lidarDevices, &support.FakeLidar{})
-			continue
-		}
-		lidarDev, err := rplidar.NewRPLidar(devPath)
-		if err != nil {
-			golog.Global.Fatal(err)
-		}
-		lidarDevices = append(lidarDevices, lidarDev)
-	}
-
-	for i, lidarDev := range lidarDevices {
-		if rpl, ok := lidarDev.(*rplidar.RPLidar); ok {
-			golog.Global.Infow("rplidar",
-				"dev_path", devicePaths[i],
-				"model", rpl.Model(),
-				"serial", rpl.SerialNumber(),
-				"firmware_ver", rpl.FirmwareVersion(),
-				"hardware_rev", rpl.HardwareRevision())
-		}
-		lidarDev.Start()
+	lidarDevices := registerDevices(devicePaths)
+	for _, lidarDev := range lidarDevices {
 		defer lidarDev.Stop()
 	}
-
-	golog.Global.Debugw("setting up room")
 
 	// The room is 600m^2 tracked in centimeters
 	// 0 means no detected obstacle
 	// 1 means a detected obstacle
 	// TODO(erd): where is center? is a hack to just square the whole thing?
-	squareMeters := 600
-	squareMillis := squareMeters * 100
-	roomPoints := sparse.NewDOK(squareMillis, squareMillis)
-	centerX := squareMillis / 2
-	centerY := centerX
+	roomSizeMeters := 600
+	roomScale := 100 // cm
+	room := support.NewSquareRoom(roomSizeMeters, roomScale)
+	roomCenter := room.Center()
+	roomSize, roomSizeScale := room.Size()
 
-	base := &support.FakeBase{centerX, centerY, squareMillis, squareMillis}
-	roomPointsMu := &sync.Mutex{}
-	baseRoomPoints := make([]*sparse.DOK, 0, len(lidarDevices))
-	for range lidarDevices {
-		baseRoomPoints = append(baseRoomPoints, sparse.NewDOK(squareMillis, squareMillis))
+	base := &support.FakeBase{
+		roomCenter.X, roomCenter.Y,
+		roomSize * roomSizeScale, roomSize * roomSizeScale,
 	}
-	lar := &support.LocationAwareLidar{
-		Base:               base,
-		Devices:            lidarDevices,
-		RoomPointsCombined: roomPoints,
-		RoomPoints:         baseRoomPoints,
-		RoomPointsMu:       roomPointsMu,
-		ScaleDown:          100,
-	}
+
+	lar := support.NewLocationAwareRobot(base, lidarDevices, room)
+	lar.Start()
+	defer lar.Stop()
 
 	config := vpx.DefaultRemoteViewConfig
 	config.Debug = false
-	config.StreamName = "base view"
-	baseView, err := gostream.NewRemoteView(config)
+
+	// setup robot view
+	config.StreamName = "robot view"
+	robotView, err := gostream.NewRemoteView(config)
 	if err != nil {
 		golog.Global.Fatal(err)
 	}
 
-	baseView.SetOnDataHandler(func(data []byte) {
+	robotView.SetOnDataHandler(func(data []byte) {
 		golog.Global.Debugw("data", "raw", string(data))
-		if err := lar.HandleData(data, baseView.SendText); err != nil {
-			baseView.SendText(err.Error())
+		if err := lar.HandleData(data, robotView.SendText); err != nil {
+			robotView.SendText(err.Error())
 		}
 	})
 
-	baseView.SetOnClickHandler(func(x, y int) {
+	robotView.SetOnClickHandler(func(x, y int) {
 		golog.Global.Debugw("click", "x", x, "y", y)
-		if err := lar.HandleClick(x, y, 800, 600, baseView.SendText); err != nil {
-			baseView.SendText(err.Error())
+		if err := lar.HandleClick(x, y, 800, 600, robotView.SendText); err != nil {
+			robotView.SendText(err.Error())
 		}
 	})
 
+	// setup world view
 	config.StreamNumber = 1
 	config.StreamName = "world view"
-	worldRemoteView, err := gostream.NewRemoteView(config)
+	worldViewerRemoteView, err := gostream.NewRemoteView(config)
 	if err != nil {
 		golog.Global.Fatal(err)
 	}
-
-	worldView := &support.WorldViewer{roomPoints, roomPointsMu, 100}
-	worldRemoteView.SetOnDataHandler(func(data []byte) {
+	worldViewer := &support.WorldViewer{room, 100}
+	worldViewerRemoteView.SetOnDataHandler(func(data []byte) {
 		golog.Global.Debugw("data", "raw", string(data))
 		if bytes.HasPrefix(data, []byte("set_scale ")) {
 			newScaleStr := string(bytes.TrimPrefix(data, []byte("set_scale ")))
 			newScale, err := strconv.ParseInt(newScaleStr, 10, 32)
 			if err != nil {
-				worldRemoteView.SendText(err.Error())
+				worldViewerRemoteView.SendText(err.Error())
 				return
 			}
-			worldView.Scale = int(newScale)
-			worldRemoteView.SendText(fmt.Sprintf("scale set to %d", newScale))
+			worldViewer.ViewScale = int(newScale)
+			worldViewerRemoteView.SendText(fmt.Sprintf("scale set to %d", newScale))
 		}
 	})
 
-	server := gostream.NewRemoteViewServer(port, baseView, golog.Global)
-	server.AddView(worldRemoteView)
+	server := gostream.NewRemoteViewServer(port, robotView, golog.Global)
+	server.AddView(worldViewerRemoteView)
 	server.Run()
 
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
@@ -168,12 +168,14 @@ func main() {
 		cancelFunc()
 	}()
 
-	lar.Cull()
+	clientWidth := 800
+	clientHeight := 600
+	frameSpeed := 33 * time.Millisecond
 
-	baseViewMatSource := stream.ResizeMatSource{lar, 800, 600}
-	worldViewMatSource := stream.ResizeMatSource{worldView, 800, 600}
-	go stream.MatSource(cancelCtx, worldViewMatSource, worldRemoteView, 33*time.Millisecond, golog.Global)
-	stream.MatSource(cancelCtx, baseViewMatSource, baseView, 33*time.Millisecond, golog.Global)
+	robotViewMatSource := stream.ResizeMatSource{lar, clientWidth, clientHeight}
+	worldViewMatSource := stream.ResizeMatSource{worldViewer, clientWidth, clientHeight}
+	go stream.MatSource(cancelCtx, worldViewMatSource, worldViewerRemoteView, frameSpeed, golog.Global)
+	stream.MatSource(cancelCtx, robotViewMatSource, robotView, frameSpeed, golog.Global)
 
 	if err := server.Stop(context.Background()); err != nil {
 		golog.Global.Error(err)
