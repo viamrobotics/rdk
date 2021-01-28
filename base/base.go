e6fa4d9aa996fa5d5f51be353b3866f559d50a6f
Eric Daniels <eric@erdaniels.com>
2021-01-28 15:56:05 -0500

    improved rplidar reliability

--- base/base.go
+++ base/base.go
@@ -1,33 +1,5 @@
 package base
 
-import (
-	"fmt"
-)
-
-type Action struct {
-	DistanceMM int
-	Angle      int
-
-	Speed int
-	Block bool
-}
-
-func (a Action) Move(b Base) error {
-	if a.DistanceMM != 0 && a.Angle != 0 {
-		return fmt.Errorf("can only specify DistanceMM or Angle for now")
-	}
-
-	if a.DistanceMM != 0 {
-		return b.MoveStraight(a.DistanceMM, a.Speed, a.Block)
-	}
-
-	if a.Angle != 0 {
-		return b.Spin(a.Angle, a.Speed, a.Block)
-	}
-
-	return fmt.Errorf("need to specify DistanceMM or Angle")
-}
-
 type Base interface {
 	MoveStraight(distanceMM int, speed int, block bool) error
 	Spin(degrees int, power int, block bool) error
