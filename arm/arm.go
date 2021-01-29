5725c83fcd40e355e4a82f5e5ccdb7e8545a9c9e
Alexa Greenberg <alexa.greenberg@viam.com>
2021-01-28 23:26:23 -0500

    create arm api

---
+++ arm/arm.go
@@ -0,0 +1,11 @@
+package arm
+
+// this is probably wrong, just trying to start abstracting
+type Arm interface {
+	State() RobotState
+	MoveToPositionC(c CartesianInfo) error
+	MoveToPosition(x, y, z, rx, ry, rz float64) error
+	JointMoveDelta(joint int, amount float64) error
+
+	Close()
+}
