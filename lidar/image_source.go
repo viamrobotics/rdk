112c436ba5f802e20dada41263481a9ebc64fdec
Alexa Greenberg <alexa.greenberg@viam.com>
2021-02-01 14:27:13 -0500

    use gg for drawing in slam/lidar

--- lidar/image_source.go
+++ lidar/image_source.go
@@ -5,7 +5,7 @@ import (
 	"image"
 	"image/color"
 
-	"gocv.io/x/gocv"
+	"github.com/fogleman/gg"
 )
 
 // ImageSource generates images from the current scan of a lidar device
@@ -31,7 +31,7 @@ func (is *ImageSource) Next(ctx context.Context) (image.Image, error) {
 	centerX := bounds.X / 2
 	centerY := bounds.Y / 2
 
-	out := gocv.NewMatWithSize(bounds.X, bounds.Y, gocv.MatTypeCV8UC3)
+	dc := gg.NewContext(bounds.X, bounds.Y)
 
 	measurements, err := is.device.Scan()
 	if err != nil {
@@ -40,11 +40,14 @@ func (is *ImageSource) Next(ctx context.Context) (image.Image, error) {
 
 	for _, next := range measurements {
 		x, y := next.Coords()
-		p := image.Point{centerX + int(x*float64(scaleDown)), centerY + int(y*float64(scaleDown))}
-		gocv.Circle(&out, p, 4, color.RGBA{R: 255}, 1)
+		x = float64(centerX) + (x * float64(scaleDown))
+		y = float64(centerY) + (y * float64(scaleDown))
+		dc.DrawPoint(x, y, 4)
+		dc.SetColor(color.RGBA{255, 0, 0, 255})
+		dc.Fill()
 	}
 
-	return out.ToImage()
+	return dc.Image(), nil
 }
 
 func (is *ImageSource) Close() error {
