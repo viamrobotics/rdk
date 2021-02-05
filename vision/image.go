feb0b27492fe74eec56fa78d0c2a0971ba4a7c63
Eliot Horowitz <eliothorowitz@gmail.com>
2021-02-05 17:07:41 -0500

    fix another average bug missing the right side

--- vision/image.go
+++ vision/image.go
@@ -186,8 +186,8 @@ func (i *Image) AverageColorXY(x, y int, radius int) utils.HSV {
 
 	num := 0.0
 
-	for X := x - radius; X < x+radius; X++ {
-		for Y := y - radius; Y < y+radius; Y++ {
+	for X := x - radius; X <= x+radius; X++ {
+		for Y := y - radius; Y <= y+radius; Y++ {
 			if X < 0 || Y < 0 || X >= i.width || Y >= i.height {
 				continue
 			}
