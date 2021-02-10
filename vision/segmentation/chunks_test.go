43ab8c50c66b441f40b8f15490201c4d513def28
Eliot Horowitz <eliothorowitz@gmail.com>
2021-02-10 16:13:17 -0500

    cleaning test

--- vision/segmentation/chunks_test.go
+++ vision/segmentation/chunks_test.go
@@ -1,12 +1,10 @@
 package segmentation
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"testing"
 
-	"github.com/viamrobotics/robotcore/utils"
 	"github.com/viamrobotics/robotcore/vision"
 )
 
@@ -30,14 +28,6 @@ func (cid *chunkImageDebug) Process(d *vision.MultipleImageTestDebugger, fn stri
 		return err
 	}
 
-	type MyPoint struct {
-		corner      image.Point
-		avgDistance float64
-	}
-
-	points := []MyPoint{}
-	maxDistance := 0.0
-
 	t1 := .2
 
 	out1 := image.NewGray(img.Bounds())
@@ -46,12 +36,6 @@ func (cid *chunkImageDebug) Process(d *vision.MultipleImageTestDebugger, fn stri
 		for y := 0; y < img.Height(); y += 3 {
 
 			_, avgDistance := img.AverageColorAndStats(image.Point{x + 1, y + 1}, 1)
-
-			points = append(points, MyPoint{image.Point{x, y}, avgDistance})
-
-			if avgDistance > maxDistance {
-				maxDistance = avgDistance
-			}
 
 			clr := color.Gray{0}
 			if avgDistance > t1 {
@@ -71,46 +55,12 @@ func (cid *chunkImageDebug) Process(d *vision.MultipleImageTestDebugger, fn stri
 
 	d.GotDebugImage(out1, "t")
 
-	if maxDistance < t1 {
-		return fmt.Errorf("maxDistance too low %v", maxDistance)
-	}
-
 	if true {
-		out := image.NewGray(img.Bounds())
-
-		for _, p := range points {
-			scale := (p.avgDistance - t1) / (maxDistance - t1)
-			if scale < 0 {
-				scale = 0
-			}
-			clr := color.Gray{uint8(254 * scale)}
-
-			for a := 0; a < 3; a++ {
-				for b := 0; b < 3; b++ {
-					xx := p.corner.X + a
-					yy := p.corner.Y + b
-					out.SetGray(xx, yy, clr)
-				}
-			}
+		out, err := ShapeWalkEntireDebug(img, false)
+		if err != nil {
+			return err
 		}
-
-		d.GotDebugImage(out, "interesting1")
-	}
-
-	if false {
-		// play with 1 blue quare
-
-		out := image.NewRGBA(img.Bounds())
-		for x := 410; x < 490; x++ {
-			for y := 210; y < 290; y++ {
-				out.Set(x, y, img.At(x, y))
-				fmt.Println(utils.ConvertToColorful2(img.At(x, y)).Hex())
-			}
-
-		}
-
-		d.GotDebugImage(out, "play1")
-
+		d.GotDebugImage(out, "entire")
 	}
 
 	if true {
