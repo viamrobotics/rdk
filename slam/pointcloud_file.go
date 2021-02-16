d4f7356b6378bd5d20e0191adeabe89d7054d14f
Eric Daniels <eric@erdaniels.com>
2021-02-16 14:23:07 -0500

    start pc

---
+++ slam/pointcloud_file.go
@@ -0,0 +1,99 @@
+package slam
+
+import (
+	"fmt"
+	"math/rand"
+	"path/filepath"
+
+	"github.com/jblindsay/lidario"
+)
+
+func NewPointCloudFromFile(fn string) (*PointCloud, error) {
+	switch filepath.Ext(fn) {
+	case ".las":
+		return newPointCloudFromLASFile(fn)
+	default:
+		return nil, fmt.Errorf("do not know how to read file %q", fn)
+	}
+}
+
+func newPointCloudFromLASFile(fn string) (*PointCloud, error) {
+	lf, err := lidario.NewLasFile(fn, "r")
+	if err != nil {
+		return nil, err
+	}
+	defer lf.Close()
+
+	pc := NewPointCloud()
+	for i := 0; i < lf.Header.NumberPoints; i++ {
+		p, err := lf.LasPoint(i)
+		if err != nil {
+			return nil, err
+		}
+		data := p.PointData()
+
+		// TODO(erd): losing float data
+		// TODO(erd): color data
+		pc.Set(NewPoint(
+			int(data.X),
+			int(data.Y),
+			int(data.Z)))
+	}
+	return pc, nil
+}
+
+func (pc *PointCloud) WriteToFile(fn string) error {
+	lf, err := lidario.NewLasFile(fn, "w")
+	if err != nil {
+		return err
+	}
+	if err := lf.AddHeader(lidario.LasHeader{
+		PointFormatID: 2,
+	}); err != nil {
+		return err
+	}
+
+	var lastErr error
+	var count int
+	pc.Iterate(func(p Point) bool {
+		pos := p.Position()
+		redVal := rand.Intn(256) * 256
+		greenVal := rand.Intn(256) * 256
+		blueVal := rand.Intn(256) * 256
+		count++
+		if err := lf.AddLasPoint(&lidario.PointRecord2{
+			PointRecord0: &lidario.PointRecord0{
+				X:         float64(pos.X),
+				Y:         float64(pos.Y),
+				Z:         float64(pos.Z),
+				Intensity: 0,
+				BitField: lidario.PointBitField{
+					Value: (1) | (1 << 3) | (0 << 6) | (0 << 7),
+				},
+				ClassBitField: lidario.ClassificationBitField{
+					Value: 0,
+				},
+				ScanAngle:     0,
+				UserData:      0,
+				PointSourceID: 1,
+			},
+			RGB: &lidario.RgbData{
+				Red:   uint16(redVal),
+				Green: uint16(greenVal),
+				Blue:  uint16(blueVal),
+			},
+		}); err != nil {
+			lastErr = err
+			return false
+		}
+		return true
+	})
+	if lastErr != nil {
+		if err := lf.Close(); err != nil {
+			return err
+		}
+		return lastErr
+	}
+
+	return lf.Close()
+}
