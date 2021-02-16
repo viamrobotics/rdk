c71280ed2dcf0d4d6bce1546154014a7e9c96904
Eric Daniels <eric@erdaniels.com>
2021-02-16 10:56:09 -0500

    add dump to disk for lidar view

--- slam/area.go
+++ slam/area.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/james-bowman/sparse"
+	"github.com/jblindsay/lidario"
 )
 
 func NewSquareArea(meters int, scaleTo int) *SquareArea {
@@ -49,4 +50,50 @@ func (sa *SquareArea) Mutate(mutator func(room MutableArea)) {
 	sa.mu.Lock()
 	defer sa.mu.Unlock()
 	mutator(sa.points)
+}
+
+func (sa *SquareArea) WriteToFile(fn string) error {
+	lf, err := lidario.NewLasFile(fn, "w")
+	if err != nil {
+		return err
+	}
+	if err := lf.AddHeader(lidario.LasHeader{}); err != nil {
+		return err
+	}
+
+	var lastErr error
+	sa.Mutate(func(area MutableArea) {
+		area.DoNonZero(func(x, y int, v float64) {
+			if err := lf.AddLasPoint(&lidario.PointRecord2{
+				PointRecord0: &lidario.PointRecord0{
+					X:         float64(x),
+					Y:         float64(y),
+					Z:         0,
+					Intensity: 0,
+					BitField: lidario.PointBitField{
+						Value: (1) | (1 << 3) | (0 << 6) | (0 << 7),
+					},
+					ClassBitField: lidario.ClassificationBitField{
+						Value: 0,
+					},
+					ScanAngle:     0,
+					UserData:      0,
+					PointSourceID: 1,
+				},
+				RGB: &lidario.RgbData{
+					Red: 255,
+				},
+			}); err != nil {
+				lastErr = err
+			}
+		})
+	})
+	if lastErr != nil {
+		if err := lf.Close(); err != nil {
+			return err
+		}
+		return lastErr
+	}
+
+	return lf.Close()
 }
