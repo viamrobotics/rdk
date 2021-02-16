85f3f314d287c9279188c08a4a2afe5b60c0b466
Eric Daniels <eric@erdaniels.com>
2021-02-16 14:23:07 -0500

    start pc

---
+++ slam/pointcloud_file_test.go
@@ -0,0 +1,27 @@
+package slam
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/edaniels/test"
+)
+
+func TestNewPointCloudFromFile(t *testing.T) {
+	pc, err := NewPointCloudFromFile("data/test.las")
+	test.That(t, err, test.ShouldBeNil)
+	numPoints := pc.Size()
+	test.That(t, numPoints, test.ShouldEqual, 874)
+
+	temp, err := ioutil.TempFile("", "*.las")
+	test.That(t, err, test.ShouldBeNil)
+	defer os.Remove(temp.Name())
+
+	err = pc.WriteToFile(temp.Name())
+	test.That(t, err, test.ShouldBeNil)
+
+	nextPC, err := NewPointCloudFromFile(temp.Name())
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, nextPC, test.ShouldResemble, pc)
+}
