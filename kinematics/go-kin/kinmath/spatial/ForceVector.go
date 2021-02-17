08b9705cf8453ccb85a2f25c4ddff59d25c93e7d
Alexa Greenberg <alexa.greenberg@viam.com>
2021-02-17 14:36:30 -0800

    Update import path for kinematics

--- kinematics/go-kin/kinmath/spatial/ForceVector.go
+++ kinematics/go-kin/kinmath/spatial/ForceVector.go
@@ -1,19 +1,19 @@
 package spatial
 
 import (
-  "github.com/go-gl/mathgl/mgl64"
+	"github.com/go-gl/mathgl/mgl64"
 )
 
-type ForceVector struct{
-	Moment   mgl64.Vec3
-	Force    mgl64.Vec3
+type ForceVector struct {
+	Moment mgl64.Vec3
+	Force  mgl64.Vec3
 }
 
-func (f *ForceVector) Dot (other MotionVector) float64{
+func (f *ForceVector) Dot(other MotionVector) float64 {
 	return f.Moment.Dot(other.Angular) + f.Force.Dot(other.Linear)
 }
 
-func (m *ForceVector) SetZero(){
-	m.Moment = mgl64.Vec3{0,0,0}
-	m.Force = mgl64.Vec3{0,0,0}
+func (m *ForceVector) SetZero() {
+	m.Moment = mgl64.Vec3{0, 0, 0}
+	m.Force = mgl64.Vec3{0, 0, 0}
 }
--- kinematics/go-kin/kinmath/spatial/RigidBodyInertia.go
+++ kinematics/go-kin/kinmath/spatial/RigidBodyInertia.go
@@ -1,19 +1,19 @@
 package spatial
 
 import (
-  "github.com/go-gl/mathgl/mgl64"
+	"github.com/go-gl/mathgl/mgl64"
 )
 
-type RigidBodyInertia struct{
-	Inertia  mgl64.Vec3
-	Cog      mgl64.Vec3
-	Mass     mgl64.Vec3
+type RigidBodyInertia struct {
+	Inertia mgl64.Vec3
+	Cog     mgl64.Vec3
+	Mass    mgl64.Vec3
 }
 
 //~ func (r *RigidBodyInertia) Times (other *MotionVector) ForceVector {
-	//~ res := ForceVector{}
-	//~ res.Moment = r.Inertia * other.Angular + r.Cog.cross(other.Linear)
-	//~ res.Force = r.Mass * other.Linear - r.Cog.cross(other.Angular)
-	
-	//~ return res
+//~ res := ForceVector{}
+//~ res.Moment = r.Inertia * other.Angular + r.Cog.cross(other.Linear)
+//~ res.Force = r.Mass * other.Linear - r.Cog.cross(other.Angular)
+
+//~ return res
 //~ }
