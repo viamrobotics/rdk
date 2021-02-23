465966d8697fc2419c2263f6a7424f3b2fcab265
Peter LoVerso <biotinker@gmail.com>
2021-02-23 11:56:30 -0800

    Add eva json file

--- kinematics/kinematic_test.go
+++ kinematics/kinematic_test.go
@@ -21,5 +21,13 @@ func TestAllKinematics(t *testing.T) {
 	if !mat.ApproxEqualThreshold(m1, 0.0000001) {
 		t.Fatalf("Starting 6d position incorrect")
 	}
-
+	
+	newPos := []float64{1.1, 0.1, 1.3, 0, 0, -1}
+	newExpect := mgl64.NewVecNFromData([]float64{69.80961299265694, -35.53086645234494, 674.4093770982129, -84.6760069731363, 8.222778583003974, 6.1125444468247565})
+	m.SetPosition(newPos)
+	m.ForwardPosition()
+	new6d := mgl64.NewVecNFromData(m.Get6dPosition(0))
+	if !new6d.ApproxEqual(newExpect) {
+		t.Fatalf("Calculated 6d position incorrect")
+	}
 }
