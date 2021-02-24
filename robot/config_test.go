6b1df8033207ccf68ba36fbfd8d4b2ec5c49f8ea
Eliot Horowitz <eliothorowitz@gmail.com>
2021-02-24 10:09:02 -0500

    can't run board code on non-board machines

--- robot/config_test.go
+++ robot/config_test.go
@@ -27,9 +27,6 @@ func TestConfig1(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t, 1, len(cfg.Boards))
-	assert.Equal(t, "38", cfg.Boards[0].Motors[0].Pins["b"])
-
 	r, err := NewRobot(cfg)
 	if err != nil {
 		t.Fatal(err)
@@ -48,6 +45,16 @@ func TestConfig1(t *testing.T) {
 
 	assert.Equal(t, fmt.Sprintf("a%sb%sc", os.Getenv("HOME"), os.Getenv("HOME")), cfg.Components[0].Attributes["bar"])
 
+}
+
+func TestConfig2(t *testing.T) {
+	cfg, err := ReadConfig("data/cfgtest2.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 1, len(cfg.Boards))
+	assert.Equal(t, "38", cfg.Boards[0].Motors[0].Pins["b"])
 }
 
 func TestConfigFake(t *testing.T) {
