d833e5b130084d9b75e83f976d2b3f9c40b7457b
Alexa Greenberg <alexa.greenberg@viam.com>
2021-02-26 17:11:03 -0500

    webcam now supports intel camera, seems to work pretty well

--- vision/pc_test.go
+++ vision/pc_test.go
@@ -3,6 +3,8 @@ package vision
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPC1(t *testing.T) {
@@ -25,4 +27,29 @@ func TestPC1(t *testing.T) {
 	defer file.Close()
 
 	pc.ToPCD(file)
+}
+
+func TestPCRoundTrip(t *testing.T) {
+	pc, err := NewPointCloud("chess/data/board1.png", "chess/data/board1.dat.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.MkdirAll("out", 0775)
+
+	fn := "out/roundtrip1.both.gz"
+	err = pc.WriteTo(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pc2, err := NewPointCloudFromBoth(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, pc.Color.Width(), pc2.Color.Width())
+	assert.Equal(t, pc.Color.Height(), pc2.Color.Height())
+	assert.Equal(t, pc.Depth.Width(), pc2.Depth.Width())
+	assert.Equal(t, pc.Depth.Height(), pc2.Depth.Height())
 }
