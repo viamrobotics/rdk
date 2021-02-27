73e632e1b8b7cc33a34faf714eec9eb6f1699e3d
Alexa Greenberg <alexa.greenberg@viam.com>
2021-02-26 16:41:40 -0800

    Add nlopt IK implementation, update nlopt go lib to work on 32-bit CPUs

--- kinematics/nloptInverseKinematics_test.go
+++ kinematics/nloptInverseKinematics_test.go
@@ -1,10 +1,25 @@
 package kinematics
 
-import (
-	"testing"
-)
+//~ import (
+	//~ "testing"
 
-func TestCreateIKSolver(t *testing.T) {
-	ik := CreateIKSolver(&Model{})
-	ik.Solve()
-}
+	//~ "github.com/edaniels/test"
+	//~ "go.viam.com/robotcore/arm"
+	//~ "go.viam.com/robotcore/testutils"
+//~ )
+
+//~ func TestCreateIKSolver(t *testing.T) {
+	//~ m, err := ParseJSONFile(testutils.ResolveFile("kinematics/models/mdl/wx250s_test.json"))
+	//~ test.That(t, err, test.ShouldBeNil)
+	//~ ik := CreateNloptIKSolver(m)
+	
+	//~ pos := arm.Position{0, -368, 355, 0, 0, 10}
+	//~ transform := kinmath.NewTransformFromRotation(pos.Rx, pos.Ry, pos.Rz)
+	//~ transform.SetX(pos.X)
+	//~ transform.SetY(pos.Y)
+	//~ transform.SetZ(pos.Z)
+
+	//~ k.ik.AddGoal(transform, k.effectorID)
+	
+	//~ ik.Solve()
+//~ }
