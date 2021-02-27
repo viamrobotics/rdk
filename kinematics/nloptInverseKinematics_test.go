d8f788b8e51a225249a5877a6039f92079f9b0d9
Alexa Greenberg <alexa.greenberg@viam.com>
2021-02-26 17:03:09 -0800

    gofmt

--- kinematics/nloptInverseKinematics_test.go
+++ kinematics/nloptInverseKinematics_test.go
@@ -1,25 +1,25 @@
 package kinematics
 
 //~ import (
-	//~ "testing"
+//~ "testing"
 
-	//~ "github.com/edaniels/test"
-	//~ "go.viam.com/robotcore/arm"
-	//~ "go.viam.com/robotcore/testutils"
+//~ "github.com/edaniels/test"
+//~ "go.viam.com/robotcore/arm"
+//~ "go.viam.com/robotcore/testutils"
 //~ )
 
 //~ func TestCreateIKSolver(t *testing.T) {
-	//~ m, err := ParseJSONFile(testutils.ResolveFile("kinematics/models/mdl/wx250s_test.json"))
-	//~ test.That(t, err, test.ShouldBeNil)
-	//~ ik := CreateNloptIKSolver(m)
-	
-	//~ pos := arm.Position{0, -368, 355, 0, 0, 10}
-	//~ transform := kinmath.NewTransformFromRotation(pos.Rx, pos.Ry, pos.Rz)
-	//~ transform.SetX(pos.X)
-	//~ transform.SetY(pos.Y)
-	//~ transform.SetZ(pos.Z)
+//~ m, err := ParseJSONFile(testutils.ResolveFile("kinematics/models/mdl/wx250s_test.json"))
+//~ test.That(t, err, test.ShouldBeNil)
+//~ ik := CreateNloptIKSolver(m)
 
-	//~ k.ik.AddGoal(transform, k.effectorID)
-	
-	//~ ik.Solve()
+//~ pos := arm.Position{0, -368, 355, 0, 0, 10}
+//~ transform := kinmath.NewTransformFromRotation(pos.Rx, pos.Ry, pos.Rz)
+//~ transform.SetX(pos.X)
+//~ transform.SetY(pos.Y)
+//~ transform.SetZ(pos.Z)
+
+//~ k.ik.AddGoal(transform, k.effectorID)
+
+//~ ik.Solve()
 //~ }
