d838af901cbae201f5d3566c94920bd5a8cd2064
Eliot Horowitz <eliothorowitz@gmail.com>
2021-02-26 20:15:02 -0500

    setup script for rpi 4

---
+++ etc/rpi4.sh
@@ -0,0 +1,37 @@
+sudo systemctl enable ssh
+sudo apt install cheese streamer wiringpi git autoconf swig yasm libpython2.7-dev cmake
+
+cd /usr/local
+sudo wget https://golang.org/dl/go1.16.linux-armv6l.tar.gz
+sudo tar zxvf go1.16.linux-armv6l.tar.gz
+sudo rm go1.16.linux-armv6l.tar.gz
+sudo ln -s /usr/local/go/bin/* /usr/local/bin/
+
+cd ~
+mkdir stuff
+cd stuff
+
+git clone https://github.com/sarfata/pi-blaster
+cd pi-blaster
+./autogen.sh
+./configure
+make -j 4
+sudo make install
+
+cd ~/stuff
+wget -q --no-check-certificate https://codeload.github.com/stevengj/nlopt/tar.gz/v2.7.0
+tar -xzvf v2.7.0 && rm -f v2.7.0
+cd nlopt-2.7.0 && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j 4 && sudo make install
+
+cd ~/stuff
+sudo apt-get -y remove wiringpi
+git clone https://github.com/WiringPi/WiringPi.git
+cd WiringPi
+./build
+
+cd ~/stuff
+git clone https://github.com/webmproject/libvpx.git
+cd libvpx
+cd build
+../configure --enable-runtime-cpu-detect --enable-vp8 --enable-postproc --enable-multi-res-encoding --enable-webm-io --enable-better-hw-compatibility --enable-onthefly-bitpacking --enable-pic
+sudo make -j4 install
