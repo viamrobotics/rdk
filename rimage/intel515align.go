9e46e080f21472dcd0016cb66a9629c8bf5a6a6b
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-02 14:20:04 -0500

    alignment not working, progress

--- rimage/intel515align.go
+++ rimage/intel515align.go
@@ -1,17 +1,33 @@
 package rimage
 
 import (
+	"fmt"
 	"image"
+	"time"
 )
 
 type Intel515Align struct {
 }
 
 func (i *Intel515Align) Align(ii *ImageWithDepth) (*ImageWithDepth, error) {
-	minX := 0
+
+	if false {
+		err := ii.WriteTo(fmt.Sprintf("data/align-test-%d.both.gz", time.Now().Unix()))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if ii.Color.Width() != 1280 || ii.Color.Height() != 720 ||
+		ii.Depth.Width() != 1024 || ii.Depth.Height() != 768 {
+		return nil, fmt.Errorf("unexpected intel dimensions c:(%d,%d) d:(%d,%d)",
+			ii.Color.Width(), ii.Color.Height(), ii.Depth.Width(), ii.Depth.Height())
+	}
+
+	minX := 100
 	minY := 120
 
-	maxX := 900
+	maxX := 950
 	maxY := 680
 
 	m := GetPerspectiveTransform(
