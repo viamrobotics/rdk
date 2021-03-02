eb49a8a35c5a56f7b76eecc96e61caa12cc167d2
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-02 15:18:55 -0800

    Goformat

--- kinematics/nloptInverseKinematics_test.go
+++ kinematics/nloptInverseKinematics_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 
 	"github.com/edaniels/test"
-	"go.viam.com/robotcore/testutils"
 	"go.viam.com/robotcore/kinematics/kinmath"
+	"go.viam.com/robotcore/testutils"
 )
 
 type Position struct {
@@ -18,7 +18,7 @@ func TestCreateIKSolver(t *testing.T) {
 	m, err := ParseJSONFile(testutils.ResolveFile("kinematics/models/mdl/wx250s_test.json"))
 	test.That(t, err, test.ShouldBeNil)
 	ik := CreateNloptIKSolver(m)
-	
+
 	pos := Position{0, -365, 360.25, 0, 0, 0}
 	transform := kinmath.NewTransformFromRotation(pos.Rx, pos.Ry, pos.Rz)
 	transform.SetX(pos.X)
@@ -26,9 +26,9 @@ func TestCreateIKSolver(t *testing.T) {
 	transform.SetZ(pos.Z)
 
 	ik.AddGoal(transform, 0)
-	
-	m.SetPosition([]float64{1,1,1,0,0,1})
-	
+
+	m.SetPosition([]float64{1, 1, 1, 0, 0, 1})
+
 	solved := ik.Solve()
 	test.That(t, solved, test.ShouldBeTrue)
 }
