1a04bd4bde25b94034f0ba075a1e37a2640b92e5
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-09 10:58:44 -0500

    moving depth and stream camera into appropriate places

---
+++ rimage/cmd/depth/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"go.viam.com/robotcore/rimage"
+)
+
+func main() {
+
+	hardMin := flag.Int("min", 0, "min depth")
+	hardMax := flag.Int("max", int(rimage.MaxDepth), "max depth")
+
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		panic("need two args <in> <out>")
+	}
+
+	var dm *rimage.DepthMap
+	var pc *rimage.ImageWithDepth
+	var err error
+
+	fn := flag.Arg(0)
+	if strings.HasSuffix(fn, ".both.gz") {
+		pc, err = rimage.BothReadFromFile(fn)
+		if pc != nil {
+			dm = pc.Depth
+		}
+	} else {
+		dm, err = rimage.ParseDepthMap(flag.Arg(0))
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	img := dm.ToPrettyPicture(rimage.Depth(*hardMin), rimage.Depth(*hardMax))
+	if err := rimage.WriteImageToFile(flag.Arg(1), img); err != nil {
+		panic(err)
+	}
+
+	if pc != nil {
+		fn2 := flag.Arg(1) + "-color.png"
+		fmt.Println(fn2)
+		err = pc.Color.WriteTo(fn2)
+		if err != nil {
+			panic(err)
+		}
+
+	}
+}
