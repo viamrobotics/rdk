4efa62b145ba2c7f1cb13986b82a373bc6326c15
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-10 14:45:40 -0500

    make pointcloud point api simpler, maybe a tiny bit slower, but not noticable

--- rimage/pcd.go
+++ rimage/pcd.go
@@ -2,7 +2,10 @@ package rimage
 
 import (
 	"fmt"
+	"image/color"
 	"io"
+
+	"go.viam.com/robotcore/pointcloud"
 )
 
 func _colorToInt(c Color) int {
@@ -21,11 +24,11 @@ func _scale(x, max int) float32 {
 	res := float32(x) / float32(max)
 	return res - .5
 }
-func (pc *ImageWithDepth) ToPCD(out io.Writer) error {
-	if pc.Depth.Width() != pc.Color.Width() ||
-		pc.Depth.Height() != pc.Color.Height() {
+func (iwd *ImageWithDepth) ToPCD(out io.Writer) error {
+	if iwd.Depth.Width() != iwd.Color.Width() ||
+		iwd.Depth.Height() != iwd.Color.Height() {
 		return fmt.Errorf("depth map and color dimensions don't match %d,%d -> %d,%d",
-			pc.Depth.Width(), pc.Depth.Height(), pc.Color.Width(), pc.Color.Height())
+			iwd.Depth.Width(), iwd.Depth.Height(), iwd.Color.Width(), iwd.Color.Height())
 	}
 
 	_, err := fmt.Fprintf(out, "VERSION .7\n"+
@@ -38,29 +41,29 @@ func (pc *ImageWithDepth) ToPCD(out io.Writer) error {
 		"VIEWPOINT 0 0 0 1 0 0 0\n"+
 		"POINTS %d\n"+
 		"DATA ascii\n",
-		pc.Depth.Width()*pc.Depth.Height(),
-		1, //pc.Depth.Height(),
-		pc.Depth.Width()*pc.Depth.Height(),
+		iwd.Depth.Width()*iwd.Depth.Height(),
+		1, //iwd.Depth.Height(),
+		iwd.Depth.Width()*iwd.Depth.Height(),
 	)
 
 	if err != nil {
 		return err
 	}
 
-	min, max := pc.Depth.MinMax()
+	min, max := iwd.Depth.MinMax()
 	scale := float32(max - min)
 
-	for x := 0; x < pc.Depth.Width(); x++ {
-		for y := 0; y < pc.Depth.Height(); y++ {
-			height := pc.Depth.GetDepth(x, y)
+	for x := 0; x < iwd.Depth.Width(); x++ {
+		for y := 0; y < iwd.Depth.Height(); y++ {
+			height := iwd.Depth.GetDepth(x, y)
 			diff := float32(height - min)
 			scaledHeight := diff / scale
 
 			_, err = fmt.Fprintf(out, "%f %f %f %d\n",
-				_scale(x, pc.Depth.Width()),
-				_scale(y, pc.Depth.Width()),
+				_scale(x, iwd.Depth.Width()),
+				_scale(y, iwd.Depth.Width()),
 				scaledHeight,
-				_colorToInt(pc.Color.GetXY(x, y)))
+				_colorToInt(iwd.Color.GetXY(x, y)))
 			if err != nil {
 				return err
 			}
@@ -68,4 +71,26 @@ func (pc *ImageWithDepth) ToPCD(out io.Writer) error {
 	}
 
 	return nil
+}
+
+func (iwd *ImageWithDepth) ToPointCloud() (*pointcloud.PointCloud, error) {
+	pc := pointcloud.New()
+
+	height := iwd.Height()
+	width := iwd.Width()
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			z := iwd.Depth.GetDepth(x, y)
+			if z == 0 {
+				continue
+			}
+			c := iwd.Color.GetXY(x, y)
+			r, g, b := c.RGB255()
+			err := pc.Set(pointcloud.NewColoredPoint(x, y, int(z), color.NRGBA{r, g, b, 255}))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return pc, nil
 }
