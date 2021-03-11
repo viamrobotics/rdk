63245d8af1e45708daa947f6af5e208344ab84d3
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-11 15:53:35 -0500

    move base to api

--- testutils/inject/base.go
+++ testutils/inject/base.go
@@ -3,11 +3,11 @@ package inject
 import (
 	"context"
 
-	"go.viam.com/robotcore/base"
+	"go.viam.com/robotcore/api"
 )
 
 type Base struct {
-	base.Device
+	api.Base
 	MoveStraightFunc func(ctx context.Context, distanceMillis int, millisPerSec float64, block bool) error
 	SpinFunc         func(ctx context.Context, angleDeg float64, speed int, block bool) error
 	WidthMillisFunc  func(ctx context.Context) (int, error)
@@ -17,35 +17,35 @@ type Base struct {
 
 func (b *Base) MoveStraight(ctx context.Context, distanceMillis int, millisPerSec float64, block bool) error {
 	if b.MoveStraightFunc == nil {
-		return b.Device.MoveStraight(ctx, distanceMillis, millisPerSec, block)
+		return b.Base.MoveStraight(ctx, distanceMillis, millisPerSec, block)
 	}
 	return b.MoveStraightFunc(ctx, distanceMillis, millisPerSec, block)
 }
 
 func (b *Base) Spin(ctx context.Context, angleDeg float64, speed int, block bool) error {
 	if b.SpinFunc == nil {
-		return b.Device.Spin(ctx, angleDeg, speed, block)
+		return b.Base.Spin(ctx, angleDeg, speed, block)
 	}
 	return b.SpinFunc(ctx, angleDeg, speed, block)
 }
 
 func (b *Base) WidthMillis(ctx context.Context) (int, error) {
 	if b.WidthMillisFunc == nil {
-		return b.Device.WidthMillis(ctx)
+		return b.Base.WidthMillis(ctx)
 	}
 	return b.WidthMillisFunc(ctx)
 }
 
 func (b *Base) Stop(ctx context.Context) error {
 	if b.StopFunc == nil {
-		return b.Device.Stop(ctx)
+		return b.Base.Stop(ctx)
 	}
 	return b.StopFunc(ctx)
 }
 
 func (b *Base) Close(ctx context.Context) error {
 	if b.CloseFunc == nil {
-		return b.Device.Close(ctx)
+		return b.Base.Close(ctx)
 	}
 	return b.CloseFunc(ctx)
 }
