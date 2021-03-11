4c541b9bf22617c25f7d52584915f0c4dfc4d6ae
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-11 14:12:38 -0500

    more tests

--- api/config_test.go
+++ api/config_test.go
@@ -45,6 +45,12 @@ func TestConfig3(t *testing.T) {
 	assert.Equal(t, 5, cfg.Components[0].Attributes.GetInt("foo", 0))
 	assert.Equal(t, true, cfg.Components[0].Attributes.GetBool("foo2", false))
 	assert.Equal(t, false, cfg.Components[0].Attributes.GetBool("foo3", false))
+	assert.Equal(t, true, cfg.Components[0].Attributes.GetBool("xxxx", true))
+	assert.Equal(t, false, cfg.Components[0].Attributes.GetBool("xxxx", false))
+	assert.Equal(t, "no", cfg.Components[0].Attributes.GetString("foo4"))
+	assert.Equal(t, "", cfg.Components[0].Attributes.GetString("xxxx"))
+	assert.Equal(t, true, cfg.Components[0].Attributes.Has("foo"))
+	assert.Equal(t, false, cfg.Components[0].Attributes.Has("xxxx"))
 
 	bb := cfg.Components[0].Attributes["bar"]
 	b := bb.(*temp)
