c3b1c5d7e9a8ffef665ae66a780c4633c159eb9b
Bijan Haney <bijanhaney@gmail.com>
2021-03-12 11:34:13 -0500

    Update setup.sh
    fixes indent

--- etc/setup.sh
+++ etc/setup.sh
@@ -21,7 +21,7 @@ if [ "$(uname)" = "Darwin" ]; then
     exit 1
   fi
 	brew install libvpx x264 pkgconfig
-    export PKG_CONFIG_PATH=/System/Library/Frameworks/Python.framework/Versions/2.7/lib/pkgconfig/:$PKG_CONFIG_PATH
+        export PKG_CONFIG_PATH=/System/Library/Frameworks/Python.framework/Versions/2.7/lib/pkgconfig/:$PKG_CONFIG_PATH
 fi
 
 NLOPT_OK=1
