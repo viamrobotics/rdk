97488ddb681bfc39a94fb09b8e42cd4bb4ad3ccf
Eric Daniels <eric@erdaniels.com>
2021-03-13 11:03:54 -0500

    start testing compass client

--- sensor/compass/cmd/client/main.go
+++ sensor/compass/cmd/client/main.go
@@ -1,34 +1,66 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"flag"
-	"os"
-	"os/signal"
 	"time"
 
 	"go.viam.com/robotcore/sensor/compass"
+	"go.viam.com/robotcore/utils"
 
 	"github.com/edaniels/golog"
 )
 
+var logger = golog.Global
+
 func main() {
+	utils.ContextualMainQuit(mainWithArgs)
+}
+
+func mainWithArgs(ctx context.Context, args []string) error {
+	parsed, err := parseFlags(args)
+	if err != nil {
+		return err
+	}
+
+	return readCompass(ctx, parsed.DeviceAddress)
+}
+
+// Arguments for the command (parsed).
+type Arguments struct {
+	DeviceAddress string
+}
+
+const (
+	deviceFlagName = "device"
+)
+
+func parseFlags(args []string) (Arguments, error) {
+	cmdLine := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	var buf bytes.Buffer
+	cmdLine.SetOutput(&buf)
+
 	var address string
-	flag.StringVar(&address, "device", "", "device address")
-	flag.Parse()
+	cmdLine.StringVar(&address, deviceFlagName, "", "device address")
+	if err := cmdLine.Parse(args[1:]); err != nil {
+		return Arguments{}, err
+	}
 
 	if len(address) == 0 {
-		flag.Usage()
-		os.Exit(1)
+		cmdLine.Usage()
+		return Arguments{}, errors.New(buf.String())
 	}
 
-	sensor, err := compass.NewWSDevice(context.Background(), address)
+	return Arguments{DeviceAddress: address}, nil
+}
+
+func readCompass(ctx context.Context, deviceAddress string) error {
+	sensor, err := compass.NewWSDevice(ctx, deviceAddress)
 	if err != nil {
-		golog.Global.Fatal(err)
+		return err
 	}
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
 
 	tickRate := 100 * time.Millisecond
 	ticker := time.NewTicker(tickRate)
@@ -36,24 +68,25 @@ func main() {
 
 	count := 0
 	start := time.Now()
-READ:
+	defer func() {
+		logger.Infow("stats", "rate", float64(count)/time.Since(start).Seconds())
+	}()
 	for {
 		select {
-		case <-sig:
-			break READ
+		case <-ctx.Done():
+			return nil
 		default:
 		}
 		select {
-		case <-sig:
-			break READ
+		case <-ctx.Done():
+			return nil
 		case <-ticker.C:
 		}
 		heading, err := sensor.Heading(context.Background())
 		if err != nil {
-			golog.Global.Errorw("failed to get sensor heading", "error", err)
+			logger.Errorw("failed to get sensor heading", "error", err)
 			continue
 		}
-		golog.Global.Infow("heading", "data", heading)
+		logger.Infow("heading", "data", heading)
 	}
-	golog.Global.Infow("stats", "rate", float64(count)/time.Since(start).Seconds())
 }
