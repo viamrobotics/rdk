a6f4b57e3c3a4d08eb0a26ce14d1e16df4a6a8f0
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-15 17:03:53 -0400

    interrupt test

---
+++ board/interrupts_test.go
@@ -0,0 +1,31 @@
+package board
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicDigitalInterrupt1(t *testing.T) {
+	config := DigitalInterruptConfig{
+		Formula: "(+ 1 raw)",
+	}
+
+	i, err := createDigitalInterrupt(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, int64(1), i.Value())
+	i.Tick()
+	assert.Equal(t, int64(2), i.Value())
+
+	c := make(chan int64)
+	i.AddCallbackDelta(1, c)
+
+	go func() { i.Tick() }()
+
+	v := <-c
+	assert.Equal(t, int64(2), v)
+
+}
