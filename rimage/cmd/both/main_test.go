6e63a729f4447c61242d139add3486d72d60aff8
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-16 15:23:26 -0400

    testing main for both cmd

---
+++ rimage/cmd/both/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBothMain(t *testing.T) {
+	assert.Error(t, realMain([]string{}))
+	assert.Error(t, realMain([]string{"merge"}))
+	assert.Error(t, realMain([]string{"merge", "x"}))
+	assert.Error(t, realMain([]string{"to-las"}))
+	assert.Error(t, realMain([]string{"to-las", "x"}))
+	assert.Error(t, realMain([]string{"xxx"}))
+
+	os.MkdirAll("out", 0775)
+
+	out := "out/board1.both.gz"
+	err := realMain([]string{"merge", "../../data/board1.png", "../../data/board1.dat.gz", out})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out2 := "out/board1.las"
+	err = realMain([]string{"to-las", out, out2})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s, err := os.Stat(out2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Less(t, int64(0), s.Size())
+}
