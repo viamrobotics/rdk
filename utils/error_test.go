bf87920c255f495861a4bf9dd7f1a0b9fd2fef39
Eric Daniels <eric@erdaniels.com>
2021-03-16 09:44:48 -0700

    Add TestFilterOutError

---
+++ utils/error_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/edaniels/test"
+	"go.uber.org/multierr"
+)
+
+func TestFilterOutError(t *testing.T) {
+	test.That(t, FilterOutError(nil, nil), test.ShouldBeNil)
+	err1 := errors.New("error1")
+	test.That(t, FilterOutError(err1, nil), test.ShouldEqual, err1)
+	test.That(t, FilterOutError(err1, err1), test.ShouldBeNil)
+	err2 := errors.New("error2")
+	test.That(t, FilterOutError(err1, err2), test.ShouldEqual, err1)
+	err3 := errors.New("error") // substring
+	test.That(t, FilterOutError(err1, err3), test.ShouldBeNil)
+	err4 := errors.New("error4")
+	errM := multierr.Combine(err1, err2, err4)
+	test.That(t, FilterOutError(errM, err2), test.ShouldResemble, multierr.Combine(err1, err4))
+	test.That(t, FilterOutError(errM, err3), test.ShouldBeNil)
+}
