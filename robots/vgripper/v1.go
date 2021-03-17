bb44fe3051cf6328580b8872b40c27b830f772a8
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-17 11:30:06 -0400

    remove potentiometer from v1 gripper

--- robots/vgripper/v1.go
+++ robots/vgripper/v1.go
@@ -11,12 +11,13 @@ import (
 )
 
 type GripperV1 struct {
-	motor         board.Motor
-	potentiometer board.AnalogReader
-	pressure      board.AnalogReader
+	motor       board.Motor
+	encoder     board.DigitalInterrupt
+	current     board.AnalogReader
+	pressure    board.AnalogReader
+	limitSwitch board.AnalogReader
 
-	potentiometerOpen   int
-	potentiometerClosed int
+	encoderGap int64
 
 	defaultSpeed byte
 
@@ -26,18 +27,25 @@ type GripperV1 struct {
 func NewGripperV1(theBoard board.Board) (*GripperV1, error) {
 
 	vg := &GripperV1{
-		motor:         theBoard.Motor("g"),
-		potentiometer: theBoard.AnalogReader("potentiometer"),
-		pressure:      theBoard.AnalogReader("pressure"),
-		defaultSpeed:  16,
+		motor:        theBoard.Motor("g"),
+		encoder:      theBoard.DigitalInterrupt("encoder"),
+		current:      theBoard.AnalogReader("current"),
+		pressure:     theBoard.AnalogReader("pressure"),
+		limitSwitch:  theBoard.AnalogReader("limit"),
+		defaultSpeed: 16,
 	}
 
 	if vg.motor == nil {
 		return nil, fmt.Errorf("gripper needs a motor named 'g'")
 	}
-
-	if vg.potentiometer == nil || vg.pressure == nil {
-		return nil, fmt.Errorf("gripper needs a potentiometer and a pressure reader")
+	if vg.encoder == nil {
+		return nil, fmt.Errorf("gripper needs an encoder named 'encoder'")
+	}
+	if vg.current == nil || vg.pressure == nil {
+		return nil, fmt.Errorf("gripper needs a current and a pressure reader")
+	}
+	if vg.limitSwitch == nil {
+		return nil, fmt.Errorf("gripper needs a limit switch")
 	}
 
 	// pick a direction and move till it stops
@@ -55,92 +63,97 @@ func NewGripperV1(theBoard board.Board) (*GripperV1, error) {
 		return nil, fmt.Errorf("pressure same open and closed, something is wrong potentiometer: %d %d", sideA, sideB)
 	}
 
+	vg.encoderGap = utils.AbsInt64(sideB - sideA)
+
 	if hasPressureA {
 		vg.closeDirection = board.DirForward
 		vg.openDirection = board.DirBackward
-		vg.potentiometerOpen = sideB
-		vg.potentiometerClosed = sideA
 	} else {
 		vg.closeDirection = board.DirBackward
 		vg.openDirection = board.DirForward
-		vg.potentiometerOpen = sideA
-		vg.potentiometerClosed = sideB
 	}
 
 	return vg, vg.Open()
 }
 
 func (vg *GripperV1) Open() error {
-	err := vg.motor.Go(vg.openDirection, vg.defaultSpeed)
-	if err != nil {
-		return err
-	}
-
-	msPer := 10
-	total := 0
-	for {
-		time.Sleep(time.Duration(msPer) * time.Millisecond)
-		now, err := vg.readPotentiometer()
+	_, _, err := vg.moveInDirectionTillWontMoveMore(vg.openDirection)
+	return err
+	/*
+		err := vg.motor.Go(vg.openDirection, vg.defaultSpeed)
 		if err != nil {
 			return err
 		}
-		if vg.potentiometerSame(now, vg.potentiometerOpen) {
-			return vg.Stop()
-		}
 
-		total += msPer
-		if total > 5000 {
-			err = vg.Stop()
-			return fmt.Errorf("open timed out, wanted: %d at: %d stop error: %s", vg.potentiometerOpen, now, err)
-		}
-	}
+		msPer := 10
+		total := 0
+		for {
+			time.Sleep(time.Duration(msPer) * time.Millisecond)
+			now, err := vg.readPotentiometer()
+			if err != nil {
+				return err
+			}
+			if vg.potentiometerSame(now, vg.potentiometerOpen) {
+				return vg.Stop()
+			}
 
+			total += msPer
+			if total > 5000 {
+				err = vg.Stop()
+				return fmt.Errorf("open timed out, wanted: %d at: %d stop error: %s", vg.potentiometerOpen, now, err)
+			}
+		}
+	*/
 }
 
 func (vg *GripperV1) Grab() (bool, error) {
-	err := vg.motor.Go(vg.closeDirection, vg.defaultSpeed)
-	if err != nil {
-		return false, err
-	}
+	_, _, err := vg.moveInDirectionTillWontMoveMore(vg.closeDirection)
+	return false, err
 
-	msPer := 10
-	total := 0
-	for {
-		time.Sleep(time.Duration(msPer) * time.Millisecond)
-		now, err := vg.readPotentiometer()
+	/*
+		err := vg.motor.Go(vg.closeDirection, vg.defaultSpeed)
 		if err != nil {
 			return false, err
 		}
 
-		if vg.potentiometerSame(now, vg.potentiometerClosed) {
-			// we fully closed
-			return false, vg.Stop()
-		}
-
-		pressure, err := vg.hasPressure()
-		if err != nil {
-			return false, err
-		}
-
-		if pressure {
-			// don't turn motor off, keep pressure being applied
-			return true, nil
-		}
-
-		total += msPer
-		if total > 5000 {
-			err = vg.Stop()
+		msPer := 10
+		total := 0
+		for {
+			time.Sleep(time.Duration(msPer) * time.Millisecond)
+			now, err := vg.readPotentiometer()
 			if err != nil {
 				return false, err
 			}
-			pressureRaw, err := vg.readPressure()
+
+			if vg.potentiometerSame(now, vg.potentiometerClosed) {
+				// we fully closed
+				return false, vg.Stop()
+			}
+
+			pressure, err := vg.hasPressure()
 			if err != nil {
 				return false, err
 			}
-			return false, fmt.Errorf("close timed out, wanted: %d at: %d pressure: %d", vg.potentiometerOpen, now, pressureRaw)
-		}
-	}
 
+			if pressure {
+				// don't turn motor off, keep pressure being applied
+				return true, nil
+			}
+
+			total += msPer
+			if total > 5000 {
+				err = vg.Stop()
+				if err != nil {
+					return false, err
+				}
+				pressureRaw, err := vg.readPressure()
+				if err != nil {
+					return false, err
+				}
+				return false, fmt.Errorf("close timed out, wanted: %d at: %d pressure: %d", vg.potentiometerOpen, now, pressureRaw)
+			}
+		}
+	*/
 }
 
 func (vg *GripperV1) Close() error {
@@ -151,12 +164,12 @@ func (vg *GripperV1) Stop() error {
 	return vg.motor.Off()
 }
 
-func (vg *GripperV1) readPotentiometer() (int, error) {
-	return vg.potentiometer.Read()
+func (vg *GripperV1) readCurrent() (int, error) {
+	return vg.current.Read()
 }
 
-func (vg *GripperV1) potentiometerSame(a, b int) bool {
-	return utils.AbsInt(b-a) < 5
+func (vg *GripperV1) encoderSame(a, b int64) bool {
+	return utils.AbsInt64(b-a) < 5
 }
 
 func (vg *GripperV1) readPressure() (int, error) {
@@ -168,7 +181,36 @@ func (vg *GripperV1) hasPressure() (bool, error) {
 	return p < 1000, err
 }
 
-func (vg *GripperV1) moveInDirectionTillWontMoveMore(dir board.Direction) (int, bool, error) {
+func (vg *GripperV1) readLimitSwitch() (int, error) {
+	return vg.limitSwitch.Read()
+}
+
+func (vg *GripperV1) limitSwitched() (bool, error) {
+	v, err := vg.readLimitSwitch()
+	return v > 800, err
+}
+
+// return hasPressure, current, limitSwitch
+func (vg *GripperV1) analogs() (hasPressure bool, current int, limitSwitch bool, err error) {
+	hasPressure, err = vg.hasPressure()
+	if err != nil {
+		return
+	}
+
+	current, err = vg.readCurrent()
+	if err != nil {
+		return
+	}
+
+	limitSwitch, err = vg.limitSwitched()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (vg *GripperV1) moveInDirectionTillWontMoveMore(dir board.Direction) (int64, bool, error) {
 	defer func() {
 		err := vg.Stop()
 		if err != nil {
@@ -181,28 +223,34 @@ func (vg *GripperV1) moveInDirectionTillWontMoveMore(dir board.Direction) (int, 
 		return -1, false, err
 	}
 
-	last, err := vg.readPotentiometer()
+	last := vg.encoder.Value()
 	if err != nil {
 		return -1, false, err
 	}
 
-	time.Sleep(300 * time.Millisecond)
+	time.Sleep(2000 * time.Millisecond)
 
 	for {
-		now, err := vg.readPotentiometer()
+		now := vg.encoder.Value()
 		if err != nil {
 			return -1, false, err
 		}
 
-		golog.Global.Debugf("dir: %s last: %v now: %v", dir, last, now)
-		if vg.potentiometerSame(last, now) {
+		hasPressure, _, limitSwitched, err := vg.analogs()
+		if err != nil {
+			return -1, false, err
+		}
+
+		golog.Global.Debugf("dir: %v last: %v now: %v hasPressure: %v limitSwitched: %v",
+			dir, last, now, hasPressure, limitSwitched)
+
+		if vg.encoderSame(last, now) || hasPressure || limitSwitched {
 			// increase power temporarily
 			err := vg.motor.Force(128)
 			if err != nil {
 				return -1, false, err
 			}
 			time.Sleep(500 * time.Millisecond)
-			hasPressure, err := vg.hasPressure()
 			return now, hasPressure, err
 		}
 		last = now
