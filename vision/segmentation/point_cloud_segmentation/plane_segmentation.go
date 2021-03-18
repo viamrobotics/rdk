ca78bc54b6ef1f348692d4b6beece5e8b41f05eb
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-18 14:47:45 -0400

    Changes for PR
    - removed print statement in depth_map.go
    - Added Sensor parameter structs in a new calibration module
    - added json file containing Intel 515 calibration parameters
    - Moved pixel-point transforms to the calibration module
    - renamed Meter2MillimeterPoint to Meter2DepthUnit for better readability
    - Fixed tests accordingly
    - Sorted imports
    - gofmt files

--- vision/segmentation/point_cloud_segmentation/plane_segmentation.go
+++ vision/segmentation/point_cloud_segmentation/plane_segmentation.go
@@ -1,23 +1,19 @@
 package point_cloud_segmentation
 
 import (
-	"github.com/golang/geo/r3"
-	pc "go.viam.com/robotcore/pointcloud"
-	"go.viam.com/robotcore/rimage"
-	"gonum.org/v1/gonum/mat"
 	"image"
 	"image/color"
 	"math"
 	"math/rand"
-	"time"
+
+	"github.com/golang/geo/r3"
+	pc "go.viam.com/robotcore/pointcloud"
+	"go.viam.com/robotcore/rimage"
+	cal "go.viam.com/robotcore/vision/calibration"
 )
 
-type PlaneSegmenter interface {
-	SegmentPlane(int, float64, float64) (*pc.PointCloud, []float64)
-}
-
 type Points3D struct {
-	points []r3.Vector
+	Points []r3.Vector
 }
 
 func New3DPoints() *Points3D {
@@ -25,11 +21,11 @@ func New3DPoints() *Points3D {
 	return &Points3D{pts}
 }
 
-// Convert float 3d points in meters to pointcloud
+// Convert float 3d Points in meters to pointcloud
 func (pts *Points3D) convert3DPointsToPointCloud(pixel2Meter float64) *pc.PointCloud {
 	pointCloud := pc.New()
-	for _, pt := range pts.points {
-		x, y, z := MeterToMillimeterPoint(pt.X, pt.Y, pt.Z, pixel2Meter)
+	for _, pt := range pts.Points {
+		x, y, z := MeterToDepthUnit(pt.X, pt.Y, pt.Z, pixel2Meter)
 		ptPc := pc.NewBasicPoint(x, y, z)
 		pointCloud.Set(ptPc)
 	}
@@ -41,8 +37,8 @@ func (pts *Points3D) convert3DPointsToPointCloudWithValue(
 	selectedPoints map[pc.Vec3]int,
 ) *pc.PointCloud {
 	pointCloud := pc.New()
-	for _, pt := range pts.points {
-		x, y, z := MeterToMillimeterPoint(pt.X, pt.Y, pt.Z, pixel2Meter)
+	for _, pt := range pts.Points {
+		x, y, z := MeterToDepthUnit(pt.X, pt.Y, pt.Z, pixel2Meter)
 		val := 0
 		if _, ok := selectedPoints[pc.Vec3{x, y, z}]; ok {
 			val = 1
@@ -53,46 +49,40 @@ func (pts *Points3D) convert3DPointsToPointCloudWithValue(
 	return pointCloud
 }
 
-// Function to segment the biggest plane in the 3D points cloud
+// Function to segment the biggest plane in the 3D Points cloud
 // nIterations is the number of iteration for ransac
 // threshold is the float64 value for the maximum allowed distance to the found plane for a point to belong to it
 // pixel2meter is the conversion factor from the depth value to its value in meters
 // This function returns a pointcloud with values; the values are set to 1 if a point belongs to the plane, 0 otherwise
 func (pts *Points3D) SegmentPlane(nIterations int, threshold, pixel2meter float64) (*pc.PointCloud, []float64) {
-	points3d := pts.points
-	nPoints := len(points3d)
+	nPoints := len(pts.Points)
 	bestEquation := make([]float64, 4)
 	currentEquation := make([]float64, 4)
 	var bestInliers []r3.Vector
 
-	var currentInliers []r3.Vector
-	var p1, p2, p3, v1, v2, cross, vec r3.Vector
-	var n1, n2, n3 int
-	var d, dist float64
-
 	for i := 0; i < nIterations; i++ {
-		// sample 3 points from the slice of 3D points
-		n1, n2, n3 = SampleRandomIntRange(1, nPoints-1), SampleRandomIntRange(1, nPoints-1), SampleRandomIntRange(1, nPoints-1)
-		p1, p2, p3 = points3d[n1], points3d[n2], points3d[n3]
+		// sample 3 Points from the slice of 3D Points
+		n1, n2, n3 := SampleRandomIntRange(1, nPoints-1), SampleRandomIntRange(1, nPoints-1), SampleRandomIntRange(1, nPoints-1)
+		p1, p2, p3 := pts.Points[n1], pts.Points[n2], pts.Points[n3]
 
 		// get 2 vectors that are going to define the plane
-		v1 = p2.Sub(p1)
-		v2 = p3.Sub(p1)
+		v1 := p2.Sub(p1)
+		v2 := p3.Sub(p1)
 		// cross product to get the normal unit vector to the plane (v1, v2)
-		cross = v1.Cross(v2)
-		vec = cross.Normalize()
+		cross := v1.Cross(v2)
+		vec := cross.Normalize()
 		// find current plane equation denoted as:
 		// cross[0]*x + cross[1]*y + cross[2]*z + d = 0
 		// to find d, we just need to pick a point and deduce d from the plane equation (vec orth to p1, p2, p3)
-		d = -vec.Dot(p2)
+		d := -vec.Dot(p2)
 		// current plane equation
 		currentEquation[0], currentEquation[1], currentEquation[2], currentEquation[3] = vec.X, vec.Y, vec.Z, d
 
 		// compute distance to plane of each point in the cloud
-		currentInliers = nil
-		// store all the points that are below a certain distance to the plane
-		for _, pt := range points3d {
-			dist = (currentEquation[0]*pt.X + currentEquation[1]*pt.Y + currentEquation[2]*pt.Z + currentEquation[3]) / vec.Norm()
+		currentInliers := make([]r3.Vector, 0)
+		// store all the Points that are below a certain distance to the plane
+		for _, pt := range pts.Points {
+			dist := (currentEquation[0]*pt.X + currentEquation[1]*pt.Y + currentEquation[2]*pt.Z + currentEquation[3]) / vec.Norm()
 			if math.Abs(dist) < threshold {
 				currentInliers = append(currentInliers, pt)
 			}
@@ -107,7 +97,7 @@ func (pts *Points3D) SegmentPlane(nIterations int, threshold, pixel2meter float6
 	// Output as slice of r3.Vector
 	bestInliersPointCloud := make(map[pc.Vec3]int)
 	for _, pt := range bestInliers {
-		x, y, z := MeterToMillimeterPoint(pt.X, pt.Y, pt.Z, pixel2meter)
+		x, y, z := MeterToDepthUnit(pt.X, pt.Y, pt.Z, pixel2meter)
 		bestInliersPointCloud[pc.Vec3{x, y, z}] = 1
 	}
 	return pts.convert3DPointsToPointCloudWithValue(pixel2meter, bestInliersPointCloud), bestEquation
@@ -119,9 +109,10 @@ func (pts *Points3D) SegmentPlane(nIterations int, threshold, pixel2meter float6
 // fx, fy (Focal Length)            : 900.538, 900.818
 //Distortion Coefficients : [0.158701,-0.485405,-0.00143327,-0.000705919,0.435342]
 // depthMin and DepthMax are supposed to contain the range of depth values for which the confidence is high
-func (pts *Points3D) CreatePoints3DFromDepthMap(depthImage *rimage.DepthMap, pixel2meter, cx, cy, fx, fy float64, depthMin, depthMax rimage.Depth) {
+func CreatePoints3DFromDepthMap(depthImage *rimage.DepthMap, pixel2meter float64, params cal.PinholeCameraIntrinsics, depthMin, depthMax rimage.Depth) *Points3D {
 	// TODO(louise): add distortion model for better accuracy
-	// go through depth map pixels and get 3D points
+	// go through depth map pixels and get 3D Points
+	pts := New3DPoints()
 
 	for y := 0; y < depthImage.Height(); y++ {
 		for x := 0; x < depthImage.Width(); x++ {
@@ -131,19 +122,20 @@ func (pts *Points3D) CreatePoints3DFromDepthMap(depthImage *rimage.DepthMap, pix
 				// get z distance to meter for unit uniformity
 				z := float64(d) * pixel2meter
 				// get x and y of 3D point
-				x_, y_, _ := PixelToPoint(x, y, z, cx, cy, fx, fy)
+				x_, y_, _ := params.PixelToPoint(x, y, z)
 				// Get point in PointCloud format
-				pts.points = append(pts.points, r3.Vector{x_, y_, z})
+				pts.Points = append(pts.Points, r3.Vector{x_, y_, z})
 			}
 		}
 	}
+	return pts
 }
 
 // Function to project 3D point in a given camera image plane
-func (pts *Points3D) ProjectPlane3dPointsToRGBPlane(h, w int, cx, cy, fx, fy, pixel2meter float64) []r3.Vector {
+func (pts *Points3D) ProjectPlane3dPointsToRGBPlane(h, w int, params cal.PinholeCameraIntrinsics, pixel2meter float64) []r3.Vector {
 	var coordinates []r3.Vector
-	for _, pt := range pts.points {
-		j, i := PointToPixel(pt.X, pt.Y, pt.Z, cx, cy, fx, fy)
+	for _, pt := range pts.Points {
+		j, i := params.PointToPixel(pt.X, pt.Y, pt.Z)
 		j = math.Round(j)
 		i = math.Round(i)
 		// if point is inside the RGB image bounds, add it to the coordinate list
@@ -166,19 +158,19 @@ func IsPixelInImage(i, j, h, w int) bool {
 }
 
 // Function to project 3D point in a given camera image plane
-func (pts *Points3D) ApplyRigidBodyTransform(rotationMatrix mat.Dense, translationVector r3.Vector) *Points3D {
+func (pts *Points3D) ApplyRigidBodyTransform(params *cal.Extrinsics) *Points3D {
 	transformedPoints := New3DPoints()
-	for _, pt := range pts.points {
-		ptTransformed := TransformPointToPoint(pt.X, pt.Y, pt.Z, rotationMatrix, translationVector)
-		transformedPoints.points = append(transformedPoints.points, ptTransformed)
+	for _, pt := range pts.Points {
+		ptTransformed := params.TransformPointToPoint(pt.X, pt.Y, pt.Z)
+		transformedPoints.Points = append(transformedPoints.Points, ptTransformed)
 	}
 	return transformedPoints
 }
 
-// utils for 3D float points transforms
+// utils for 3D float Points transforms
 
 // Convert point from meters (float64) to mm (int)
-func MeterToMillimeterPoint(x, y, z float64, pixel2Meter float64) (int, int, int) {
+func MeterToDepthUnit(x, y, z float64, pixel2Meter float64) (int, int, int) {
 	if pixel2Meter < 0.0000001 {
 		panic("pixel2Meter is too close to zero to make the conversion from meters to millimeters.")
 	}
@@ -188,56 +180,16 @@ func MeterToMillimeterPoint(x, y, z float64, pixel2Meter float64) (int, int, int
 	return xMm, yMm, zMm
 }
 
-// Function to transform a pixel with depth to a 3D point cloud
-// the intrinsics parameters should be the ones of the sensor used to obtain the image that contains the pixel
-func PixelToPoint(x, y int, z float64, cx, cy, fx, fy float64) (float64, float64, float64) {
-	//TODO(louise): add unit test
-	xOverZ := (cx - float64(x)) / fx
-	yOverZ := (cy - float64(y)) / fy
-	// get x and y
-	x_ := xOverZ * z
-	y_ := yOverZ * z
-	return x_, y_, z
-}
-
-// Function to project a 3D point to a pixel in an image plane
-// the intrinsics parameters should be the ones of the sensor we want to project to
-func PointToPixel(x, y, z float64, cx, cy, fx, fy float64) (float64, float64) {
-	//TODO(louise): add unit test
-	if z != 0. {
-		x_px := math.Round(x*fx/(z) + cx)
-		y_px := math.Round(y*fy/(z) + cy)
-		return x_px, y_px
-	}
-	// if depth is zero at this pixel, return negative coordinates so that the cropping to RGB bounds will filter it out
-	return -1.0, -1.0
-}
-
-// Function to apply a rigid body transform between two cameras to a 3D point
-func TransformPointToPoint(x, y, z float64, rotationMatrix mat.Dense, translationVector r3.Vector) r3.Vector {
-	r, c := rotationMatrix.Dims()
-	if r != 3 || c != 3 {
-		panic("Rotation Matrix to transform point cloud should be a 3x3 matrix")
-	}
-	x_transformed := rotationMatrix.At(0, 0)*x + rotationMatrix.At(0, 1)*y + rotationMatrix.At(0, 2)*z + translationVector.X
-	y_transformed := rotationMatrix.At(1, 0)*x + rotationMatrix.At(1, 1)*y + rotationMatrix.At(1, 2)*z + translationVector.Y
-	z_transformed := rotationMatrix.At(2, 0)*x + rotationMatrix.At(2, 1)*y + rotationMatrix.At(2, 2)*z + translationVector.Z
-
-	return r3.Vector{x_transformed, y_transformed, z_transformed}
-}
-
-// Function to sample a random integer within a range given by [mib, max]
+// Function to sample a random integer within a range given by [min, max]
 func SampleRandomIntRange(min, max int) int {
-	// reset seed at every sampling operation
-	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(max-min+1) + min
 }
 
 // Convert Depth map to point cloud (units in mm to get int coordinates) as defined in pointcloud/pointcloud.go
-func DepthMapToPointCloud(depthImage *rimage.DepthMap, pixel2meter, cx, cy, fx, fy float64) *pc.PointCloud {
+func DepthMapToPointCloud(depthImage *rimage.DepthMap, pixel2meter float64, params cal.PinholeCameraIntrinsics) *pc.PointCloud {
 	// create new point cloud
 	pc_ := pc.New()
-	// go through depth map pixels and get 3D points
+	// go through depth map pixels and get 3D Points
 	for y := 0; y < depthImage.Height(); y++ {
 		for x := 0; x < depthImage.Width(); x++ {
 			// get depth value
@@ -245,7 +197,7 @@ func DepthMapToPointCloud(depthImage *rimage.DepthMap, pixel2meter, cx, cy, fx, 
 			// get z distance to meter for unit uniformity
 			z := float64(d) * pixel2meter
 			// get x and y of 3D point
-			x_, y_, z := PixelToPoint(x, y, z, cx, cy, fx, fy)
+			x_, y_, z := params.PixelToPoint(x, y, z)
 			// Get point in PointCloud format
 			x_int := int(math.Round(x_ / pixel2meter))
 			y_int := int(math.Round(y_ / pixel2meter))
