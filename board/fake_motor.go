714e1197bbbbbce1bda2dc12753c895a235ee100
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-21 19:34:37 -0400

    refactor entire web app into vuew make api.Status encompass all info

---
+++ board/fake_motor.go
@@ -0,0 +1,34 @@
+package board
+
+import (
+	"fmt"
+)
+
+type FakeMotor struct {
+	force byte
+	d     Direction
+}
+
+func (m *FakeMotor) Force(force byte) error {
+	m.force = force
+	return nil
+}
+
+func (m *FakeMotor) Go(d Direction, force byte) error {
+	m.d = d
+	m.force = force
+	return nil
+}
+
+func (m *FakeMotor) GoFor(d Direction, rpm float64, rotations float64) error {
+	return fmt.Errorf("should not be called")
+}
+
+func (m *FakeMotor) Off() error {
+	m.d = DirNone
+	return nil
+}
+
+func (m *FakeMotor) IsOn() bool {
+	return m.d != DirNone && m.force > 0
+}
