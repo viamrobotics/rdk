9006717051940510c6f1f9f806c40fa480c40286
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-21 19:34:37 -0400

    refactor entire web app into vuew make api.Status encompass all info

---
+++ board/status.go
@@ -0,0 +1,69 @@
+package board
+
+import (
+	"fmt"
+)
+
+type MotorStatus struct {
+	On bool
+}
+
+type ServoStatus struct {
+	Angle uint8
+}
+
+type AnalogStatus struct {
+	Value int
+}
+
+type DigitalInterruptStatus struct {
+	Value int64
+}
+
+type Status struct {
+	Motors            map[string]MotorStatus
+	Servos            map[string]ServoStatus
+	Analogs           map[string]AnalogStatus
+	DigitalInterrupts map[string]DigitalInterruptStatus
+}
+
+func CreateStatus(b Board) (Status, error) {
+	s := Status{
+		Motors:            map[string]MotorStatus{},
+		Servos:            map[string]ServoStatus{},
+		Analogs:           map[string]AnalogStatus{},
+		DigitalInterrupts: map[string]DigitalInterruptStatus{},
+	}
+
+	cfg := b.GetConfig()
+
+	for _, c := range cfg.Motors {
+		name := c.Name
+		x := b.Motor(name)
+		s.Motors[name] = MotorStatus{On: x.IsOn()}
+	}
+
+	for _, c := range cfg.Servos {
+		name := c.Name
+		x := b.Servo(name)
+		s.Servos[name] = ServoStatus{x.Current()}
+	}
+
+	for _, c := range cfg.Analogs {
+		name := c.Name
+		x := b.AnalogReader(name)
+		val, err := x.Read()
+		if err != nil {
+			return s, fmt.Errorf("couldn't read analog (%s) : %s", name, err)
+		}
+		s.Analogs[name] = AnalogStatus{val}
+	}
+
+	for _, c := range cfg.DigitalInterrupts {
+		name := c.Name
+		x := b.DigitalInterrupt(name)
+		s.DigitalInterrupts[name] = DigitalInterruptStatus{x.Value()}
+	}
+
+	return s, nil
+}
