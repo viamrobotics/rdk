c5d7f74841a172e502ed27aff27190d133def7b3
Alexa Greenberg <alexa.greenberg@viam.com>
2021-03-22 16:36:14 -0400

    Test lidar ImageSource

--- lidar/image_source.go
+++ lidar/image_source.go
@@ -3,41 +3,34 @@ package lidar
 import (
 	"context"
 	"image"
-	"image/color"
-
-	"go.viam.com/robotcore/utils"
 
 	"github.com/fogleman/gg"
-	"gonum.org/v1/gonum/mat"
+	"go.viam.com/robotcore/rimage"
 )
 
 // ImageSource generates images from the current scan of a lidar device
 type ImageSource struct {
-	device    Device
-	scaleDown int // scale down amount
-	noFilter  bool
+	device        Device
+	unitsPerMeter int
+	noFilter      bool
 }
 
-const scaleDown = 100 // centimeters
+const unitsPerMeter = 100 // centimeters
 
 func NewImageSource(device Device) *ImageSource {
-	return &ImageSource{device: device, scaleDown: scaleDown}
+	return &ImageSource{device: device, unitsPerMeter: unitsPerMeter}
 }
 
-func NewImageSourceNoFiltering(device Device) *ImageSource {
-	return &ImageSource{device: device, scaleDown: scaleDown, noFilter: true}
-}
-
-var first *mat.Dense
+var red = rimage.Red
 
 func (is *ImageSource) Next(ctx context.Context) (image.Image, func(), error) {
 	bounds, err := is.device.Bounds(ctx)
 	if err != nil {
 		return nil, nil, err
 	}
-	scaleDown := is.scaleDown
-	bounds.X *= scaleDown
-	bounds.Y *= scaleDown
+	unitsPerMeter := is.unitsPerMeter
+	bounds.X *= unitsPerMeter
+	bounds.Y *= unitsPerMeter
 	centerX := bounds.X / 2
 	centerY := bounds.Y / 2
 
@@ -48,33 +41,11 @@ func (is *ImageSource) Next(ctx context.Context) (image.Image, func(), error) {
 		return nil, nil, err
 	}
 
-	if first == nil {
-		measureMat := mat.NewDense(3, len(measurements), nil)
-		for i, next := range measurements {
-			x, y := next.Coords()
-			measureMat.Set(0, i, x)
-			measureMat.Set(1, i, y)
-			measureMat.Set(2, i, 1)
-		}
-		first = measureMat
-	}
-
 	for _, next := range measurements {
 		x, y := next.Coords()
-		x = float64(centerX) + (x * float64(scaleDown))
-		y = float64(centerY) + (y * float64(scaleDown))
-		dc.SetColor(color.RGBA{255, 0, 0, 255})
-		dc.SetPixel(int(x), int(y))
-	}
-
-	rotMat := (*mat.Dense)((*utils.Vec2Matrix)(first).RotateMatrixAbout(float64(0), float64(0), 90))
-	_, numCols := rotMat.Dims()
-	for i := 0; i < numCols; i++ {
-		x := rotMat.At(0, i)
-		y := rotMat.At(1, i)
-		x = float64(centerX) + (x * float64(scaleDown))
-		y = float64(centerY) + (y * float64(scaleDown))
-		dc.SetColor(color.RGBA{0, 255, 0, 255})
+		x = float64(centerX) + (x * float64(unitsPerMeter))
+		y = float64(centerY) + (y * float64(unitsPerMeter))
+		dc.SetColor(red)
 		dc.SetPixel(int(x), int(y))
 	}
 
