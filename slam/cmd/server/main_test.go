cf74a95c209d6bdd5b852cc3866b023ac9c18d0c
Eric Daniels <eric@erdaniels.com>
2021-03-23 09:21:14 -0400

    Test SLAM server

---
+++ slam/cmd/server/main_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"image"
+	"math"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+
+	"go.uber.org/zap/zaptest/observer"
+	"go.viam.com/robotcore/lidar"
+	"go.viam.com/robotcore/robots/fake"
+	"go.viam.com/robotcore/sensor/compass"
+	"go.viam.com/robotcore/testutils"
+	"go.viam.com/robotcore/testutils/inject"
+	"go.viam.com/robotcore/utils"
+
+	"github.com/edaniels/golog"
+	"github.com/edaniels/test"
+	"github.com/edaniels/wsapi"
+)
+
+func TestMain(t *testing.T) {
+	listener, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	port := listener.Addr().(*net.TCPAddr).Port
+	wsAddr := fmt.Sprintf("ws://127.0.0.1:%d", port)
+	httpServer := &http.Server{
+		Addr:           listener.Addr().String(),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	wsServer := wsapi.NewServer()
+	wsServer.RegisterCommand(compass.WSCommandHeading, wsapi.CommandHandlerFunc(func(ctx context.Context, cmd *wsapi.Command) (interface{}, error) {
+		return 23.45, nil
+	}))
+	httpServer.Handler = wsServer.HTTPHandler()
+	go func() {
+		httpServer.Serve(listener)
+	}()
+	defer httpServer.Close()
+
+	lidar.RegisterDeviceType("fail_info", lidar.DeviceTypeRegistration{
+		New: func(ctx context.Context, desc lidar.DeviceDescription) (lidar.Device, error) {
+			dev := &inject.LidarDevice{Device: &fake.Lidar{}}
+			dev.InfoFunc = func(ctx context.Context) (map[string]interface{}, error) {
+				return nil, errors.New("whoops")
+			}
+			return dev, nil
+		},
+	})
+	lidar.RegisterDeviceType("fail_width", lidar.DeviceTypeRegistration{
+		New: func(ctx context.Context, desc lidar.DeviceDescription) (lidar.Device, error) {
+			dev := &inject.LidarDevice{Device: &fake.Lidar{}}
+			dev.BoundsFunc = func(ctx context.Context) (image.Point, error) {
+				return image.Point{}, errors.New("whoops")
+			}
+			return dev, nil
+		},
+	})
+	lidar.RegisterDeviceType("fail_ang", lidar.DeviceTypeRegistration{
+		New: func(ctx context.Context, desc lidar.DeviceDescription) (lidar.Device, error) {
+			dev := &inject.LidarDevice{Device: &fake.Lidar{}}
+			dev.AngularResolutionFunc = func(ctx context.Context) (float64, error) {
+				return math.NaN(), errors.New("whoops")
+			}
+			return dev, nil
+		},
+	})
+	lidar.RegisterDeviceType("fail_stop", lidar.DeviceTypeRegistration{
+		New: func(ctx context.Context, desc lidar.DeviceDescription) (lidar.Device, error) {
+			dev := &inject.LidarDevice{Device: &fake.Lidar{}}
+			dev.StopFunc = func(ctx context.Context) error {
+				return errors.New("whoops")
+			}
+			return dev, nil
+		},
+	})
+
+	before := func(t *testing.T, tLogger golog.Logger, exec *testutils.ContextualMainExecution) {
+		logger = tLogger
+		randomPort, err := utils.TryReserveRandomPort()
+		test.That(t, err, test.ShouldBeNil)
+		defaultPort = randomPort
+	}
+	testutils.TestMain(t, mainWithArgs, []testutils.MainTestCase{
+		// parsing
+		{"no args", nil, "", before, nil, func(t *testing.T, logs *observer.ObservedLogs) {
+			test.That(t, logs.FilterMessageSnippet(`lidar "0"`).All(), test.ShouldHaveLength, 1)
+		}},
+		{"bad port", []string{"ten"}, "invalid syntax", before, nil, nil},
+		{"too big port", []string{"65536"}, "out of range", before, nil, nil},
+		{"unknown named arg", []string{"--unknown"}, "not defined", before, nil, nil},
+		{"bad lidar", []string{"--lidar=foo"}, "format", before, nil, nil},
+		{"bad offset", []string{"--lidar-offset=foo"}, "format", before, nil, nil},
+		{"lidar and offset mismatch", []string{"--lidar=fake,one", "--lidar=fake,two", "--lidar-offset=1,2,3", "--lidar-offset=1,2,4"}, "have up to", before, nil, nil},
+
+		// running
+		{"bad lidar device type", []string{"--lidar=foo,blah"}, "do not know how", before, nil, nil},
+		{"bad lidar device info", []string{"--lidar=fail_info,zero"}, "whoops", before, nil, nil},
+		{"bad lidar device width", []string{"--lidar=fail_width,zero"}, "whoops", before, nil, nil},
+		{"bad lidar device ang res", []string{"--lidar=fail_ang,zero"}, "whoops", before, nil, nil},
+		{"bad lidar device stop", []string{"--lidar=fail_stop,zero"}, "whoops", before, nil, nil},
+		{"bad compass", []string{"--compass=ws"}, "scheme", before, nil, nil},
+		{"normal", []string{"--lidar=fake,1"}, "", before, nil, nil},
+		{"normal with compass", []string{"--compass=" + wsAddr}, "", before, nil, nil},
+	})
+}
