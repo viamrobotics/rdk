e36ca79150015b3ad3c40dc10425ee7d59cd69cb
Bijan Haney <bijan@viam.com>
2021-03-24 14:31:13 -0400

    changes location of some tests

--- rimage/image_align_test.go
+++ rimage/image_align_test.go
@@ -105,3 +105,57 @@ func TestAlignImage(t *testing.T) {
 	}
 
 }
+
+func TestTransformPointToPoint(t *testing.T) {
+	x1, y1, z1 := 0., 0., 1.
+	rot1 := []float64{1, 0, 0, 0, 1, 0, 0, 0, 1}
+
+	t1 := []float64{0, 0, 1}
+	// Get rigid body transform between Depth and RGB sensor
+	extrinsics1 := Extrinsics{
+		RotationMatrix:    rot1,
+		TranslationVector: t1,
+	}
+	vt1 := extrinsics1.TransformPointToPoint(x1, y1, z1)
+	if vt1.X != 0. {
+		t.Error("x value for I rotation and {0,0,1} translation is not 0.")
+	}
+	if vt1.Y != 0. {
+		t.Error("y value for I rotation and {0,0,1} translation is not 0.")
+	}
+	if vt1.Z != 2. {
+		t.Error("z value for I rotation and {0,0,1} translation is not 2.")
+	}
+
+	t2 := []float64{0, 2, 0}
+	extrinsics2 := Extrinsics{
+		RotationMatrix:    rot1,
+		TranslationVector: t2,
+	}
+	vt2 := extrinsics2.TransformPointToPoint(x1, y1, z1)
+	if vt2.X != 0. {
+		t.Error("x value for I rotation and {0,2,0} translation is not 0.")
+	}
+	if vt2.Y != 2. {
+		t.Error("y value for I rotation and {0,2,0} translation is not 2.")
+	}
+	if vt2.Z != 1. {
+		t.Error("z value for I rotation and {0,2,0} translation is not 1.")
+	}
+	// Rotation in the (z,x) plane of 90 degrees
+	rot2 := []float64{0, 0, 1, 0, 1, 0, 0, 0, -1}
+	extrinsics3 := Extrinsics{
+		RotationMatrix:    rot2,
+		TranslationVector: t2,
+	}
+	vt3 := extrinsics3.TransformPointToPoint(x1, y1, z1)
+	if vt3.X != 1. {
+		t.Error("x value for rotation z->x and {0,2,0} translation is not 1.")
+	}
+	if vt3.Y != 2. {
+		t.Error("y value for rotation z->x and {0,2,0} translation is not 2.")
+	}
+	if vt3.Z != -1. {
+		t.Error("z value for rotation z->x and {0,2,0} translation is not 0.")
+	}
+}
