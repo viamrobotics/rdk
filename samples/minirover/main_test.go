c9c046a1dd0ebfae35a0a1deb12871e6d3d56684
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-24 17:01:10 -0400

    segmentation too slow on this data

--- samples/minirover/main_test.go
+++ samples/minirover/main_test.go
@@ -7,19 +7,12 @@ import (
 	"github.com/edaniels/golog"
 
 	"go.viam.com/robotcore/rimage"
-	"go.viam.com/robotcore/vision/segmentation"
 )
 
 type ChargeDebug struct {
 }
 
 func (cd ChargeDebug) Process(d *rimage.MultipleImageTestDebugger, fn string, img image.Image, logger golog.Logger) error {
-	m2, err := segmentation.ShapeWalkEntireDebug(rimage.ConvertImage(img), segmentation.ShapeWalkOptions{}, logger)
-	if err != nil {
-		return err
-	}
-	d.GotDebugImage(m2, "segments")
-
 	return nil
 }
 
