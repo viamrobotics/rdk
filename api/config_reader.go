74ebc8671552ff7a38ddc7aaff2ab97e6eb45602
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-26 10:03:15 -0400

    load attribuets of a robot config from a different file

--- api/config_reader.go
+++ api/config_reader.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path"
+	"strings"
 )
 
 type AttributeConverter func(val interface{}) (interface{}, error)
@@ -30,6 +33,42 @@ func findRegisterd(comptype ComponentType, model, attr string) *registered {
 	return nil
 }
 
+func findPath(original, fn string) (*os.File, error) {
+	f, err := os.Open(fn)
+	if err == nil {
+		return f, nil
+	}
+
+	fn2 := path.Join(path.Dir(original), fn)
+	f, err = os.Open(fn2)
+	if err == nil {
+		return f, nil
+	}
+
+	return nil, fmt.Errorf("cannot find file: %s", fn)
+}
+
+func loadSubFromFile(original, cmd string) (interface{}, bool, error) {
+	if !strings.HasPrefix(cmd, "$load{") {
+		return cmd, false, nil
+	}
+
+	cmd = cmd[6:]
+	cmd = cmd[0 : len(cmd)-1]
+
+	f, err := findPath(original, cmd)
+	if err != nil {
+		return cmd, false, err
+	}
+	defer f.Close()
+
+	sub := map[string]interface{}{}
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&sub)
+
+	return sub, true, err
+}
+
 func ReadConfig(fn string) (Config, error) {
 	cfg := Config{}
 
@@ -50,6 +89,14 @@ func ReadConfig(fn string) (Config, error) {
 			s, ok := v.(string)
 			if ok {
 				cfg.Components[idx].Attributes[k] = os.ExpandEnv(s)
+				loaded := false
+				v, loaded, err = loadSubFromFile(fn, s)
+				if err != nil {
+					return cfg, err
+				}
+				if loaded {
+					cfg.Components[idx].Attributes[k] = v
+				}
 			}
 
 			r := findRegisterd(c.Type, c.Model, k)
