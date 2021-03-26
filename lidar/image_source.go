2603dc9c99f0be257ffc2e5fcd8f243652a9c405
Eliot Horowitz <eliothorowitz@gmail.com>
2021-03-26 13:15:25 -0400

    lidar image auto scales and puts 0 degres on top

--- lidar/image_source.go
+++ lidar/image_source.go
@@ -3,6 +3,7 @@ package lidar
 import (
 	"context"
 	"image"
+	"math"
 
 	"github.com/fogleman/gg"
 	"go.viam.com/robotcore/rimage"
@@ -10,6 +11,7 @@ import (
 
 // ImageSource generates images from the current scan of a lidar device
 type ImageSource struct {
+	Size          image.Point
 	device        Device
 	unitsPerMeter int
 	noFilter      bool
@@ -18,40 +20,57 @@ type ImageSource struct {
 const unitsPerMeter = 100 // centimeters
 
 func NewImageSource(device Device) *ImageSource {
-	return &ImageSource{device: device, unitsPerMeter: unitsPerMeter}
+	return &ImageSource{Size: image.Point{800, 800}, device: device, unitsPerMeter: unitsPerMeter}
 }
 
 var red = rimage.Red
 
 func (is *ImageSource) Next(ctx context.Context) (image.Image, func(), error) {
-	bounds, err := is.device.Bounds(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	unitsPerMeter := is.unitsPerMeter
-	bounds.X *= unitsPerMeter
-	bounds.Y *= unitsPerMeter
-	centerX := bounds.X / 2
-	centerY := bounds.Y / 2
-
-	dc := gg.NewContext(bounds.X, bounds.Y)
-
 	measurements, err := is.device.Scan(ctx, ScanOptions{NoFilter: is.noFilter})
 	if err != nil {
 		return nil, nil, err
 	}
 
-	for _, next := range measurements {
-		x, y := next.Coords()
-		x = float64(centerX) + (x * float64(unitsPerMeter))
-		y = float64(centerY) + (y * float64(unitsPerMeter))
-		dc.SetColor(red)
-		dc.SetPixel(int(x), int(y))
-	}
-
-	return dc.Image(), func() {}, nil
+	return MeasurementsToImage(measurements, is.Size), func() {}, nil
 }
 
 func (is *ImageSource) Close() error {
 	return nil
+}
+
+func MeasurementsToImage(measurements Measurements, size image.Point) image.Image {
+
+	if size.X != size.Y {
+		panic("size has to be square")
+	}
+	maxDistance := .001
+
+	for _, next := range measurements {
+		if next.Distance() > maxDistance {
+			maxDistance = next.Distance()
+		}
+	}
+
+	// if maxDistance is 10 meters, and size is 100, 100
+	// distance from center to edge is 50 pixels, which means we have
+	// 5 pixels per meter
+	pixelsPerMeter := float64(size.X/2) / maxDistance
+
+	// round up to the next power of 2 to make it less jumpy
+	pixelsPerMeter = math.Pow(2, math.Floor(math.Log2(pixelsPerMeter)))
+
+	centerX := float64(size.X) / 2.0
+	centerY := float64(size.Y) / 2.0
+
+	dc := gg.NewContext(size.X, size.Y)
+
+	for _, next := range measurements {
+		x, y := next.Coords()
+		x = centerX + (x * pixelsPerMeter)
+		y = centerY + (y * -1 * pixelsPerMeter)
+		dc.SetColor(red)
+		dc.SetPixel(int(x), int(y))
+	}
+
+	return dc.Image()
 }
