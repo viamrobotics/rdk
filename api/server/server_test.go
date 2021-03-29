ff893289c75438ef14765adf682a5a4ff4e3be08
Eric Daniels <eric@erdaniels.com>
2021-03-29 19:40:02 -0400

    add TODO

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -30,4 +30,6 @@ func TestServer(t *testing.T) {
 
 	// TODO(erd): more exhaustive test with errors too
 	test.That(t, strings.ReplaceAll(statusResp.GetStatus().String(), " ", ""), test.ShouldResemble, `arms:{key:"arm1"value:{grid_position:{}joint_positions:{degrees:0degrees:0degrees:0degrees:0degrees:0degrees:0}}}bases:{key:"base1"value:true}grippers:{key:"gripper1"value:true}boards:{key:"board1"value:{motors:{key:"g"value:{}}servos:{key:"servo1"value:{}}analogs:{key:"analog1"value:{}}digital_interrupts:{key:"encoder"value:{}}}}`)
+
+	// TODO(erd): test server.StatusStream
 }
