517bed468bd090b916ae510ceb08a112bf0691a4
Eric Daniels <eric@erdaniels.com>
2021-03-29 19:22:01 -0400

    fix test

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -2,6 +2,7 @@ package server_test
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"go.viam.com/robotcore/api"
@@ -28,5 +29,5 @@ func TestServer(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 
 	// TODO(erd): more exhaustive test with errors too
-	test.That(t, statusResp.GetStatus().String(), test.ShouldResemble, `arms:{key:"arm1" value:{grid_position:{} joint_positions:{degrees:0 degrees:0 degrees:0 degrees:0 degrees:0 degrees:0}}} bases:{key:"base1" value:true} grippers:{key:"gripper1" value:true} boards:{key:"board1" value:{motors:{key:"g" value:{}} servos:{key:"servo1" value:{}} analogs:{key:"analog1" value:{}} digital_interrupts:{key:"encoder" value:{}}}}`)
+	test.That(t, strings.ReplaceAll(statusResp.GetStatus().String(), " ", ""), test.ShouldResemble, `arms:{key:"arm1"value:{grid_position:{}joint_positions:{degrees:0degrees:0degrees:0degrees:0degrees:0degrees:0}}}bases:{key:"base1"value:true}grippers:{key:"gripper1"value:true}boards:{key:"board1"value:{motors:{key:"g"value:{}}servos:{key:"servo1"value:{}}analogs:{key:"analog1"value:{}}digital_interrupts:{key:"encoder"value:{}}}}`)
 }
