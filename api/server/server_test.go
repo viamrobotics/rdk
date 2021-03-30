8b4cdaecadd6e409ea9ee4c60046c8822ea974ee
Eric Daniels <eric@erdaniels.com>
2021-03-30 16:47:22 -0400

    Start work on api/server tests

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -2,7 +2,6 @@ package server_test
 
 import (
 	"context"
-	"strings"
 	"testing"
 
 	"go.viam.com/robotcore/api"
@@ -25,15 +24,72 @@ func newServer(t *testing.T) pb.RobotServiceServer {
 	return apiserver.New(myRobot)
 }
 
+var emptyStatus = &pb.StatusResponse{
+	Status: &pb.Status{
+		Arms: map[string]*pb.ArmStatus{
+			"arm1": {
+				GridPosition: &pb.ArmPosition{
+					X:  0.0,
+					Y:  0.0,
+					Z:  0.0,
+					RX: 0.0,
+					RY: 0.0,
+					RZ: 0.0,
+				},
+				JointPositions: &pb.JointPositions{
+					Degrees: []float64{0, 0, 0, 0, 0, 0},
+				},
+			},
+		},
+		Bases: map[string]bool{
+			"base1": true,
+		},
+		Grippers: map[string]bool{
+			"gripper1": true,
+		},
+		Boards: map[string]*pb.BoardStatus{
+			"board1": {
+				Motors: map[string]*pb.MotorStatus{
+					"g": {},
+				},
+				Servos: map[string]*pb.ServoStatus{
+					"servo1": {},
+				},
+				Analogs: map[string]*pb.AnalogStatus{
+					"analog1": {},
+				},
+				DigitalInterrupts: map[string]*pb.DigitalInterruptStatus{
+					"encoder": {},
+				},
+			},
+		},
+	},
+}
+
 func TestServer(t *testing.T) {
 	t.Run("Status", func(t *testing.T) {
 		server := newServer(t)
 		statusResp, err := server.Status(context.Background(), &pb.StatusRequest{})
 		test.That(t, err, test.ShouldBeNil)
+		test.That(t, statusResp, test.ShouldResemble, emptyStatus)
 
-		// TODO(erd): more exhaustive test with errors too
-		test.That(t, strings.ReplaceAll(statusResp.GetStatus().String(), " ", ""), test.ShouldResemble, `arms:{key:"arm1"value:{grid_position:{}joint_positions:{degrees:0degrees:0degrees:0degrees:0degrees:0degrees:0}}}bases:{key:"base1"value:true}grippers:{key:"gripper1"value:true}boards:{key:"board1"value:{motors:{key:"g"value:{}}servos:{key:"servo1"value:{}}analogs:{key:"analog1"value:{}}digital_interrupts:{key:"encoder"value:{}}}}`)
+		newPosition := &pb.ArmPosition{
+			X:  1.0,
+			Y:  2.0,
+			Z:  3.0,
+			RX: 4.0,
+			RY: 5.0,
+			RZ: 6.0,
+		}
+		_, err = server.MoveArmToPosition(context.Background(), &pb.MoveArmToPositionRequest{
+			Name: "arm1",
+			To:   newPosition,
+		})
+		test.That(t, err, test.ShouldBeNil)
+
+		statusResp, err = server.Status(context.Background(), &pb.StatusRequest{})
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, statusResp, test.ShouldNotResemble, emptyStatus)
+		test.That(t, statusResp.Status.Arms["arm1"].GridPosition, test.ShouldResemble, newPosition)
 	})
-
-	// TODO(erd): test server.StatusStream
 }
