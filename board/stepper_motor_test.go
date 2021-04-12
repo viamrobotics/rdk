656a062f7e65a3811cab3e741354898c13490b87
Peter LoVerso <biotinker@gmail.com>
2021-04-12 16:51:48 -0700

    lint

--- board/stepper_motor_test.go
+++ board/stepper_motor_test.go
@@ -9,13 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
-
 func TestStepperMotor(t *testing.T) {
 	ctx := context.Background()
 	b := &testGPIOBoard{}
 
-	m, err := NewGPIOMotor(b, MotorConfig{Pins: map[string]string{"a": "1", "b": "2", "c": "3", "d": "4", "pwm": "5"},TicksPerRotation: 200})
+	m, err := NewGPIOMotor(b, MotorConfig{Pins: map[string]string{"a": "1", "b": "2", "c": "3", "d": "4", "pwm": "5"}, TicksPerRotation: 200})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,16 +26,16 @@ func TestStepperMotor(t *testing.T) {
 	on, err := m.IsOn(ctx)
 	assert.Nil(t, err)
 	assert.False(t, on)
-	
+
 	supported, err := m.PositionSupported(ctx)
 	assert.Nil(t, err)
 	assert.True(t, supported)
-	
+
 	assert.Nil(t, m.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_FORWARD, 50.0, 40.0))
 	steps, err := m.Position(ctx)
 	assert.Nil(t, err)
 	assert.Equal(t, 160.0, steps)
-	
+
 	assert.Nil(t, m.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 50.0, 20.0))
 	steps, err = m.Position(ctx)
 	assert.Nil(t, err)
