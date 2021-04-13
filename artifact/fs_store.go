69c2a58dcd454602b22f1f5b50bb8caf9cb6138f
Eric Daniels <eric@erdaniels.com>
2021-04-12 20:44:19 -0400

    Update fs_store.go

--- artifact/fs_store.go
+++ artifact/fs_store.go
@@ -81,5 +81,5 @@ func (s *fileSystemStore) Emplace(hash, path string) error {
 	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 		return err
 	}
-	return os.Symlink(s.pathToHash(hash), path)
+	return os.Symlink(s.pathToHashFile(hash), path)
 }
