56793b5e68fecc2c5d51d029c6a5f5cdbe4cccb1
Bijan Haney <bijan@viam.com>
2021-04-24 19:21:06 -0400

    edits to point storage

--- slam/point_storage.go
+++ slam/point_storage.go
@@ -2,6 +2,7 @@ package slam
 
 import (
 	"fmt"
+	"image/color"
 	"math"
 
 	"go.viam.com/robotcore/pointcloud"
@@ -9,6 +10,7 @@ import (
 	"github.com/edaniels/golog"
 )
 
+// SLAM stores and retrieves its visited locations as points as defined in pointcloud/point.go.
 type PointStorage interface {
 	Size() int
 	At(x, y float64) pointcloud.Point
@@ -18,6 +20,10 @@ type PointStorage interface {
 	WriteToFile(fn string) error
 }
 
+// RoundingPointCloud is a simple PointStorage implementation that rounds all points to the closest
+// integer before it sets or gets the location. The bare floats measured from devices are not
+// stored because even if the points only 0.00000000002 apart, they would be considered different
+// locations.
 type RoundingPointCloud struct {
 	pc *pointcloud.PointCloud
 }
@@ -27,11 +33,35 @@ func NewRoundingPointCloud() *RoundingPointCloud {
 }
 
 func NewRoundingPointCloudFromFile(fn string, logger golog.Logger) (*RoundingPointCloud, error) {
+	var err error
 	pc, err := pointcloud.NewFromFile(fn, logger)
 	if err != nil {
 		return nil, fmt.Errorf("error creating NewRoundingPointCloudFromFile - %s", err)
 	}
-	return &RoundingPointCloud{pc}, nil
+	// Round all the points in the pointcloud
+	newPc := pointcloud.New()
+	pc.Iterate(func(pt pointcloud.Point) bool {
+		var ptTransformed pointcloud.Point
+		pos := pt.Position()
+		x, y, z := math.Round(pos.X), math.Round(pos.Y), math.Round(pos.Z)
+		if pt.HasValue() {
+			ptTransformed = pointcloud.NewValuePoint(x, y, z, pt.Value())
+		} else if pt.HasColor() {
+			ptTransformed = pointcloud.NewColoredPoint(x, y, z, pt.Color().(color.NRGBA))
+		} else {
+			ptTransformed = pointcloud.NewBasicPoint(x, y, z)
+		}
+		err = newPc.Set(ptTransformed)
+		if err != nil {
+			err = fmt.Errorf("error setting point (%v, %v, %v) in point cloud - %s", x, y, z, err)
+			return false
+		}
+		return true
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &RoundingPointCloud{newPc}, nil
 }
 
 func (cloud *RoundingPointCloud) Size() int {
