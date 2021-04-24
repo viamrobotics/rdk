27bbf23521cf4a7ee6b81945e214a52ec25742c5
James Otting <james@viam.com>
2021-04-23 18:28:48 -0700

    Add templatedir argument to run in non-dev machiens

--- robot/web/options.go
+++ robot/web/options.go
@@ -1,15 +1,17 @@
 package web
 
 type Options struct {
-	AutoTile bool
-	Pprof    bool
-	Port     int
+	AutoTile    bool
+	Pprof       bool
+	Port        int
+	TemplateDir string
 }
 
 func NewOptions() Options {
 	return Options{
-		AutoTile: true,
-		Pprof:    false,
-		Port:     8080,
+		AutoTile:    true,
+		Pprof:       false,
+		Port:        8080,
+		TemplateDir: "",
 	}
 }
