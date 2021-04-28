0e737b1b151dd33914a5ceb0a03b6d2296f86c9c
Bijan Haney <bijan@viam.com>
2021-04-28 12:55:50 -0400

    adds functions for segmentation

--- vision/segmentation/plane_segmentation.go
+++ vision/segmentation/plane_segmentation.go
@@ -1,4 +1,4 @@
-package pointcloudsegmentation
+package segmentation
 
 import (
 	"fmt"
@@ -8,28 +8,31 @@ import (
 
 	pc "go.viam.com/robotcore/pointcloud"
 	"go.viam.com/robotcore/rimage"
+	"go.viam.com/robotcore/rimage/calib"
 	"go.viam.com/robotcore/utils"
 
 	"github.com/golang/geo/r3"
 )
 
 // Extract the positions of the points from the pointcloud into an r3 slice.
-func GetPointCloudPositions(cloud pc.PointCloud) []r3.Vector {
-	positions := make([]r3.Vector, 0, cloud.Size())
+func GetPointCloudPositions(cloud pc.PointCloud) []pc.Vec3 {
+	positions := make([]pc.Vec3, 0, cloud.Size())
 	cloud.Iterate(func(pt pc.Point) bool {
-		positions = append(positions, r3.Vector(pt.Position()))
+		positions = append(positions, pt.Position())
 		return true
 	})
 	return positions
 }
 
 // Return two pointclouds, one with points found in a map of point positions, and the other with those not in the map.
-func PointCloudSplit(cloud pc.PointCloud, inMap map[r3.Vector]bool) (pc.PointCloud, pc.PointCloud, error) {
+func PointCloudSplit(cloud pc.PointCloud, inMap map[pc.Vec3]bool) (pc.PointCloud, pc.PointCloud, error) {
 	mapCloud := pc.New()
 	nonMapCloud := pc.New()
 	var err error
+	seen := make(map[pc.Vec3]bool)
 	cloud.Iterate(func(pt pc.Point) bool {
-		if _, ok := inMap[r3.Vector(pt.Position())]; ok {
+		if _, ok := inMap[pt.Position()]; ok {
+			seen[pt.Position()] = true
 			err = mapCloud.Set(pt)
 		} else {
 			err = nonMapCloud.Set(pt)
@@ -44,26 +47,29 @@ func PointCloudSplit(cloud pc.PointCloud, inMap map[r3.Vector]bool) (pc.PointClo
 	if err != nil {
 		return nil, nil, err
 	}
+	if len(seen) != len(inMap) {
+		err = fmt.Errorf("map of points contains invalid points not found in the point cloud")
+		return nil, nil, err
+	}
 	return mapCloud, nonMapCloud, nil
 }
 
 // Function to segment the biggest plane in the 3D Pointcloud.
 // nIterations is the number of iteration for ransac
 // threshold is the float64 value for the maximum allowed distance to the found plane for a point to belong to it
-// pixel2meter is the conversion factor from the depth value to its value in meters
 // This function returns 2 pointclouds, the pointcloud of the plane and one without the plane
 // It also returns the equation of the found plane
-func SegmentPlane(cloud pc.PointCloud, nIterations int, threshold, pixel2meter float64) (pc.PointCloud, pc.PointCloud, []float64, error) {
+func SegmentPlane(cloud pc.PointCloud, nIterations int, threshold float64) (pc.PointCloud, pc.PointCloud, []float64, error) {
 	pts := GetPointCloudPositions(cloud)
 	nPoints := cloud.Size()
 	bestEquation := make([]float64, 4)
 	currentEquation := make([]float64, 4)
-	bestInliers := make(map[r3.Vector]bool)
+	bestInliers := make(map[pc.Vec3]bool)
 
 	for i := 0; i < nIterations; i++ {
 		// sample 3 Points from the slice of 3D Points
 		n1, n2, n3 := utils.SampleRandomIntRange(1, nPoints-1), utils.SampleRandomIntRange(1, nPoints-1), utils.SampleRandomIntRange(1, nPoints-1)
-		p1, p2, p3 := pts[n1], pts[n2], pts[n3]
+		p1, p2, p3 := r3.Vector(pts[n1]), r3.Vector(pts[n2]), r3.Vector(pts[n3])
 
 		// get 2 vectors that are going to define the plane
 		v1 := p2.Sub(p1)
@@ -79,7 +85,7 @@ func SegmentPlane(cloud pc.PointCloud, nIterations int, threshold, pixel2meter f
 		currentEquation[0], currentEquation[1], currentEquation[2], currentEquation[3] = vec.X, vec.Y, vec.Z, d
 
 		// compute distance to plane of each point in the cloud
-		currentInliers := make(map[r3.Vector]bool)
+		currentInliers := make(map[pc.Vec3]bool)
 		// store all the Points that are below a certain distance to the plane
 		for _, pt := range pts {
 			dist := (currentEquation[0]*pt.X + currentEquation[1]*pt.Y + currentEquation[2]*pt.Z + currentEquation[3]) / vec.Norm()
@@ -98,6 +104,54 @@ func SegmentPlane(cloud pc.PointCloud, nIterations int, threshold, pixel2meter f
 		return nil, nil, nil, err
 	}
 	return planeCloud, nonPlaneCloud, bestEquation, nil
+}
+
+// GetPlanesInPointCloud takes in a point cloud and outputs an array of the plane pointclouds and a point cloud of
+// the leftover points.
+// threshold is the float64 value for the maximum allowed distance to the found plane for a point to belong to it.
+// minPoints is the minimum number of points necessary in the found plane.
+func GetPlanesInPointCloud(cloud pc.PointCloud, threshold float64, minPoints int) ([]pc.PointCloud, pc.PointCloud, error) {
+	planes := make([]pc.PointCloud, 0)
+	var err error
+	planeCloud, nonPlaneCloud, _, err := SegmentPlane(cloud, 1500, threshold)
+	if err != nil {
+		return nil, nil, err
+	}
+	planes = append(planes, planeCloud)
+	for planeCloud.Size() > minPoints {
+		planeCloud, nonPlaneCloud, _, err = SegmentPlane(nonPlaneCloud, 1500, threshold)
+		if err != nil {
+			return nil, nil, err
+		}
+		planes = append(planes, planeCloud)
+	}
+	return planes, nonPlaneCloud, nil
+}
+
+// PointCloudSegmentsToMask takes in an instrinsic camera matrix and a slice of pointclouds and projects
+// each pointcloud down to an image.
+func PointCloudSegmentsToMask(params calib.PinholeCameraIntrinsics, segments []pc.PointCloud) (*SegmentedImage, error) {
+	img := newSegmentedImage(rimage.NewImage(params.Width, params.Height))
+	visitedPoints := make(map[pc.Vec3]bool)
+	var err error
+	for i, cloud := range segments {
+		cloud.Iterate(func(pt pc.Point) bool {
+			pos := pt.Position()
+			if seen := visitedPoints[pos]; seen {
+				err = fmt.Errorf("point clouds in array must be distinct, have already seen point (%v,%v,%v)", pos.X, pos.Y, pos.Z)
+				return false
+			}
+			visitedPoints[pos] = true
+			x, y := params.PointToPixel(pos.X, pos.Y, pos.Z)
+			x, y = math.Round(x), math.Round(y)
+			img.set(image.Point{int(x), int(y)}, i+1)
+			return true
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return img, nil
 }
 
 // utils for 3D float Points transforms
