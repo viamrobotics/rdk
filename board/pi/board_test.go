5e640762d6ab426202fd2f02ce194e4f282016b7
Alexa Greenberg <alexa.greenberg@viam.com>
2021-05-03 17:13:42 -0400

    cleanup rpi test and document better

--- board/pi/board_test.go
+++ board/pi/board_test.go
@@ -3,47 +3,53 @@
 package pi
 
 import (
+	"context"
 	"testing"
 	"time"
 
+	"github.com/edaniels/golog"
 	"github.com/stretchr/testify/assert"
 
 	"go.viam.com/robotcore/board"
+	pb "go.viam.com/robotcore/proto/api/v1"
 )
 
 func TestPiPigpio(t *testing.T) {
+	ctx := context.Background()
+	logger := golog.NewTestLogger(t)
 
 	cfg := board.Config{
-		Analogs: []board.AnalogConfig{{Name: "blue", Pin: "0"}},
+		//Analogs: []board.AnalogConfig{{Name: "blue", Pin: "0"}},
 		Servos: []board.ServoConfig{
-			{Name: "s1", Pin: "16"},
-			{Name: "s2", Pin: "29"},
+			{Name: "servo", Pin: "18"}, // bcom-24
 		},
 		DigitalInterrupts: []board.DigitalInterruptConfig{
-			{Name: "i1", Pin: "35"},
-			{Name: "i2", Pin: "31", Type: "servo"},
-			{Name: "hall-a", Pin: "38"},
-			{Name: "hall-b", Pin: "40"},
+			{Name: "i1", Pin: "11"},                     // plug physical 12(18) into this (17)
+			{Name: "servo-i", Pin: "22", Type: "servo"}, // bcom-25
+			{Name: "hall-a", Pin: "33"},                 // bcom 13
+			{Name: "hall-b", Pin: "37"},                 // bcom 26
 		},
 		Motors: []board.MotorConfig{
 			{
-				Name:             "m",
-				Pins:             map[string]string{"a": "11", "b": "13", "pwm": "15"},
+				Name: "m",
+				Pins: map[string]string{
+					"a":   "13", // bcom 27
+					"b":   "40", // bcom 21
+					"pwm": "7",  // bcom 4
+				},
 				Encoder:          "hall-a",
 				EncoderB:         "hall-b",
-				TicksPerRotation: 100,
+				TicksPerRotation: 200,
 			},
 		},
 	}
 
-	pp, err := NewPigpio(cfg)
+	pp, err := NewPigpio(ctx, cfg, logger)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	p := pp.(*piPigpio)
-
-	assert.Equal(t, cfg, p.GetConfig())
 
 	defer func() {
 		err := p.Close()
@@ -52,10 +58,16 @@ func TestPiPigpio(t *testing.T) {
 		}
 	}()
 
+	cfgGet, err := p.GetConfig(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, cfg, cfgGet)
 	t.Run("analog test", func(t *testing.T) {
 		reader := p.AnalogReader("blue")
 		if reader == nil {
-			t.Fatalf("no blue?")
+			t.Skip("no blue? analog")
+			return
 		}
 
 		// try to set low
@@ -64,7 +76,7 @@ func TestPiPigpio(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		v, err := reader.Read()
+		v, err := reader.Read(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -76,7 +88,7 @@ func TestPiPigpio(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		v, err = reader.Read()
+		v, err = reader.Read(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -88,43 +100,15 @@ func TestPiPigpio(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		v, err = reader.Read()
+		v, err = reader.Read(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
 		assert.InDelta(t, 0, v, 150)
 	})
 
-	t.Run("physical servo test", func(t *testing.T) {
-		s := p.Servo("s1")
-		if s == nil {
-			t.Fatal("no servo s1")
-		}
-
-		err = s.Move(90)
-		if err != nil {
-			t.Fatal(err)
-		}
-		assert.Equal(t, byte(90), s.Current())
-		time.Sleep(200 * time.Millisecond)
-
-		err = s.Move(0)
-		if err != nil {
-			t.Fatal(err)
-		}
-		assert.Equal(t, byte(0), s.Current())
-		time.Sleep(200 * time.Millisecond)
-
-		err = s.Move(180)
-		if err != nil {
-			t.Fatal(err)
-		}
-		assert.Equal(t, byte(180), s.Current())
-		time.Sleep(200 * time.Millisecond)
-	})
-
 	t.Run("basic interrupts", func(t *testing.T) {
-		err = p.GPIOSetBcom(13, false)
+		err = p.GPIOSetBcom(18, false)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -133,7 +117,7 @@ func TestPiPigpio(t *testing.T) {
 
 		before := p.DigitalInterrupt("i1").Value()
 
-		err = p.GPIOSetBcom(13, true)
+		err = p.GPIOSetBcom(18, true)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -145,37 +129,69 @@ func TestPiPigpio(t *testing.T) {
 	})
 
 	t.Run("servo in/out", func(t *testing.T) {
-		s := p.Servo("s2")
+		s := p.Servo("servo")
 		if s == nil {
-			t.Fatal("no s2")
+			t.Fatal("no servo")
 		}
 
-		err := s.Move(90)
+		err := s.Move(ctx, 90)
 		if err != nil {
 			t.Fatal(err)
 		}
 
+		v, err := s.Current(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, 90, int(v))
+
 		time.Sleep(300 * time.Millisecond)
 
-		assert.InDelta(t, int64(1500), p.DigitalInterrupt("i2").Value(), 500) // this is a tad noisy
+		assert.InDelta(t, int64(1500), p.DigitalInterrupt("servo-i").Value(), 500) // this is a tad noisy
 	})
 
 	t.Run("motor forward", func(t *testing.T) {
 		m := p.Motor("m")
-		err := m.GoFor(board.pb.DirectionRelative_DIRECTION_RELATIVE_FORWARD, 250, 5)
+
+		pos, err := m.Position(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
-		assert.True(t, m.IsOn())
+		assert.InDelta(t, 0, pos, .01)
+
+		// 15 rpm is about what we can get from 5v. 2 rotations should take 8 seconds
+		err = m.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_FORWARD, 15, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		on, err := m.IsOn(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.True(t, on)
 
 		loops := 0
-		for m.IsOn() {
+		for {
+			on, err := m.IsOn(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !on {
+				break
+			}
+
 			time.Sleep(100 * time.Millisecond)
+
 			loops++
 			if loops > 100 {
-				t.Fatalf("motor didn't move enough, a: %v b: %v",
+				pos, err = m.Position(ctx)
+				if err != nil {
+					t.Fatal(err)
+				}
+				t.Fatalf("motor didn't move enough, a: %v b: %v pos: %v",
 					p.DigitalInterrupt("hall-a").Value(),
 					p.DigitalInterrupt("hall-b").Value(),
+					pos,
 				)
 			}
 		}
@@ -184,14 +200,28 @@ func TestPiPigpio(t *testing.T) {
 
 	t.Run("motor backward", func(t *testing.T) {
 		m := p.Motor("m")
-		err := m.GoFor(board.pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 250, 5)
+		// 15 rpm is about what we can get from 5v. 2 rotations should take 8 seconds
+		err := m.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 15, 2)
 		if err != nil {
 			t.Fatal(err)
 		}
-		assert.True(t, m.IsOn())
+
+		on, err := m.IsOn(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.True(t, on)
 
 		loops := 0
-		for m.IsOn() {
+		for {
+			on, err := m.IsOn(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !on {
+				break
+			}
+
 			time.Sleep(100 * time.Millisecond)
 			loops++
 			if loops > 100 {
