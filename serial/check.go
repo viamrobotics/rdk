2feaa1206be24c77b9bbd8d198b9e0e76e5b0fd5
Eliot Horowitz <eliothorowitz@gmail.com>
2021-05-04 14:44:57 -0400

    add numato board

--- serial/check.go
+++ serial/check.go
@@ -4,5 +4,8 @@ func checkProductDeviceIDs(vendorID, productID int) DeviceType {
 	if vendorID == 0x2341 && productID == 0x0043 {
 		return DeviceTypeArduino
 	}
+	if vendorID == 0x2a19 && productID == 0x0805 {
+		return DeviceTypeNumatoGPIO
+	}
 	return DeviceTypeUnknown
 }
