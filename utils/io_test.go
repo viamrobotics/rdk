d1d9edb0d3d182d630cfa1b3a4e1c99b0f4f6469
Eliot Horowitz <eliothorowitz@gmail.com>
2021-05-10 12:24:49 -0400

    fix some network issues, runtime error handling, and arm position differences on ur arm

--- utils/io_test.go
+++ utils/io_test.go
@@ -34,3 +34,23 @@ func (stc *somethingToClose) Close() error {
 	}
 	return nil
 }
+
+func TestReadBytes(t *testing.T) {
+	x, err := ReadBytes(&dummyReader{}, 4)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, len(x), test.ShouldEqual, 4)
+	test.That(t, x[0], test.ShouldEqual, 0x5)
+	test.That(t, x[1], test.ShouldEqual, 0x5)
+	test.That(t, x[2], test.ShouldEqual, 0x5)
+	test.That(t, x[3], test.ShouldEqual, 0x5)
+
+}
+
+type dummyReader struct {
+}
+
+func (r *dummyReader) Read(buf []byte) (int, error) {
+	buf[0] = 0x5
+	buf[1] = 0x5
+	return 2, nil
+}
