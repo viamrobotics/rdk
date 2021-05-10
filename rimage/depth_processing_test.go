7d26cee456087a94ab94b8bc35a3a10cb841cc35
Bijan Haney <bijan@viam.com>
2021-05-10 17:26:03 -0400

    adds morphological filters

---
+++ rimage/depth_processing_test.go
@@ -0,0 +1,33 @@
+package rimage
+
+import (
+	"testing"
+
+	"go.viam.com/test"
+)
+
+type rangeArrayHelper struct {
+	dim      int
+	expected []int
+}
+
+func TestRangeArray(t *testing.T) {
+	cases := []rangeArrayHelper{
+		{5, []int{-2, -1, 0, 1, 2}},
+		{4, []int{-2, -1, 1, 2}},
+		{3, []int{-1, 0, 1}},
+		{0, []int{}},
+		{-2, []int{}},
+	}
+	for _, c := range cases {
+		got := makeRangeArray(c.dim)
+		test.That(t, c.expected, test.ShouldResemble, got)
+	}
+}
+
+func TestStructuringElement(t *testing.T) {
+	expected := &DepthMap{3, 3, []Depth{1, 1, 1, 1, 2, 1, 1, 1, 1}}
+	got := makeStructuringElement(3)
+	test.That(t, expected, test.ShouldResemble, got)
+
+}
