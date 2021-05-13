fc28cf9a9d5a2fbde2517fd1abfbd0afb0db14c9
Eliot Horowitz <eliothorowitz@gmail.com>
2021-05-13 16:25:21 -0400

    put base impls back via registry

--- web/cmd/server/main.go
+++ web/cmd/server/main.go
@@ -23,6 +23,7 @@ import (
 	"go.viam.com/core/web"
 
 	// These are the robot pieces we want by default
+	_ "go.viam.com/core/base/impl"
 	_ "go.viam.com/core/board/detector"
 	_ "go.viam.com/core/rimage/imagesource"
 	_ "go.viam.com/core/robots/eva" // for eva
