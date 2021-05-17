c9493b90da461f9326215adc9190a92799306df4
Alexa Greenberg <alexa.greenberg@viam.com>
2021-05-17 17:00:08 -0400

    Close artifact after tests using it via VerifyTestMain

--- arm/verify_main_test.go
+++ arm/verify_main_test.go
@@ -3,10 +3,10 @@ package arm
 import (
 	"testing"
 
-	"go.viam.com/core/testutils"
+	testutilsext "go.viam.com/core/testutils/ext"
 )
 
 // TestMain is used to control the execution of all tests run within this package (including _test packages)
 func TestMain(m *testing.M) {
-	testutils.VerifyTestMain(m)
+	testutilsext.VerifyTestMain(m)
 }
