7c90e8a84b7de0a6cfd44219037edd1cd57117ab
Alexa Greenberg <alexa.greenberg@viam.com>
2021-06-10 09:26:22 -0400

    Use improbable grpc for js client

--- web/frontend/src/client.js
+++ web/frontend/src/client.js
@@ -1,8 +1,9 @@
+const { grpc } = require("@improbable-eng/grpc-web");
 window.robotApi = require('proto/api/v1/robot_pb.js');
-const { RobotServicePromiseClient } = require('proto/api/v1/robot_grpc_web_pb.js');
+const { RobotServiceClient } = require('proto/api/v1/robot_pb_service.js');
 
-const url = `${location.protocol}//${location.hostname}${location.port?':'+location.port:''}`;
-window.robotService = new RobotServicePromiseClient(url);
+const url = `${location.protocol}//${location.hostname}${location.port ? ':' + location.port : ''}`;
+window.robotService = new RobotServiceClient(url);
 
 window.THREE = require("three/build/three.module.js")
 window.pcdLib = require("three/examples/jsm/loaders/PCDLoader.js")
