7560676e652b368cef7bb29989c94d7cd40c7c72
Alexa Greenberg <alexa.greenberg@viam.com>
2021-06-10 11:47:23 -0400

    Connect to webrtc client if js app requests it

--- web/frontend/src/control.js
+++ web/frontend/src/control.js
@@ -1,11 +1,29 @@
 const { grpc } = require("@improbable-eng/grpc-web");
 window.robotApi = require('proto/api/v1/robot_pb.js');
 const { RobotServiceClient } = require('proto/api/v1/robot_pb_service.js');
-
-const url = `${location.protocol}//${location.hostname}${location.port ? ':' + location.port : ''}`;
-window.robotService = new RobotServiceClient(url);
-
+const { dial } = require("rpc");
 window.THREE = require("three/build/three.module.js")
 window.pcdLib = require("three/examples/jsm/loaders/PCDLoader.js")
 window.orbitLib = require("three/examples/jsm/controls/OrbitControls.js")
+
+let pResolve, pReject;
+window.robotServiceReady = new Promise((resolve, reject) => {
+	pResolve = resolve;
+	pReject = reject;
+})
+if (window.webrtcEnabled) {
+	console.log("dialing", window.webrtcSignalingAddress, "for", window.webrtcHost)
+	dial(window.webrtcSignalingAddress, window.webrtcHost).then(cc => {
+		console.log("connected!!??", cc)
+		window.robotService = new RobotServiceClient(window.webrtcHost, { transport: cc.transportFactory() });
+		pResolve(undefined);
+	}).catch(e => {
+		console.error("error dialing", e);
+		pReject(e);
+	})
+} else {
+	const url = `${location.protocol}//${location.hostname}${location.port ? ':' + location.port : ''}`;
+	window.robotService = new RobotServiceClient(url);
+	pResolve(undefined);
+}
 
