f45727e03e2b640ee24be3d4f0b136fe4b4ecdbb
Eric Daniels <eric@erdaniels.com>
2021-06-17 10:04:20 -0400

    Add TODO issue for TURN auth

--- web/server/www.go
+++ web/server/www.go
@@ -275,7 +275,7 @@ func init() {
 			},
 			{
 				URLs: []string{"turn:stun.viam.cloud"},
-				// TODO(erd): needs real creds so as to not be abused
+				// TODO(https://github.com/viamrobotics/core/issues/101): needs real creds so as to not be abused
 				Username:       "username",
 				Credential:     "password",
 				CredentialType: webrtc.ICECredentialTypePassword,
