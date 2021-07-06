7d501ced868d6ce0a9627a521be2f6a911cafa6c
Bijan Haney <bijan@viam.com>
2021-07-06 11:33:29 -0400

    tests

---
+++ vision/segmentation/object_segmentation_test.go
@@ -0,0 +1,37 @@
+package segmentation
+
+import (
+	"bytes"
+	"io/ioutil"
+	"math"
+	"testing"
+
+	"go.viam.com/test"
+	"go.viam.com/utils/artifact"
+
+	pc "go.viam.com/core/pointcloud"
+
+	"github.com/golang/geo/r3"
+)
+
+func roundVector(v r3.Vector) r3.Vector {
+	return r3.Vector{math.Round(v.X), math.Round(v.Y), math.Round(v.Z)}
+}
+
+// get a segmentation of a pointcloud and calculate each object's center
+func TestCalculateSegmentMeans(t *testing.T) {
+	// get file
+	pcd, err := ioutil.ReadFile(artifact.MustPath("segmentation/aligned_intel/pointcloud-pieces.pcd"))
+	test.That(t, err, test.ShouldBeNil)
+	cloud, err := pc.ReadPCD(bytes.NewReader(pcd))
+	test.That(t, err, test.ShouldBeNil)
+	// do segmentation
+	segments, err := CreateObjectSegmentation(cloud, 50000, 500, 10.0)
+	test.That(t, err, test.ShouldBeNil)
+	// get center points
+	for i, s := range segments.PointClouds {
+		mean := pc.CalculateMeanOfPointCloud(s)
+		mean = roundVector(mean)
+		t.Logf("center of object %d is: %v", i, mean)
+	}
+}
