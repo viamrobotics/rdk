780f0db44fd3dafe86aef8691ac75fcfa214da99
James Otting <james@viam.com>
2021-07-06 19:05:46 -0700

    First pass at working shared SPI

--- board/pi/pi.h
+++ board/pi/pi.h
@@ -1,7 +1,4 @@
 #pragma once
 
-// doAnalogRead reads an SPI device given by h on the given channel. 
-int doAnalogRead(int h, int channel);
-
 // interruptCallback calls through to the go linked interrupt callback.
 void setupInterrupt(int gpio);
