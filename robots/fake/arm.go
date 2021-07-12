625319c69027fb1088ac599f80c4231dc460da68
Alexa Greenberg <alexa.greenberg@viam.com>
2021-07-12 10:11:55 -0400

    Revert "use fake arm model for fake arm"
    This reverts commit 0a1c1ae17240629bc3750303f672fd7a3cb3ab1d.

--- robots/fake/arm.go
+++ robots/fake/arm.go
@@ -2,13 +2,11 @@ package fake
 
 import (
 	"context"
-	_ "embed" // for arm model
 
 	"github.com/go-errors/errors"
 
 	"go.viam.com/core/arm"
 	"go.viam.com/core/config"
-	"go.viam.com/core/kinematics"
 	pb "go.viam.com/core/proto/api/v1"
 	"go.viam.com/core/registry"
 	"go.viam.com/core/robot"
@@ -16,33 +14,22 @@ import (
 	"github.com/edaniels/golog"
 )
 
-//go:embed arm_model.json
-var armModelJSON string
-
 func init() {
 	registry.RegisterArm("fake", func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (arm.Arm, error) {
 		if config.Attributes.Bool("fail_new", false) {
 			return nil, errors.New("whoops")
 		}
-		return NewArm(config.Name, logger)
+		return NewArm(config.Name), nil
 	})
 }
 
 // NewArm returns a new fake arm.
-func NewArm(name string, logger golog.Logger) (*Arm, error) {
-	model, err := kinematics.ParseJSON([]byte(armModelJSON))
-	if err != nil {
-		return nil, err
-	}
-
-	ik := kinematics.CreateCombinedIKSolver(model, logger, 4)
-
+func NewArm(name string) *Arm {
 	return &Arm{
 		Name:     name,
 		position: &pb.ArmPosition{},
 		joints:   &pb.JointPositions{Degrees: []float64{0, 0, 0, 0, 0, 0}},
-		ik:       ik,
-	}, nil
+	}
 }
 
 // Arm is a fake arm that can simply read and set properties.
@@ -50,27 +37,18 @@ type Arm struct {
 	Name       string
 	position   *pb.ArmPosition
 	joints     *pb.JointPositions
-	ik         kinematics.InverseKinematics
 	CloseCount int
 }
 
 // CurrentPosition returns the set position.
 func (a *Arm) CurrentPosition(ctx context.Context) (*pb.ArmPosition, error) {
-	joints, err := a.CurrentJointPositions(ctx)
-	return kinematics.ComputePosition(a.ik.Mdl(), joints), err
+	return a.position, nil
 }
 
 // MoveToPosition sets the position.
-func (a *Arm) MoveToPosition(ctx context.Context, pos *pb.ArmPosition) error {
-	joints, err := a.CurrentJointPositions(ctx)
-	if err != nil {
-		return err
-	}
-	solution, err := a.ik.Solve(ctx, pos, joints)
-	if err != nil {
-		return err
-	}
-	return a.MoveToJointPositions(ctx, solution)
+func (a *Arm) MoveToPosition(ctx context.Context, c *pb.ArmPosition) error {
+	a.position = c
+	return nil
 }
 
 // MoveToJointPositions sets the joints.
