1e45199f32a09a66c20d05b5900c8e2a9c2eaa55
James Otting <james@viam.com>
2021-07-14 14:44:14 -0700

    Resetbox supports paired linear homing

--- samples/resetbox/cmd.go
+++ samples/resetbox/cmd.go
@@ -12,19 +12,23 @@ import (
 	//"net/http"
 	"sync"
 	//"time"
+	// "fmt"
+	// "reflect"
 
 	"github.com/go-errors/errors"
 
-	"go.viam.com/utils/rpc/dialer"
 	"go.viam.com/utils"
+	//"go.viam.com/utils/rpc/dialer"
 
 	"go.viam.com/core/action"
 	"go.viam.com/core/board"
 	"go.viam.com/core/config"
 	pb "go.viam.com/core/proto/api/v1"
+
 	//"go.viam.com/core/rlog"
 	"go.viam.com/core/robot"
 	robotimpl "go.viam.com/core/robot/impl"
+
 	//"go.viam.com/core/sensor"
 	"go.viam.com/core/web"
 	webserver "go.viam.com/core/web/server"
@@ -35,49 +39,101 @@ import (
 	"go.uber.org/multierr"
 )
 
-
 var logger = golog.NewDevelopmentLogger("resetbox")
 
-
-type MotorPair struct {
-	a,b board.Motor
-}
-
-type LinearMotor struct {
-	m board.Motor
+type LinearAxis struct {
+	m       []board.Motor
 	mmPerRev float64
 }
 
-type ResetBox struct {
-	theBoard        board.Board
-	gate, squeeze   MotorPair
-	elevator        LinearMotor
-	//hammer          board.Motor
+// GoTo moves to a position specified in mm and at a speed in mm/s
+func (a *LinearAxis) GoTo(ctx context.Context, speed float64, position float64) error {
+	var errors error
+	for _, m := range a.m {
+		if m == nil {
+			continue
+		}
+		raw := m.GetRaw(ctx).(*board.TMCStepperMotor)
+		multierr.AppendInto(&errors, raw.GoTo(ctx, speed * 60 / a.mmPerRev, position / a.mmPerRev))
+	}
+	return errors
+}
 
-	activeBackgroundWorkers            *sync.WaitGroup
+// GoFor moves for the distance specified in mm and at a speed in mm/s
+func (a *LinearAxis) GoFor(ctx context.Context, d pb.DirectionRelative, speed float64, position float64) error {
+	var errors error
+	for _, m := range a.m {
+		if m == nil {
+			continue
+		}
+		raw := m.GetRaw(ctx).(*board.TMCStepperMotor)
+		multierr.AppendInto(&errors, raw.GoFor(ctx, d, speed * 60 / a.mmPerRev, position / a.mmPerRev))
+	}
+	return errors
+}
+
+
+// Home simultaneously homes all motors on an axis
+func (a *LinearAxis) Home(ctx context.Context, d pb.DirectionRelative, speed float64) error {
+	var homeWorkers sync.WaitGroup
+	var errors error
+	for _, m := range a.m {
+		raw := m.GetRaw(ctx).(*board.TMCStepperMotor)
+	 	homeWorkers.Add(1)
+	 	utils.ManagedGo(func() {
+	 		multierr.AppendInto(&errors, raw.Home(ctx, d, speed * 60 / a.mmPerRev))
+	 	}, homeWorkers.Done)
+	}
+	homeWorkers.Wait()
+	return errors
+}
+
+func (a *LinearAxis) Off(ctx context.Context) error {
+	var errors error
+	for _, m := range a.m {
+		multierr.AppendInto(&errors, m.Off(ctx))
+	}
+	return errors
+}
+
+
+type ResetBox struct {
+	logger        golog.Logger
+	board      board.Board
+	gate, squeeze LinearAxis
+	elevator      LinearAxis
+	hammer        board.Motor
+
+	activeBackgroundWorkers *sync.WaitGroup
 
 	cancel    func()
 	cancelCtx context.Context
 }
 
-
-
-func NewResetBox(r robot.Robot) (*ResetBox, error) {
+func NewResetBox(r robot.Robot, logger golog.Logger) (*ResetBox, error) {
 	cancelCtx, cancel := context.WithCancel(context.Background())
-	b := &ResetBox{activeBackgroundWorkers: &sync.WaitGroup{}, cancelCtx: cancelCtx, cancel: cancel}
-	b.theBoard = r.BoardByName("resetboard")
-	if b.theBoard == nil {
+	b := &ResetBox{activeBackgroundWorkers: &sync.WaitGroup{}, cancelCtx: cancelCtx, cancel: cancel, logger: logger}
+	b.board = r.BoardByName("resetboard")
+	if b.board == nil {
 		return nil, errors.New("Cannot find board: resetboard")
 	}
 
-	b.gate.a = b.theBoard.Motor("gateL")
-	b.gate.b = b.theBoard.Motor("gateR")
-	b.squeeze.a = b.theBoard.Motor("squeezeL")
-	b.squeeze.b = b.theBoard.Motor("squeezeR")
-	b.elevator.m = b.theBoard.Motor("elevator")
-	//b.hammer = b.theBoard.Motor("hammer")
+	b.gate.m = append(b.gate.m, b.board.Motor("gateL"))
+	b.gate.m = append(b.gate.m, b.board.Motor("gateR"))
+	b.gate.mmPerRev = 8.0
+
+	b.squeeze.m = append(b.squeeze.m, b.board.Motor("squeezeL"))
+	b.squeeze.m = append(b.squeeze.m, b.board.Motor("squeezeR"))
+	b.squeeze.mmPerRev = 8.0
+
+	b.elevator.m = append(b.elevator.m, b.board.Motor("elevator"))
+	b.elevator.mmPerRev = 8.0
+
+	b.hammer = b.board.Motor("hammer")
 
 	// TODO Check for missing motors
+
+
 
 	return b, nil
 }
@@ -91,22 +147,18 @@ func (b *ResetBox) Close() error {
 
 func (b *ResetBox) Stop(ctx context.Context) error {
 	return multierr.Combine(
-		b.elevator.m.Off(ctx), 
-		b.gate.a.Off(ctx), 
-		b.gate.b.Off(ctx), 
-		b.squeeze.a.Off(ctx),
-		b.squeeze.b.Off(ctx),
-		//b.hammer.Off(ctx),
+		b.elevator.Off(ctx),
+		b.gate.Off(ctx),
+		b.gate.Off(ctx),
+		b.squeeze.Off(ctx),
+		b.squeeze.Off(ctx),
+		b.hammer.Off(ctx),
 	)
 }
-
-
 
 func (b *ResetBox) Ready(r robot.Robot) error {
 	return nil
 }
-
-
 
 func main() {
 	utils.ContextualMain(mainWithArgs, logger)
@@ -126,49 +178,17 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) (err 
 	}
 	defer myRobot.Close()
 
-	box, err := NewResetBox(myRobot)
+	box, err := NewResetBox(myRobot, logger)
 	if err != nil {
 		return err
 	}
 	defer box.Close()
-	//boat.StartRC(ctx)
-
-	myRobot.AddProvider(box, config.Component{Name: "resetbox"})
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	rpcDialer := dialer.NewCachedDialer()
-	defer func() {
-		err = multierr.Combine(err, rpcDialer.Close())
-	}()
-	ctx = dialer.ContextWithDialer(ctx, rpcDialer)
 
-
-	// var activeBackgroundWorkers sync.WaitGroup
-	// activeBackgroundWorkers.Add(2)
-	// defer activeBackgroundWorkers.Wait()
-	// utils.ManagedGo(func() {
-	// 	trackGPS(ctx)
-	// }, activeBackgroundWorkers.Done)
-	// utils.ManagedGo(func() {
-	// 	recordDepthWorker(ctx, myRobot.SensorByName("depth1"))
-	// }, activeBackgroundWorkers.Done)
-
-
-	action.RegisterAction("HomeGate", func(ctx context.Context, r robot.Robot) {
-		err := box.home(ctx, r)
-		if err != nil {
-			logger.Errorf("error homing gate: %s", err)
-		}
-	})
-
-	action.RegisterAction("Go300", func(ctx context.Context, r robot.Robot) {
-		err := box.go300(ctx, r)
-		if err != nil {
-			logger.Errorf("error homing gate: %s", err)
-		}
-	})
-
+	action.RegisterAction("Home", box.home)
+	action.RegisterAction("Go Fast", box.goFast)
 
 	webOpts := web.NewOptions()
 	webOpts.Insecure = true
@@ -183,12 +203,21 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) (err 
 	return nil
 }
 
+func (b *ResetBox) home(ctx context.Context, r robot.Robot) {
+	errors := multierr.Combine(
+		b.gate.Home(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 10),
+		//b.squeeze.Home(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 10),
+		//b.elevator.Home(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 10),
+		//b.hammer.Home(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 10),
+	)
 
-func (b *ResetBox) home(ctx context.Context, r robot.Robot) error {
-	raw := b.gate.a.GetRaw(ctx)
-	return raw.(*board.TMCStepperMotor).Home(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 100)
+	if errors != nil {
+		b.logger.Error(errors)
+	}
 }
 
-func (b *ResetBox) go300(ctx context.Context, r robot.Robot) error {
-	return b.gate.a.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_BACKWARD, 300, 1000)
+func (b *ResetBox) goFast(ctx context.Context, r robot.Robot) {
+	b.gate.GoFor(ctx, pb.DirectionRelative_DIRECTION_RELATIVE_FORWARD, 500, 1000)
 }
+
+
