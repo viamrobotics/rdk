356a47ccb2c9ecce34f728e3735305aa942f45a9
Peter LoVerso <biotinker@gmail.com>
2021-07-30 13:54:17 -0700

    Add offsets to frame wrappers

--- referenceframe/wrapper.go
+++ referenceframe/wrapper.go
@@ -6,13 +6,25 @@ import (
 
 // A FrameWrapper will wrap a Frame, allowing a new Parent to be set
 type FrameWrapper struct{
-	real Frame
+	real   Frame
+	offset *spatialmath.DualQuaternion
 	parent string
 }
 
-// Transform returns the quaternion associated with the wrapped frame
+// WrapFrame will take a frame and wrap it to have the given parent. Offset is not part of this constructor and must be
+// set separately with SetOffset(). The reason for this is to ensure that if no offset is specified, the identity
+// dual quaternion is used ((1,0,0,0),(0,0,0,0)) rather than someone passing in a spatialmath.DualQuaternion{}
+func WrapFrame(frame Frame, parent string) *FrameWrapper{
+	return &FrameWrapper{
+		real: frame,
+		offset: spatialmath.NewDualQuaternion(),
+		parent: parent,
+	}
+}
+
+// Transform returns the quaternion associated with the wrapped frame, transformed by the 
 func (f *FrameWrapper) Transform(input []Input) *spatialmath.DualQuaternion {
-	return f.real.Transform(input)
+	return &spatialmath.DualQuaternion{f.offset.Transformation(f.real.Transform(input).Quat)}
 }
 
 // Parent will return the name of the next transform up the kinematics chain from this frame
@@ -23,4 +35,9 @@ func (f *FrameWrapper) Parent() string {
 // Dof returns the dof of the wrapped frame
 func (f *FrameWrapper) Dof() int {
 	return f.real.Dof()
+}
+
+// Dof returns the dof of the wrapped frame
+func (f *FrameWrapper) SetOffset(offset *spatialmath.DualQuaternion) {
+	f.offset = offset
 }
