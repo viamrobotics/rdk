1d1bb91ef324c32eca3654dbaad5849e34c19e17
Bijan Haney <bijan@viam.com>
2021-08-03 13:00:31 -0400

    adds interfaces and struct stubs

--- referenceframe/frame_test.go
+++ referenceframe/frame_test.go
@@ -1,1 +1,26 @@
 package referenceframe
+
+import (
+	"go.viam.com/test"
+	"testing"
+
+	"gonum.org/v1/gonum/num/dualquat"
+)
+
+/*
+Create a test that successfully transforms the pose of *object from *frame1 into *frame2. The Orientation of *frame1 and *frame2
+are the same, so the transformation is only made up of two translations.
+
+|              |
+|*frame1       |*object
+|              |
+|
+|
+|              *frame2
+|________________
+world
+*/
+
+func TestFrameTranslation(t *testing.T) {
+	return nil
+}
