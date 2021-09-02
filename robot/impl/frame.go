fd91b2e8e9e33e51b9575d253d87ea13ab71c955
Bijan Haney <bijan@viam.com>
2021-09-02 17:50:18 -0400

    bug fix, if registration doesn't exist OR frame functions doesnt exist return nil

--- robot/impl/frame.go
+++ robot/impl/frame.go
@@ -92,37 +92,37 @@ func makeModelFrame(comp *config.Component) (ref.Frame, error) {
 	switch comp.Type {
 	case config.ComponentTypeProvider:
 		registration := registry.ProviderLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
 	case config.ComponentTypeBase:
 		registration := registry.BaseLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
 	case config.ComponentTypeArm:
 		registration := registry.ArmLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
 	case config.ComponentTypeGripper:
 		registration := registry.GripperLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
 	case config.ComponentTypeCamera:
 		registration := registry.CameraLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
 	case config.ComponentTypeLidar:
 		registration := registry.LidarLookup(comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
@@ -131,7 +131,7 @@ func makeModelFrame(comp *config.Component) (ref.Frame, error) {
 			return nil, errors.New("sensor component requires subtype")
 		}
 		registration := registry.SensorLookup(sensor.Type(comp.SubType), comp.Model)
-		if registration == nil && registration.Frame == nil {
+		if registration == nil || registration.Frame == nil {
 			return nil, nil
 		}
 		modelFrame, err = registration.Frame(comp.Name)
