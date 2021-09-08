ebe05f2027c267d725dcf8b9f7c57073324fb074
Cheuk <90270663+cheukt@users.noreply.github.com>
2021-09-08 16:11:34 -0400

    Minor setup script updates (#158)
    * Updates PREFIX for go install -> That's probably why my go installed into /usr/local and not /usr/local/go
    * Adds cmake to install list -> Script failed somewhat silently here because nlopt uses cmake
    * Expands x264 valid version range
    * Move version check into if block -> right now, the check happens regardless of whether the binary/library exists

--- etc/setup.sh
+++ etc/setup.sh
@@ -15,7 +15,8 @@ if which go; then
   echo "golang installed"
 else
   ENV_OK=0
-  PREFIX="/usr/local" && \
+  PREFIX="/usr/local/go"
+  sudo mkdir -p $PREFIX
   VERSION="1.17" && \
     curl -sSL \
       "https://golang.org/dl/go${VERSION}.${PLATFORM}-${ARCH}.tar.gz" | \
@@ -28,7 +29,7 @@ if [ "$(uname)" = "Linux" ]; then
   case $DISTRO in
     "Debian"|"Ubuntu")
       sudo apt update
-      sudo apt -y install libvpx-dev libx264-dev pkg-config
+      sudo apt -y install libvpx-dev libx264-dev pkg-config cmake
       if which npm; then
         echo "node installed"
       else
@@ -97,6 +98,7 @@ if [ "$(uname)" = "Darwin" ]; then
     brew "pkgconfig"
     brew "protobuf"
     brew "buf"
+    brew "cmake"
 EOS
   curl -L https://github.com/grpc/grpc-web/releases/download/1.2.1/protoc-gen-grpc-web-1.2.1-darwin-x86_64 --output protoc-gen-grpc-web
   chmod +x protoc-gen-grpc-web
