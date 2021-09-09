9777bbd35bde1efb5126928c09db073a86791ad0
Alexa Greenberg <alexa.greenberg@viam.com>
2021-09-09 08:07:08 -0700

    IK intuitive motion updates (#155)
    * Add support for xArm7. Improve IK solver with a measure of intuitiveness of movement, and update so motions returned are either intuitive, or if no excellent solution is found, then use the best among those found. Add a check to detect if the user is pointing at a pole and using the increment UI, and adjusting theta during motion near poles to what is likely desired.

--- kinematics/inverseKinematics.go
+++ kinematics/inverseKinematics.go
@@ -2,7 +2,9 @@ package kinematics
 
 import (
 	"context"
+	"math"
 
+	"go.viam.com/core/arm"
 	pb "go.viam.com/core/proto/api/v1"
 	"go.viam.com/core/spatialmath"
 )
@@ -23,9 +25,10 @@ type InverseKinematics interface {
 	Mdl() *Model
 }
 
-// toArray returns the SolverDistanceWeights as a slice with the components in the same order as the array returned from ToDelta
+// toArray returns the SolverDistanceWeights as a slice with the components in the same order as the array returned from
+// quaternion ToDelta. Note that orientation components are multiplied by 100 since they are usually small to avoid drift.
 func (dc *SolverDistanceWeights) toArray() []float64 {
-	return []float64{dc.Trans.X, dc.Trans.Y, dc.Trans.Z, dc.Orient.TH, dc.Orient.X, dc.Orient.Y, dc.Orient.Z}
+	return []float64{dc.Trans.X, dc.Trans.Y, dc.Trans.Z, 10 * dc.Orient.TH, 100 * dc.Orient.X, 100 * dc.Orient.Y, 100 * dc.Orient.Z}
 }
 
 // SquaredNorm returns the dot product of a vector with itself
@@ -34,16 +37,43 @@ func SquaredNorm(vec []float64) float64 {
 	for _, v := range vec {
 		norm += v * v
 	}
-	return norm - 1
+	return norm
 }
 
-// WeightedSquaredNorm TODO
+// WeightedSquaredNorm returns the dot product of a vector with itself, applying the given weights to each piece.
 func WeightedSquaredNorm(vec []float64, config SolverDistanceWeights) float64 {
 	configArr := config.toArray()
 	norm := 0.0
 	for i, v := range vec {
 		norm += v * v * configArr[i]
 	}
-	// With R4 angle axis, we wind up at 0 1 0 0
-	return norm - 1
+	return norm
+}
+
+// calcSwingPct will calculate the distance from the start position to the halfway point, and also the start position to
+// the end position, and return the ratio of the two. If the result >1.0, then the halfway point is further from the
+// start position than the end position is, and thus solution searching should continue
+func calcSwingPct(from, to *pb.JointPositions, model *Model) float64 {
+	startPos := JointRadToQuat(model, arm.JointPositionsToRadians(from))
+	endPos := JointRadToQuat(model, arm.JointPositionsToRadians(to))
+	halfPos := JointRadToQuat(model, arm.JointPositionsToRadians(interpolateJoints(from, to, 0.5)))
+
+	endDist := WeightedSquaredNorm(startPos.ToDelta(endPos), model.SolveWeights)
+	halfDist := WeightedSquaredNorm(startPos.ToDelta(halfPos), model.SolveWeights)
+	return (halfDist + 1) / (endDist + 1)
+}
+
+// bestSolution will select the best solution from a slice of possible solutions for a given model. "Best" is defined
+// such that the interpolated halfway point of the motion is most in line with the movement from start to end.
+func bestSolution(seedAngles *pb.JointPositions, solutions []*pb.JointPositions, model *Model) *pb.JointPositions {
+	var best *pb.JointPositions
+	dist := math.Inf(1)
+	for _, solution := range solutions {
+		newDist := calcSwingPct(seedAngles, solution, model)
+		if newDist < dist {
+			dist = newDist
+			best = solution
+		}
+	}
+	return best
 }
