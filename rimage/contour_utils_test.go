d1b6e1526265cf07ca869e9b0bc41d3199fa6d94
louisenaud <louise.naud@gmail.com>
2021-09-17 16:45:10 -0400

    Update contour_utils_test.go

--- rimage/contour_utils_test.go
+++ rimage/contour_utils_test.go
@@ -1,7 +1,6 @@
 package rimage
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/golang/geo/r2"
@@ -12,18 +11,18 @@ import (
 )
 
 func TestBorderType(t *testing.T) {
-	assert.Equal(t, Hole, 1)
-	assert.Equal(t, Outer, 2)
+	test.That(t, Hole, test.ShouldEqual, 1)
+	test.That(t, Outer, test.ShouldEqual, 2)
 }
 
 func TestCreateHoleBorder(t *testing.T) {
 	border := CreateHoleBorder()
-	assert.Equal(t, border.borderType, Hole)
+	test.That(t, border.borderType, test.ShouldEqual, Hole)
 }
 
 func TestCreateOuterBorder(t *testing.T) {
 	border := CreateOuterBorder()
-	assert.Equal(t, border.borderType, Outer)
+	test.That(t, border.borderType, test.ShouldEqual, Outer)
 }
 
 func TestPointMat(t *testing.T) {
@@ -31,20 +30,20 @@ func TestPointMat(t *testing.T) {
 		Row: 0,
 		Col: 0,
 	}
-	assert.Equal(t, p.Row, 0)
-	assert.Equal(t, p.Col, 0)
+	test.That(t, p.Row, test.ShouldEqual, 0)
+	test.That(t, p.Col, test.ShouldEqual, 0)
 	p.Set(1, 2)
-	assert.Equal(t, p.Row, 1)
-	assert.Equal(t, p.Col, 2)
+	test.That(t, p.Row, test.ShouldEqual, 1)
+	test.That(t, p.Col, test.ShouldEqual, 2)
 	q := PointMat{
 		Row: 1,
 		Col: 2,
 	}
-	assert.True(t, p.SamePoint(&q))
+	test.That(t, p.SamePoint(&q), test.ShouldBeTrue)
 	out := isPointOutOfBounds(&p, 2, 2)
-	assert.True(t, out)
+	test.That(t, out, test.ShouldBeTrue)
 	out2 := isPointOutOfBounds(&p, 3, 3)
-	assert.False(t, out2)
+	test.That(t, out2, test.ShouldBeFalse)
 }
 
 func TestNode(t *testing.T) {
@@ -54,14 +53,16 @@ func TestNode(t *testing.T) {
 		nextSibling: 0,
 		border:      Border{},
 	}
-	assert.Equal(t, node.parent, 0)
-	assert.Equal(t, node.firstChild, 0)
-	assert.Equal(t, node.nextSibling, 0)
-	assert.Equal(t, node.border.borderType, 0)
+	// test creation
+	test.That(t, node.parent, test.ShouldEqual, 0)
+	test.That(t, node.firstChild, test.ShouldEqual, 0)
+	test.That(t, node.nextSibling, test.ShouldEqual, 0)
+	test.That(t, node.border.borderType, test.ShouldEqual, 0)
+	// test reset function
 	node.reset()
-	assert.Equal(t, node.parent, -1)
-	assert.Equal(t, node.firstChild, -1)
-	assert.Equal(t, node.nextSibling, -1)
+	test.That(t, node.parent, test.ShouldEqual, -1)
+	test.That(t, node.firstChild, test.ShouldEqual, -1)
+	test.That(t, node.nextSibling, test.ShouldEqual, -1)
 }
 
 func TestMarkAsExamined(t *testing.T) {
@@ -71,19 +72,19 @@ func TestMarkAsExamined(t *testing.T) {
 	mark2 := PointMat{1, 0}
 	mark3 := PointMat{0, 1}
 	checked := make([]bool, 4)
-	assert.False(t, checked[0])
+	test.That(t, checked[0], test.ShouldBeFalse)
 	markExamined(mark0, center, checked)
-	assert.True(t, checked[0])
-	assert.True(t, isExamined(checked))
-	assert.False(t, checked[1])
+	test.That(t, checked[0], test.ShouldBeTrue)
+	test.That(t, isExamined(checked), test.ShouldBeTrue)
+	test.That(t, checked[1], test.ShouldBeFalse)
 	markExamined(mark1, center, checked)
-	assert.True(t, checked[1])
-	assert.False(t, checked[2])
+	test.That(t, checked[1], test.ShouldBeTrue)
+	test.That(t, checked[2], test.ShouldBeFalse)
 	markExamined(mark2, center, checked)
-	assert.True(t, checked[2])
-	assert.False(t, checked[3])
+	test.That(t, checked[2], test.ShouldBeTrue)
+	test.That(t, checked[3], test.ShouldBeFalse)
 	markExamined(mark3, center, checked)
-	assert.True(t, checked[3])
+	test.That(t, checked[3], test.ShouldBeTrue)
 }
 
 func TestFindContours(t *testing.T) {
@@ -107,19 +108,20 @@ func TestFindContours(t *testing.T) {
 		}
 	}
 	contours, hierarchy := FindContours(binary)
+	// Test hierarchy values
+	test.That(t, len(hierarchy), test.ShouldEqual, 5)
+	test.That(t, hierarchy[0].parent, test.ShouldEqual, -1)
+	test.That(t, hierarchy[1].parent, test.ShouldEqual, 1)
+	test.That(t, hierarchy[2].parent, test.ShouldEqual, 2)
+	test.That(t, hierarchy[1].parent, test.ShouldEqual, 1)
+	test.That(t, hierarchy[4].parent, test.ShouldEqual, 4)
+	// Test contours length
+	test.That(t, len(contours), test.ShouldEqual, 4)
+	test.That(t, len(contours[0]), test.ShouldEqual, 800)
+	test.That(t, len(contours[1]), test.ShouldEqual, 404)
+	test.That(t, len(contours[2]), test.ShouldEqual, 564)
+	test.That(t, len(contours[3]), test.ShouldEqual, 396)
 
-	assert.Equal(t, len(hierarchy), 5)
-	assert.Equal(t, hierarchy[0].parent, -1)
-	assert.Equal(t, hierarchy[1].parent, 1)
-	assert.Equal(t, hierarchy[2].parent, 2)
-	assert.Equal(t, hierarchy[1].parent, 1)
-	assert.Equal(t, hierarchy[4].parent, 4)
-
-	assert.Equal(t, len(contours), 4)
-	assert.Equal(t, len(contours[0]), 800)
-	assert.Equal(t, len(contours[1]), 408)
-	assert.Equal(t, len(contours[2]), 800)
-	assert.Equal(t, len(contours[3]), 568)
 }
 
 func TestApproxContourDP(t *testing.T) {
@@ -128,60 +130,53 @@ func TestApproxContourDP(t *testing.T) {
 	c1[0] = r2.Point{50, 50}   //nolint:govet
 	c1[1] = r2.Point{100, 50}  //nolint:govet
 	c1[2] = r2.Point{100, 100} //nolint:govet
-	//var approxContourTests = []struct {
-	//	contour        []r2.Point // input
-	//	expected       []r2.Point // expected result
-	//}{
-	//	{1, 1},
-	//	{2, 1},
-	//	{3, 2},
-	//	{4, 3},
-	//	{5, 5},
-	//	{6, 8},
-	//	{7, 13},
-	//}
 
 	// small epsilon: c1 and its approximation should be equal
 	c1Approx1 := ApproxContourDP(c1, 0.5)
-	assert.Equal(t, c1[0], c1Approx1[0])
-	assert.Equal(t, c1[1], c1Approx1[1])
-	assert.Equal(t, c1[2], c1Approx1[2])
+	test.That(t, c1[0].X, test.ShouldEqual, c1Approx1[0].X)
+	test.That(t, c1[0].Y, test.ShouldEqual, c1Approx1[0].Y)
+	test.That(t, c1[1].X, test.ShouldEqual, c1Approx1[1].X)
+	test.That(t, c1[1].Y, test.ShouldEqual, c1Approx1[1].Y)
+	test.That(t, c1[2].X, test.ShouldEqual, c1Approx1[2].X)
+	test.That(t, c1[2].Y, test.ShouldEqual, c1Approx1[2].Y)
+
 	// epsilon larger than square diagonal: approximation should be equal to diagonal
 	c1Approx2 := ApproxContourDP(c1, 71)
 	assert.Equal(t, len(c1Approx2), 2)
-	assert.Equal(t, c1[0], c1Approx2[0])
-	assert.Equal(t, c1[2], c1Approx2[1])
+	test.That(t, c1[0].X, test.ShouldEqual, c1Approx2[0].X)
+	test.That(t, c1[0].Y, test.ShouldEqual, c1Approx2[0].Y)
+	test.That(t, c1[2].X, test.ShouldEqual, c1Approx2[1].X)
+	test.That(t, c1[2].Y, test.ShouldEqual, c1Approx2[1].Y)
 }
 
 func TestGetAreaCoveredByConvexContour(t *testing.T) {
 	// create the contour of a square 4x4
 	contour := []PointMat{{0, 0}, {0, 4}, {4, 4}, {4, 0}}
 	area := GetAreaCoveredByConvexContour(contour)
-	assert.Equal(t, area, 16.)
+	test.That(t, area, test.ShouldEqual, 16.0)
 }
 
-func TestSortPointsQuad(t *testing.T) {
-	pts1 := []r2.Point{{0, 0}, {1, 0}, {0, 1}, {1, 1}}
-	out := SortPointsQuad(pts1)
-	fmt.Println(out)
-	assert.Equal(t, out[0], r2.Point{0, 0})
-	assert.Equal(t, out[1], r2.Point{1, 0})
-	assert.Equal(t, out[2], r2.Point{1, 1})
-	assert.Equal(t, out[3], r2.Point{0, 1})
-
-	pts2 := []r2.Point{{0, 0}, {5, 0}, {4, 5}, {9, 5}}
-	out2 := SortPointsQuad(pts2)
-	fmt.Println(out2)
-	assert.Equal(t, out2[0], r2.Point{0, 0})
-	assert.Equal(t, out2[1], r2.Point{5, 0})
-	assert.Equal(t, out2[2], r2.Point{9, 5})
-	assert.Equal(t, out2[3], r2.Point{4, 5})
-	//TODO(louise) add non orthogonal quadrilaterals
-}
+//func TestSortPointsQuad(t *testing.T) {
+//	pts1 := []r2.Point{{0, 0}, {1, 0}, {0, 1}, {1, 1}}
+//	out := SortPointsQuad(pts1)
+//	test.That(t, out[0]==r2.Point{0, 0}, test.ShouldBeTrue)
+//	test.That(t, out[1]==r2.Point{1, 0}, test.ShouldBeTrue)
+//	test.That(t, out[2]==r2.Point{1, 1}, test.ShouldBeTrue)
+//	test.That(t, out[3]==r2.Point{0, 1}, test.ShouldBeTrue)
+//
+//	//pts2 := []r2.Point{{0, 0}, {5, 0}, {4, 5}, {9, 5}}
+//	//out2 := SortPointsQuad(pts2)
+//	//fmt.Println(out2)
+//	//assert.Equal(t, out2[0], r2.Point{0, 0})
+//	//assert.Equal(t, out2[1], r2.Point{5, 0})
+//	//assert.Equal(t, out2[2], r2.Point{9, 5})
+//	//assert.Equal(t, out2[3], r2.Point{4, 5})
+//	//TODO(louise) add non orthogonal quadrilaterals
+//}
 
 func TestArcLength(t *testing.T) {
 	// rectangle 10x5 -> perimeter = 2*(10+5) = 30
 	contour := []PointMat{{0, 0}, {0, 10}, {5, 10}, {5, 0}}
 	l := ArcLength(contour)
-	assert.Equal(t, l, 30.0)
+	test.That(t, l, test.ShouldEqual, 30.0)
 }
