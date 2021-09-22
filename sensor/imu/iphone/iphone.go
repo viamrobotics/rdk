ffd9354cd0317d5228fc64e2f4e0c0bdc2fa0cff
aaroncasas <aaron@viam.com>
2021-09-22 19:02:47 -0400

    Flesh out (in need of refactoring) methods for parsing angular velocity/orientation data.

--- sensor/imu/iphone/iphone.go
+++ sensor/imu/iphone/iphone.go
@@ -2,17 +2,17 @@
 package iphone
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"github.com/edaniels/golog"
-	"github.com/go-errors/errors"
 	"go.viam.com/core/config"
 	"go.viam.com/core/registry"
 	"go.viam.com/core/robot"
 	"go.viam.com/core/sensor"
-	"go.viam.com/core/sensor/compass"
 	"go.viam.com/core/sensor/imu"
-	"log"
 	"net"
+	"strconv"
 	"time"
 )
 
@@ -28,18 +28,6 @@ type iphoneMeasurement struct {
 	Yaw *string `json:"motionYaw"`
 }
 
-// ScanOptions modify how scan results will be produced and are subject
-// to interpretation by the lidar implementation.
-type ScanOptions struct {
-	// Count determines how many scans to perform.
-	Count int
-
-	// NoFilter hints to the implementation to give as raw results as possible.
-	// Normally an implementation may do some extra work to eliminate false
-	// positives but this can be expensive and undesired.
-	NoFilter bool
-}
-
 // init registers the iphone IMU type.
 func init() {
 	registry.RegisterSensor(imu.Type, ModelName, func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (sensor.Sensor, error) {
@@ -49,40 +37,119 @@ func init() {
 
 // IPhone is an iPhone based IMU.
 type IPhone struct {
-	conn net.Conn // Connection to iPhone to pull sensor data from.
+	reader *bufio.Reader // Read connection to iPhone to pull sensor data from.
 }
 
 // New returns a new IPhone IMU that that pulls data from the iPhone at host.
 func New(ctx context.Context, host string, logger golog.Logger) (imu *IPhone, err error) {
-	conn, err := getConn(host, 3 * time.Second)
+	conn, err := net.DialTimeout("tcp", host, 3 * time.Second)
 	if err != nil {
 		return nil, err
 	}
 
 	// TODO: check if iPhone is actually sending necessary data? fail fast and all
+	//       but also that would require the iphone to be on whenever this component is initialized.
+	//       So when would that happen?
+	if err = validateReceivingIMUData(conn); err != nil {
+		return nil, err
+	}
 
-	return &IPhone{conn: conn}, nil
+	r := bufio.NewReader(conn)
+
+	return &IPhone{reader: r}, nil
 }
 
-
+func validateReceivingIMUData(conn net.Conn) error {
+	return nil
+}
 
 // Desc returns a description of the compass.
 func (ip *IPhone) Desc() sensor.Description {
 	return sensor.Description{Type: imu.Type, Path: ""}
 }
 
-// TODO: think i need to rework this. What if dial takes longer than timeout? this must be a solved problem,
-//       overthinking
-// Attempts to connect to host over tcp. Times out after timeoutMillis.
-func getConn(host string, timeout time.Duration) (net.Conn, error) {
-	var err error
-	for start := time.Now(); time.Since(start) < timeout; {
-		conn, err := net.Dial("tcp", host)
-		if err == nil {
-			return conn, nil
-		}
+func (ip *IPhone) AngularVelocities(ctx context.Context) ([3]float64, error) {
+	var ret [3]float64
+
+	measurement, err := ip.reader.ReadString('\n')
+	if err != nil {
+		return ret, err
 	}
-	return nil, err
+
+	var imuReading iphoneMeasurement
+	err = json.Unmarshal([]byte(measurement), &imuReading)
+	if err != nil {
+		return ret, err
+	}
+
+	if err = measurementToVelocityArr(imuReading, ret); err != nil {
+		return ret, err
+	}
+
+	return ret, nil
 }
 
+func (ip *IPhone) Orientation(ctx context.Context) ([3]float64, error) {
+	var ret [3]float64
 
+	measurement, err := ip.reader.ReadString('\n')
+	if err != nil {
+		return ret, err
+	}
+
+	var imuReading iphoneMeasurement
+	err = json.Unmarshal([]byte(measurement), &imuReading)
+	if err != nil {
+		return ret, err
+	}
+
+	if err = measurementToOrientationArr(imuReading, ret); err != nil {
+		return ret, err
+	}
+
+	return ret, nil
+}
+
+func measurementToVelocityArr(meas iphoneMeasurement, arr [3]float64) error {
+	if x, err := strconv.ParseFloat(*meas.RotationRateX, 64); err != nil {
+		return err
+	} else {
+		arr[0] = x
+	}
+
+	if y, err := strconv.ParseFloat(*meas.RotationRateX, 64); err != nil {
+		return err
+	} else {
+		arr[1] = y
+	}
+
+	if z, err := strconv.ParseFloat(*meas.RotationRateX, 64); err != nil {
+		return err
+	} else {
+		arr[2] = z
+	}
+
+	return nil
+}
+
+func measurementToOrientationArr(meas iphoneMeasurement, arr [3]float64) error {
+	if x, err := strconv.ParseFloat(*meas.Pitch, 64); err != nil {
+		return err
+	} else {
+		arr[0] = x
+	}
+
+	if y, err := strconv.ParseFloat(*meas.Roll, 64); err != nil {
+		return err
+	} else {
+		arr[1] = y
+	}
+
+	if z, err := strconv.ParseFloat(*meas.Yaw, 64); err != nil {
+		return err
+	} else {
+		arr[2] = z
+	}
+
+	return nil
+}
