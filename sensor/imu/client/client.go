dbe8efa615faf54dc03ff1568450af513db28828
Gautham Varadarajan <gautham@viam.com>
2021-09-23 15:52:19 -0400

    checkpoint

---
+++ sensor/imu/client/client.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/go-errors/errors"
+
+	"go.viam.com/core/config"
+	"go.viam.com/core/grpc/client"
+	grpcclient "go.viam.com/core/grpc/client"
+	"go.viam.com/core/registry"
+	"go.viam.com/core/robot"
+	"go.viam.com/core/sensor"
+	"go.viam.com/core/sensor/imu"
+
+	"github.com/edaniels/golog"
+	"go.uber.org/multierr"
+)
+
+// ModelNameClient is used to register the sensor to a model name.
+const ModelNameClient = "imu"
+
+// init registers an imu.
+func init() {
+	registry.RegisterSensor(imu.Type, ModelNameClient, func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (sensor.Sensor, error) {
+		address := config.Host
+		if config.Port != 0 {
+			address = fmt.Sprintf("%s:%d", address, config.Port)
+		}
+		return New(ctx, address, logger)
+	})
+}
+
+func New(ctx context.Context, address string, logger golog.Logger) (imu.IMU, error) {
+	// still using gRPC client? seems wrong
+	robotClient, err := grpcclient.NewClient(ctx, address, logger)
+	if err != nil {
+		return nil, err
+	}
+	names := robotClient.SensorNames()
+	if len(names) == 0 {
+		return nil, multierr.Combine(errors.New("no sensor devices found"), robotClient.Close())
+	}
+	var imuDevice imu.IMU
+	namesStr := strings.Join(names, ", ")
+	for _, name := range names {
+		sensorDevice, ok := robotClient.SensorByName(name)
+		if !ok {
+			continue
+		}
+		if c, ok := sensorDevice.(imu.IMU); ok {
+			imuDevice = c
+			break
+		}
+	}
+	if imuDevice == nil {
+		// return nil, multierr.Combine(errors.New("no imu devices found"), robotClient.Close())
+		return nil, multierr.Combine(errors.New("no imu devices found, devices: "+namesStr), robotClient.Close())
+	}
+	// TODO: handle multiple imu device types?
+
+	return &wrappedImuDevice{imuDevice, robotClient}, nil
+}
+
+type wrappedImuDevice struct {
+	imu.IMU
+	robotClient *client.RobotClient
+}
+
+func (wcd *wrappedImuDevice) Close() error {
+	return wcd.robotClient.Close()
+}
---
+++ sensor/imu/client/client_test.go
@@ -0,0 +1,96 @@
+package client_test
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"go.viam.com/utils"
+
+	"go.viam.com/core/config"
+	"go.viam.com/core/grpc/server"
+	pb "go.viam.com/core/proto/api/v1"
+	"go.viam.com/core/registry"
+	"go.viam.com/core/sensor"
+	"go.viam.com/core/sensor/imu"
+	"go.viam.com/core/sensor/imu/client"
+	"go.viam.com/core/testutils/inject"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/test"
+	"google.golang.org/grpc"
+)
+
+func TestClient(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	listener1, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	listener2, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	gServer1 := grpc.NewServer()
+	injectRobot1 := &inject.Robot{}
+	gServer2 := grpc.NewServer()
+	injectRobot2 := &inject.Robot{}
+	pb.RegisterRobotServiceServer(gServer1, server.New(injectRobot1))
+	pb.RegisterRobotServiceServer(gServer2, server.New(injectRobot2))
+	injectRobot1.StatusFunc = func(ctx context.Context) (*pb.Status, error) {
+		return &pb.Status{}, nil
+	}
+	injectRobot2.StatusFunc = func(ctx context.Context) (*pb.Status, error) {
+		return &pb.Status{
+			Sensors: map[string]*pb.SensorStatus{
+				"sensor1": {
+					Type: imu.Type,
+				},
+			},
+		}, nil
+	}
+
+	go gServer1.Serve(listener1)
+	defer gServer1.Stop()
+	go gServer2.Serve(listener2)
+	defer gServer2.Stop()
+
+	f := registry.SensorLookup(imu.Type, client.ModelNameClient)
+	test.That(t, f, test.ShouldNotBeNil)
+	_, err = f(context.Background(), nil, config.Component{
+		Host: listener1.Addr().(*net.TCPAddr).IP.String(),
+		Port: listener1.Addr().(*net.TCPAddr).Port,
+	}, logger)
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "no sensor")
+
+	injectDev := &inject.IMU{}
+	rotationVector := [3]float64{5.2, 1.0, -0.9}
+	orientationVector := [3]float64{5.2, 1.0, -0.9}
+	injectDev.AngularVelocitiesFunc = func(ctx context.Context) ([3]float64, error) {
+		return rotationVector, nil
+	}
+	injectDev.OrientationFunc = func(ctx context.Context) ([3]float64, error) {
+		return orientationVector, nil
+	}
+	injectRobot2.SensorByNameFunc = func(name string) (sensor.Sensor, bool) {
+		return injectDev, true
+	}
+
+	dev, err := f(context.Background(), nil, config.Component{
+		Host: listener2.Addr().(*net.TCPAddr).IP.String(),
+		Port: listener2.Addr().(*net.TCPAddr).Port,
+	}, logger)
+	test.That(t, err, test.ShouldBeNil)
+	imuDev := dev.(imu.IMU)
+
+	// test angular velocity properly set
+	angularVelocities, err := imuDev.AngularVelocities(context.Background())
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, angularVelocities, test.ShouldEqual, rotationVector)
+
+	// test orientation properly set
+	orientation, err := imuDev.Orientation(context.Background())
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, orientation, test.ShouldEqual, orientationVector)
+
+	// test close
+	test.That(t, utils.TryClose(imuDev), test.ShouldBeNil)
+
+}
