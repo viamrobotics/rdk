2b9de4b778050cdb34c7aeaf4927e50eceb28f8c
Bijan Haney <bijan@viam.com>
2021-10-04 12:05:19 -0400

    fix test

--- robot/impl/moving_test.go
+++ robot/impl/moving_test.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"go.viam.com/test"
+
 	"go.viam.com/core/config"
 	robotimpl "go.viam.com/core/robot/impl"
 	"go.viam.com/core/spatialmath"
-	"go.viam.com/test"
 
 	"github.com/edaniels/golog"
 	"github.com/golang/geo/r3"
@@ -25,4 +26,5 @@ func TestArmThatDoesntMove(t *testing.T) {
 	cameraName := r.CameraNames()[0]
 	// point to move to in camera frame is (500, 0, 0)
 	err = robotimpl.MoveGripper(context.Background(), r, spatialmath.NewPoseFromPoint(r3.Vector{500, 0, 0}), cameraName)
+	test.That(t, err, test.ShouldBeError, "cannot set upper or lower bounds for nlopt, slice is empty")
 }
