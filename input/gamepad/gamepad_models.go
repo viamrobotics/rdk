d5ae0672cf5c8b069fe83fce95c7a7c146df0680
James Otting <james@viam.com>
2021-10-04 16:50:24 -0700

    Gamepad, part 2!

--- input/gamepad/gamepad_models.go
+++ input/gamepad/gamepad_models.go
@@ -1,14 +1,17 @@
+//go:build linux
+
 package gamepad
 
 import (
-		"go.viam.com/core/input"
-		"github.com/kenshaw/evdev"
-)
+	"github.com/kenshaw/evdev"
 
+	"go.viam.com/core/input"
+)
 
 // GamepadModels contains all the axes/button translations for each model
 var GamepadModels = map[string]Mapping{
-	"8BitDo Pro 2": Mapping{
+	// Wireless, X-input mode
+	"8BitDo Pro 2": {
 		Axes: map[evdev.AbsoluteType]input.ControlCode{
 			0: input.AbsoluteX,
 			1: input.AbsoluteY,
@@ -31,6 +34,85 @@ var GamepadModels = map[string]Mapping{
 			312: input.ButtonLThumb,
 			313: input.ButtonRThumb,
 			139: input.ButtonMenu,
+		},
+	},
+	// Wired, X-input mode 8BitDo Pro 2
+	"Microsoft X-Box 360 pad": {
+		Axes: map[evdev.AbsoluteType]input.ControlCode{
+			0:  input.AbsoluteX,
+			1:  input.AbsoluteY,
+			2:  input.AbsoluteZ,
+			3:  input.AbsoluteRX,
+			4:  input.AbsoluteRY,
+			5:  input.AbsoluteRZ,
+			16: input.AbsoluteHat0X,
+			17: input.AbsoluteHat0Y,
+		},
+		Buttons: map[evdev.KeyType]input.ControlCode{
+			304: input.ButtonSouth,
+			305: input.ButtonEast,
+			307: input.ButtonWest,
+			308: input.ButtonNorth,
+			310: input.ButtonLT,
+			311: input.ButtonRT,
+			314: input.ButtonSelect,
+			315: input.ButtonStart,
+			317: input.ButtonLThumb,
+			318: input.ButtonRThumb,
+			316: input.ButtonMenu,
+		},
+	},
+	// Xbox Series X|S, wireless mode
+	"Xbox Wireless Controller": {
+		Axes: map[evdev.AbsoluteType]input.ControlCode{
+			0:  input.AbsoluteX,
+			1:  input.AbsoluteY,
+			10: input.AbsoluteZ,
+			2:  input.AbsoluteRX,
+			5:  input.AbsoluteRY,
+			9:  input.AbsoluteRZ,
+			16: input.AbsoluteHat0X,
+			17: input.AbsoluteHat0Y,
+		},
+		Buttons: map[evdev.KeyType]input.ControlCode{
+			304: input.ButtonSouth,
+			305: input.ButtonEast,
+			307: input.ButtonWest,
+			308: input.ButtonNorth,
+			310: input.ButtonLT,
+			311: input.ButtonRT,
+			314: input.ButtonSelect,
+			315: input.ButtonStart,
+			317: input.ButtonLThumb,
+			318: input.ButtonRThumb,
+			316: input.ButtonMenu,
+			167: input.ButtonRecord,
+		},
+	},
+	// Xbox Series X|S, wired mode
+	"Microsoft Xbox One X pad": {
+		Axes: map[evdev.AbsoluteType]input.ControlCode{
+			0:  input.AbsoluteX,
+			1:  input.AbsoluteY,
+			2:  input.AbsoluteZ,
+			3:  input.AbsoluteRX,
+			4:  input.AbsoluteRY,
+			5:  input.AbsoluteRZ,
+			16: input.AbsoluteHat0X,
+			17: input.AbsoluteHat0Y,
+		},
+		Buttons: map[evdev.KeyType]input.ControlCode{
+			304: input.ButtonSouth,
+			305: input.ButtonEast,
+			307: input.ButtonWest,
+			308: input.ButtonNorth,
+			310: input.ButtonLT,
+			311: input.ButtonRT,
+			314: input.ButtonSelect,
+			315: input.ButtonStart,
+			317: input.ButtonLThumb,
+			318: input.ButtonRThumb,
+			316: input.ButtonMenu,
 		},
 	},
 }
--- input/input.go
+++ input/input.go
@@ -1,3 +1,4 @@
+//go:generate stringer -output=eventtypes_controlcodes_string.go -type=EventType,ControlCode
 package input
 
 import (
@@ -5,12 +6,10 @@ import (
 	"time"
 )
 
-
 // This is a logical "container" more than an actual device
 // Could be a single gamepad, or a collection of digitalInterrupts and analogReaders, some local, some remote
 type Controller interface {
 	Inputs(ctx context.Context) (map[ControlCode]Input, error)
-	EventDispatcher(ctx context.Context) 
 }
 
 type Input interface {
@@ -19,18 +18,19 @@ type Input interface {
 	RegisterControl(ctx context.Context, ctrlFunc ControlFunction, trigger EventType) error
 }
 
-type ControlFunction func(ctx context.Context, input Input, event Event) error
+type ControlFunction func(ctx context.Context, input Input, event Event)
 
 // Extensible for further events
 // Ex: LongPress might be useful on remote network connections where ButtonDown/ButtonUp timing might be too variable
 type EventType uint8
+
 const (
-	All EventType = 0
-	Connect EventType = 1
-	Disconnect EventType = 2 // If unplugged, or wireless/network times out
-	ButtonDown EventType = 3
-	ButtonUp EventType = 4
-	ButtonChange EventType = 5 // Both up and down for convinence
+	AllEvents         EventType = 0
+	Connect           EventType = 1
+	Disconnect        EventType = 2 // If unplugged, or wireless/network times out
+	ButtonDown        EventType = 3
+	ButtonUp          EventType = 4
+	ButtonChange      EventType = 5 // Both up and down for convinence
 	PositionChangeAbs EventType = 6 // Absolute position is reported, a la joysticks
 	PositionChangeRel EventType = 7 // Relative position is reported, a la mice, or simulating axes with up/down buttons
 )
@@ -39,36 +39,38 @@ const (
 // This allows one InputButton to provide multiple keys via codes, so a keyboard doesn't need to define 104 keys
 // Can be completely ignored for basic gamepad/arrows/select type use as well.
 type ControlCode uint32
+
 const (
 	// Reserving keys under 1000 for overlap with standard keycodes
 	// Axes
-	AbsoluteX             ControlCode = 1000
-	AbsoluteY             ControlCode = 1001
-	AbsoluteZ             ControlCode = 1002
-	AbsoluteRX            ControlCode = 1003
-	AbsoluteRY            ControlCode = 1004
-	AbsoluteRZ            ControlCode = 1005
-	AbsoluteHat0X         ControlCode = 1006
-	AbsoluteHat0Y         ControlCode = 1007
+	AbsoluteX     ControlCode = 1000
+	AbsoluteY     ControlCode = 1001
+	AbsoluteZ     ControlCode = 1002
+	AbsoluteRX    ControlCode = 1003
+	AbsoluteRY    ControlCode = 1004
+	AbsoluteRZ    ControlCode = 1005
+	AbsoluteHat0X ControlCode = 1006
+	AbsoluteHat0Y ControlCode = 1007
 
 	// Buttons
-	ButtonSouth ControlCode = 2000
-	ButtonEast ControlCode = 2001
-	ButtonWest ControlCode = 2002
-	ButtonNorth ControlCode = 2003
-	ButtonLT ControlCode = 2004
-	ButtonRT ControlCode = 2005
+	ButtonSouth  ControlCode = 2000
+	ButtonEast   ControlCode = 2001
+	ButtonWest   ControlCode = 2002
+	ButtonNorth  ControlCode = 2003
+	ButtonLT     ControlCode = 2004
+	ButtonRT     ControlCode = 2005
 	ButtonLThumb ControlCode = 2006
 	ButtonRThumb ControlCode = 2007
 	ButtonSelect ControlCode = 2008
-	ButtonStart ControlCode = 2009
-	ButtonMenu ControlCode = 2010
+	ButtonStart  ControlCode = 2009
+	ButtonMenu   ControlCode = 2010
+	ButtonRecord ControlCode = 2011
 )
 
 // The event returned to the registered ControlFunction
 type Event struct {
-	Time time.Time
+	Time  time.Time
 	Event EventType
-	Code ControlCode // Key or Axis code
-	Value float64 // 0 or 1 for buttons, -1.0 to +1.0 for axes
+	Code  ControlCode // Key or Axis code
+	Value float64     // 0 or 1 for buttons, -1.0 to +1.0 for axes
 }
