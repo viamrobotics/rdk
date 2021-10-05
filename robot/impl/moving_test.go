b538233bcbc5a5bb6d66349b5e582881db32a5e3
Bijan Haney <bijan@viam.com>
2021-10-05 19:31:12 -0400

    adds moving tests

--- robot/impl/moving_test.go
+++ robot/impl/moving_test.go
@@ -28,6 +28,22 @@ func TestArmThatDoesntMove(t *testing.T) {
 	test.That(t, err, test.ShouldBeError, "cannot set upper or lower bounds for nlopt, slice is empty")
 }
 
+func TestMoveGripperFrame(t *testing.T) {
+	cfg, err := config.Read("data/moving_arm.json")
+	test.That(t, err, test.ShouldBeNil)
+	logger := golog.NewTestLogger(t)
+
+	r, err := robotimpl.New(context.Background(), cfg, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	gripperName := r.GripperNames()[0]
+
+	// can't move gripper with respect to gripper
+	goal := spatialmath.NewPoseFromPoint(r3.Vector{0, 0, 200})
+	err = robotimpl.MoveGripper(context.Background(), r, goal, gripperName)
+	test.That(t, err, test.ShouldBeError, "cannot move gripper with respect to gripper frame, gripper will always be at its own origin")
+}
+
 func TestMovingArm(t *testing.T) {
 	cfg, err := config.Read("data/moving_arm.json")
 	test.That(t, err, test.ShouldBeNil)
@@ -36,15 +52,13 @@ func TestMovingArm(t *testing.T) {
 	r, err := robotimpl.New(context.Background(), cfg, logger)
 	test.That(t, err, test.ShouldBeNil)
 
-	moveName := r.GripperNames()[0]
-
 	// don't move at all
-	goal := spatialmath.NewPoseFromPoint(r3.Vector{0, 0, 0})
-	err = robotimpl.MoveGripper(context.Background(), r, goal, moveName)
+	goal := spatialmath.NewPoseFromPoint(r3.Vector{0, 0, 200}) // gripper has implicit 200 z offset
+	err = robotimpl.MoveGripper(context.Background(), r, goal, r.ArmNames()[0])
 	test.That(t, err, test.ShouldBeNil)
 
 	// move to a different location
-	goal = spatialmath.NewPoseFromPoint(r3.Vector{0, 0, 200})
-	err = robotimpl.MoveGripper(context.Background(), r, goal, moveName)
+	goal = spatialmath.NewPoseFromPoint(r3.Vector{500, 0, 500})
+	err = robotimpl.MoveGripper(context.Background(), r, goal, "world")
 	test.That(t, err, test.ShouldBeNil)
 }
