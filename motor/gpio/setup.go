8dcdada7e2226a753e75aa4d9c19b34f3a58cb98
Eliot Horowitz <eliothorowitz@gmail.com>
2021-10-07 14:18:31 -0400

    fix pi compile and gpio registration

--- motor/gpio/setup.go
+++ motor/gpio/setup.go
@@ -1,13 +1,4 @@
-//go:build pi
-// +build pi
-
-package pi
-
-// #include <stdlib.h>
-// #include <pigpio.h>
-// #include "pi.h"
-// #cgo LDFLAGS: -lpigpio
-import "C"
+package gpio
 
 import (
 	"context"
@@ -21,32 +12,41 @@ import (
 	"go.viam.com/core/motor"
 	"go.viam.com/core/registry"
 	"go.viam.com/core/robot"
-	"go.viam.com/core/utils"
 )
+
+const modelName = "gpio"
 
 // init registers a pi motor based on pigpio.
 func init() {
-	registry.RegisterMotor(modelName, registry.Motor{Constructor: func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (motor.Motor, error) {
+
+	x := registry.Motor{Constructor: func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (motor.Motor, error) {
 		actualBoard, motorConfig, err := getBoardFromRobotConfig(r, config)
 		if err != nil {
 			return nil, err
 		}
 
-		m, err := board.NewGPIOMotor(actualBoard, *motorConfig, logger)
+		m, err := NewMotor(actualBoard, *motorConfig, logger)
 		if err != nil {
 			return nil, err
 		}
 
-		m, err = board.WrapMotorWithEncoder(ctx, actualBoard, config, *motorConfig, m, logger)
+		m, err = WrapMotorWithEncoder(ctx, actualBoard, config, *motorConfig, m, logger)
 		if err != nil {
 			return nil, err
 		}
 		return m, nil
-	}})
+	}}
+
+	registry.RegisterMotor(modelName, x)
 	motor.RegisterConfigAttributeConverter(modelName)
+
+	// For backwards compatibility
+	registry.RegisterMotor("pi", x)
+	motor.RegisterConfigAttributeConverter("pi")
+
 }
 
-func getBoardFromRobotConfig(r robot.Robot, config config.Component) (*piPigpio, *motor.Config, error) {
+func getBoardFromRobotConfig(r robot.Robot, config config.Component) (board.Board, *motor.Config, error) {
 	motorConfig := config.ConvertedAttributes.(*motor.Config)
 	if motorConfig.BoardName == "" {
 		return nil, nil, errors.New("expected board name in config for motor")
@@ -55,10 +55,5 @@ func getBoardFromRobotConfig(r robot.Robot, config config.Component) (*piPigpio,
 	if !ok {
 		return nil, nil, fmt.Errorf("expected to find board %q", motorConfig.BoardName)
 	}
-	// Note(erd): this would not be needed if encoders were a component
-	actualBoard, ok := utils.UnwrapProxy(b).(*piPigpio)
-	if !ok {
-		return nil, nil, errors.New("expected board to be a pi board")
-	}
-	return actualBoard, motorConfig, nil
+	return b, motorConfig, nil
 }
