b0811fa6181792e492b7e4b3f20fd635abd7f637
Cheuk Tse <cheuk@viam.com>
2021-10-19 14:45:13 -0400

    update control js

--- web/frontend/src/control.js
+++ web/frontend/src/control.js
@@ -1,6 +1,8 @@
 const { grpc } = require("@improbable-eng/grpc-web");
 window.robotApi = require('proto/api/v1/robot_pb.js');
 const { RobotServiceClient } = require('proto/api/v1/robot_pb_service.js');
+window.metadataApi = require('proto/api/service/v1/metadata_pb.js');
+const { MetadataServiceClient } = require('proto/api/service/v1/metadata_pb_service.js');
 const { dial } = require("@viamrobotics/rpc");
 window.THREE = require("three/build/three.module.js")
 window.pcdLib = require("three/examples/jsm/loaders/PCDLoader.js")
@@ -33,6 +35,7 @@ if (window.webrtcEnabled) {
 		try {
 			let cc = await dial(window.webrtcSignalingAddress, window.webrtcHost, rtcConfig);
 			window.robotService = new RobotServiceClient(window.webrtcHost, { transport: cc.transportFactory() });
+			window.metadataService = new MetadataServiceClient(window.webrtcHost, { transport: cc.transportFactory() });
 		} catch (e) {
 			console.error("error dialing:", e);
 			throw e;
@@ -43,6 +46,7 @@ if (window.webrtcEnabled) {
 } else {
 	const url = `${location.protocol}//${location.hostname}${location.port ? ':' + location.port : ''}`;
 	window.robotService = new RobotServiceClient(url);
+	window.metadataService = new MetadataServiceClient(url);
 	pResolve(undefined);
 }
 
