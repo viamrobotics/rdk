7e836b4120faed52e9383fc672638726c908003b
Peter LoVerso <biotinker@gmail.com>
2021-11-05 09:02:44 -0700

    add path test for ur5

--- motionplan/motionPlanner_test.go
+++ motionplan/motionPlanner_test.go
@@ -4,37 +4,33 @@ import (
 	"context"
 	"fmt"
 
-	"math"
-	"math/rand"
-
 	"runtime"
 	"testing"
 
 	"go.viam.com/core/kinematics"
 	pb "go.viam.com/core/proto/api/v1"
 	frame "go.viam.com/core/referenceframe"
-	spatial "go.viam.com/core/spatialmath"
 	"go.viam.com/core/utils"
 
-	"github.com/golang/geo/r3"
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
 )
 
 var (
-	home = frame.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0, 0})
+	home7 = frame.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0, 0})
+	home6 = frame.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0})
 	nCPU = runtime.NumCPU()
 )
 
-// This should test a simple linear motion
+//~ // This should test a simple linear motion
 func TestSimpleMotion(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	m, err := kinematics.ParseJSONFile(utils.ResolveFile("robots/xarm/xArm7_kinematics.json"))
 	test.That(t, err, test.ShouldBeNil)
-	ik, err := kinematics.CreateCombinedIKSolver(m, logger, nCPU)
+	
+	mp, err := NewCBiRRTMotionPlanner(m, logger, 4)
 	test.That(t, err, test.ShouldBeNil)
-
-	mp := &linearMotionPlanner{solver: ik, frame: m}
+	mp.AddConstraint("orientation", NewPoseConstraint())
 
 	// Test ability to arrive at another position
 	pos := &pb.ArmPosition{
@@ -43,39 +39,29 @@ func TestSimpleMotion(t *testing.T) {
 		Z:  200,
 		OZ: -1,
 	}
-	solution, err := mp.Plan(context.Background(), pos, home)
+	path, err := mp.Plan(context.Background(), pos, home7)
 	test.That(t, err, test.ShouldBeNil)
 
-	fmt.Println(solution)
+	fmt.Println(path)
 }
 
 // This should test a simple linear motion
-func TestSample(t *testing.T) {
-	rnd := rand.New(rand.NewSource(1))
-	m, err := kinematics.ParseJSONFile(utils.ResolveFile("robots/xarm/xArm7_kinematics.json"))
+func TestSimpleMotionUR5(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	m, err := kinematics.ParseJSONFile(utils.ResolveFile("robots/universalrobots/ur5e.json"))
+	test.That(t, err, test.ShouldBeNil)
+	mp, err := NewCBiRRTMotionPlanner(m, logger, 4)
 	test.That(t, err, test.ShouldBeNil)
 
-	// goal orientation of no rotation
-	//~ goalOrient := spatial.NewZeroPose()
-	ov := &spatial.OrientationVector{math.Pi / 2, 0, 0, 1}
-	ov.Normalize()
-	goalOrient := spatial.NewPoseFromOrientationVector(r3.Vector{100, 100, 200}, ov)
-	
-	match := 0
-	nSamp := 1000000
-	for i := 0; i < nSamp; i++ {
-		inputs := randPos(m, rnd)
-		rPos, _ := m.Transform(inputs)
-		dist := spatial.PoseDelta(goalOrient, rPos)
-		//~ fmt.Println(dist)
-		if (dist[3]*dist[3]) + (dist[4]*dist[4]) + (dist[5]*dist[5]) < 0.01 {
-			match++
-		}
-		if i % 10000 == 0 {
-			fmt.Println(match, " / ", i)
-		}
+	// Test ability to arrive at another position
+	pos := &pb.ArmPosition{
+		X:  -400,
+		Y:  -250,
+		Z:  200,
+		OZ: -1,
 	}
-	fmt.Println(match, " / ", nSamp)
+	path, err := mp.Plan(context.Background(), pos, home6)
+	test.That(t, err, test.ShouldBeNil)
+
+	fmt.Println(path)
 }
-
-
