91f770af8cf26ec5859ea7f59ee23673cc5cabd0
Eliot Horowitz <eliothorowitz@gmail.com>
2021-11-09 11:50:25 -0500

    use weights correctly

--- robots/yahboom/dofbot_test.go
+++ robots/yahboom/dofbot_test.go
@@ -8,29 +8,24 @@ import (
 
 	"go.viam.com/test"
 
-	"go.viam.com/core/kinematics"
 	pb "go.viam.com/core/proto/api/v1"
 	"go.viam.com/core/referenceframe"
 )
 
 func TestJointConfig(t *testing.T) {
-	test.That(t, joints[0].toDegrees(joints[0].toHw(0)), test.ShouldAlmostEqual, 0)
-	test.That(t, joints[0].toDegrees(joints[0].toHw(45)), test.ShouldAlmostEqual, 45)
-	test.That(t, joints[0].toDegrees(joints[0].toHw(90)), test.ShouldAlmostEqual, 90)
-	test.That(t, joints[0].toDegrees(joints[0].toHw(135)), test.ShouldAlmostEqual, 135)
+	test.That(t, joints[0].toDegrees(joints[0].toHw(-45)), test.ShouldAlmostEqual, -45, .1)
+	test.That(t, joints[0].toDegrees(joints[0].toHw(0)), test.ShouldAlmostEqual, 0, .1)
+	test.That(t, joints[0].toDegrees(joints[0].toHw(45)), test.ShouldAlmostEqual, 45, .1)
+	test.That(t, joints[0].toDegrees(joints[0].toHw(90)), test.ShouldAlmostEqual, 90, .1)
+	test.That(t, joints[0].toDegrees(joints[0].toHw(135)), test.ShouldAlmostEqual, 135, .1)
 	test.That(t, joints[0].toDegrees(joints[0].toHw(200)), test.ShouldAlmostEqual, 200, .1)
-	test.That(t, joints[0].toDegrees(joints[0].toHw(300)), test.ShouldAlmostEqual, 300, .1)
-	test.That(t, joints[0].toDegrees(joints[0].toHw(350)), test.ShouldAlmostEqual, 350, .1)
 }
 
 func TestDofBotIK(t *testing.T) {
 	ctx := context.Background()
 	logger := golog.NewTestLogger(t)
 
-	model, err := dofbotModel()
-	test.That(t, err, test.ShouldBeNil)
-
-	ik, err := kinematics.CreateCombinedIKSolver(model, logger, 4)
+	ik, err := createDofBotSolver(logger)
 	test.That(t, err, test.ShouldBeNil)
 
 	goal := pb.Pose{X: 206.59, Y: -1.57, Z: 253.05, Theta: -180, OX: -.53, OY: 0, OZ: .85}
