ae11586d17cfddeb462edac55ea5e4db6f2f3a4d
Eliot Horowitz <eliothorowitz@gmail.com>
2021-11-12 09:43:50 -0500

    serialize model

--- kinematics/solvableFrameSystem.go
+++ kinematics/solvableFrameSystem.go
@@ -123,6 +123,10 @@ func (sf *solverFrame) MarshalJSON() ([]byte, error) {
 	return nil, errors.New("cannot serialize solverFrame")
 }
 
+func (sf *solverFrame) AlmostEquals(otherFrame frame.Frame) bool {
+	return false
+}
+
 // uniqInPlaceSlice will deduplicate the values in a slice using in-place replacement on the slice. This is faster than
 // a solution using append().
 // This function does not remove anything from the input slice, but it does rearrange the elements.
