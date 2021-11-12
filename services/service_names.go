e5ec8f83bd69e12789b2e09587a31eadd734cb90
Gautham Varadarajan <gautham@viam.com>
2021-11-12 13:33:34 -0500

    oops

--- services/service_names.go
+++ services/service_names.go
@@ -3,8 +3,8 @@ package services
 const (
 	// FrameSystemName is the name key associated with the FrameSystemService
 	FrameSystemName = "frame_system"
-	// NavigationSystemName is the name key associated with the NavigationSystemService
+	// NavigationServiceName is the name key associated with the NavigationService
 	NavigationServiceName = "navigation"
-	// ObjectManipulationSystemName is the name key associated with the ObjectManipulationSystemService
+	// ObjectManipulationServiceName is the name key associated with the ObjectManipulationService
 	ObjectManipulationServiceName = "object_manipulation"
 )
