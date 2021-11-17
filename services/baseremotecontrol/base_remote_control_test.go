53fc59485eedd07c27bc1b147ed82fe7147be414
jeremyhyde-viam <92594844+jeremyhyde-viam@users.noreply.github.com>
2021-11-16 20:32:53 -0500

    added test cases and git issues

--- services/baseremotecontrol/base_remote_control_test.go
+++ services/baseremotecontrol/base_remote_control_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/go-errors/errors"
+
 	"go.viam.com/core/base"
 	"go.viam.com/core/config"
 	"go.viam.com/core/input"
@@ -32,17 +34,65 @@ func TestBaseRemoteControl(t *testing.T) {
 		return nil
 	}
 
+	cfg := &Config{
+		BaseName:            "",
+		InputControllerName: "",
+		JoyStickModeName:    "",
+	}
+
+	// New base_remote_control check
 	svc, err := New(ctx, fakeRobot,
 		config.Service{
 			Name:                "base_remote_control",
 			Type:                "base_remote_control",
-			ConvertedAttributes: &Config{},
+			ConvertedAttributes: cfg,
 		},
 		rlog.Logger)
-
 	test.That(t, err, test.ShouldBeNil)
 
-	// Starting point: high speed, straight
+	cfg.JoyStickModeName = "triggerSpeedControl"
+	svc1, err := New(ctx, fakeRobot,
+		config.Service{
+			Name:                "base_remote_control",
+			Type:                "base_remote_control",
+			ConvertedAttributes: cfg,
+		},
+		rlog.Logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	// Controller import failure
+	fakeRobot.InputControllerByNameFunc = func(name string) (input.Controller, bool) {
+		return fakeController, false
+	}
+
+	_, err = New(ctx, fakeRobot,
+		config.Service{
+			Name:                "base_remote_control",
+			Type:                "base_remote_control",
+			ConvertedAttributes: cfg,
+		},
+		rlog.Logger)
+	test.That(t, err, test.ShouldBeError, errors.Errorf("no input controller named %q", cfg.InputControllerName))
+
+	// Base import failure
+	fakeRobot.BaseByNameFunc = func(name string) (base.Base, bool) {
+		return &fake.Base{}, false
+	}
+
+	_, err = New(ctx, fakeRobot,
+		config.Service{
+			Name:                "base_remote_control",
+			Type:                "base_remote_control",
+			ConvertedAttributes: cfg,
+		},
+		rlog.Logger)
+	test.That(t, err, test.ShouldBeError, errors.Errorf("no base named %q", cfg.BaseName))
+
+	// Start checks
+	err = svc.start(ctx)
+	test.That(t, err, test.ShouldBeNil)
+
+	// Math tests: Starting point - high speed, straight
 	t.Run("full speed to half speed", func(t *testing.T) {
 		millisPerSec, degsPerSec := svc.speedAndAngleMathMag(0.4, 0.0, 1.0, 0.0)
 		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.4, .001)
@@ -67,7 +117,7 @@ func TestBaseRemoteControl(t *testing.T) {
 		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.4, .001)
 	})
 
-	// Starting point: low speed, arcing
+	// Math tests: Starting point - low speed, arcing
 	t.Run("slow arc to straight", func(t *testing.T) {
 		millisPerSec, degsPerSec := svc.speedAndAngleMathMag(0.4, 0.0, 0.2, 0.2)
 		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.4, .001)
@@ -91,5 +141,80 @@ func TestBaseRemoteControl(t *testing.T) {
 		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.1, .001)
 		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.4, .001)
 	})
+
+	// Controller event by mode
+	t.Run("controller events one joy stick control mode", func(t *testing.T) {
+		i := svc.controllerInputs()
+		test.That(t, i[0], test.ShouldEqual, input.AbsoluteX)
+		test.That(t, i[1], test.ShouldEqual, input.AbsoluteY)
+	})
+
+	t.Run("controller events one trigger speed control mode", func(t *testing.T) {
+		i := svc1.controllerInputs()
+		test.That(t, i[0], test.ShouldEqual, input.AbsoluteX)
+		test.That(t, i[1], test.ShouldEqual, input.AbsoluteZ)
+		test.That(t, i[2], test.ShouldEqual, input.AbsoluteRZ)
+	})
+
+	// Event tests
+	eventX := input.Event{
+		Control: input.AbsoluteX,
+		Value:   1.0,
+	}
+
+	eventY := input.Event{
+		Control: input.AbsoluteY,
+		Value:   1.0,
+	}
+
+	eventZ := input.Event{
+		Control: input.AbsoluteZ,
+		Value:   1.0,
+	}
+
+	eventRZ := input.Event{
+		Control: input.AbsoluteRZ,
+		Value:   1.0,
+	}
+
+	t.Run("trigger speed control mode for input X", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.triggerSpeedEvent(eventX, 0.5, 0.6)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.5, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 1.0, .001)
+	})
+
+	t.Run("trigger speed control mode for input Z", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.triggerSpeedEvent(eventZ, 0.8, 0.8)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.75, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.8, .001)
+	})
+
+	t.Run("trigger speed control mode for input RZ", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.triggerSpeedEvent(eventRZ, 0.8, 0.8)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.85, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.8, .001)
+	})
+
+	t.Run("one joy stick control mode for input X", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.oneJoyStickEvent(eventX, 0.5, 0.6)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.5, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 1.0, .001)
+	})
+
+	t.Run("one joy stick control mode for input Y", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.oneJoyStickEvent(eventY, 0.5, 0.6)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 1.0, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.6, .001)
+	})
+
+	t.Run("one joy stick control mode for input Z", func(t *testing.T) {
+		millisPerSec, degsPerSec := svc.oneJoyStickEvent(eventZ, 0.5, 0.6)
+		test.That(t, millisPerSec, test.ShouldAlmostEqual, 0.5, .001)
+		test.That(t, degsPerSec, test.ShouldAlmostEqual, 0.6, .001)
+	})
+
+	// Close out check
+	err = svc.Close(ctx)
+	test.That(t, err, test.ShouldBeNil)
 
 }
