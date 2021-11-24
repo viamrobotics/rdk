73990b180f53db769b2b6337a2ad56bfef5661f4
Maxim Pertsov <maxim.pertsov@gmail.com>
2021-11-24 15:51:18 -0500

    add subtype identifying consts and methods

--- component/imu/imu.go
+++ component/imu/imu.go
@@ -5,13 +5,24 @@ package imu
 import (
 	"context"
 
+	"go.viam.com/core/resource"
 	"go.viam.com/core/spatialmath"
-
-	"go.viam.com/core/sensor"
 )
 
-// Type is the identifier of an IMU.
-const Type = "imu"
+// SubtypeName is a constant that identifies the component resource subtype string "arm"
+const SubtypeName = resource.SubtypeName("imu")
+
+// Subtype is a constant that identifies the component resource subtype
+var Subtype = resource.NewSubtype(
+	resource.ResourceNamespaceCore,
+	resource.ResourceTypeComponent,
+	SubtypeName,
+)
+
+// Named is a helper for getting the named IMU's typed resource name
+func Named(name string) resource.Name {
+	return resource.NewFromSubtype(Subtype, name)
+}
 
 // An IMU represents a sensor that can report AngularVelocity and Orientation measurements.
 type IMU interface {
