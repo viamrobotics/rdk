61a2b61fa23eee56aaf85df5fad4bd1769f2e155
Maxim Pertsov <maxim.pertsov@gmail.com>
2021-11-24 14:12:14 -0500

    update imports

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -28,6 +28,7 @@ import (
 
 	"go.viam.com/core/action"
 	"go.viam.com/core/board"
+	"go.viam.com/core/component/imu"
 	functionrobot "go.viam.com/core/function/robot"
 	functionvm "go.viam.com/core/function/vm"
 	"go.viam.com/core/grpc"
@@ -41,7 +42,6 @@ import (
 	"go.viam.com/core/sensor/compass"
 	"go.viam.com/core/sensor/forcematrix"
 	"go.viam.com/core/sensor/gps"
-	"go.viam.com/core/sensor/imu"
 	"go.viam.com/core/services"
 	"go.viam.com/core/services/framesystem"
 	"go.viam.com/core/services/navigation"
