ca15c17e7f2d8057e929b953b01c9630389e7c6d
Peter LoVerso <biotinker@gmail.com>
2021-11-29 08:37:42 -0800

    Update constraint and metric types

--- kinematics/metrics.go
+++ kinematics/metrics.go
@@ -5,27 +5,11 @@ import (
 )
 
 // Metric defines a distance function to be minimized by gradient descent algorithms
-type Metric interface {
-	Distance(spatial.Pose, spatial.Pose) float64
-}
-
-type flexibleMetric struct {
-	f func(spatial.Pose, spatial.Pose) float64
-}
-
-// Distance returns the computed distance
-func (m *flexibleMetric) Distance(from, to spatial.Pose) float64 {
-	return m.f(from, to)
-}
-
-// NewBasicMetric wraps a function that then can be used as a Metric
-func NewBasicMetric(f func(spatial.Pose, spatial.Pose) float64) Metric {
-	return &flexibleMetric{f}
-}
+type Metric func(spatial.Pose, spatial.Pose) float64
 
 // NewSquaredNormMetric is the default distance function between two poses to be used for gradient descent
 func NewSquaredNormMetric() Metric {
-	return &flexibleMetric{sqNormDist}
+	return sqNormDist
 }
 func sqNormDist(from, to spatial.Pose) float64 {
 	return SquaredNorm(spatial.PoseDelta(from, to))
--- kinematics/metrics_test.go
+++ kinematics/metrics_test.go
@@ -15,21 +15,20 @@ func TestDefaultMetric(t *testing.T) {
 	p1 := spatial.NewPoseFromPoint(r3.Vector{0, 0, 0})
 	p2 := spatial.NewPoseFromPoint(r3.Vector{0, 0, 10})
 
-	d1 := sqMet.Distance(p1, p1)
+	d1 := sqMet(p1, p1)
 	test.That(t, d1, test.ShouldAlmostEqual, 0)
-	d2 := sqMet.Distance(p1, p2)
+	d2 := sqMet(p1, p2)
 	test.That(t, d2, test.ShouldAlmostEqual, 100)
 }
 
 func TestBasicMetric(t *testing.T) {
-	f := func(from, to spatial.Pose) float64 {
+	sqMet := func(from, to spatial.Pose) float64 {
 		return SquaredNorm(spatial.PoseDelta(from, to)[:3])
 	}
-	sqMet := NewBasicMetric(f)
 	p1 := spatial.NewPoseFromPoint(r3.Vector{0, 0, 0})
 	p2 := spatial.NewPoseFromPoint(r3.Vector{0, 0, 10})
-	d1 := sqMet.Distance(p1, p1)
+	d1 := sqMet(p1, p1)
 	test.That(t, d1, test.ShouldAlmostEqual, 0)
-	d2 := sqMet.Distance(p1, p2)
+	d2 := sqMet(p1, p2)
 	test.That(t, d2, test.ShouldAlmostEqual, 100)
 }
