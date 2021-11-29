3b38928bbf7f8d46cc74f9ac8e9e1ed278922516
Maxim Pertsov <maxim.pertsov@gmail.com>
2021-11-29 13:15:10 -0500

    add imuClient to ResourceByName
    CR: https://github.com/viamrobotics/core/pull/302/files#r758532605

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -400,14 +400,21 @@ func (rc *RobotClient) ServiceByName(name string) (interface{}, bool) {
 
 // ResourceByName returns resource by name.
 func (rc *RobotClient) ResourceByName(name resource.Name) (interface{}, bool) {
-	c := registry.ResourceSubtypeLookup(name.Subtype)
-	if c == nil || c.RPCClient == nil {
-		// registration doesn't exist
-		return nil, false
+	switch name.Subtype {
+	case imu.Subtype:
+		sensorType := rc.sensorTypes[name.Name]
+		sc := &sensorClient{rc, name.Name, sensorType}
+		return &imuClient{sc}, true
+	default:
+		c := registry.ResourceSubtypeLookup(name.Subtype)
+		if c == nil || c.RPCClient == nil {
+			// registration doesn't exist
+			return nil, false
+		}
+		// pass in conn
+		resourceClient := c.RPCClient(rc.conn, name.Name, rc.Logger())
+		return resourceClient, true
 	}
-	// pass in conn
-	resourceClient := c.RPCClient(rc.conn, name.Name, rc.Logger())
-	return resourceClient, true
 }
 
 // Refresh manually updates the underlying parts of the robot based
