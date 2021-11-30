d3a006d37b4bed23b4860ec8a345d2e3dd6158cd
john nicholson <john@viam.com>
2021-11-30 12:39:31 -0800

    cleanup and comments

--- samples/gpsTest/cmd.go
+++ samples/gpsTest/cmd.go
@@ -9,33 +9,16 @@ import (
 	"flag"
 	"fmt"
 
-	// "io"
 	"io/ioutil"
 	"net/http"
-
-	// "math"
-	// "strconv"
-	// "strings"
-	// "time"
-
-	// "github.com/go-errors/errors"
-
-	// "go.uber.org/multierr"
 
 	"go.viam.com/utils"
 
 	"go.viam.com/core/config"
-	// pb "go.viam.com/core/proto/api/v1"
-	// "go.viam.com/core/robot"
 	robotimpl "go.viam.com/core/robot/impl"
-	// "go.viam.com/core/services/navigation"
 	"go.viam.com/core/services/web"
-	// "go.viam.com/core/sensor/gps"
-	// "go.viam.com/core/spatialmath"
-	// coreutils "go.viam.com/core/utils"
 	webserver "go.viam.com/core/web/server"
 
-	// "github.com/de-bkg/gognss/pkg/ntrip"
 	"github.com/edaniels/golog"
 	"github.com/go-gnss/ntrip"
 	slib "github.com/jacobsa/go-serial/serial"
@@ -47,45 +30,30 @@ func main() {
 	utils.ContextualMain(mainWithArgs, logger)
 }
 
-func connectClient(req *http.Request, i int) (resp *http.Response, err error, j int) {
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Printf("error making NTRIP request: %s\n", err)
+//generate sourcetable for different mountpoints on gpsnetwork
+// good for debugging or finding a new working mountpoint
+func ExampleNewClientRequest_sourcetable(urlStr string, username string, password string) (err error) {
 
-		return nil, err, i
-	} else {
-		j = 1
-	}
-	return resp, nil, j
-}
-func ExampleNewClientRequest_sourcetable(urlStr string) (err error) {
-	// var resp http.DefaultClient
-	// req, _ := ntrip.NewClientRequest("http://rtn.dot.ny.gov:8080")
+	// talk to the gps network, looking for mount points
 	req, _ := ntrip.NewClientRequest(urlStr)
-	// resp, err := http.DefaultClient.Do(req)
-
-	req.SetBasicAuth("viam", "checkmate")
+	req.SetBasicAuth(username, password)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("error making NTRIP request: %s\n", err)
 		return err
 	}
-	// resp, err, i := connectClient(req, 0)
+
 	defer resp.Body.Close()
-	// for i != 1 {
-	// 	fmt.Println("Trying Again")
-	// 	resp, err, i = connectClient(req, 0)
-	// }
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("received non-200 response code: %d", resp.StatusCode)
 	}
-
+	// read all of the mount points we got back
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("error reading from response body")
 	}
-
+	// print the mountpoints
 	sourcetable, _ := ntrip.ParseSourcetable(string(data))
 	fmt.Printf("caster has %d mountpoints available\n", len(sourcetable.Mounts))
 	for i := 0; i < len(sourcetable.Mounts); i++ {
@@ -95,20 +63,14 @@ func ExampleNewClientRequest_sourcetable(urlStr string) (err error) {
 	return nil
 }
 
-func ExampleNewClientRequest() (err error) {
-	// readMount, writeMount := io.Pipe()
-	req, _ := ntrip.NewClientRequest("http://rtn.dot.ny.gov:8082/NJI2")
-	// req, _ := ntrip.NewClientRequest("http://rtn.dot.ny.gov:8080/near_msm")
-	// c := http.Client{Timeout: time.Duration(1) * time.Second}
-	// resp, _ := c.Get("https://www.google.com")//ntrip.NewClientRequest("http://rtn.dot.ny.gov:8080/near_msm")
-	req.SetBasicAuth("viam", "checkmate")
+// setup the system as a ntrip client, and write corrections to the gps via serial
+func ExampleNewClientRequest(urlStr string, username string, password string, gpsport string) (err error) {
+
+	// talk to the gps network, looking for mount points
+	req, _ := ntrip.NewClientRequest(urlStr)
+	req.SetBasicAuth(username, password)
 	resp, err := http.DefaultClient.Do(req)
-	// resp, err, i := connectClient(req, 0)
-	// defer resp.Body.Close()
-	// for i != 1 {
-	// 	fmt.Println("Trying Again")
-	// 	resp, err, i = connectClient(req, 0)
-	// }
+
 	if err != nil {
 		fmt.Printf("error making NTRIP request: %s\n", err)
 		return err
@@ -119,20 +81,20 @@ func ExampleNewClientRequest() (err error) {
 		return err
 	}
 	fmt.Printf("Status Code : %d \n", resp.StatusCode)
-	// for{
+	fmt.Println("Response Body: ", resp.Body)
 
-	fmt.Println("Response Body 2: ", resp.Body)
-
+	// setup port to write to
 	options := slib.OpenOptions{
 		BaudRate:        38400,
 		DataBits:        8,
 		StopBits:        1,
 		MinimumReadSize: 1,
 	}
-	options.PortName = "/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00" // "/dev/serial0" //
+	options.PortName = gpsport
 	port, err := slib.Open(options)
 	w := bufio.NewWriter(port)
 
+	// Read from resp.Body until EOF
 	r := io.TeeReader(resp.Body, w)
 	io.ReadAll(r)
 
@@ -141,9 +103,8 @@ func ExampleNewClientRequest() (err error) {
 		return err
 	}
 
-	// fmt.Println("we might have written")
 	return nil
-	// Read from resp.Body until EOF
+
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) (err error) {
@@ -162,8 +123,11 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) (err 
 	webOpts := web.NewOptions()
 	webOpts.Insecure = true
 
-	// go ExampleNewClientRequest_sourcetable("http://rtn.dot.ny.gov:8080")
-	// go ExampleNewClientRequest_sourcetable("http://rtn.dot.ny.gov:8082")
-	go ExampleNewClientRequest()
+	gpsport := "/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00"
+
+	// go ExampleNewClientRequest_sourcetable("http://rtn.dot.ny.gov:8080","viam","checkmate")
+	// go ExampleNewClientRequest_sourcetable("http://rtn.dot.ny.gov:8082","viam","checkmate")
+	go ExampleNewClientRequest("http://rtn.dot.ny.gov:8082/NJI2", "viam", "checkmate", gpsport)
+
 	return webserver.RunWeb(ctx, myRobot, webOpts, logger)
 }
