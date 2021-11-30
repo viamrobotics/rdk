d062ebe24e3dee898c4729dafd0d7e31f7514e74
Peter LoVerso <biotinker@gmail.com>
2021-11-30 12:21:37 -0800

    lint

--- kinematics/metrics.go
+++ kinematics/metrics.go
@@ -16,7 +16,7 @@ func sqNormDist(from, to spatial.Pose) float64 {
 	// Increase weight for orientation since it's a small number
 	for i, v := range d {
 		if i > 2 {
-			d[i] = v*10
+			d[i] = v * 10
 		}
 	}
 	return SquaredNorm(d)
