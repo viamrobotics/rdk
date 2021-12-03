c6bdd5d7865ed0040d0aa14ee5f9cbcabe0b2656
john nicholson <john@viam.com>
2021-12-03 10:45:49 -0800

    moved ntrip client to nmea/serial

--- sensor/gps/nmea/serial.go
+++ sensor/gps/nmea/serial.go
@@ -5,10 +5,13 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net/http"
 	"sync"
 
 	"github.com/adrianmo/go-nmea"
 	"github.com/edaniels/golog"
+	"github.com/go-gnss/ntrip"
+	slib "github.com/jacobsa/go-serial/serial"
 	geo "github.com/kellydunn/golang-geo"
 	"go.viam.com/utils"
 
@@ -27,20 +30,33 @@ func init() {
 }
 
 type serialNMEAGPS struct {
-	mu     sync.RWMutex
-	dev    io.ReadWriteCloser
-	logger golog.Logger
-
-	data gpsData
+	mu        sync.RWMutex
+	dev       io.ReadWriteCloser
+	logger    golog.Logger
+	urlStr    string
+	username  string
+	password  string
+	writepath string
+	wbaud     int
+	data      gpsData
 
 	cancelCtx               context.Context
 	cancelFunc              func()
 	activeBackgroundWorkers sync.WaitGroup
 }
 
-const pathAttrName = "path"
+const (
+	pathAttrName  = "path"
+	ntripAttrName = "ntrip"
+	userAttrName  = "username"
+	passAttrName  = "password"
+	wpathAttrName = "writepath"
+	wbaudAttrName = "writebaud"
+)
 
 func newSerialNMEAGPS(config config.Component, logger golog.Logger) (gps.GPS, error) {
+	var g *serialNMEAGPS
+	var wbaud int
 	serialPath := config.Attributes.String(pathAttrName)
 	if serialPath == "" {
 		return nil, fmt.Errorf("serialNMEAGPS expected non-empty string for %q", pathAttrName)
@@ -51,13 +67,72 @@ func newSerialNMEAGPS(config config.Component, logger golog.Logger) (gps.GPS, er
 	}
 
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
-
-	g := &serialNMEAGPS{dev: dev, cancelCtx: cancelCtx, cancelFunc: cancelFunc, logger: logger}
-	g.Start()
+	ntripPath := config.Attributes.String(ntripAttrName)
+	if ntripPath != "" {
+		username := config.Attributes.String(userAttrName)
+		password := config.Attributes.String(passAttrName)
+		writepath := config.Attributes.String(wpathAttrName)
+		wbaud = config.Attributes.Int(wbaudAttrName, wbaud)
+		if writepath == "" {
+			writepath = serialPath
+		}
+		g := &serialNMEAGPS{urlStr: ntripPath, username: username, password: password, writepath: writepath, wbaud: wbaud, dev: dev, cancelCtx: cancelCtx, cancelFunc: cancelFunc, logger: logger}
+		g.Start()
+		g.NtripClientRequest()
+	} else {
+		g := &serialNMEAGPS{dev: dev, cancelCtx: cancelCtx, cancelFunc: cancelFunc, logger: logger}
+		g.Start()
+	}
 
 	return g, nil
 }
+func (g *serialNMEAGPS) NtripClientRequest() {
+	var resp *http.Response
+	var err error
+	g.activeBackgroundWorkers.Add(1)
+	utils.PanicCapturingGo(func() {
+		defer g.activeBackgroundWorkers.Done()
 
+		// talk to the gps network, looking for mount points
+		req, _ := ntrip.NewClientRequest(g.urlStr)
+		req.SetBasicAuth(g.username, g.password)
+
+		reconnFlag := 1
+		for reconnFlag == 1 {
+			resp, err = http.DefaultClient.Do(req)
+			if err != nil {
+				g.logger.Debugf("error making NTRIP request: %s\n", err)
+			} else if resp.StatusCode != http.StatusOK {
+				g.logger.Debugf("received non-200 response code: %d", resp.StatusCode)
+			} else {
+				reconnFlag = 0
+			}
+		}
+
+		defer resp.Body.Close()
+
+		// setup port to write to
+		options := slib.OpenOptions{
+			BaudRate:        uint(g.wbaud),
+			DataBits:        8,
+			StopBits:        1,
+			MinimumReadSize: 1,
+		}
+
+		options.PortName = g.writepath
+		port, err := slib.Open(options)
+		w := bufio.NewWriter(port)
+
+		// Read from resp.Body until EOF
+		r := io.TeeReader(resp.Body, w)
+		io.ReadAll(r)
+
+		if err != nil {
+			g.logger.Fatalf("Error with RTCM stream: %s\n", err)
+
+		}
+	})
+}
 func (g *serialNMEAGPS) Start() {
 	g.activeBackgroundWorkers.Add(1)
 	utils.PanicCapturingGo(func() {
@@ -74,7 +149,7 @@ func (g *serialNMEAGPS) Start() {
 			if err != nil {
 				g.logger.Fatalf("can't read gps serial %s", err)
 			}
-
+			// fmt.Println(line)
 			// Update our struct's gps data in-place
 			g.mu.Lock()
 			err = parseAndUpdate(line, &g.data)
@@ -91,6 +166,7 @@ func (g *serialNMEAGPS) Readings(ctx context.Context) ([]interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return []interface{}{loc}, nil
 }
 
