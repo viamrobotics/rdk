5750a2837a609156c477dbbb0cff00769bd3b99e
Cheuk Tse <cheuk@viam.com>
2021-12-03 19:56:05 -0500

    add client, server

--- component/camera/register/register.go
+++ component/camera/register/register.go
@@ -2,9 +2,16 @@
 package register
 
 import (
+	"context"
+
+	"github.com/edaniels/golog"
 	"go.viam.com/core/component/camera"
+	pb "go.viam.com/core/proto/api/component/v1"
 	"go.viam.com/core/registry"
 	"go.viam.com/core/resource"
+	"go.viam.com/core/subtype"
+	"go.viam.com/utils/rpc/dialer"
+	rpcserver "go.viam.com/utils/rpc/server"
 
 	_ "go.viam.com/core/component/camera/fake"        // for camera
 	_ "go.viam.com/core/component/camera/gopro"       // for camera
@@ -16,6 +23,17 @@ func init() {
 	registry.RegisterResourceSubtype(camera.Subtype, registry.ResourceSubtype{
 		Reconfigurable: func(r interface{}) (resource.Reconfigurable, error) {
 			return camera.WrapWithReconfigurable(r)
+		},
+		RegisterRPCService: func(ctx context.Context, rpcServer rpcserver.Server, subtypeSvc subtype.Service) error {
+			return rpcServer.RegisterServiceServer(
+				ctx,
+				&pb.CameraService_ServiceDesc,
+				camera.NewServer(subtypeSvc),
+				pb.RegisterCameraServiceHandlerFromEndpoint,
+			)
+		},
+		RPCClient: func(conn dialer.ClientConn, name string, logger golog.Logger) interface{} {
+			return camera.NewClientFromConn(conn, name, logger)
 		},
 	})
 }
---
+++ component/camera/server.go
@@ -0,0 +1,244 @@
+// Package camera contains a gRPC based camera service server.
+package camera
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+
+	"github.com/pkg/errors"
+	"google.golang.org/genproto/googleapis/api/httpbody"
+
+	"go.viam.com/core/grpc"
+	"go.viam.com/core/pointcloud"
+	commonpb "go.viam.com/core/proto/api/common/v1"
+	pb "go.viam.com/core/proto/api/component/v1"
+	"go.viam.com/core/rimage"
+	"go.viam.com/core/subtype"
+	"go.viam.com/core/vision/segmentation"
+)
+
+// subtypeServer implements the contract from camera.proto
+type subtypeServer struct {
+	pb.UnimplementedCameraServiceServer
+	s subtype.Service
+}
+
+// NewServer constructs an camera gRPC service server.
+func NewServer(s subtype.Service) pb.CameraServiceServer {
+	return &subtypeServer{s: s}
+}
+
+// getCamera returns the camera specified, nil if not.
+func (s *subtypeServer) getCamera(name string) (Camera, error) {
+	resource := s.s.Resource(name)
+	if resource == nil {
+		return nil, errors.Errorf("no camera with name (%s)", name)
+	}
+	camera, ok := resource.(Camera)
+	if !ok {
+		return nil, errors.Errorf("resource with name (%s) is not a camera", name)
+	}
+	return camera, nil
+}
+
+// Frame returns a frame from a camera of the underlying robot. A specific MIME type
+// can be requested but may not necessarily be the same one returned.
+func (s *subtypeServer) Frame(ctx context.Context, req *pb.CameraServiceFrameRequest) (*pb.CameraServiceFrameResponse, error) {
+	camera, err := s.getCamera(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	img, release, err := camera.Next(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if release != nil {
+			release()
+		}
+	}()
+
+	// choose the best/fastest representation
+	if req.MimeType == grpc.MimeTypeViamBest {
+		iwd, ok := img.(*rimage.ImageWithDepth)
+		if ok && iwd.Depth != nil && iwd.Color != nil {
+			req.MimeType = grpc.MimeTypeRawIWD
+		} else {
+			req.MimeType = grpc.MimeTypeRawRGBA
+		}
+	}
+
+	bounds := img.Bounds()
+	resp := pb.CameraServiceFrameResponse{
+		MimeType: req.MimeType,
+		DimX:     int64(bounds.Dx()),
+		DimY:     int64(bounds.Dy()),
+	}
+
+	var buf bytes.Buffer
+	switch req.MimeType {
+	case grpc.MimeTypeRawRGBA:
+		resp.MimeType = grpc.MimeTypeRawRGBA
+		imgCopy := image.NewRGBA(bounds)
+		draw.Draw(imgCopy, bounds, img, bounds.Min, draw.Src)
+		buf.Write(imgCopy.Pix)
+	case grpc.MimeTypeRawIWD:
+		resp.MimeType = grpc.MimeTypeRawIWD
+		iwd, ok := img.(*rimage.ImageWithDepth)
+		if !ok {
+			return nil, errors.Errorf("want %s but don't have %T", grpc.MimeTypeRawIWD, iwd)
+		}
+		err := iwd.RawBytesWrite(&buf)
+		if err != nil {
+			return nil, fmt.Errorf("error writing %s: %w", grpc.MimeTypeRawIWD, err)
+		}
+
+	case grpc.MimeTypeBoth:
+		resp.MimeType = grpc.MimeTypeBoth
+		iwd, ok := img.(*rimage.ImageWithDepth)
+		if !ok {
+			return nil, errors.Errorf("want %s but don't have %T", grpc.MimeTypeBoth, iwd)
+		}
+		if iwd.Color == nil || iwd.Depth == nil {
+			return nil, errors.Errorf("for %s need depth and color info", grpc.MimeTypeBoth)
+		}
+		if err := rimage.EncodeBoth(iwd, &buf); err != nil {
+			return nil, err
+		}
+	case grpc.MimeTypeJPEG:
+		resp.MimeType = grpc.MimeTypeJPEG
+		if err := jpeg.Encode(&buf, img, nil); err != nil {
+			return nil, err
+		}
+	case "", grpc.MimeTypePNG:
+		resp.MimeType = grpc.MimeTypePNG
+		if err := png.Encode(&buf, img); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.Errorf("do not know how to encode %q", req.MimeType)
+	}
+	resp.Frame = buf.Bytes()
+	return &resp, nil
+}
+
+// RenderFrame renders a frame from a camera of the underlying robot to an HTTP response. A specific MIME type
+// can be requested but may not necessarily be the same one returned.
+func (s *subtypeServer) RenderFrame(ctx context.Context, req *pb.CameraServiceRenderFrameRequest) (*httpbody.HttpBody, error) {
+	resp, err := s.Frame(ctx, (*pb.CameraServiceFrameRequest)(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return &httpbody.HttpBody{
+		ContentType: resp.MimeType,
+		Data:        resp.Frame,
+	}, nil
+}
+
+// PointCloud returns a frame from a camera of the underlying robot. A specific MIME type
+// can be requested but may not necessarily be the same one returned.
+func (s *subtypeServer) PointCloud(ctx context.Context, req *pb.CameraServicePointCloudRequest) (*pb.CameraServicePointCloudResponse, error) {
+	camera, err := s.getCamera(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	pc, err := camera.NextPointCloud(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	err = pc.ToPCD(&buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CameraServicePointCloudResponse{
+		MimeType: grpc.MimeTypePCD,
+		Frame:    buf.Bytes(),
+	}, nil
+}
+
+// ObjectPointClouds returns an array of objects from the frame from a camera of the underlying robot. A specific MIME type
+// can be requested but may not necessarily be the same one returned. Also returns a Vector3 array of the center points of each object.
+func (s *subtypeServer) ObjectPointClouds(ctx context.Context, req *pb.CameraServiceObjectPointCloudsRequest) (*pb.CameraServiceObjectPointCloudsResponse, error) {
+	camera, err := s.getCamera(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	pc, err := camera.NextPointCloud(ctx)
+	if err != nil {
+		return nil, err
+	}
+	config := segmentation.ObjectConfig{
+		MinPtsInPlane:    int(req.MinPointsInPlane),
+		MinPtsInSegment:  int(req.MinPointsInSegment),
+		ClusteringRadius: req.ClusteringRadius,
+	}
+	segments, err := segmentation.NewObjectSegmentation(ctx, pc, config)
+	if err != nil {
+		return nil, err
+	}
+
+	frames := make([][]byte, segments.N())
+	centers := make([]pointcloud.Vec3, segments.N())
+	boundingBoxes := make([]pointcloud.BoxGeometry, segments.N())
+	for i, seg := range segments.Objects {
+		var buf bytes.Buffer
+		err := seg.ToPCD(&buf)
+		if err != nil {
+			return nil, err
+		}
+		frames[i] = buf.Bytes()
+		centers[i] = seg.Center
+		boundingBoxes[i] = seg.BoundingBox
+	}
+
+	return &pb.CameraServiceObjectPointCloudsResponse{
+		MimeType:      grpc.MimeTypePCD,
+		Frames:        frames,
+		Centers:       pointsToProto(centers),
+		BoundingBoxes: boxesToProto(boundingBoxes),
+	}, nil
+}
+
+func pointToProto(p pointcloud.Vec3) *commonpb.Vector3 {
+	return &commonpb.Vector3{
+		X: p.X,
+		Y: p.Y,
+		Z: p.Z,
+	}
+}
+
+func pointsToProto(vs []pointcloud.Vec3) []*commonpb.Vector3 {
+	pvs := make([]*commonpb.Vector3, 0, len(vs))
+	for _, v := range vs {
+		pvs = append(pvs, pointToProto(v))
+	}
+	return pvs
+}
+
+func boxToProto(b pointcloud.BoxGeometry) *commonpb.BoxGeometry {
+	return &commonpb.BoxGeometry{
+		Width:  b.Width,
+		Length: b.Length,
+		Depth:  b.Depth,
+	}
+}
+
+func boxesToProto(bs []pointcloud.BoxGeometry) []*commonpb.BoxGeometry {
+	pbs := make([]*commonpb.BoxGeometry, 0, len(bs))
+	for _, v := range bs {
+		pbs = append(pbs, boxToProto(v))
+	}
+	return pbs
+}
