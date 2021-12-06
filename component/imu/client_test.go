9c990a5e806e3f2038b11ed8d8c6817b075015cb
Maxim Pertsov <maxim.pertsov@gmail.com>
2021-12-06 15:33:26 -0500

    factor out imu client tests

---
+++ component/imu/client_test.go
@@ -0,0 +1,135 @@
+package imu_test
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"go.viam.com/utils"
+	rpcclient "go.viam.com/utils/rpc/client"
+	"go.viam.com/utils/rpc/dialer"
+
+	"go.viam.com/core/component/imu"
+	pb "go.viam.com/core/proto/api/component/v1"
+	"go.viam.com/core/resource"
+	"go.viam.com/core/subtype"
+	"go.viam.com/core/testutils/inject"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/test"
+	"google.golang.org/grpc"
+
+	viamgrpc "go.viam.com/core/grpc"
+)
+
+func TestClient(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	listener1, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	gServer := grpc.NewServer()
+
+	imu1 := "imu1"
+
+	av := &pb.AngularVelocity{X: 1, Y: 2, Z: 3}
+	ea := &pb.EulerAngles{Roll: 4, Pitch: 5, Yaw: 6}
+
+	injectIMU := &inject.IMU{}
+	injectIMU.AngularVelocityFunc = func(ctx context.Context) (*pb.AngularVelocity, error) {
+		return av, nil
+	}
+	injectIMU.OrientationFunc = func(ctx context.Context) (*pb.EulerAngles, error) {
+		return ea, nil
+	}
+
+	imuSvc, err := subtype.New((map[resource.Name]interface{}{imu.Named(imu1): injectIMU}))
+	test.That(t, err, test.ShouldBeNil)
+	pb.RegisterIMUServiceServer(gServer, imu.NewServer(imuSvc))
+
+	go gServer.Serve(listener1)
+	defer gServer.Stop()
+
+	// failing
+	t.Run("Failing client", func(t *testing.T) {
+		cancelCtx, cancel := context.WithCancel(context.Background())
+		cancel()
+		_, err = imu.NewClient(cancelCtx, imu1, listener1.Addr().String(), rpcclient.DialOptions{Insecure: true}, logger)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldContainSubstring, "canceled")
+	})
+
+	// working
+	imu1Client, err := imu.NewClient(context.Background(), imu1, listener1.Addr().String(), rpcclient.DialOptions{Insecure: true}, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	t.Run("IMU client 1", func(t *testing.T) {
+		av1, err := imu1Client.AngularVelocity(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, av1.String(), test.ShouldResemble, av.String())
+
+		ea1, err := imu1Client.Orientation(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, ea1.String(), test.ShouldResemble, ea.String())
+	})
+
+	t.Run("IMU client 2", func(t *testing.T) {
+		conn, err := viamgrpc.Dial(context.Background(), listener1.Addr().String(), rpcclient.DialOptions{Insecure: true}, logger)
+		test.That(t, err, test.ShouldBeNil)
+		imu1Client2 := imu.NewClientFromConn(conn, imu1, logger)
+		test.That(t, err, test.ShouldBeNil)
+
+		av2, err := imu1Client2.AngularVelocity(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, av2.String(), test.ShouldResemble, av.String())
+
+		ea2, err := imu1Client2.Orientation(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, ea2.String(), test.ShouldResemble, ea.String())
+
+		test.That(t, conn.Close(), test.ShouldBeNil)
+	})
+	test.That(t, utils.TryClose(imu1Client), test.ShouldBeNil)
+}
+
+func TestClientDialerOption(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	listener, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	gServer := grpc.NewServer()
+	injectIMU := &inject.IMU{}
+	imu1 := "imu1"
+
+	imuSvc, err := subtype.New((map[resource.Name]interface{}{imu.Named(imu1): injectIMU}))
+	test.That(t, err, test.ShouldBeNil)
+	pb.RegisterIMUServiceServer(gServer, imu.NewServer(imuSvc))
+
+	go gServer.Serve(listener)
+	defer gServer.Stop()
+
+	td := &trackingDialer{Dialer: dialer.NewCachedDialer()}
+	ctx := dialer.ContextWithDialer(context.Background(), td)
+	client1, err := imu.NewClient(ctx, imu1, listener.Addr().String(), rpcclient.DialOptions{Insecure: true}, logger)
+	test.That(t, err, test.ShouldBeNil)
+	client2, err := imu.NewClient(ctx, imu1, listener.Addr().String(), rpcclient.DialOptions{Insecure: true}, logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, td.dialCalled, test.ShouldEqual, 2)
+
+	err = utils.TryClose(client1)
+	test.That(t, err, test.ShouldBeNil)
+	err = utils.TryClose(client2)
+	test.That(t, err, test.ShouldBeNil)
+}
+
+type trackingDialer struct {
+	dialer.Dialer
+	dialCalled int
+}
+
+func (td *trackingDialer) DialDirect(ctx context.Context, target string, opts ...grpc.DialOption) (dialer.ClientConn, error) {
+	td.dialCalled++
+	return td.Dialer.DialDirect(ctx, target, opts...)
+}
+
+func (td *trackingDialer) DialFunc(proto string, target string, f func() (dialer.ClientConn, error)) (dialer.ClientConn, error) {
+	td.dialCalled++
+	return td.Dialer.DialFunc(proto, target, f)
+}
