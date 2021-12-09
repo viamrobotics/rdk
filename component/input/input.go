7ee4741587d53a4799ae31cf6c961639474562cc
Cheuk Tse <cheuk@viam.com>
2021-12-09 17:46:09 -0500

    add some helpers

--- component/input/input.go
+++ component/input/input.go
@@ -3,8 +3,30 @@ package input
 
 import (
 	"context"
+	"sync"
 	"time"
+
+	"github.com/pkg/errors"
+	"go.viam.com/utils"
+
+	"go.viam.com/core/resource"
+	"go.viam.com/core/rlog"
 )
+
+// SubtypeName is a constant that identifies the component resource subtype string input
+const SubtypeName = resource.SubtypeName("input_controller")
+
+// Subtype is a constant that identifies the component resource subtype
+var Subtype = resource.NewSubtype(
+	resource.ResourceNamespaceCore,
+	resource.ResourceTypeComponent,
+	SubtypeName,
+)
+
+// Named is a helper for getting the named input's typed resource name
+func Named(name string) resource.Name {
+	return resource.NewFromSubtype(Subtype, name)
+}
 
 // Controller is a logical "container" more than an actual device
 // Could be a single gamepad, or a collection of digitalInterrupts and analogReaders, a keyboard, etc.
@@ -80,4 +102,82 @@ type Event struct {
 type Injectable interface {
 	// InjectEvent allows directly sending an Event (such as a button press) from external code
 	InjectEvent(ctx context.Context, event Event) error
+}
+
+// WrapWithReconfigurable wraps a Controller with a reconfigurable and locking interface
+func WrapWithReconfigurable(r interface{}) (resource.Reconfigurable, error) {
+	c, ok := r.(Controller)
+	if !ok {
+		return nil, errors.Errorf("expected resource to be Controller but got %T", r)
+	}
+	if reconfigurable, ok := c.(*reconfigurableInputController); ok {
+		return reconfigurable, nil
+	}
+	return &reconfigurableInputController{actual: c}, nil
+}
+
+var (
+	_ = Controller(&reconfigurableInputController{})
+	_ = resource.Reconfigurable(&reconfigurableInputController{})
+)
+
+type reconfigurableInputController struct {
+	mu     sync.RWMutex
+	actual Controller
+}
+
+func (c *reconfigurableInputController) ProxyFor() interface{} {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.actual
+}
+
+func (c *reconfigurableInputController) Controls(ctx context.Context) ([]Control, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.actual.Controls(ctx)
+}
+
+func (c *reconfigurableInputController) LastEvents(ctx context.Context) (map[Control]Event, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.actual.LastEvents(ctx)
+}
+
+// InjectEvent allows directly sending an Event (such as a button press) from external code
+func (c *reconfigurableInputController) InjectEvent(ctx context.Context, event Event) error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	iActual, ok := c.actual.(Injectable)
+	if !ok {
+		return errors.New("controller is not Injectable")
+	}
+	return iActual.InjectEvent(ctx, event)
+}
+
+func (c *reconfigurableInputController) RegisterControlCallback(ctx context.Context, control Control, triggers []EventType, ctrlFunc ControlFunction) error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.actual.RegisterControlCallback(ctx, control, triggers, ctrlFunc)
+}
+
+func (c *reconfigurableInputController) Close() error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return utils.TryClose(c.actual)
+}
+
+// Reconfigure reconfigures the resource
+func (c *reconfigurableInputController) Reconfigure(newController resource.Reconfigurable) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	actual, ok := newController.(*reconfigurableInputController)
+	if !ok {
+		return errors.Errorf("expected new Controller to be %T but got %T", c, newController)
+	}
+	if err := utils.TryClose(c.actual); err != nil {
+		rlog.Logger.Errorw("error closing old", "error", err)
+	}
+	c.actual = actual.actual
+	return nil
 }
