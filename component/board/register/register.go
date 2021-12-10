13aae44b61567671ce5c325f5094b7cdcbd255a5
Maxim Pertsov <maxim.pertsov@gmail.com>
2021-12-10 11:28:14 -0500

    add register

---
+++ component/board/register/register.go
@@ -0,0 +1,38 @@
+// Package register registers all relevant Boards and also subtype specific functions
+package register
+
+import (
+	"go.viam.com/core/component/board"
+
+	// TODO(maximpertsov): add fake board
+	// _ "go.viam.com/core/component/board/fake"
+
+	_ "go.viam.com/core/component/board/arduino" // for board
+	_ "go.viam.com/core/component/board/jetson"  // for board
+	_ "go.viam.com/core/component/board/numato"  // for board
+	_ "go.viam.com/core/component/board/pi"      // for board
+
+	"go.viam.com/core/registry"
+	"go.viam.com/core/resource"
+)
+
+func init() {
+	registry.RegisterResourceSubtype(board.Subtype, registry.ResourceSubtype{
+		Reconfigurable: func(r interface{}) (resource.Reconfigurable, error) {
+			return board.WrapWithReconfigurable(r)
+		},
+		// TODO(maximpertsov): add this when migrating contracts
+		//
+		// 	RegisterRPCService: func(ctx context.Context, rpcServer rpcserver.Server, subtypeSvc subtype.Service) error {
+		// 		return rpcServer.RegisterServiceServer(
+		// 			ctx,
+		// 			&componentpb.BoardService_ServiceDesc,
+		// 			board.NewServer(subtypeSvc),
+		// 			componentpb.RegisterBoardServiceHandlerFromEndpoint,
+		// 		)
+		// 	},
+		// 	RPCClient: func(conn dialer.ClientConn, name string, logger golog.Logger) interface{} {
+		// 		return board.NewClientFromConn(conn, name, logger)
+		// 	},
+	})
+}
