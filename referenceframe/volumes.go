42f2de2b8b94b10ecde91c4658c4032341200eac
raybjork <raymond@viam.com>
2021-12-14 14:04:01 -0500

    comment new structs

--- referenceframe/volumes.go
+++ referenceframe/volumes.go
@@ -2,10 +2,12 @@ package referenceframe
 
 import spatial "go.viam.com/core/spatialmath"
 
+// VolumeCreator provides a common way to instantiate Volumes
 type VolumeCreator interface {
 	NewVolume(spatial.Pose) (Volume, error)
 }
 
+// Volume is an entry point with which to access all types of collision geometries
 type Volume interface {
 	CollidesWith(Volume) (bool, error)
 }
