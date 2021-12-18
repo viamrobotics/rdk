87aa528c56496b889b19ce6d64ded9eb42e3e8d6
Nicolas Menard <nicolas.p.menard@gmail.com>
2021-12-17 21:26:42 -0500

    Lint

--- component/motor/gpio/motor_encoder.go
+++ component/motor/gpio/motor_encoder.go
@@ -13,10 +13,10 @@ import (
 	"github.com/pkg/errors"
 	"go.viam.com/utils"
 
-	"go.viam.com/core/control"
 	"go.viam.com/rdk/component/board"
 	"go.viam.com/rdk/component/motor"
 	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/control"
 )
 
 var (
