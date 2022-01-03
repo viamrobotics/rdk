04d90702afefb8b3f91ba0cd50fab5f3a9ab3095
Nicolas Menard <nicolas.p.menard@gmail.com>
2022-01-03 10:27:35 -0500

    pulsePerRevolution -> ticksPerRevolution

--- control/encoder_speed.go
+++ control/encoder_speed.go
@@ -13,7 +13,7 @@ type encoderToRPM struct {
 	mu                 sync.Mutex
 	cfg                ControlBlockConfig
 	y                  []Signal
-	pulsesPerReolution int
+	ticksPerRevolution int
 	prevEncCount       int
 	logger             golog.Logger
 }
@@ -28,19 +28,19 @@ func newEncoderSpeed(config ControlBlockConfig, logger golog.Logger) (ControlBlo
 
 func (b *encoderToRPM) Next(ctx context.Context, x []Signal, dt time.Duration) ([]Signal, bool) {
 	currEncCount := int(x[0].GetSignalValueAt(0))
-	b.y[0].SetSignalValueAt(0, (float64(currEncCount-b.prevEncCount)/float64(b.pulsesPerReolution))*60.0/(dt.Seconds()))
+	b.y[0].SetSignalValueAt(0, (float64(currEncCount-b.prevEncCount)/float64(b.ticksPerRevolution))*60.0/(dt.Seconds()))
 	b.prevEncCount = currEncCount
 	return b.y, true
 }
 
 func (b *encoderToRPM) reset() error {
-	if !b.cfg.Attribute.Has("PulsesPerRevolution") {
-		return errors.Errorf("encoderToRPM block %s doesn't have a PulsesPerRevolution field", b.cfg.Name)
+	if !b.cfg.Attribute.Has("ticks_per_revolution") {
+		return errors.Errorf("encoderToRPM block %s doesn't have a ticks_per_revolution field", b.cfg.Name)
 	}
 	if len(b.cfg.DependsOn) != 1 {
 		return errors.Errorf("invalid number of inputs for encoderToRPM block %s expected 1 got %d", b.cfg.Name, len(b.cfg.DependsOn))
 	}
-	b.pulsesPerReolution = b.cfg.Attribute.Int("PulsesPerRevolution", 0)
+	b.ticksPerRevolution = b.cfg.Attribute.Int("ticks_per_revolution", 0)
 	b.prevEncCount = 0
 	b.y = make([]Signal, 1)
 	b.y[0] = makeSignal(b.cfg.Name, 1)
