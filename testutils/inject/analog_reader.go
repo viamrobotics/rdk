ced1d7c02f4a324277ab9f4c9fcd6388b0156199
Maxim Pertsov <maxim.pertsov@gmail.com>
2022-01-04 01:06:38 -0500

    WIP: update server tests

--- testutils/inject/analog_reader.go
+++ testutils/inject/analog_reader.go
@@ -10,12 +10,22 @@ import (
 type AnalogReader struct {
 	board.AnalogReader
 	ReadFunc func(ctx context.Context) (int, error)
+	readCap  []interface{}
 }
 
 // Read calls the injected Read or the real version.
 func (a *AnalogReader) Read(ctx context.Context) (int, error) {
+	a.readCap = []interface{}{ctx}
 	if a.ReadFunc == nil {
 		return a.AnalogReader.Read(ctx)
 	}
 	return a.ReadFunc(ctx)
+}
+
+func (a *AnalogReader) ReadCap() []interface{} {
+	if a == nil {
+		return nil
+	}
+	defer func() { a.readCap = nil }()
+	return a.readCap
 }
