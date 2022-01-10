87cbc34cb99b77b8f2f5328fff2642d92ffa5d9e
louisenaud <louise.naud@gmail.com>
2022-01-10 17:01:22 -0500

    Added utils for homography estimation

---
+++ utils/ransac.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// SelectNIndicesWithoutReplacement select N random indices from [0,nMax] without replacement (no duplicate indices).
+func SelectNIndicesWithoutReplacement(nSamples, nMax int) ([]int, error) {
+	if nSamples > nMax {
+		return nil, fmt.Errorf("number of elements to be sampled greater than total number of elements: %v > %v", nSamples, nMax)
+	}
+	a := make([]int, nMax)
+	for i := range a {
+		a[i] = i
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+	return a[:nSamples], nil
+}
