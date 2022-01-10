df190d9e859be95b601222b776a04fe329c1833f
louisenaud <louise.naud@gmail.com>
2022-01-10 16:33:56 -0500

    Added homography estimation

---
+++ rimage/transform/homographies.go
@@ -0,0 +1,360 @@
+package transform
+
+import (
+	"log"
+	"math"
+
+	"github.com/go-errors/errors"
+	"github.com/golang/geo/r2"
+	"github.com/golang/geo/r3"
+	"github.com/gonum/floats"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+
+	"go.viam.com/rdk/rimage"
+	"go.viam.com/rdk/utils"
+)
+
+// ComputeNormalizationMatFromSliceVecs computes the normalization matrix from a slice of vectors
+// from Multiple View Geometry. Richard Hartley and Andrew Zisserman. Alg 4.2 p109.
+func ComputeNormalizationMatFromSliceVecs(pts []r2.Point) *mat.Dense {
+	out := mat.NewDense(3, 3, nil)
+	xs, ys := rimage.SliceVecsToXsYs(pts)
+	avgX := stat.Mean(xs, nil)
+	avgY := stat.Mean(ys, nil)
+	diffX := make([]float64, len(xs))
+	copy(diffX, xs)
+	floats.AddConst(avgX, diffX)
+	diffY := make([]float64, len(ys))
+	copy(diffY, ys)
+	floats.AddConst(avgY, diffY)
+	norms := make([]float64, len(xs))
+
+	for i := 0; i < len(pts); i++ {
+		norms[i] = math.Sqrt(diffX[i]*diffX[i] + diffY[i]*diffY[i])
+	}
+	scaleFactor := math.Sqrt(2) / stat.Mean(norms, nil)
+	out.Set(0, 0, scaleFactor)
+	out.Set(0, 2, -scaleFactor*avgX)
+	out.Set(1, 1, scaleFactor)
+	out.Set(1, 2, -scaleFactor*avgY)
+	out.Set(2, 2, 1)
+	return out
+}
+
+// EstimateExactHomographyFrom8Points computes the exact homography from 2 sets of 4 matching points
+// from Multiple View Geometry. Richard Hartley and Andrew Zisserman. Alg 4.1 p91.
+func EstimateExactHomographyFrom8Points(s1, s2 []r2.Point, normalize bool) (*mat.Dense, error) {
+	if len(s1) != 4 {
+		panic("slice s1 must have 4 points each")
+	}
+	if len(s2) != 4 {
+		panic("slice s2 must have 4 points each")
+	}
+	st1 := s1
+	st2 := s2
+	norm1 := ComputeNormalizationMatFromSliceVecs(s1)
+	norm2 := ComputeNormalizationMatFromSliceVecs(s1)
+	if normalize {
+		st1 = ApplyNormalizationMat(norm1, s1)
+		st2 = ApplyNormalizationMat(norm2, s2)
+	}
+
+	x1 := st1[0].X
+	y1 := st1[0].Y
+	X1 := st2[0].X
+	Y1 := st2[0].Y
+
+	x2 := st1[1].X
+	y2 := st1[1].Y
+	X2 := st2[1].X
+	Y2 := st2[1].Y
+
+	x3 := st1[2].X
+	y3 := st1[2].Y
+	X3 := st2[2].X
+	Y3 := st2[2].Y
+
+	x4 := st1[3].X
+	y4 := st1[3].Y
+	X4 := st2[3].X
+	Y4 := st2[3].Y
+	// create homography system
+	a := []float64{
+		x1, y1, 1, 0, 0, 0, -X1 * x1, -X1 * y1,
+		0, 0, 0, x1, y1, 1, -Y1 * x1, -Y1 * y1,
+		x2, y2, 1, 0, 0, 0, -X2 * x2, -X2 * y2,
+		0, 0, 0, x2, y2, 1, -Y2 * x2, -Y2 * y2,
+		x3, y3, 1, 0, 0, 0, -X3 * x3, -X3 * y3,
+		0, 0, 0, x3, y3, 1, -Y3 * x3, -Y3 * y3,
+		x4, y4, 1, 0, 0, 0, -X4 * x4, -X4 * y4,
+		0, 0, 0, x4, y4, 1, -Y4 * x4, -Y4 * y4,
+	}
+
+	// Set matrices with data from slice
+	A := mat.NewDense(8, 8, a)
+	bSlice := []float64{X1, Y1, X2, Y2, X3, Y3, X4, Y4}
+	b := mat.NewDense(8, 1, bSlice)
+
+	// If matrix A is invertible, get the least square solution
+	if mat.Det(A) != 0 {
+		// x := mat.NewDense(8, 1, nil)
+		// Perform an SVD retaining all singular vectors.
+		var svd mat.SVD
+		ok := svd.Factorize(A, mat.SVDFull)
+		if !ok {
+			log.Fatal("failed to factorize A")
+		}
+
+		// Determine the rank of the A matrix with a near zero condition threshold.
+		const rcond = 1e-15
+		rank := svd.Rank(rcond)
+		if rank == 0 {
+			log.Panic("zero rank system")
+			return mat.NewDense(3, 3, nil), nil
+		}
+
+		// Find a least-squares solution using the determined parts of the system.
+		var x mat.Dense
+		svd.SolveTo(&x, b, rank)
+		// homography is a 3x3 matrix, with last element =1
+		s := make([]float64, len(x.RawMatrix().Data)+1)
+		for i, v := range x.RawMatrix().Data {
+			s[i] = v
+		}
+		s[len(x.RawMatrix().Data)] = 1.
+		outMat := mat.NewDense(3, 3, s)
+
+		if normalize {
+			// de-normalize data
+			invNorm1 := mat.NewDense(3, 3, nil)
+			err := invNorm1.Inverse(norm1)
+			if err != nil {
+				panic(err)
+			}
+			invNorm2 := mat.NewDense(3, 3, nil)
+			err = invNorm2.Inverse(norm2)
+			if err != nil {
+				panic(err)
+			}
+			var m1, m2, m3 mat.Dense
+			m1.Mul(norm1, outMat)
+			m2.Mul(&m1, invNorm2)
+			m3.Scale(1./m2.At(2, 2), &m2)
+			return &m3, nil
+		}
+
+		return outMat, nil
+	}
+	// Otherwise, matrix cannot be inverted; return nothing
+	return mat.NewDense(3, 3, nil), nil
+}
+
+// MeanVarianceCol is a helper function to compute the normalization matrix for least squares homography estimation
+// It estimates the mean and variance of a column in a *mat.Dense.
+func MeanVarianceCol(m mat.Matrix, j int) (float64, float64) {
+	r, _ := m.Dims()
+	col := make([]float64, r)
+
+	mat.Col(col, j, m)
+	mean := stat.Mean(col, nil)
+	std := stat.StdDev(col, nil)
+
+	return mean, std
+}
+
+// getNormalizationMatrix gets the matrix to center the points on (0,0).
+func getNormalizationMatrix(pts *mat.Dense) *mat.Dense {
+	avgX, stdX := MeanVarianceCol(pts, 0)
+	avgY, stdY := MeanVarianceCol(pts, 1)
+
+	sX := math.Sqrt(2. / stdX)
+	sY := math.Sqrt(2. / stdY)
+	data := []float64{sX, 0., -sX * avgX, 0., sY, -sY * avgY, 0, 0, 1}
+	outMat := mat.NewDense(3, 3, data)
+	//	[
+	//	[s_x, 0, -s_x * avg_x],
+	//	[0, s_y, -s_y * avg_y],
+	//	[0, 0, 1]
+	//]
+	return outMat
+}
+
+// EstimateLeastSquaresHomography estimates an homography from 2 sets of corresponding points.
+func EstimateLeastSquaresHomography(pts1, pts2 *mat.Dense) (*mat.Dense, error) {
+	normalizationMat1 := getNormalizationMatrix(pts1)
+	normalizationMat2 := getNormalizationMatrix(pts2)
+	M := make([]float64, 0)
+	nRows, nCols := pts1.Dims()
+	for i := 0; i < nRows; i++ {
+		p1 := mat.NewDense(nCols+1, 1, []float64{pts1.At(i, 0), pts1.At(i, 1), 1.})
+		p2 := mat.NewDense(nCols+1, 1, []float64{pts2.At(i, 0), pts2.At(i, 1), 1.})
+		p1.Mul(normalizationMat1, p1)
+		p2.Mul(normalizationMat2, p2)
+		currentSlice1 := []float64{
+			p1.At(0, 0), p1.At(1, 0), 1,
+			0., 0., 0.,
+			-p1.At(0, 0) * p2.At(0, 0), -p1.At(1, 0) * p2.At(0, 0), -p2.At(0, 0),
+		}
+		M = append(M, currentSlice1...)
+		currentSlice2 := []float64{
+			0., 0., 0.,
+			p1.At(0, 0), p1.At(1, 0), 1,
+
+			-p1.At(0, 0) * p2.At(1, 0), -p1.At(1, 0) * p2.At(1, 0), -p2.At(1, 0),
+		}
+		M = append(M, currentSlice2...)
+	}
+	m := mat.NewDense(2*nRows, 9, M)
+	var svd mat.SVD
+	ok := svd.Factorize(m, mat.SVDFull)
+	if !ok {
+		log.Fatal("failed to factorize A")
+	}
+	// Determine the rank of the A matrix with a near zero condition threshold.
+	const rcond = 1e-15
+	if svd.Rank(rcond) == 0 {
+		log.Fatal("zero rank system")
+	}
+	var V, m1, m2, m3 mat.Dense
+	svd.VTo(&V)
+	L := V.ColView(8)
+	var l mat.VecDense
+	l.CloneFromVec(L)
+	H := mat.NewDense(3, 3, l.RawVector().Data)
+	invNorm1 := mat.NewDense(3, 3, nil)
+	err := invNorm1.Inverse(normalizationMat1)
+	if err != nil {
+		panic(err)
+	}
+	invNorm2 := mat.NewDense(3, 3, nil)
+	err = invNorm2.Inverse(normalizationMat2)
+	if err != nil {
+		panic(err)
+	}
+	m1.Mul(normalizationMat1, H)
+	m2.Mul(&m1, invNorm2)
+	m3.Scale(1./m2.At(2, 2), &m2)
+
+	return &m3, nil
+}
+
+// SelectFourPointPairs randomly selects 4 pairs of points in two point slices of the same length.
+func SelectFourPointPairs(p1, p2 []r2.Point) ([]r2.Point, []r2.Point, error) {
+	if len(p1) != len(p2) {
+		err := errors.New("p1 and p2 should have the same length")
+		return nil, nil, err
+	}
+	indices, err := utils.SelectNIndicesWithoutReplacement(4, len(p1))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// create 2 4-point-slices of selected indices
+	s1 := make([]r2.Point, 0, 4)
+	s2 := make([]r2.Point, 0, 4)
+	for _, idx := range indices {
+		s1 = append(s1, p1[idx])
+		s2 = append(s2, p2[idx])
+	}
+	return s1, s2, nil
+}
+
+// geometricDistance computes the distance of point1 transformed by the homography h 1->2 to point 2.
+func geometricDistance(p1, p2 r2.Point, h mat.Matrix) float64 {
+	pt1 := mat.NewDense(3, 1, []float64{p1.X, p1.Y, 1.0})
+	pt2 := mat.NewDense(3, 1, []float64{p2.X, p2.Y, 1.0})
+	pt2Tilde := mat.NewDense(3, 1, nil)
+	pt2Tilde.Mul(h, pt1)
+	pt2Tilde.Scale(1./pt2Tilde.At(2, 0), pt2Tilde)
+
+	p := r3.Vector{X: pt2.At(0, 0), Y: pt2.At(1, 0), Z: pt2.At(2, 0)}
+	q := r3.Vector{X: pt2Tilde.At(0, 0), Y: pt2Tilde.At(1, 0), Z: pt2Tilde.At(2, 0)}
+	errVec := p.Sub(q)
+
+	return errVec.Norm()
+}
+
+// Are4PointsNonCollinear returns true if 4 points are not collinear 3 by 3.
+func Are4PointsNonCollinear(p1, p2, p3, p4 r2.Point) bool {
+	return !rimage.AreCollinear(p1, p2, p3, 0.01) && !rimage.AreCollinear(p2, p3, p4, 0.01) &&
+		!rimage.AreCollinear(p1, p3, p4, 0.01) && !rimage.AreCollinear(p1, p2, p4, 0.01)
+}
+
+// EstimateHomographyRANSAC estimates a homography from matches of 2 sets of
+// points with the RANdom SAmple Consensus method
+// from Multiple View Geometry. Richard Hartley and Andrew Zisserman. Alg 4.4 p118.
+func EstimateHomographyRANSAC(pts1, pts2 []r2.Point, thresh float64, nMaxIteration int) (*mat.Dense, []int, error) {
+	// test len(pts1)==len(pts2)
+	// test len(pts1) > 4
+	maxInliers := make([]int, 0, len(pts1))
+	finalH := mat.NewDense(3, 3, nil)
+	// RANSAC iterations
+	for i := 0; i < nMaxIteration; i++ {
+		// select 4 random matches
+		s1, s2, err := SelectFourPointPairs(pts1, pts2)
+		if err != nil {
+			return nil, nil, err
+		}
+		for !Are4PointsNonCollinear(s1[0], s1[1], s1[2], s1[3]) {
+			s1, s2, err = SelectFourPointPairs(pts1, pts2)
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+
+		// estimate exact homography from these 4 matches
+		h, err := EstimateExactHomographyFrom8Points(s1, s2, false)
+		if err != nil {
+			return nil, nil, err
+		}
+		if h != nil {
+			// compute inliers
+			currentInliers := make([]int, 0, len(pts1))
+			for k := 0; k < 4; k++ {
+				d := geometricDistance(s1[k], s2[k], h)
+				if d < 5. {
+					currentInliers = append(currentInliers, k)
+				}
+			}
+			// keep current set of inliers and homography if number of inliers is bigger than before
+			if len(currentInliers) > len(maxInliers) {
+				maxInliers = currentInliers
+				finalH = h
+			}
+			// if the current homography has a number of inliers that exceeds a certain ratio of points in matches,
+			// iterations can be stopped - the homography estimation is accurate enough
+			nReasonable := int(float64(len(pts1)) * thresh)
+			if len(currentInliers) > nReasonable {
+				break
+			}
+		} else {
+			continue
+		}
+	}
+	return finalH, maxInliers, nil
+}
+
+// ApplyHomography applies a homography on a slice of r2.Vec.
+func ApplyHomography(h rimage.Matrix, pts []r2.Point) []r2.Point {
+	outPoints := make([]r2.Point, len(pts))
+	for i, pt := range pts {
+		x := h.At(0, 0)*pt.X + h.At(0, 1)*pt.Y + h.At(0, 2)
+		y := h.At(1, 0)*pt.X + h.At(1, 1)*pt.Y + h.At(1, 2)
+		z := h.At(2, 0)*pt.X + h.At(2, 1)*pt.Y + h.At(2, 2)
+		outPoints[i] = r2.Point{X: x / z, Y: y / z}
+	}
+	return outPoints
+}
+
+// ApplyNormalizationMat applies a normalization matrix to a slice of r2.Point.
+func ApplyNormalizationMat(h rimage.Matrix, pts []r2.Point) []r2.Point {
+	outPoints := make([]r2.Point, len(pts))
+	for i, pt := range pts {
+		x := h.At(0, 0)*pt.X + h.At(0, 1)*pt.Y + h.At(0, 2)
+		y := h.At(1, 0)*pt.X + h.At(1, 1)*pt.Y + h.At(1, 2)
+		// z := h.At(2, 0)*pt.X + h.At(2, 1)*pt.Y + h.At(2, 2)
+		outPoints[i] = r2.Point{X: x, Y: y}
+	}
+	return outPoints
+}
---
+++ rimage/transform/homographies_test.go
@@ -0,0 +1,224 @@
+package transform
+
+import (
+	"math"
+	"testing"
+
+	"github.com/golang/geo/r2"
+	"go.viam.com/test"
+	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/mat"
+
+	"go.viam.com/rdk/utils"
+)
+
+func Single(dim int, x []float64) [][]float64 {
+	dims := make([]int, dim)
+	for i := range dims {
+		dims[i] = len(x)
+	}
+	sz := size(dims)
+	pts := make([][]float64, sz)
+	sub := make([]int, dim)
+	for i := 0; i < sz; i++ {
+		SubFor(sub, i, dims)
+		pt := make([]float64, dim)
+		for j := range pt {
+			pt[j] = x[sub[j]]
+		}
+		pts[i] = pt
+	}
+	return pts
+}
+
+func size(dims []int) int {
+	n := 1
+	for _, v := range dims {
+		n *= v
+	}
+	return n
+}
+
+// SubFor constructs the multi-dimensional subscript for the input linear index.
+// Dims specifies the maximum size in each dimension. SubFor is the converse of
+// IdxFor.
+//
+// If sub is non-nil the result is stored in-place into sub. If it is nil a new
+// slice of the appropriate length is allocated.
+func SubFor(sub []int, idx int, dims []int) []int {
+	for _, v := range dims {
+		if v <= 0 {
+			panic("bad dims")
+		}
+	}
+	if sub == nil {
+		sub = make([]int, len(dims))
+	}
+	if len(sub) != len(dims) {
+		panic("size mismatch")
+	}
+	if idx < 0 {
+		panic("bad index")
+	}
+	stride := 1
+	for i := len(dims) - 1; i >= 1; i-- {
+		stride *= dims[i]
+	}
+	for i := 0; i < len(dims)-1; i++ {
+		v := idx / stride
+		if v >= dims[i] {
+			panic("bad index")
+		}
+		sub[i] = v
+		idx -= v * stride
+		stride /= dims[i+1]
+	}
+	if idx > dims[len(sub)-1] {
+		panic("bad dims")
+	}
+	sub[len(sub)-1] = idx
+	return sub
+}
+
+// CreateRotationMatrix creates a 2x2 rotation matrix with given angle in radians.
+func CreateRotationMatrix(angle float64) *mat.Dense {
+	r := mat.NewDense(2, 2, nil)
+	r.Set(0, 0, math.Cos(angle))
+	r.Set(0, 1, -math.Sin(angle))
+	r.Set(1, 0, math.Sin(angle))
+	r.Set(1, 1, math.Cos(angle))
+
+	return r
+}
+
+func SlicesXsYsToPoints(points [][]float64) []r2.Point {
+	pts := make([]r2.Point, len(points))
+	for i, pt := range points {
+		x := pt[0]
+		y := pt[1]
+		pts[i] = r2.Point{x, y}
+	}
+	return pts
+}
+
+func TestEstimateHomographyFrom8Points(t *testing.T) {
+	h := []float64{
+		7.82502613e-01, -9.71005496e-02, 9.73247024e+00,
+		9.71005496e-02, 7.82502613e-01, 7.26666735e+00,
+		8.96533720e-04, -9.39239890e-04, 1.00000000e+00,
+	}
+	homography := mat.NewDense(3, 3, h)
+	pts1 := []r2.Point{{0., 0.}, {25., 0.}, {0., 25.}, {25., 25.}}
+	pts2 := []r2.Point{{9.7324705, 7.2666674}, {28.65283, 9.481666}, {7.4806085, 27.474358}, {26.896238, 29.288015}}
+	H, _ := EstimateExactHomographyFrom8Points(pts1, pts2, false)
+	test.That(t, mat.EqualApprox(H, homography, 0.00001), test.ShouldBeTrue)
+}
+
+func repeatedSlice(value float64, n int) []float64 {
+	arr := make([]float64, n)
+	for i := 0; i < n; i++ {
+		arr[i] = value
+	}
+	return arr
+}
+
+func TestEstimateLeastSquaresHomography(t *testing.T) {
+	// create a rotation as a simple homography
+	h := CreateRotationMatrix(math.Pi / 8.)
+	// create grid of points
+	x := make([]float64, 9)
+	floats.Span(x, 0, 200)
+	pts1Slice := utils.Single(2, x)
+	pts1 := mat.NewDense(len(pts1Slice), len(pts1Slice[0]), nil)
+	for i, pt := range pts1Slice {
+		pts1.Set(i, 0, pt[0])
+		pts1.Set(i, 1, pt[1])
+	}
+	// rotate point with H
+	r, c := pts1.Dims()
+	pts2 := mat.NewDense(c, r, nil)
+	pts2.Mul(h, pts1.T())
+	b := pts2.T()
+	pts3 := mat.DenseCopyOf(b)
+	// estimate homography with least squares method
+	estH, _ := EstimateLeastSquaresHomography(pts1, pts3)
+	// check that 2x2 block are close to each other
+	test.That(t, estH.At(0, 0), test.ShouldAlmostEqual, h.At(0, 0), 0.001)
+	test.That(t, estH.At(0, 1), test.ShouldAlmostEqual, h.At(0, 1), 0.001)
+	test.That(t, estH.At(1, 0), test.ShouldAlmostEqual, h.At(1, 0), 0.001)
+	test.That(t, estH.At(1, 1), test.ShouldAlmostEqual, h.At(1, 1), 0.001)
+
+	// test translation (2,2)
+	pts1Copy := mat.DenseCopyOf(pts1)
+
+	pts4Data := pts1Copy.RawMatrix().Data
+	vals := repeatedSlice(2., r*c)
+	floats.Add(pts4Data, vals)
+	pts4 := mat.NewDense(r, c, pts4Data)
+	estH2, _ := EstimateLeastSquaresHomography(pts1, pts4)
+
+	// check that 2x2 block are close to identity
+	test.That(t, estH2.At(0, 0), test.ShouldAlmostEqual, 1.0, 0.001)
+	test.That(t, estH2.At(0, 1), test.ShouldBeLessThanOrEqualTo, 0.001)
+	test.That(t, estH2.At(1, 0), test.ShouldBeLessThanOrEqualTo, 0.001)
+	test.That(t, estH2.At(1, 1), test.ShouldAlmostEqual, 1.0, 0.001)
+	// check that translation terms are close to sqrt(2) / 4. (from homography decomposition formula)
+	test.That(t, estH2.At(0, 2), test.ShouldAlmostEqual, 0.3535533905932738, 0.01)
+	test.That(t, estH2.At(1, 2), test.ShouldAlmostEqual, 0.3535533905932738, 0.01)
+	// test that translation terms are equal tx = ty
+	test.That(t, estH2.At(1, 2), test.ShouldAlmostEqual, estH2.At(0, 2), 0.01)
+}
+
+func TestGeometricDistance(t *testing.T) {
+	pt1 := r2.Point{0, 0}
+	pt2 := r2.Point{1, 0}
+	// h = Id, distance should be 1
+	h1 := mat.NewDense(3, 3, nil)
+	h1.Set(0, 0, 1)
+	h1.Set(1, 1, 1)
+	h1.Set(2, 2, 1)
+	d1 := geometricDistance(pt1, pt2, h1)
+	test.That(t, d1, test.ShouldEqual, 1.0)
+	// rotation -pi/2
+	h2 := mat.NewDense(3, 3, nil)
+	h2.Set(0, 1, 1)
+	h2.Set(1, 0, -1)
+	h2.Set(2, 2, 1)
+	d2 := geometricDistance(pt1, pt2, h2)
+	test.That(t, d2, test.ShouldEqual, 1.0)
+	// rotation -pi/2
+	h3 := mat.NewDense(3, 3, nil)
+	h3.Set(0, 1, 1)
+	h3.Set(1, 0, -1)
+	h3.Set(2, 2, 1)
+	pt3 := r2.Point{1, 0}
+	d3 := geometricDistance(pt3, pt2, h3)
+	test.That(t, d3, test.ShouldEqual, 1.4142135623730951)
+}
+
+func TestEstimateHomographyRANSAC(t *testing.T) {
+	dim := 2
+	grid := make([]float64, 8)
+	floats.Span(grid, 0, 7)
+	pts := Single(dim, grid)
+	pts1 := SlicesXsYsToPoints(pts)
+
+	pts2 := make([]r2.Point, len(pts1))
+	for i := 0; i < len(pts1); i++ {
+		pts2[i] = r2.Point{pts1[i].X + 2, pts1[i].Y + 3}
+	}
+	h, _, _ := EstimateHomographyRANSAC(pts1, pts2, 0.5, 2000)
+	// homography should be close to
+	// [[1 0 2],
+	//  [0 1 3],
+	//  [0 0 1]]
+	test.That(t, h.At(0, 0), test.ShouldAlmostEqual, 1, 0.001)
+	test.That(t, h.At(1, 1), test.ShouldAlmostEqual, 1, 0.001)
+	test.That(t, h.At(0, 1), test.ShouldAlmostEqual, 0, 0.001)
+	test.That(t, h.At(1, 0), test.ShouldAlmostEqual, 0, 0.001)
+	test.That(t, h.At(0, 2), test.ShouldAlmostEqual, 2, 0.001)
+	test.That(t, h.At(1, 2), test.ShouldAlmostEqual, 3, 0.001)
+	test.That(t, h.At(2, 0), test.ShouldAlmostEqual, 0, 0.001)
+	test.That(t, h.At(2, 1), test.ShouldAlmostEqual, 0, 0.001)
+	test.That(t, h.At(2, 2), test.ShouldAlmostEqual, 1, 0.001)
+}
