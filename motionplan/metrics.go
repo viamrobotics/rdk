2a78d0c8f90d9a0039e5d8f07792233f4a9aed13
Peter LoVerso <biotinker@gmail.com>
2022-01-18 16:58:35 -0800

    initial default constraint implementation

--- motionplan/metrics.go
+++ motionplan/metrics.go
@@ -1,6 +1,8 @@
 package motionplan
 
 import (
+	"math"
+
 	spatial "go.viam.com/rdk/spatialmath"
 )
 
@@ -16,4 +18,52 @@ func weightedSqNormDist(from, to spatial.Pose) float64 {
 	delta := spatial.PoseDelta(from, to)
 	// Increase weight for orientation since it's a small number
 	return delta.Point().Norm2() + spatial.QuatToR3AA(delta.Orientation().Quaternion()).Mul(10.).Norm2()
+}
+
+type combinableMetric struct {
+	metrics []Metric
+}
+
+func (m *combinableMetric) combinedDist(p1, p2 spatial.Pose) float64 {
+	dist := 0.
+	for _, metric := range m.metrics {
+		dist = +metric(p1, p2)
+	}
+	return dist
+}
+
+// CombineMetrics will take a variable number of Metrics and return a new Metric which will combine all given metrics into one, summing
+// their distances.
+func CombineMetrics(metrics ...Metric) Metric {
+	cm := &combinableMetric{metrics: metrics}
+	return cm.combinedDist
+}
+
+func newDefaultMetric(start, end spatial.Pose) Metric {
+	delta := spatial.PoseDelta(start, end)
+	tDist := delta.Point().Norm2() * deviationFactor
+	oDist := spatial.QuatToR3AA(delta.Orientation().Quaternion()).Norm2() * deviationFactor
+	endpoints := []spatial.Pose{start, end}
+	return func(from, to spatial.Pose) float64 {
+		minDist := math.Inf(1)
+		for _, endpoint := range endpoints {
+			dist := 0.
+			delta := spatial.PoseDelta(from, endpoint)
+			transDist := delta.Point().Norm2()
+			orientDist := spatial.QuatToR3AA(delta.Orientation().Quaternion()).Norm2()
+			if transDist > tDist {
+				dist += transDist - tDist
+			}
+			if orientDist > oDist {
+				dist = +orientDist - oDist
+			}
+			if dist == 0. {
+				return dist
+			}
+			if dist < minDist {
+				minDist = dist
+			}
+		}
+		return minDist
+	}
 }
