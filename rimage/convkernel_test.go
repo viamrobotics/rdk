561eef946ce339dc5bf91aaabfcbc1000d698829
louisenaud <louise.naud@gmail.com>
2022-01-19 11:30:42 -0500

    Added convolution operator

---
+++ rimage/convkernel_test.go
@@ -0,0 +1,28 @@
+package rimage
+
+import (
+	"testing"
+
+	"go.viam.com/test"
+)
+
+func TestNewKernel(t *testing.T) {
+	k, err := NewKernel(5, 5)
+	// test creation
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, k.Width, test.ShouldEqual, 5)
+	test.That(t, k.Height, test.ShouldEqual, 5)
+	// test content
+	test.That(t, k.Content[0], test.ShouldResemble, []float64{0, 0, 0, 0, 0})
+	test.That(t, k.At(4, 4), test.ShouldEqual, 0)
+	test.That(t, k.At(3, 2), test.ShouldEqual, 0)
+	// test set
+	k.Set(3, 2, 1)
+	test.That(t, k.At(3, 2), test.ShouldEqual, 1)
+	// test AbsSum
+	k.Set(1, 2, -1)
+	test.That(t, k.AbSum(), test.ShouldEqual, 2)
+	// test Normalize
+	kNormalized := k.Normalize()
+	test.That(t, kNormalized.At(3, 2), test.ShouldEqual, 0.5)
+}
