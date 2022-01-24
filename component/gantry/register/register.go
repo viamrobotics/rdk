5e7d830e8ef32d6dd6e551e63642c4aa3aafff35
randhid <rand@viam.com>
2022-01-24 15:26:07 -0500

    moved multiAxis tests to oneAxis tests

--- component/gantry/register/register.go
+++ component/gantry/register/register.go
@@ -5,5 +5,13 @@ import (
 
 	// for gantries.
 	_ "go.viam.com/rdk/component/gantry/fake"
+<<<<<<< HEAD
 	_ "go.viam.com/rdk/component/gantry/simple"
+=======
+	_ "go.viam.com/rdk/component/gantry/multiAxis"
+	_ "go.viam.com/rdk/component/gantry/oneAxis"
+	componentpb "go.viam.com/rdk/proto/api/component/v1"
+	"go.viam.com/rdk/registry"
+	"go.viam.com/rdk/subtype"
+>>>>>>> de97de24 (moved multiAxis tests to oneAxis tests)
 )
