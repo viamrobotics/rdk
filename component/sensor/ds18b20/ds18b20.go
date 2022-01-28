d195db636ce47e8ce61296f620929fab0618040d
Matt Vella <mcvella@gmail.com>
2022-01-28 11:39:15 -0500

    Output sensor readings in remote control, and some linter cleanup

--- component/sensor/ds18b20/ds18b20.go
+++ component/sensor/ds18b20/ds18b20.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"math"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -22,7 +23,7 @@ const (
 
 // AttrConfig is used for converting config attributes.
 type AttrConfig struct {
-	UniqueId string `json:"unique_id"`
+	UniqueID string `json:"unique_id"`
 }
 
 func init() {
@@ -35,7 +36,7 @@ func init() {
 			config config.Component,
 			logger golog.Logger,
 		) (interface{}, error) {
-			return newSensor(config.Name, config.ConvertedAttributes.(*AttrConfig).UniqueId)
+			return newSensor(config.Name, config.ConvertedAttributes.(*AttrConfig).UniqueID), nil
 		}})
 
 	config.RegisterComponentAttributeMapConverter(config.ComponentTypeSensor, modelname,
@@ -45,26 +46,27 @@ func init() {
 		}, &AttrConfig{})
 }
 
-func newSensor(name string, id string) (sensor.Sensor, error) {
+func newSensor(name string, id string) sensor.Sensor {
 	// temp sensors are in family 28
-	return &Sensor{Name: name, OneWireId: id, OneWireFamily: "28"}, nil
+	return &Sensor{Name: name, OneWireID: id, OneWireFamily: "28"}
 }
 
 // Sensor is a fake Sensor device that always returns the set location.
 type Sensor struct {
 	Name          string
-	OneWireId     string
+	OneWireID     string
 	OneWireFamily string
 }
 
+// ReadTemperatureCelsius returns temperature in celsius.
 func (s *Sensor) ReadTemperatureCelsius(ctx context.Context) (float64, error) {
 	/*
 	* logic here is specific to 1-wire protocol, could be abstracted next time we
 	* want to build support for a different 1-wire device,
 	* or look at support via periph (or other library)
 	 */
-	devPath := "/sys/bus/w1/devices/" + s.OneWireFamily + "-" + s.OneWireId + "/w1_slave"
-	dat, err := os.ReadFile(devPath)
+	devPath := "/sys/bus/w1/devices/" + s.OneWireFamily + "-" + s.OneWireID + "/w1_slave"
+	dat, err := os.ReadFile(filepath.Clean(devPath))
 	if err != nil {
 		return math.NaN(), err
 	}
@@ -78,6 +80,7 @@ func (s *Sensor) ReadTemperatureCelsius(ctx context.Context) (float64, error) {
 	return tempMili / 1000, nil
 }
 
+// Readings returns a list of all readings.
 func (s *Sensor) Readings(ctx context.Context) ([]interface{}, error) {
 	temp, err := s.ReadTemperatureCelsius(ctx)
 	if err != nil {
