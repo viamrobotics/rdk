c51a5de30d280292c128fa3b07303092dd4fd555
Bijan Haney <bijanhaney@gmail.com>
2022-02-02 11:51:43 -0500

    Add Object Detection Module (#469)

--- rimage/image_with_depth_test.go
+++ rimage/image_with_depth_test.go
@@ -2,10 +2,14 @@ package rimage
 
 import (
 	"bytes"
+	"image"
 	"testing"
 
+	"github.com/golang/geo/r3"
 	"go.viam.com/test"
 	"go.viam.com/utils/artifact"
+
+	"go.viam.com/rdk/pointcloud"
 )
 
 func TestPCRoundTrip(t *testing.T) {
@@ -55,6 +59,39 @@ func TestDefaultToPointCloud(t *testing.T) {
 	pc2, err := iwd2.ToPointCloud()
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, pc2, test.ShouldBeNil)
+}
+
+type dummyProjector struct{}
+
+func (d *dummyProjector) ImageWithDepthToPointCloud(*ImageWithDepth) (pointcloud.PointCloud, error) {
+	return nil, nil
+}
+
+func (d *dummyProjector) PointCloudToImageWithDepth(pointcloud.PointCloud) (*ImageWithDepth, error) {
+	return &ImageWithDepth{}, nil
+}
+
+func (d *dummyProjector) ImagePointTo3DPoint(image.Point, Depth) (r3.Vector, error) {
+	return r3.Vector{}, nil
+}
+
+func TestCloneImageWithDepth(t *testing.T) {
+	iwd, err := NewImageWithDepth(artifact.MustPath("rimage/board1.png"), artifact.MustPath("rimage/board1.dat.gz"), true)
+	test.That(t, err, test.ShouldBeNil)
+
+	ii := CloneToImageWithDepth(iwd)
+	for y := 0; y < ii.Height(); y++ {
+		for x := 0; x < ii.Width(); x++ {
+			test.That(t, ii.Depth.GetDepth(x, y), test.ShouldResemble, iwd.Depth.GetDepth(x, y))
+			test.That(t, ii.Color.GetXY(x, y), test.ShouldResemble, iwd.Color.GetXY(x, y))
+		}
+	}
+	test.That(t, ii.IsAligned(), test.ShouldEqual, iwd.IsAligned())
+	test.That(t, ii.Projector(), test.ShouldResemble, iwd.Projector())
+
+	dummy := &dummyProjector{}
+	ii.SetProjector(dummy)
+	test.That(t, ii.Projector(), test.ShouldNotResemble, iwd.Projector())
 }
 
 func TestImageWithDepthFromImages(t *testing.T) {
---
+++ vision/objectdetection/detection_utils.go
@@ -0,0 +1,73 @@
+package objectdetection
+
+import (
+	"image"
+	"image/color"
+
+	"github.com/fogleman/gg"
+	"github.com/pkg/errors"
+
+	"go.viam.com/rdk/rimage"
+)
+
+// Overlay returns a color image with the bounding boxes overlaid on the original image.
+func Overlay(img image.Image, dets []Detection) (image.Image, error) {
+	rimg := rimage.ConvertImage(img)
+	for _, det := range dets {
+		if !det.BoundingBox().In(rimg.Bounds()) {
+			return nil, errors.Errorf("bounding box (%v) does not fit in image (%v)", det.BoundingBox(), rimg.Bounds())
+		}
+		drawBox(rimg, det.BoundingBox())
+	}
+	return rimg, nil
+}
+
+// OverlayText writes a string in the top of the image.
+func OverlayText(img image.Image, text string) image.Image {
+	gimg := gg.NewContextForImage(img)
+	rimage.DrawString(gimg, text, image.Point{30, 30}, color.NRGBA{255, 0, 0, 255}, 30)
+	return gimg.Image()
+}
+
+// drawBox draws a red box over the image, each side is 3 pixels wide.
+func drawBox(img *rimage.Image, rec *image.Rectangle) {
+	x0, y0, x1, y1 := rec.Min.X, rec.Min.Y, rec.Max.X, rec.Max.Y
+	horizontal(x0, y0, x1, img, rimage.Red)
+	horizontal(x0, y1, x1, img, rimage.Red)
+	vertical(x0, y0, y1, img, rimage.Red)
+	vertical(x1, y0, y1, img, rimage.Red)
+}
+
+// horizontal draws a horizontal line 3 pixels wide.
+func horizontal(x0, y, x1 int, img *rimage.Image, col rimage.Color) {
+	for x := x0; x <= x1; x++ {
+		img.SetXY(x, y, col)
+	}
+	if y-1 >= 0 {
+		for x := x0; x <= x1; x++ {
+			img.SetXY(x, y-1, col)
+		}
+	}
+	if y+1 < img.Height() {
+		for x := x0; x <= x1; x++ {
+			img.SetXY(x, y+1, col)
+		}
+	}
+}
+
+// vertical draws a veritcal line 3 pixels wide.
+func vertical(x, y0, y1 int, img *rimage.Image, col rimage.Color) {
+	for y := y0; y <= y1; y++ {
+		img.SetXY(x, y, col)
+	}
+	if x-1 >= 0 {
+		for y := y0; y <= y1; y++ {
+			img.SetXY(x-1, y, col)
+		}
+	}
+	if x+1 < img.Width() {
+		for y := y0; y <= y1; y++ {
+			img.SetXY(x+1, y, col)
+		}
+	}
+}
