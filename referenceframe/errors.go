9989d0a7260a01fc9d2caebfac8eb973aea71a71
Gautham Varadarajan <gautham@viam.com>
2022-02-04 12:01:09 -0500

    lint

--- referenceframe/errors.go
+++ referenceframe/errors.go
@@ -2,7 +2,7 @@ package referenceframe
 
 import "errors"
 
-// returns an error indicating that a frame is missing a parent
+// NewParentFrameMissingError returns an error indicating that a frame is missing a parent
 func NewParentFrameMissingError() error {
 	return errors.New("parent frame is nil")
 }
