4790cff27046ffbdf76b7089f05335d85229f362
Gautham Varadarajan <gautham@viam.com>
2022-02-08 10:44:20 -0500

    fix bug introduced in stepper motor tests

--- component/motor/tmcstepper/stepper_motor_tmc_test.go
+++ component/motor/tmcstepper/stepper_motor_tmc_test.go
@@ -108,14 +108,14 @@ func TestTMCStepperMotor(t *testing.T) {
 			{160, 0, 0, 0, 1},
 			{167, 0, 4, 35, 42},
 		})
-		test.That(t, _motor.SetPower(ctx, 0.5*maxRpm), test.ShouldBeNil)
+		test.That(t, _motor.SetPower(ctx, 0.5), test.ShouldBeNil)
 
 		// Test Go backward at quarter speed
 		go checkTx(t, c, [][]byte{
 			{160, 0, 0, 0, 2},
 			{167, 0, 2, 17, 149},
 		})
-		test.That(t, _motor.SetPower(ctx, -0.25*maxRpm), test.ShouldBeNil)
+		test.That(t, _motor.SetPower(ctx, -0.25), test.ShouldBeNil)
 	})
 
 	t.Run("motor Off testing", func(t *testing.T) {
