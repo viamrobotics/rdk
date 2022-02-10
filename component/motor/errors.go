7201fac4818eb6ad07bf58c6b5058b18bda31bf9
Gautham Varadarajan <gautham@viam.com>
2022-02-10 14:48:48 -0500

    error msg

--- component/motor/errors.go
+++ component/motor/errors.go
@@ -5,5 +5,5 @@ import "github.com/pkg/errors"
 // NewGoTillStopUnsupportedError returns a standard error for when a motor
 // is required to support the GoTillStop feature.
 func NewGoTillStopUnsupportedError(motorName string) error {
-	return errors.Errorf("motor with name %s must support GoTillStop", motorName)
+	return errors.Errorf("motor with name %s does not support GoTillStop", motorName)
 }
