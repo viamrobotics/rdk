1510974a14d602e8872fda2a67f6fd17a8e95ac9
Eliot Horowitz <eliothorowitz@gmail.com>
2022-02-17 08:30:01 -0500

    move servo config into common so it always gets loaded

--- component/board/pi/common/common.go
+++ component/board/pi/common/common.go
@@ -1,11 +1,30 @@
 // Package picommon contains shared information for supported and non-supported pi boards.
 package picommon
 
-import "go.viam.com/rdk/component/board"
+import (
+	"go.viam.com/rdk/component/board"
+	"go.viam.com/rdk/config"
+)
 
 // ModelName is the name used refer to any implementation of a pi based component.
 const ModelName = "pi"
 
+// ServoConfig is the config for a pi servo.
+type ServoConfig struct {
+	Pin string `json:"pin"`
+	Min int    `json:"min,omitempty"`
+	Max int    `json:"max,omitempty"`
+}
+
 func init() {
 	board.RegisterConfigAttributeConverter(ModelName)
+
+	config.RegisterComponentAttributeMapConverter(
+		config.ComponentTypeServo,
+		ModelName,
+		func(attributes config.AttributeMap) (interface{}, error) {
+			var conf ServoConfig
+			return config.TransformAttributeMapToStruct(&conf, attributes)
+		},
+		&ServoConfig{})
 }
