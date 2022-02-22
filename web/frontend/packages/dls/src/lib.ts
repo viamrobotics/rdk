60bfcda7e87593264bc4d10b8a952b6a91a704f0
Aleksei Tryputsen <atryoutsen@theatlantic.com>
2022-02-22 11:53:41 -0600

    Added new camera component into the application

--- web/frontend/packages/dls/src/lib.ts
+++ web/frontend/packages/dls/src/lib.ts
@@ -5,12 +5,18 @@ import MotorDetail from "./components/MotorDetail.vue";
 import InputController from "./components/InputController.vue";
 import WebGamepad from "./components/WebGamepad.vue";
 import Camera from "./components/Camera.vue";
+import Accordion from "./components/Accordion.vue";
+import ToggleButton from "./components/ToggleButton.vue";
+import VueAccordion from "./components/VueAccordion.vue";
 
 const Components: { [key: string]: VueConstructor<Vue> } = {
     MotorDetail,
     InputController,
     WebGamepad,
-    Camera
+    Camera,
+    Accordion,
+    ToggleButton,
+    VueAccordion
   };
   
   Object.keys(Components).forEach((name) => {
