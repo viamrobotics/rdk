9d63bdad00edf0e44d39cf49833c157ddee65067
Bijan Haney <bijan@viam.com>
2022-02-25 17:11:06 -0500

    testing

--- vision/objectdetection/detection_test.go
+++ vision/objectdetection/detection_test.go
@@ -4,15 +4,31 @@ import (
 	"image"
 	"testing"
 
+	"go.viam.com/rdk/rimage"
 	"go.viam.com/test"
 )
 
 func TestBuildFunc(t *testing.T) {
+	img := rimage.NewImage(400, 400)
 	_, err := Build(nil, nil, nil)
 	test.That(t, err.Error(), test.ShouldContainSubstring, "must have a Detector")
+	// make simple detector
 	det := func(image.Image) ([]Detection, error) {
 		return []Detection{&detection2D{}}, nil
 	}
-	res, err := Build(nil, det, nil)
+	pipeline, err := Build(nil, det, nil)
+	test.That(t, err, test.ShouldBeNil)
+	res, err := pipeline(img)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldHaveLength, 1)
+	// make simple filter
+	filt := func(d []Detection) []Detection {
+		return []Detection{}
+	}
+	pipeline, err = Build(nil, det, filt)
+	test.That(t, err, test.ShouldBeNil)
+	res, err = pipeline(img)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res, test.ShouldHaveLength, 0)
 
 }
