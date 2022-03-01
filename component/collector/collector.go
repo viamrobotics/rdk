4c79aff990bee854d6dc167486ce425ba0c6cda8
aaroncasas <aaron@viam.com>
2022-03-01 11:04:24 -0500

    Add validation stub.

--- component/collector/collector.go
+++ component/collector/collector.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Metadata struct {
-	component string
-	method    string
+	Component string
+	Method    string
 	// TODO: should this be an absolute path instead of a File?
-	destination *os.File
-	params      map[string]string
+	Destination *os.File
+	Params      []string
 }
 
 type Collector struct {
@@ -22,10 +22,31 @@ type Collector struct {
 	queue    chan []byte // TODO: Will actually be chan of SensorData; just putting any.Any because I want this to compile.
 }
 
+func NewCollector(md Metadata, client interface{}) (Collector, error) {
+	c := Collector{
+		lock:     &sync.Mutex{},
+		metadata: md,
+		client:   client,
+		queue:    make(chan []byte),
+	}
+	err := c.validate()
+	if err != nil {
+		// TODO: return appropriate error
+		return c, nil
+	}
+
+	return c, nil
+}
+
+// TODO: validate. Check that method exists. Maybe check that params are correct.
+func (c Collector) validate() error {
+	return nil
+}
+
 func (c Collector) Collect(periodMS int64) error {
 	for {
 		// TODO: pass params
-		raw := reflect.ValueOf(&c).MethodByName(c.metadata.method).Call([]reflect.Value{})
+		raw := reflect.ValueOf(&c).MethodByName(c.metadata.Method).Call([]reflect.Value{})
 		reading, ok := reflect.ValueOf(raw).Interface().(any.Any)
 		if !ok {
 			// TODO: return some appropriate error
@@ -40,7 +61,7 @@ func (c Collector) Collect(periodMS int64) error {
 func (c Collector) Write() error {
 	for r := range c.queue {
 		c.Lock()
-		err := bufferedWrite(c.metadata.destination, r)
+		err := bufferedWrite(c.metadata.Destination, r)
 		c.Unlock()
 		if err != nil {
 			return err
@@ -72,62 +93,3 @@ func (c Collector) SetMetadata(md Metadata) {
 func (c Collector) GetMetadata() Metadata {
 	return c.metadata
 }
-
-//type Collector interface {
-//	Lock() error
-//	// periodMs is period in ms at which data is captured.
-//	Collect(periodMs int64) error
-//	SetMetadata(md Metadata) error
-//	GetMetadata() Metadata
-//}
-//
-//type RotVelocityCollector struct {
-//	lock       *sync.Mutex
-//	metadata   Metadata
-//	params     map[string]string
-//	client     pb.IMUServiceClient // TODO: find generic interface for a PB service client
-//	methodName string
-//	// Will actually be chan of SensorData; just putting any.Any because I want this to compile.
-//	queue chan any.Any
-//}
-//
-//// TODO: Add MD and Client as constructor params
-//func NewRotVelocityCollector() Collector {
-//	md := Metadata{
-//		component:   "IMU",
-//		method:      "ReadAngularVelocity",
-//		destination: nil,
-//	}
-//
-//	ret := RotVelocityCollector{
-//		lock:       &sync.Mutex{},
-//		metadata:   md,
-//		params:     nil,
-//		client:     pb.NewIMUServiceClient(nil),
-//		methodName: "ReadAngularVelocity",
-//		queue:      make(chan any.Any),
-//	}
-//
-//	return ret
-//}
-//
-//func (c RotVelocityCollector) Collect(periodMS int64) error {
-//	for {
-//		// TODO: pass params
-//		reflect.ValueOf(&c).MethodByName(c.methodName).Call([]reflect.Value{})
-//	}
-//
-//	return nil
-//}
-//
-//func (c RotVelocityCollector) Lock() error {
-//	return nil
-//}
-//
-//func (c RotVelocityCollector) SetMetadata(md Metadata) error {
-//	return nil
-//}
-//
-//func (c RotVelocityCollector) GetMetadata() Metadata {
-//	return c.metadata
-//}
