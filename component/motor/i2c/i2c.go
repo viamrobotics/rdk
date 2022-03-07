55690229cac28fc296fffba8f3deb950c2c63beb
Alexis Wei <qq_wei369@hotmail.com>
2022-03-07 15:56:56 -0500

    getting i2c address and bus name from json configs

--- component/motor/i2c/i2c.go
+++ component/motor/i2c/i2c.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/edaniels/golog"
+	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
 
 	"go.viam.com/rdk/component/board"
@@ -13,13 +14,12 @@ import (
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/registry"
 	"go.viam.com/rdk/robot"
-	"go.viam.com/rdk/utils"
 )
 
 type I2CConfig struct {
 	motor.Config
-	I2CBusName string `json:"i2c_bus_name"`
-	I2CAddress string `json:"i2c_address"`
+	BusName    string `json:"bus_name"`
+	I2CAddress byte   `json:"i2c_address"`
 }
 
 const modelName = "i2c"
@@ -27,23 +27,36 @@ const modelName = "i2c"
 func init() {
 	_motor := registry.Component{
 		Constructor: func(ctx context.Context, r robot.Robot, config config.Component, logger golog.Logger) (interface{}, error) {
-			actualBoard, motorConfig, err := getBoardFromRobotConfig(r, config)
-			if err != nil {
-				return nil, err
-			}
+			// actualBoard, _, err := getBoardFromRobotConfig(r, config)
+			// if err != nil {
+			// 	return nil, err
+			// }
 
-			return NewMotor(ctx, actualBoard, *motorConfig, logger)
+			return NewMotor(ctx, r, config.ConvertedAttributes.(*I2CConfig), logger)
 		},
 	}
 	registry.RegisterComponent(motor.Subtype, modelName, _motor)
-	motor.RegisterConfigAttributeConverter(modelName)
+	config.RegisterComponentAttributeMapConverter(
+		config.ComponentTypeMotor,
+		modelName,
+		func(attributes config.AttributeMap) (interface{}, error) {
+			var conf I2CConfig
+			decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{TagName: "json", Squash: true, Result: &conf})
+			if err != nil {
+				return nil, err
+			}
+			if err := decoder.Decode(attributes); err != nil {
+				return nil, err
+			}
+			return &conf, nil
+		}, &I2CConfig{})
 }
 
 // A Motor represents a motor connected via the I2C protocol.
 type Motor struct {
 	board      board.Board
 	bus        board.I2C
-	I2CAddress uint8
+	I2CAddress byte
 	logger     golog.Logger
 }
 
@@ -60,37 +73,26 @@ const (
 	stop                   = "X"
 )
 
-func getBoardFromRobotConfig(r robot.Robot, config config.Component) (board.Board, *motor.Config, error) {
-	motorConfig, ok := config.ConvertedAttributes.(*motor.Config)
-	if !ok {
-		return nil, nil, utils.NewUnexpectedTypeError(motorConfig, config.ConvertedAttributes)
-	}
-	if motorConfig.BoardName == "" {
-		return nil, nil, errors.New("expected board name in config for motor")
-	}
-	b, err := board.FromRobot(r, motorConfig.BoardName)
-	if err != nil {
-		return nil, nil, err
-	}
-	return b, motorConfig, nil
-}
-
 // NewMotor returns a motor with I2C protocol.
-func NewMotor(ctx context.Context, b board.Board, c motor.Config, logger golog.Logger) (*Motor, error) {
+func NewMotor(ctx context.Context, r robot.Robot, c *I2CConfig, logger golog.Logger) (*Motor, error) {
+	b, err := board.FromRobot(r, c.BoardName)
+	if err != nil {
+		return nil, err
+	}
+
 	localB, ok := b.(board.LocalBoard)
 	if !ok {
 		return nil, fmt.Errorf("board %s is not local", c.BoardName)
 	}
-	busName := "bus1"
-	bus, ok := localB.I2CByName(busName)
+	bus, ok := localB.I2CByName(c.BusName)
 	if !ok {
-		return nil, errors.Errorf("can't find I2C bus (%s) requested by Motor", busName)
+		return nil, errors.Errorf("can't find I2C bus (%s) requested by Motor", c.BusName)
 	}
 
 	m := &Motor{
 		board:      b,
 		bus:        bus,
-		I2CAddress: 103,
+		I2CAddress: c.I2CAddress,
 		logger:     logger,
 	}
 
@@ -100,7 +102,7 @@ func NewMotor(ctx context.Context, b board.Board, c motor.Config, logger golog.L
 func (m *Motor) writeReg(ctx context.Context, command []byte) error {
 
 	// no
-	handle, err := m.bus.OpenHandle(0x67)
+	handle, err := m.bus.OpenHandle(m.I2CAddress)
 	if err != nil {
 		fmt.Println("broken at openhandle")
 		return err
@@ -117,20 +119,14 @@ func (m *Motor) writeReg(ctx context.Context, command []byte) error {
 // SetPower sets the percentage of power the motor should employ between -1 and 1.
 // Negative power implies a backward directional rotational
 func (m *Motor) SetPower(ctx context.Context, powerPct float64) error {
-	// var base = 16
-	// var size = 16
-	// command := uint16(0)
 	command := []byte{}
-	if powerPct == 0 {
-		fmt.Println("power to 0")
-		// value, _ := strconv.ParseUint(find, base, size)
-		// command = uint16(value) // done!
-		command = []byte(dispenseTilStop)
-	} else {
+	if powerPct < 0 {
+		command = []byte(reverseDispenseTilStop)
+	} else if powerPct == 0 {
 		fmt.Println("power to something else")
-		// value, _ := strconv.ParseUint("dfgdhshgs", base, size)
-		// command = uint16(value) // done!
 		command = []byte(stop)
+	} else {
+		command = []byte(dispenseTilStop)
 	}
 	fmt.Println(command)
 	writeErr := m.writeReg(ctx, command)
