33ed2ffdb5c87791e0de5520d2122a5e77779dfe
Bijan Haney <bijan@viam.com>
2022-03-09 16:49:35 -0500

    adds tests

---
+++ component/camera/config_test.go
@@ -0,0 +1,29 @@
+package camera
+
+import (
+	"testing"
+
+	"go.viam.com/test"
+
+	"go.viam.com/rdk/config"
+)
+
+func TestCommonCameraAttributes(t *testing.T) {
+	testConfWrong := config.AttributeMap{
+		"source": "TestSource",
+		"width":  5,
+		"height": "7",
+	}
+	_, err := CommonCameraAttributes(testConfWrong)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "'height' expected type 'int', got unconvertible type")
+	testConf := config.AttributeMap{
+		"source": "TestSource",
+		"width":  5,
+		"height": 7,
+	}
+	res, err := CommonCameraAttributes(testConf)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, res.Source, test.ShouldEqual, "TestSource")
+	test.That(t, res.Width, test.ShouldEqual, 5)
+	test.That(t, res.Height, test.ShouldEqual, 7)
+}
