4d5e818d6053c1e00463cf077dd6e6b45364d647
randhid <rand@viam.com>
2022-03-14 15:23:33 -0400

    Rename imu symbols, add control Kalman filter

--- control/kalman_filter.go
+++ control/kalman_filter.go
@@ -1,1 +1,16 @@
 package control
+
+import "gonum.org/v1/gonum/mat"
+
+//
+
+type kalmanFilter struct {
+	X *mat.VecDense // System State Matrix
+	P *mat.Dense    // Covariance Matrix
+}
+
+// input model/ current state/
+func (kF *kalmanFilter) NextCovariance() error {
+	kF.P = mat.NewDense(1, 1, []float64{1})
+	return nil
+}
