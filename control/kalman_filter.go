7d3b0816d4fcef32147558f373e42ec5c8069ddf
randhid <rand@viam.com>
2022-03-15 12:24:40 -0400

    New Filters created

--- control/kalman_filter.go
+++ control/kalman_filter.go
@@ -5,12 +5,32 @@ import "gonum.org/v1/gonum/mat"
 //
 
 type kalmanFilter struct {
-	X *mat.VecDense // System State Matrix
-	P *mat.Dense    // Covariance Matrix
+	state      *mat.VecDense // System State Matrix
+	covariance *mat.Dense    // Covariance Matrix
+}
+
+type model struct {
 }
 
 // input model/ current state/
 func (kF *kalmanFilter) NextCovariance() error {
-	kF.P = mat.NewDense(1, 1, []float64{1})
+	//has to be square
+	kF.covariance = mat.NewDense(1, 1, []float64{1})
 	return nil
+}
+
+func (kF *kalmanFilter) NextState() (mat.Matrix, error) {
+	kF.state = mat.NewVecDense(1, []float64{1})
+	return kF.covariance, nil
+}
+
+func (kF *kalmanFilter) Predict() error {
+	return nil
+}
+func (kF *kalmanFilter) Update() error {
+	return nil
+}
+
+func (kF *kalmanFilter) State() (mat.Vector, error) {
+	return kF.state, nil
 }
