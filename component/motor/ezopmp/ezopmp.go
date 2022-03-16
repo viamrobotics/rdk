4ec0cb1118a20ad9800f0a9da32d8bff9c12cd05
Alexis Wei <qq_wei369@hotmail.com>
2022-03-16 17:27:42 -0400

    updated ezopmp methods

--- component/motor/ezopmp/ezopmp.go
+++ component/motor/ezopmp/ezopmp.go
@@ -2,10 +2,12 @@
 package ezopmp
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"math"
 	"strconv"
+	"strings"
 
 	"github.com/edaniels/golog"
 	"github.com/mitchellh/mapstructure"
@@ -20,8 +22,9 @@ import (
 
 type EZOPMPConfig struct {
 	motor.Config
-	BusName    string `json:"bus_name"`
-	I2CAddress byte   `json:"i2c_address"`
+	BusName     string `json:"bus_name"`
+	I2CAddress  byte   `json:"i2c_address"`
+	MaxReadBits int    `json:"max_read_bits"`
 }
 
 const modelName = "ezopmp"
@@ -56,10 +59,12 @@ func init() {
 
 // A Motor represents a motor connected via the I2C protocol.
 type Motor struct {
-	board      board.Board
-	bus        board.I2C
-	I2CAddress byte
-	logger     golog.Logger
+	board       board.Board
+	bus         board.I2C
+	I2CAddress  byte
+	maxReadBits int
+	logger      golog.Logger
+	maxPowerPct float64
 }
 
 // avaiable commands
@@ -99,10 +104,12 @@ func NewMotor(ctx context.Context, r robot.Robot, c *EZOPMPConfig, logger golog.
 	}
 
 	m := &Motor{
-		board:      b,
-		bus:        bus,
-		I2CAddress: c.I2CAddress,
-		logger:     logger,
+		board:       b,
+		bus:         bus,
+		I2CAddress:  c.I2CAddress,
+		maxReadBits: c.MaxReadBits,
+		logger:      logger,
+		maxPowerPct: 1.0,
 	}
 
 	return m, nil
@@ -122,10 +129,10 @@ func (m *Motor) writeReg(ctx context.Context, command []byte) error {
 	return handle.Write(ctx, command)
 }
 
-func (m *Motor) readReg(ctx context.Context, command []byte) error {
+func (m *Motor) readReg(ctx context.Context) ([]byte, error) {
 	handle, err := m.bus.OpenHandle(m.I2CAddress)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer func() {
 		if err := handle.Close(); err != nil {
@@ -133,52 +140,114 @@ func (m *Motor) readReg(ctx context.Context, command []byte) error {
 		}
 	}()
 
-	readInput, err := strconv.Atoi(string(command))
-	readVal, err := handle.Read(ctx, readInput)
-	fmt.Println(readVal)
-	return err
+	readVal := []byte{254, 0}
+	for readVal[0] == 254 {
+		readVal, err = handle.Read(ctx, m.maxReadBits)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if readVal[0] == 1 {
+		return bytes.Trim(readVal[1:], "\x00"), nil
+	} else if readVal[0] == 2 {
+		return nil, errors.New("syntax error, code: 2")
+	} else if readVal[0] == 255 {
+		return nil, errors.New("no data to send, code: 255")
+	}
+	return nil, nil
+}
+
+func (m *Motor) writeRegWithCheck(ctx context.Context, command []byte) error {
+	writeErr := m.writeReg(ctx, command)
+	if writeErr != nil {
+		return writeErr
+	}
+	_, readErr := m.readReg(ctx)
+	if readErr != nil {
+		return readErr
+	}
+	return nil
 }
 
 // SetPower sets the percentage of power the motor should employ between -1 and 1.
 // Negative power implies a backward directional rotational
 // for this pump, it goes between 0.5ml to 105ml/min
 func (m *Motor) SetPower(ctx context.Context, powerPct float64) error {
-	command := []byte{}
-	powerVal := (powerPct / 100 * 104.5) + 0.5
-	if powerPct < 0 {
-		stringVal := "DC," + fmt.Sprintf("%f", -1*math.Round(powerVal*100)/100) + ",*"
-		fmt.Println(stringVal)
-		command = []byte(stringVal)
-	} else if powerPct == 0 {
+	powerPct = math.Min(powerPct, m.maxPowerPct)
+	powerPct = math.Max(powerPct, -1*m.maxPowerPct)
+	var command []byte
+	fmt.Println("print power")
+	powerVal := (powerPct * 104.5) + 0.5
+	if powerPct == 0 {
 		command = []byte(stop)
 	} else {
 		stringVal := "DC," + fmt.Sprintf("%f", math.Round(powerVal*100)/100) + ",*"
 		fmt.Println(stringVal)
 		command = []byte(stringVal)
 	}
-	fmt.Println(command)
+	return m.writeRegWithCheck(ctx, command)
+}
+
+// rpm here will actually map onto the mm/s as calculated based on the datasheet of this motor
+// the max rpm: 61, min rpm:1
+// every second is assume to be 1 revolution because the rpm on this pump motor remains the same
+func (m *Motor) GoFor(ctx context.Context, rpm float64, revolutions float64) error {
+	volPerMin := rpm * 9 * math.Pi * 60 / 1000
+	time := revolutions / 60
+	commandString := "DC," + fmt.Sprintf("%f", volPerMin) + "," + fmt.Sprintf("%f", time)
+	command := []byte(commandString)
+	return m.writeRegWithCheck(ctx, command)
+}
+
+// using the Dose Over Time Command in the EZO-PMP datasheet
+// position = desired mm
+// rpm = desired mm/s
+func (m *Motor) GoTo(ctx context.Context, rpm float64, positionRevolutions float64) error {
+	// if pos is negative, then we have to turn in megative rpm,
+	// we cannot have negative time, that doesn't make any sense
+	mLPerMin := rpm * 9 * math.Pi * 60 / 1000
+	totTimeInMin := math.Abs(positionRevolutions/rpm) / 60
+	if positionRevolutions < 0 {
+		mLPerMin = mLPerMin * -1
+	}
+	commandString := "D," + fmt.Sprintf("%f", mLPerMin) + "," + fmt.Sprintf("%f", totTimeInMin)
+	command := []byte(commandString)
+	return m.writeRegWithCheck(ctx, command)
+}
+
+// in this case, we clear the amount of volume that has been dispensed
+func (m *Motor) ResetZeroPosition(ctx context.Context, offset float64) error {
+	command := []byte(clear)
 	writeErr := m.writeReg(ctx, command)
 	if writeErr != nil {
-		return errors.New(writeErr.Error())
+		return writeErr
 	}
-	return nil
-}
-
-func (m *Motor) GoFor(ctx context.Context, rpm float64, revolutions float64) error {
-	return nil
-}
-
-func (m *Motor) GoTo(ctx context.Context, rpm float64, positionRevolutions float64) error {
-	return nil
-}
-
-func (m *Motor) ResetZeroPosition(ctx context.Context, offset float64) error {
+	val, err := m.readReg(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(val)
 	return nil
 }
 
 // for this pump, it will return the volume dispensed
 func (m *Motor) GetPosition(ctx context.Context) (float64, error) {
-	return 0, nil
+	command := []byte(totVolDispensed)
+	//fmt.Println("getting position")
+	writeErr := m.writeReg(ctx, command)
+	if writeErr != nil {
+		return 0, writeErr
+	}
+	val, err := m.readReg(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	splitMsg := strings.Split(string(val), ",")
+	floatVal, err := strconv.ParseFloat(splitMsg[1], 64)
+	return floatVal, err
 }
 
 // GetFeatures returns the status of optional features on the motor.
@@ -190,24 +259,32 @@ func (m *Motor) GetFeatures(ctx context.Context) (map[motor.Feature]bool, error)
 
 func (m *Motor) Stop(ctx context.Context) error {
 	command := []byte(stop)
-	writeErr := m.writeReg(ctx, command)
-	if writeErr != nil {
-		return errors.New(writeErr.Error())
-	}
-	return nil
+	return m.writeRegWithCheck(ctx, command)
 }
 
 func (m *Motor) IsPowered(ctx context.Context) (bool, error) {
-	// command := []byte(status)
-	// readErr := m.read
+	command := []byte(dispenseStatus)
+	writeErr := m.writeReg(ctx, command)
+	if writeErr != nil {
+		return false, writeErr
+	}
+	val, err := m.readReg(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+
+	splitMsg := strings.Split(string(val), ",")
+
+	pumpStatus, err := strconv.ParseFloat(splitMsg[2], 64)
+	if err != nil {
+		return false, err
+	}
+
+	fmt.Println(pumpStatus)
+
+	if pumpStatus == 1 {
+		return true, nil
+	}
 	return false, nil
-}
-
-// allows you to set and change the I2C address of the device
-func (m *Motor) SetAddress() {
-
-}
-
-func (m *Motor) ControlLED(command string) error {
-	return nil
 }
