495e1350f86c3dd96abb39ce5fbc6f3e2fc43c0e
Aleksei Tryputsen <atryoutsen@theatlantic.com>
2022-03-18 09:22:00 -0500

    Added grid component

--- web/frontend/packages/dls/.storybook/config.js
+++ web/frontend/packages/dls/.storybook/config.js
@@ -3,7 +3,7 @@ import Vue from "vue";
 import "./index.css";
 
 import InputController from '../src/components/InputController';
-
+import Grid from '../src/components/Grid';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -12,6 +12,7 @@ library.add(faCheckSquare)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('InputController', InputController);
+Vue.component('Grid', Grid);
 
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
