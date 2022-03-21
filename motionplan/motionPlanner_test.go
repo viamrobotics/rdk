ec3a70e0dae14ba89a6c1c856f19133f05994bd1
Alexa Greenberg <alexa.greenberg@viam.com>
2022-03-21 14:09:03 -0400

    Implement motion planning for a mobile base (#595)

--- motionplan/motionPlanner_test.go
+++ motionplan/motionPlanner_test.go
@@ -5,23 +5,82 @@ import (
 	"testing"
 
 	"github.com/edaniels/golog"
+	"github.com/golang/geo/r3"
 	"go.viam.com/test"
 
 	commonpb "go.viam.com/rdk/proto/api/common/v1"
-	"go.viam.com/rdk/referenceframe"
+	frame "go.viam.com/rdk/referenceframe"
 	spatial "go.viam.com/rdk/spatialmath"
 	"go.viam.com/rdk/utils"
 )
 
+func TestSimple2DMotion(t *testing.T) {
+	// Test Map:
+	//      - bounds are from (-10, -10) to (10, 10)
+	//      - obstacle from (-4, 4) to (4, 10)
+	// ------------------------
+	// | +      |    |      + |
+	// |        |    |        |
+	// |        |    |        |
+	// |        |    |        |
+	// |        ------        |
+	// |          *           |
+	// |                      |
+	// |                      |
+	// |                      |
+	// ------------------------
+
+	// setup problem parameters
+	start := frame.FloatsToInputs([]float64{-9., 9.})
+	goal := spatial.PoseToProtobuf(spatial.NewPoseFromPoint(r3.Vector{X: 9, Y: 9, Z: 0}))
+	limits := []frame.Limit{{Min: -10, Max: 10}, {Min: -10, Max: 10}}
+
+	// build obstacles
+	obstacles := map[string]spatial.Geometry{}
+	box, err := spatial.NewBox(spatial.NewPoseFromPoint(r3.Vector{0, 6, 0}), r3.Vector{8, 8, 1})
+	test.That(t, err, test.ShouldBeNil)
+	obstacles["box"] = box
+
+	// build model
+	physicalGeometry, err := spatial.NewBoxCreator(r3.Vector{X: 1, Y: 1, Z: 1}, spatial.NewZeroPose())
+	test.That(t, err, test.ShouldBeNil)
+	model, err := frame.NewMobile2DFrame("mobile-base", limits, physicalGeometry)
+	test.That(t, err, test.ShouldBeNil)
+
+	// plan
+	cbert, err := NewCBiRRTMotionPlanner(model, 1, logger)
+	test.That(t, err, test.ShouldBeNil)
+	opt := NewDefaultPlannerOptions()
+	constraint := NewCollisionConstraintFromFrame(model, obstacles)
+	test.That(t, err, test.ShouldBeNil)
+	opt.AddConstraint("collision", constraint)
+	waypoints, err := cbert.Plan(context.Background(), goal, start, opt)
+	test.That(t, err, test.ShouldBeNil)
+
+	obstacle := obstacles["box"]
+	workspace, err := spatial.NewBox(spatial.NewZeroPose(), r3.Vector{20, 20, 1})
+	test.That(t, err, test.ShouldBeNil)
+	for _, waypoint := range waypoints {
+		pt := r3.Vector{waypoint[0].Value, waypoint[1].Value, 0}
+		collides, err := obstacle.CollidesWith(spatial.NewPoint(pt))
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, collides, test.ShouldBeFalse)
+		inWorkspace, err := workspace.CollidesWith(spatial.NewPoint(pt))
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, inWorkspace, test.ShouldBeTrue)
+		logger.Debugf("%f\t%f\n", pt.X, pt.Y)
+	}
+}
+
 var (
-	home7 = referenceframe.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0, 0})
-	home6 = referenceframe.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0})
+	home7 = frame.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0, 0})
+	home6 = frame.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0})
 )
 
-// This should test a simple linear motion.
-func TestSimpleMotion(t *testing.T) {
+// This should test a simple linear motion for Arms.
+func TestSimpleArmMotion(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	m, err := referenceframe.ParseModelJSONFile(utils.ResolveFile("component/arm/xarm/xArm7_kinematics.json"), "")
+	m, err := frame.ParseModelJSONFile(utils.ResolveFile("component/arm/xarm/xArm7_kinematics.json"), "")
 	test.That(t, err, test.ShouldBeNil)
 
 	mp, err := NewCBiRRTMotionPlanner(m, nCPU/4, logger)
@@ -40,9 +99,9 @@ func TestSimpleMotion(t *testing.T) {
 }
 
 // This should test a simple linear motion on a longer path, with a no-spill constraint.
-func TestComplexMotion(t *testing.T) {
+func TestComplexArmMotion(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	m, err := referenceframe.ParseModelJSONFile(utils.ResolveFile("component/arm/xarm/xArm7_kinematics.json"), "")
+	m, err := frame.ParseModelJSONFile(utils.ResolveFile("component/arm/xarm/xArm7_kinematics.json"), "")
 	test.That(t, err, test.ShouldBeNil)
 
 	mp, err := NewCBiRRTMotionPlanner(m, nCPU/4, logger)
@@ -76,10 +135,10 @@ func TestComplexMotion(t *testing.T) {
 	test.That(t, len(path), test.ShouldNotEqual, 0)
 }
 
-// This should test a simple linear motion.
-func TestSimpleMotionUR5(t *testing.T) {
+// This should test a simple linear motion for a UR5e.
+func TestSimpleMotionUR5e(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	m, err := referenceframe.ParseModelJSONFile(utils.ResolveFile("component/arm/universalrobots/ur5e.json"), "")
+	m, err := frame.ParseModelJSONFile(utils.ResolveFile("component/arm/universalrobots/ur5e.json"), "")
 	test.That(t, err, test.ShouldBeNil)
 	mp, err := NewCBiRRTMotionPlanner(m, nCPU/4, logger)
 	test.That(t, err, test.ShouldBeNil)
