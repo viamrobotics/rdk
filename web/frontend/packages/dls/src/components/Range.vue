22d2c0b1d5de5b130e4459fd515b3cd982c8d46d
sajadmsNew <smousavi.ee@gmail.com>
2022-03-22 22:33:51 +0300

    fix-vue-typescript-issue

--- web/frontend/packages/dls/src/components/Range.vue
+++ web/frontend/packages/dls/src/components/Range.vue
@@ -43,7 +43,7 @@ export default class ViamRange extends Vue {
   @Prop({ default: 0 }) min?: number;
   @Prop({ default: 10 }) step?: number;
   @Prop({ default: null }) name?: string;
-  @Prop({ default: 0 }) percentage?: number | undefined;
+  @Prop({ default: 0 }) percentage?: number | undefined=0;
 
   value: number | undefined = this.percentage;
 }
