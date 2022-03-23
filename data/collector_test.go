4d33fd22711b57b43a1576891845a99496aef5bf
aaroncasas <aaron@viam.com>
2022-03-23 15:43:20 -0400

    Refactor tests.

--- data/collector_test.go
+++ data/collector_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
-	"sync/atomic"
 	"testing"
 	"time"
 
@@ -32,50 +31,44 @@ func (r *exampleReading) toProto() *structpb.Struct {
 }
 
 var (
+	dummyCapturer = CaptureFunc(func(ctx context.Context, _ map[string]string) (interface{}, error) {
+		return dummyReading, nil
+	})
 	dummyReading      = exampleReading{}
 	dummyReadingProto = dummyReading.toProto()
 )
-
-type dummyCapturer struct {
-	ShouldError  bool
-	CaptureCount int64
-}
-
-func (c *dummyCapturer) Capture(_ context.Context, _ map[string]string) (interface{}, error) {
-	if c.ShouldError {
-		return nil, errors.New("error")
-	}
-
-	atomic.AddInt64(&c.CaptureCount, 1)
-	return dummyReading, nil
-}
 
 func TestNewCollector(t *testing.T) {
 	c := NewCollector(nil, time.Second, nil, nil, nil)
 	test.That(t, c, test.ShouldNotBeNil)
 }
 
+// Test that SensorData is written correctly and can be read, and that interval is respected and that capture()
+// is called floor(time_passed/interval) times.
 func TestSuccessfulWrite(t *testing.T) {
 	l := golog.NewTestLogger(t)
 	target1, _ := ioutil.TempFile("", "whatever")
 	defer os.Remove(target1.Name())
-
-	// Verify that it writes to the file.
-	c := NewCollector(&dummyCapturer{}, time.Millisecond*10, map[string]string{"name": "test"}, target1, l)
+	c := NewCollector(dummyCapturer, time.Millisecond*25, map[string]string{"name": "test"}, target1, l)
 	go c.Collect()
-	time.Sleep(time.Millisecond * 20)
+
+	// Verify that it writes to the file at all.
+	time.Sleep(time.Millisecond * 70)
 	c.Close()
 	fileSize := getFileSize(target1)
 	test.That(t, fileSize, test.ShouldBeGreaterThan, 0)
 
-	// Verify that the data it wrote matches what we expect.
+	// Verify that the data it wrote matches what we expect (two SensorData's containing dummyReading).
 	_, _ = target1.Seek(0, 0)
-	read, err := readNextSensorData(target1)
-	if err != nil {
-		t.Fatalf("failed to read SensorData from file: %v", err)
+	// Give 20ms of leeway so slight changes in execution ordering don't impact the test.
+	// floor(70/25) = 2
+	for i := 0; i < 2; i++ {
+		read, err := readNextSensorData(target1)
+		if err != nil {
+			t.Fatalf("failed to read SensorData from file: %v", err)
+		}
+		test.That(t, proto.Equal(dummyReadingProto, read.Data), test.ShouldBeTrue)
 	}
-
-	test.That(t, proto.Equal(dummyReadingProto, read.Data), test.ShouldBeTrue)
 }
 
 func TestClose(t *testing.T) {
@@ -83,35 +76,17 @@ func TestClose(t *testing.T) {
 	l := golog.NewTestLogger(t)
 	target1, _ := ioutil.TempFile("", "whatever")
 	defer os.Remove(target1.Name())
-	dummy := &dummyCapturer{}
-	c := NewCollector(dummy, time.Millisecond*15, map[string]string{"name": "test"}, target1, l)
+	c := NewCollector(dummyCapturer, time.Millisecond*15, map[string]string{"name": "test"}, target1, l)
 	go c.Collect()
 	time.Sleep(time.Millisecond * 25)
 
-	// Measure CaptureCount/fileSize.
-	captureCount := atomic.LoadInt64(&dummy.CaptureCount)
+	// Close and measure fileSize.
 	c.Close()
 	fileSize := getFileSize(target1)
 
-	// Assert capture is no longer being called and the file is not being written to.
+	// Assert capture is no longer being called/file is no longer being written to.
 	time.Sleep(time.Millisecond * 25)
-	test.That(t, atomic.LoadInt64(&dummy.CaptureCount), test.ShouldEqual, captureCount)
 	test.That(t, getFileSize(target1), test.ShouldEqual, fileSize)
-}
-
-// Test that interval is respected and that capture() is called floor(time_passed/interval) times.
-func TestInterval(t *testing.T) {
-	l := golog.NewTestLogger(t)
-	target1, _ := ioutil.TempFile("", "whatever")
-	defer os.Remove(target1.Name())
-	dummy := &dummyCapturer{}
-	c := NewCollector(dummy, time.Millisecond*25, map[string]string{"name": "test"}, target1, l)
-	go c.Collect()
-
-	// Give 20ms of leeway so slight changes in execution ordering don't impact the test.
-	// floor(70/25) = 2
-	time.Sleep(time.Millisecond * 70)
-	test.That(t, atomic.LoadInt64(&dummy.CaptureCount), test.ShouldEqual, 2)
 }
 
 func TestSetTarget(t *testing.T) {
@@ -121,23 +96,20 @@ func TestSetTarget(t *testing.T) {
 	defer os.Remove(target1.Name())
 	defer os.Remove(target2.Name())
 
-	dummy := &dummyCapturer{}
-	c := NewCollector(dummy, time.Millisecond*20, map[string]string{"name": "test"}, target1, l)
+	c := NewCollector(dummyCapturer, time.Millisecond*15, map[string]string{"name": "test"}, target1, l)
 	go c.Collect()
-
-	// Let it write to tgt1 for a bit.
 	time.Sleep(time.Millisecond * 25)
 
-	// Change target, let run for a bit.
+	// Change target, verify that target1 was written to.
 	c.SetTarget(target2)
-	time.Sleep(time.Millisecond * 25)
-
-	// Verify tgt1 and tgt2 were written to, and that any buffered data was flushed when the target was changed.
-	c.Close()
 	sizeTgt1 := getFileSize(target1)
-	sizeTgt2 := getFileSize(target2)
-	test.That(t, sizeTgt1, test.ShouldBeGreaterThan, 0)
-	test.That(t, sizeTgt2, test.ShouldBeGreaterThan, 0)
+	test.That(t, getFileSize(target1), test.ShouldBeGreaterThan, 0)
+
+	// Verify that tgt2 was written to, and that target1 was not written to after the target was changed.
+	time.Sleep(time.Millisecond * 25)
+	c.Close()
+	test.That(t, getFileSize(target1), test.ShouldEqual, sizeTgt1)
+	test.That(t, getFileSize(target2), test.ShouldBeGreaterThan, 0)
 }
 
 // Verifies that Collect does not error if it receives a single error when calling capture, and that those errors are
@@ -146,9 +118,11 @@ func TestSwallowsErrors(t *testing.T) {
 	logger, logs := golog.NewObservedTestLogger(t)
 	target1, _ := ioutil.TempFile("", "whatever")
 	defer os.Remove(target1.Name())
-	dummy := &dummyCapturer{ShouldError: true}
 
-	c := NewCollector(dummy, time.Millisecond*10, map[string]string{"name": "test"}, target1, logger)
+	errorCapturer := CaptureFunc(func(ctx context.Context, _ map[string]string) (interface{}, error) {
+		return nil, errors.New("error")
+	})
+	c := NewCollector(errorCapturer, time.Millisecond*10, map[string]string{"name": "test"}, target1, logger)
 	errorChannel := make(chan error)
 	defer close(errorChannel)
 	go func() {
