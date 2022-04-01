26eee2c0d7491848c04bb5b03bae61a3ccdfcc71
James Otting <james@viam.com>
2022-03-31 18:29:32 -0700

    Add GPIO (interrupt/adc) input controller

--- component/input/register/register.go
+++ component/input/register/register.go
@@ -6,6 +6,7 @@ import (
 	// for inputs.
 	_ "go.viam.com/rdk/component/input/fake"
 	_ "go.viam.com/rdk/component/input/gamepad"
+	_ "go.viam.com/rdk/component/input/gpio"
 	_ "go.viam.com/rdk/component/input/mux"
 	_ "go.viam.com/rdk/component/input/webgamepad"
 )
