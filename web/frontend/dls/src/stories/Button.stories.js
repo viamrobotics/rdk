923d0d241244751a5f8c056e38a85d509498a906
Aleksei Tryputsen <atryoutsen@theatlantic.com>
2022-04-04 08:27:47 -0500

    Added loading example

--- web/frontend/dls/src/stories/Button.stories.js
+++ web/frontend/dls/src/stories/Button.stories.js
@@ -7,5 +7,5 @@ storiesOf("Button", module).add("Default Button", () => ({
     };
   },
   template:
-    '<div><ViamButton color="primary" group variant="primary"><template v-slot:icon><ViamIcon  name="check">Check</ViamIcon></template>Primary</ViamButton><ViamButton color="black" group variant="primary"><template v-slot:icon><ViamIcon  name="check">Check</ViamIcon></template>Primary</ViamButton><ViamButton color="success" group variant="primary">Success</ViamButton><ViamButton color="danger" group variant="primary">Danger</ViamButton></div>',
+    '<div><ViamButton color="primary" group variant="primary"><template v-slot:icon><ViamIcon  name="check">Check</ViamIcon></template>Primary</ViamButton><ViamButton color="black" group variant="primary"><template v-slot:icon><ViamIcon  name="check">Check</ViamIcon></template>Primary</ViamButton><ViamButton color="success" group variant="primary">Success</ViamButton><ViamButton color="danger" group variant="primary">Danger</ViamButton><ViamButton color="success" group variant="primary" loading="true">Loading</ViamButton></div>',
 }));
