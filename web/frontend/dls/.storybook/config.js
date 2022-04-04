b5db2b5175096e7926788fa9f4cc1dbbc235c497
aaroncasas <aaron@viam.com>
2022-04-04 09:26:35 -0400

    errors.Errrof > fmt.Errorf

--- web/frontend/dls/.storybook/config.js
+++ web/frontend/dls/.storybook/config.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import "./index.css";
 
 import InputController from '../src/components/InputController';
+import ViamBadge from '../src/components/Badge';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
@@ -12,6 +13,7 @@ library.add(faCheckSquare)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('InputController', InputController);
+Vue.component('ViamBadge', ViamBadge);
 
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
