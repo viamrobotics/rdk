1a84f4f533e52d6a4744a3f433925268692832ac
raybjork <raymond@viam.com>
2022-04-06 10:45:17 -0400

    add EncompassedBy function to Geometry interface

--- spatialmath/point.go
+++ spatialmath/point.go
@@ -110,6 +110,12 @@ func (pt *point) DistanceFrom(g Geometry) (float64, error) {
 	return math.Inf(-1), newCollisionTypeUnsupportedError(pt, g)
 }
 
+// EncompassedBy returns a bool describing if the given point is completely encompassed by the given geometry.
+// Note: This is only possible if the given geometry is equal to the point.
+func (pt *point) EncompassedBy(g Geometry) (bool, error) {
+	return pt.AlmostEqual(g), nil
+}
+
 // pointVsBoxCollision takes a box and a point as arguments and returns a bool describing if they are in collision. \
 // true == collision / false == no collision.
 func pointVsBoxCollision(b *box, pt r3.Vector) bool {
