44389b812d584795111c698317779cba84e81d09
Cheuk Tse <cheuk@viam.com>
2022-04-07 18:33:25 -0400

    move config processing away from robotutils

--- robotutils/robotutils.go
+++ robotutils/robotutils.go
@@ -3,99 +3,16 @@ package robotutils
 
 import (
 	"context"
-	"crypto/tls"
-	"sync"
 
 	"github.com/edaniels/golog"
 	"go.uber.org/multierr"
 	"go.viam.com/utils"
-	"go.viam.com/utils/rpc"
 
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/robot"
 	robotimpl "go.viam.com/rdk/robot/impl"
 	"go.viam.com/rdk/services/web"
-	rutils "go.viam.com/rdk/utils"
 )
-
-// TLSConfig stores the TLS config for the robot.
-type TLSConfig struct {
-	*tls.Config
-	certMu  sync.Mutex
-	tlsCert *tls.Certificate
-}
-
-// NewTLSConfig creates a new tls config.
-func NewTLSConfig(cfg *config.Config) *TLSConfig {
-	tlsCfg := &TLSConfig{}
-	var tlsConfig *tls.Config
-	if cfg.Cloud != nil && cfg.Cloud.TLSCertificate != "" {
-		tlsConfig = &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			GetCertificate: func(_ *tls.ClientHelloInfo) (*tls.Certificate, error) {
-				// always return same cert
-				tlsCfg.certMu.Lock()
-				defer tlsCfg.certMu.Unlock()
-				return tlsCfg.tlsCert, nil
-			},
-			GetClientCertificate: func(_ *tls.CertificateRequestInfo) (*tls.Certificate, error) {
-				// always return same cert
-				tlsCfg.certMu.Lock()
-				defer tlsCfg.certMu.Unlock()
-				return tlsCfg.tlsCert, nil
-			},
-		}
-	}
-	tlsCfg.Config = tlsConfig
-	return tlsCfg
-}
-
-// UpdateCert updates the TLS certificate to be returned.
-func (t *TLSConfig) UpdateCert(cfg *config.Config) error {
-	cert, err := tls.X509KeyPair([]byte(cfg.Cloud.TLSCertificate), []byte(cfg.Cloud.TLSPrivateKey))
-	if err != nil {
-		return err
-	}
-	t.certMu.Lock()
-	t.tlsCert = &cert
-	t.certMu.Unlock()
-	return nil
-}
-
-// ProcessConfig processes robot configs.
-func ProcessConfig(in *config.Config, tlsCfg *TLSConfig) (*config.Config, error) {
-	out := *in
-	var selfCreds *rpc.Credentials
-	if in.Cloud != nil {
-		if in.Cloud.TLSCertificate != "" {
-			if err := tlsCfg.UpdateCert(in); err != nil {
-				return nil, err
-			}
-		}
-
-		selfCreds = &rpc.Credentials{rutils.CredentialsTypeRobotSecret, in.Cloud.Secret}
-		out.Network.TLSConfig = tlsCfg.Config // override
-	}
-
-	out.Remotes = make([]config.Remote, len(in.Remotes))
-	copy(out.Remotes, in.Remotes)
-	for idx, remote := range out.Remotes {
-		remoteCopy := remote
-		if in.Cloud == nil {
-			remoteCopy.Auth.SignalingCreds = remoteCopy.Auth.Credentials
-		} else {
-			if remote.ManagedBy != in.Cloud.ManagedBy {
-				continue
-			}
-			remoteCopy.Auth.Managed = true
-			remoteCopy.Auth.SignalingServerAddress = in.Cloud.SignalingAddress
-			remoteCopy.Auth.SignalingAuthEntity = in.Cloud.ID
-			remoteCopy.Auth.SignalingCreds = selfCreds
-		}
-		out.Remotes[idx] = remoteCopy
-	}
-	return &out, nil
-}
 
 // RobotFromConfigPath is a helper to read and process a config given its path and then create a robot based on it.
 func RobotFromConfigPath(ctx context.Context, cfgPath string, logger golog.Logger) (robot.LocalRobot, error) {
@@ -109,8 +26,8 @@ func RobotFromConfigPath(ctx context.Context, cfgPath string, logger golog.Logge
 
 // RobotFromConfig is a helper to process a config and then create a robot based on it.
 func RobotFromConfig(ctx context.Context, cfg *config.Config, logger golog.Logger) (robot.LocalRobot, error) {
-	tlsConfig := NewTLSConfig(cfg)
-	processedCfg, err := ProcessConfig(cfg, tlsConfig)
+	tlsConfig := config.NewTLSConfig(cfg)
+	processedCfg, err := config.ProcessConfig(cfg, tlsConfig)
 	if err != nil {
 		return nil, err
 	}
