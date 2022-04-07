27a1e96eb2bfbf31b4585fec39011088241c6d6b
Cheuk Tse <cheuk@viam.com>
2022-04-07 17:58:58 -0400

    add prev change back in

--- robot/impl/resource_manager.go
+++ robot/impl/resource_manager.go
@@ -317,6 +317,10 @@ func (manager *resourceManager) newRemotes(ctx context.Context, remotes []config
 					RemoveAuthCredentials: true,
 				}))
 			}
+		} else {
+			dialOpts = append(dialOpts, rpc.WithWebRTCOptions(rpc.DialWebRTCOptions{
+				Config: &rpc.DefaultWebRTCConfiguration,
+			}))
 		}
 
 		var outerError error
