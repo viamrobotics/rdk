d6ddbc87ce35260da6d3f01c5d50a5eb721fc7b9
Bijan Haney <bijan@viam.com>
2022-04-08 10:49:33 -0400

    String() method

--- services/framesystem/client.go
+++ services/framesystem/client.go
@@ -90,11 +90,3 @@ func (c *client) FrameSystem(ctx context.Context) (referenceframe.FrameSystem, e
 	}
 	return NewFrameSystemFromParts("remote robot", "", parts, c.logger)
 }
-
-func (c *client) String(ctx context.Context) (string, error) {
-	parts, err := c.Config(ctx)
-	if err != nil {
-		return "", err
-	}
-	return parts.String(), nil
-}
