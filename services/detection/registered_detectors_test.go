a489544e6fccd754313436d08100cc4995a4409f
Bijan Haney <bijan@viam.com>
2022-04-15 15:24:34 -0400

    [DATA-70] create detection registry, add Label() to Detection interface (#727)

---
+++ services/detection/registered_detectors_test.go
@@ -0,0 +1,34 @@
+package detection
+
+import (
+	"image"
+	"testing"
+
+	"go.viam.com/test"
+
+	objdet "go.viam.com/rdk/vision/objectdetection"
+)
+
+func TestDetectorRegistry(t *testing.T) {
+	fn := func(image.Image) ([]objdet.Detection, error) {
+		return []objdet.Detection{objdet.NewDetection(image.Rectangle{}, 0.0, "")}, nil
+	}
+	fnName := "x"
+	// no detector
+	test.That(t, func() { RegisterDetector(fnName, nil) }, test.ShouldPanic)
+	// success
+	RegisterDetector(fnName, fn)
+	// detector names
+	names := DetectorNames()
+	test.That(t, names, test.ShouldNotBeNil)
+	test.That(t, names, test.ShouldContain, fnName)
+	// look up
+	det, err := DetectorLookup(fnName)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, det, test.ShouldEqual, fn)
+	det, err = DetectorLookup("z")
+	test.That(t, err.Error(), test.ShouldContainSubstring, "no Detector with name")
+	test.That(t, det, test.ShouldBeNil)
+	// duplicate
+	test.That(t, func() { RegisterDetector(fnName, fn) }, test.ShouldPanic)
+}
