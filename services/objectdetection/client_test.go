7ac79f8eaa475530b9affaac523f7d0a381009e4
Bijan Haney <bijan@viam.com>
2022-04-19 18:07:00 -0400

    more tests and linting

--- services/objectdetection/client_test.go
+++ services/objectdetection/client_test.go
@@ -9,11 +9,15 @@ import (
 	"go.viam.com/test"
 	"go.viam.com/utils"
 	"go.viam.com/utils/rpc"
+	"google.golang.org/grpc"
 
+	"go.viam.com/rdk/config"
+	servicepb "go.viam.com/rdk/proto/api/service/objectdetection/v1"
 	"go.viam.com/rdk/registry"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/objectdetection"
 	"go.viam.com/rdk/subtype"
+	"go.viam.com/rdk/testutils"
 	"go.viam.com/rdk/testutils/inject"
 )
 
@@ -24,9 +28,9 @@ func TestClient(t *testing.T) {
 	rpcServer, err := rpc.NewServer(logger, rpc.WithUnauthenticated())
 	test.That(t, err, test.ShouldBeNil)
 
-	injectODS := &inject.ObjectDetectionService{}
+	srv := createService(t, "data/fake.json")
 	m := map[resource.Name]interface{}{
-		objectdetection.Name: injectODS,
+		objectdetection.Name: srv,
 	}
 	svc, err := subtype.New(m)
 	test.That(t, err, test.ShouldBeNil)
@@ -48,15 +52,73 @@ func TestClient(t *testing.T) {
 		client, err := objectdetection.NewClient(context.Background(), "", listener1.Addr().String(), logger)
 		test.That(t, err, test.ShouldBeNil)
 
-		expSlice := []string{"client_test"}
-		injectODS.DetectorNamesFunc = func(ctx context.Context) ([]string, error) {
-			return expSlice, nil
-		}
-
+		expSlice := []string{"detector_3"}
 		names, err := client.DetectorNames(context.Background())
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, names, test.ShouldResemble, expSlice)
 
 		test.That(t, utils.TryClose(context.Background(), client), test.ShouldBeNil)
 	})
+
+	t.Run("add detector", func(t *testing.T) {
+		client, err := objectdetection.NewClient(context.Background(), "", listener1.Addr().String(), logger)
+		test.That(t, err, test.ShouldBeNil)
+
+		cfg := objectdetection.RegistryConfig{
+			Name: "new_detector",
+			Type: "color",
+			Parameters: config.AttributeMap{
+				"detect_color": "#112233",
+				"tolerance":    0.9,
+				"segment_size": 3333333,
+			},
+		}
+		// success
+		ok, err := client.AddDetector(context.Background(), cfg)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, ok, test.ShouldBeTrue)
+
+		expSlice := []string{"detector_3", "new_detector"}
+		names, err := client.DetectorNames(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, names, test.ShouldResemble, expSlice)
+		// failure - tries to add a detector again
+		ok, err = client.AddDetector(context.Background(), cfg)
+		test.That(t, err.Error(), test.ShouldContainSubstring, "trying to register two detectors with the same name")
+		test.That(t, ok, test.ShouldBeFalse)
+
+		test.That(t, utils.TryClose(context.Background(), client), test.ShouldBeNil)
+	})
+}
+
+func TestClientDialerOption(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	listener, err := net.Listen("tcp", "localhost:0")
+	test.That(t, err, test.ShouldBeNil)
+	gServer := grpc.NewServer()
+
+	injectODS := &inject.ObjectDetectionService{}
+	m := map[resource.Name]interface{}{
+		objectdetection.Name: injectODS,
+	}
+	server, err := newServer(m)
+	test.That(t, err, test.ShouldBeNil)
+	servicepb.RegisterObjectDetectionServiceServer(gServer, server)
+
+	go gServer.Serve(listener)
+	defer gServer.Stop()
+
+	td := &testutils.TrackingDialer{Dialer: rpc.NewCachedDialer()}
+	ctx := rpc.ContextWithDialer(context.Background(), td)
+	client1, err := objectdetection.NewClient(ctx, "", listener.Addr().String(), logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, td.NewConnections, test.ShouldEqual, 3)
+	client2, err := objectdetection.NewClient(ctx, "", listener.Addr().String(), logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, td.NewConnections, test.ShouldEqual, 3)
+
+	err = utils.TryClose(context.Background(), client1)
+	test.That(t, err, test.ShouldBeNil)
+	err = utils.TryClose(context.Background(), client2)
+	test.That(t, err, test.ShouldBeNil)
 }
