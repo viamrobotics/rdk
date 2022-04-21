25a4cf0ce498665e3c4aeec2a28b2c353f8ae6d7
Alexis Wei <qq_wei369@hotmail.com>
2022-04-21 08:52:48 -0400

    before i break any more, commit

--- ml/inference/inference.go
+++ ml/inference/inference.go
@@ -9,13 +9,31 @@ import (
 	_ "image/png"
 	_ "path/filepath"
 
-	"github.com/mattn/go-tflite"
+	tflite "github.com/mattn/go-tflite"
 )
 
+type GoTflite interface {
+	NewModelFromFile(path string) *tflite.Model
+	NewInterpreterOptions() *tflite.InterpreterOptions
+	NewInterpreter(model *tflite.Model, options *tflite.InterpreterOptions) *tflite.Interpreter
+}
+
+type TfliteModel interface {
+	Delete()
+}
+
+type TfliteInterpreterOptions interface {
+	Delete()
+	SetNumThreads(num int)
+	SetErrorReporter()
+}
+
+type TfliteInterpreter interface {
+	Delete()
+}
+
 type InterpreterLoader struct {
-	modelLoader       func(path string) *tflite.Model
-	optionsLoader     func() *tflite.InterpreterOptions
-	interpreterLoader func(model *tflite.Model, options *tflite.InterpreterOptions) *tflite.Interpreter
+	tflite GoTflite
 }
 
 type FullInterpreter struct {
@@ -26,11 +44,7 @@ type FullInterpreter struct {
 
 // GetDefaultInterpreterLoader returns the default loader when using tflite
 func GetDefaultInterpreterLoader() *InterpreterLoader {
-	loader := &InterpreterLoader{
-		modelLoader:       tflite.NewModelFromFile,
-		optionsLoader:     tflite.NewInterpreterOptions,
-		interpreterLoader: tflite.NewInterpreter,
-	}
+	loader := InterpreterLoader{tflite}
 	return loader
 }
 
