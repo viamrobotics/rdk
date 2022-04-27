eeae18f5ee6d1560b826f507bea5b798d91422da
raybjork <raymond@viam.com>
2022-04-27 10:43:58 -0400

    wip

--- motionplan/collision.go
+++ motionplan/collision.go
@@ -53,42 +53,6 @@ type collisionEntity struct {
 	geometry spatial.Geometry
 }
 
-<<<<<<< HEAD
-type (
-	// type for function that performs a collision check between a key entity and and a test entity.
-	collisionCheckFn func(key, test *collisionEntity) (float64, error)
-
-	// collisionReportFn is a type for function that takes in a list of distances reported by a collision check between a key entity
-	// and a set of test entities and returns a list of ints corresponding to elemements in the array that should be treated as collisions.
-	collisionReportFn func(distances []float64) []int
-)
-
-// CollisionEntities defines an interface for a set of collisionEntities that can be treated as a single batch.
-type CollisionEntities interface {
-	count() int
-	entityFromIndex(int) *collisionEntity
-	indexFromName(string) (int, error)
-	collisionCheckFn() collisionCheckFn
-	collisionReportFn() collisionReportFn
-}
-
-// defaultCollisionEntities defines an implementation for CollisionEntities that other implementations can inherit from.
-type defaultCollisionEntities struct {
-	entities []*collisionEntity
-	indices  map[string]int
-	checkFn  collisionCheckFn
-	reportFn collisionReportFn
-}
-
-// newCollisionEntities is a constructor for a defaultCollisionEntities and takes in geometries, and 2 functions defining their treatment
-//     - checkFn defines how the entities should be checked for collision
-//     - reportFn defines the collision entities will report their collisions
-func newCollisionEntities(
-	geometries map[string]spatial.Geometry,
-	checkFn collisionCheckFn,
-	reportFn collisionReportFn,
-) (*defaultCollisionEntities, error) {
-=======
 // CollisionEntities defines an interface for a set of collisionEntities that can be treated as a single batch.
 type CollisionEntities interface {
 	count() int
@@ -107,7 +71,6 @@ type ObjectCollisionEntities struct {
 
 // NewObjectCollisionEntities is a constructor for ObjectCollisionEntities, an exported implementation of CollisionEntities.
 func NewObjectCollisionEntities(geometries map[string]spatial.Geometry) (*ObjectCollisionEntities, error) {
->>>>>>> b7db533b0b0c01c54fd46e2b719c0072a6fab7bc
 	entities := make([]*collisionEntity, len(geometries))
 	indices := make(map[string]int, len(geometries))
 	size := 0
@@ -118,143 +81,6 @@ func NewObjectCollisionEntities(geometries map[string]spatial.Geometry) (*Object
 		entities[size] = &collisionEntity{name, geometry}
 		indices[name] = size
 		size++
-<<<<<<< HEAD
-	}
-	return &defaultCollisionEntities{entities, indices, checkFn, reportFn}, nil
-}
-
-// count returns the number of collisionEntities in a CollisionEntities class.
-func (gce *defaultCollisionEntities) count() int {
-	return len(gce.entities)
-}
-
-// entityFromIndex returns the entity in the CollisionEntities class that corresponds to the given index.
-func (gce *defaultCollisionEntities) entityFromIndex(index int) *collisionEntity {
-	return gce.entities[index]
-}
-
-// indexFromName returns the index in the CollisionEntities class that corresponds to the given name.
-func (gce *defaultCollisionEntities) indexFromName(name string) (int, error) {
-	if index, ok := gce.indices[name]; ok {
-		return index, nil
-	}
-	return -1, fmt.Errorf("collision entity %q not found", name)
-}
-
-// collisionCheckFn returns the collisionCheckFn associated with the CollisionEntities class.
-func (gce *defaultCollisionEntities) collisionCheckFn() collisionCheckFn {
-	return gce.checkFn
-}
-
-// collisionReportFn returns the collisionReportFn associated with the CollisionEntities class.
-func (gce *defaultCollisionEntities) collisionReportFn() collisionReportFn {
-	return gce.reportFn
-}
-
-// ObjectCollisionEntities is an implementation of CollisionEntities for entities that occupy physical space and should not be intersected
-// it is exported because the key CollisionEntities in a CollisionSystem must be of this type.
-type ObjectCollisionEntities struct{ *defaultCollisionEntities }
-
-// NewObjectCollisionEntities is a constructor for ObjectCollisionEntities, an exported implementation of CollisionEntities.
-func NewObjectCollisionEntities(geometries map[string]spatial.Geometry) (*ObjectCollisionEntities, error) {
-	entities, err := newCollisionEntities(
-		geometries,
-		func(key, test *collisionEntity) (float64, error) {
-			distance, err := key.geometry.DistanceFrom(test.geometry)
-			return -distance, err // multiply distance by -1 so that weights of edges are positive
-		},
-		func(distances []float64) []int {
-			collisionIndices := make([]int, 0)
-			for i := range distances {
-				if distances[i] >= 0 {
-					collisionIndices = append(collisionIndices, i)
-				}
-			}
-			return collisionIndices
-		},
-	)
-	return &ObjectCollisionEntities{entities}, err
-}
-
-// spaceCollisionEntities is an implementation of CollisionEntities for entities that do not occupy physical space but
-// represent an area in which other entities should be encompassed by.
-type spaceCollisionEntities struct{ *defaultCollisionEntities }
-
-// NewSpaceCollisionEntities is a constructor for spaceCollisionEntities.
-func NewSpaceCollisionEntities(geometries map[string]spatial.Geometry) (CollisionEntities, error) {
-	entities, err := newCollisionEntities(
-		geometries,
-		func(key, test *collisionEntity) (float64, error) {
-			encompassed, err := key.geometry.EncompassedBy(test.geometry)
-			if err != nil {
-				return math.NaN(), err
-			}
-			// TODO(rb): EncompassedBy should also report distance required to resolve the collision
-			if !encompassed {
-				return 1, nil
-			}
-			return -1, nil
-		},
-		func(distances []float64) []int {
-			collisionIndices := make([]int, 0)
-			for i := range distances {
-				if distances[i] >= 0 {
-					collisionIndices = append(collisionIndices, i)
-				} else {
-					return []int{}
-				}
-			}
-			return collisionIndices
-		},
-	)
-	return &spaceCollisionEntities{entities}, err
-}
-
-// collisionGraph is an implementation of an undirected graph used to track collisions between two set of CollisionEntities.
-type collisionGraph struct {
-	// key CollisionEntities
-	key *ObjectCollisionEntities
-
-	// test CollisionEntities are the set of CollisionEntities from which the collisionGraph takes its
-	// collisionCheckFn and collisionReportFn functions to check and report collisions between the
-	// test CollisionEntities and key Collision Entities
-	test CollisionEntities
-
-	// adjacencies is 2D array encoding edges between collisionEntiies in the collisionGraph.
-	// if adjacencies[i][j] >= 0 this corresponds to an edge between the entities at key[i] and test[j]
-	adjacencies [][]float64
-
-	// triangular is a bool that describes if the adjacencies matrix is triangular, which will be the case when key == test
-	triangular bool
-}
-
-// newCollisionGraph instantiates a collisionGraph object and checks for collisions between the key and test sets of CollisionEntities
-// collisions that are reported in the reference CollisionSystem argument will be ignore and not stored as edges in the graph.
-func newCollisionGraph(key *ObjectCollisionEntities, test CollisionEntities, reference *CollisionSystem) (*collisionGraph, error) {
-	var err error
-	cg := &collisionGraph{key: key, test: test, adjacencies: make([][]float64, key.count()), triangular: key == test}
-	for i := range cg.adjacencies {
-		cg.adjacencies[i] = make([]float64, test.count())
-		keyi := key.entityFromIndex(i)
-		startIndex := 0
-		if cg.triangular {
-			startIndex = i + 1
-			for j := 0; j < startIndex; j++ {
-				cg.adjacencies[i][j] = math.NaN()
-			}
-		}
-		for j := startIndex; j < len(cg.adjacencies[i]); j++ {
-			testj := test.entityFromIndex(j)
-			if reference.CollisionBetween(keyi.name, testj.name) {
-				cg.adjacencies[i][j] = math.NaN() // represent previously seen collisions as NaNs
-			} else {
-				cg.adjacencies[i][j], err = test.collisionCheckFn()(keyi, testj)
-				if err != nil {
-					return nil, err
-				}
-			}
-		}
-=======
 	}
 	return &ObjectCollisionEntities{entities, indices}, nil
 }
@@ -273,32 +99,10 @@ func (oce *ObjectCollisionEntities) entityFromIndex(index int) *collisionEntity 
 func (oce *ObjectCollisionEntities) indexFromName(name string) (int, error) {
 	if index, ok := oce.indices[name]; ok {
 		return index, nil
->>>>>>> b7db533b0b0c01c54fd46e2b719c0072a6fab7bc
 	}
 	return -1, fmt.Errorf("collision entity %q not found", name)
 }
 
-<<<<<<< HEAD
-// collisionBetween returns a bool describing if the collisionGraph has an edge between the two entities that are specified by name.
-func (cg *collisionGraph) collisionBetween(keyName, testName string) bool {
-	i, iOk := cg.key.indexFromName(keyName)
-	j, jOk := cg.test.indexFromName(testName)
-	if cg.triangular && i > j {
-		i, j = j, i
-	}
-	if iOk == nil && jOk == nil && cg.adjacencies[i][j] >= 0 {
-		return true
-	}
-	return false
-}
-
-// collisions returns a list of all the Collisions as reported by test CollisionEntities' collisionReportFn.
-func (cg *collisionGraph) collisions() []Collision {
-	collisions := make([]Collision, 0)
-	for i := range cg.adjacencies {
-		for _, j := range cg.test.collisionReportFn()(cg.adjacencies[i]) {
-			collisions = append(collisions, Collision{cg.key.entityFromIndex(i).name, cg.test.entityFromIndex(j).name, cg.adjacencies[i][j]})
-=======
 func (oce *ObjectCollisionEntities) checkCollision(key, test *collisionEntity) (float64, error) {
 	distance, err := key.geometry.DistanceFrom(test.geometry)
 	return -distance, err // multiply distance by -1 so that weights of edges are positive
@@ -309,31 +113,11 @@ func (oce *ObjectCollisionEntities) reportCollisions(distances []float64) []int 
 	for i := range distances {
 		if distances[i] >= 0 {
 			collisionIndices = append(collisionIndices, i)
->>>>>>> b7db533b0b0c01c54fd46e2b719c0072a6fab7bc
 		}
 	}
 	return collisionIndices
 }
 
-<<<<<<< HEAD
-// CollisionSystem is an object that checks for and records collisions between CollisionEntities.
-type CollisionSystem struct {
-	graphs []*collisionGraph
-}
-
-// NewCollisionSystemFromReference creates a new collision system that checks for collisions
-// between the entities in the key CollisionEntities and the entities in each of the optional CollisionEntities
-// a reference CollisionSystem can also be specified, and edges between entities that exist in this reference system will
-// not be duplicated in the newly constructed system.
-func NewCollisionSystemFromReference(
-	key *ObjectCollisionEntities,
-	optional []CollisionEntities,
-	reference *CollisionSystem,
-) (*CollisionSystem, error) {
-	var err error
-	cs := &CollisionSystem{make([]*collisionGraph, len(optional)+1)}
-	cs.graphs[0], err = newCollisionGraph(key, key, reference)
-=======
 // spaceCollisionEntities is an implementation of CollisionEntities for entities that do not occupy physical space but
 // represent an area in which other entities should be encompassed by.
 type spaceCollisionEntities struct{ *ObjectCollisionEntities }
@@ -346,7 +130,6 @@ func NewSpaceCollisionEntities(geometries map[string]spatial.Geometry) (Collisio
 
 func (sce spaceCollisionEntities) checkCollision(key, test *collisionEntity) (float64, error) {
 	encompassed, err := key.geometry.EncompassedBy(test.geometry)
->>>>>>> b7db533b0b0c01c54fd46e2b719c0072a6fab7bc
 	if err != nil {
 		return math.NaN(), err
 	}
@@ -354,37 +137,6 @@ func (sce spaceCollisionEntities) checkCollision(key, test *collisionEntity) (fl
 	if !encompassed {
 		return 1, nil
 	}
-<<<<<<< HEAD
-	for i := range optional {
-		cs.graphs[i+1], err = newCollisionGraph(key, optional[i], reference)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return cs, nil
-}
-
-// NewCollisionSystem creates a new collision system that checks for collisions
-// between the entities in the key CollisionEntities and the entities in each of the optional CollisionEntities.
-func NewCollisionSystem(key *ObjectCollisionEntities, optional []CollisionEntities) (*CollisionSystem, error) {
-	return NewCollisionSystemFromReference(key, optional, &CollisionSystem{})
-}
-
-// Collisions returns a list of all the reported collisions in the CollisionSystem.
-func (cs *CollisionSystem) Collisions() []Collision {
-	collisions := make([]Collision, 0)
-	for _, graph := range cs.graphs {
-		collisions = append(collisions, graph.collisions()...)
-	}
-	return collisions
-}
-
-// CollisionBetween returns a bool describing if a collision between the two named entities was reported in the CollisionSystem.
-func (cs *CollisionSystem) CollisionBetween(keyName, testName string) bool {
-	for _, graph := range cs.graphs {
-		if graph.collisionBetween(keyName, testName) {
-			return true
-=======
 	return -1, nil
 }
 
@@ -443,10 +195,9 @@ func newCollisionGraph(key *ObjectCollisionEntities, test CollisionEntities, ref
 					return nil, err
 				}
 			}
->>>>>>> b7db533b0b0c01c54fd46e2b719c0072a6fab7bc
 		}
 	}
-	return false
+	return cg, nil
 }
 
 // collisionBetween returns a bool describing if the collisionGraph has an edge between the two entities that are specified by name.
