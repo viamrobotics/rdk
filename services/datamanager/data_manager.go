53b75304d0a1049f8bdf00976a6f411eeb30e9fc
aaroncasas <aaron@viam.com>
2022-04-27 10:05:23 -0400

    Make tests run faster.

--- services/datamanager/data_manager.go
+++ services/datamanager/data_manager.go
@@ -62,7 +62,7 @@ var Subtype = resource.NewSubtype(
 // Name is the DataManager's typed resource name.
 var Name = resource.NameFromSubtype(Subtype, "")
 
-// The Collector's enqueue should be big enough to ensure that .capture() is never blocked by the enqueue being
+// The Collector's queue should be big enough to ensure that .capture() is never blocked by the queue being
 // written to disk. A default value of 250 was chosen because even with the fastest reasonable capture interval (1ms),
 // this would leave 250ms for a (buffered) disk write before blocking, which seems sufficient for the size of
 // writes this would be performing.
