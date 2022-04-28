6983719d1cf8d39d8144e2c255247f258e172c23
Alexis Wei <qq_wei369@hotmail.com>
2022-04-28 13:05:53 -0400

    why does it error and not match to an interface?

--- ml/inference/inference_test.go
+++ ml/inference/inference_test.go
@@ -2,7 +2,6 @@
 package inference
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/mattn/go-tflite"
@@ -15,37 +14,72 @@ var (
 	interpreter *tflite.Interpreter
 )
 
-func TestCheckDefaultInterpreter(t *testing.T) {
-	loader := GetDefaultInterpreterLoader()
-	test.That(t, reflect.TypeOf(loader.modelLoader) == reflect.TypeOf(tflite.NewModelFromFile), test.ShouldBeTrue)
-	test.That(t, reflect.TypeOf(loader.optionsLoader) == reflect.TypeOf(tflite.NewInterpreterOptions), test.ShouldBeTrue)
-	test.That(t, reflect.TypeOf(loader.interpreterLoader) == reflect.TypeOf(tflite.NewInterpreter), test.ShouldBeTrue)
+type mockInterpreterOptions struct {
+	numThreads int
 }
 
-func TestCheckRightType(t *testing.T) {
-	loader := getMockLoader()
-	interpreter, err := loader.GetTfliteInterpreter("/hello", 4)
+func TestGoodModel(t *testing.T) {
+	goodModelLoader := func(path string) (TfliteModel, error) { return &tflite.Model{}, nil }
+	goodInterpreterLoader := func(model TfliteModel, options mockInterpreterOptions) (TfliteInterpreter, error) {
+		return &tflite.Interpreter{}, nil
+	}
+
+	goodOptions := func() (mockInterpreterOptions, error) { return mockInterpreterOptions{}, nil }
+
+	loader := &InterpreterLoader{
+		NewModelFromFile:      goodModelLoader,
+		NewInterpreter:        goodInterpreterLoader,
+		NewInterpreterOptions: goodOptions,
+	}
+	interpreter, err := loader.GetTfliteInterpreter("random path", 0)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, interpreter, test.ShouldNotBeNil)
-	interpreter.Model.Delete()
-	interpreter.Options.Delete()
-	interpreter.Interpreter.Delete()
+
 }
 
-func TestCanDelete(t *testing.T) {
-	i := getMockInterpreterStruct()
-	err := DeleteInterpreter(i)
-	test.That(t, err, test.ShouldBeNil)
+func (mIo *mockInterpreterOptions) SetNumThreads(num int) {
+	return
 }
 
-func getMockLoader() *InterpreterLoader {
-	loader := &InterpreterLoader{
-		modelLoader:       mockGetModel,
-		optionsLoader:     mockOptions,
-		interpreterLoader: mockInterpreter,
-	}
-	return loader
+func (mIo *mockInterpreterOptions) Delete() {
+	return
 }
+
+func (mIo *mockInterpreterOptions) SetErrorReporter(f func(string, interface{}), user_data interface{}) {
+	return
+}
+
+// func TestCheckDefaultInterpreter(t *testing.T) {
+// 	loader := GetDefaultInterpreterLoader()
+// 	test.That(t, reflect.TypeOf(loader.modelLoader) == reflect.TypeOf(tflite.NewModelFromFile), test.ShouldBeTrue)
+// 	test.That(t, reflect.TypeOf(loader.optionsLoader) == reflect.TypeOf(tflite.NewInterpreterOptions), test.ShouldBeTrue)
+// 	test.That(t, reflect.TypeOf(loader.interpreterLoader) == reflect.TypeOf(tflite.NewInterpreter), test.ShouldBeTrue)
+// }
+
+// func TestCheckRightType(t *testing.T) {
+// 	loader := getMockLoader()
+// 	interpreter, err := loader.GetTfliteInterpreter("/hello", 4)
+// 	test.That(t, err, test.ShouldBeNil)
+// 	test.That(t, interpreter, test.ShouldNotBeNil)
+// 	interpreter.Model.Delete()
+// 	interpreter.Options.Delete()
+// 	interpreter.Interpreter.Delete()
+// }
+
+// func TestCanDelete(t *testing.T) {
+// 	i := getMockInterpreterStruct()
+// 	err := DeleteInterpreter(i)
+// 	test.That(t, err, test.ShouldBeNil)
+// }
+
+// func getMockLoader() *InterpreterLoader {
+// 	loader := &InterpreterLoader{
+// 		modelLoader:       mockGetModel,
+// 		optionsLoader:     mockOptions,
+// 		interpreterLoader: mockInterpreter,
+// 	}
+// 	return loader
+// }
 
 func getMockInterpreterStruct() *FullInterpreter {
 	fullInterpreter := &FullInterpreter{
@@ -56,7 +90,7 @@ func getMockInterpreterStruct() *FullInterpreter {
 	return fullInterpreter
 }
 
-func mockGetModel(path string) *tflite.Model {
+func goodModelLoader(path string) *tflite.Model {
 	model = &tflite.Model{}
 	return model
 }
@@ -64,11 +98,15 @@ func mockGetModel(path string) *tflite.Model {
 func mockOptions() *tflite.InterpreterOptions {
 	return options
 }
-func mockInterpreter(model *tflite.Model, options *tflite.InterpreterOptions) *tflite.Interpreter {
+func mockInterpreter(model *tflite.Model, options mockInterpreterOptions) *tflite.Interpreter {
 	return interpreter
 }
 
-func (option *tflite.InterpreterOptions) SetNumThreads(num int) {
-	//do Nothing
-	return
-}
+// func (option *tflite.InterpreterOptions) SetNumThreads(num int) {
+// 	//do Nothing
+// 	return
+// }
+
+// func (m *mockModel) Delete() {
+// 	return
+// }
