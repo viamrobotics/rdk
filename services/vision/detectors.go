eb1d74ab62f703782f5b7b514e2933871e0ff899
Bijan Haney <bijan@viam.com>
2022-05-02 09:09:56 -0400

    edits

--- services/vision/detectors.go
+++ services/vision/detectors.go
@@ -1,11 +1,39 @@
 package vision
 
 import (
+	"context"
+
+	"github.com/edaniels/golog"
 	"github.com/mitchellh/copystructure"
 	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
 
+	"go.viam.com/rdk/config"
 	objdet "go.viam.com/rdk/vision/objectdetection"
 )
+
+// DetectorType defines what detector types are known.
+type DetectorType string
+
+// The set of allowed detector types.
+const (
+	TFLiteType     = DetectorType("tflite")
+	TensorFlowType = DetectorType("tensorflow")
+	ColorType      = DetectorType("color")
+)
+
+// newDetectorTypeNotImplemented is used when the detector type is not implemented.
+func newDetectorTypeNotImplemented(name string) error {
+	return errors.Errorf("detector type %q is not implemented", name)
+}
+
+// DetectorConfig specifies the name of the detector, the type of detector,
+// and the necessary parameters needed to build the detector.
+type DetectorConfig struct {
+	Name       string              `json:"name"`
+	Type       string              `json:"type"`
+	Parameters config.AttributeMap `json:"parameters"`
+}
 
 // detectorMap stores the registered detectors of the service.
 type detectorMap map[string]objdet.Detector
@@ -48,4 +76,28 @@ func (dm detectorMap) detectorNames() []string {
 		names = append(names, name)
 	}
 	return names
+}
+
+// registerNewDetectors take an attributes struct and parses each element by type to create an RDK Detector
+// and register it to the detector map.
+func registerNewDetectors(ctx context.Context, dm detectorMap, attrs *Attributes, logger golog.Logger) error {
+	_, span := trace.StartSpan(ctx, "service::vision::registerNewDetectors")
+	defer span.End()
+	for _, attr := range attrs.DetectorRegistry {
+		logger.Debugf("adding detector %q of type %s", attr.Name, attr.Type)
+		switch DetectorType(attr.Type) {
+		case TFLiteType:
+			return newDetectorTypeNotImplemented(attr.Type)
+		case TensorFlowType:
+			return newDetectorTypeNotImplemented(attr.Type)
+		case ColorType:
+			err := registerColorDetector(dm, &attr)
+			if err != nil {
+				return err
+			}
+		default:
+			return newDetectorTypeNotImplemented(attr.Type)
+		}
+	}
+	return nil
 }
