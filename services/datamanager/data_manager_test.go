b2aa3a655134c41582c095f9b3bfa44226568168
Alexa Greenberg <alexa.greenberg@viam.com>
2022-05-02 14:59:17 -0400

    Test

--- services/datamanager/data_manager_test.go
+++ services/datamanager/data_manager_test.go
@@ -38,3 +38,33 @@ func TestNewDataManager(t *testing.T) {
 
 	test.That(t, err, test.ShouldBeNil)
 }
+
+func TestCheckStorage(t *testing.T) {
+	cfg := &Config{
+		CaptureDir:        "/path/to/capture",
+		MaxStoragePercent: 80,
+		EnableAutoDelete:  false,
+		ComponentAttributes: map[string]componentAttributes{
+			"imu1": {
+				Type:               "imu",
+				Method:             "ReadAngularVelocity",
+				CaptureFrequencyHz: 10,
+				AdditionalParams: map[string]string{
+					"param1": "thing",
+					"param2": "thing2",
+				},
+			},
+		},
+	}
+	cfgService := config.Service{
+		Type:                "data_manager",
+		ConvertedAttributes: cfg,
+	}
+
+	logger := golog.NewTestLogger(t)
+	r := &inject.Robot{}
+
+	_, err := New(context.Background(), r, cfgService, logger)
+
+	test.That(t, err, test.ShouldBeNil)
+}
