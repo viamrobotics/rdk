d83a0772a76c7a50c7d2ae1ea8c564c63f4d1bd4
Ethan Rodkin <ethanrodkin@protonmail.com>
2022-05-05 15:29:44 -0400

    test reading and writing of cached configs

---
+++ config/caching_test.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/test"
+)
+
+func TestStoreToCache(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+	cfg, err := FromReader(context.Background(), "", strings.NewReader(`{}`), logger)
+
+	test.That(t, err, test.ShouldBeNil)
+
+	cloud := &Cloud{
+		ManagedBy:        "acme",
+		SignalingAddress: "abc",
+		ID:               "forCachingTest",
+		Secret:           "ghi",
+		FQDN:             "fqdn",
+		LocalFQDN:        "localFqdn",
+		TLSCertificate:   "cert",
+		TLSPrivateKey:    "key",
+	}
+	cfg.Cloud = cloud
+
+	// store our config to the cloud
+	err = storeToCache(cfg.Cloud.ID, cfg)
+	test.That(t, err, test.ShouldBeNil)
+
+	// read config from cloud, confirm consistency
+	cloudCfg, _, err := readFromCloud(ctx, cfg.Cloud, true, true, logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, cloudCfg, test.ShouldResemble, cfg)
+
+	// Modify our config
+	newRemote := Remote{Name: "test", Address: "foo", Prefix: true}
+	cfg.Remotes = append(cfg.Remotes, newRemote)
+
+	// read config from cloud again, confirm that the cached config differs from cfg
+	cloudCfg2, _, err := readFromCloud(ctx, cfg.Cloud, true, true, logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, cloudCfg2, test.ShouldNotResemble, cfg)
+
+	// store the updated config to the cloud
+	err = storeToCache(cfg.Cloud.ID, cfg)
+	test.That(t, err, test.ShouldBeNil)
+
+	// read updated cloud config, confirm that it now matches our updated cfg
+	cloudCfg3, _, err := readFromCloud(ctx, cfg.Cloud, true, true, logger)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, cloudCfg3, test.ShouldResemble, cfg)
+}
