ef8d55b2f61314ead64887a49a46bad7a57b8859
Eliot Horowitz <eliothorowitz@gmail.com>
2022-05-09 15:02:12 -0400

    WIP

---
+++ component/base/boat/base_test.go
@@ -0,0 +1,83 @@
+package boat
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"go.viam.com/test"
+)
+
+const testTheta = .01
+
+func TestWeights(t *testing.T) {
+
+	type d struct {
+		cfg motorConfig
+		res motorWeights
+	}
+
+	tests := []d{
+		{
+			motorConfig{
+				LateralOffset: 0,
+				VerticalOffset: -10,
+				AngleDegrees: 0,
+				Weight: 1,
+			},
+			motorWeights{0, 1, 0},
+		},
+		{
+			motorConfig{
+				LateralOffset: 0,
+				VerticalOffset: -10,
+				AngleDegrees: 180,
+				Weight: 1,
+			},
+			motorWeights{0, -1, 0},
+		},
+		{
+			motorConfig{
+				AngleDegrees: 45,
+				Weight: math.Sqrt(2),
+			},
+			motorWeights{1, 1, 0},
+		},
+		{
+			motorConfig{
+				LateralOffset: -10,
+				VerticalOffset: -10,
+				AngleDegrees: 45,
+				Weight: math.Sqrt(2),
+			},
+			motorWeights{1, 1, 0},
+		},
+
+		{
+			motorConfig{
+				AngleDegrees: 1, // this should be almost entirely linearY
+				Weight: 1,
+			},
+			motorWeights{.017, .99, 0 },
+		},
+		{
+			motorConfig{
+				LateralOffset: 0,
+				VerticalOffset: -10,
+				AngleDegrees: 90,
+				Weight: 1,
+			},
+			motorWeights{1, 0, 1},
+		},
+	}
+
+	for _, x := range tests {
+		t.Run(fmt.Sprintf("%#v", x), func(t *testing.T) {
+			w := x.cfg.computeWeights(10)
+			test.That(t, w.angular, test.ShouldAlmostEqual, x.res.angular, testTheta)
+			test.That(t, w.linearX, test.ShouldAlmostEqual, x.res.linearX, testTheta)
+			test.That(t, w.linearY, test.ShouldAlmostEqual, x.res.linearY, testTheta)
+		})
+	}
+
+}
