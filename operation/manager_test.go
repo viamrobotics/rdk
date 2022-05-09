6c114d41a0c91918d6509bab94e1678fd5eca20b
Eliot Horowitz <eliothorowitz@gmail.com>
2022-05-09 18:13:05 -0400

    pr

--- operation/manager_test.go
+++ operation/manager_test.go
@@ -2,6 +2,7 @@ package operation
 
 import (
 	"context"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -15,7 +16,7 @@ func TestSingleOperationManager(t *testing.T) {
 
 	test.That(t, som.NewTimedWaitOp(ctx, time.Millisecond), test.ShouldBeTrue)
 
-	t.Run("sub", func(t *testing.T) {
+	t.Run("nested operation does not cancel parent", func(t *testing.T) {
 		ctx1, close1 := som.New(ctx)
 		defer close1()
 		_, close2 := som.New(ctx1)
@@ -23,9 +24,17 @@ func TestSingleOperationManager(t *testing.T) {
 		test.That(t, ctx1.Err(), test.ShouldBeNil)
 	})
 
-	t.Run("child", func(t *testing.T) {
+	t.Run("cancelling on different context works", func(t *testing.T) {
+		res := int32(0)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
 		go func() {
-			test.That(t, som.NewTimedWaitOp(context.Background(), 10*time.Second), test.ShouldBeFalse)
+			defer wg.Done()
+			x := som.NewTimedWaitOp(context.Background(), 10*time.Second)
+			if x {
+				atomic.StoreInt32(&res, 1)
+			}
 		}()
 
 		for !som.OpRunning() {
@@ -33,6 +42,9 @@ func TestSingleOperationManager(t *testing.T) {
 		}
 
 		test.That(t, som.NewTimedWaitOp(ctx, time.Millisecond), test.ShouldBeTrue)
+
+		wg.Wait()
+		test.That(t, res, test.ShouldEqual, 0)
 	})
 
 	t.Run("WaitForSuccess", func(t *testing.T) {
