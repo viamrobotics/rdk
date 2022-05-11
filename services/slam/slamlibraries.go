4b42d29c7a4df49510f51b5ba5db3cf12df4c343
jeremyhyde-viam <92594844+jeremyhyde-viam@users.noreply.github.com>
2022-05-11 11:52:07 -0400

    linter issues with slamMode and library, required addition of const

--- services/slam/slamlibraries.go
+++ services/slam/slamlibraries.go
@@ -1,6 +1,19 @@
 // Package slam implements simultaneous localization and mapping
 package slam
 
+type (
+	slamLibrary uint8
+	fileType    uint8
+)
+
+const (
+	cartographer = slamLibrary(iota)
+	orbslamv3
+	mono = fileType(iota)
+	rgbd
+	twod
+)
+
 var slamLibraries = map[string]metadata{
 	"cartographer": cartographerMetadata,
 	"orbslamv3":    orbslamv3Metadata,
@@ -9,19 +22,22 @@ var slamLibraries = map[string]metadata{
 // Define currently implemented slam libraries.
 var cartographerMetadata = metadata{
 	AlgoName:       "cartographer",
-	SlamMode:       map[string]bool{"2d": true, "3d": false},
+	AlgoType:       cartographer,
+	SlamMode:       map[string]fileType{"2d": twod},
 	BinaryLocation: "",
 }
 
 var orbslamv3Metadata = metadata{
 	AlgoName:       "orbslamv3",
-	SlamMode:       map[string]bool{"mono": true, "rgbd": true},
+	AlgoType:       orbslamv3,
+	SlamMode:       map[string]fileType{"mono": mono, "rgbd": rgbd},
 	BinaryLocation: "",
 }
 
 // Metadata contains all pertinant information for defining a SLAM library/algorithm including the sparse/dense definition.
 type metadata struct {
 	AlgoName       string
-	SlamMode       map[string]bool
+	AlgoType       slamLibrary
+	SlamMode       map[string]fileType
 	BinaryLocation string
 }
