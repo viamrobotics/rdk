2f59a4d9afb6545e1a4a0c335b074a9059b25c40
Eliot Horowitz <eliothorowitz@gmail.com>
2022-05-11 13:48:19 -0400

    wip

--- component/base/boat/base.go
+++ component/base/boat/base.go
@@ -1,9 +1,11 @@
 package boat
 
 import (
-	"fmt"
+
+	//"fmt".
 	"math"
-	
+
+
 	"github.com/golang/geo/r3"
 
 	"go.viam.com/rdk/utils"
@@ -16,15 +18,15 @@ type motorWeights struct {
 }
 
 type motorConfig struct {
-	Name string 
-	LateralOffset float64
+	Name           string
+	LateralOffset  float64
 	VerticalOffset float64
-	AngleDegrees  float64 // 0 is thrusting forward, 90 is thrusting to starboard, or positive x
-	Weight float64
+	AngleDegrees   float64 // 0 is thrusting forward, 90 is thrusting to starboard, or positive x
+	Weight         float64
 }
 
 // percentDistanceFromCenterOfMass: if the boat is a circle with a radius of 5m,
-// this is the distance from center in m / 5m
+// this is the distance from center in m / 5m.
 func (mc *motorConfig) computeWeights(radius float64) motorWeights {
 	x := math.Sin(utils.DegToRad(mc.AngleDegrees)) * mc.Weight
 	y := math.Cos(utils.DegToRad(mc.AngleDegrees)) * mc.Weight
@@ -35,19 +37,19 @@ func (mc *motorConfig) computeWeights(radius float64) motorWeights {
 			angleFromCenter = 90
 		} else if mc.LateralOffset < 0 {
 			angleFromCenter = -90
-		} 
+		}
 	} else {
 		angleFromCenter = utils.RadToDeg(math.Atan(mc.LateralOffset / mc.VerticalOffset))
 	}
 
 	percentDistanceFromCenterOfMass :=
-		math.Sqrt(mc.LateralOffset*mc.LateralOffset + mc.VerticalOffset*mc.VerticalOffset) /
+		math.Sqrt(mc.LateralOffset*mc.LateralOffset+mc.VerticalOffset*mc.VerticalOffset) /
 			radius
 
 	angleOffset := mc.AngleDegrees - angleFromCenter
-	
-	fmt.Printf("angle: %v angleFromCenter: %v angleOffset: %v percentDistanceFromCenterOfMass: %0.2f, x: %0.2f y: %0.2f\n", mc.AngleDegrees, angleFromCenter, angleOffset, percentDistanceFromCenterOfMass, x, y)
-	
+
+	// fmt.Printf("angle: %v angleFromCenter: %v angleOffset: %v percentDistanceFromCenterOfMass: %0.2f, x: %0.2f y: %0.2f\n", mc.AngleDegrees, angleFromCenter, angleOffset, percentDistanceFromCenterOfMass, x, y)
+
 	return motorWeights{
 		linearX: x,
 		linearY: y,
@@ -56,7 +58,7 @@ func (mc *motorConfig) computeWeights(radius float64) motorWeights {
 }
 
 type boatConfig struct {
-	Motors []motorConfig
+	Motors        []motorConfig
 	Length, Width float64
 }
 
@@ -64,7 +66,7 @@ type boatConfig struct {
 // forwardPercent: -1 -> 1 percent of power in which you want to move laterally
 //                  note only x & y are relevant. y is forward back, x is lateral
 // angularPercent: -1 -> 1 percent of power you want applied to move angularly
-//                 note only z is relevant here 
+//                 note only z is relevant here
 func (bc *boatConfig) computePower(linear, angular r3.Vector) []float64 {
 	panic(1)
 }
