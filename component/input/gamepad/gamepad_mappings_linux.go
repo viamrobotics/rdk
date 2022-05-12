d5914c4157621c6765ec133034a22b39acc2039c
Peter LoVerso <biotinker@gmail.com>
2022-05-12 15:28:19 -0700

    add mapping for s-mode 8bitdo (#826)
    * add mapping for s-mode 8bitdo
    
    * lint

--- component/input/gamepad/gamepad_mappings_linux.go
+++ component/input/gamepad/gamepad_mappings_linux.go
@@ -11,6 +11,32 @@ import (
 
 // GamepadMappings contains all the axes/button translations for each model.
 var GamepadMappings = map[string]Mapping{
+	// 8BitDo Pro 2 Wireless, S-input mode
+	"Pro Controller": {
+		Axes: map[evdev.AbsoluteType]input.Control{
+			0:   input.AbsoluteX,
+			1:   input.AbsoluteY,
+			310: input.AbsoluteZ,
+			3:   input.AbsoluteRX,
+			4:   input.AbsoluteRY,
+			311: input.AbsoluteRZ,
+			16:  input.AbsoluteHat0X,
+			17:  input.AbsoluteHat0Y,
+		},
+		Buttons: map[evdev.KeyType]input.Control{
+			304: input.ButtonSouth,
+			305: input.ButtonEast,
+			306: input.ButtonWest,
+			307: input.ButtonNorth,
+			308: input.ButtonLT,
+			309: input.ButtonRT,
+			312: input.ButtonSelect,
+			313: input.ButtonStart,
+			314: input.ButtonLThumb,
+			315: input.ButtonRThumb,
+			139: input.ButtonMenu,
+		},
+	},
 	// Wireless, X-input mode
 	"8BitDo Pro 2": {
 		Axes: map[evdev.AbsoluteType]input.Control{
