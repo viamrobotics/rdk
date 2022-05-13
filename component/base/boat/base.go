561af4e4beff3070d7d0b7d141a1e8f8e2b6591e
Eliot Horowitz <eliothorowitz@gmail.com>
2022-05-13 15:57:40 -0400

    more boat

--- component/base/boat/base.go
+++ component/base/boat/base.go
@@ -10,6 +10,8 @@ import (
 	"go.uber.org/multierr"
 	"github.com/golang/geo/r3"
 
+	"github.com/go-nlopt/nlopt"
+	
 	"go.viam.com/rdk/component/base"
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/component/motor"
@@ -80,13 +82,62 @@ func (mc *motorConfig) computeWeights(radius float64) motorWeights {
 	return motorWeights{
 		linearX: x,
 		linearY: y,
-		angular: percentDistanceFromCenterOfMass * mc.Weight * math.Sin(utils.DegToRad(angleOffset)),
+		angular: -1 * percentDistanceFromCenterOfMass * mc.Weight * math.Sin(utils.DegToRad(angleOffset)),
 	}
 }
 
 type boatConfig struct {
 	Motors        []motorConfig
 	Length, Width float64
+}
+
+func (bc *boatConfig) maxWeights() motorWeights {
+	var max motorWeights
+	for _, mc := range bc.Motors {
+		w := mc.computeWeights(math.Hypot(bc.Width, bc.Length))
+		max.linearX += math.Abs(w.linearX)
+		max.linearY += math.Abs(w.linearY)
+		max.angular += math.Abs(w.angular)
+	}
+	return max
+}
+
+func (bc *boatConfig) computeGoal(linear, angular r3.Vector) motorWeights {
+	w := bc.maxWeights()
+	w.linearX *= linear.X
+	w.linearY *= linear.Y
+	w.angular *= angular.Z
+	return w
+}
+
+func (bc *boatConfig) applyMotors(powers []float64) motorWeights {
+	if len(powers) != len(bc.Motors) {
+		panic(fmt.Errorf("different number of powers (%d) to motors (%d)", len(powers), len(bc.Motors)))
+	}
+	total := motorWeights{}
+
+	for idx, mc := range bc.Motors {
+		w := mc.computeWeights(math.Hypot(bc.Width, bc.Length))
+		total.linearX += w.linearX * powers[idx]
+		total.linearY += w.linearY * powers[idx]
+		total.angular += w.angular * powers[idx]
+	}
+	
+	return total
+}
+
+func powerLowLevel(desire, weight float64) float64 {
+	if math.Abs(desire) < .05 || math.Abs(weight) < 0.05 {
+		return 0
+	}
+
+	p := desire
+
+	if weight < 0 {
+		p *= -1
+	}
+	
+	return p
 }
 
 // returns an array of power for each motors
@@ -100,19 +151,72 @@ func (bc *boatConfig) computePower(linear, angular r3.Vector) []float64 {
 	for _, mc := range bc.Motors {
 		w := mc.computeWeights(math.Hypot(bc.Width, bc.Length))
 		p := 0.0
-		if linear.Y > 0 && w.linearY > 0 {
-			p = math.Max(linear.Y, w.linearY)
-		} else if linear.Y < 0 && w.linearY < 0 {
-			p = math.Min(linear.Y, w.linearY)
-		} else if angular.Z > 0 && w.angular > 0 {
-			p = math.Max(angular.Z, w.angular)
-		} else if angular.Z < 0 && w.angular < 0 {
-			p = math.Min(angular.Z, w.angular)
-		}
+
+		p += powerLowLevel(linear.X, w.linearX)
+		p += powerLowLevel(linear.Y, w.linearY)
+		p += powerLowLevel(angular.Z, w.angular)
+
 		fmt.Printf("\t w: %#v power: %v\n", w, p)
 		powers = append(powers, p)
 	}
+
+	fmt.Printf("powers: %v\n", powers)
+	
+	if false {
+		goal := bc.computeGoal(linear, angular)
+		
+		opt, err := nlopt.NewNLopt(nlopt.LD_MMA, 2)
+		if err != nil {
+			panic(err)
+		}
+		defer opt.Destroy()
+		
+		mins := []float64{}
+		maxs := []float64{}
+		
+		for _, _ = range bc.Motors {
+			mins = append(mins, -1)
+			maxs = append(maxs, -1)
+		}
+		
+		err = multierr.Combine(
+			opt.SetLowerBounds(mins),
+			opt.SetUpperBounds(maxs),
+			opt.SetXtolAbs1(.01),
+			opt.SetXtolRel(.01),
+		)
+		if err != nil {
+			panic(1)
+		}
+		
+		var evals int
+		myfunc := func(x, gradient []float64) float64 {
+			fmt.Printf("yo: %v\n", x)
+			evals++
+			
+			total := bc.applyMotors(x)
+			diff := math.Pow(total.linearX - goal.linearX,2) +
+				math.Pow(total.linearY - goal.linearY,2) +
+				math.Pow(total.angular - goal.angular,2)
+			diff = math.Sqrt(diff)
+			
+			if len(gradient) > 0 {
+				
+			}
+			
+			return diff
+		}
+		
+		
+		opt.SetMinObjective(myfunc)
+		powers, _, err = opt.Optimize(powers)
+		if err != nil {
+			panic(err)
+		}
+	}
+	
 	return powers
+
 }
 
 func createBoat(ctx context.Context, r robot.Robot, config *boatConfig, logger golog.Logger) (base.LocalBase, error) {
--- component/base/boat/base_test.go
+++ component/base/boat/base_test.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"testing"
 
+	"github.com/golang/geo/r3"
+	
 	"go.viam.com/test"
 )
 
@@ -67,7 +69,7 @@ func TestWeights(t *testing.T) {
 				AngleDegrees: 90,
 				Weight: 1,
 			},
-			motorWeights{1, 0, 1},
+			motorWeights{1, 0, -1},
 		},
 	}
 
@@ -79,5 +81,61 @@ func TestWeights(t *testing.T) {
 			test.That(t, w.linearY, test.ShouldAlmostEqual, x.res.linearY, testTheta)
 		})
 	}
+
+}
+
+func TestBoatConfig(t *testing.T) {
+	cfg := boatConfig{
+		Motors: []motorConfig{
+			{ Name : "starboard-rotation", LateralOffset: 0.3, VerticalOffset: 0, AngleDegrees: 0, Weight: 1},
+			{ Name : "port-rotation", LateralOffset: -0.3, VerticalOffset: 0, AngleDegrees: 0, Weight: 1},
+			{ Name : "forward", LateralOffset: 0, VerticalOffset: -0.3, AngleDegrees: 0, Weight: 1},
+			{ Name : "reverse", LateralOffset: 0, VerticalOffset: 0.3, AngleDegrees: 180, Weight: 1},
+			{ Name : "starboard-lateral", LateralOffset: 0.45, VerticalOffset: 0, AngleDegrees: 90, Weight: 1},
+			{ Name : "port-lateral", LateralOffset: -0.45, VerticalOffset: 0, AngleDegrees: -90, Weight: 1},
+		},
+		Length: .5,
+		Width: .5,
+	}
+
+	max := cfg.maxWeights()
+	test.That(t, max.linearY, test.ShouldAlmostEqual, 4, testTheta)
+	test.That(t, max.linearX, test.ShouldAlmostEqual, 2, testTheta)
+	test.That(t, max.angular, test.ShouldAlmostEqual, .845, testTheta) // TODO(erh): is this right?
+
+	g := cfg.computeGoal( r3.Vector{0,1,0,}, r3.Vector{})
+	test.That(t, g.linearY, test.ShouldAlmostEqual, 4)
+
+	powers := cfg.computePower(r3.Vector{0,1,0,}, r3.Vector{})
+	test.That(t, powers[0], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[1], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[2], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[3], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[4], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[5], test.ShouldAlmostEqual, 0, testTheta)
+
+	powers = cfg.computePower(r3.Vector{0,-1,0,}, r3.Vector{})
+	test.That(t, powers[0], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[1], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[2], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[3], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[4], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[5], test.ShouldAlmostEqual, 0, testTheta)
+
+	powers = cfg.computePower(r3.Vector{0,0,0,}, r3.Vector{Z: 1})
+	test.That(t, powers[0], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[1], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[2], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[3], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[4], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[5], test.ShouldAlmostEqual, 0, testTheta)
+
+	powers = cfg.computePower(r3.Vector{0,0,0,}, r3.Vector{Z: -1})
+	test.That(t, powers[0], test.ShouldAlmostEqual, -1, testTheta)
+	test.That(t, powers[1], test.ShouldAlmostEqual, 1, testTheta)
+	test.That(t, powers[2], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[3], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[4], test.ShouldAlmostEqual, 0, testTheta)
+	test.That(t, powers[5], test.ShouldAlmostEqual, 0, testTheta)
 
 }
