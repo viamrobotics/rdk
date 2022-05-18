e4323d85c8c61111dd712dae2dec74f8e26ed5c5
sapozhkoved <ceo@wall.black>
2022-05-18 23:01:31 +0300

    fix lint

--- web/frontend/dls/src/components/KeyboardInput.vue
+++ web/frontend/dls/src/components/KeyboardInput.vue
@@ -11,13 +11,13 @@
         color="primary"
         group
         variant="primary"
-        @pointerdown="setKeyPressed(key,true)"
-        @pointerup="setKeyPressed(key,false)"
+        @pointerdown="setKeyPressed(key, true)"
+        @pointerup="setKeyPressed(key, false)"
         class="w-15 h-10 m-1 bg-white dark:bg-gray-900 border-gray-500"
         :class="{
-          'bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 keyboard-button-pressed': pressedKeys[key],
-          'keyboard-button-not-pressed': !pressedKeys[key]
-
+          'bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 keyboard-button-pressed':
+            pressedKeys[key],
+          'keyboard-button-not-pressed': !pressedKeys[key],
         }"
       >
         <template v-slot:icon>
@@ -120,10 +120,10 @@ export default class KeyboardInput extends Vue {
 }
 </script>
 <style>
-  .keyboard-button-not-pressed:focus {
-    background-color: white;
-  }
-  .keyboard-button-pressed:focus {
-    background-color: rgba(228, 228, 231, 1);
-  }
+.keyboard-button-not-pressed:focus {
+  background-color: white;
+}
+.keyboard-button-pressed:focus {
+  background-color: rgba(228, 228, 231, 1);
+}
 </style>
