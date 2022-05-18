cfbc04a1af4bb40f865a21cbbe6292bcdd18213b
Maxim Pertsov <maxim@viam.com>
2022-05-18 12:08:11 -0400

    lint

--- services/discovery/client_test.go
+++ services/discovery/client_test.go
@@ -31,14 +31,6 @@ import (
 	"go.viam.com/rdk/testutils/inject"
 )
 
-func newServer(sMap map[resource.Name]interface{}) (pb.DiscoveryServiceServer, error) {
-	sSvc, err := subtype.New(sMap)
-	if err != nil {
-		return nil, err
-	}
-	return discovery.NewServer(sSvc), nil
-}
-
 func TestClient(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	listener1, err := net.Listen("tcp", "localhost:0")
