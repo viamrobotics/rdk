da49bed953e0f61b35542eae925964d7cd484a2f
Khari Jarrett <kharijarrett@Kharis-MacBook-Air.local>
2022-05-31 10:26:54 -0400

    adding tflite detector

---
+++ services/vision/tflite_registration.go
@@ -0,0 +1,19 @@
+package vision
+
+import (
+	"github.com/edaniels/golog"
+	"github.com/pkg/errors"
+)
+
+func registerTfliteDetector(dm detectorMap, conf *DetectorConfig, logger golog.Logger) error {
+	if conf == nil {
+		return errors.New("object detection config for tflite detector cannot be nil")
+	}
+	detector, err := NewTfliteDetector(conf)
+	if err != nil {
+		return errors.Wrapf(err, "could not register tflite detector %s", conf.Name)
+	}
+
+	return dm.registerDetector(conf.Name, detector, logger)
+
+}
