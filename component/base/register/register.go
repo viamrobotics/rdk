b31a140d92221bdc5a2a37fd0517ef771ef67140
Matt Vella <mcvella@gmail.com>
2022-06-03 15:20:52 -0400

    fix bad merge

--- component/base/register/register.go
+++ component/base/register/register.go
@@ -4,10 +4,7 @@ package register
 import (
 
 	// register bases.
-<<<<<<< HEAD
 	_ "go.viam.com/rdk/component/base/agilex"
-=======
->>>>>>> 4c30371baa74e84d8abd0a393178d28c77ea68e6
 	_ "go.viam.com/rdk/component/base/boat"
 	_ "go.viam.com/rdk/component/base/fake"
 	_ "go.viam.com/rdk/component/base/wheeled"
