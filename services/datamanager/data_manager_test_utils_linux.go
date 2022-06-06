71630d4929a1a70ba89ae28396a5c9905e3b33b7
Alexa Greenberg <alexa.greenberg@viam.com>
2022-06-06 11:45:52 -0400

    Simplify linux/nonlinux test

---
+++ services/datamanager/data_manager_test_utils_linux.go
@@ -0,0 +1,14 @@
+//go:build linux
+// +build linux
+
+package datamanager
+
+func getMockStatfsFn(blocks int, bsize int, bavail int, bfree int) func(string, *syscall.Statfs_t) error {
+	return func(path string, stat *syscall.Statfs_t) error {
+		stat.Blocks = uint64(blocks)
+		stat.Bsize = int64(bsize)
+		stat.Bavail = uint64(bavail)
+		stat.Bfree = uint64(bfree)
+		return nil
+	}
+}
