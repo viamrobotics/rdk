2ef17d5551fa4d2996a2076e26822be50a06ac92
Alexis Wei <qq_wei369@hotmail.com>
2022-06-07 11:17:37 -0400

    small updates with Load tests

--- ml/inference/inference.go
+++ ml/inference/inference.go
@@ -15,8 +15,14 @@ const (
 )
 
 type MLModel interface {
+	// Infer takes an already ordered input tensor as an array,
+	// and makes an inference on the model, returning an output tensor map
 	Infer(inputTensor interface{}) (config.AttributeMap, error)
-	GetInfo() (interface{}, error)
+
+	// GetMetadata gets the entire model metadata structure from file
 	GetMetadata() (interface{}, error)
+
+	// Close closes the model and interpreter that allows inferences to be made, opens up space in memory.
+	// All models must be closed when done using
 	Close()
 }
