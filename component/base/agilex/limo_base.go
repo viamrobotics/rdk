226386066058233cc5c69efaf1af24e8c8f9d45f
Matt Vella <mcvella@gmail.com>
2022-06-08 00:27:40 -0400

    added velocity loop, but now serial commands are not being respected

--- component/base/agilex/limo_base.go
+++ component/base/agilex/limo_base.go
@@ -4,7 +4,6 @@ package limo
 import (
 	"context"
 	"io"
-	"math"
 	"strings"
 	"sync"
 	"time"
@@ -13,6 +12,7 @@ import (
 	"github.com/golang/geo/r3"
 	"github.com/jacobsa/go-serial/serial"
 	"github.com/pkg/errors"
+	utils "go.viam.com/utils"
 
 	"go.viam.com/rdk/component/base"
 	"go.viam.com/rdk/component/generic"
@@ -65,11 +65,19 @@ type limoFrame struct {
 	count uint8
 }
 
+type limoState struct {
+	velocityThreadStarted                   bool
+	velocityLinearGoal, velocityAngularGoal r3.Vector
+}
 type limoBase struct {
 	generic.Unimplemented
 	driveMode  string
 	controller *controller
+	state      limoState
+	stateMutex sync.Mutex
 	opMgr      operation.SingleOperationManager
+	cancel     context.CancelFunc
+	waitGroup  sync.WaitGroup
 }
 
 // Config is how you configure a limo base.
@@ -93,8 +101,8 @@ func CreateLimoBase(ctx context.Context, r robot.Robot, config *Config, logger g
 	}
 
 	globalMu.Lock()
-	ctrl, ok := controllers[config.SerialDevice]
-	if !ok {
+	ctrl, controllerExists := controllers[config.SerialDevice]
+	if !controllerExists {
 		logger.Debug("creating controller")
 		newCtrl, err := newController(config, logger)
 		if err != nil {
@@ -102,35 +110,45 @@ func CreateLimoBase(ctx context.Context, r robot.Robot, config *Config, logger g
 		}
 		controllers[config.SerialDevice] = newCtrl
 		ctrl = newCtrl
+
+		// enable commanded mode
+		frame := new(limoFrame)
+		frame.id = 0x421
+		frame.data = make([]uint8, 8)
+		frame.data[0] = 0x01
+		frame.data[1] = 0
+		frame.data[2] = 0
+		frame.data[3] = 0
+		frame.data[4] = 0
+		frame.data[5] = 0
+		frame.data[6] = 0
+		frame.data[7] = 0
+
+		logger.Debug("Will send init frame")
+		err = ctrl.sendFrame(frame)
+		if err != nil && !strings.HasPrefix(err.Error(), "error enabling commanded mode") {
+			return nil, err
+		}
 	}
 	globalMu.Unlock()
-
-	ctrl.mu.Lock()
-	defer ctrl.mu.Unlock()
 
 	base := &limoBase{
 		driveMode:  config.DriveMode,
 		controller: ctrl,
 	}
 
-	// enable commanded mode
-	frame := new(limoFrame)
-	frame.id = 0x421
-	frame.data = make([]uint8, 8)
-	frame.data[0] = 0x01
-	frame.data[1] = 0
-	frame.data[2] = 0
-	frame.data[3] = 0
-	frame.data[4] = 0
-	frame.data[5] = 0
-	frame.data[6] = 0
-	frame.data[7] = 0
-
-	logger.Debug("Will send init frame")
-	err := ctrl.sendFrame(frame)
-	if err != nil && !strings.HasPrefix(err.Error(), "error enabling commanded mode") {
-		return nil, err
+	base.stateMutex.Lock()
+	if !base.state.velocityThreadStarted {
+		// nolint:contextcheck
+		err := base.startVelocityThread()
+		if err != nil {
+			return base, err
+		}
+		base.state.velocityThreadStarted = true
 	}
+	base.stateMutex.Unlock()
+
+	logger.Debug("Base initialized")
 
 	return base, nil
 }
@@ -164,6 +182,38 @@ func newController(c *Config, logger golog.Logger) (*controller, error) {
 	return ctrl, nil
 }
 
+func (b *limoBase) startVelocityThread() error {
+	var ctx context.Context
+	ctx, b.cancel = context.WithCancel(context.Background())
+	b.controller.logger.Debug("Starting velocity thread")
+
+	b.waitGroup.Add(1)
+	go func() {
+		defer b.waitGroup.Done()
+
+		for {
+			utils.SelectContextOrWait(ctx, time.Duration(float64(time.Millisecond)*100))
+			err := b.velocityThreadLoop(ctx)
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					return
+				}
+				b.controller.logger.Warn(err)
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (base *limoBase) velocityThreadLoop(ctx context.Context) error {
+	err := base.setMotionCommand(ctx, base.state.velocityLinearGoal.Y, base.state.velocityAngularGoal.Z, 0, 0)
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+	return err
+}
+
 // Must be run inside a lock.
 func (c *controller) sendFrame(frame *limoFrame) error {
 	var checksum uint32 = 0
@@ -186,27 +236,20 @@ func (c *controller) sendFrame(frame *limoFrame) error {
 		n, err := c.port.Write(data)
 		if err != nil {
 			return err
-		} else {
-			c.logger.Debugf("Sent %d bytes %v", n, data)
 		}
+		c.logger.Debugf("Sent %d bytes %v", n, data)
 	}
 
 	return nil
 }
 
-func (base *limoBase) setMotionCommand(linear_vel float64, angular_vel float64, lateral_velocity float64, steering_angle float64) error {
-	base.controller.mu.Lock()
-	defer base.controller.mu.Unlock()
-
-	base.controller.logger.Debugf("Will set motion command %f %f %f %f", linear_vel, angular_vel, lateral_velocity, steering_angle)
-
-	// enable commanded mode
+func (base *limoBase) setMotionCommand(ctx context.Context, linear_vel float64, angular_vel float64, lateral_velocity float64, steering_angle float64) error {
 	frame := new(limoFrame)
 	frame.id = 0x111
-	linear_cmd := int16(linear_vel * 1000)
-	angular_cmd := int16(angular_vel * 1000)
-	lateral_cmd := int16(lateral_velocity * 1000)
-	steering_cmd := int16(steering_angle * 1000)
+	linear_cmd := int16(linear_vel)
+	angular_cmd := int16(angular_vel)
+	lateral_cmd := int16(lateral_velocity)
+	steering_cmd := int16(steering_angle)
 
 	frame.data = make([]uint8, 8)
 	frame.data[0] = uint8(linear_cmd >> 8)
@@ -217,63 +260,75 @@ func (base *limoBase) setMotionCommand(linear_vel float64, angular_vel float64, 
 	frame.data[5] = uint8(lateral_cmd & 0x00ff)
 	frame.data[6] = uint8(steering_cmd >> 8)
 	frame.data[7] = uint8(steering_cmd & 0x00ff)
+
+	base.controller.mu.Lock()
 	err := base.controller.sendFrame(frame)
 	if err != nil {
 		base.controller.logger.Error(err)
 	}
+	base.controller.mu.Unlock()
+
 	return err
 }
 
 func (base *limoBase) Spin(ctx context.Context, angleDeg float64, degsPerSec float64) error {
-	ctx, done := base.opMgr.New(ctx)
-	defer done()
-
-	// Stop the motors if the speed is 0
-	if math.Abs(degsPerSec) < 0.0001 {
-		err := base.Stop(ctx)
-		if err != nil {
-			return errors.Errorf("error when trying to spin at a speed of 0: %v", err)
-		}
+	base.controller.logger.Debugf("Spin(%f, %f)", angleDeg, degsPerSec)
+	millis := 1000 * (angleDeg / degsPerSec)
+	err := base.SetVelocity(ctx, r3.Vector{}, r3.Vector{Z: -1 * degsPerSec})
+	if err != nil {
 		return err
 	}
-
-	return nil
+	utils.SelectContextOrWait(ctx, time.Duration(float64(time.Millisecond)*millis))
+	return base.Stop(ctx)
 }
 
 func (base *limoBase) MoveStraight(ctx context.Context, distanceMm int, mmPerSec float64) error {
-	ctx, done := base.opMgr.New(ctx)
-	defer done()
+	base.controller.logger.Debugf("MoveStraight(%d, %f)", distanceMm, mmPerSec)
+	base.SetVelocity(ctx, r3.Vector{Y: mmPerSec}, r3.Vector{})
 
-	// stop motors after calculated distance
-	timeInSecs := float64(distanceMm) / mmPerSec
-	timer := time.AfterFunc(time.Second*time.Duration(timeInSecs), func() {
-		base.setMotionCommand(0, 0, 0, 0)
-	})
-	defer timer.Stop()
-
-	return base.setMotionCommand(mmPerSec, 0, 0, 0)
+	// stop base after calculated distance
+	timeInSecs := time.Second * time.Duration(float64(distanceMm)/mmPerSec)
+	utils.SelectContextOrWait(ctx, timeInSecs)
+	return base.Stop(ctx)
 }
 
 func (base *limoBase) SetVelocity(ctx context.Context, linear, angular r3.Vector) error {
-	base.opMgr.CancelRunning(ctx)
-	return base.setMotionCommand(linear.Y, angular.Z, 0, 0)
+	base.controller.logger.Debugf("Will set linear velocity %f angular velocity %f", linear, angular)
+
+	_, done := base.opMgr.New(ctx)
+	defer done()
+
+	base.stateMutex.Lock()
+
+	base.state.velocityLinearGoal = linear
+	base.state.velocityAngularGoal = angular
+	base.stateMutex.Unlock()
+
+	return nil
 }
 
 func (base *limoBase) SetPower(ctx context.Context, linear, angular r3.Vector) error {
 	return nil
 }
 
-func (base *limoBase) WaitForMotorsToStop(ctx context.Context) error {
-	return base.Stop(ctx)
-}
-
 func (base *limoBase) Stop(ctx context.Context) error {
-	err := base.setMotionCommand(0, 0, 0, 0)
-	return err
+	base.controller.logger.Debug("Stop()")
+	base.SetVelocity(ctx, r3.Vector{}, r3.Vector{})
+	base.opMgr.CancelRunning(ctx)
+	return nil
 }
 
 func (base *limoBase) Close(ctx context.Context) error {
-	return base.Stop(ctx)
+	base.controller.logger.Debug("Close()")
+	base.Stop(ctx)
+	if base.cancel != nil {
+		base.controller.logger.Debug("calling cancel()")
+		base.cancel()
+		base.cancel = nil
+		base.waitGroup.Wait()
+		base.controller.logger.Debug("done waiting on cancel")
+	}
+	return nil
 }
 
 func (base *limoBase) GetWidth(ctx context.Context) (int, error) {
