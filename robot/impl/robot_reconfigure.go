3b8c501cdf20e6fc9635af3d7b8479772ed42ab5
Alexa Greenberg <alexa.greenberg@viam.com>
2022-06-08 17:09:29 -0400

    RSDK-393 Add way to figure out if a resource is remote (#896)
    Tracks if a resource is remote and if so, the remote name after robot is created and when robot is reconfigured.
    Have GetStatus pull from that so that we can reduce number of grpc calls.

--- robot/impl/robot_reconfigure.go
+++ robot/impl/robot_reconfigure.go
@@ -32,7 +32,7 @@ func (r *localRobot) Reconfigure(ctx context.Context, newConfig *config.Config) 
 	if err := r.updateDefaultServices(ctx); err != nil {
 		return err
 	}
-
+	r.manager.updateResourceRemoteNames()
 	return nil
 }
 
