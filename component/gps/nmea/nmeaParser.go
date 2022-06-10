b6d0d4f03eaa3f415459346d2458566e4d84281d
Evelyn <evelyn.fu@viam.com>
2022-06-10 18:46:06 +0100

    remove rtk from serial

--- component/gps/nmea/nmeaParser.go
+++ component/gps/nmea/nmeaParser.go
@@ -23,7 +23,7 @@ type gpsData struct {
 
 // parseAndUpdate will attempt to parse a line to an NMEA sentence, and if valid, will try to update the given struct
 // with the values for that line. Nothing will be updated if there is not a valid gps fix.
-func (g *gpsData) parseAndUpdate(line string, n ntripInfo) error {
+func (g *gpsData) parseAndUpdate(line string) error {
 	s, err := nmea.Parse(line)
 	if err != nil {
 		return err
@@ -73,13 +73,6 @@ func (g *gpsData) parseAndUpdate(line string, n ntripInfo) error {
 			g.hDOP = gga.HDOP
 			g.alt = gga.Altitude
 			// Send gga to ntrip server if using server
-
-			if n.sendNMEA {
-				_, err := n.nmeaW.Write([]byte(line))
-				if err != nil {
-					return err
-				}
-			}
 		}
 	} else if gll, ok := s.(nmea.GLL); ok {
 		// GLL provides just lat/lon
