05323ecb9ba59ae644e3c99b1ef20c65107753a7
Khari Jarrett <kharijarrett@Kharis-MacBook-Air.local>
2022-06-15 14:06:56 -0400

    the detector worksgit status

--- services/vision/cmd/main.go
+++ services/vision/cmd/main.go
@@ -11,20 +11,8 @@ import (
 )
 
 func main() {
-
 	logger := golog.NewLogger("test")
 
-	/*
-		Make a DetectorConfig with the params =
-		type TfliteDetectorConfig struct {
-		//this should come from the attributes part of the detector config
-		ModelPath  string  `json:"model_path"`
-		NumThreads *int    `json:"num_threads"`
-		LabelPath  *string `json:"label_path"`
-		ServiceURL *string `json:"service_url"`
-		}
-
-	*/
 
 	modelLoc := "data/effdet0.tflite"
 	picLoc := "data/dogscute.jpeg"
@@ -38,23 +26,18 @@ func main() {
 	cfg := vision.DetectorConfig{Name: "testdetector", Type: "tflite", Parameters: attrs}
 	detector, err := vision.NewTfliteDetector(&cfg, logger)
 	if err != nil {
-		fmt.Println("error1")
 		fmt.Println(err)
 	}
 
 	img, err := rimage.NewImageFromFile(picLoc)
 	if err != nil {
-		fmt.Println("error2")
 		fmt.Println(err)
-	} // the image is totally working...
+	} 
 
 	detections, err := detector(img)
 	if err != nil {
-		fmt.Println("error3")
 		fmt.Println(err)
 	}
-
-	//fmt.Println(detections)
 
 	for i := 0; i < 5; i++ {
 		fmt.Println(detections[i])
