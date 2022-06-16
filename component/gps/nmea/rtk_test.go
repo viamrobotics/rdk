638ca1aa8b681152da217aea3fdd04d55a9bad53
Evelyn <evelyn.fu@viam.com>
2022-06-16 17:42:49 +0100

    code cleanup

--- component/gps/nmea/rtk_test.go
+++ component/gps/nmea/rtk_test.go
@@ -1,31 +1,30 @@
-
 package nmea
 
 import (
+	"context"
 	"testing"
 
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
 )
 
-func TestConnect(t *testing.T) { 
+func TestConnect(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	var g  = RTKGPS{logger: logger}
+	ctx := context.Background()
+	cancelCtx, cancelFunc := context.WithCancel(ctx)
+	g := RTKGPS{cancelCtx: cancelCtx, cancelFunc: cancelFunc, logger: logger}
 	url := "http://rtn.dot.ny.gov:8082"
 	username := "evelyn"
 	password := "checkmate"
 	mountPoint := "NJI2"
-	
-	//create new ntrip client and connect
-	_, err := g.Connect(url, username, password, 10)
+
+	// create new ntrip client and connect
+	err := g.Connect(url, username, password, 10)
 	test.That(t, err, test.ShouldBeNil)
 
-	//failed to get stream
-	_, err = g.GetStream("", 10)
+	err = g.GetStream("", 10)
 	test.That(t, err, test.ShouldNotBeNil)
 
-	//successful get stream
-	_, err = g.GetStream(mountPoint,10)
+	err = g.GetStream(mountPoint, 10)
 	test.That(t, err, test.ShouldBeNil)
-
 }
