87f12b3e02189d75ab6a48380c3ca4d63273ac40
Marco Nocito <mnocito@mit.edu>
2022-06-17 17:05:38 -0400

    fix CI error

--- services/datamanager/export_test.go
+++ services/datamanager/export_test.go
@@ -3,10 +3,21 @@ package datamanager
 
 import (
 	"context"
+
+	"go.viam.com/rdk/config"
 )
 
 func (svc *dataManagerService) SetUploadFn(fn func(ctx context.Context, path string) error) {
 	svc.uploadFn = fn
 }
 
-var GetDataManagerServiceConfig = getDataManagerServiceConfig
+// Get the config associated with the data manager service.
+func GetDataManagerServiceConfig(cfg *config.Config) (*config.Service, bool) {
+	for _, c := range cfg.Services {
+		// Compare service type and name.
+		if c.ResourceName() == Name {
+			return &c, true
+		}
+	}
+	return &config.Service{}, false
+}
