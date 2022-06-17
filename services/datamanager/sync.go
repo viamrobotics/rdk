16d6aaba979821e33edf98a6f5bf06260cc22a76
aaroncasas <aaron@viam.com>
2022-06-16 22:51:06 -0400

    Refactor away queue.

--- services/datamanager/sync.go
+++ services/datamanager/sync.go
@@ -2,10 +2,7 @@ package datamanager
 
 import (
 	"context"
-	"fmt"
 	"io/fs"
-	"os"
-	"path"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -25,14 +22,14 @@ var (
 // syncManager is responsible for uploading files to the cloud every syncInterval.
 type syncManager interface {
 	Start()
-	Enqueue(filesToQueue []string) error
+	Sync(paths []string)
 	Close()
 }
 
 // syncer is responsible for enqueuing files in captureDir and uploading them to the cloud.
 type syncer struct {
 	captureDir        string
-	syncQueue         string
+	intervalMillis    time.Duration
 	logger            golog.Logger
 	queueWaitTime     time.Duration
 	progressTracker   progressTracker
@@ -43,11 +40,10 @@ type syncer struct {
 }
 
 // newSyncer returns a new syncer.
-func newSyncer(queuePath string, logger golog.Logger, captureDir string) *syncer {
+func newSyncer(logger golog.Logger, captureDir string) *syncer {
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
 
 	ret := syncer{
-		syncQueue:     queuePath,
 		logger:        logger,
 		captureDir:    captureDir,
 		queueWaitTime: time.Minute,
@@ -66,90 +62,17 @@ func newSyncer(queuePath string, logger golog.Logger, captureDir string) *syncer
 	return &ret
 }
 
-// Enqueue moves files that are no longer being written to from captureDir to SyncQueuePath.
-func (s *syncer) Enqueue(filesToQueue []string) error {
-	for _, filePath := range filesToQueue {
-		subPath, err := s.getPathUnderCaptureDir(filePath)
-		if err != nil {
-			return errors.Errorf("could not get path under capture directory: %v", err)
-		}
-
-		if err := os.MkdirAll(filepath.Dir(path.Join(s.syncQueue, subPath)), 0o700); err != nil {
-			return errors.Errorf("failed create directories under sync enqueue: %v", err)
-		}
-
-		if err := os.Rename(filePath, path.Join(s.syncQueue, subPath)); err != nil {
-			return errors.Errorf("failed to move file to sync enqueue: %v", err)
-		}
-	}
-	return nil
-}
-
 // Start queues any files already in captureDir that haven't been modified in s.queueWaitTime time, and kicks off a
 // goroutine to constantly upload files in the queue.
 func (s *syncer) Start() {
-	// First, move any files in captureDir to queue.
-	if err := filepath.WalkDir(s.captureDir, s.queueFile); err != nil {
-		s.logger.Errorf("failed to move files to sync queue: %v", err)
-	}
-
 	s.backgroundWorkers.Add(1)
 	goutils.PanicCapturingGo(func() {
-		ticker := time.NewTicker(time.Millisecond * 500)
-		defer ticker.Stop()
 		defer s.backgroundWorkers.Done()
-		for {
-			if err := s.cancelCtx.Err(); err != nil {
-				if !errors.Is(err, context.Canceled) {
-					s.logger.Errorw("sync context closed unexpectedly", "error", err)
-				}
-				return
-			}
-			select {
-			case <-s.cancelCtx.Done():
-				return
-			case <-ticker.C:
-				if err := filepath.WalkDir(s.syncQueue, s.upload); err != nil {
-					s.logger.Errorf("failed to upload queued file: %v", err)
-				}
-			}
+		if err := filepath.WalkDir(s.captureDir, s.uploadWalkDirFunc); err != nil {
+			s.logger.Errorf("failed to upload queued file: %v", err)
 		}
 	})
-}
-
-// queueFile is an fs.WalkDirFunc that moves matching files to s.syncQueue.
-func (s *syncer) queueFile(filePath string, di fs.DirEntry, err error) error {
-	if err != nil {
-		return err
-	}
-
-	if di.IsDir() {
-		return nil
-	}
-
-	fileInfo, err := di.Info()
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("failed to get file info for filepath %s", filePath))
-	}
-
-	// If it's been written to in the last s.queueWaitTime, it's still active and shouldn't be queued.
-	if time.Since(fileInfo.ModTime()) < s.queueWaitTime {
-		return nil
-	}
-
-	subPath, err := s.getPathUnderCaptureDir(filePath)
-	if err != nil {
-		return errors.Wrap(err, "could not get path under capture directory")
-	}
-
-	if err = os.MkdirAll(filepath.Dir(path.Join(s.syncQueue, subPath)), 0o700); err != nil {
-		return errors.Wrap(err, "failed create directories under sync enqueue")
-	}
-
-	if err := os.Rename(filePath, path.Join(s.syncQueue, subPath)); err != nil {
-		return errors.Wrap(err, "failed to move file to sync enqueue")
-	}
-	return nil
+	return
 }
 
 func (s *syncer) getPathUnderCaptureDir(filePath string) (string, error) {
@@ -166,7 +89,7 @@ func (s *syncer) Close() {
 }
 
 // upload is an fs.WalkDirFunc that uploads files to Viam cloud storage.
-func (s *syncer) upload(path string, di fs.DirEntry, err error) error {
+func (s *syncer) uploadWalkDirFunc(path string, di fs.DirEntry, err error) error {
 	if err != nil {
 		s.logger.Errorw("failed to upload queued file", "error", err)
 
@@ -177,8 +100,13 @@ func (s *syncer) upload(path string, di fs.DirEntry, err error) error {
 		return nil
 	}
 
+	s.upload(s.cancelCtx, path)
+	return nil
+}
+
+func (s *syncer) upload(ctx context.Context, path string) {
 	if s.progressTracker.inProgress(path) {
-		return nil
+		return
 	}
 
 	s.progressTracker.mark(path)
@@ -186,13 +114,21 @@ func (s *syncer) upload(path string, di fs.DirEntry, err error) error {
 	goutils.PanicCapturingGo(func() {
 		defer s.backgroundWorkers.Done()
 		exponentialRetry(
-			s.cancelCtx,
+			ctx,
 			func(ctx context.Context) error { return s.uploadFn(ctx, path) },
 			s.logger,
 		)
 	})
 	// TODO: If upload completed successfully, unmark in-progress and delete file.
-	return nil
+	return
+}
+
+func (s *syncer) Sync(paths []string) {
+	for _, path := range paths {
+		s.upload(s.cancelCtx, path)
+	}
+
+	return
 }
 
 type progressTracker struct {
--- services/datamanager/sync_test.go
+++ services/datamanager/sync_test.go
@@ -18,12 +18,10 @@ func newTestSyncer(t *testing.T, uploadFn func(ctx context.Context, path string)
 	t.Helper()
 	cancelCtx, cancelFn := context.WithCancel(context.Background())
 	captureDir := t.TempDir()
-	syncQueue := t.TempDir()
 	l := golog.NewTestLogger(t)
 
 	return syncer{
 		captureDir:    captureDir,
-		syncQueue:     syncQueue,
 		logger:        l,
 		queueWaitTime: time.Nanosecond,
 		progressTracker: progressTracker{
@@ -36,8 +34,8 @@ func newTestSyncer(t *testing.T, uploadFn func(ctx context.Context, path string)
 	}
 }
 
-// Validates that for some captureDir, files are enqueued and uploaded exactly once.
-func TestQueuesAndUploadsOnce(t *testing.T) {
+// Validates that for some captureDir, files are uploaded exactly once.
+func TestUploadsOnce(t *testing.T) {
 	var uploadCount uint64
 	uploadFn := func(ctx context.Context, path string) error {
 		atomic.AddUint64(&uploadCount, 1)
@@ -46,30 +44,22 @@ func TestQueuesAndUploadsOnce(t *testing.T) {
 	}
 	sut := newTestSyncer(t, uploadFn)
 
-	// Start syncer.
-	sut.Start()
-
 	// Put a couple files in captureDir.
 	file1, _ := ioutil.TempFile(sut.captureDir, "whatever")
 	defer os.Remove(file1.Name())
 	file2, _ := ioutil.TempFile(sut.captureDir, "whatever2")
 	defer os.Remove(file2.Name())
-	err := sut.Enqueue([]string{file1.Name(), file2.Name()})
-	test.That(t, err, test.ShouldBeNil)
+	// Start syncer.
+	sut.Start()
 	// Give it a second to run and upload files.
 	time.Sleep(time.Second)
 
-	// Verify files were enqueued and uploaded.
+	// Verify files were  uploaded.
 	filesInCaptureDir, err := ioutil.ReadDir(sut.captureDir)
 	if err != nil {
 		t.Fatalf("failed to list files in captureDir")
 	}
-	filesInQueue, err := ioutil.ReadDir(sut.syncQueue)
-	if err != nil {
-		t.Fatalf("failed to list files in captureDir")
-	}
 	test.That(t, len(filesInCaptureDir), test.ShouldEqual, 0)
-	test.That(t, len(filesInQueue), test.ShouldEqual, 0)
 	test.That(t, atomic.LoadUint64(&uploadCount), test.ShouldEqual, 2)
 	sut.Close()
 }
@@ -87,7 +77,7 @@ func TestRecoversAfterKilled(t *testing.T) {
 	sut := newTestSyncer(t, uploadFn)
 
 	// Put a file in syncDir; this simulates a file that was enqueued by some previous syncer.
-	file1, _ := ioutil.TempFile(sut.syncQueue, "whatever")
+	file1, _ := ioutil.TempFile(sut.captureDir, "whatever")
 	defer os.Remove(file1.Name())
 
 	// Put a file in captureDir; this simulates a file that was written but not yet queued by some previous syncer.
@@ -97,21 +87,14 @@ func TestRecoversAfterKilled(t *testing.T) {
 
 	// Start syncer, let it run for a second.
 	sut.Start()
-	err := sut.Enqueue([]string{})
-	test.That(t, err, test.ShouldBeNil)
 	time.Sleep(time.Second)
 
-	// Verify enqueued files were uploaded.
-	filesInQueue, err := ioutil.ReadDir(sut.syncQueue)
-	if err != nil {
-		t.Fatalf("failed to list files in syncDir")
-	}
-	// Verify previously captured but not queued files were uploaded.
+	// Verify files were uploaded.
 	filesInCaptureDir, err := ioutil.ReadDir(sut.captureDir)
 	if err != nil {
 		t.Fatalf("failed to list files in syncDir")
 	}
-	test.That(t, len(filesInQueue), test.ShouldEqual, 0)
+
 	test.That(t, len(filesInCaptureDir), test.ShouldEqual, 0)
 	test.That(t, atomic.LoadUint64(&uploadCount), test.ShouldEqual, 2)
 	sut.Close()
@@ -141,8 +124,6 @@ func TestUploadExponentialRetry(t *testing.T) {
 	initialWaitTime = time.Millisecond * 25
 	maxRetryInterval = time.Millisecond * 150
 	sut.Start()
-	err := sut.Enqueue([]string{})
-	test.That(t, err, test.ShouldBeNil)
 	time.Sleep(time.Second)
 	sut.Close()
 
