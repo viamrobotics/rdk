713b52f9d34b906c0c22ea2488c639757056ec03
maximpertsov <maxim.pertsov@gmail.com>
2022-06-17 15:25:12 -0400

    rm newline

--- robot/impl/local_robot.go
+++ robot/impl/local_robot.go
@@ -356,7 +356,6 @@ func (r *localRobot) newService(ctx context.Context, config config.Service) (int
 	if f == nil {
 		return nil, errors.Errorf("unknown service type: %s", rName.Subtype)
 	}
-
 	return f.Constructor(ctx, r, config, r.logger)
 }
 
