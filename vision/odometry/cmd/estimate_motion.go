471dea62fedcb6002756cf18273241f802bc68c5
louisenaud <louise.naud@gmail.com>
2022-06-24 11:01:30 -0400

    Added display

--- vision/odometry/cmd/estimate_motion.go
+++ vision/odometry/cmd/estimate_motion.go
@@ -2,32 +2,39 @@ package main
 
 import (
 	"github.com/edaniels/golog"
-	"go.viam.com/utils/artifact"
-
 	"go.viam.com/rdk/rimage"
 	"go.viam.com/rdk/vision/odometry"
+	"go.viam.com/utils/artifact"
 )
 
 var logger = golog.NewLogger("visual-odometry")
 
-func main() {
+func run() error {
 	// load cfg
 	cfg := odometry.LoadMotionEstimationConfig(artifact.MustPath("vision/odometry/vo_config.json"))
 	// load images
 	im1, err := rimage.NewImageFromFile(artifact.MustPath("vision/odometry/000001.png"))
 	if err != nil {
-		logger.Fatal(err.Error())
+		return err
 	}
 
 	im2, err := rimage.NewImageFromFile(artifact.MustPath("vision/odometry/000002.png"))
 	if err != nil {
-		logger.Fatal(err.Error())
+		return err
 	}
 	// Estimate motion
-	motion, err := odometry.EstimateMotionFrom2Frames(im1, im2, cfg)
+	motion, err := odometry.EstimateMotionFrom2Frames(im1, im2, cfg, true)
 	if err != nil {
-		logger.Fatal(err.Error())
+		return err
 	}
 	logger.Info(motion.Rotation)
 	logger.Info(motion.Translation)
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
 }
