5b99ce96ca99cb8110225bc6ef56214dd36c35ed
louisenaud <louise.naud@gmail.com>
2022-06-27 16:37:32 -0400

    removed debug helper
    - displaying both images in a html page / localhost

--- vision/odometry/cmd/estimate_motion_test.go
+++ vision/odometry/cmd/estimate_motion_test.go
@@ -1,31 +1,12 @@
 package main
 
 import (
-	"image"
 	"testing"
 
-	"github.com/edaniels/golog"
 	"go.viam.com/test"
-
-	"go.viam.com/rdk/rimage"
 )
 
-type voTestHelper struct{}
-
-func (h *voTestHelper) Process(t *testing.T, pCtx *rimage.ProcessorContext, fn string, img image.Image, logger golog.Logger) error {
-	t.Helper()
-	var err error
-	// Calling the function `RunMotionEstimation` which is defined in `vision/odometry/main.go`
-	img1, img2, err := RunMotionEstimation()
-	test.That(t, err, test.ShouldBeNil)
-	pCtx.GotDebugImage(img1, "img1_kps")
-	pCtx.GotDebugImage(img2, "img2_kps")
-
-	return nil
-}
-
 func TestRun(t *testing.T) {
-	d := rimage.NewMultipleImageTestDebugger(t, "vision/odometry", "*.png", false)
-	err := d.Process(t, &voTestHelper{})
+	_, _, err := RunMotionEstimation()
 	test.That(t, err, test.ShouldBeNil)
 }
