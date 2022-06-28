c15d340ba1509b7077df4e73a1ff4885bb7ea4b3
aaroncasas <aaron@viam.com>
2022-06-28 11:26:00 -0400

    our linters are garbage

--- services/datamanager/data_capture_file.go
+++ services/datamanager/data_capture_file.go
@@ -6,6 +6,9 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/matttproud/golang_protobuf_extensions/pbutil"
+	"github.com/pkg/errors"
+	v1 "go.viam.com/api/proto/viam/datasync/v1"
 )
 
 const dataCaptureFileExt = ".sd"
