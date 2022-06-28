b00c4ef6424d7e4aa45b67321621d5d7450eb4d5
shreya <skarpoor@mit.edu>
2022-06-28 16:19:54 +0100

    i2c implemented

--- component/gps/rtk/i2c.go
+++ component/gps/rtk/i2c.go
@@ -1,1 +1,162 @@
 package rtk
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"sync"
+	"errors"
+
+	"github.com/adrianmo/go-nmea"
+	"github.com/go-gnss/rtcm/rtcm3"
+	"github.com/edaniels/golog"
+	geo "github.com/kellydunn/golang-geo"
+	"go.viam.com/utils"
+	"go.viam.com/utils/serial"
+
+	"go.viam.com/rdk/component/generic"
+	"go.viam.com/rdk/component/gps"
+	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/registry"
+)
+
+type I2CCorrectionSource struct {
+	correctionReader    	io.ReadCloser // reader for rctm corrections only
+	port					io.ReadCloser // reads all messages from port
+	logger             		golog.Logger
+	ntripStatus        		bool
+	bus    board.I2C
+	addr   byte
+
+	cancelCtx               context.Context
+	cancelFunc              func()
+	activeBackgroundWorkers sync.WaitGroup
+}
+
+const (
+	correctionPathName = "correction_path"
+)
+
+func newI2CCorrectionSource(ctx context.Context, config config.Component, logger golog.Logger) (I2CCorrectionSource, error) {
+	b, err := board.FromDependencies(deps, config.Attributes.String("board"))
+	if err != nil {
+		return nil, fmt.Errorf("gps init: failed to find board: %w", err)
+	}
+	localB, ok := b.(board.LocalBoard)
+	if !ok {
+		return nil, fmt.Errorf("board %s is not local", config.Attributes.String("board"))
+	}
+	i2cbus, ok := localB.I2CByName(config.Attributes.String("bus"))
+	if !ok {
+		return nil, fmt.Errorf("gps init: failed to find i2c bus %s", config.Attributes.String("bus"))
+	}
+	addr := config.Attributes.Int("i2c_addr", -1)
+	if addr == -1 {
+		return nil, errors.New("must specify gps i2c address")
+	}
+
+	cancelCtx, cancelFunc := context.WithCancel(ctx)
+
+	s := &I2CCorrectionSource{bus: i2cbus, addr: byte(addr), cancelCtx: cancelCtx, cancelFunc: cancelFunc, logger: logger}
+
+	return s, nil
+}
+
+func (s *I2CCorrectionSource) Start(ctx context.Context, ready chan<- bool) {
+	ntrip.correctionReader, w := io.Pipe()
+	ready <- true
+
+	// open I2C handle every time
+	handle, err := g.bus.OpenHandle(g.addr)
+	if err != nil {
+		g.logger.Fatalf("can't open gps i2c handle: %s", err)
+		return
+	}
+	buffer, err := handle.Read(context.Background(), 1024)
+
+	//read from handle and check if it's a proper RTCM message
+	scanner := rtcm3.NewScanner(buffer)
+
+	//close I2C handle
+	err = handle.Close()
+	if err != nil {
+		g.logger.Debug("failed to close handle: %s", err)
+		return
+	}
+
+	for err == nil {
+		select {
+		case <-s.cancelCtx.Done():
+			return
+		default:
+		}
+
+		// Open I2C handle every time
+		handle, err := g.bus.OpenHandle(g.addr)
+		if err != nil {
+			g.logger.Fatalf("can't open gps i2c handle: %s", err)
+			return
+		}
+
+		buffer, err := handle.Read(context.Background(), 1024)
+		hErr := handle.Close()
+		if hErr != nil {
+			g.logger.Fatalf("failed to close handle: %s", hErr)
+			return
+		}
+		if err != nil {
+			g.logger.Error(err)
+			continue
+		}
+
+		buffer, err := handle.Read(context.Background(), 1024)
+		if err != nil {
+			g.logger.Fatalf("Error reading RTCM message: %s", err)
+		}
+		fmt.Println(msg.Number())
+
+		n, err := w.Write(buffer.Serialize())
+		if err != nil {
+			g.logger.Fatalf("Error writing RTCM message: %s", err)
+		}
+
+		//close i2c
+		err = handle.Close()
+		if err != nil {
+			g.logger.Debug("failed to close handle: %s", err)
+			return
+		}
+	}
+}
+
+func (s *I2CCorrectionSource) GetReader() (io.ReadCloser, error) {
+	if s.correctionReader == nil {
+		return nil, errors.New("No Stream")
+	}
+
+	return s.correctionReader, nil
+}
+
+func (s *I2CCorrectionSource) Close() error {
+	s.cancelFunc()
+	s.activeBackgroundWorkers.Wait()
+
+	// close port reader
+	if s.port != nil {
+		if err := s.port.Close(); err != nil {
+			return err
+		}
+		s.port = nil
+	}
+
+	// close correction reader
+	if s.correctionReader != nil {
+		if err := s.correctionReader.Close(); err != nil {
+			return err
+		}
+		s.correctionReader = nil
+	}
+
+	return nil
+}
