134db280e67eab923b95e475f700a2a8d8646db6
Nicolas Menard <nicolas.p.menard@gmail.com>
2022-06-29 05:51:48 -0400

    Removed NewClient from datamanager

--- services/datamanager/client.go
+++ services/datamanager/client.go
@@ -7,7 +7,6 @@ import (
 	"github.com/edaniels/golog"
 	"go.viam.com/utils/rpc"
 
-	"go.viam.com/rdk/grpc"
 	pb "go.viam.com/rdk/proto/api/service/datamanager/v1"
 )
 
@@ -27,16 +26,6 @@ func newSvcClientFromConn(conn rpc.ClientConn, logger golog.Logger) *client {
 		logger: logger,
 	}
 	return sc
-}
-
-// NewClient constructs a new client that is served at the given address.
-func NewClient(ctx context.Context, name string, address string, logger golog.Logger, opts ...rpc.DialOption) (Service, error) {
-	conn, err := grpc.Dial(ctx, address, logger, opts...)
-	if err != nil {
-		return nil, err
-	}
-	sc := newSvcClientFromConn(conn, logger)
-	return sc, nil
 }
 
 // NewClientFromConn constructs a new Client from connection passed in.
