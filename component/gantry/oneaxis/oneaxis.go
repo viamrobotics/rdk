461d7ecddd5096c53a84de21778b62c98822ae45
Maxim Pertsov <maxim@viam.com>
2022-06-29 19:34:59 -0400

    refactor one-axis gantry validation

--- component/gantry/oneaxis/oneaxis.go
+++ component/gantry/oneaxis/oneaxis.go
@@ -41,46 +41,49 @@ type AttrConfig struct {
 
 // Validate ensures all parts of the config are valid.
 func (config *AttrConfig) Validate(path string) ([]string, error) {
-	deps := []string{}
+	deps, err := config.validate()
+	if err != nil {
+		err = utils.NewConfigValidationError(path, err)
+	}
+	return deps, err
+}
+
+func (config *AttrConfig) validate() ([]string, error) {
+	var deps []string
 
 	if len(config.Motor) == 0 {
-		// TODO: wrap utils.NewConfigValidationError upstream?
-		return nil, utils.NewConfigValidationError(path, errors.New("cannot find motor for gantry"))
+		return nil, errors.New("cannot find motor for gantry")
 	}
 	deps = append(deps, config.Motor)
 
 	if config.LengthMm <= 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("each axis needs a non-zero and positive length"))
+		return nil, errors.New("each axis needs a non-zero and positive length")
 	}
 
 	if len(config.LimitSwitchPins) == 0 && len(config.Board) > 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("gantry with encoders have to assign boards or controllers to motors"))
+		return nil, errors.New("gantry with encoders have to assign boards or controllers to motors")
 	}
 
 	if len(config.Board) == 0 && len(config.LimitSwitchPins) > 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("cannot find board for gantry"))
+		return nil, errors.New("cannot find board for gantry")
 	}
 	deps = append(deps, config.Board)
 
 	if len(config.LimitSwitchPins) == 1 && config.MmPerRevolution == 0 {
-		return nil, utils.NewConfigValidationError(path,
-			errors.New("gantry has one limit switch per axis, needs pulley radius to set position limits"))
+		return nil, errors.New("gantry has one limit switch per axis, needs pulley radius to set position limits")
 	}
 
 	if len(config.LimitSwitchPins) > 0 && config.LimitPinEnabled == nil {
-		return nil, utils.NewConfigValidationError(path, errors.New("limit pin enabled muist be set to true or false"))
+		return nil, errors.New("limit pin enabled muist be set to true or false")
 	}
 
 	if config.Axis.X == 0 && config.Axis.Y == 0 && config.Axis.Z == 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("gantry axis undefined, need one translational axis"))
+		return nil, errors.New("gantry axis undefined, need one translational axis")
 	}
 
 	if config.Axis.X == 1 && config.Axis.Y == 1 ||
 		config.Axis.X == 1 && config.Axis.Z == 1 || config.Axis.Y == 1 && config.Axis.Z == 1 {
-		return nil, utils.NewConfigValidationError(
-			path,
-			errors.New("only one translational axis of movement allowed for single axis gantry"),
-		)
+		return nil, errors.New("only one translational axis of movement allowed for single axis gantry")
 	}
 
 	return deps, nil
