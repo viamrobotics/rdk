704c6a5003b213b2e12f414be977773a30a8c404
Alexa Greenberg <alexa.greenberg@viam.com>
2022-07-05 16:08:18 -0400

    RSDK-405 add isMoving to base (#979)
    pretty straight forward, just checks if motors are moving in most cases

--- component/base/base_test.go
+++ component/base/base_test.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"testing"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
 	"go.viam.com/test"
 	"go.viam.com/utils"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/rdk/component/arm"
 	"go.viam.com/rdk/component/base"
+	commonpb "go.viam.com/rdk/proto/api/common/v1"
+	"go.viam.com/rdk/protoutils"
 	"go.viam.com/rdk/registry"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/testutils/inject"
@@ -116,6 +120,69 @@ func TestBaseNamed(t *testing.T) {
 	baseName := base.Named("test_base")
 	test.That(t, baseName.String(), test.ShouldResemble, "rdk:component:base/test_base")
 	test.That(t, baseName.Subtype, test.ShouldResemble, base.Subtype)
+}
+
+func TestStatusValid(t *testing.T) {
+	status := &commonpb.ActuatorStatus{
+		IsMoving: true,
+	}
+	map1, err := protoutils.InterfaceToMap(status)
+	test.That(t, err, test.ShouldBeNil)
+	newStruct, err := structpb.NewStruct(map1)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(
+		t,
+		newStruct.AsMap(),
+		test.ShouldResemble,
+		map[string]interface{}{
+			"is_moving": true,
+		},
+	)
+
+	convMap := &commonpb.ActuatorStatus{}
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{TagName: "json", Result: &convMap})
+	test.That(t, err, test.ShouldBeNil)
+	err = decoder.Decode(newStruct.AsMap())
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, convMap, test.ShouldResemble, status)
+}
+
+func TestCreateStatus(t *testing.T) {
+	_, err := base.CreateStatus(context.Background(), "not a base")
+	test.That(t, err, test.ShouldBeError, rutils.NewUnimplementedInterfaceError("LocalBase", "string"))
+
+	t.Run("is moving", func(t *testing.T) {
+		status := &commonpb.ActuatorStatus{
+			IsMoving: true,
+		}
+
+		injectBase := &inject.Base{}
+		injectBase.IsMovingFunc = func(context.Context) (bool, error) {
+			return true, nil
+		}
+		status1, err := base.CreateStatus(context.Background(), injectBase)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, status1, test.ShouldResemble, status)
+
+		resourceSubtype := registry.ResourceSubtypeLookup(base.Subtype)
+		status2, err := resourceSubtype.Status(context.Background(), injectBase)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, status2, test.ShouldResemble, status)
+	})
+
+	t.Run("is not moving", func(t *testing.T) {
+		status := &commonpb.ActuatorStatus{
+			IsMoving: false,
+		}
+
+		injectBase := &inject.Base{}
+		injectBase.IsMovingFunc = func(context.Context) (bool, error) {
+			return false, nil
+		}
+		status1, err := base.CreateStatus(context.Background(), injectBase)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, status1, test.ShouldResemble, status)
+	})
 }
 
 func TestWrapWithReconfigurable(t *testing.T) {
