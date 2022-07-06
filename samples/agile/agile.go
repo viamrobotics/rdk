589e74436e86069affe1fa16fdf626c78a2b8128
shreya <skarpoor@mit.edu>
2022-07-06 15:33:27 +0100

    move from point to point

--- samples/agile/agile.go
+++ samples/agile/agile.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "context"
+  "math"
+  "fmt"
 
   "github.com/edaniels/golog"
   "go.viam.com/rdk/grpc/client"
@@ -15,6 +17,8 @@ import (
 )
 
 var logger = golog.NewDevelopmentLogger("agile")
+
+
 
 func main() {
 	utils.ContextualMain(mainWithArgs, logger)
@@ -31,7 +35,8 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
       })),
   )
   if err != nil {
-      logger.Fatal(err)
+      logger.Debug(err)
+      return err
   }
   defer robot.Close(context.Background())
   logger.Info("Resources:")
@@ -41,8 +46,23 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 
   limo1 := limo.(base.Base)
 
+  MoveToWaypoint(ctx, limo1, 0, 0, 1, 0)
+
   limo1.MoveStraight(ctx, 1000, 100)
 
   return nil
   
+}
+
+func MoveToWaypoint(ctx context.Context, limo base.Base, x1 float64, y1 float64, x2 float64, y2 float64) error {
+  dir := 0.0  //get direction from state, but assume we're pointing at x direction for rn
+  dist := math.Sqrt(math.Pow((y2-y1), 2) + math.Pow((x2-x1), 2))*500  //each grid square is half a meter
+  theta := math.Acos((x2-x1)/dist)  //angle to x axis
+
+  limo.Spin(ctx, (dir-theta)*(180/math.Pi), 50) //constant velocity for rn
+  fmt.Println(dir-theta)
+  limo.MoveStraight(ctx, int(dist), 100)
+
+  return nil
+
 }
