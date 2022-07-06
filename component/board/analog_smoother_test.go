6af249e1bdd8fdc0a2a7a6d69acb4767ada34bbb
James Otting <james@viam.com>
2022-07-05 19:07:51 -0700

    Make AnalogSmoother test deterministic.

--- component/board/analog_smoother_test.go
+++ component/board/analog_smoother_test.go
@@ -15,22 +15,25 @@ import (
 type testReader struct {
 	mu   sync.Mutex
 	r    *rand.Rand
+	n    int64
+	lim  int64
 	stop bool
 }
 
 func (t *testReader) Read(ctx context.Context) (int, error) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	if t.stop {
+	if t.stop || t.n >= t.lim {
 		return 0, errStopReading
 	}
-
+	t.n++
 	return t.r.Intn(100), nil
 }
 
 func TestAnalogSmoother1(t *testing.T) {
 	testReader := testReader{
 		r: rand.New(rand.NewSource(11)),
+		lim: 200,
 	}
 	defer func() {
 		testReader.mu.Lock()
@@ -50,11 +53,13 @@ func TestAnalogSmoother1(t *testing.T) {
 	_, ok = as.(*AnalogSmoother)
 	test.That(t, ok, test.ShouldBeTrue)
 
-	time.Sleep(200 * time.Millisecond)
+	// Sleep far longer than needed. Want to hit the reader limit for deterministic behavior.
+	time.Sleep(500 * time.Millisecond)
 
 	v, err := as.Read(context.Background())
-	test.That(t, err, test.ShouldBeNil)
-	test.That(t, v, test.ShouldAlmostEqual, 50.0, 10.0)
+	test.That(t, testReader.n, test.ShouldEqual, testReader.lim)
+	test.That(t, err, test.ShouldEqual, errStopReading)
+	test.That(t, v, test.ShouldEqual, 52)
 
 	err = utils.TryClose(context.Background(), as)
 	test.That(t, err, test.ShouldBeNil)
