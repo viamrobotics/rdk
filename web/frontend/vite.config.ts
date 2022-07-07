026865992bca374a86c69b2c015597c4e8456e6d
Micheal Parks <micheal.parks@outlook.com>
2022-07-07 16:22:00 -0400

    add custom element rules to vite

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -3,7 +3,16 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.startsWith('v-'),
+        },
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
