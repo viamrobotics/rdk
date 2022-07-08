b4050af44e68e3f82fef66582a534abce2b56c32
Micheal Parks <micheal.parks@outlook.com>
2022-07-08 13:57:37 -0400

    convert base to vue3

--- web/frontend/src/components/Base.vue
+++ web/frontend/src/components/Base.vue
@@ -1,299 +1,291 @@
+<script setup lang="ts">
+
+import { onClickOutside } from '@vueuse/core';
+import { ref } from 'vue';
+import KeyboardInput from './KeyboardInput.vue';
+
+interface Props {
+  streamName: string
+  baseName: string
+  crumbs: string[]
+  connectedCamera: boolean
+}
+
+interface Emits {
+  (event: 'base-stop'): void
+  (event: 'show-base-camera'): void
+  (event: 'base-change-tab', selectedItem: string): void
+  (event: 'base-spin', data: { direction: 1 | -1, speed: number, angle: number }): void
+  (event: 'base-straight', data: { movementType: string, direction: 1 | -1, speed: number, distance: number }): void
+  (event: 'keyboard-ctl', data: Record<string, boolean>): void
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+const root = ref();
+const keyboard = ref();
+const camera = ref(props.connectedCamera);
+const selectedValue = ref('NoCamera');
+const selectedItem = ref<'Keyboard' | 'Discrete'>('Keyboard');
+const movementMode = ref('Straight');
+const movementType = ref('Continuous');
+const direction = ref('Forwards');
+const spinType = ref('Clockwise');
+const increment = ref(1000);
+const speed = ref(200); // straight mm/s
+const spinSpeed = ref(90); // spin deg/s
+const angle = ref(0);
+
+const cameraOptions = [
+  { value: 'NoCamera', label: 'No Camera' },
+  { value: 'Camera1', label: 'Camera1' },
+];
+
+const handleTabSelect = (tab: 'Keyboard' | 'Discrete') => {
+  selectedItem.value = tab;
+
+  if (tab === 'Keyboard') {
+    emit('base-change-tab', selectedItem.value.toLowerCase());
+  } else {
+    resetDiscreteState();
+  }
+};
+
+const resetDiscreteState = () => {
+  movementMode.value = 'Straight';
+  movementType.value = 'Continuous';
+  direction.value = 'Forwards';
+  spinType.value = 'Clockwise';
+};
+
+const setMovementMode = (mode: string) => {
+  movementMode.value = mode;
+  movementType.value = 'Continuous';
+};
+
+const setMovementType = (type: string) => {
+  movementType.value = type;
+};
+
+const setSpinType = (type: string) => {
+  spinType.value = type;
+};
+
+const setDirection = (dir: string) => {
+  direction.value = dir;
+};
+
+const baseRun = () => {
+  if (movementMode.value === 'Spin') {
+    emit('base-spin', {
+      direction: spinType.value === 'Clockwise' ? -1 : 1,
+      speed: spinSpeed.value,
+      angle: angle.value,
+    });
+  } else if (movementMode.value === 'Straight') {
+    emit('base-straight', {
+      movementType: movementType.value,
+      direction: direction.value === 'Forwards' ? 1 : -1,
+      speed: speed.value,
+      distance: increment.value,
+    });
+  } else {
+    console.log(`Unrecognized discrete movement mode: ${movementMode.value}`);
+  }
+};
+
+const keyboardCtl = (keysPressed: Record<string, boolean>) => {
+  emit('keyboard-ctl', keysPressed);
+};
+
+const handleCameraOptionsInput = (event: CustomEvent) => {
+  selectedValue.value = event.detail.value;
+  emit('show-base-camera');
+};
+
+onClickOutside(root, () => keyboard.value.removeKeyboardListeners());
+
+</script>
+
 <template>
   <v-collapse :title="baseName">
-    <v-breadcrumbs slot="header" :crumbs="crumbs.join(',')" />
+    <v-breadcrumbs
+      slot="header"
+      :crumbs="crumbs.join(',')"
+    />
 
-    <v-button slot="header" variant="danger" icon="stop" label="STOP" @click="baseStop" />
+    <v-button
+      slot="header"
+      variant="danger"
+      icon="stop"
+      label="STOP"
+      @click="emit('base-stop')"
+    />
+
+    <div
+      ref="root"
+      class="h-80 border border-t-0 border-black pt-2 pb-4"
+    >
+      <v-tabs
+        tabs="Keyboard, Discrete"
+        :selected="selectedItem"
+        @input="handleTabSelect($event.detail.value)"
+      />
 
       <div
-        class="border border-t-0 border-black pt-2 pb-4 h-80"
-        v-click-outside="removeKeyboardListeners"
-        :style="{ height: height }"
+        v-if="selectedItem === 'Keyboard'"
+        class="h-auto p-4"
       >
         <div>
-          <Tabs>
-            <Tab
-              :selected="selectedItem === 'keyboard'"
-              @select="selectedItem = 'keyboard'"
-              @click="$emit('base-change-tab', selectedItem)"
-              >Keyboard</Tab
-            >
-            <Tab
-              :selected="selectedItem === 'discrete'"
-              @select="selectedItem = 'discrete'"
-              @click="resetDiscreteState()"
-              >Discrete</Tab
-            >
-          </Tabs>
-        </div>
-        <div
-          v-if="selectedItem === 'keyboard'"
-          class="p-4"
-          :style="{ height: height }"
-        >
           <div>
-            <div>
-              <div class="grid grid-cols-2">
-                <div class="flex pt-6">
-                  <KeyboardInput
-                    @keyboard-ctl="keyboardCtl"
-                    ref="keyboardRef"
-                  >
-                  </KeyboardInput>
-                </div>
-                <div class="flex" v-if="camera">
-                  <div class="pr-4">
-                    <div class="w-64">
-                      <p
-                        class="mb-1 text-gray-800 font-label dark:text-gray-200"
-                      >
-                        Select Camera
-                      </p>
-                      <div class="relative">
-                        <ViamSelect
-                          :options="cameraOptions"
-                          v-model="selectedValue"
-                          @selected="$emit('show-base-camera')"
-                        >
-                        </ViamSelect>
-                      </div>
+            <div class="grid grid-cols-2">
+              <div class="flex pt-6">
+                <KeyboardInput
+                  ref="keyboard"
+                  @keyboard-ctl="keyboardCtl"
+                />
+              </div>
+              <div
+                v-if="camera"
+                class="flex"
+              >
+                <div class="pr-4">
+                  <div class="w-64">
+                    <p
+                      class="font-label mb-1 text-gray-800 dark:text-gray-200"
+                    >
+                      Select Camera
+                    </p>
+                    <div class="relative">
+                      <v-select
+                        :options="cameraOptions.join(',')"
+                        :selected="selectedValue"
+                        @input="handleCameraOptionsInput"
+                      />
                     </div>
                   </div>
-                  <div
-                    class="w-48 h-48 transition-all duration-300 ease-in-out"
-                    v-if="selectedValue !== 'NoCamera'"
-                    :id="streamId"
-                  ></div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          v-if="selectedItem === 'discrete'"
-          class="pr-4 pl-4 pt-4 flex"
-          :style="{ height: height }"
-        >
-          <div class="flex-grow">
-            <div>
-              <div class="column">
-                <p class="text-xs">Movement Mode</p>
-                <RadioButtons
-                  :options="['Straight', 'Spin']"
-                  defaultOption="Straight"
-                  :disabledOptions="[]"
-                  v-on:selectOption="setMovementMode($event)"
+                <div
+                  v-if="selectedValue !== 'NoCamera'"
+                  :id="`stream-preview-${props.streamName}`"
+                  class="h-48 w-48 transition-all duration-300 ease-in-out"
                 />
               </div>
             </div>
-            <div
-              :class="movementMode === 'Spin' ? 'inline-flex' : 'flex'"
-              class="items-center pt-4"
-            >
-              <div class="column pr-2" v-if="movementMode === 'Straight'">
-                <p class="text-xs">Movement Type</p>
-                <RadioButtons
-                  :options="['Continuous', 'Discrete']"
-                  defaultOption="Continuous"
-                  :disabledOptions="[]"
-                  v-on:selectOption="setMovementType($event)"
-                />
-              </div>
-              <div v-if="movementMode === 'Straight'" class="column pr-2">
-                <p class="text-xs">Direction</p>
-                <RadioButtons
-                  :options="['Forwards', 'Backwards']"
-                  defaultOption="Forwards"
-                  :disabledOptions="[]"
-                  v-on:selectOption="setDirection($event)"
-                />
-              </div>
-              <NumberInput
-                v-model="speed"
-                class="mr-2"
-                inputId="speed"
-                label="Speed (mm/sec)"
-                v-if="movementMode === 'Straight'"
-              ></NumberInput>
-              <NumberInput
-                v-model="increment"
-                class="mr-2"
-                inputId="distance"
-                :disabled="movementType === 'Continuous'"
-                label="Distance (mm)"
-                v-if="movementMode === 'Straight'"
-              ></NumberInput>
-              <NumberInput
-                v-model="spinSpeed"
-                class="mr-2"
-                inputId="spinspeed"
-                label="Speed (deg/sec)"
-                v-if="movementMode === 'Spin'"
-              ></NumberInput>
-            </div>
-            <div
-              :class="movementMode === 'Spin' ? 'inline-flex' : 'flex'"
-              class="pt-4"
-              v-if="movementMode === 'Spin'"
-            >
-              <div class="column pr-2">
-                <p class="text-xs">Movement Type</p>
-                <RadioButtons
-                  data-cy="movement-type-radio"
-                  :options="['Clockwise', 'Counterclockwise']"
-                  defaultOption="Clockwise"
-                  :disabledOptions="[]"
-                  v-on:selectOption="setSpinType($event)"
-                />
-              </div>
-              <div class="column pl-4">
-                <Range
-                  id="angle"
-                  :min="0"
-                  :key="movementMode"
-                  :max="360"
-                  :step="90"
-                  v-model="angle"
-                  unit="°"
-                  name="Angle"
-                ></Range>
-              </div>
-            </div>
-          </div>
-          <div class="self-end">
-            <ViamButton
-              color="success"
-              group
-              variant="primary"
-              @click="baseRun()"
-            >
-              <template v-slot:icon>
-                <ViamIcon color="white" :path="mdiPlayCircleOutline"
-                  >RUN</ViamIcon
-                >
-              </template>
-              RUN
-            </ViamButton>
           </div>
         </div>
       </div>
+      <div
+        v-if="selectedItem === 'Discrete'"
+        class="flex h-auto px-4 pt-4"
+      >
+        <div class="grow">
+          <div>
+            <div class="column">
+              <p class="text-xs">
+                Movement Mode
+              </p>
+              <v-radio
+                options="Straight, Spin"
+                :selected="movementMode"
+                @input="setMovementMode($event.detail.value)"
+              />
+            </div>
+          </div>
+          <div
+            :class="movementMode === 'Spin' ? 'inline-flex' : 'flex'"
+            class="items-center pt-4"
+          >
+            <div
+              v-if="movementMode === 'Straight'"
+              class="column pr-2"
+            >
+              <p class="text-xs">
+                Movement Type
+              </p>
+              <v-radio
+                options="Continuous, Discrete"
+                :selected="movementType"
+                @input="setMovementType($event.detail.value)"
+              />
+            </div>
+            <div
+              v-if="movementMode === 'Straight'"
+              class="column pr-2"
+            >
+              <p class="text-xs">
+                Direction
+              </p>
+              <v-radio
+                options="Forwards, Backwards"
+                :selected="direction"
+                @input="setDirection($event.detail.value)"
+              />
+            </div>
+            <v-input
+              v-if="movementMode === 'Straight'"
+              type="number"
+              :value="speed"
+              class="mr-2"
+              label="Speed (mm/sec)"
+              @input="speed = $event.detail.value"
+            />
+            <v-input
+              v-if="movementMode === 'Straight'"
+              type="number"
+              class="mr-2"
+              :value="increment"
+              :disabled="movementType === 'Continuous'"
+              label="Distance (mm)"
+              @input="increment = $event.detail.value"
+            />
+            <v-input
+              v-if="movementMode === 'Spin'"
+              type="number"
+              :value="spinSpeed"
+              class="mr-2"
+              label="Speed (deg/sec)"
+              @input="spinSpeed = $event.detail.value"
+            />
+          </div>
+          <div
+            v-if="movementMode === 'Spin'"
+            :class="movementMode === 'Spin' ? 'inline-flex' : 'flex'"
+            class="pt-4"
+          >
+            <div class="column pr-2">
+              <p class="text-xs">
+                Movement Type
+              </p>
+              <v-radio
+                options="Clockwise, Counterclockwise"
+                :selected="spinType"
+                @input="setSpinType($event.detail.value)"
+              />
+            </div>
+            <div class="column pl-4">
+              <Range
+                id="angle"
+                :key="movementMode"
+                v-model="angle"
+                :min="0"
+                :max="360"
+                :step="90"
+                unit="°"
+                name="Angle"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="self-end">
+          <v-button
+            icon="play-circle"
+            label="RUN"
+            @click="baseRun()"
+          />
+        </div>
+      </div>
+    </div>
   </v-collapse>
 </template>
-
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import "vue-class-component/hooks";
-import Collapse from "./Collapse.vue";
-import NumberInput from "./NumberInput.vue";
-import Breadcrumbs from "./Breadcrumbs.vue";
-import ViamIcon from "./ViamIcon.vue";
-import {
-  mdiRestore,
-  mdiPlayCircleOutline,
-  mdiCloseOctagonOutline,
-} from "@mdi/js";
-import Tabs from "./Tabs.vue";
-import Tab from "./Tab.vue";
-import RadioButtons from "./RadioButtons.vue";
-import ViamButton from "./Button.vue";
-
-@Component({
-  components: {
-    Collapse,
-    Breadcrumbs,
-    ViamIcon,
-    RadioButtons,
-    Tabs,
-    Tab,
-    ViamButton,
-    NumberInput,
-  },
-})
-export default class Base extends Vue {
-  @Prop({ default: null }) streamName!: string;
-  @Prop({ default: null }) baseName!: string;
-  @Prop({ default: null }) crumbs!: [string];
-  @Prop({ default: true }) connectedCamera!: boolean;
-
-  mdiRestore = mdiRestore;
-  mdiPlayCircleOutline = mdiPlayCircleOutline;
-  mdiCloseOctagonOutline = mdiCloseOctagonOutline;
-
-  camera = this.connectedCamera;
-  height = "auto";
-  selectedValue = "NoCamera";
-  isContinuous = true;
-  streamId = "stream-preview-" + this.streamName;
-  selectedItem = "keyboard";
-  movementMode = "Straight";
-  movementType = "Continuous";
-  direction = "Forwards";
-  spinType = "Clockwise";
-  increment = 1000;
-
-  speed = 200; // straight mm/s
-  spinSpeed = 90; // spin deg/s
-  angle = 0;
-
-  cameraOptions = [
-    { value: "NoCamera", label: "No Camera" },
-    { value: "Camera1", label: "Camera1" },
-  ];
-
-  resetDiscreteState(): void {
-    this.movementMode = "Straight";
-    this.movementType = "Continuous";
-    this.direction = "Forwards";
-    this.spinType = "Clockwise";
-  }
-
-  setMovementMode(e: string): void {
-    this.movementMode = e;
-    this.movementType = "Continuous";
-  }
-  setMovementType(e: string): void {
-    this.movementType = e;
-  }
-  setSpinType(e: string): void {
-    this.spinType = e;
-  }
-  setDirection(e: string): void {
-    this.direction = e;
-  }
-  baseRun(): void {
-    if (this.movementMode == "Spin") {
-      this.$emit("base-spin", {
-        direction: this.spinType == "Clockwise" ? -1 : 1,
-        speed: this.spinSpeed,
-        angle: this.angle,
-      });
-    } else if (this.movementMode == "Straight") {
-      this.$emit("base-straight", {
-        movementType: this.movementType,
-        direction: this.direction == "Forwards" ? 1 : -1,
-        speed: this.speed,
-        distance: this.increment,
-      });
-    } else {
-      console.log("Unrecognized discrete movement mode: " + this.movementMode);
-    }
-  }
-  baseStop(e: Event): void {
-    e.preventDefault();
-    e.stopPropagation();
-    this.$emit("base-stop");
-  }
-  keyboardCtl(keysPressed: Record<string, unknown>): void {
-    this.$emit("keyboard-ctl", {
-      forward: keysPressed.forward,
-      backward: keysPressed.backward,
-      right: keysPressed.right,
-      left: keysPressed.left,
-    });
-  }
-  removeKeyboardListeners(): void {
-    // eslint-disable-next-line
-    const keyboardRef: any = this.$refs.keyboardRef;
-    keyboardRef.removeKeyboardListeners();
-  }
-}
-</script>
