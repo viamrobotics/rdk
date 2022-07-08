b6911559e689f697acf4d9f2d5e71be4102de227
Micheal Parks <micheal.parks@outlook.com>
2022-07-08 17:33:36 -0400

    delete dls

--- web/frontend/dls/src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.vue" {
-  import Vue from "vue";
-  export default Vue;
-}
-
-declare module "vue-slide-bar" {
-  import VueSlideBar from "vue-slide-bar";
-  export default VueSlideBar;
-}
