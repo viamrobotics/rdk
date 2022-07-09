038bede22c1b1f849c4305e51d0eac7b6bd1ef05
Alexa Greenberg <alexa.greenberg@viam.com>
2022-07-08 21:31:52 -0400

    Add extra options to arm (#995)

--- component/arm/client.go
+++ component/arm/client.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/edaniels/golog"
 	"go.viam.com/utils/rpc"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/rdk/component/generic"
 	commonpb "go.viam.com/rdk/proto/api/common/v1"
@@ -50,9 +51,14 @@ func clientFromSvcClient(sc *serviceClient, name string) Arm {
 	return &client{sc, name}
 }
 
-func (c *client) GetEndPosition(ctx context.Context) (*commonpb.Pose, error) {
+func (c *client) GetEndPosition(ctx context.Context, extra map[string]interface{}) (*commonpb.Pose, error) {
+	ext, err := structpb.NewStruct(extra)
+	if err != nil {
+		return nil, err
+	}
 	resp, err := c.client.GetEndPosition(ctx, &pb.GetEndPositionRequest{
-		Name: c.name,
+		Name:  c.name,
+		Extra: ext,
 	})
 	if err != nil {
 		return nil, err
@@ -60,26 +66,46 @@ func (c *client) GetEndPosition(ctx context.Context) (*commonpb.Pose, error) {
 	return resp.Pose, nil
 }
 
-func (c *client) MoveToPosition(ctx context.Context, pose *commonpb.Pose, worldState *commonpb.WorldState) error {
-	_, err := c.client.MoveToPosition(ctx, &pb.MoveToPositionRequest{
+func (c *client) MoveToPosition(
+	ctx context.Context,
+	pose *commonpb.Pose,
+	worldState *commonpb.WorldState,
+	extra map[string]interface{},
+) error {
+	ext, err := structpb.NewStruct(extra)
+	if err != nil {
+		return err
+	}
+	_, err = c.client.MoveToPosition(ctx, &pb.MoveToPositionRequest{
 		Name:       c.name,
 		To:         pose,
 		WorldState: worldState,
+		Extra:      ext,
 	})
 	return err
 }
 
-func (c *client) MoveToJointPositions(ctx context.Context, positions *pb.JointPositions) error {
-	_, err := c.client.MoveToJointPositions(ctx, &pb.MoveToJointPositionsRequest{
+func (c *client) MoveToJointPositions(ctx context.Context, positions *pb.JointPositions, extra map[string]interface{}) error {
+	ext, err := structpb.NewStruct(extra)
+	if err != nil {
+		return err
+	}
+	_, err = c.client.MoveToJointPositions(ctx, &pb.MoveToJointPositionsRequest{
 		Name:      c.name,
 		Positions: positions,
+		Extra:     ext,
 	})
 	return err
 }
 
-func (c *client) GetJointPositions(ctx context.Context) (*pb.JointPositions, error) {
+func (c *client) GetJointPositions(ctx context.Context, extra map[string]interface{}) (*pb.JointPositions, error) {
+	ext, err := structpb.NewStruct(extra)
+	if err != nil {
+		return nil, err
+	}
 	resp, err := c.client.GetJointPositions(ctx, &pb.GetJointPositionsRequest{
-		Name: c.name,
+		Name:  c.name,
+		Extra: ext,
 	})
 	if err != nil {
 		return nil, err
@@ -87,9 +113,14 @@ func (c *client) GetJointPositions(ctx context.Context) (*pb.JointPositions, err
 	return resp.Positions, nil
 }
 
-func (c *client) Stop(ctx context.Context) error {
-	_, err := c.client.Stop(ctx, &pb.StopRequest{
-		Name: c.name,
+func (c *client) Stop(ctx context.Context, extra map[string]interface{}) error {
+	ext, err := structpb.NewStruct(extra)
+	if err != nil {
+		return err
+	}
+	_, err = c.client.Stop(ctx, &pb.StopRequest{
+		Name:  c.name,
+		Extra: ext,
 	})
 	return err
 }
