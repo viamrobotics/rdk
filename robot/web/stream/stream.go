a95f01d5ee5fce28c3f9c298f5d9630a69f24361
Maxim Pertsov <maxim@viam.com>
2022-07-11 21:05:18 -0400

    update docstring + naming changes + speed up test more

--- robot/web/stream/stream.go
+++ robot/web/stream/stream.go
@@ -18,29 +18,29 @@ func StreamSource(ctx context.Context, source gostream.ImageSource, stream gostr
 	gostream.StreamSourceWithErrorHandler(ctx, source, stream, backoffOpts.getErrorThrottledHandler())
 }
 
-// BackoffTuningOptions represents a set of parameters for determining
-// exponential backoff when receiving multiple simultaneous errors. This is
-// to reduce the number of errors logged in the case of minor discontinuity
-// in the camera stream.
-// The number of milliseconds slept at a particular attempt i is determined by
-// min(ExpBase^(i) + Offset, MaxSleepMilliSec).
+// BackoffTuningOptions represents a set of parameters for determining exponential
+// backoff when receiving multiple simultaneous errors.
+//
+// `BaseSleep` is the duration to wait after receiving a new error. After that, the wait
+// time doubles for every subsequent, consecutive error of the same type, until the wait
+// duration reaches the `MaxSleep` duration.
 type BackoffTuningOptions struct {
-	// The initial amount of time to wait after an error.
+	// `BaseSleep` sets the initial amount of time to wait after an error.
 	BaseSleep time.Duration
 	// MaxSleep determines the maximum amount of time that streamSource is
-	// permitted to a sleep after receiving a single error
+	// permitted to a sleep after receiving a single error.
 	MaxSleep time.Duration
 	// MaxSleepAttempts determines the number of consecutive errors for which
-	// streamSource will sleep
+	// streamSource will sleep.
 	MaxSleepAttempts int
 }
 
 // GetSleepTimeFromErrorCount returns a sleep time from an error count.
-func (opts *BackoffTuningOptions) GetSleepTimeFromErrorCount(errCount int) time.Duration {
-	if errCount < 1 {
+func (opts *BackoffTuningOptions) GetSleepTimeFromErrorCount(errorCount int) time.Duration {
+	if errorCount < 1 {
 		return time.Duration(0)
 	}
-	multiplier := math.Pow(2, float64(errCount-1))
+	multiplier := math.Pow(2, float64(errorCount-1))
 	uncappedSleepNanosec := float64(opts.BaseSleep.Nanoseconds()) * multiplier
 	sleepNanosec := math.Min(uncappedSleepNanosec, float64(opts.MaxSleep.Nanoseconds()))
 	return time.Duration(int64(sleepNanosec))
@@ -60,8 +60,8 @@ func (opts *BackoffTuningOptions) getErrorThrottledHandler() func(context.Contex
 			}
 
 			if errorCount <= opts.MaxSleepAttempts {
-				rlog.Logger.Debugw("error getting frame", "error", err)
 				sleep := opts.GetSleepTimeFromErrorCount(errorCount)
+				rlog.Logger.Debugw("error getting frame", "error", err)
 				utils.SelectContextOrWait(ctx, sleep)
 			} else {
 				panic(err)
