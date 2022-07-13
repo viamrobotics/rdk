69063e10e1540936c4d969d5d13657c4ed5394c1
Alexis Wei <qq_wei369@hotmail.com>
2022-07-13 17:40:52 -0400

    add close to vision service

--- services/vision/detectors_test.go
+++ services/vision/detectors_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
+	viamutils "go.viam.com/utils"
 	"go.viam.com/utils/artifact"
 
 	"go.viam.com/rdk/config"
@@ -158,4 +159,58 @@ func TestRegisterUnknownDetector(t *testing.T) {
 	reg := make(detectorMap)
 	err := registerNewDetectors(context.Background(), reg, conf, golog.NewTestLogger(t))
 	test.That(t, err, test.ShouldBeError, newDetectorTypeNotImplemented("not_real"))
+}
+
+func TestCloseService(t *testing.T) {
+	ctx := context.Background()
+	srv := createService(ctx, t, "data/empty.json")
+	// success
+	cfg := DetectorConfig{
+		Name: "test",
+		Type: "color",
+		Parameters: config.AttributeMap{
+			"detect_color": "#112233",
+			"tolerance":    0.4,
+			"segment_size": 100,
+		},
+	}
+	err := srv.AddDetector(ctx, cfg)
+	test.That(t, err, test.ShouldBeNil)
+	vService := srv.(*visionService)
+	fakeStruct := newStruct()
+	det := func(image.Image) ([]objdet.Detection, error) {
+		return []objdet.Detection{}, nil
+	}
+	registeredFn := registeredDetector{detector: det, closer: fakeStruct}
+	logger := golog.NewTestLogger(t)
+	err = vService.detReg.registerDetector("fake", &registeredFn, logger)
+	test.That(t, err, test.ShouldBeNil)
+	err = viamutils.TryClose(ctx, srv)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, fakeStruct.val, test.ShouldEqual, 1)
+
+	detectors, err := srv.GetDetectorNames(ctx)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, len(detectors), test.ShouldEqual, 0)
+}
+
+func newStruct() *fakeClosingStruct {
+	return &fakeClosingStruct{val: 0}
+}
+
+type fakeClosingStruct struct {
+	val int
+}
+
+func (s *fakeClosingStruct) Close() error {
+	s.val++
+	return nil
+}
+
+func createService(ctx context.Context, t *testing.T, filePath string) Service {
+	t.Helper()
+	logger := golog.NewTestLogger(t)
+	srv, err := New(ctx, nil, config.Service{}, logger)
+	test.That(t, err, test.ShouldBeNil)
+	return srv
 }
