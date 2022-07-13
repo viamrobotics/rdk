a7eaee803b26f973db5c2e388f2ffda6ab587377
maximpertsov <maxim.pertsov@gmail.com>
2022-07-13 09:02:21 -0400

    Apply suggestions from code review

--- robot/web/stream/stream.go
+++ robot/web/stream/stream.go
@@ -21,11 +21,11 @@ func StreamSource(ctx context.Context, source gostream.ImageSource, stream gostr
 // BackoffTuningOptions represents a set of parameters for determining exponential
 // backoff when receiving multiple simultaneous errors.
 //
-// `BaseSleep` is the duration to wait after receiving a new error. After that, the wait
+// BaseSleep is the duration to wait after receiving a new error. After that, the wait
 // time doubles for every subsequent, consecutive error of the same type, until the wait
-// duration reaches the `MaxSleep` duration.
+// duration reaches the MaxSleep duration.
 type BackoffTuningOptions struct {
-	// `BaseSleep` sets the initial amount of time to wait after an error.
+	// BaseSleep sets the initial amount of time to wait after an error.
 	BaseSleep time.Duration
 	// MaxSleep determines the maximum amount of time that streamSource is
 	// permitted to a sleep after receiving a single error.
@@ -35,7 +35,7 @@ type BackoffTuningOptions struct {
 // GetSleepTimeFromErrorCount returns a sleep time from an error count.
 func (opts *BackoffTuningOptions) GetSleepTimeFromErrorCount(errorCount int) time.Duration {
 	if errorCount < 1 || opts == nil {
-		return time.Duration(0)
+		return 0
 	}
 	multiplier := math.Pow(2, float64(errorCount-1))
 	uncappedSleep := opts.BaseSleep * time.Duration(multiplier)
@@ -45,7 +45,7 @@ func (opts *BackoffTuningOptions) GetSleepTimeFromErrorCount(errorCount int) tim
 
 func (opts *BackoffTuningOptions) getErrorThrottledHandler() func(context.Context, error) bool {
 	var prevErr error
-	errorCount := 0
+	var errorCount int
 
 	return func(ctx context.Context, err error) bool {
 		if err != nil {
