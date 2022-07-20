ef5736dc5e8b8a19d0e5fe7532744af8ed6d9c84
Ray Bjorkman <raymond@viam.com>
2022-07-20 17:04:41 -0400

    make things thread safe

--- component/camera/ffmpeg/ffmpeg_test.go
+++ component/camera/ffmpeg/ffmpeg_test.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
+	"go.viam.com/utils"
 )
 
 func TestFFmpegCamera(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	ctx := context.Background()
-	cam, err := NewFFmpegCamera(ctx, &ffmpegAttrs{URL: "rstp://10.1.1.29:8555/unicast"}, logger)
+	cam, err := NewFFmpegCamera(&ffmpegAttrs{URL: "rtsp://10.1.1.29:8555/unicast"}, logger)
 	test.That(t, err, test.ShouldBeNil)
 	for i := 0; i < 10; i++ {
 		img, _, err := cam.Next(ctx)
-		logger.Debug("this shouldn't happen")
 		test.That(t, err, test.ShouldBeNil)
-		logger.Debug(img)
+		_ = img
 	}
+	test.That(t, utils.TryClose(context.Background(), cam), test.ShouldBeNil)
 }
