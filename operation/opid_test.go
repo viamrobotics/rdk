e1a015b1ef63c3190640b5b09ca543ce2e5a680d
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2022-07-20 14:04:02 -0400

    add tests to ensure filter works

--- operation/opid_test.go
+++ operation/opid_test.go
@@ -23,6 +23,7 @@ func TestBasic(t *testing.T) {
 
 		o := Get(ctx2)
 		test.That(t, o, test.ShouldNotBeNil)
+		test.That(t, len(o.myManager.ops), test.ShouldNotEqual, 0)
 		test.That(t, o.ID.String(), test.ShouldNotEqual, "")
 		test.That(t, len(h.All()), test.ShouldEqual, 1)
 		test.That(t, h.All()[0].ID, test.ShouldEqual, o.ID)
@@ -45,5 +46,18 @@ func TestBasic(t *testing.T) {
 
 		test.That(t, ctx3.Err(), test.ShouldBeNil)
 		test.That(t, ctx2.Err(), test.ShouldNotBeNil)
+	}()
+
+	func() {
+		ctx4, cleanup4 := h.Create(ctx, "proto.rpc.webrtc.v1.SignalingService", nil)
+		defer cleanup4()
+
+		ctx5, cleanup5 := h.Create(ctx, "/proto.api.robot.v1.RobotService/StreamStatus", nil)
+		defer cleanup5()
+
+		o4 := Get(ctx4)
+		o5 := Get(ctx5)
+		test.That(t, len(o4.myManager.ops), test.ShouldEqual, 0)
+		test.That(t, len(o5.myManager.ops), test.ShouldEqual, 0)
 	}()
 }
