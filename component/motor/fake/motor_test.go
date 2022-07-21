3dcea02a1400ecdfbbf1565de64ad77a94f64f0e
Evelyn <evelyn.fu@viam.com>
2022-07-21 19:28:43 +0100

    update tests

--- component/motor/fake/motor_test.go
+++ component/motor/fake/motor_test.go
@@ -41,7 +41,7 @@ func TestEncoder(t *testing.T) {
 	test.That(t, e.speed, test.ShouldEqual, 1)
 }
 
-func TestMotor(t *testing.T) {
+func TestMotorInit(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	ctx := context.Background()
 
@@ -53,7 +53,6 @@ func TestMotor(t *testing.T) {
 		MaxRPM:            60,
 	}
 
-	// Test initial position/features
 	pos, err := m.GetPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, pos, test.ShouldEqual, 0)
@@ -61,39 +60,98 @@ func TestMotor(t *testing.T) {
 	featureMap, err := m.GetFeatures(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, featureMap[motor.PositionReporting], test.ShouldBeTrue)
+}
 
-	// Test GoFor
+func TestGoFor(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+
+	m := &Motor{
+		Encoder:           Encoder{Valid: true},
+		Logger:            logger,
+		PositionReporting: true,
+		TicksPerRotation:  1,
+		MaxRPM:            60,
+	}
+
 	m.Encoder.Start(ctx, &m.activeBackgroundWorkers)
-	err = m.GoFor(ctx, 60, 1)
+	err := m.GoFor(ctx, 60, 1)
 	test.That(t, err, test.ShouldBeNil)
 
-	pos, err = m.GetPosition(ctx)
+	pos, err := m.GetPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, pos, test.ShouldBeGreaterThan, 0)
+}
 
-	// Test GoTo
-	prevPos := pos
-	err = m.GoTo(ctx, 60, 2)
+func TestGoTo(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+
+	m := &Motor{
+		Encoder:           Encoder{Valid: true},
+		Logger:            logger,
+		PositionReporting: true,
+		TicksPerRotation:  1,
+		MaxRPM:            60,
+	}
+
+	err := m.GoTo(ctx, 60, 1)
 	test.That(t, err, test.ShouldBeNil)
 
-	pos, err = m.GetPosition(ctx)
+	pos, err := m.GetPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
-	test.That(t, pos, test.ShouldBeGreaterThan, prevPos)
+	test.That(t, pos, test.ShouldBeGreaterThan, 0)
+}
 
-	// Test GoTillStop
-	err = m.GoTillStop(ctx, 0, func(ctx context.Context) bool { return false })
+func TestGoTillStop(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+
+	m := &Motor{
+		Encoder:           Encoder{Valid: true},
+		Logger:            logger,
+		PositionReporting: true,
+		TicksPerRotation:  1,
+		MaxRPM:            60,
+	}
+
+	err := m.GoTillStop(ctx, 0, func(ctx context.Context) bool { return false })
 	test.That(t, err, test.ShouldNotBeNil)
+}
 
-	// Test ResetZeroPosition
-	err = m.ResetZeroPosition(ctx, 0)
+func TestResetZeroPosition(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+
+	m := &Motor{
+		Encoder:           Encoder{Valid: true},
+		Logger:            logger,
+		PositionReporting: true,
+		TicksPerRotation:  1,
+		MaxRPM:            60,
+	}
+
+	err := m.ResetZeroPosition(ctx, 0)
 	test.That(t, err, test.ShouldBeNil)
 
-	pos, err = m.GetPosition(ctx)
+	pos, err := m.GetPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, pos, test.ShouldEqual, 0)
+}
 
-	// Test SetPower
-	err = m.SetPower(ctx, 1.0)
+func TestPower(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+
+	m := &Motor{
+		Encoder:           Encoder{Valid: true},
+		Logger:            logger,
+		PositionReporting: true,
+		TicksPerRotation:  1,
+		MaxRPM:            60,
+	}
+
+	err := m.SetPower(ctx, 1.0)
 	test.That(t, err, test.ShouldBeNil)
 
 	powerPct := m.PowerPct()
