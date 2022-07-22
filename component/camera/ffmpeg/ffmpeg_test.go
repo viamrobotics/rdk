34f1e9f2fafe4adcfd6d714040047ff01cd7c563
raybjork <raymond@viam.com>
2022-07-22 11:24:14 -0400

    change to artifact

--- component/camera/ffmpeg/ffmpeg_test.go
+++ component/camera/ffmpeg/ffmpeg_test.go
@@ -8,14 +8,13 @@ import (
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
 	viamutils "go.viam.com/utils"
-
-	"go.viam.com/rdk/utils"
+	"go.viam.com/utils/artifact"
 )
 
 func TestFFmpegCamera(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	ctx := context.Background()
-	path := utils.ResolveFile("component/camera/ffmpeg/data/testsrc.mpg")
+	path := artifact.MustPath("component/camera/ffmpeg/testsrc.mpg")
 	cam, err := NewFFMPEGCamera(&AttrConfig{Source: path}, logger)
 	test.That(t, err, test.ShouldBeNil)
 	for i := 0; i < 5; i++ {
