65b27281312e42ab109eae01fd07f5630d2e3358
raybjork <raymond@viam.com>
2022-07-26 14:43:58 -0400

    basic rrt POC

--- motionplan/rrtConnect.go
+++ motionplan/rrtConnect.go
@@ -3,7 +3,6 @@ package motionplan
 import (
 	"context"
 	"errors"
-	"math"
 	"math/rand"
 	"sort"
 
@@ -34,7 +33,7 @@ import (
 // 	iterBeforeRand = 50
 // )
 
-type rrtStarMotionPlanner struct {
+type rrtConnectMotionPlanner struct {
 	solDist         float64
 	solver          InverseKinematics
 	fastGradDescent *NloptIK
@@ -47,8 +46,8 @@ type rrtStarMotionPlanner struct {
 	randseed        *rand.Rand
 }
 
-// NewrrtStarMotionPlanner creates a rrtStarMotionPlanner object.
-func NewRRTStarMotionPlanner(frame referenceframe.Frame, nCPU int, logger golog.Logger) (MotionPlanner, error) {
+// NewRRTConnectMotionPlan creates a rrtStarMotionPlanner object.
+func NewRRTConnectMotionPlan(frame referenceframe.Frame, nCPU int, logger golog.Logger) (MotionPlanner, error) {
 	ik, err := CreateCombinedIKSolver(frame, logger, nCPU)
 	if err != nil {
 		return nil, err
@@ -58,7 +57,7 @@ func NewRRTStarMotionPlanner(frame referenceframe.Frame, nCPU int, logger golog.
 	if err != nil {
 		return nil, err
 	}
-	mp := &rrtStarMotionPlanner{solver: ik, fastGradDescent: nlopt, frame: frame, logger: logger, solDist: jointSolveDist, nCPU: nCPU}
+	mp := &rrtConnectMotionPlanner{solver: ik, fastGradDescent: nlopt, frame: frame, logger: logger, solDist: jointSolveDist, nCPU: nCPU}
 
 	mp.qstep = getFrameSteps(frame, frameStep)
 	mp.iter = planIter
@@ -70,15 +69,15 @@ func NewRRTStarMotionPlanner(frame referenceframe.Frame, nCPU int, logger golog.
 	return mp, nil
 }
 
-func (mp *rrtStarMotionPlanner) Frame() referenceframe.Frame {
+func (mp *rrtConnectMotionPlanner) Frame() referenceframe.Frame {
 	return mp.frame
 }
 
-func (mp *rrtStarMotionPlanner) Resolution() float64 {
+func (mp *rrtConnectMotionPlanner) Resolution() float64 {
 	return mp.stepSize
 }
 
-func (mp *rrtStarMotionPlanner) Plan(ctx context.Context,
+func (mp *rrtConnectMotionPlanner) Plan(ctx context.Context,
 	goal *commonpb.Pose,
 	seed []referenceframe.Input,
 	opt *PlannerOptions,
@@ -101,7 +100,7 @@ func (mp *rrtStarMotionPlanner) Plan(ctx context.Context,
 
 // planRunner will execute the plan. When Plan() is called, it will call planRunner in a separate thread and wait for the results.
 // Separating this allows other things to call planRunner in parallel while also enabling the thread-agnostic Plan to be accessible.
-func (mp *rrtStarMotionPlanner) planRunner(ctx context.Context,
+func (mp *rrtConnectMotionPlanner) planRunner(ctx context.Context,
 	goal *commonpb.Pose,
 	seed []referenceframe.Input,
 	opt *PlannerOptions,
@@ -181,144 +180,52 @@ func (mp *rrtStarMotionPlanner) planRunner(ctx context.Context,
 		nearest2 := nm.nearestNeighbor(nmContext, target, map2)
 
 		// attempt to extend the map to connect the target to map 1, then try to connect the maps together
-		map1reached := mp.constrainedExtend(ctx, opt, map1, nearest1, target)
-		map2reached := mp.constrainedExtend(ctx, opt, map2, nearest2, map1reached)
+		map1reached := mp.checkPath(ctx, opt, nearest1.inputs, target.inputs)
+		if map1reached {
+			map1[target] = nearest1
+		}
+		map2reached := mp.checkPath(ctx, opt, nearest2.inputs, target.inputs)
+		if map2reached {
+			map2[target] = nearest2
+		}
 
-		if inputDist(map1reached.inputs, map2reached.inputs) < mp.solDist {
+		if map1reached && map2reached {
 			cancel()
-
-			path := extractPath(startMap, goalMap, map1reached, map2reached)
-			// if endpointPreview != nil {
-			// 	endpointPreview <- inputSteps[len(inputSteps)-1]
-			// }
-
-			solutionChan <- &planReturn{steps: finalSteps}
+			solutionChan <- &planReturn{steps: extractPath(startMap, goalMap, target, target)}
 			return
 		}
 
-		target = mp.sample(map1reached, i)
+		target = mp.sample()
 
-		// Swap the maps
 		map1, map2 = map2, map1
 	}
 
 	solutionChan <- &planReturn{err: errors.New("could not solve path")}
 }
 
-func (mp *rrtStarMotionPlanner) sample(nearby *configuration, sampleNum int) *configuration {
-	// If we have done more than 50 iterations, start seeding off completely random positions 2 at a time
-	// The 2 at a time is to ensure random seeds are added onto both the seed and goal maps.
-	if sampleNum >= iterBeforeRand && sampleNum%4 >= 2 {
-		return &configuration{referenceframe.RandomFrameInputs(mp.frame, mp.randseed)}
-	} else {
-		// Seeding nearby to valid points results in much faster convergence in less constrained space
-		q := &configuration{referenceframe.RestrictedRandomFrameInputs(mp.frame, mp.randseed, 0.2)}
-		for j, v := range nearby.inputs {
-			q.inputs[j].Value += v.Value
-		}
-		return q
-	}
+func (mp *rrtConnectMotionPlanner) sample() *configuration {
+	return &configuration{referenceframe.RandomFrameInputs(mp.frame, mp.randseed)}
 }
 
-// constrainedExtend will try to extend the map towards the target while meeting constraints along the way. It will
-// return the closest solution to the target that it reaches, which may or may not actually be the target.
-func (mp *rrtStarMotionPlanner) constrainedExtend(
-	ctx context.Context,
-	opt *PlannerOptions,
-	rrtMap map[*configuration]*configuration,
-	near, target *configuration,
-) *configuration {
-	oldNear := near
-	for i := 0; true; i++ {
-		switch {
-		case inputDist(near.inputs, target.inputs) < mp.solDist:
-			return near
-		case inputDist(near.inputs, target.inputs) > inputDist(oldNear.inputs, target.inputs):
-			return oldNear
-		case i > 2 && inputDist(near.inputs, oldNear.inputs) < math.Pow(mp.solDist, 3):
-			// not moving enough to make meaningful progress. Do not trigger on first iteration.
-			return oldNear
-		}
-
-		oldNear = near
-
-		newNear := make([]referenceframe.Input, 0, len(near.inputs))
-
-		// alter near to be closer to target
-		for j, nearInput := range near.inputs {
-			if nearInput.Value == target.inputs[j].Value {
-				newNear = append(newNear, nearInput)
-			} else {
-				v1, v2 := nearInput.Value, target.inputs[j].Value
-				newVal := math.Min(mp.qstep[j], math.Abs(v2-v1))
-				// get correct sign
-				newVal *= (v2 - v1) / math.Abs(v2-v1)
-				newNear = append(newNear, referenceframe.Input{nearInput.Value + newVal})
-			}
-		}
-		// if we are not meeting a constraint, gradient descend to the constraint
-		newNear = mp.constrainNear(ctx, opt, oldNear.inputs, newNear)
-
-		if newNear != nil {
-			// constrainNear will ensure path between oldNear and newNear satisfies constraints along the way
-			near = &configuration{newNear}
-			rrtMap[near] = oldNear
-		} else {
-			break
-		}
-	}
-	return oldNear
-}
-
-// constrainNear will do a IK gradient descent from seedInputs to target. If a gradient descent distance
-// function has been specified, this will use that.
-func (mp *rrtStarMotionPlanner) constrainNear(
-	ctx context.Context,
-	opt *PlannerOptions,
-	seedInputs,
-	target []referenceframe.Input,
-) []referenceframe.Input {
+func (mp *rrtConnectMotionPlanner) checkPath(ctx context.Context, opt *PlannerOptions, seedInputs, target []referenceframe.Input) bool {
 	seedPos, err := mp.frame.Transform(seedInputs)
 	if err != nil {
-		return nil
+		return false
 	}
 	goalPos, err := mp.frame.Transform(target)
 	if err != nil {
-		return nil
+		return false
 	}
 	// Check if constraints need to be met
-	ok, _ := opt.CheckConstraintPath(&ConstraintInput{
-		seedPos,
-		goalPos,
-		seedInputs,
-		target,
-		mp.frame,
-	}, mp.Resolution())
-	if ok {
-		return target
-	}
-
-	solutionGen := make(chan []referenceframe.Input, 1)
-	// Spawn the IK solver to generate solutions until done
-	err = mp.fastGradDescent.Solve(ctx, solutionGen, goalPos, target, opt.pathDist)
-	// We should have zero or one solutions
-	var solved []referenceframe.Input
-	select {
-	case solved = <-solutionGen:
-	default:
-	}
-	close(solutionGen)
-	if err != nil {
-		return nil
-	}
-
-	ok, failpos := opt.CheckConstraintPath(&ConstraintInput{StartInput: seedInputs, EndInput: solved, Frame: mp.frame}, mp.Resolution())
-	if !ok {
-		if failpos != nil && inputDist(target, failpos.EndInput) > mp.solDist {
-			// If we have a first failing position, and that target is updating (no infinite loop), then recurse
-			return mp.constrainNear(ctx, opt, failpos.StartInput, failpos.EndInput)
-		}
-		return nil
-	}
-	return solved
+	ok, _ := opt.CheckConstraintPath(
+		&ConstraintInput{
+			StartPos:   seedPos,
+			EndPos:     goalPos,
+			StartInput: seedInputs,
+			EndInput:   target,
+			Frame:      mp.frame,
+		},
+		mp.Resolution(),
+	)
+	return ok
 }
