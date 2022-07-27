efd79e149c9be78fe0901837b8a4e92dc4576b73
maximpertsov <maxim@viam.com>
2022-07-27 15:41:12 -0400

    Expose robot api for discovery service (#1070)
    * expose robot api for discovery service
    
    * add note
    
    * lint

--- web/frontend/src/api.js
+++ web/frontend/src/api.js
@@ -23,6 +23,7 @@ import armApi from './gen/proto/api/component/arm/v1/arm_pb.esm';
 import baseApi from './gen/proto/api/component/base/v1/base_pb.esm';
 import cameraApi from './gen/proto/api/component/camera/v1/camera_pb.esm';
 import gripperApi from './gen/proto/api/component/gripper/v1/gripper_pb.esm';
+import robotApi from './gen/proto/api/robot/v1/robot_pb.esm';
 import sensorsApi from './gen/proto/api/service/sensors/v1/sensors_pb.esm';
 import servoApi from './gen/proto/api/component/servo/v1/servo_pb.esm';
 import streamApi from './gen/proto/stream/v1/stream_pb.esm';
@@ -39,6 +40,12 @@ window.gripperApi = gripperApi;
 window.sensorsApi = sensorsApi;
 window.servoApi = servoApi;
 window.streamApi = streamApi;
+/**
+ * This window variable is used by the config page to access the discovery service.
+ * As with variables above, once we switch to using import / export we should
+ * remove / clean up these window variables.
+ */
+window.robotApi = robotApi;
 
 let savedAuthEntity;
 let savedCreds;
