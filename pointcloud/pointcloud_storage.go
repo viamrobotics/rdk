0c645a80abdc35c4fa16ba49346cf653656b4870
Aidan Glickman <aidan.glickman@viam.com>
2022-07-28 16:40:50 -0400

    add initial matrix backed storage and break out map storage

--- pointcloud/pointcloud_storage.go
+++ pointcloud/pointcloud_storage.go
@@ -11,14 +11,7 @@ type storage interface {
 	At(x, y, z float64) (Data, bool)
 	Iterate(numBatches, myBatch int, fn func(p r3.Vector, d Data) bool)
 	EditSupported() bool
-}
-
-type mapStorage struct {
-	points map[r3.Vector]Data
-}
-
-func (ms *mapStorage) Size() int {
-	return len(ms.points)
+	IsOrdered() bool
 }
 
 // With 64-bit floating point numbers, you get about 16 decimal digits of precision.
@@ -32,39 +25,4 @@ const (
 // be stored precisely.
 func newOutOfRangeErr(dim string, val float64) error {
 	return errors.Errorf("%s component (%v) is out of range [%v,%v]", dim, val, minPreciseFloat64, maxPreciseFloat64)
-}
-
-func (ms *mapStorage) Set(v r3.Vector, d Data) error {
-	if v.X > maxPreciseFloat64 || v.X < minPreciseFloat64 {
-		return newOutOfRangeErr("x", v.X)
-	}
-	if v.Y > maxPreciseFloat64 || v.Y < minPreciseFloat64 {
-		return newOutOfRangeErr("y", v.Y)
-	}
-	if v.Z > maxPreciseFloat64 || v.Z < minPreciseFloat64 {
-		return newOutOfRangeErr("z", v.Z)
-	}
-	ms.points[v] = d
-	return nil
-}
-
-func (ms *mapStorage) At(x, y, z float64) (Data, bool) {
-	d, found := ms.points[r3.Vector{x, y, z}]
-	return d, found
-}
-
-func (ms *mapStorage) Iterate(numBatches, myBatch int, fn func(p r3.Vector, d Data) bool) {
-	if numBatches > 0 && myBatch > 0 {
-		// TODO(erh) finish me
-		return
-	}
-	for p, d := range ms.points {
-		if cont := fn(p, d); !cont {
-			return
-		}
-	}
-}
-
-func (ms *mapStorage) EditSupported() bool {
-	return true
 }
