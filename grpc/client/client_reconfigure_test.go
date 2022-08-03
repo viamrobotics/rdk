3e56d19c58c747e15f344539fe003f694a5b901b
Alexis Wei <qq_wei369@hotmail.com>
2022-08-03 12:48:04 -0700

    goroutine

--- grpc/client/client_reconfigure_test.go
+++ grpc/client/client_reconfigure_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/edaniels/golog"
+	"go.viam.com/rdk/resource"
 	weboptions "go.viam.com/rdk/robot/web/options"
 	"go.viam.com/test"
 	"go.viam.com/utils/testutils"
@@ -40,25 +41,27 @@ func TestReconfigurableClient(t *testing.T) {
 	err = robot.StartWeb(ctx, options)
 	test.That(t, err, test.ShouldBeNil)
 
+	dur := 100 * time.Millisecond
 	// start robot client
 	robotClient, err := client.New(
 		ctx1,
 		addr,
 		logger,
-		client.WithCheckConnectedEvery(time.Second),
-		client.WithReconnectEvery(time.Second),
+		client.WithCheckConnectedEvery(dur),
+		client.WithReconnectEvery(dur),
 	)
 	test.That(t, err, test.ShouldBeNil)
 	defer robotClient.Close(ctx1)
 
 	res, err := arm.FromRobot(robotClient, "arm1")
 	test.That(t, err, test.ShouldBeNil)
-	for i := 0; i < 2; i++ {
-		test.That(t, robotClient.ResourceNames(), test.ShouldNotBeNil)
-		_, err := res.GetEndPosition(context.Background(), map[string]interface{}{})
-		test.That(t, err, test.ShouldBeNil)
-		time.Sleep(time.Second)
-	}
+	// these should now still be found
+	ch := make(chan int)
+	ch1 := make(chan error)
+	go checkRobot(t, robotClient, res, ch, ch1)
+
+	test.That(t, <-ch, test.ShouldEqual, 4)
+	test.That(t, <-ch1, test.ShouldBeNil)
 
 	// close/disconnect the robot
 	robot.Close(ctx)
@@ -74,10 +77,19 @@ func TestReconfigurableClient(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 
 	time.Sleep(time.Second * 20)
-	// these should now still be found
-	test.That(t, robotClient.ResourceNames(), test.ShouldNotBeNil)
-	_, err = res.GetEndPosition(context.Background(), map[string]interface{}{})
-	test.That(t, err, test.ShouldBeNil)
-	time.Sleep(time.Second)
+	test.That(t, <-ch, test.ShouldEqual, 4)
+	test.That(t, <-ch1, test.ShouldBeNil)
+	close(ch)
 
+}
+
+func checkRobot(t *testing.T, c *client.RobotClient, a arm.Arm, ch chan int, ch1 chan error) {
+	var names []resource.Name
+	for {
+		names = c.ResourceNames()
+		ch <- len(names)
+		_, err := a.GetEndPosition(context.Background(), map[string]interface{}{})
+		ch1 <- err
+		time.Sleep(time.Second)
+	}
 }
