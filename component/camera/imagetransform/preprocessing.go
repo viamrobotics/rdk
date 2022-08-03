498f5487c20bbc6229c2e5e6aab2941ace6a8b7a
Alexa Greenberg <alexa.greenberg@viam.com>
2022-08-03 12:16:08 -0400

    [DATA-208] Make imageWithDepth private to rdk/rimage (#1079)

--- component/camera/imagetransform/preprocessing.go
+++ component/camera/imagetransform/preprocessing.go
@@ -26,7 +26,7 @@ func init() {
 			config config.Component,
 			logger golog.Logger,
 		) (interface{}, error) {
-			attrs, ok := config.ConvertedAttributes.(*camera.AttrConfig)
+			attrs, ok := config.ConvertedAttributes.(*transformConfig)
 			if !ok {
 				return nil, utils.NewUnexpectedTypeError(attrs, config.ConvertedAttributes)
 			}
@@ -35,9 +35,9 @@ func init() {
 
 	config.RegisterComponentAttributeMapConverter(camera.SubtypeName, "preprocess_depth",
 		func(attributes config.AttributeMap) (interface{}, error) {
-			var conf camera.AttrConfig
+			var conf transformConfig
 			return config.TransformAttributeMapToStruct(&conf, attributes)
-		}, &camera.AttrConfig{})
+		}, &transformConfig{})
 }
 
 // preprocessDepthSource applies pre-processing functions to depth maps in order to smooth edges and fill holes.
@@ -47,28 +47,27 @@ type preprocessDepthSource struct {
 
 // Next applies depth preprocessing to the next image.
 func (os *preprocessDepthSource) Next(ctx context.Context) (image.Image, func(), error) {
-	i, closer, err := os.source.Next(ctx)
+	i, release, err := os.source.Next(ctx)
 	if err != nil {
-		return i, closer, err
-	}
-	defer closer()
-	ii := rimage.ConvertToImageWithDepth(i)
-	if ii.Depth == nil {
-		return nil, nil, errors.New("no depth")
-	}
-	ii.Depth, err = rimage.PreprocessDepthMap(ii.Depth, ii.Color)
-	if ii.Depth == nil {
 		return nil, nil, err
 	}
-	return ii, func() {}, nil
+	dm, err := rimage.ConvertImageToDepthMap(i)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "camera does not provide depth image")
+	}
+	dm, err = rimage.PreprocessDepthMap(dm, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	return dm, release, nil
 }
 
-func newPreprocessDepth(ctx context.Context, deps registry.Dependencies, attrs *camera.AttrConfig) (camera.Camera, error) {
+func newPreprocessDepth(ctx context.Context, deps registry.Dependencies, attrs *transformConfig) (camera.Camera, error) {
 	source, err := camera.FromDependencies(deps, attrs.Source)
 	if err != nil {
 		return nil, fmt.Errorf("no source camera (%s): %w", attrs.Source, err)
 	}
 	imgSrc := &preprocessDepthSource{source}
-	proj, _ := camera.GetProjector(ctx, attrs, source)
+	proj, _ := camera.GetProjector(ctx, nil, source)
 	return camera.New(imgSrc, proj)
 }
