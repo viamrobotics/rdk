dd748c3aadad5a1f320610c2387e9a35a6f30954
Max Horowitz <maxhorowitz@gmail.com>
2022-08-04 12:04:17 -0400

    Progress, still not working though.

--- services/datamanager/mock_upload_service_eric_test.go
+++ services/datamanager/mock_upload_service_eric_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"testing"
 
 	"github.com/edaniels/golog"
+	"github.com/matttproud/golang_protobuf_extensions/pbutil"
 	v1 "go.viam.com/api/proto/viam/datasync/v1"
 	"go.viam.com/test"
 	"go.viam.com/utils"
@@ -62,16 +64,23 @@ func uploadReqToUploadResp(m *mockDataSyncService_UploadServer, req *v1.UploadRe
 }
 
 type mockDataSyncServiceServer struct {
+	messagesSent               int
+	sendAckEveryNMessages      int
+	cancelStreamAfterNMessages int
+	shouldSendEOF              bool
+	shouldSendACK              bool
+	shouldSendCancelCtx        bool
 	v1.UnimplementedDataSyncServiceServer
 }
 
 func (m mockDataSyncServiceServer) Upload(stream v1.DataSyncService_UploadServer) error {
 	mockServer := &mockDataSyncService_UploadServer{
-		messagesSent:               0,
-		sendAckEveryNMessages:      3,
-		cancelStreamAfterNMessages: -1,
-		shouldSendEOF:              false,
-		shouldSendACK:              false,
+		messagesSent:               m.messagesSent,
+		sendAckEveryNMessages:      m.sendAckEveryNMessages,
+		cancelStreamAfterNMessages: m.cancelStreamAfterNMessages,
+		shouldSendEOF:              m.shouldSendEOF,
+		shouldSendACK:              m.shouldSendACK,
+		shouldSendCancelCtx:        m.shouldSendCancelCtx,
 	}
 	for {
 		req, err := stream.Recv()
@@ -93,48 +102,121 @@ func (m mockDataSyncServiceServer) Upload(stream v1.DataSyncService_UploadServer
 //nolint: unused
 func (m mockDataSyncServiceServer) mustEmbedUnimplementedDataSyncServiceServer() {}
 
+type mockServerBehavior struct {
+	sendAckEveryNMessages      int
+	cancelStreamAfterNMessages int
+	shouldSendEOF              bool
+	shouldSendAck              bool
+	shouldSendCancelCtx        bool
+}
+
 func TestDataCaptureUpload(t *testing.T) {
-	// Register mock datamanager service with a mock server.
-	logger, _ := golog.NewObservedTestLogger(t)
-	rpcServer, err := rpc.NewServer(logger, rpc.WithUnauthenticated())
-	test.That(t, err, test.ShouldBeNil)
-	rpcServer.RegisterServiceServer(
-		context.Background(),
-		&v1.DataSyncService_ServiceDesc,
-		mockDataSyncServiceServer{},
-		v1.RegisterDataSyncServiceHandlerFromEndpoint,
-	)
+	msgBin1 := []byte("Robots are really cool.")
+	msgBin2 := []byte("This work is helping develop the robotics space.")
+	msgBin3 := []byte("This message is used for testing.")
+	tests := []struct {
+		name    string
+		toSend  [][]byte
+		expData [][]byte
+		msb     *mockServerBehavior
+	}{
+		{
+			name:    "stream of binary sensor data readings",
+			toSend:  [][]byte{msgBin1, msgBin2, msgBin3, msgBin1, msgBin2, msgBin3},
+			expData: [][]byte{msgBin1, msgBin2, msgBin3, msgBin1, msgBin2, msgBin3},
+			msb: &mockServerBehavior{
+				sendAckEveryNMessages:      2,
+				cancelStreamAfterNMessages: -1,
+				shouldSendEOF:              false,
+				shouldSendAck:              false,
+				shouldSendCancelCtx:        false,
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mc := &mockDataSyncServiceServer{
+				messagesSent:               0,
+				sendAckEveryNMessages:      tc.msb.sendAckEveryNMessages,
+				cancelStreamAfterNMessages: tc.msb.cancelStreamAfterNMessages,
+				shouldSendEOF:              tc.msb.shouldSendEOF,
+				shouldSendACK:              tc.msb.shouldSendAck,
+				shouldSendCancelCtx:        tc.msb.shouldSendCancelCtx,
+			}
 
-	// Stand up the server. Defer stopping the server.
-	go func() {
-		err := rpcServer.Start()
-		test.That(t, err, test.ShouldBeNil)
-	}()
-	defer func() {
-		err := rpcServer.Stop()
-		test.That(t, err, test.ShouldBeNil)
-	}()
+			// Register mock datamanager service with a mock server.
+			logger, _ := golog.NewObservedTestLogger(t)
+			rpcServer, err := rpc.NewServer(logger, rpc.WithUnauthenticated())
+			test.That(t, err, test.ShouldBeNil)
+			rpcServer.RegisterServiceServer(
+				context.Background(),
+				&v1.DataSyncService_ServiceDesc,
+				mc,
+				v1.RegisterDataSyncServiceHandlerFromEndpoint,
+			)
 
-	// Dial connection.
-	port, err := utils.TryReserveRandomPort()
-	test.That(t, err, test.ShouldBeNil)
-	rawAddress := fmt.Sprintf("localhost:%d", port)
-	test.That(t, err, test.ShouldBeNil)
-	conn, err := rpc.DialDirectGRPC(
-		context.Background(),
-		rawAddress,
-		logger,
-	)
-	test.That(t, err, test.ShouldBeNil)
+			// Stand up the server. Defer stopping the server.
+			go func() {
+				err := rpcServer.Start()
+				test.That(t, err, test.ShouldBeNil)
+			}()
+			defer func() {
+				err := rpcServer.Stop()
+				test.That(t, err, test.ShouldBeNil)
+			}()
 
-	// Defer closing the connection.
-	defer func() {
-		test.That(t, conn.Close(), test.ShouldBeNil)
-	}()
-	test.That(t, err, test.ShouldBeNil)
-	client := v1.NewDataSyncServiceClient(conn)
-	print(client)
+			// Dial connection.
+			port, err := utils.TryReserveRandomPort()
+			test.That(t, err, test.ShouldBeNil)
+			rawAddress := fmt.Sprintf("localhost:%d", port)
+			test.That(t, err, test.ShouldBeNil)
+			conn, err := rpc.DialDirectGRPC(
+				context.Background(),
+				rawAddress,
+				logger,
+			)
+			test.That(t, err, test.ShouldBeNil)
 
-	// Validate that the client responds properly to whatever is sent by the mocked server.
+			// Defer closing the connection.
+			defer func() {
+				err := conn.Close()
+				test.That(t, err, test.ShouldBeNil)
+			}()
+
+			// Create temp file to be used as examples of reading data from the files into buffers and finally to have
+			// that data be uploaded to the cloud
+			tf, err := createTmpDataCaptureFile()
+			if err != nil {
+				t.Errorf("%s cannot create temporary file to be used for sensorUpload/fileUpload testing: %v",
+					tc.name, err)
+			}
+			defer os.Remove(tf.Name())
+
+			// First write metadata to file.
+			syncMetadata := v1.DataCaptureMetadata{
+				ComponentType:    componentType,
+				ComponentName:    componentName,
+				MethodName:       methodName,
+				Type:             v1.DataType_DATA_TYPE_BINARY_SENSOR,
+				MethodParameters: nil,
+			}
+			if _, err := pbutil.WriteDelimited(tf, &syncMetadata); err != nil {
+				t.Errorf("%s cannot write protobuf struct to temporary file as part of setup for sensorUpload testing: %v",
+					tc.name, err)
+			}
+
+			// Write the data from the test cases into the files to prepare them for reading by the sensorUpload function.
+			if err := writeBinarySensorData(tf, tc.toSend); err != nil {
+				t.Errorf("%s cannot write byte slice to temporary file as part of setup for "+
+					"sensorUpload/fileUpload testing: %v", tc.name, err)
+			}
+
+			// Validate that the client responds properly to whatever is sent by the mocked server.
+			client := v1.NewDataSyncServiceClient(conn)
+			_, err = client.Upload(context.Background())
+			test.That(t, err, test.ShouldBeNil)
+
+		})
+	}
 
 }
