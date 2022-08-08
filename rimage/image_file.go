6da73f34d535e1660625a581f3aa3a39a9a3a2c8
Bijan Haney <bijan@viam.com>
2022-08-08 12:46:46 -0400

    remove .both tests

--- rimage/image_file.go
+++ rimage/image_file.go
@@ -219,16 +219,6 @@ func EncodeImage(ctx context.Context, img image.Image, mimeType string) ([]byte,
 		imgCopy := image.NewRGBA(bounds)
 		draw.Draw(imgCopy, bounds, img, bounds.Min, draw.Src)
 		buf.Write(imgCopy.Pix)
-	case ut.MimeTypeRawDepth:
-		// TODO(DATA-237) remove this data type
-		dm, ok := img.(*DepthMap)
-		if !ok {
-			return nil, ut.NewUnexpectedTypeError(dm, img)
-		}
-		_, err := dm.WriteTo(&buf)
-		if err != nil {
-			return nil, err
-		}
 	case ut.MimeTypePNG:
 		if err := png.Encode(&buf, img); err != nil {
 			return nil, err
