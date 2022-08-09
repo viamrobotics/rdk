965f468d8bbd52d44d23c27d7eefe4970b1ff21e
Nick Franczak <nicholasgfranczak@gmail.com>
2022-08-09 15:02:49 -0400

    changing BuildCaptureMetadata back to DataCaptureMetadata

--- services/datamanager/datacapture/data_capture_file.go
+++ services/datamanager/datacapture/data_capture_file.go
@@ -25,7 +25,7 @@ func EmptyReadingErr(fileName string) error {
 }
 
 // CreateDataCaptureFile creates a timestamped file within the given capture directory.
-func CreateDataCaptureFile(captureDir string, md *v1.BuildCaptureMetadata) (*os.File, error) {
+func CreateDataCaptureFile(captureDir string, md *v1.DataCaptureMetadata) (*os.File, error) {
 	// First create directories and the file in it.
 	fileDir := filepath.Join(captureDir, md.GetComponentType(), md.GetComponentName(), md.GetMethodName())
 	if err := os.MkdirAll(fileDir, 0o700); err != nil {
@@ -45,11 +45,11 @@ func CreateDataCaptureFile(captureDir string, md *v1.BuildCaptureMetadata) (*os.
 	return f, nil
 }
 
-// BuildCaptureMetadata builds a BuildCaptureMetadata object.
-func BuildCaptureMetadata(compType resource.SubtypeName, compName string, compModel string, method string,
+// DataCaptureMetadata builds a DataCaptureMetadata object.
+func DataCaptureMetadata(compType resource.SubtypeName, compName string, compModel string, method string,
 	additionalParams map[string]string,
-) *v1.BuildCaptureMetadata {
-	return &v1.BuildCaptureMetadata{
+) *v1.DataCaptureMetadata {
+	return &v1.DataCaptureMetadata{
 		ComponentType:    string(compType),
 		ComponentName:    compName,
 		ComponentModel:   compModel,
@@ -60,12 +60,12 @@ func BuildCaptureMetadata(compType resource.SubtypeName, compName string, compMo
 }
 
 // ReadBuildCaptureMetadata reads the SyncMetadata message from the beginning of the capture file.
-func ReadBuildCaptureMetadata(f *os.File) (*v1.BuildCaptureMetadata, error) {
+func ReadDataCaptureMetadata(f *os.File) (*v1.DataCaptureMetadata, error) {
 	if _, err := f.Seek(0, 0); err != nil {
 		return nil, err
 	}
 
-	r := &v1.BuildCaptureMetadata{}
+	r := &v1.DataCaptureMetadata{}
 	if _, err := pbutil.ReadDelimited(f, r); err != nil {
 		return nil, errors.Wrapf(err, fmt.Sprintf("failed to read SyncMetadata from %s", f.Name()))
 	}
