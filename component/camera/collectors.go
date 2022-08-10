0237fbdb781d689da43210027aa53345f2e0d229
Alexa Greenberg <alexa.greenberg@viam.com>
2022-08-10 14:49:38 -0400

    init

--- component/camera/collectors.go
+++ component/camera/collectors.go
@@ -9,17 +9,24 @@ import (
 
 	"go.viam.com/rdk/data"
 	"go.viam.com/rdk/pointcloud"
+	pb "go.viam.com/rdk/proto/api/component/camera/v1"
+	"go.viam.com/rdk/rimage"
+	"go.viam.com/rdk/utils"
 )
 
 type method int64
 
 const (
 	nextPointCloud method = iota
+	next           method = iota
 )
 
 func (m method) String() string {
-	if m == nextPointCloud {
+	switch m {
+	case nextPointCloud:
 		return "NextPointCloud"
+	case next:
+		return "Next"
 	}
 	return "Unknown"
 }
@@ -33,7 +40,7 @@ func newNextPointCloudCollector(resource interface{}, params data.CollectorParam
 	}
 
 	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]string) (interface{}, error) {
-		_, span := trace.StartSpan(ctx, "camera::data::collector::CaptureFunc")
+		_, span := trace.StartSpan(ctx, "camera::data::collector::CaptureFunc::NextPointCloud")
 		defer span.End()
 
 		v, err := camera.NextPointCloud(ctx)
@@ -49,6 +56,48 @@ func newNextPointCloudCollector(resource interface{}, params data.CollectorParam
 			return nil, errors.Errorf("failed to convert returned point cloud to PCD: %v", err)
 		}
 		return buf.Bytes(), nil
+	})
+	return data.NewCollector(cFunc, params)
+}
+
+func newNextCollector(resource interface{}, params data.CollectorParams) (data.Collector, error) {
+	camera, err := assertCamera(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]string) (interface{}, error) {
+		_, span := trace.StartSpan(ctx, "camera::data::collector::CaptureFunc::NextPointCloud")
+		defer span.End()
+
+		img, release, err := camera.Next(ctx)
+		if err != nil {
+			return nil, data.FailedToReadErr(params.ComponentName, next.String(), err)
+		}
+		defer func() {
+			if release != nil {
+				release()
+			}
+		}()
+
+		// choose the best/fastest representation
+		mimeType := params.MethodParams["mime_type"]
+		if mimeType == "" || mimeType == utils.MimeTypeViamBest {
+			mimeType = utils.MimeTypeRawRGBA
+		}
+
+		bounds := img.Bounds()
+		resp := pb.GetFrameResponse{
+			MimeType: mimeType,
+			WidthPx:  int64(bounds.Dx()),
+			HeightPx: int64(bounds.Dy()),
+		}
+		outBytes, err := rimage.EncodeImage(ctx, img, mimeType)
+		if err != nil {
+			return nil, err
+		}
+		resp.Image = outBytes
+		return &resp, nil
 	})
 	return data.NewCollector(cFunc, params)
 }
