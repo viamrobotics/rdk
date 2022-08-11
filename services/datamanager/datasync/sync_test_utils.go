f67313daa25f0285a92eb7e41224b74e72a7774e
Max Horowitz <maxhorowitz@gmail.com>
2022-08-11 16:27:34 -0400

    Resolved.

--- services/datamanager/datasync/sync_test_utils.go
+++ services/datamanager/datasync/sync_test_utils.go
@@ -25,10 +25,11 @@ import (
 )
 
 var (
-	partID        = "partid"
-	componentType = "componenttype"
-	componentName = "componentname"
-	methodName    = "methodname"
+	partID         = "partid"
+	componentType  = "componenttype"
+	componentName  = "componentname"
+	componentModel = "componentmodel"
+	methodName     = "methodname"
 )
 
 // Compares UploadRequests containing either binary or tabular sensor data.
@@ -68,6 +69,7 @@ func compareMetadata(t *testing.T, actualMetadata *v1.UploadMetadata,
 	test.That(t, filepath.Base(actualMetadata.FileName), test.ShouldEqual, filepath.Base(expectedMetadata.FileName))
 	test.That(t, actualMetadata.PartId, test.ShouldEqual, expectedMetadata.PartId)
 	test.That(t, actualMetadata.ComponentName, test.ShouldEqual, expectedMetadata.ComponentName)
+	test.That(t, actualMetadata.ComponentModel, test.ShouldEqual, expectedMetadata.ComponentModel)
 	test.That(t, actualMetadata.ComponentType, test.ShouldEqual, expectedMetadata.ComponentType)
 	test.That(t, actualMetadata.MethodName, test.ShouldEqual, expectedMetadata.MethodName)
 	test.That(t, actualMetadata.Type, test.ShouldEqual, expectedMetadata.Type)
