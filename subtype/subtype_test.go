ddb758057d96d976415c5b34bfd8832d60584109
Nicolas Menard <nicolas.p.menard@gmail.com>
2022-08-12 11:20:48 -0400

    Allow access to remote resource using the short name

--- subtype/subtype_test.go
+++ subtype/subtype_test.go
@@ -43,3 +43,67 @@ func TestSubtypeService(t *testing.T) {
 	test.That(t, svc.Resource(name1), test.ShouldBeNil)
 	test.That(t, svc.Resource(name2), test.ShouldBeNil)
 }
+
+func TestSubtypeRemoteNames(t *testing.T) {
+	strType := resource.SubtypeName("string")
+	name0 := "name0"
+	name1 := "remote1:name1"
+	name2 := "remote2:name2"
+	name3 := "remote2:remote1:name1"
+	name4 := "remote2:remote1:name4"
+	name5 := "remote2:remote1:name5name6"
+	resources := map[resource.Name]interface{}{
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name0,
+		): name0,
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name1,
+		): name1,
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name2,
+		): name2,
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name3,
+		): name3,
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name4,
+		): name4,
+		resource.NewName(
+			resource.ResourceNamespaceRDK,
+			resource.ResourceTypeComponent,
+			strType,
+			name5,
+		): name5,
+	}
+	svc, err := subtype.New(resources)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, svc.Resource(name1), test.ShouldEqual, name1)
+	test.That(t, svc.Resource(name2), test.ShouldEqual, name2)
+	test.That(t, svc.Resource(name3), test.ShouldEqual, name3)
+	test.That(t, svc.Resource(name4), test.ShouldEqual, name4)
+
+	test.That(t, svc.Resource("name2"), test.ShouldEqual, name2)
+	test.That(t, svc.Resource("remote1:name2"), test.ShouldBeNil)
+	test.That(t, svc.Resource("remote2:name2"), test.ShouldEqual, name2)
+	test.That(t, svc.Resource("name1"), test.ShouldBeNil)
+	test.That(t, svc.Resource("name4"), test.ShouldEqual, name4)
+	test.That(t, svc.Resource("remote1:name3"), test.ShouldBeNil)
+	test.That(t, svc.Resource("remote1:name3"), test.ShouldBeNil)
+	test.That(t, svc.Resource("name5"), test.ShouldBeNil)
+	test.That(t, svc.Resource("name6"), test.ShouldBeNil)
+}
