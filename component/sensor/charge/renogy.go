13c0e2aaabcef6a3395cb1fed6c478cdd40676d9
Matt Vella <mcvella@gmail.com>
2022-08-18 23:10:57 -0400

    reading registers, yet to test

--- component/sensor/charge/renogy.go
+++ component/sensor/charge/renogy.go
@@ -3,6 +3,8 @@ package charge
 
 import (
 	"context"
+	"encoding/binary"
+	"math"
 	"sync"
 	"time"
 
@@ -40,9 +42,9 @@ type charge struct {
 	LoadAmp           float32
 	LoadWatt          float32
 	BattVolt          float32
-	BattChargePct     uint
-	BattDegC          float32
-	ControllerDegC    float32
+	BattChargePct     float32
+	BattDegC          int16
+	ControllerDegC    int16
 	MaxSolarTodayWatt float32
 	MinSolarTodayWatt float32
 	MaxBattTodayVolt  float32
@@ -88,8 +90,17 @@ type Sensor struct {
 	generic.Unimplemented
 }
 
-// ReadTemperatureCelsius returns current temperature in celsius.
-func (s *Sensor) ReadFromController(ctx context.Context) (charge, error) {
+// GetReadings returns a list containing single item (current temperature).
+func (s *Sensor) GetReadings(ctx context.Context) ([]interface{}, error) {
+	readings, err := s.GetControllerOutput(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return []interface{}{readings}, nil
+}
+
+// GetControllerOutput returns current readings from the charge controller
+func (s *Sensor) GetControllerOutput(ctx context.Context) (charge, error) {
 	var chargeRes charge
 	handler := modbus.NewRTUClientHandler(s.path)
 	handler.BaudRate = s.baud
@@ -105,16 +116,49 @@ func (s *Sensor) ReadFromController(ctx context.Context) (charge, error) {
 		return chargeRes, err
 	}
 	client := modbus.NewClient(handler)
-	results, err := client.ReadDiscreteInputs(15, 2)
+	chargeRes.SolarVolt = readRegister(client, 263, 1)
+	chargeRes.SolarAmp = readRegister(client, 264, 2)
+	chargeRes.SolarWatt = readRegister(client, 265, 1)
+	chargeRes.LoadVolt = readRegister(client, 260, 1)
+	chargeRes.LoadAmp = readRegister(client, 261, 2)
+	chargeRes.LoadWatt = readRegister(client, 262, 1)
+	chargeRes.BattVolt = readRegister(client, 257, 1)
+	chargeRes.BattChargePct = readRegister(client, 256, 1)
+	tempReading := readRegister(client, 259, 1)
+	battTempSign := (int16(tempReading) & 0b0000000010000000) >> 7
+	battTemp := int16(tempReading) & 0b0000000001111111
+	if battTempSign == 1 {
+		battTemp = -battTemp
+	}
+	chargeRes.BattDegC = battTemp
+	ctlTempSign := (int32(tempReading) & 0b1000000000000000) >> 15
+	ctlTemp := (int16(tempReading) & 0b0111111100000000) >> 8
+	if ctlTempSign == 1 {
+		ctlTemp = -ctlTemp
+	}
+	chargeRes.ControllerDegC = ctlTemp
+	chargeRes.MaxSolarTodayWatt = readRegister(client, 271, 1)
+	chargeRes.MinSolarTodayWatt = readRegister(client, 272, 1)
+	chargeRes.MaxBattTodayVolt = readRegister(client, 268, 1)
+	chargeRes.MinBattTodayVolt = readRegister(client, 267, 1)
 
 	return chargeRes, nil
 }
 
-// GetReadings returns a list containing single item (current temperature).
-func (s *Sensor) GetReadings(ctx context.Context) ([]interface{}, error) {
-	readings, err := s.ReadFromController(ctx)
+func readRegister(client modbus.Client, register uint16, precision uint) (result float32) {
+	b, err := client.ReadHoldingRegisters(register, 1)
 	if err != nil {
-		return nil, err
+		result = 0
+	} else {
+		result = float32frombytes(b, precision)
 	}
-	return []interface{}{readings}, nil
+	return result
+}
+
+func float32frombytes(bytes []byte, precision uint) float32 {
+	bits := binary.LittleEndian.Uint32(bytes)
+	float := math.Float32frombits(bits)
+	ratio := math.Pow(10, float64(precision))
+	float = float32(math.Round(float64(float)*ratio) / ratio)
+	return float
 }
