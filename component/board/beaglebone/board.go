ee3a287c1c7d2b5b7624a62c42cd7b640a1e2950
randhid <rand@viam.com>
2022-08-22 12:41:44 -0400

    Clean files
    Add tests

--- component/board/beaglebone/board.go
+++ component/board/beaglebone/board.go
@@ -1,5 +1,5 @@
-// Package bb implements a beaglebone based board.
-package bb
+// Package beaglebone implements a beaglebone based board.
+package beaglebone
 
 import (
 	"github.com/pkg/errors"
@@ -22,5 +22,20 @@ func init() {
 		rlog.Logger.Debugw("error getting beaglebone GPIO board mapping", "error", err)
 	}
 
-	commonsysfs.RegisterBoard(modelName, gpioMappings)
+	rlog.Logger.Info(
+		"beaglebone gpio mapping uses directory sys/devices/platform/bus@100000/*.gpio",
+		"\t\t\t\tbeaglebone 600000.gpio/ corresponds to gpiochip1 and /sys/class/gpio/gpiochip300/\n",
+		"\t\t\t\tbeaglebone 601000.gpio/ corresponds to gpiochip2 and /sys/class/gpio/gpiochip264/\n",
+	)
+
+	rlog.Logger.Info(
+		"beaglebone has 3 pwm chips for the following pins:\n",
+		"\t\t\t\t/sys/devices/platform/bus@100000/3000000.pwm/pwm/pwmchip0 for P8_13 and P8_19\n",
+		"\t\t\t\t/sys/devices/platform/bus@100000/3020000.pwm/pwm/pwmchip4 for P9_14 and P9_16\n",
+		"\t\t\t\t/sys/devices/platform/bus@100000/3010000.pwm/pwm/pwmchip2 for P9_21 and P9_22\n",
+	)
+
+	if err == nil {
+		commonsysfs.RegisterBoard(modelName, gpioMappings)
+	}
 }
