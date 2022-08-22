a6dbf925196927126ba3a90d9d4d691d75a6366e
Matt Vella <mcvella@gmail.com>
2022-08-22 13:11:46 -0400

    register and properly convert to floats

--- component/sensor/register/register.go
+++ component/sensor/register/register.go
@@ -3,6 +3,7 @@ package register
 
 import (
 	// for Sensors.
+	_ "go.viam.com/rdk/component/sensor/charge"
 	_ "go.viam.com/rdk/component/sensor/ds18b20"
 	_ "go.viam.com/rdk/component/sensor/fake"
 	_ "go.viam.com/rdk/component/sensor/ultrasonic"
