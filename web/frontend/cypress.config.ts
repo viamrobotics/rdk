3ffd69caf31963e804dac2635b8dd60510240d86
Devin T. Currie <devin@viam.com>
2022-08-25 16:52:44 -0400

    Update runner

--- web/frontend/cypress.config.ts
+++ web/frontend/cypress.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
-    taskTimeout: 60000,
     
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
