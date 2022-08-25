c13e2dcc8bc5cb5bbe550ecbb808789c84149c9b
Devin T. Currie <devin@viam.com>
2022-08-25 16:41:48 -0400

    Update runner

--- etc/e2e.sh
+++ etc/e2e.sh
@@ -7,7 +7,7 @@ cd $ROOT_DIR
 
 # Run server with test config
 
-sudo nohup ./bin/test-e2e/server --config web/frontend/cypress/data/test_robot_config.json &
+nohup ./bin/test-e2e/server --config web/frontend/cypress/data/test_robot_config.json &
 
 # Wait for interface to be live before running tests
 until nc -vz 127.0.0.1 8080; do sleep 2; done
@@ -15,7 +15,7 @@ until nc -vz 127.0.0.1 8080; do sleep 2; done
 # Run tests
 
 cd web/frontend
-npm run cypress:ci -- -c defaultCommandTimeout=10000
+npm run cypress:ci -- -c defaultCommandTimeout=20000
 
 # Teardown
 
