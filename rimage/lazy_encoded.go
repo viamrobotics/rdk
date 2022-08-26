3c5dcc28825f40e1b7a9594fd1aefce9bf169e6f
Eric Daniels <eric@erdaniels.com>
2022-08-26 12:43:24 -0400

    more tests

--- rimage/lazy_encoded.go
+++ rimage/lazy_encoded.go
@@ -20,7 +20,7 @@ type LazyEncodedImage struct {
 }
 
 // NewLazyEncodedImage returns a new image that will only get decoded once actual data is needed
-// from it. This is helpful for zero copy scenarios. If a width or height of less is unknown,
+// from it. This is helpful for zero copy scenarios. If a width or height of the image is unknown,
 // pass 0 or -1; when done a decode will happen on Bounds. In the future this can probably go
 // away with reading all metadata from the header of the image bytes.
 // NOTE: Usage of an image that would fail to decode causes a lazy panic.
