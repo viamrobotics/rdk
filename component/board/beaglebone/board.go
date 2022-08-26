9a8088d9895791be45a04368c73a8f80e69ab813
randhid <35934754+randhid@users.noreply.github.com>
2022-08-26 11:46:00 -0400

    Remove beaglebone logger info from init (#1228)
    * Remove sysfs information from debug logger
    
    * Register board model always

--- component/board/beaglebone/board.go
+++ component/board/beaglebone/board.go
@@ -22,20 +22,5 @@ func init() {
 		rlog.Logger.Debugw("error getting beaglebone GPIO board mapping", "error", err)
 	}
 
-	rlog.Logger.Debug(
-		"beaglebone gpio mapping uses directory sys/devices/platform/bus@100000/*.gpio",
-		"\t\t\t\tbeaglebone 600000.gpio/ (128 lines) corresponds to gpiochip1 and /sys/class/gpio/gpiochip300/\n",
-		"\t\t\t\tbeaglebone 601000.gpio/ (36 lines) corresponds to gpiochip2 and /sys/class/gpio/gpiochip264/\n",
-	)
-
-	rlog.Logger.Debug(
-		"beaglebone has 3 pwm chips for the following pins:\n",
-		"\t\t\t\t/sys/devices/platform/bus@100000/3000000.pwm/pwm/pwmchip0 for P8_13 and P8_19\n",
-		"\t\t\t\t/sys/devices/platform/bus@100000/3020000.pwm/pwm/pwmchip4 for P9_14 and P9_16\n",
-		"\t\t\t\t/sys/devices/platform/bus@100000/3010000.pwm/pwm/pwmchip2 for P9_21 and P9_22\n",
-	)
-
-	if err == nil {
-		commonsysfs.RegisterBoard(modelName, gpioMappings)
-	}
+	commonsysfs.RegisterBoard(modelName, gpioMappings)
 }
