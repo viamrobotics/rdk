280b9551965b9f7a44af7ca08090c36ae917066b
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2022-09-07 11:14:39 -0400

    add default navigation service

---
+++ services/navigation/register/register.go
@@ -0,0 +1,7 @@
+// Package register registers all relevant cameras and also subtype specific functions
+package register
+
+import (
+	// for cameras.
+	_ "go.viam.com/rdk/services/navigation/defaultnavigation"
+)
