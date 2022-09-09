cda7ff88fad2136846065fb83cfb79d30302b06c
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2022-09-09 11:11:33 -0400

    change behavior so the remote name is used to create a client if a match is found.

--- grpc/client/client_test.go
+++ grpc/client/client_test.go
@@ -1462,9 +1462,9 @@ func TestRemoteClientMatch(t *testing.T) {
 	)
 	test.That(t, err, test.ShouldBeNil)
 
-	client.ResourceByName(arm.Named("arm1"))
 	resource1, err := client.ResourceByName(arm.Named("arm1"))
 	test.That(t, err, test.ShouldBeNil)
+	test.That(t, client.resourceClients[arm.Named("remote:arm1")], test.ShouldEqual, resource1)
 	pos, err := resource1.(arm.Arm).GetEndPosition(context.Background(), nil)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, pos.String(), test.ShouldResemble, pose1.String())
@@ -1508,9 +1508,9 @@ func TestRemoteClientDuplicate(t *testing.T) {
 	)
 	test.That(t, err, test.ShouldBeNil)
 
-	client.ResourceByName(arm.Named("arm1"))
 	resource1, err := client.ResourceByName(arm.Named("arm1"))
 	test.That(t, err, test.ShouldBeNil)
+	test.That(t, client.resourceClients[arm.Named("arm1")], test.ShouldEqual, resource1)
 	pos, err := resource1.(arm.Arm).GetEndPosition(context.Background(), nil)
 	test.That(t, err.Error(), test.ShouldEqual, "rpc error: code = Unknown desc = no arm with name (arm1)")
 	test.That(t, pos, test.ShouldBeNil)
