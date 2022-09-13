dba0fef3d461e0ea1a2ef1ab3288a665d497d67a
randhid <rand@viam.com>
2022-09-13 15:46:53 -0400

    Refactor, add posetracker

---
+++ components/movementsensor/posetracker/posetracker.go
@@ -0,0 +1,44 @@
+// package posetracker implements a wrapper for a posetracker for the movement sensor interface
+package posetracker
+
+import (
+	"context"
+
+	"github.com/edaniels/golog"
+
+	"go.viam.com/rdk/components/generic"
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/referenceframe"
+	"go.viam.com/rdk/registry"
+)
+
+func init() {
+	registry.RegisterComponent(
+		movementsensor.Subtype,
+		"posetracker",
+		registry.Component{Constructor: func(
+			ctx context.Context,
+			_ registry.Dependencies,
+			config config.Component,
+			logger golog.Logger,
+		) (interface{}, error) {
+			return newPoseTracker(ctx, config, logger)
+		}})
+}
+
+func newPoseTracker(
+	ctx context.Context,
+	config config.Component,
+	logger golog.Logger,
+) (interface{}, error) {
+	return &posetracker{}, nil
+}
+
+type BodyToPoseInFrame map[string]*referenceframe.PoseInFrame
+
+type posetracker struct {
+	sensor.Sensor
+	generic.Generic
+}
