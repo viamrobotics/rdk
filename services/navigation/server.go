7c62e7f7e03db8c5501ab08184f629a89722e183
Cheuk Tse <cheuk@viam.com>
2022-09-16 17:58:26 -0400

    navigation service

--- services/navigation/server.go
+++ services/navigation/server.go
@@ -43,7 +43,7 @@ func (server *subtypeServer) GetMode(ctx context.Context, req *pb.GetModeRequest
 	if err != nil {
 		return nil, err
 	}
-	mode, err := svc.GetMode(ctx)
+	mode, err := svc.Mode(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -83,14 +83,14 @@ func (server *subtypeServer) SetMode(ctx context.Context, req *pb.SetModeRequest
 	return &pb.SetModeResponse{}, nil
 }
 
-func (server *subtypeServer) GetLocation(ctx context.Context, req *pb.GetLocationRequest) (
+func (server *subtypeServer) Location(ctx context.Context, req *pb.GetLocationRequest) (
 	*pb.GetLocationResponse, error,
 ) {
 	svc, err := server.service(req.Name)
 	if err != nil {
 		return nil, err
 	}
-	loc, err := svc.GetLocation(ctx)
+	loc, err := svc.Location(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -106,7 +106,7 @@ func (server *subtypeServer) GetWaypoints(ctx context.Context, req *pb.GetWaypoi
 	if err != nil {
 		return nil, err
 	}
-	waypoints, err := svc.GetWaypoints(ctx)
+	waypoints, err := svc.Waypoints(ctx)
 	if err != nil {
 		return nil, err
 	}
