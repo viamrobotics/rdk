1b1f3de41c6a497396d4a12673441f5839688f68
Nicolas Menard <nicolas.p.menard@gmail.com>
2022-09-16 11:47:57 -0400

    [RSDK-276] Add function to remove digital interrupts callbacks (#1323)

--- components/input/gpio/gpio.go
+++ components/input/gpio/gpio.go
@@ -228,6 +228,7 @@ func (c *Controller) newButton(ctx context.Context, brd board.Board, intName str
 
 	c.activeBackgroundWorkers.Add(1)
 	utils.ManagedGo(func() {
+		defer interrupt.RemoveCallback(intChan)
 		debounced := debounce.New(time.Millisecond * time.Duration(cfg.DebounceMs))
 		for {
 			var val bool
