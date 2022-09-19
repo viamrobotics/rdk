fd2a0e3409e3cfe9e4b716099e9e89775bdea589
Tess Avitabile <tess.avitabile@gmail.com>
2022-09-19 10:59:44 -0400

    combine into single server

--- services/slam/orbslam_int_test.go
+++ services/slam/orbslam_int_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"strconv"
 	"sync/atomic"
 	"testing"
@@ -21,72 +22,101 @@ import (
 	"go.viam.com/rdk/utils"
 )
 
-var (
-	colorIndex uint64
-	depthIndex uint64
-)
+var numImages uint64 = 15
 
-// Creates a mock camera server that serves 'pattern' using the files in 'path'.
-// Assumes the files are named 0.png, 1.png, 2.png, etc.
-//nolint:dupl
-func getMockCameraServer(t *testing.T, path, pattern string) *http.ServeMux {
+// Creates a mock camera server that serves the color and depth images in slam/temp_mock_camera.
+// The color and depth handles each signal to the returned channel when done.
+func getMockCameraServer(t *testing.T) (*httptest.Server, <-chan int) {
 	t.Helper()
+	out := make(chan int, 2)
+	var colorIndex uint64
+	var depthIndex uint64
 
-	var index uint64
-
-	handle := func(w http.ResponseWriter, r *http.Request) {
-		i := atomic.AddUint64(&index, 1) - 1
-		t.Logf("Handle called with pattern %v and image %v", pattern, i)
-		if pattern == "/color.png" {
-			test.That(t, i, test.ShouldEqual, atomic.LoadUint64(&colorIndex))
-		} else {
-			test.That(t, i, test.ShouldEqual, atomic.LoadUint64(&depthIndex))
+	colorHandle := func(w http.ResponseWriter, r *http.Request) {
+		i := atomic.AddUint64(&colorIndex, 1) - 1
+		t.Logf("Color handle called with image %v", i)
+		if i >= numImages {
+			t.Logf("Color handle no more images")
+			w.WriteHeader(http.StatusNotFound)
+			return
 		}
-
-		path := artifact.MustPath(path + strconv.FormatUint(i, 10) + ".png")
+		path := artifact.MustPath("slam/temp_mock_camera/color/" + strconv.FormatUint(i, 10) + ".png")
 		bytes, err := os.ReadFile(path)
-		test.That(t, err, test.ShouldBeNil)
+		if err != nil {
+			t.Logf("Unexpected error in color handle with image %v: %v", i, err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 		w.Write(bytes)
+		if i >= numImages-1 {
+			t.Logf("Color handle reached end of images")
+			out <- 1
+		}
+	}
+
+	depthHandle := func(w http.ResponseWriter, r *http.Request) {
+		i := atomic.AddUint64(&depthIndex, 1) - 1
+		t.Logf("Depth handle called with image %v", i)
+		if i >= numImages {
+			t.Logf("Depth handle no more images")
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		path := artifact.MustPath("slam/temp_mock_camera/depth/" + strconv.FormatUint(i, 10) + ".png")
+		bytes, err := os.ReadFile(path)
+		if err != nil {
+			t.Logf("Unexpected error in depth handle with image %v: %v", i, err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.Write(bytes)
+		if i >= numImages-1 {
+			t.Logf("Depth handle reached end of images")
+			out <- 1
+		}
 	}
 
 	router := http.NewServeMux()
-	router.HandleFunc(pattern, handle)
-	return router
-}
-
-//nolint:dupl
-func getMockDepthCameraServer(t *testing.T, path, pattern string) *http.ServeMux {
-	t.Helper()
-
-	var index2 uint64
-
-	handle := func(w http.ResponseWriter, r *http.Request) {
-		i := atomic.AddUint64(&index2, 1) - 1
-		t.Logf("Handle called with pattern %v and image %v", pattern, i)
-		if pattern == "/color.png" {
-			test.That(t, i, test.ShouldEqual, atomic.LoadUint64(&colorIndex))
-		} else {
-			test.That(t, i, test.ShouldEqual, atomic.LoadUint64(&depthIndex))
-		}
-
-		path := artifact.MustPath(path + strconv.FormatUint(i, 10) + ".png")
-		bytes, err := os.ReadFile(path)
-		test.That(t, err, test.ShouldBeNil)
-		w.Write(bytes)
-	}
-
-	router := http.NewServeMux()
-	router.HandleFunc(pattern, handle)
-	return router
-}
-
-// Creates a mock color camera for orbslam integration testing. The returned Server must be
-// closed when the test is finished.
-func getMockColorCamera(t *testing.T, logger golog.Logger) (camera.Camera, *httptest.Server) {
-	t.Helper()
-
-	router := getMockCameraServer(t, "slam/temp_mock_camera/color/", "/color.png")
+	router.HandleFunc("/color.png", colorHandle)
+	router.HandleFunc("/depth.png", depthHandle)
 	svr := httptest.NewServer(router)
+	return svr, out
+}
+
+// Tests that the next image read from the camera is image i in the directory.
+// If not, checks other images in the directory for a match.
+func testImage(t *testing.T, cam camera.Camera, dir string, i int) {
+	t.Helper()
+	img, release, err := camera.ReadImage(
+		gostream.WithMIMETypeHint(context.Background(), utils.WithLazyMIMEType(utils.MimeTypePNG)), cam)
+	test.That(t, err, test.ShouldBeNil)
+	defer release()
+	lazyImg, ok := img.(*rimage.LazyEncodedImage)
+	test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, lazyImg.MIMEType(), test.ShouldEqual, utils.MimeTypePNG)
+	path := artifact.MustPath(dir + strconv.Itoa(i) + ".png")
+	expectedBytes, err := os.ReadFile(path)
+	test.That(t, err, test.ShouldBeNil)
+	if !reflect.DeepEqual(lazyImg.RawData(), expectedBytes) {
+		t.Logf("Expected read from camera to match image %v in directory %v", i, dir)
+		for j := 0; j < int(numImages); j++ {
+			jPath := artifact.MustPath(dir + strconv.Itoa(j) + ".png")
+			jBytes, err := os.ReadFile(jPath)
+			test.That(t, err, test.ShouldBeNil)
+			if reflect.DeepEqual(lazyImg.RawData(), jBytes) {
+				t.Logf("Camera returned image %v instead of image %v from directory %v", j, i, dir)
+			}
+		}
+		test.That(t, false, test.ShouldBeTrue)
+	}
+}
+
+func TestMockCameraServer(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	svr, done := getMockCameraServer(t)
+	defer svr.Close()
+
+	// Create color camera
 	intrinsics := &transform.PinholeCameraIntrinsics{
 		Width:  1280,
 		Height: 720,
@@ -95,80 +125,51 @@ func getMockColorCamera(t *testing.T, logger golog.Logger) (camera.Camera, *http
 		Ppx:    100,
 		Ppy:    100,
 	}
-	attrs := videosource.ServerAttrs{
+	colorAttrs := videosource.ServerAttrs{
 		URL: svr.URL + "/color.png",
 		AttrConfig: &camera.AttrConfig{
 			CameraParameters: intrinsics,
 			Stream:           "color",
 		},
 	}
-	cam, err := videosource.NewServerSource(context.Background(), &attrs, logger)
+	colorCam, err := videosource.NewServerSource(context.Background(), &colorAttrs, logger)
 	test.That(t, err, test.ShouldBeNil)
-	return cam, svr
-}
 
-// Creates a mock depth camera for orbslam integration testing. The returned Server must be
-// closed when the test is finished.
-func getMockDepthCamera(t *testing.T, logger golog.Logger) (camera.Camera, *httptest.Server) {
-	t.Helper()
-
-	router := getMockDepthCameraServer(t, "slam/temp_mock_camera/depth/", "/depth.png")
-	svr := httptest.NewServer(router)
-	attrs := videosource.ServerAttrs{
+	// Create depth camera
+	depthAttrs := videosource.ServerAttrs{
 		URL: svr.URL + "/depth.png",
 		AttrConfig: &camera.AttrConfig{
 			Stream: "depth",
 		},
 	}
-	cam, err := videosource.NewServerSource(context.Background(), &attrs, logger)
+	depthCam, err := videosource.NewServerSource(context.Background(), &depthAttrs, logger)
 	test.That(t, err, test.ShouldBeNil)
-	return cam, svr
-}
 
-// Tests that the next image read from the camera is the same as the image stored at 'path'.
-func testImage(t *testing.T, cam camera.Camera, path string) {
-	t.Helper()
-
-	img, release, err := camera.ReadImage(
-		gostream.WithMIMETypeHint(context.Background(), utils.WithLazyMIMEType(utils.MimeTypePNG)), cam)
+	// Check color intrinsics
+	proj, err := colorCam.Projector(context.Background())
 	test.That(t, err, test.ShouldBeNil)
-	defer release()
-	lazyImg, ok := img.(*rimage.LazyEncodedImage)
+	intrinsics, ok := proj.(*transform.PinholeCameraIntrinsics)
 	test.That(t, ok, test.ShouldBeTrue)
-	test.That(t, lazyImg.MIMEType(), test.ShouldEqual, utils.MimeTypePNG)
-	_, err = os.ReadFile(path)
-	test.That(t, err, test.ShouldBeNil)
-}
+	test.That(t, intrinsics.CheckValid(), test.ShouldBeNil)
 
-func TestMockCameraServer(t *testing.T) {
-	logger := golog.NewTestLogger(t)
+	// Check images
+	for i := 0; i < int(numImages); i++ {
+		t.Logf("Test color image %v", i)
+		testImage(t, colorCam, "slam/temp_mock_camera/color/", i)
 
-	t.Run("Color camera", func(t *testing.T) {
-		colorCam, colorSvr := getMockColorCamera(t, logger)
-		defer colorSvr.Close()
+		t.Logf("Test depth image %v", i)
+		testImage(t, depthCam, "slam/temp_mock_camera/depth/", i)
+	}
 
-		proj, err := colorCam.Projector(context.Background())
-		test.That(t, err, test.ShouldBeNil)
-		intrinsics, ok := proj.(*transform.PinholeCameraIntrinsics)
-		test.That(t, ok, test.ShouldBeTrue)
-		test.That(t, intrinsics.CheckValid(), test.ShouldBeNil)
+	// Both color and depth should be done.
+	<-done
+	<-done
 
-		for i := 0; i < 15; i++ {
-			t.Logf("Test color image %v", i)
-			path := artifact.MustPath("slam/temp_mock_camera/color/" + strconv.Itoa(i) + ".png")
-			testImage(t, colorCam, path)
-			t.Logf("Test iteration %v incremented colorIndex to %v", i, atomic.AddUint64(&colorIndex, 1))
-		}
-	})
-
-	t.Run("Depth camera", func(t *testing.T) {
-		depthCam, depthSvr := getMockDepthCamera(t, logger)
-		defer depthSvr.Close()
-		for i := 0; i < 15; i++ {
-			t.Logf("Test depth image %v", i)
-			path := artifact.MustPath("slam/temp_mock_camera/depth/" + strconv.Itoa(i) + ".png")
-			testImage(t, depthCam, path)
-			t.Logf("Test iteration %v incremented depthIndex to %v", i, atomic.AddUint64(&depthIndex, 1))
-		}
-	})
+	// Check error responses
+	_, _, err = camera.ReadImage(
+		gostream.WithMIMETypeHint(context.Background(), utils.WithLazyMIMEType(utils.MimeTypePNG)), colorCam)
+	test.That(t, err, test.ShouldNotBeNil)
+	_, _, err = camera.ReadImage(
+		gostream.WithMIMETypeHint(context.Background(), utils.WithLazyMIMEType(utils.MimeTypePNG)), depthCam)
+	test.That(t, err, test.ShouldNotBeNil)
 }
