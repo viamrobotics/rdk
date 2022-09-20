5ab0924658908264e48fad7170b4c177415b27c2
Cheuk Tse <cheuk@viam.com>
2022-09-20 19:04:04 -0400

    lint

--- robot/client/doc.go
+++ robot/client/doc.go
@@ -9,8 +9,8 @@ To install the Go SDK, run
 
 # Basic Usage
 
-The easiest way to get started writing a client application is to navigate to the robot page on app.viam.com, select the CONNECT tab,
-and copy the boilerplate code from the section labeled Golang SDK.
+The easiest way to get started writing a client application is to navigate to the robot page on app.viam.com,
+select the CONNECT tab, and copy the boilerplate code from the section labeled Golang SDK.
 
 It is recommended that you save and run this simple program. Doing so will ensure that the Golang SDK is
 properly installed, that the viam-server instance on your robot is alive, and that the computer running
