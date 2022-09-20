9bb218398c8b22477b079dba66d632cdb7150af6
Cheuk Tse <cheuk@viam.com>
2022-09-20 18:59:57 -0400

    add some sdk docs

--- robot/client/doc.go
+++ robot/client/doc.go
@@ -1,5 +1,19 @@
 /*
-Package client implements a Go SDK for the .
+Package client implements a Go client for the Viam RDK and can function as an SDK to connect to a robot.
 
+# Install
+
+To install the Go SDK, run
+
+	go get go.viam.com/rdk/robot/client
+
+# Basic Usage
+
+The easiest way to get started writing a client application is to navigate to the robot page on app.viam.com, select the CONNECT tab,
+and copy the boilerplate code from the section labeled Golang SDK.
+
+It is recommended that you save and run this simple program. Doing so will ensure that the Golang SDK is
+properly installed, that the viam-server instance on your robot is alive, and that the computer running
+the program is able to connect to that instance.
 */
 package client
