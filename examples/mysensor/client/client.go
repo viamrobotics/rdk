adfcd1bf4eac31c7fc52d86c71684e414341765d
Cheuk Tse <cheuk@viam.com>
2022-09-21 17:14:02 -0400

    add new example

---
+++ examples/mysensor/client/client.go
@@ -0,0 +1,33 @@
+// Package main tests out the mySensor component type.
+package main
+
+import (
+	"context"
+
+	"github.com/edaniels/golog"
+
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/robot/client"
+)
+
+func main() {
+	logger := golog.NewDevelopmentLogger("client")
+	robot, err := client.New(
+		context.Background(),
+		"localhost:8080",
+		logger,
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	sensor, err := sensor.FromRobot(robot, "sensor1")
+	if err != nil {
+		logger.Error(err)
+	}
+	reading, err := sensor.Readings(context.Background())
+	if err != nil {
+		logger.Error(err)
+	}
+	logger.Info(reading)
+}
