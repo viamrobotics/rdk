0a0e103ca61c965b1a3c3c695fcfbc5c4e757607
mcvella <1242958+mcvella@users.noreply.github.com>
2022-09-26 13:06:13 -0400

    APP-718: make visionApi available on the window level for blockly (#1382)
    * make visionApi available on the window level for blockly

--- web/frontend/src/api.ts
+++ web/frontend/src/api.ts
@@ -35,6 +35,7 @@ import robotApi from './gen/proto/api/robot/v1/robot_pb.esm';
 import sensorsApi from './gen/proto/api/service/sensors/v1/sensors_pb.esm';
 import servoApi from './gen/proto/api/component/servo/v1/servo_pb.esm';
 import streamApi from './gen/proto/stream/v1/stream_pb.esm';
+import visionApi from './gen/proto/api/service/vision/v1/vision_pb.esm';
 
 /**
  * Every window variable on this page is being currently used by the blockly page in App.
@@ -55,6 +56,7 @@ window.motorApi = motorApi;
 window.sensorsApi = sensorsApi;
 window.servoApi = servoApi;
 window.streamApi = streamApi;
+window.visionApi = visionApi;
 
 /**
  * This window variable is used by the config page to access the discovery service.
