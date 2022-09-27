e7f31bceb14b2c2c5987a868b35f2353fb81b72c
aaroncasas <aaron@viam.com>
2022-09-27 13:01:55 -0400

    More yeetery

--- services/datamanager/datasync/upload_data_capture_file.go
+++ services/datamanager/datasync/upload_data_capture_file.go
@@ -2,12 +2,9 @@ package datasync
 
 import (
 	"context"
-	"github.com/pkg/errors"
 	v1 "go.viam.com/api/app/datasync/v1"
 	"go.viam.com/rdk/services/datamanager/datacapture"
-	"io"
 	"os"
-	"path/filepath"
 )
 
 func uploadDataCaptureFile(ctx context.Context, client v1.DataSyncServiceClient, f *datacapture.File, partID string) error {
@@ -36,43 +33,6 @@ func uploadDataCaptureFile(ctx context.Context, client v1.DataSyncServiceClient,
 	return nil
 }
 
-func initDataCaptureUpload(ctx context.Context, f *os.File, pt progressTracker, dcFileName string) error {
-	finfo, err := f.Stat()
-	if err != nil {
-		return err
-	}
-	// Get file progress to see if upload has been attempted. If yes, resume from upload progress point and if not,
-	// create an upload progress file.
-	progressFilePath := filepath.Join(pt.progressDir, filepath.Base(dcFileName))
-	progressIndex, err := pt.getProgressFileIndex(progressFilePath)
-	if errors.Is(err, os.ErrNotExist) {
-		if err := pt.createProgressFile(progressFilePath); err != nil {
-			return err
-		}
-		return nil
-	}
-	if err != nil {
-		return err
-	}
-
-	// Sets the next file pointer to the next sensordata message that needs to be uploaded.
-	for i := 0; i < progressIndex; i++ {
-		if _, err := getNextSensorUploadRequest(ctx, f); err != nil {
-			return err
-		}
-	}
-
-	// Check remaining data capture file contents so we know whether to continue upload process.
-	currentOffset, err := f.Seek(0, io.SeekCurrent)
-	if err != nil {
-		return err
-	}
-	if currentOffset == finfo.Size() {
-		return io.EOF
-	}
-	return nil
-}
-
 func getNextSensorUploadRequest(ctx context.Context, f *os.File) (*v1.UploadRequest, error) {
 	select {
 	case <-ctx.Done():
@@ -90,73 +50,4 @@ func getNextSensorUploadRequest(ctx context.Context, f *os.File) (*v1.UploadRequ
 			},
 		}, nil
 	}
-}
-
-func sendNextUploadRequest(ctx context.Context, f *os.File, stream v1.DataSyncService_UploadClient) error {
-	select {
-	case <-ctx.Done():
-		return context.Canceled
-	default:
-		// Get the next UploadRequest from the file.
-		uploadReq, err := getNextSensorUploadRequest(ctx, f)
-		if err != nil {
-			return err
-		}
-
-		if err = stream.Send(uploadReq); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func recvStream(ctx context.Context, stream v1.DataSyncService_UploadClient,
-	pt progressTracker, progressFile string,
-) error {
-	for {
-		recvChannel := make(chan error)
-		go func() {
-			defer close(recvChannel)
-			ur, err := stream.Recv()
-			if err != nil {
-				recvChannel <- err
-				return
-			}
-			if err := pt.updateProgressFileIndex(progressFile, int(ur.GetRequestsWritten())); err != nil {
-				recvChannel <- err
-				return
-			}
-		}()
-
-		select {
-		case <-ctx.Done():
-			return context.Canceled
-		case e := <-recvChannel:
-			if e != nil {
-				if !errors.Is(e, io.EOF) {
-					return e
-				}
-				return nil
-			}
-		}
-	}
-}
-
-func sendStream(ctx context.Context, stream v1.DataSyncService_UploadClient,
-	captureFile *os.File,
-) error {
-	// Loop until there is no more content to be read from file.
-	for {
-		err := sendNextUploadRequest(ctx, captureFile, stream)
-		if errors.Is(err, io.EOF) {
-			break
-		}
-		if err != nil {
-			return err
-		}
-	}
-	if err := stream.CloseSend(); err != nil {
-		return err
-	}
-	return nil
 }
