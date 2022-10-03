08cd1d574d7bed21c74369b71bc5a2d58481d280
Cheuk Tse <cheuk@viam.com>
2022-10-03 15:36:24 -0400

    update example

--- examples/mysensor/server/server.go
+++ examples/mysensor/server/server.go
@@ -7,15 +7,52 @@ import (
 	"github.com/edaniels/golog"
 	goutils "go.viam.com/utils"
 
+	"go.viam.com/rdk/components/generic"
+	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/config"
-	// import the custom sensor package to register it properly
-	_ "go.viam.com/rdk/examples/mysensor/mysensor"
+	"go.viam.com/rdk/registry"
+
 	robotimpl "go.viam.com/rdk/robot/impl"
 	"go.viam.com/rdk/robot/web"
 	"go.viam.com/rdk/utils"
 )
 
 var logger = golog.NewDevelopmentLogger("mysensor")
+
+// registering the component model on init is how we make sure the new model is picked up and usable.
+func init() {
+	registry.RegisterComponent(
+		sensor.Subtype,
+		"mySensor",
+		registry.Component{Constructor: func(
+			ctx context.Context,
+			deps registry.Dependencies,
+			config config.Component,
+			logger golog.Logger,
+		) (interface{}, error) {
+			return newSensor(config.Name), nil
+		}})
+}
+
+func newSensor(name string) sensor.Sensor {
+	return &mySensor{Name: name}
+}
+
+// this checks that the mySensor struct implements the sensor.Sensor interface.
+var _ = sensor.Sensor(&mySensor{})
+
+// mySensor is a sensor device that always returns "hello world".
+type mySensor struct {
+	Name string
+
+	// generic.Unimplemented is a helper that embeds an unimplemented error in the Do method.
+	generic.Unimplemented
+}
+
+// Readings always returns "hello world".
+func (s *mySensor) Readings(ctx context.Context) (map[string]interface{}, error) {
+	return map[string]interface{}{"hello": "world"}, nil
+}
 
 func main() {
 	goutils.ContextualMain(mainWithArgs, logger)
