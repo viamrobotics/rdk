1f7a271ed4dbd97226555c0bc6e9c18a9a7d4401
randhid <rand@viam.com>
2022-10-04 15:13:58 -0400

    Fix test logic

--- components/movementsensor/movementsensor.go
+++ components/movementsensor/movementsensor.go
@@ -131,32 +131,32 @@ func Readings(ctx context.Context, g MovementSensor) (map[string]interface{}, er
 	readings := map[string]interface{}{}
 
 	pos, altitide, err := g.Position(ctx)
-	if err != nil && errors.Is(err, ErrMethodUnimplementedPosition) {
+	if err != nil && !errors.Is(err, ErrMethodUnimplementedPosition) {
 		return nil, err
 	}
 	readings["position"] = pos
 	readings["altitide"] = altitide
 
 	vel, err := g.LinearVelocity(ctx)
-	if err != nil && errors.Is(err, ErrMethodUnimplementedLinearVelocity) {
+	if err != nil && !errors.Is(err, ErrMethodUnimplementedLinearVelocity) {
 		return nil, err
 	}
 	readings["linear_velocity"] = vel
 
 	avel, err := g.AngularVelocity(ctx)
-	if err != nil && errors.Is(err, ErrMethodUnimplementedAngularVelocity) {
+	if err != nil && !errors.Is(err, ErrMethodUnimplementedAngularVelocity) {
 		return nil, err
 	}
 	readings["angular_velocity"] = avel
 
 	compass, err := g.CompassHeading(ctx)
-	if err != nil && errors.Is(err, ErrMethodUnimplementedCompassHeading) {
+	if err != nil && !errors.Is(err, ErrMethodUnimplementedCompassHeading) {
 		return nil, err
 	}
 	readings["compass"] = compass
 
 	ori, err := g.Orientation(ctx)
-	if err != nil && errors.Is(err, ErrMethodUnimplementedOrientation) {
+	if err != nil && !errors.Is(err, ErrMethodUnimplementedOrientation) {
 		return nil, err
 	}
 	readings["orientation"] = ori
