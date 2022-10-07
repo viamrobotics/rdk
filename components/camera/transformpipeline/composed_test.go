d3189e92182063529e206a3e5170a75b2b4d4471
Bijan Haney <bijan@viam.com>
2022-10-07 16:27:30 -0400

    add tests

---
+++ components/camera/transformpipeline/composed_test.go
@@ -0,0 +1,11 @@
+package transformpipeline
+
+import "testing"
+
+func TestComposed(t *testing.T) {
+	// create pointcloud source
+	// get intrinsic parameters
+	// make transform pipeline
+	// expected result with correct config
+	// wrong result with bad config
+}
