8e3155c6216e10c81fc44e4b118be78254fe0869
Alexa Greenberg <alexa.greenberg@viam.com>
2022-10-07 10:36:04 -0400

    Add methods to movement sensor

--- components/movementsensor/movementsensor.go
+++ components/movementsensor/movementsensor.go
@@ -43,9 +43,24 @@ func init() {
 		},
 	})
 
-	registerCollector("GetPosition", func(ctx context.Context, ms MovementSensor) (interface{}, error) {
+	registerCollector("Position", func(ctx context.Context, ms MovementSensor) (interface{}, error) {
 		p, _, err := ms.Position(ctx)
 		return p, err
+	})
+	registerCollector("LinearVelocity", func(ctx context.Context, ms MovementSensor) (interface{}, error) {
+		v, err := ms.LinearVelocity(ctx)
+		return v, err
+	})
+	registerCollector("AngularVelocity", func(ctx context.Context, ms MovementSensor) (interface{}, error) {
+		v, err := ms.AngularVelocity(ctx)
+		return v, err
+	})
+	registerCollector("CompassHeading", func(ctx context.Context, ms MovementSensor) (interface{}, error) {
+		type Heading struct {
+			Heading float64
+		}
+		h, err := ms.CompassHeading(ctx)
+		return Heading{Heading: h}, err
 	})
 }
 
