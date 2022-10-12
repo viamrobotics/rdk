a8ed88c828e8bf37027358c31f9c4ad2f80c7270
Alan Davidson <alan@viam.com>
2022-10-12 17:48:18 -0400

    pr feedback

--- components/motor/gpio/basic.go
+++ components/motor/gpio/basic.go
@@ -129,10 +129,10 @@ func (m *Motor) setPWM(ctx context.Context, powerPct float64, extra map[string]i
 
 	if math.Abs(powerPct) <= 0.001 {
 		if m.EnablePinLow != nil {
-			errs = multierr.Combine(m.EnablePinLow.Set(ctx, true, extra))
+			errs = multierr.Combine(errs, m.EnablePinLow.Set(ctx, true, extra))
 		}
 		if m.EnablePinHigh != nil {
-			errs = multierr.Combine(m.EnablePinHigh.Set(ctx, false, extra))
+			errs = multierr.Combine(errs, m.EnablePinHigh.Set(ctx, false, extra))
 		}
 
 		if m.A != nil && m.B != nil {
