68da1e40bb1e252503f67b15fab63c90c1f5cb36
Clyde Bazile <clyde@viam.com>
2022-10-13 12:37:54 -0400

    nit

--- vision/segmentation/radius_clustering_voxel.go
+++ vision/segmentation/radius_clustering_voxel.go
@@ -36,7 +36,7 @@ func (rcc *RadiusClusteringVoxelConfig) CheckValid() error {
 	if rcc.Lambda <= 0 {
 		return errors.Errorf("lambda must be greater than 0, got %v", rcc.Lambda)
 	}
-	radiusClustering := RadiusClusteringConfig{rcc.MinPtsInPlane, rcc.MinPtsInSegment, rcc.ClusteringRadiusMm, 50.0, ""}
+	radiusClustering := RadiusClusteringConfig{rcc.MinPtsInPlane, rcc.MinPtsInSegment, rcc.ClusteringRadiusMm, 50.0, rcc.Label}
 	err := radiusClustering.CheckValid()
 	if err != nil {
 		return err
