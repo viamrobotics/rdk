7cb9cabb4212460ea98383e7214ded161ed2d7b8
Devin T. Currie <DTCurrie@users.noreply.github.com>
2022-10-17 15:33:12 -0400

    Expose fetch camera discovery function to resolve proto conflicts (#1478)

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import './rc/control_helpers';
 import '@viamrobotics/prime';
+import '@fontsource/space-mono/400.css';
+import '@fontsource/space-mono/400-italic.css';
+import '@fontsource/space-mono/700.css';
+import '@fontsource/space-mono/700-italic.css';
 import './index.css';
 import App from './app.vue';
 
