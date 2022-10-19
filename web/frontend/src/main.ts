1ac38597bf20d2aacc366db67478328110f3ec9c
mrloureed <mrloureed@gmail.com>
2022-10-19 10:20:53 -0400

    remove unnecessary imports for fonts

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from 'vue';
 import './rc/control_helpers';
 import '@viamrobotics/prime';
-import '@fontsource/space-mono/400.css';
-import '@fontsource/space-mono/400-italic.css';
-import '@fontsource/space-mono/700.css';
-import '@fontsource/space-mono/700-italic.css';
+import '@fontsource/space-mono';
 import './index.css';
 import App from './app.vue';
 
