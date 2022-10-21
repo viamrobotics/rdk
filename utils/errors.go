3e1f5ae2646302ed475ea6cf73c66d7e2356863a
Cheuk Tse <cheuk@viam.com>
2022-10-21 18:13:57 -0400

    changes

--- utils/errors.go
+++ utils/errors.go
@@ -11,6 +11,11 @@ func NewResourceNotFoundError(name resource.Name) error {
 	return errors.Errorf("resource %q not found", name)
 }
 
+// NewResourceGetError is used when a resource could not be retrieved because it did not start up properly.
+func NewResourceGetError(name resource.Name, err error) error {
+	return errors.Wrapf(err, "failed to get resource %q", name)
+}
+
 // NewRemoteResourceClashError is used when you are more than one resource with the same name exist.
 func NewRemoteResourceClashError(name string) error {
 	return errors.Errorf("more that one remote resources with name %q exists", name)
