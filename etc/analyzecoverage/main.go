ae044ee7b2d6d594fe61f529d687a36b86cd9665
Eric Daniels <eric@erdaniels.com>
2022-10-27 17:06:32 -0400

    ci: try fixing coverage; pt3

--- etc/analyzecoverage/main.go
+++ etc/analyzecoverage/main.go
@@ -46,6 +46,7 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 	profileDatas := bytes.Split(profileDataAll, []byte("mode: "))
 
 	gitSHA, _ := os.LookupEnv("GITHUB_HEAD_SHA")
+	repository, _ := os.LookupEnv("GITHUB_REPOSITORY")
 	var gitHubRunID, gitHubRunNumber, gitHubRunAttempt int64
 	gitHubRunIDStr, ok := os.LookupEnv("GITHUB_RUN_ID")
 	if ok {
@@ -141,6 +142,7 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 				CreatedAt:           createdAt,
 				GitSHA:              gitSHA,
 				GitBranch:           branchName,
+				GitHubRepository:    repository,
 				GitHubRunID:         gitHubRunID,
 				GitHubRunNumber:     gitHubRunNumber,
 				GitHubRunAttempt:    gitHubRunAttempt,
@@ -505,6 +507,7 @@ type coverageResult struct {
 	CreatedAt           time.Time `bson:"created_at"`
 	GitSHA              string    `bson:"git_sha,omitempty"`
 	GitBranch           string    `bson:"git_branch"`
+	GitHubRepository    string    `bson:"github_repository"`
 	GitHubRunID         int64     `bson:"github_run_id,omitempty"`
 	GitHubRunNumber     int64     `bson:"github_run_number,omitempty"`
 	GitHubRunAttempt    int64     `bson:"github_run_attempt,omitempty"`
