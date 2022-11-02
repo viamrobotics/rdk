61fcf25e692fb6254f10e7f375cf5d7a89867e6c
jeremyhyde-viam <92594844+jeremyhyde-viam@users.noreply.github.com>
2022-11-02 17:50:15 -0400

    map_rate_check

--- services/slam/builtin/orbslam_int_test.go
+++ services/slam/builtin/orbslam_int_test.go
@@ -245,7 +245,7 @@ func integrationTestHelper(t *testing.T, mode slam.Mode) {
 	// Test online mode using the map generated in the offline test
 	t.Log("Testing online mode with saved map")
 
-	mapRate = -1
+	mapRate = 9999
 
 	attrCfg = &builtin.AttrConfig{
 		Algorithm: "orbslamv3",
