3ebd29ec5e26c2c8f9e662f70b44b952883137d3
mrloureed <mrloureed@gmail.com>
2022-11-03 11:39:53 -0400

    remove font-source

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from 'vue';
 import '@viamrobotics/prime';
-import '@fontsource/space-mono/400.css';
-import '@fontsource/space-mono/400-italic.css';
-import '@fontsource/space-mono/700.css';
-import '@fontsource/space-mono/700-italic.css';
 import './index.css';
 import App from './app.vue';
 
