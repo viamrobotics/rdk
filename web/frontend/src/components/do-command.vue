eaecb24a7982bb3011cd9eb4576e116854024932
mrloureed <mrloureed@gmail.com>
2022-11-07 15:18:21 -0500

    build

--- web/frontend/src/components/do-command.vue
+++ web/frontend/src/components/do-command.vue
@@ -81,8 +81,9 @@ const namesToPrettySelect = (resourcesToPretty: Resource[]): string => {
         :value="selectedComponent"
         :disabled="executing ? 'true' : 'false'"
         @input="selectedComponent = $event.detail.value"
+        class="mb-4"
       />
-      <div class="flex h-full w-full flex-row gap-2">
+      <div class="flex flex-wrap h-full w-full flex-row gap-2">
         <div class="h-full w-full">
           <p class="text-large">
             Input
