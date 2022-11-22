bcbd9af688592249bbb48d9ebe753d585bd8d1ca
Peter LoVerso <biotinker@gmail.com>
2022-11-21 16:27:47 -0800

    Minor collision optimization

--- motionplan/constraint_test.go
+++ motionplan/constraint_test.go
@@ -201,7 +201,7 @@ func TestCollisionConstraint(t *testing.T) {
 	model, err := frame.ParseModelJSONFile(utils.ResolveFile("components/arm/xarm/xarm6_kinematics.json"), "")
 	test.That(t, err, test.ShouldBeNil)
 	handler := &constraintHandler{}
-	handler.AddConstraint("collision", NewCollisionConstraint(model, zeroPos, obstacles, map[string]spatial.Geometry{}, true))
+	handler.AddConstraint("collision", NewCollisionConstraint(model, zeroPos, obstacles, map[string]spatial.Geometry{}, false))
 
 	// loop through cases and check constraint handler processes them correctly
 	for i, c := range cases {
@@ -227,7 +227,7 @@ func BenchmarkCollisionConstraint(b *testing.B) {
 	model, err := frame.ParseModelJSONFile(utils.ResolveFile("components/arm/xarm/xarm6_kinematics.json"), "")
 	test.That(b, err, test.ShouldBeNil)
 	handler := &constraintHandler{}
-	handler.AddConstraint("collision", NewCollisionConstraint(model, zeroPos, obstacles, map[string]spatial.Geometry{}, true))
+	handler.AddConstraint("collision", NewCollisionConstraint(model, zeroPos, obstacles, map[string]spatial.Geometry{}, false))
 
 	rseed := rand.New(rand.NewSource(1))
 	var b1 bool
