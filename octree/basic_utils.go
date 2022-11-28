d022db290736dafb82f66f5a47b73fdd66d6954e
jeremyhyde-viam <92594844+jeremyhyde-viam@users.noreply.github.com>
2022-11-28 13:39:47 -0500

    valdiate octree structure

--- octree/basic_utils.go
+++ octree/basic_utils.go
@@ -42,44 +42,49 @@ func newLeafNodeFilled(p r3.Vector, d pc.Data) basicOctreeNode {
 // Splits a basic octree into multiple octants and will place any stored point in appropriate child
 // node. Note: splitIntoOctants should only be called when an octree is a LeafNodeFilled.
 func (octree *basicOctree) splitIntoOctants() error {
-	if octree.node.nodeType == InternalNode {
+	switch octree.node.nodeType {
+	case InternalNode:
 		return errors.New("error attempted to split internal node")
-	}
+	case LeafNodeEmpty:
+		return errors.New("error attempted to split empty leaf node")
+	case LeafNodeFilled:
 
-	children := []*basicOctree{}
-	newSideLength := octree.sideLength / 2
-	for _, i := range []float64{-1.0, 1.0} {
-		for _, j := range []float64{-1.0, 1.0} {
-			for _, k := range []float64{-1.0, 1.0} {
-				centerOffset := r3.Vector{
-					X: i * newSideLength / 2.,
-					Y: j * newSideLength / 2.,
-					Z: k * newSideLength / 2.,
-				}
-				newCenter := octree.center.Add(centerOffset)
+		children := []*basicOctree{}
+		newSideLength := octree.sideLength / 2
+		for _, i := range []float64{-1.0, 1.0} {
+			for _, j := range []float64{-1.0, 1.0} {
+				for _, k := range []float64{-1.0, 1.0} {
+					centerOffset := r3.Vector{
+						X: i * newSideLength / 2.,
+						Y: j * newSideLength / 2.,
+						Z: k * newSideLength / 2.,
+					}
+					newCenter := octree.center.Add(centerOffset)
 
-				// Create a new basic octree child
-				child := &basicOctree{
-					center:     newCenter,
-					sideLength: newSideLength,
-					size:       0,
-					logger:     octree.logger,
-					node:       newLeafNodeEmpty(),
-					meta:       pc.NewMetaData(),
+					// Create a new basic octree child
+					child := &basicOctree{
+						center:     newCenter,
+						sideLength: newSideLength,
+						size:       0,
+						logger:     octree.logger,
+						node:       newLeafNodeEmpty(),
+						meta:       pc.NewMetaData(),
+					}
+					children = append(children, child)
 				}
-				children = append(children, child)
 			}
 		}
-	}
 
-	// Extract potential data before redefining node as InternalNode with eight new children
-	// nodes
-	p := octree.node.point.P
-	d := octree.node.point.D
-	octree.node = newInternalNode(children)
-	octree.meta = pc.NewMetaData()
-	octree.size = 0
-	return octree.Set(p, d)
+		// Extract potential data before redefining node as InternalNode with eight new children
+		// nodes
+		p := octree.node.point.P
+		d := octree.node.point.D
+		octree.node = newInternalNode(children)
+		octree.meta = pc.NewMetaData()
+		octree.size = 0
+		return octree.Set(p, d)
+	}
+	return errors.Errorf("error attempted to split invalid node type (%v)", octree.node.nodeType)
 }
 
 // Checks that a point should be inside a basic octree based on its center and defined side length.
