8a0d645f14ee475ffcb31882d7f4fda6dbc1a2ad
Bijan Haney <bijan@viam.com>
2022-12-01 17:51:36 -0500

    two alignment models

--- components/camera/align/extrinsics.go
+++ components/camera/align/extrinsics.go
@@ -1,7 +1,8 @@
-package videosource
+package align
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"image"
 
@@ -22,11 +23,11 @@ import (
 )
 
 func init() {
-	registry.RegisterComponent(camera.Subtype, "align_color_depth_homography",
+	registry.RegisterComponent(camera.Subtype, "align_color_depth_extrinsics",
 		registry.Component{Constructor: func(ctx context.Context, deps registry.Dependencies,
 			config config.Component, logger golog.Logger,
 		) (interface{}, error) {
-			attrs, ok := config.ConvertedAttributes.(*alignHomographyAttrs)
+			attrs, ok := config.ConvertedAttributes.(*extrinsicsAttrs)
 			if !ok {
 				return nil, rdkutils.NewUnexpectedTypeError(attrs, config.ConvertedAttributes)
 			}
@@ -41,36 +42,51 @@ func init() {
 			if err != nil {
 				return nil, fmt.Errorf("no depth camera (%s): %w", depthName, err)
 			}
-			return newAlignColorDepthHomography(ctx, color, depth, attrs, logger)
+			return newColorDepthExtrinsics(ctx, color, depth, attrs, logger)
 		}})
 
-	config.RegisterComponentAttributeMapConverter(camera.SubtypeName, "align_color_depth_homography",
+	config.RegisterComponentAttributeMapConverter(camera.SubtypeName, "align_color_depth_extrinsics",
 		func(attributes config.AttributeMap) (interface{}, error) {
-			var conf alignHomographyAttrs
+			var conf extrinsicsAttrs
 			attrs, err := config.TransformAttributeMapToStruct(&conf, attributes)
 			if err != nil {
 				return nil, err
 			}
-			result, ok := attrs.(*alignHomographyAttrs)
+			result, ok := attrs.(*extrinsicsAttrs)
 			if !ok {
 				return nil, rdkutils.NewUnexpectedTypeError(result, attrs)
 			}
 			return result, nil
-		}, &alignHomographyAttrs{})
+		}, &extrinsicsAttrs{})
+
+	config.RegisterComponentAttributeConverter(camera.SubtypeName, "align_color_depth_extrinsics", "system",
+		func(val interface{}) (interface{}, error) {
+			b, err := json.Marshal(val)
+			if err != nil {
+				return nil, err
+			}
+			matrices, err := transform.NewDepthColorIntrinsicsExtrinsicsFromBytes(b)
+			if err != nil {
+				return nil, err
+			}
+			err = matrices.CheckValid()
+			return matrices, err
+		})
+
 }
 
-// alignHomographyAttrs is the attribute struct for aligning.
-type alignHomographyAttrs struct {
+// extrinsicsAttrs is the attribute struct for aligning.
+type extrinsicsAttrs struct {
 	CameraParameters     *transform.PinholeCameraIntrinsics `json:"intrinsic_parameters"`
-	Homography           *transform.RawDepthColorHomography `json:"homography"`
+	IntrinsicExtrinsic   interface{}                        `json:"system"`
+	ImageType            string                             `json:"output_image_type"`
 	Color                string                             `json:"color_camera_name"`
 	Depth                string                             `json:"depth_camera_name"`
-	ImageType            string                             `json:"output_image_type"`
-	DistortionParameters *transform.BrownConrady            `json:"distortion_parameters,omitempty"`
 	Debug                bool                               `json:"debug,omitempty"`
+	DistortionParameters *transform.BrownConrady            `json:"distortion_parameters,omitempty"`
 }
 
-func (cfg *alignHomographyAttrs) Validate(path string) ([]string, error) {
+func (cfg *extrinsicsAttrs) Validate(path string) ([]string, error) {
 	var deps []string
 	if cfg.Color == "" {
 		return nil, utils.NewConfigValidationFieldRequiredError(path, "color_camera_name")
@@ -83,8 +99,8 @@ func (cfg *alignHomographyAttrs) Validate(path string) ([]string, error) {
 	return deps, nil
 }
 
-// alignColorDepthHomography takes a color and depth image source and aligns them together using homography.
-type alignColorDepthHomography struct {
+// colorDepthExtrinsics takes a color and depth image source and aligns them together.
+type colorDepthExtrinsics struct {
 	color, depth gostream.VideoStream
 	aligner      transform.Aligner
 	projector    transform.Projector
@@ -95,32 +111,26 @@ type alignColorDepthHomography struct {
 	logger       golog.Logger
 }
 
-// newAlignColorDepthHomography creates a gostream.VideoSource that aligned color and depth channels.
-func newAlignColorDepthHomography(ctx context.Context, color, depth camera.Camera, attrs *alignHomographyAttrs, logger golog.Logger,
+// newColorDepthExtrinsics creates a gostream.VideoSource that aligned color and depth channels.
+func newColorDepthExtrinsics(ctx context.Context, color, depth camera.Camera, attrs *extrinsicsAttrs, logger golog.Logger,
 ) (camera.Camera, error) {
-	if attrs.Homography == nil {
-		return nil, errors.New("homography field in attributes cannot be empty")
-	}
-	if attrs.CameraParameters == nil {
-		return nil, errors.New("intrinsic_parameters field in attributes cannot be empty")
+	alignment, ok := attrs.IntrinsicExtrinsic.(*transform.DepthColorIntrinsicsExtrinsics)
+	if !ok {
+		return nil, rdkutils.NewUnexpectedTypeError(alignment, attrs.IntrinsicExtrinsic)
 	}
 	if attrs.CameraParameters.Height <= 0 || attrs.CameraParameters.Width <= 0 {
 		return nil, errors.Errorf(
-			"alignColorDepthHomography needs Width and Height fields set in intrinsic_parameters. Got illegal dimensions (%d, %d)",
+			"colorDepthExtrinsics needs Width and Height fields set in intrinsic_parameters. Got illegal dimensions (%d, %d)",
 			attrs.CameraParameters.Width,
 			attrs.CameraParameters.Height,
 		)
 	}
-	homography, err := transform.NewDepthColorHomography(attrs.Homography)
-	if err != nil {
-		return nil, err
-	}
+	// get the projector for the alignment camera
 	imgType := camera.StreamType(attrs.ImageType)
-
-	videoSrc := &alignColorDepthHomography{
+	videoSrc := &colorDepthExtrinsics{
 		color:     gostream.NewEmbeddedVideoStream(color),
 		depth:     gostream.NewEmbeddedVideoStream(depth),
-		aligner:   homography,
+		aligner:   alignment,
 		projector: attrs.CameraParameters,
 		imageType: imgType,
 		height:    attrs.CameraParameters.Height,
@@ -137,18 +147,18 @@ func newAlignColorDepthHomography(ctx context.Context, color, depth camera.Camer
 }
 
 // Read aligns the next images from the color and the depth sources to the frame of the color camera.
-// imageType parameter will determine which channel gets streamed.
-func (acd *alignColorDepthHomography) Read(ctx context.Context) (image.Image, func(), error) {
-	ctx, span := trace.StartSpan(ctx, "videosource::alignColorDepthHomography::Read")
+// stream parameter will determine which channel gets streamed.
+func (cde *colorDepthExtrinsics) Read(ctx context.Context) (image.Image, func(), error) {
+	ctx, span := trace.StartSpan(ctx, "videosource::colorDepthExtrinsics::Read")
 	defer span.End()
-	switch acd.imageType {
+	switch cde.imageType {
 	case camera.ColorStream, camera.UnspecifiedStream:
 		// things are being aligned to the color image, so just return the color image.
-		return acd.color.Next(ctx)
+		return cde.color.Next(ctx)
 	case camera.DepthStream:
 		// don't need to request the color image, just its dimensions
-		colDimImage := rimage.NewImage(acd.width, acd.height)
-		depth, depthCloser, err := acd.depth.Next(ctx)
+		colDimImage := rimage.NewImage(cde.width, cde.height)
+		depth, depthCloser, err := cde.depth.Next(ctx)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -156,36 +166,36 @@ func (acd *alignColorDepthHomography) Read(ctx context.Context) (image.Image, fu
 		if err != nil {
 			return nil, nil, err
 		}
-		if acd.aligner == nil {
+		if cde.aligner == nil {
 			return dm, depthCloser, nil
 		}
-		_, alignedDepth, err := acd.aligner.AlignColorAndDepthImage(colDimImage, dm)
+		_, alignedDepth, err := cde.aligner.AlignColorAndDepthImage(colDimImage, dm)
 		return alignedDepth, depthCloser, err
 	default:
-		return nil, nil, camera.NewUnsupportedStreamError(acd.imageType)
+		return nil, nil, camera.NewUnsupportedStreamError(cde.imageType)
 	}
 }
 
-func (acd *alignColorDepthHomography) NextPointCloud(ctx context.Context) (pointcloud.PointCloud, error) {
-	ctx, span := trace.StartSpan(ctx, "videosource::alignColorDepthHomography::NextPointCloud")
+func (cde *colorDepthExtrinsics) NextPointCloud(ctx context.Context) (pointcloud.PointCloud, error) {
+	ctx, span := trace.StartSpan(ctx, "videosource::colorDepthExtrinsics::NextPointCloud")
 	defer span.End()
-	if acd.projector == nil {
+	if cde.projector == nil {
 		return nil, transform.NewNoIntrinsicsError("")
 	}
-	col, dm := camera.SimultaneousColorDepthNext(ctx, acd.color, acd.depth)
+	col, dm := camera.SimultaneousColorDepthNext(ctx, cde.color, cde.depth)
 	if col == nil || dm == nil {
 		return nil, errors.New("requested color or depth image from camera is nil")
 	}
-	if acd.aligner == nil {
-		return acd.projector.RGBDToPointCloud(rimage.ConvertImage(col), dm)
+	if cde.aligner == nil {
+		return cde.projector.RGBDToPointCloud(rimage.ConvertImage(col), dm)
 	}
-	alignedColor, alignedDepth, err := acd.aligner.AlignColorAndDepthImage(rimage.ConvertImage(col), dm)
+	alignedColor, alignedDepth, err := cde.aligner.AlignColorAndDepthImage(rimage.ConvertImage(col), dm)
 	if err != nil {
 		return nil, err
 	}
-	return acd.projector.RGBDToPointCloud(alignedColor, alignedDepth)
+	return cde.projector.RGBDToPointCloud(alignedColor, alignedDepth)
 }
 
-func (acd *alignColorDepthHomography) Close(ctx context.Context) error {
-	return multierr.Combine(acd.color.Close(ctx), acd.depth.Close(ctx))
+func (cde *colorDepthExtrinsics) Close(ctx context.Context) error {
+	return multierr.Combine(cde.color.Close(ctx), cde.depth.Close(ctx))
 }
--- components/camera/align/extrinsics_test.go
+++ components/camera/align/extrinsics_test.go
@@ -1,4 +1,4 @@
-package videosource
+package align
 
 import (
 	"context"
@@ -6,6 +6,7 @@ import (
 
 	"github.com/edaniels/golog"
 	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/components/camera/videosource"
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/rimage"
 	"go.viam.com/rdk/utils"
@@ -13,15 +14,15 @@ import (
 	"go.viam.com/utils/artifact"
 )
 
-func TestAlignHomography(t *testing.T) {
+func TestAlignExtrinsics(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	conf, err := config.Read(context.Background(), utils.ResolveFile("components/camera/videosource/data/homography_cam.json"), logger)
+	conf, err := config.Read(context.Background(), utils.ResolveFile("components/camera/align/data/extrinsics_cam.json"), logger)
 	test.That(t, err, test.ShouldBeNil)
 
-	c := conf.FindComponent("homography_cam")
+	c := conf.FindComponent("extrinsics_cam")
 	test.That(t, c, test.ShouldNotBeNil)
 
-	attrs, ok := c.ConvertedAttributes.(*alignHomographyAttrs)
+	attrs, ok := c.ConvertedAttributes.(*extrinsicsAttrs)
 	test.That(t, ok, test.ShouldBeTrue)
 	test.That(t, attrs, test.ShouldNotBeNil)
 	img, err := rimage.NewImageFromFile(artifact.MustPath("align/intel515/chairs_color.png"))
@@ -29,15 +30,15 @@ func TestAlignHomography(t *testing.T) {
 	dm, err := rimage.NewDepthMapFromFile(context.Background(), artifact.MustPath("align/intel515/chairs.png"))
 	test.That(t, err, test.ShouldBeNil)
 	// create the source cameras
-	colorSrc := &StaticSource{ColorImg: img}
+	colorSrc := &videosource.StaticSource{ColorImg: img}
 	colorCam, err := camera.NewFromReader(context.Background(), colorSrc, nil, camera.ColorStream)
 	test.That(t, err, test.ShouldBeNil)
-	depthSrc := &StaticSource{DepthImg: dm}
+	depthSrc := &videosource.StaticSource{DepthImg: dm}
 	depthCam, err := camera.NewFromReader(context.Background(), depthSrc, nil, camera.DepthStream)
 	test.That(t, err, test.ShouldBeNil)
 
 	// create the alignment camera
-	is, err := newAlignColorDepthHomography(context.Background(), colorCam, depthCam, attrs, logger)
+	is, err := newColorDepthExtrinsics(context.Background(), colorCam, depthCam, attrs, logger)
 	test.That(t, err, test.ShouldBeNil)
 	// get images and point clouds
 	alignedPointCloud, err := is.NextPointCloud(context.Background())
