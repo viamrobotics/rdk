fdd0053e3d4cc1a1e1f44d9f1cd61e8682aeed08
Bijan Haney <bijan@viam.com>
2022-12-06 16:10:40 -0500

    adds rtsp model

--- components/camera/rtsp/rtsp.go
+++ components/camera/rtsp/rtsp.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"image"
 	"net/url"
-	"sync"
+	"sync/atomic"
 
 	"github.com/aler9/gortsplib"
 	"github.com/edaniels/golog"
+	"github.com/pkg/errors"
 	"go.viam.com/rdk/components/camera"
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/registry"
@@ -53,17 +54,57 @@ type RtspAttrs struct {
 	DistortionParams *transform.BrownConrady            `json:"distortion_parameters,omitempty"`
 }
 
+// rtspCamera contains the rtsp client, and the reader function that fulfills the camera interface
 type rtspCamera struct {
 	gostream.VideoReader
-	cancelFunc              context.CancelFunc
-	activeBackgroundWorkers sync.WaitGroup
+	client  gortsplib.Client
+	decoder *h264Decoder
+}
+
+func (rc *rtspCamera) Close(ctx context.Context) error {
+	err := rc.client.Close()
+	if err != nil {
+		return err
+	}
+	rc.decoder.Close()
+	return nil
 }
 
 func NewRTSPCamera(ctx context.Context, attrs *RtspAttrs, logger golog.Logger) (camera.Camera, error) {
 	cameraModel = &transform.PinholeCameraModel{attrs.IntrinsicParams, attrs.DistortionParams}
-	// make a channel to put images into from the Client
-	// turn the output into an image, put it in the channel
 	c := gortsplib.Client{}
+	// parse URL
+	u, err := url.Parse(attrs.Address)
+	if err != nil {
+		return nil, err
+	}
+	// find published tracks
+	tracks, baseURL, _, err := c.Describe(u)
+	if err != nil {
+		return nil, err
+	}
+
+	// find the H264 track
+	track := func() *gortsplib.TrackH264 {
+		for _, track := range tracks {
+			if track, ok := track.(*gortsplib.TrackH264); ok {
+				return track
+			}
+		}
+		return nil
+	}()
+	if track == nil {
+		return nil, errors.Errorf("H264 track not found in rtsp camera %s", u)
+	}
+	// get the RTP decoder and the h264 decoder
+	rtpDec, h264RawDec, err := rtpH264Decoder(track)
+	if err != nil {
+		return nil, err
+	}
+	// On packet retreival, turn it into an image, and store it in shared memory
+	var latestFrame atomic.Value
+	var gotFirstFrameOnce bool
+	gotFirstFrame := make(chan struct{})
 	c.OnPacketRTP = func(ctx *gortsplib.ClientOnPacketRTPCtx) {
 		// convert RTP packets into NALUs
 		nalus, _, err := rtpDec.Decode(ctx.Packet)
@@ -73,7 +114,7 @@ func NewRTSPCamera(ctx context.Context, attrs *RtspAttrs, logger golog.Logger) (
 
 		for _, nalu := range nalus {
 			// convert NALUs into RGBA frames
-			img, err := h264RawDec.decode(nalu)
+			img, err := h264RawDec.Decode(nalu)
 			if err != nil {
 				panic(err)
 			}
@@ -82,21 +123,24 @@ func NewRTSPCamera(ctx context.Context, attrs *RtspAttrs, logger golog.Logger) (
 			if img == nil {
 				continue
 			}
+			latestFrame.Store(img)
+			if !gotFirstFrameOnce {
+				close(gotFirstFrame)
+				gotFirstFrameOnce = true
+			}
+		}
 	}
-	// parse URL
-	u, err := url.Parse(attrs.Address)
-	if err != nil {
-		return nil, err
-	}
-	cancelCtx, cancel := context.WithCancel(context.Background())
-	rtspCam := &rtspCamera{cancelFunc: cancel}
-
 	// connect to the server
 	err = c.Start(u.Scheme, u.Host)
 	if err != nil {
 		return nil, err
 	}
-	// read the image from the channel
+	// setup and read the H264 track only
+	err = c.SetupAndPlay(gortsplib.Tracks{track}, baseURL)
+	if err != nil {
+		return nil, err
+	}
+	// read the image from shared memory when it is requested
 	reader := gostream.VideoReaderFunc(func(ctx context.Context) (image.Image, func(), error) {
 		select {
 		case <-cancelCtx.Done():
@@ -107,6 +151,7 @@ func NewRTSPCamera(ctx context.Context, attrs *RtspAttrs, logger golog.Logger) (
 		}
 		return latestFrame.Load().(image.Image), func() {}, nil
 	})
-	rtspCam.VideoReader = reader
+	// define and return the camera
+	rtspCam := &rtspCamera{reader, c, h264RawDec}
 	return camera.NewFromReader(ctx, rtspCam, cameraModel, camera.ColorStream)
 }
