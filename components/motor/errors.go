cbf97f7e946937aa48809a101deb0fc64620f895
Susmita Sanyal <susmita@viam.com>
2022-12-19 16:14:04 -0500

    comments

--- components/motor/errors.go
+++ components/motor/errors.go
@@ -26,7 +26,7 @@ func NewZeroRPMError() error {
 	return errors.New("Cannot move motor at 0 RPM")
 }
 
-// NewGoToUnsupportedError returns error when a motor is required to support GoTo feature
+// NewGoToUnsupportedError returns error when a motor is required to support GoTo feature.
 func NewGoToUnsupportedError(motorName string) error {
 	return errors.Errorf("motor with name %s does not support GoTo", motorName)
 }
