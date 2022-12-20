48d156c3aa1b896be09b12e0c77011d77a57d759
Nick Franczak <nicholasgfranczak@gmail.com>
2022-12-20 13:29:34 -0500

    removed Pose and replaced with r3.Vector in Point struct

--- spatialmath/point.go
+++ spatialmath/point.go
@@ -11,34 +11,34 @@ import (
 
 // PointCreator implements the GeometryCreator interface for point structs.
 type pointCreator struct {
-	offset Pose
+	offset r3.Vector // this needs to be changed
 	label  string
 }
 
 // point is a collision geometry that represents a single point in 3D space that occupies no geometry.
 type point struct {
-	pose  Pose // remove this field
+	pose  r3.Vector // remove this field
 	label string
 }
 
 // NewPointCreator instantiates a PointCreator class, which allows instantiating point geometries given only a pose which is applied
 // at the specified offset from the pose. These pointers have dimensions given by the provided halfSize vector.
-func NewPointCreator(offset Pose, label string) GeometryCreator {
+func NewPointCreator(offset r3.Vector, label string) GeometryCreator {
 	return &pointCreator{offset, label}
 }
 
 // NewGeometry instantiates a new point from a PointCreator class.
 func (pc *pointCreator) NewGeometry(pose Pose) Geometry {
-	return &point{Compose(pc.offset, pose), pc.label}
+	return &point{pose.Point(), pc.label}
 }
 
 func (pc *pointCreator) Offset() Pose {
-	return pc.offset
+	return NewPoseFromPoint(pc.offset)
 }
 
 // String returns a human readable string that represents the pointCreator.
 func (pc *pointCreator) String() string {
-	pt := pc.offset.Point()
+	pt := pc.offset
 	return fmt.Sprintf("Type: Point, Location X:%.0f, Y:%.0f, Z:%.0f", pt.X, pt.Y, pt.Z)
 }
 
@@ -53,7 +53,7 @@ func (pc *pointCreator) MarshalJSON() ([]byte, error) {
 // ToProto converts the point to a Geometry proto message.
 func (pc *pointCreator) ToProtobuf() *commonpb.Geometry {
 	return &commonpb.Geometry{
-		Center: PoseToProtobuf(pc.offset),
+		Center: PoseToProtobuf(NewPoseFromPoint(pc.offset)),
 		GeometryType: &commonpb.Geometry_Sphere{
 			Sphere: &commonpb.Sphere{
 				RadiusMm: 0,
@@ -65,7 +65,7 @@ func (pc *pointCreator) ToProtobuf() *commonpb.Geometry {
 
 // NewPoint instantiates a new point Geometry.
 func NewPoint(pt r3.Vector, label string) Geometry {
-	return &point{NewPoseFromPoint(pt), label}
+	return &point{pt, label}
 }
 
 // Label returns the labels of this point.
@@ -78,12 +78,12 @@ func (pt *point) Label() string {
 
 // Pose returns the pose of the point.
 func (pt *point) Pose() Pose {
-	return pt.pose
+	return NewPoseFromPoint(pt.pose)
 }
 
 // Vertices returns the vertices defining the point.
 func (pt *point) Vertices() []r3.Vector {
-	return []r3.Vector{pt.pose.Point()}
+	return []r3.Vector{pt.pose}
 }
 
 // AlmostEqual compares the point with another geometry and checks if they are equivalent.
@@ -92,18 +92,18 @@ func (pt *point) AlmostEqual(g Geometry) bool {
 	if !ok {
 		return false
 	}
-	return PoseAlmostEqual(pt.pose, other.pose)
+	return PoseAlmostEqual(NewPoseFromPoint(pt.pose), NewPoseFromPoint(other.pose))
 }
 
 // Transform premultiplies the point pose with a transform, allowing the point to be moved in space.
 func (pt *point) Transform(toPremultiply Pose) Geometry {
-	return &point{Compose(toPremultiply, pt.pose), pt.label}
+	return &point{Compose(toPremultiply, NewPoseFromPoint(pt.pose)).Point(), pt.label}
 }
 
 // ToProto converts the point to a Geometry proto message.
 func (pt *point) ToProtobuf() *commonpb.Geometry {
 	return &commonpb.Geometry{
-		Center: PoseToProtobuf(pt.pose),
+		Center: PoseToProtobuf(NewPoseFromPoint(pt.pose)),
 		GeometryType: &commonpb.Geometry_Sphere{
 			Sphere: &commonpb.Sphere{
 				RadiusMm: 0,
@@ -116,10 +116,10 @@ func (pt *point) ToProtobuf() *commonpb.Geometry {
 // CollidesWith checks if the given point collides with the given geometry and returns true if it does.
 func (pt *point) CollidesWith(g Geometry) (bool, error) {
 	if other, ok := g.(*box); ok {
-		return pointVsBoxCollision(other, pt.pose.Point()), nil
+		return pointVsBoxCollision(other, pt.pose), nil
 	}
 	if other, ok := g.(*sphere); ok {
-		return sphereVsPointDistance(other, pt.pose.Point()) <= 0, nil
+		return sphereVsPointDistance(other, pt.pose) <= 0, nil
 	}
 	if other, ok := g.(*point); ok {
 		return pt.AlmostEqual(other), nil
@@ -130,13 +130,13 @@ func (pt *point) CollidesWith(g Geometry) (bool, error) {
 // CollidesWith checks if the given point collides with the given geometry and returns true if it does.
 func (pt *point) DistanceFrom(g Geometry) (float64, error) {
 	if other, ok := g.(*box); ok {
-		return pointVsBoxDistance(other, pt.pose.Point()), nil
+		return pointVsBoxDistance(other, pt.pose), nil
 	}
 	if other, ok := g.(*sphere); ok {
-		return sphereVsPointDistance(other, pt.pose.Point()), nil
+		return sphereVsPointDistance(other, pt.pose), nil
 	}
 	if other, ok := g.(*point); ok {
-		return pt.pose.Point().Sub(other.pose.Point()).Norm(), nil
+		return pt.pose.Sub(other.pose).Norm(), nil
 	}
 	return math.Inf(-1), newCollisionTypeUnsupportedError(pt, g)
 }
@@ -165,5 +165,5 @@ func pointVsBoxDistance(b *box, pt r3.Vector) float64 {
 
 // ToPointCloud converts a point geometry into a []r3.Vector
 func (pt *point) ToPointCloud(options map[string]interface{}) []r3.Vector {
-	return []r3.Vector{pt.pose.Point()}
+	return []r3.Vector{pt.pose}
 }
