c67b15d8e08dc0932cd7b6a2fcef3ece44036e27
Nick Franczak <nicholasgfranczak@gmail.com>
2022-12-20 14:20:37 -0500

    linted

--- pointcloud/pointcloud_test.go
+++ pointcloud/pointcloud_test.go
@@ -171,3 +171,10 @@ func TestPointCloudMatrix(t *testing.T) {
 	})
 	test.That(t, mcv, test.ShouldResemble, mat.NewDense(1, 7, []float64{1, 2, 3, 123, 45, 67, 5}))
 }
+
+func TestVectorConvert(t *testing.T) {
+	vecLst := []r3.Vector{{0, 0.75, 0}, {0, -0.75, 0}, {1, 0.75, 1}, {-1, 0.75, -1}}
+	color := &color.NRGBA{0, 0, 255, 255}
+	_, err := vectorConvert(vecLst, *color)
+	test.That(t, err, test.ShouldBeNil)
+}
