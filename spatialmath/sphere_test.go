3dc9b83cf131f42e00e10bec3793548291052e1a
Peter LoVerso <biotinker@gmail.com>
2023-01-05 12:42:25 -0800

    Remove GeometryCreators, and use Geometries in place of them (#1734)

--- spatialmath/sphere_test.go
+++ spatialmath/sphere_test.go
@@ -9,24 +9,24 @@ import (
 )
 
 func makeTestSphere(point r3.Vector, radius float64, label string) Geometry {
-	sphere, _ := NewSphere(point, radius, label)
+	sphere, _ := NewSphere(NewPoseFromPoint(point), radius, label)
 	return sphere
 }
 
 func TestNewSphere(t *testing.T) {
 	offset := NewPose(r3.Vector{X: 1, Y: 0, Z: 0}, &EulerAngles{0, 0, math.Pi})
 
-	// test sphere created from NewBox method
-	geometry, err := NewSphere(offset.Point(), 1, "")
+	// test sphere created from NewSphere method
+	geometry, err := NewSphere(offset, 1, "")
 	test.That(t, err, test.ShouldBeNil)
-	test.That(t, geometry, test.ShouldResemble, &sphere{pose: NewPoseFromPoint(offset.Point()), radius: 1})
-	_, err = NewSphere(offset.Point(), -1, "")
+	test.That(t, geometry, test.ShouldResemble, &sphere{pose: offset, radius: 1})
+	_, err = NewSphere(offset, -1, "")
 	test.That(t, err.Error(), test.ShouldContainSubstring, newBadGeometryDimensionsError(&sphere{}).Error())
 
-	// test sphere created from GeometryCreator with offset
-	gc, err := NewSphereCreator(1, offset, "")
+	// test sphere created from initial sphere with offset
+	gc, err := NewSphere(offset, 1, "")
 	test.That(t, err, test.ShouldBeNil)
-	geometry = gc.NewGeometry(PoseInverse(offset))
+	geometry = gc.Transform(PoseInverse(offset))
 	test.That(t, PoseAlmostCoincident(geometry.Pose(), NewZeroPose()), test.ShouldBeTrue)
 }
 
@@ -36,10 +36,6 @@ func TestSphereAlmostEqual(t *testing.T) {
 	bad := makeTestSphere(r3.Vector{1e-2, 1e-2, 1e-2}, 1+1e-2, "")
 	test.That(t, original.AlmostEqual(good), test.ShouldBeTrue)
 	test.That(t, original.AlmostEqual(bad), test.ShouldBeFalse)
-}
-
-func TestSphereVertices(t *testing.T) {
-	test.That(t, R3VectorAlmostEqual(makeTestSphere(r3.Vector{}, 1, "").Vertices()[0], r3.Vector{}, 1e-8), test.ShouldBeTrue)
 }
 
 func TestSpherePC(t *testing.T) {
