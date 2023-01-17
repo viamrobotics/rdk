e544b2c1a4e835eade29a6d806228263a30c8a1c
Bijan Haney <bijan@viam.com>
2023-01-17 16:33:47 -0500

    WIP

--- components/camera/fake/camera.go
+++ components/camera/fake/camera.go
@@ -6,6 +6,7 @@ import (
 	"image"
 
 	"github.com/edaniels/golog"
+	"github.com/pkg/errors"
 	"go.viam.com/utils/artifact"
 
 	"go.viam.com/rdk/components/camera"
@@ -28,12 +29,14 @@ func init() {
 			config config.Component,
 			logger golog.Logger,
 		) (interface{}, error) {
+			res := config.Attributes.String("resolution")
+			resModel, err := fakeModel(res)
 			cam := &Camera{Name: config.Name, Model: fakeModel}
 			return camera.NewFromReader(ctx, cam, fakeModel, camera.ColorStream)
 		}})
 }
 
-var fakeIntrinsics = &transform.PinholeCameraIntrinsics{
+var fakeIntrinsicsHigh = &transform.PinholeCameraIntrinsics{
 	Width:  1024,
 	Height: 768,
 	Fx:     821.32642889,
@@ -41,8 +44,7 @@ var fakeIntrinsics = &transform.PinholeCameraIntrinsics{
 	Ppx:    494.95941428,
 	Ppy:    370.70529534,
 }
-
-var fakeDistortion = &transform.BrownConrady{
+var fakeDistortionHigh = &transform.BrownConrady{
 	RadialK1:     0.11297234,
 	RadialK2:     -0.21375332,
 	RadialK3:     -0.01584774,
@@ -50,16 +52,56 @@ var fakeDistortion = &transform.BrownConrady{
 	TangentialP2: 0.19969297,
 }
 
-var fakeModel = &transform.PinholeCameraModel{
-	PinholeCameraIntrinsics: fakeIntrinsics,
-	Distortion:              fakeDistortion,
+var fakeIntrinsicsMed = &transform.PinholeCameraIntrinsics{
+	Width:  512,
+	Height: 384,
+	Fx:     410.66321445,
+	Fy:     410.84303680,
+	Ppx:    247.47970714,
+	Ppy:    185.35264767,
+}
+
+var fakeIntrinsicsLow = &transform.PinholeCameraIntrinsics{
+	Width:  256,
+	Height: 192,
+	Fx:     205.3316072,
+	Fy:     205.4215184,
+	Ppx:    123.73985357,
+	Ppy:    92.676323835,
+}
+
+var fakeModelHigh = &transform.PinholeCameraModel{
+	PinholeCameraIntrinsics: fakeIntrinsicsHigh,
+	Distortion:              fakeDistortionHigh,
+}
+
+var fakeModelMed = &transform.PinholeCameraModel{
+	PinholeCameraIntrinsics: fakeIntrinsicsMed,
+}
+
+var fakeModelLow = &transform.PinholeCameraModel{
+	PinholeCameraIntrinsics: fakeIntrinsicsLow,
+}
+
+func fakeModel(res string) (*transform.PinholeCameraModel, error) {
+	switch res {
+	case "high", "hi", "":
+		return fakeModelHigh, nil
+	case "medium", "med":
+		return fakeModelMed, nil
+	case "low", "lo":
+		return fakeModelLow, nil
+	default:
+		return nil, errors.Errorf(`do not know resolution %q, only "high", "medium", or "low" are available`, res)
+	}
 }
 
 // Camera is a fake camera that always returns the same image.
 type Camera struct {
 	generic.Echo
-	Name  string
-	Model *transform.PinholeCameraModel
+	Name       string
+	Model      *transform.PinholeCameraModel
+	Resolution string
 }
 
 // Read always returns the same image of a chess board.
