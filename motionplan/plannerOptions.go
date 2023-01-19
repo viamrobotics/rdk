8f574a81df1f844d500d0b037e1d981f01f5f246
Ray Bjorkman <raymond@viam.com>
2023-01-19 16:29:53 -0500

    Update plannerOptions.go

--- motionplan/plannerOptions.go
+++ motionplan/plannerOptions.go
@@ -32,7 +32,7 @@ const (
 	defaultEpsilon = 0.001
 
 	// default number of seconds to try to solve in total before returning.
-	defaultTimeout = 1.
+	defaultTimeout = 2.
 
 	// default number of times to try to smooth the path.
 	defaultSmoothIter = 20
