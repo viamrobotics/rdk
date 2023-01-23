86ee15e4ce269ab149b01cb6766bb16f76f629c2
Nick Sanford <nick.sanford@viam.com>
2023-01-23 14:58:08 -0500

    create separate test cases for nil & non nil

--- rimage/transform/brown_conrady_test.go
+++ rimage/transform/brown_conrady_test.go
@@ -7,10 +7,15 @@ import (
 )
 
 func TestBrownConradyCheckValid(t *testing.T) {
-	distortionsA := &BrownConrady{}
-	test.That(t, distortionsA.CheckValid(), test.ShouldBeNil)
-	var nilBrownConradyPtr *BrownConrady
-	err := nilBrownConradyPtr.CheckValid()
-	expected := "BrownConrady shaped distortion_parameters not provided: invalid distortion_parameters"
-	test.That(t, err.Error(), test.ShouldContainSubstring, expected)
+	t.Run("nil &BrownConrady{} are invalid", func(t *testing.T) {
+		var nilBrownConradyPtr *BrownConrady
+		err := nilBrownConradyPtr.CheckValid()
+		expected := "BrownConrady shaped distortion_parameters not provided: invalid distortion_parameters"
+		test.That(t, err.Error(), test.ShouldContainSubstring, expected)
+	})
+
+	t.Run("non nil &BrownConrady{} are valid", func(t *testing.T) {
+		distortionsA := &BrownConrady{}
+		test.That(t, distortionsA.CheckValid(), test.ShouldBeNil)
+	})
 }
