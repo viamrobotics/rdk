fab5562328fc997262be18b09130df128e6e577c
Nick Sanford <nick.sanford@viam.com>
2023-01-23 14:53:31 -0500

    respond to feedback

--- rimage/transform/brown_conrady_test.go
+++ rimage/transform/brown_conrady_test.go
@@ -10,7 +10,7 @@ func TestBrownConradyCheckValid(t *testing.T) {
 	distortionsA := &BrownConrady{}
 	test.That(t, distortionsA.CheckValid(), test.ShouldBeNil)
 	var nilBrownConradyPtr *BrownConrady
-	err := nilBrownConradyPtr.CheckValid().Error()
+	err := nilBrownConradyPtr.CheckValid()
 	expected := "BrownConrady shaped distortion_parameters not provided: invalid distortion_parameters"
-	test.That(t, err, test.ShouldContainSubstring, expected)
+	test.That(t, err.Error(), test.ShouldContainSubstring, expected)
 }
