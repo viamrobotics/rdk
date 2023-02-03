5818434eddccda3d8ce3763880e618c98451d033
Alexa Greenberg <alexa.greenberg@viam.com>
2023-02-03 15:00:40 -0500

    DATA-885 - Add PackageManager service (#1829)
    Robot now exposes a PackageManager service that allows other components/services to reference a Package based on their config.
    
    The local robot will load and sync all config.Packages before reconfiguring the robot.
    
    Note: Today this will block reconfiguring until the packages are loaded or fail to load. This can be problematic if there are very large files that need t be downloaded but a decent compromise for v0.
    
    After reconfiguration the local robot will cleanup the package manager. This is done after reconfiguration to ensure no running processes / services / components are referencing the files before removing them.

---
+++ robot/packages/package_manager.go
@@ -0,0 +1,60 @@
+// Package packages contains utilities and manager to sync Viam packages defined in the RDK config from the Viam app to the local robot.
+package packages
+
+import (
+	"context"
+
+	"github.com/docker/go-units"
+	"github.com/pkg/errors"
+
+	"go.viam.com/rdk/config"
+)
+
+type (
+	// PackageName is the logical name of the package on the local rdk. Can point to ID/Version of an actual package.
+	PackageName string
+	// PackageID is a gobally unique package id.
+	PackageID string
+	// PackageVersion is an immutable package version for a given package id.
+	PackageVersion string
+)
+
+const maxPackageSize = int64(50 * units.GB)
+
+// ErrPackageMissing is an error when a package cannot be found.
+var ErrPackageMissing = errors.New("package missing")
+
+// ErrInvalidPackageRef is an error when a invalid package reference syntax.
+var ErrInvalidPackageRef = errors.New("invalid package reference")
+
+// Manager provides a managed interface for looking up package paths. This is separated from ManagerSyncer to avoid passing
+// the full sync interface to all components.
+type Manager interface {
+	// PackagePath returns the package if it exists and is already downloaded. If it does not exist it returns a ErrPackageMissing error.
+	PackagePath(name PackageName) (string, error)
+
+	// RefPath returns the absolute path of the package reference for a given path with a package reference.
+	// - If not the original path is not a package reference the original path is returned without an error.
+	// - If the path contains a package reference and the package does not exist a ErrPackageMissing will be returned.
+	// - Any syntax errors in the package reference will produce an ErrInvalidPackageRef.
+	RefPath(name string) (string, error)
+}
+
+// ManagerSyncer provides a managed interface for both reading package paths and syncing packages from the RDK config.
+type ManagerSyncer interface {
+	Manager
+
+	// Close the manager
+	Close() error
+
+	// Sync will download and create the symbolic logic links to all the given PackageConfig. Sync will not remove any unused
+	// data packages. You must call Cleanup() to remove leftovers. Sync will block until all packages are loaded to the file system.
+	// Sync should only be used by one goroutine at once.
+	// If errors occur during sync the manager should continue trying to download all packages and then return any errors that occurred.
+	// If the context is canceled the manager will stop syncing and return an interrupted error.
+	Sync(ctx context.Context, packages []config.PackageConfig) error
+
+	// Cleanup removes any unused packages known to the Manager that are no longer used. It removes the packages from the file system.
+	// Returns any errors during the cleanup process.
+	Cleanup(ctx context.Context) error
+}
