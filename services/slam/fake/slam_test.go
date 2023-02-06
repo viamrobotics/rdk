ab46625108ac09fc3ebf1282a252a93e4e67b3e6
Nick Sanford <nick.sanford@viam.com>
2023-02-06 17:05:17 -0500

    Add fake slam tests

--- services/slam/fake/slam_test.go
+++ services/slam/fake/slam_test.go
@@ -2,7 +2,6 @@ package fake
 
 import (
 	"context"
-	"image"
 	"testing"
 
 	"github.com/golang/geo/r3"
@@ -27,6 +26,10 @@ func TestFakeSLAMPosition(t *testing.T) {
 	expectedOri.RZ = -1
 	expectedOri.Theta = 3.0542483867902197
 	test.That(t, pInFrame.Pose().Orientation().AxisAngles(), test.ShouldResemble, expectedOri)
+
+	pInFrame2, err := slamSvc.Position(context.Background(), slamSvc.Name, map[string]interface{}{})
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, pInFrame2, test.ShouldResemble, pInFrame2)
 }
 
 func TestFakeSLAMGetInternalState(t *testing.T) {
@@ -34,79 +37,28 @@ func TestFakeSLAMGetInternalState(t *testing.T) {
 	data, err := slamSvc.GetInternalState(context.Background(), slamSvc.Name)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, len(data), test.ShouldBeGreaterThan, 0)
+	data2, err := slamSvc.GetInternalState(context.Background(), slamSvc.Name)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, data, test.ShouldResemble, data2)
+}
+
+func TestFakeSLAMStateful(t *testing.T) {
+	slamSvc := &FakeSLAM{Name: "test"}
+	extra := map[string]interface{}{}
+
+	t.Run("Test getting a PCD map advances the test data", func(t *testing.T) {
+		verifyGetMapStateful(t, rdkutils.MimeTypePCD, slamSvc, extra)
+	})
+
+	t.Run("Test getting a JPEG map advances the test data", func(t *testing.T) {
+		verifyGetMapStateful(t, rdkutils.MimeTypeJPEG, slamSvc, extra)
+	})
 }
 
 func TestFakeSLAMGetMap(t *testing.T) {
 	slamSvc := &FakeSLAM{Name: "test"}
 	pInFrame := referenceframe.NewPoseInFrame(slamSvc.Name, spatialmath.NewPose(r3.Vector{X: 0, Y: 0, Z: 0}, spatialmath.NewOrientationVector()))
 	extra := map[string]interface{}{}
-
-	t.Run("Test getting a PCD map advances the test data", func(t *testing.T) {
-		testDataCount := maxDataCount
-		results := []*vision.Object{}
-		// Call GetMap twice for every testData artifact
-		for i := 0; i < testDataCount*2; i++ {
-			_, _, vObj, err := slamSvc.GetMap(
-				context.Background(),
-				slamSvc.Name,
-				rdkutils.MimeTypePCD,
-				pInFrame,
-				true,
-				extra,
-			)
-			results = append(results, vObj)
-
-			test.That(t, err, test.ShouldBeNil)
-		}
-
-		// Confirm that the first half of vObjs
-		// is equal to the last
-		// This proves that each call to GetMap
-		// advances the test data over a finite
-		// dataset that loops around
-		test.That(t, results[len(results)/2:], test.ShouldResemble, results[:len(results)/2])
-	})
-
-	t.Run("Test getting a JPEG map advances the test data", func(t *testing.T) {
-		testDataCount := maxDataCount
-		results := []image.Image{}
-		// Call GetMap twice for every testData artifact
-		for i := 0; i < testDataCount*2; i++ {
-			_, im, _, err := slamSvc.GetMap(
-				context.Background(),
-				slamSvc.Name,
-				rdkutils.MimeTypeJPEG,
-				pInFrame,
-				true,
-				extra,
-			)
-			test.That(t, err, test.ShouldBeNil)
-			results = append(results, im)
-		}
-
-		// Confirm that the first half of vObjs
-		// is equal to the last
-		// This proves that each call to GetMap
-		// advances the test data over a finite
-		// dataset that loops around
-		test.That(t, results[len(results)/2:], test.ShouldResemble, results[:len(results)/2])
-	})
-
-	t.Run("Test getting valid PCD map", func(t *testing.T) {
-		mimeType, im, vObj, err := slamSvc.GetMap(
-			context.Background(),
-			slamSvc.Name,
-			rdkutils.MimeTypePCD,
-			pInFrame,
-			true,
-			extra,
-		)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, mimeType, test.ShouldEqual, rdkutils.MimeTypePCD)
-		test.That(t, im, test.ShouldBeNil)
-		test.That(t, vObj, test.ShouldNotBeNil)
-		test.That(t, vObj.Size(), test.ShouldEqual, 247)
-	})
 
 	t.Run("Test getting valid JPEG map", func(t *testing.T) {
 		mimeType, im, vObj, err := slamSvc.GetMap(
@@ -134,4 +86,68 @@ func TestFakeSLAMGetMap(t *testing.T) {
 		test.That(t, vObj, test.ShouldBeNil)
 		test.That(t, im, test.ShouldBeNil)
 	})
+
+}
+func verifyGetMapStateful(t *testing.T, mimeType string, slamSvc *FakeSLAM, extra map[string]interface{}) {
+	testDataCount := maxDataCount
+	getMapResults := []*vision.Object{}
+	getPositionResults := []spatialmath.Pose{}
+	getInternalStateResults := [][]byte{}
+
+	// Call GetMap twice for every testData artifact
+	for i := 0; i < testDataCount*2; i++ {
+		pInFrame, err := slamSvc.Position(context.Background(), slamSvc.Name, map[string]interface{}{})
+		test.That(t, err, test.ShouldBeNil)
+		getPositionResults = append(getPositionResults, pInFrame.Pose())
+
+		data, err := slamSvc.GetInternalState(context.Background(), slamSvc.Name)
+		getInternalStateResults = append(getInternalStateResults, data)
+
+		_, _, vObj, err := slamSvc.GetMap(
+			context.Background(),
+			slamSvc.Name,
+			rdkutils.MimeTypePCD,
+			pInFrame,
+			true,
+			extra,
+		)
+		getMapResults = append(getMapResults, vObj)
+
+		test.That(t, err, test.ShouldBeNil)
+	}
+
+	getMapResultsFirst := getMapResults[len(getMapResults)/2:]
+	getMapResultsLast := getMapResults[:len(getMapResults)/2]
+
+	getPositionResultsFirst := getPositionResults[len(getPositionResults)/2:]
+	getPositionResultsLast := getPositionResults[:len(getPositionResults)/2]
+
+	getInternalStateResultsFirst := getInternalStateResults[len(getInternalStateResults)/2:]
+	getInternalStateResultsLast := getInternalStateResults[:len(getInternalStateResults)/2]
+
+	// Confirm that the first half of the
+	// results equal the last.
+	// This proves that each call to GetMap
+	// advances the test data (both for GetMap & other endpoints)
+	// over a dataset of size maxDataCount that loops around.
+	test.That(t, getMapResultsFirst, test.ShouldResemble, getMapResultsLast)
+	test.That(t, getPositionResultsFirst, test.ShouldResemble, getPositionResultsLast)
+	test.That(t, getInternalStateResultsFirst, test.ShouldResemble, getInternalStateResultsLast)
+
+	// Confirm that the first half of the
+	// results does NOT equal the last half in reverse.
+	// This proves that each call to GetMap
+	// advances the test data (both for GetMap & other endpoints)
+	// over a dataset of size maxDataCount that loops around.
+	test.That(t, getMapResultsFirst, test.ShouldNotResemble, reverse(getMapResultsLast))
+	test.That(t, getPositionResultsFirst, test.ShouldNotResemble, reverse(getPositionResultsLast))
+	test.That(t, getInternalStateResultsFirst, test.ShouldNotResemble, reverse(getInternalStateResultsLast))
+}
+
+func reverse[T any](slice []T) []T {
+	for i := len(slice)/2 - 1; i >= 0; i-- {
+		opp := len(slice) - 1 - i
+		slice[i], slice[opp] = slice[opp], slice[i]
+	}
+	return slice
 }
