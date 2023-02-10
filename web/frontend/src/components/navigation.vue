af7d275962343297a134ddc81e6ab2b9ea1b0b0d
Michael Lee <michael@viam.com>
2023-02-10 13:54:20 -0500

    Refactor RC app into cards component for reuse within App

--- web/frontend/src/components/navigation.vue
+++ web/frontend/src/components/navigation.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-
+// TODO: fix typecheck errors in https://viam.atlassian.net/browse/RSDK-1897
+// @ts-nocheck
 import { ref, onUnmounted } from 'vue';
 import { grpc } from '@improbable-eng/grpc-web';
 import { toast } from '../lib/toast';
