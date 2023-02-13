c16339cdfd548cc1ff87047d2be456a271aaecef
Michael Lee <michael@viam.com>
2023-02-13 14:44:35 -0500

    make lint x2

--- web/frontend/src/components/pcd.vue
+++ web/frontend/src/components/pcd.vue
@@ -1,7 +1,6 @@
 <!-- eslint-disable multiline-comment-style -->
 <script setup lang="ts">
-// TODO: fix typecheck errors in https://viam.atlassian.net/browse/RSDK-1897
-// @ts-nocheck
+// @ts-nocheck TODO: fix typecheck errors in https://viam.atlassian.net/browse/RSDK-1897
 /**
  * @TODO: No disposing of THREE resources currently.
  * This is causing memory leaks.
