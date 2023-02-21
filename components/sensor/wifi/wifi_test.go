1ce6e4b042c8c2e49c1542216b8e0dad3233ce60
Maxim Pertsov <maxim@viam.com>
2023-02-21 16:47:39 -0500

    support reading multiple wifi interfaces

--- components/sensor/wifi/wifi_test.go
+++ components/sensor/wifi/wifi_test.go
@@ -39,7 +39,8 @@ func TestReadings(t *testing.T) {
 	_, err := file.WriteString(
 		`Inter-| sta-|   Quality        |   Discarded packets               | Missed | WE
  face | tus | link level noise |  nwid  crypt   frag  retry   misc | beacon | 22
-XXXXXXXXX: XXXX   58.  -52.  -256        X      X      X      X  XXXXX        X`)
+IFACE0: XXXX   58.  -52.  -256        X      X      X      X  XXXXX        X
+IFACE1: XXXX   59.  -51.  -257        X      X      X      X  XXXXX        X`)
 	test.That(t, err, test.ShouldBeNil)
 
 	sensor, err := newWifi(logger, file.Name())
@@ -48,9 +49,17 @@ XXXXXXXXX: XXXX   58.  -52.  -256        X      X      X      X  XXXXX        X`
 	readings, err := sensor.Readings(ctx, nil)
 	test.That(t, err, test.ShouldBeNil)
 	expected := map[string]interface{}{
-		"link":     int(58),
-		"level_dB": int(-52),
-		"noise_dB": int(-256),
+		"IFACE0": map[string]interface{}{
+			"link":     int(58),
+			"level_dB": int(-52),
+			"noise_dB": int(-256),
+		},
+		"IFACE1": map[string]interface{}{
+			"link":     int(59),
+			"level_dB": int(-51),
+			"noise_dB": int(-257),
+		},
 	}
+
 	test.That(t, readings, test.ShouldResemble, expected)
 }
