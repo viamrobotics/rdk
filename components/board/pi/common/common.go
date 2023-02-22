fbcde629bde1c9fd25556df138512d31bb242ad6
Alexa Greenberg <alexa.greenberg@viam.com>
2023-02-22 13:17:00 -0500

    RSDK-1764 - add maxRotation angle to the servo config (#1894)
    Co-authored-by: randhid <35934754+randhid@users.noreply.github.com>

--- components/board/pi/common/common.go
+++ components/board/pi/common/common.go
@@ -17,12 +17,13 @@ var ModelName = resource.NewDefaultModel("pi")
 
 // ServoConfig is the config for a pi servo.
 type ServoConfig struct {
-	Pin       string   `json:"pin"`
-	Min       int      `json:"min,omitempty"`
-	Max       int      `json:"max,omitempty"`
-	StartPos  *float64 `json:"starting_position_degs,omitempty"`
-	HoldPos   *bool    `json:"hold_position,omitempty"` // defaults True. False holds for 500 ms then disables servo
-	BoardName string   `json:"board"`
+	Pin         string   `json:"pin"`
+	Min         int      `json:"min,omitempty"`
+	Max         int      `json:"max,omitempty"` // specifies a user inputted position limitation
+	StartPos    *float64 `json:"starting_position_degs,omitempty"`
+	HoldPos     *bool    `json:"hold_position,omitempty"` // defaults True. False holds for 500 ms then disables servo
+	BoardName   string   `json:"board"`
+	MaxRotation int      `json:"max_rotation_deg,omitempty"` // specifies a hardware position limitation. Defaults to 180
 }
 
 // Validate ensures all parts of the config are valid.
