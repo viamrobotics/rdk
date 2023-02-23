78f647140035ee5d3bb5ea9cbb78c2e762babb2e
Alan Davidson <alan@viam.com>
2023-02-23 14:26:41 -0500

    implement the GPIOPin interface

--- components/board/genericlinux/gpio_unsupported.go
+++ components/board/genericlinux/gpio_unsupported.go
@@ -2,9 +2,36 @@
 
 package genericlinux
 
+import (
+	"context"
+	"math"
+
+	"github.com/pkg/errors"
+)
+
 type gpioPin struct {
 	// This struct is implemented in the Linux version. We have a dummy struct here just to get
 	// things to compile on non-Linux environments.
+}
+
+// We need gpioPin to implement the board.GPIOPin interface.
+func (p *gpioPin) Set(ctx context.Context, high bool, extra map[string]interface{}) error {
+	return errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
+}
+func (p *gpioPin) Get(ctx context.Context, extra map[string]interface{}) (bool, error) {
+	return false, errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
+}
+func (p *gpioPin) PWM(ctx context.Context, extra map[string]interface{}) (float64, error) {
+	return math.NaN, errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
+}
+func (p *gpioPin) SetPWM(ctx context.Context, dutyCyclePct float64, extra map[string]interface{}) error {
+	return errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
+}
+func (p *gpioPin) PWMFreq(ctx context.Context, extra map[string]interface{}) (uint, error) {
+	return 0, errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
+}
+func (p *gpioPin) SetPWMFreq(ctx context.Context, freqHz uint, extra map[string]interface{}) error {
+	return errors.New("GPIO pins using ioctl are not supported on non-Linux boards")
 }
 
 func gpioInitialize(gpioMappings map[int]GPIOBoardMapping) map[string]*gpioPin {
