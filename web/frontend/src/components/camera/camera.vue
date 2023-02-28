8682b8ee2e76e6b47892b31f3499ba5a76fe900d
Michael Lee <michael@viam.com>
2023-02-28 16:11:25 -0500

    make lint

--- web/frontend/src/components/camera/camera.vue
+++ web/frontend/src/components/camera/camera.vue
@@ -50,7 +50,7 @@ const viewCamera = async (isOn: boolean) => {
       throw new Error('expected event stream to exist');
     }
     // Ignore event if received for the wrong stream, in the case of multiple cameras
-    if(eventStream.id !== props.cameraName){
+    if (eventStream.id !== props.cameraName) {
       return;
     }
     videoEl.srcObject = eventStream;
