5ced5eeddcc4c1b3b67f8e12df7428ab720194a5
Kim Mishra <kim.mishra@viam.com>
2023-02-28 14:29:01 -0500

    enabling interrupts on adxl accelerometer

--- components/movementsensor/adxl345/adxl345.go
+++ components/movementsensor/adxl345/adxl345.go
@@ -18,7 +18,6 @@ package adxl345
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"time"
 
@@ -45,8 +44,27 @@ type AttrConfig struct {
 	BoardName              string   `json:"board"`
 	I2cBus                 string   `json:"i2c_bus"`
 	UseAlternateI2CAddress bool     `json:"use_alternate_i2c_address,omitempty"`
-	EchoInterrupt          string   `json:"echo_interrupt_pin",omitempty`
-	Interrupts             []string `json:"interrupts,omitempty`
+	EchoInterrupt          string   `json:"echo_interrupt_pin,omitempty"`
+	InterruptsEnabled      []string `json:"interrupts_enabled,omitempty"`
+	TapX                   bool     `json:"tap_x,omitempty"`
+	TapY                   bool     `json:"tap_y,omitempty"`
+	TapZ                   bool     `json:"tap_z,omitempty"`
+	Thresh_Tap             byte     `json:"thresh_tap,omitempty"`
+	Dur                    byte     `json:"dur,omitempty"`
+	Latent                 byte     `json:"latent,omitempty"`
+	Window                 byte     `json:"window,omitempty"`
+	Thresh_FF              byte     `json:"thresh_ff,omitempty"`
+	Time_FF                byte     `json:"time_ff,omitempty"`
+	Thresh_Act             byte     `json:"Thresh_Act,omitempty"`
+	Thresh_Inact           byte     `json:"Thresh_Inact,omitempty"`
+	Time_Inact             byte     `json:"Time_Inact,omitempty"`
+	Act_Inact_Ctl          byte     `json:"Act_Inact_Ctl,omitempty"`
+	ActX                   bool     `json:"act_x,omitempty"`
+	ActY                   bool     `json:"act_y,omitempty"`
+	ActZ                   bool     `json:"act_z,omitempty"`
+	InactX                 bool     `json:"inact_x,omitempty"`
+	InactY                 bool     `json:"inact_y,omitempty"`
+	InactZ                 bool     `json:"inact_z,omitempty"`
 }
 
 // Validate ensures all parts of the config are valid, and then returns the list of things we
@@ -84,13 +102,32 @@ func init() {
 }
 
 type adxl345 struct {
-	bus             board.I2C
-	i2cAddress      byte
-	logger          golog.Logger
-	echoInterrupt   board.DigitalInterrupt
-	interrupts      []string
-	ticksChan       chan board.Tick
-	interruptsFound []string
+	bus               board.I2C
+	i2cAddress        byte
+	logger            golog.Logger
+	echoInterrupt     board.DigitalInterrupt
+	interruptsEnabled []string
+	ticksChan         chan board.Tick
+	interruptsFound   []string
+	tapX              bool
+	tapY              bool
+	tapZ              bool
+	thresh_tap        byte
+	dur               byte
+	latent            byte
+	window            byte
+	thresh_ff         byte
+	time_ff           byte
+	thresh_act        byte
+	thresh_inact      byte
+	time_inact        byte
+	act_inact_ctl     byte
+	actX              bool
+	actY              bool
+	actZ              bool
+	inactX            bool
+	inactY            bool
+	inactZ            bool
 
 	// Lock the mutex when you want to read or write either the acceleration or the last error.
 	mu                 sync.Mutex
@@ -141,15 +178,34 @@ func NewAdxl345(
 	}
 
 	cancelContext, cancelFunc := context.WithCancel(ctx)
-	interrupts := cfg.Interrupts
+	interrupts := cfg.InterruptsEnabled
 	sensor := &adxl345{
-		bus:           bus,
-		i2cAddress:    address,
-		echoInterrupt: i,
-		interrupts:    interrupts,
-		logger:        logger,
-		cancelContext: cancelContext,
-		cancelFunc:    cancelFunc,
+		bus:               bus,
+		i2cAddress:        address,
+		echoInterrupt:     i,
+		interruptsEnabled: interrupts,
+		logger:            logger,
+		cancelContext:     cancelContext,
+		cancelFunc:        cancelFunc,
+		tapX:              cfg.TapX,
+		tapY:              cfg.TapY,
+		tapZ:              cfg.TapZ,
+		thresh_tap:        cfg.Thresh_Tap,
+		dur:               cfg.Dur,
+		latent:            cfg.Latent,
+		window:            cfg.Window,
+		thresh_ff:         cfg.Thresh_FF,
+		time_ff:           cfg.Time_FF,
+		thresh_act:        cfg.Thresh_Act,
+		thresh_inact:      cfg.Thresh_Inact,
+		time_inact:        cfg.Time_Inact,
+		act_inact_ctl:     cfg.Act_Inact_Ctl,
+		actX:              cfg.ActX,
+		actY:              cfg.ActY,
+		actZ:              cfg.ActZ,
+		inactX:            cfg.InactX,
+		inactY:            cfg.InactY,
+		inactZ:            cfg.InactZ,
 	}
 	sensor.ticksChan = make(chan board.Tick)
 
@@ -203,17 +259,15 @@ func NewAdxl345(
 			}
 		}
 	})
-	sensor.readInterrupts(sensor.cancelContext, sensor.interrupts)
-	sensor.startInterruptPolling()
+	sensor.readInterrupts(sensor.cancelContext)
+	sensor.setRelevantRegisters(ctx)
 	sensor.enableInterrupts(ctx)
-	sensor.setTapRelatedRegisters(ctx, map[byte]byte{})
-
+	sensor.startInterruptPolling()
 	return sensor, nil
 }
 
 func (adxl *adxl345) startInterruptPolling() {
 	utils.PanicCapturingGo(func() {
-		fmt.Println("starting interrupt polling")
 		adxl.echoInterrupt.AddCallback(adxl.ticksChan)
 		defer adxl.echoInterrupt.RemoveCallback(adxl.ticksChan)
 
@@ -222,14 +276,9 @@ func (adxl *adxl345) startInterruptPolling() {
 			case <-adxl.cancelContext.Done():
 				return
 			case tick := <-adxl.ticksChan:
-				fmt.Println("Got a signal")
-				fmt.Println(tick)
 				if tick.High {
-					fmt.Println("signal is high")
-					interruptsFound := adxl.readInterrupts(adxl.cancelContext, adxl.interrupts)
-					fmt.Println(interruptsFound)
 					adxl.mu.Lock()
-					adxl.interruptsFound = interruptsFound
+					adxl.readInterrupts(adxl.cancelContext)
 					adxl.mu.Unlock()
 				}
 			}
@@ -280,42 +329,111 @@ func (adxl *adxl345) writeByte(ctx context.Context, register, value byte) error 
 func getByteFromInterrupts(ctx context.Context, interrupts []string) byte {
 	var register byte = 0
 	for _, interrupt := range interrupts {
-		register += interruptBitMap[interrupt]
+		register += interruptBitPosition[interrupt]
 	}
 	return register
 }
 
 func (adxl *adxl345) enableInterrupts(ctx context.Context) error {
-	register := getByteFromInterrupts(ctx, adxl.interrupts)
+	register := getByteFromInterrupts(ctx, adxl.interruptsEnabled)
 	return adxl.writeByte(ctx, INT_ENABLE, register)
 }
 
-func (adxl *adxl345) setTapRelatedRegisters(ctx context.Context, tapRelatedRegisters map[byte]byte) {
-	for key := range defaultTapRelatedRegisterValues {
-		val, ok := tapRelatedRegisters[key]
+func (adxl *adxl345) setRelevantRegisters(ctx context.Context) {
+	if len(adxl.interruptsEnabled) == 0 {
+		return
+	}
+	relevantRegisters := adxl.getRelevantRegisterValues()
+	for key := range defaultRegisterValues {
+		val, ok := relevantRegisters[key]
 		if ok {
 			adxl.writeByte(ctx, key, val)
 		} else {
-			adxl.writeByte(ctx, key, defaultTapRelatedRegisterValues[key])
+			adxl.writeByte(ctx, key, defaultRegisterValues[key])
 		}
 	}
 }
+func (adxl *adxl345) getAxes(x bool, y bool, z bool) byte {
+	var tap_axes byte = 0
+	if x == true {
+		tap_axes += X
+	}
+	if y == true {
+		tap_axes += Y
+	}
+	if z == true {
+		tap_axes += Z
+	}
+	return tap_axes
+}
 
-func (adxl *adxl345) readInterrupts(ctx context.Context, interrupts []string) []string {
-	interruptsFound := []string{}
-	interuptEnabledRegister := getByteFromInterrupts(ctx, interrupts)
+func (adxl *adxl345) getRelevantRegisterValues() map[byte]byte {
+	relevantRegisterValues := map[byte]byte{}
+
+	tapAxesSpecified := adxl.tapX == true || adxl.tapY == true || adxl.tapZ == true
+	if tapAxesSpecified {
+		relevantRegisterValues[TAP_AXES] = adxl.getAxes(adxl.tapX, adxl.tapY, adxl.tapZ)
+	}
+
+	var actInactAxes byte = 0
+	activityAxesSpecified := adxl.actX == true || adxl.actY == true || adxl.actZ == true
+	if activityAxesSpecified {
+		actInactAxes = adxl.getAxes(adxl.actX, adxl.actY, adxl.actZ)
+	}
+	inactivityAxesSpecified := adxl.inactX == true || adxl.inactY == true || adxl.inactZ == true
+	if inactivityAxesSpecified {
+		actInactAxes += (adxl.getAxes(adxl.inactX, adxl.inactY, adxl.inactZ) << 4)
+	}
+	if activityAxesSpecified || inactivityAxesSpecified {
+		relevantRegisterValues[ACT_INACT_CTL] = actInactAxes
+	}
+
+	if adxl.thresh_tap != 0 {
+		relevantRegisterValues[THRESH_TAP] = adxl.thresh_tap
+	}
+	if adxl.dur != 0 {
+		relevantRegisterValues[DUR] = adxl.dur
+	}
+	if adxl.latent != 0 {
+		relevantRegisterValues[LATENT] = adxl.latent
+	}
+	if adxl.window != 0 {
+		relevantRegisterValues[WINDOW] = adxl.window
+	}
+	if adxl.thresh_ff != 0 {
+		relevantRegisterValues[THRESH_FF] = adxl.thresh_ff
+	}
+	if adxl.time_ff != 0 {
+		relevantRegisterValues[TIME_FF] = adxl.time_ff
+	}
+	if adxl.thresh_act != 0 {
+		relevantRegisterValues[THRESH_ACT] = adxl.thresh_act
+	}
+	if adxl.thresh_inact != 0 {
+		relevantRegisterValues[THRESH_INACT] = adxl.thresh_inact
+	}
+	if adxl.time_inact != 0 {
+		relevantRegisterValues[TIME_INACT] = adxl.time_inact
+	}
+	if adxl.act_inact_ctl != 0 {
+		relevantRegisterValues[ACT_INACT_CTL] = adxl.time_ff
+	}
+	return relevantRegisterValues
+}
+
+func (adxl *adxl345) readInterrupts(ctx context.Context) {
+	interuptEnabledRegister := getByteFromInterrupts(ctx, adxl.interruptsEnabled)
 	intSourceRegister, err := adxl.readByte(ctx, INT_SOURCE)
 
 	if err != nil {
 		adxl.logger.Error(err)
 	}
 
-	for _, interrupt := range interruptTypes {
-		if intSourceRegister&interruptBitMap[interrupt]&interuptEnabledRegister != 0 {
-			interruptsFound = append(interruptsFound, interrupt)
+	for key, value := range interruptBitPosition {
+		if intSourceRegister&value&interuptEnabledRegister != 0 {
+			adxl.interruptsFound = append(adxl.interruptsFound, key)
 		}
 	}
-	return interruptsFound
 }
 
 // Given a value, scales it so that the range of values read in becomes the range of +/- maxValue.
@@ -377,12 +495,33 @@ func (adxl *adxl345) Accuracy(ctx context.Context, extra map[string]interface{})
 	return map[string]float32{}, movementsensor.ErrMethodUnimplementedAccuracy
 }
 
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
 func (adxl *adxl345) Readings(ctx context.Context, extra map[string]interface{}) (map[string]interface{}, error) {
 	adxl.mu.Lock()
 	defer adxl.mu.Unlock()
-	interruptsFound := adxl.readInterrupts(adxl.cancelContext, adxl.interrupts)
-	fmt.Println(interruptsFound)
-	return map[string]interface{}{"linear_acceleration": adxl.linearAcceleration}, adxl.err.Get()
+
+	readings := make(map[string]interface{})
+	readings["linear_acceleration"] = adxl.linearAcceleration
+	readings["data_ready"] = contains(adxl.interruptsFound, DATA_READY)
+	readings["single_tap"] = contains(adxl.interruptsFound, SINGLE_TAP)
+	readings["double_tap"] = contains(adxl.interruptsFound, DOUBLE_TAP)
+	readings["activity"] = contains(adxl.interruptsFound, Activity)
+	readings["inactivity"] = contains(adxl.interruptsFound, Inactivity)
+	readings["freefall"] = contains(adxl.interruptsFound, FREE_FALL)
+	readings["watermark"] = contains(adxl.interruptsFound, WATERMARK)
+	readings["overrun"] = contains(adxl.interruptsFound, OVERRUN)
+
+	adxl.interruptsFound = []string{}
+
+	return readings, adxl.err.Get()
 }
 
 func (adxl *adxl345) Properties(ctx context.Context, extra map[string]interface{}) (*movementsensor.Properties, error) {
--- components/movementsensor/adxl345/interrupts.go
+++ components/movementsensor/adxl345/interrupts.go
@@ -1,29 +1,41 @@
 package adxl345
 
+// addresses relevant to interrupts
 const (
-	INT_ENABLE byte = 0x2E
-	INT_MAP         = 0x2F
-	INT_SOURCE      = 0x30
-	TAP_AXES        = 0x2A
-	THRESH_TAP      = 0x1D
-	DUR             = 0x21
-	LATENT          = 0x22
-	WINDOW          = 0x23
+	INT_ENABLE    byte = 0x2E
+	INT_MAP            = 0x2F
+	INT_SOURCE         = 0x30
+	TAP_AXES           = 0x2A
+	THRESH_TAP         = 0x1D
+	DUR                = 0x21
+	LATENT             = 0x22
+	WINDOW             = 0x23
+	THRESH_FF          = 0x28
+	TIME_FF            = 0x29
+	THRESH_ACT         = 0x24
+	THRESH_INACT       = 0x25
+	TIME_INACT         = 0x26
+	ACT_INACT_CTL      = 0x27
 )
 
-var interruptRegisters []byte = []byte{INT_ENABLE, INT_MAP, INT_SOURCE, TAP_AXES, THRESH_TAP, DUR, LATENT, WINDOW}
-
 var interruptRegisterNames = map[byte]string{
-	INT_ENABLE: "INT_ENABLE",
-	INT_MAP:    "INT_MAP",
-	INT_SOURCE: "INT_SOURCE",
-	TAP_AXES:   "TAP_AXES",
-	THRESH_TAP: "THRESH_TAP",
-	DUR:        "DUR",
-	LATENT:     "LATENT",
-	WINDOW:     "WINDOW",
+	INT_ENABLE:    "INT_ENABLE",
+	INT_MAP:       "INT_MAP",
+	INT_SOURCE:    "INT_SOURCE",
+	TAP_AXES:      "TAP_AXES",
+	THRESH_TAP:    "THRESH_TAP",
+	DUR:           "DUR",
+	LATENT:        "LATENT",
+	WINDOW:        "WINDOW",
+	THRESH_FF:     "THRESH_FF",
+	TIME_FF:       "TIME_FF",
+	THRESH_ACT:    "THRESH_ACT",
+	THRESH_INACT:  "THRESH_INACT",
+	TIME_INACT:    "TIME_INACT",
+	ACT_INACT_CTL: "ACT_INACT_CTL",
 }
 
+// types of interrupts
 const (
 	DATA_READY string = "DATA_READY"
 	SINGLE_TAP        = "SINGLE_TAP"
@@ -35,9 +47,7 @@ const (
 	OVERRUN           = "OVERRUN"
 )
 
-var interruptTypes []string = []string{DATA_READY, SINGLE_TAP, DOUBLE_TAP, Activity, Inactivity, FREE_FALL, WATERMARK, OVERRUN}
-
-var interruptBitMap = map[string]byte{
+var interruptBitPosition = map[string]byte{
 	DATA_READY: 1 << 7,
 	SINGLE_TAP: 1 << 6,
 	DOUBLE_TAP: 1 << 5,
@@ -56,10 +66,28 @@ than 0x10 (10 ms), the latent register to a value greater than0x10 (20 ms), the
 window register to a value greater than 0x40(80 ms), and the THRESH_TAP register
 to a value greater than 0x30 (3 g)
 */
-var defaultTapRelatedRegisterValues = map[byte]byte{
-	TAP_AXES:   0x07, //enables x, y, z respectively
+var defaultRegisterValues = map[byte]byte{
+	// Single Tap & Double Tap
+	TAP_AXES:   0x07, //enables x, y, z
 	THRESH_TAP: 0x30,
 	DUR:        0x10,
 	LATENT:     0x10,
 	WINDOW:     0x40,
+	// Free Fall
+	THRESH_FF: 0x07, //0x05 - 0x09 are recommended
+	TIME_FF:   0x20, // 0x14 - 0x46 are recommended
+	// Activity
+	THRESH_ACT:   0x80,
+	THRESH_INACT: 0x8,
+	// Inactivity
+	TIME_INACT:    0x10,
+	ACT_INACT_CTL: 0x77, // enables x, y, z for activity and inactivity
 }
+
+const (
+	X byte = 1 << 0
+	Y      = 1 << 1
+	Z      = 1 << 2
+)
+
+var tapDirections []byte = []byte{X, Y, Z}
