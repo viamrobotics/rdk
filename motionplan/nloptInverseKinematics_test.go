76f50d5180038dc97897d512801e3d5557ca2a40
randhid <35934754+randhid@users.noreply.github.com>
2023-03-01 15:35:32 -0500

    RSDK-1937 Move trossen arm out of rdk (#1953)

--- motionplan/nloptInverseKinematics_test.go
+++ motionplan/nloptInverseKinematics_test.go
@@ -16,20 +16,21 @@ import (
 
 func TestCreateNloptIKSolver(t *testing.T) {
 	logger := golog.NewTestLogger(t)
-	m, err := referenceframe.ParseModelJSONFile(utils.ResolveFile("components/arm/trossen/trossen_wx250s_kinematics.json"), "")
+	m, err := referenceframe.ParseModelJSONFile(utils.ResolveFile("components/arm/xarm/xarm6_kinematics.json"), "")
 	test.That(t, err, test.ShouldBeNil)
 	ik, err := CreateNloptIKSolver(m, logger, -1)
 	test.That(t, err, test.ShouldBeNil)
 	ik.id = 1
 
-	pos := spatialmath.NewPoseFromPoint(r3.Vector{X: 360, Z: 362})
+	// matches xarm home end effector position
+	pos := spatialmath.NewPoseFromPoint(r3.Vector{X: 207, Z: 112})
 	seed := referenceframe.FloatsToInputs([]float64{1, 1, 1, 1, 1, 0})
 	_, err = solveTest(context.Background(), ik, pos, seed)
 	test.That(t, err, test.ShouldBeNil)
 
 	pos = spatialmath.NewPose(
 		r3.Vector{X: -46, Y: -23, Z: 372},
-		&spatialmath.OrientationVectorDegrees{Theta: utils.RadToDeg(3.92), OX: -0.46, OY: 0.84, OZ: 0.28},
+		&spatialmath.OrientationVectorDegrees{Theta: 0, OX: 0, OY: 0, OZ: -1},
 	)
 
 	seed = m.InputFromProtobuf(&pb.JointPositions{Values: []float64{49, 28, -101, 0, -73, 0}})
