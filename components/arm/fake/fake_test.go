2e0c2603ca66098c76cfe3fe0cc136d90f69e290
Nick Franczak <nicholasgfranczak@gmail.com>
2023-02-28 23:23:15 -0500

    add UpdateAction tests

---
+++ components/arm/fake/fake_test.go
@@ -0,0 +1,71 @@
+package fake
+
+import (
+	"testing"
+
+	"github.com/edaniels/golog"
+	pb "go.viam.com/api/component/arm/v1"
+	"go.viam.com/test"
+
+	"go.viam.com/rdk/components/arm"
+	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/resource"
+)
+
+func TestUpdateAction(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+
+	cfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ArmModel: "ur5e",
+		},
+	}
+
+	shouldNotReconfigureCfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ArmModel: "xArm6",
+		},
+	}
+
+	shouldNotReconfigureCfgAgain := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ModelFilePath: "fake_model.json",
+		},
+	}
+
+	attrs, ok := cfg.ConvertedAttributes.(*AttrConfig)
+	test.That(t, ok, test.ShouldBeTrue)
+
+	model, err := modelFromName(attrs.ArmModel, cfg.Name)
+	test.That(t, err, test.ShouldBeNil)
+
+	fakeArm := &Arm{
+		Name:   cfg.Name,
+		joints: &pb.JointPositions{Values: make([]float64, len(model.DoF()))},
+		model:  model,
+		logger: logger,
+	}
+
+	// scenario where we do not reconfigure
+	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+
+	// scenario where we do not reconfigure again
+	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
+
+	// wrap with reconfigurable arm to test the codepath that will be executed during reconfigure
+	reconfArm, err := arm.WrapWithReconfigurable(fakeArm, resource.Name{})
+	test.That(t, err, test.ShouldBeNil)
+
+	// scenario where we do not reconfigure
+	obj, canUpdate := reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+
+	// scenario where we do not reconfigure again
+	obj, canUpdate = reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
+}
---
+++ components/arm/wrapper/wrapper_test.go
@@ -0,0 +1,74 @@
+package wrapper
+
+import (
+	"testing"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/test"
+
+	"go.viam.com/rdk/components/arm"
+	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/referenceframe"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/testutils/inject"
+)
+
+func TestUpdateAction(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+
+	cfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ModelFilePath: "../universalrobots/ur5e.json",
+			ArmName:       "does not exist0",
+		},
+	}
+
+	shouldNotReconfigureCfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ModelFilePath: "../xarm/xarm6_kinematics.json",
+		},
+	}
+
+	shouldReconfigureCfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ArmName: "does not exist1",
+		},
+	}
+
+	attrs, ok := cfg.ConvertedAttributes.(*AttrConfig)
+	test.That(t, ok, test.ShouldBeTrue)
+
+	model, err := referenceframe.ModelFromPath(attrs.ModelFilePath, cfg.Name)
+	test.That(t, err, test.ShouldBeNil)
+
+	wrapperArm := &Arm{
+		Name:   cfg.Name,
+		model:  model,
+		actual: &inject.Arm{},
+		logger: logger,
+		robot:  &inject.Robot{},
+	}
+
+	// scenario where we do not reconfigure
+	test.That(t, wrapperArm.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+
+	// scenario where we reconfigure
+	test.That(t, wrapperArm.UpdateAction(&shouldReconfigureCfg), test.ShouldEqual, config.None)
+
+	// wrap with reconfigurable arm to test the codepath that will be executed during reconfigure
+	reconfArm, err := arm.WrapWithReconfigurable(wrapperArm, resource.Name{})
+	test.That(t, err, test.ShouldBeNil)
+
+	// scenario where we do not reconfigure
+	obj, canUpdate := reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+
+	// scenario where we reconfigure
+	obj, canUpdate = reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldReconfigureCfg), test.ShouldEqual, config.None)
+}
