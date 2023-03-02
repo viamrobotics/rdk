a25265e287050c81141e73790b90d46c473a7a75
Michael Lee <michael@viam.com>
2023-03-02 15:24:21 -0500

    don't use template ref

--- web/frontend/src/components/camera/camera.vue
+++ web/frontend/src/components/camera/camera.vue
@@ -23,7 +23,7 @@ interface Props {
 
 const props = defineProps<Props>();
 
-const videoEl = $ref<HTMLVideoElement>();
+let videoStream = $ref<MediaStream>();
 const imgEl = $ref<HTMLImageElement>();
 
 let cameraOn = $ref(false);
@@ -53,13 +53,13 @@ const viewCamera = async (isOn: boolean) => {
     if (eventStream.id !== props.cameraName) {
       return;
     }
-    videoEl.srcObject = eventStream;
+    videoStream = eventStream;
   });
 
   if (props.refreshRate === 'Live') {
-    if (cameraStreamStates.get(`${props.parentName}-${props.cameraName}`)?.live) {
-      return;
-    }
+    // if (cameraStreamStates.get(`${props.parentName}-${props.cameraName}`)?.live) {
+    //   return;
+    // }
 
     cameraStreamStates.set(`${props.parentName}-${props.cameraName}`, {
       on: isOn,
@@ -193,7 +193,7 @@ watch(() => props.triggerRefresh, () => {
 
     <video
       v-show="props.refreshRate === 'Live'"
-      ref="videoEl"
+      :srcObject.prop="videoStream"
       muted
       autoplay
       controls="false"
