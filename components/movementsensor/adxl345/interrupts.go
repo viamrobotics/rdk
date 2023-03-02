77c45a698c69f8a3de1ea244b0e0ace49a0be9cd
Kim Mishra <kim.mishra@viam.com>
2023-03-02 17:31:25 -0500

    linting

--- components/movementsensor/adxl345/interrupts.go
+++ components/movementsensor/adxl345/interrupts.go
@@ -2,16 +2,26 @@ package adxl345
 
 // addresses relevant to interrupts.
 const (
-	DurAddr       byte = 0x21 // contains an unsigned time value representing the maximum time that an event must be above the THRESH_TAP threshold to qualify as a tap event [625 µs/LSB]
-	IntEnableAddr byte = 0x2E // contains info on which interrupts have been enabled
-	IntMapAddr    byte = 0x2F // contains info on which interrupt pin to send each interrupt
-	IntSourceAddr byte = 0x30 // contains info on which interrupt has gone off since the last time this address has been read from
-	LatentAddr    byte = 0x22 // contains an unsigned time value representing the wait time from the detection of a tap event to the start of the time window [1.25 ms/LSB]
-	TapAxesAddr   byte = 0x2A // contains info on which axes have been turned on for taps (X, Y, Z are bits 2, 1, 0 respectivel)
-	ThreshTapAddr byte = 0x1D // contains an unsigned threshold value for tap interrupts [62.5 mg/LSB ]
-	WindowAddr    byte = 0x23 // contains an unsigned time value representing the amount of time after the expiration of the latency time (determined by the latent register) during which a second valid tap can begin [ 1.25 ms/LSB]
-	ThreshFfAddr  byte = 0x28
-	TimeFfAddr    byte = 0x29
+	// contains an unsigned time value representing the maximum time that an event must be above the
+	// THRESH_TAP threshold to qualify as a tap event [625 µs/LSB].
+	DurAddr byte = 0x21
+	// contains info on which interrupts have been enabled.
+	IntEnableAddr byte = 0x2E
+	// contains info on which interrupt pin to send each interrupt.
+	IntMapAddr byte = 0x2F
+	// contains info on which interrupt has gone off since the last time this address has been read from.
+	IntSourceAddr byte = 0x30
+	// contains an unsigned time value representing the wait time from the detection of a tap event to the
+	// start of the time window [1.25 ms/LSB].
+	LatentAddr byte = 0x22
+	// contains info on which axes have been turned on for taps (X, Y, Z are bits 2, 1, 0 respectivel).
+	TapAxesAddr byte = 0x2A
+	// contains an unsigned threshold value for tap interrupts [62.5 mg/LSB ].
+	ThreshTapAddr byte = 0x1D
+	// contains the threshold value, in unsigned format, for free-fall detection.
+	ThreshFfAddr byte = 0x28
+	// containsthe  minimum time that the value of all axes must be less than THRESH_FF to generate a free-fall interrupt.
+	TimeFfAddr byte = 0x29
 )
 
 // types of interrupts.
@@ -43,7 +53,6 @@ var defaultRegisterValues = map[byte]byte{
 	ThreshTapAddr: 0x30,
 	DurAddr:       0x10,
 	LatentAddr:    0x10,
-	WindowAddr:    0x40,
 
 	// Free Fall
 	TimeFfAddr:   0x20, // 0x14 - 0x46 are recommended
