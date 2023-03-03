6075c40b8d933bed8bedc55256f6c4ff68a8b5a3
Nick Franczak <nicholasgfranczak@gmail.com>
2023-03-03 14:07:18 -0500

    uncomment

--- components/arm/fake/fake_test.go
+++ components/arm/fake/fake_test.go
@@ -15,64 +15,64 @@ import (
 	"go.viam.com/rdk/resource"
 )
 
-// func TestUpdateAction(t *testing.T) {
-// 	// logger := golog.NewLogger("test")
-// 	logger := golog.NewTestLogger(t)
+func TestUpdateAction(t *testing.T) {
+	// logger := golog.NewLogger("test")
+	logger := golog.NewTestLogger(t)
 
-// 	cfg := config.Component{
-// 		Name: "testArm",
-// 		ConvertedAttributes: &AttrConfig{
-// 			ArmModel: "ur5e",
-// 		},
-// 	}
+	cfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ArmModel: "ur5e",
+		},
+	}
 
-// 	shouldNotReconfigureCfg := config.Component{
-// 		Name: "testArm",
-// 		ConvertedAttributes: &AttrConfig{
-// 			ArmModel: "xArm6",
-// 		},
-// 	}
+	shouldNotReconfigureCfg := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ArmModel: "xArm6",
+		},
+	}
 
-// 	shouldNotReconfigureCfgAgain := config.Component{
-// 		Name: "testArm",
-// 		ConvertedAttributes: &AttrConfig{
-// 			ModelFilePath: "fake_model.json",
-// 		},
-// 	}
+	shouldNotReconfigureCfgAgain := config.Component{
+		Name: "testArm",
+		ConvertedAttributes: &AttrConfig{
+			ModelFilePath: "fake_model.json",
+		},
+	}
 
-// 	attrs, ok := cfg.ConvertedAttributes.(*AttrConfig)
-// 	test.That(t, ok, test.ShouldBeTrue)
+	attrs, ok := cfg.ConvertedAttributes.(*AttrConfig)
+	test.That(t, ok, test.ShouldBeTrue)
 
-// 	model, err := modelFromName(attrs.ArmModel, cfg.Name)
-// 	test.That(t, err, test.ShouldBeNil)
+	model, err := modelFromName(attrs.ArmModel, cfg.Name)
+	test.That(t, err, test.ShouldBeNil)
 
-// 	fakeArm := &Arm{
-// 		Name:   cfg.Name,
-// 		joints: &pb.JointPositions{Values: make([]float64, len(model.DoF()))},
-// 		model:  model,
-// 		logger: logger,
-// 	}
+	fakeArm := &Arm{
+		Name:   cfg.Name,
+		joints: &pb.JointPositions{Values: make([]float64, len(model.DoF()))},
+		model:  model,
+		logger: logger,
+	}
 
-// 	// scenario where we do not reconfigure
-// 	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+	// scenario where we do not reconfigure
+	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
 
-// 	// scenario where we do not reconfigure again
-// 	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
+	// scenario where we do not reconfigure again
+	test.That(t, fakeArm.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
 
-// 	// wrap with reconfigurable arm to test the codepath that will be executed during reconfigure
-// 	reconfArm, err := arm.WrapWithReconfigurable(fakeArm, resource.Name{})
-// 	test.That(t, err, test.ShouldBeNil)
+	// wrap with reconfigurable arm to test the codepath that will be executed during reconfigure
+	reconfArm, err := arm.WrapWithReconfigurable(fakeArm, resource.Name{})
+	test.That(t, err, test.ShouldBeNil)
 
-// 	// scenario where we do not reconfigure
-// 	obj, canUpdate := reconfArm.(config.ComponentUpdate)
-// 	test.That(t, canUpdate, test.ShouldBeTrue)
-// 	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
+	// scenario where we do not reconfigure
+	obj, canUpdate := reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfg), test.ShouldEqual, config.None)
 
-// 	// scenario where we do not reconfigure again
-// 	obj, canUpdate = reconfArm.(config.ComponentUpdate)
-// 	test.That(t, canUpdate, test.ShouldBeTrue)
-// 	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
-// }
+	// scenario where we do not reconfigure again
+	obj, canUpdate = reconfArm.(config.ComponentUpdate)
+	test.That(t, canUpdate, test.ShouldBeTrue)
+	test.That(t, obj.UpdateAction(&shouldNotReconfigureCfgAgain), test.ShouldEqual, config.None)
+}
 
 func TestFatalUpdate(t *testing.T) {
 	logger := golog.NewTestLogger(t)
