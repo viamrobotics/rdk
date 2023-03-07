94ca486091dcc0c86b4c698ada0663053a86f7fe
Alan Davidson <alan@viam.com>
2023-03-07 14:42:52 -0500

    fix errors with non-Linux dummy implementation

--- components/board/genericlinux/gpio_unsupported.go
+++ components/board/genericlinux/gpio_unsupported.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/edaniels/golog"
 	"github.com/pkg/errors"
+
+	"go.viam.com/rdk/components/board"
 )
 
 type gpioPin struct {
@@ -46,7 +48,8 @@ func (p *gpioPin) Close() error {
 }
 
 type digitalInterrupt struct {
-	// This is another dummy struct
+	// This is another dummy struct. However, board.go uses a field within it called interrupt.
+	interrupt board.DigitalInterrupt
 }
 
 func (di *digitalInterrupt) Close() error {
@@ -54,8 +57,8 @@ func (di *digitalInterrupt) Close() error {
 }
 
 func gpioInitialize(cancelCtx context.Context, gpioMappings map[int]GPIOBoardMapping,
-    interruptConfigs []board.DigitalInterruptConfig, waitGroup *sync.WaitGroup, logger golog.Logger,
-) (map[string]*gpioPin, map[string]*digitalInterrupt, error)
+	interruptConfigs []board.DigitalInterruptConfig, waitGroup *sync.WaitGroup, logger golog.Logger,
+) (map[string]*gpioPin, map[string]*digitalInterrupt, error) {
 	// Don't even log anything here: if someone is running in a non-Linux environment, things
 	// should work fine as long as they don't try using these pins, and the log would be an
 	// unnecessary warning.
