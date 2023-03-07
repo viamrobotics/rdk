95ef27ddee6074fc73e74fa410bb73ba2fc00dfb
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2023-03-07 11:52:06 -0500

    Update code sample comment (#1986)

--- etc/code-samples-action.py
+++ etc/code-samples-action.py
@@ -6,7 +6,7 @@ with open("./examples/apis.json") as f:
     data = data["functions"]
 with open("code-samples-warning.md", "w") as f:
     f.write("Warning your change may break code samples. ")
-    f.write("If your change modifies any of the following functions please contact the fleet team Thanks!\n")
+    f.write("If your change modifies any of the following functions please contact @viamrobotics/fleet-management. Thanks!\n")
     f.write("|component|function|\n")
     f.write("|-|-|\n")
     for k, v in data.items():
