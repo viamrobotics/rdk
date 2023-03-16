57a29b537f3873fc65ceb6f030751486d5c760b0
Devin Hilly <devin.hilly@viam.com>
2023-03-16 18:03:35 -0400

    Improve test

--- services/vision/vision_test.go
+++ services/vision/vision_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestAttributesWalker(t *testing.T) {
-	visionAttrs := Attributes{
+	visionAttrs := &Attributes{
 		ModelRegistry: []VisModelConfig{
 			{
 				Name: "my_classifier",
@@ -23,23 +23,16 @@ func TestAttributesWalker(t *testing.T) {
 		},
 	}
 
-	expectedAttrs := Attributes{
-		ModelRegistry: []VisModelConfig{
-			{
-				Name: "my_classifier",
-				Type: "classifications",
-				Parameters: config.AttributeMap(map[string]interface{}{
-					"model_path":  "test_model/model.tflite",
-					"label_path":  "test_model/textFile.txt",
-					"num_threads": 1,
-				}),
-			},
-		},
-	}
-
-	// This just returns the package name for the path; hence the expected values
 	packageManager := packages.NewNoopManager()
 	newAttrs, err := visionAttrs.Walk(packages.NewPackagePathVisitor(packageManager))
 	test.That(t, err, test.ShouldBeNil)
 
+	test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldNotBeNil)
+	test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldHaveLength, 1)
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Name, test.ShouldEqual, "my_classifier")
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Type, test.ShouldEqual, "classifications")
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters, test.ShouldNotBeNil)
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["model_path"], test.ShouldEqual, "test_model/model.tflite")
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["label_path"], test.ShouldEqual, "test_model/textFile.txt")
+	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["num_threads"], test.ShouldEqual, 1)
 }
