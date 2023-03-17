e8d71f82c8a40b4f29a5f7159dd5d716f88aba99
Devin Hilly <devin.hilly@viam.com>
2023-03-17 17:01:48 -0400

    Make sure works with or without package references

--- services/vision/vision_test.go
+++ services/vision/vision_test.go
@@ -10,30 +10,41 @@ import (
 )
 
 func TestAttributesWalker(t *testing.T) {
-	visionAttrs := &Attributes{
-		ModelRegistry: []VisModelConfig{
-			{
-				Name: "my_classifier",
-				Type: "classifications",
-				Parameters: config.AttributeMap(map[string]interface{}{
-					"model_path":  "${packages.test_model}/model.tflite",
-					"label_path":  "${packages.test_model}/textFile.txt",
-					"num_threads": 1,
-				}),
+	makeVisionAttributes := func(modelPath, labelPath string) *Attributes {
+		return &Attributes{
+			ModelRegistry: []VisModelConfig{
+				{
+					Name: "my_classifier",
+					Type: "classifications",
+					Parameters: config.AttributeMap(map[string]interface{}{
+						"model_path":  modelPath,
+						"label_path":  labelPath,
+						"num_threads": 1,
+					}),
+				},
 			},
-		},
+		}
 	}
 
-	packageManager := packages.NewNoopManager()
-	newAttrs, err := visionAttrs.Walk(packages.NewPackagePathVisitor(packageManager))
-	test.That(t, err, test.ShouldBeNil)
+	visionAttrsWithRefs := makeVisionAttributes("${packages.test_model}/model.tflite", "${packages.test_model}/textFile.txt")
+	visionAttrs := makeVisionAttributes("/some/path/on/robot/model.tflite", "/other/path/on/robot/textFile.txt")
 
-	test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldNotBeNil)
-	test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldHaveLength, 1)
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Name, test.ShouldEqual, "my_classifier")
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Type, test.ShouldEqual, "classifications")
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters, test.ShouldNotBeNil)
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["model_path"], test.ShouldEqual, "test_model/model.tflite")
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["label_path"], test.ShouldEqual, "test_model/textFile.txt")
-	test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["num_threads"], test.ShouldEqual, 1)
+	packageManager := packages.NewNoopManager()
+
+	testAttributesWalker := func(t *testing.T, attrs *Attributes, expectedModelPath, expectedLabelPath string) {
+		newAttrs, err := attrs.Walk(packages.NewPackagePathVisitor(packageManager))
+		test.That(t, err, test.ShouldBeNil)
+
+		test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldNotBeNil)
+		test.That(t, newAttrs.(*Attributes).ModelRegistry, test.ShouldHaveLength, 1)
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Name, test.ShouldEqual, "my_classifier")
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Type, test.ShouldEqual, "classifications")
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters, test.ShouldNotBeNil)
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["model_path"], test.ShouldEqual, expectedModelPath)
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["label_path"], test.ShouldEqual, expectedLabelPath)
+		test.That(t, newAttrs.(*Attributes).ModelRegistry[0].Parameters["num_threads"], test.ShouldEqual, 1)
+	}
+
+	testAttributesWalker(t, visionAttrsWithRefs, "test_model/model.tflite", "test_model/textFile.txt")
+	testAttributesWalker(t, visionAttrs, "/some/path/on/robot/model.tflite", "/other/path/on/robot/textFile.txt")
 }
