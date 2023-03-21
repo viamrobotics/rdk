2efd51661a4f999a31920d8a4069e5fd9c421847
Nick Sanford <nick.sanford@viam.com>
2023-03-21 14:48:40 -0400

    fix typescript types

--- web/frontend/src/components/slam-2d-render.vue
+++ web/frontend/src/components/slam-2d-render.vue
@@ -46,8 +46,9 @@ const raycaster = new MouseRaycaster({ camera, canvas, recursive: false });
 
 raycaster.addEventListener('click', (event) => {
   const [intersection] = event.intersections as THREE.Intersection[];
-  const { point } = intersection;
-  console.log(point);
+  if (intersection && intersection.point) {
+    console.log(intersection.point);
+  }
 });
 
 const markerSize = 0.5;
@@ -83,7 +84,7 @@ const updateCloud = (pointcloud: Uint8Array) => {
   const viewHeight = 1;
   const viewWidth = viewHeight * 2;
 
-  const points = loader.parse(pointcloud.buffer, '');
+  const points = loader.parse(pointcloud.buffer);
   points.geometry.computeBoundingSphere();
 
   const { radius = 1, center = { x: 0, z: 0 } } = points.geometry.boundingSphere ?? {};
