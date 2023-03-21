05dbdd83e488555a4ae95c3bce972901ee8f7249
Nick Franczak <nicholasgfranczak@gmail.com>
2023-03-21 16:50:39 -0400

    revert default case

--- pointcloud/basic_octree_utils.go
+++ pointcloud/basic_octree_utils.go
@@ -49,10 +49,10 @@ func getRawProb(d Data) float64 {
 	switch {
 	case !d.HasValue():
 		maxProb = 1
-	case d.Value() > 100 || d.Value() < 0:
-		maxProb = emptyProb
-	default:
+	case d.Value() <= 100 && d.Value() >= 0:
 		maxProb = float64(d.Value()) / 100
+	default:
+		maxProb = emptyProb
 	}
 	return maxProb
 }
