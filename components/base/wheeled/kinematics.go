71e33a120340b74c9098bd94251fdcb473ef6e69
Ray Bjorkman <raymond@viam.com>
2023-03-23 13:01:15 -0400

    first (very rough) pass at implmenting

--- components/base/wheeled/kinematics.go
+++ components/base/wheeled/kinematics.go
@@ -3,6 +3,7 @@ package wheeled
 import (
 	"bytes"
 	"context"
+	"math"
 
 	"github.com/pkg/errors"
 
@@ -11,12 +12,15 @@ import (
 	"go.viam.com/rdk/referenceframe"
 	"go.viam.com/rdk/services/slam"
 	"go.viam.com/rdk/spatialmath"
+	"go.viam.com/rdk/utils"
 )
 
 type kinematicWheeledBase struct {
 	*wheeledBase
-	slam  slam.Service
-	model referenceframe.Model
+	slam slam.Service
+	// TODO(RSDK-2393): When SLAM services know their own name this can be taken out
+	slamName string
+	model    referenceframe.Model
 }
 
 // WrapWithKinematics takes a wheeledBase component and adds a slam service to it
@@ -50,14 +54,71 @@ func (kwb *kinematicWheeledBase) ModelFrame() referenceframe.Model {
 	return kwb.model
 }
 
+func (kwb *kinematicWheeledBase) currentPose(ctx context.Context) (spatialmath.Pose, error) {
+	// TODO: make a transformation from the component reference to the base frame
+	pose, _, err := kwb.slam.GetPosition(ctx, kwb.slamName)
+	return pose, err
+}
+
 func (kwb *kinematicWheeledBase) CurrentInputs(ctx context.Context) ([]referenceframe.Input, error) {
-	// TODO(RSDK-2311): complete the implementation
-	return []referenceframe.Input{}, errors.New("not implemented yet")
+	// TODO: make a transformation from the component reference to the base frame
+	pose, err := kwb.currentPose(ctx)
+	if err != nil {
+		return nil, err
+	}
+	pt := pose.Point()
+
+	// Need to get X, Z from lidar because Y points down
+	// TODO: make a ticket to give rplidar kinematic information so that you don't have to do this here
+	return []referenceframe.Input{{Value: pt.X}, {Value: pt.Z}}, nil
 }
 
 func (kwb *kinematicWheeledBase) GoToInputs(ctx context.Context, goal []referenceframe.Input) error {
-	// TODO(RSDK-2311): complete the implementation
-	return errors.New("not implemented yet")
+	// TODO: may want to save the startPose separately
+	// TODO: code janitor
+	currentPose, err := kwb.currentPose(ctx)
+	if err != nil {
+		return err
+	}
+	currentPt := currentPose.Point()
+	desiredHeading := math.Atan2(currentPt.Z-goal[1].Value, currentPt.X-goal[0].Value)
+	distance := math.Hypot(currentPt.Z-goal[1].Value, currentPt.X-goal[0].Value)
+
+
+	// TODO: we do want the pitch here but this is domain limited to -90 to 90, need math to fix this
+	heading := utils.RadToDeg(currentPose.Orientation().EulerAngles().Pitch)
+	// While base is not at the goal
+	// TO DO figure out sane threshold.
+	for distance > 5 {
+		// If heading is ok, go forward
+		// Otherwise spin until base is heading correct way
+		// TODO make a threshold
+		if math.Abs(heading-desiredHeading) > 0 {
+			// TODO (rh) create context with cancel
+			// TODO use a speed that is not garbage
+			if err := kwb.Spin(ctx, heading-desiredHeading, 10, nil); err != nil {
+				return err
+			}
+		} else {
+			// TODO check if we are in mm in SLAM and multiply by 1000 if so
+			distance := math.Hypot(currentPt.Z-goal[1].Value, currentPt.X-goal[0].Value)
+			if err := kwb.MoveStraight(ctx, int(distance), 10, nil); err != nil {
+				return err
+			}
+
+		}
+
+		// Calculate current state
+		currentPose, err = kwb.currentPose(ctx)
+		if err != nil {
+			return err
+		}
+		currentPt = currentPose.Point()
+		heading = utils.RadToDeg(currentPose.Orientation().EulerAngles().Pitch)
+		distance = math.Hypot(currentPt.Z-goal[1].Value, currentPt.X-goal[0].Value)
+
+	}
+	return nil
 }
 
 // Model builds the kinematic model associated with the kinematicWheeledBase
--- components/base/wheeled/kinematics_test.go
+++ components/base/wheeled/kinematics_test.go
@@ -9,6 +9,7 @@ import (
 	"go.viam.com/test"
 
 	"go.viam.com/rdk/referenceframe"
+	"go.viam.com/rdk/services/slam"
 	"go.viam.com/rdk/services/slam/fake"
 	"go.viam.com/rdk/spatialmath"
 )
@@ -59,7 +60,7 @@ func TestWrapWithKinematics(t *testing.T) {
 			if err != nil {
 				return
 			}
-			kwb, ok := wb.(*kinematicWheeledBase)
+			kwb, ok := kb.(*kinematicWheeledBase)
 			test.That(t, ok, test.ShouldBeTrue)
 			limits := kwb.model.DoF()
 			test.That(t, limits[0].Min, test.ShouldBeLessThan, 0)
@@ -70,5 +71,59 @@ func TestWrapWithKinematics(t *testing.T) {
 			test.That(t, err, test.ShouldBeNil)
 			test.That(t, geometry.GeometryByName(testCfg.Name+":"+label).AlmostEqual(expectedSphere), test.ShouldBeTrue)
 		})
+	}
+}
+
+func TestCurrentInputs(t *testing.T) {
+	ctx := context.Background()
+	logger := golog.NewTestLogger(t)
+
+	sphere, err := spatialmath.NewSphere(spatialmath.NewZeroPose(), 400, "footprint")
+	test.That(t, err, test.ShouldBeNil)
+	base := &wheeledBase{
+		widthMm:              400,
+		wheelCircumferenceMm: 25,
+		logger:               logger,
+		name:                 "count basie",
+		collisionGeometry:    sphere,
+	}
+
+	kb, err := WrapWithKinematics(ctx, base, "", fake.NewSLAM("", logger))
+	test.That(t, err, test.ShouldBeNil)
+	kwb, ok := kb.(*kinematicWheeledBase)
+	test.That(t, ok, test.ShouldBeTrue)
+	for i := 0; i < 100; i++ {
+		slam.GetPointCloudMapFull(ctx, kwb.slam, kwb.slamName)
+	}
+	for i := 0; i < 10; i++ {
+		inputs, err := kwb.CurrentInputs(ctx)
+		test.That(t, err, test.ShouldBeNil)
+		_ = inputs
+		slam.GetPointCloudMapFull(ctx, kwb.slam, kwb.slamName)
+	}
+}
+
+func TestGoToInputs(t *testing.T) {
+	ctx := context.Background()
+	logger := golog.NewTestLogger(t)
+
+	sphere, err := spatialmath.NewSphere(spatialmath.NewZeroPose(), 400, "footprint")
+	test.That(t, err, test.ShouldBeNil)
+	base := &wheeledBase{
+		widthMm:              400,
+		wheelCircumferenceMm: 25,
+		logger:               logger,
+		name:                 "count basie",
+		collisionGeometry:    sphere,
+	}
+
+	kb, err := WrapWithKinematics(ctx, base, "", fake.NewSLAM("", logger))
+	test.That(t, err, test.ShouldBeNil)
+	kwb, ok := kb.(*kinematicWheeledBase)
+	test.That(t, ok, test.ShouldBeTrue)
+	for i := 0; i < 10; i++ {
+		err := kwb.GoToInputs(ctx, []referenceframe.Input{{10}, {10}})
+		test.That(t, err, test.ShouldBeNil)
+		slam.GetPointCloudMapFull(ctx, kwb.slam, kwb.slamName)
 	}
 }
