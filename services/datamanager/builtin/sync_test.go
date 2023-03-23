18bd5c250bcf16e2235161379e24e3205ef05a50
aaroncasas <aaron@viam.com>
2023-03-23 09:46:30 -0400

    Make mock sync service respect cancellation.

--- services/datamanager/builtin/sync_test.go
+++ services/datamanager/builtin/sync_test.go
@@ -573,12 +573,17 @@ func (m mockDataSyncServiceServer) DataCaptureUpload(ctx context.Context, ur *v1
 	(*m.lock).Lock()
 	defer (*m.lock).Unlock()
 
-	if m.fail.Load() {
-		m.failedDCRequests <- ur
-		return nil, errors.New("oh no error!!")
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		if m.fail.Load() {
+			m.failedDCRequests <- ur
+			return nil, errors.New("oh no error!!")
+		}
+		m.succesfulDCRequests <- ur
+		return &v1.DataCaptureUploadResponse{}, nil
 	}
-	m.succesfulDCRequests <- ur
-	return &v1.DataCaptureUploadResponse{}, nil
 }
 
 func (m mockDataSyncServiceServer) FileUpload(stream v1.DataSyncService_FileUploadServer) error {
