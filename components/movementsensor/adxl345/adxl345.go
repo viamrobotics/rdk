1931b2fb0c54612421d19216ffe904083b54d6d9
Kim Mishra <kim.mishra@viam.com>
2023-03-27 15:49:09 -0400

    check that raw data is the correct size before trying to convert it

--- components/movementsensor/adxl345/adxl345.go
+++ components/movementsensor/adxl345/adxl345.go
@@ -254,13 +254,14 @@ func NewAdxl345(
 					sensor.mu.Unlock()
 					continue
 				}
-
-				linearAcceleration := toLinearAcceleration(rawData)
-				// Only lock the mutex to write to the shared data, so other threads can read the
-				// data as often as they want.
-				sensor.mu.Lock()
-				sensor.linearAcceleration = linearAcceleration
-				sensor.mu.Unlock()
+				if len(rawData) >= 6 {
+					linearAcceleration := toLinearAcceleration(rawData)
+					// Only lock the mutex to write to the shared data, so other threads can read the
+					// data as often as they want.
+					sensor.mu.Lock()
+					sensor.linearAcceleration = linearAcceleration
+					sensor.mu.Unlock()
+				}
 			case <-sensor.cancelContext.Done():
 				return
 			}
