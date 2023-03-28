d1099b933872ad7d5c0b4a1c8233c250b722b3ff
martha-johnston <martha.johnston@viam.com>
2023-03-28 14:13:51 -0400

    correct path for board images

--- components/camera/fake/camera.go
+++ components/camera/fake/camera.go
@@ -184,7 +184,7 @@ func (c *Camera) NextPointCloud(ctx context.Context) (pointcloud.PointCloud, err
 
 // getColorImage always returns the same color image of a chess board.
 func (c *Camera) getColorImage() (*rimage.Image, error) {
-	img, err := rimage.NewImageFromFile("rimage/board2.png")
+	img, err := rimage.NewImageFromFile("../../rimage/board2.png")
 	if err != nil {
 		return nil, err
 	}
@@ -198,7 +198,7 @@ func (c *Camera) getColorImage() (*rimage.Image, error) {
 
 // getDepthImage always returns the same depth image of a chess board.
 func (c *Camera) getDepthImage(ctx context.Context) (*rimage.DepthMap, error) {
-	dm, err := rimage.NewDepthMapFromFile(ctx, "rimage/board2_gray.png")
+	dm, err := rimage.NewDepthMapFromFile(ctx, "../../rimage/board2_gray.png")
 	if err != nil {
 		return nil, err
 	}
