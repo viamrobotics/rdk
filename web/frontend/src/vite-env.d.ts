22b45ee4e7dbf8e370c14ef1104ae863e71fc85b
Micheal Parks <micheal.parks@outlook.com>
2023-04-04 11:14:15 -0400

    upgrade dependencies and fix typescript errors

---
+++ web/frontend/src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable multiline-comment-style */
+/* eslint-disable spaced-comment */
+/// <reference types="@types/google.maps" />
+/// <reference types="vite/client" />
+/// <reference types="vue/macros-global" />
