690908d02938be362685c362b3da0d84806df9e7
Ray Bjorkman <raymond@viam.com>
2023-04-04 15:53:39 -0400

    add MoveOnMap to motion service

--- robot/impl/resource_manager_test.go
+++ robot/impl/resource_manager_test.go
@@ -498,7 +498,6 @@ func TestManagerAdd(t *testing.T) {
 		grabPose *referenceframe.PoseInFrame,
 		worldState *referenceframe.WorldState,
 		constraints *motionpb.Constraints,
-		slamName resource.Name,
 		extra map[string]interface{},
 	) (bool, error) {
 		return false, nil
