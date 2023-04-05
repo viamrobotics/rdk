c32b48ad7921912f05e17bf1280461eec2b10b62
James Otting <james@viam.com>
2023-04-05 18:02:15 -0500

    Add pigpio to static filter

--- etc/ld_wrapper.sh
+++ etc/ld_wrapper.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# find the real linker, from env or same defaults as go
+REAL_LD="${CC}"
+if [[ -z "${REAL_LD}" ]]; then
+	REAL_LD="$(which gcc)"
+fi
+if [[ -z "${REAL_LD}" ]]; then
+	REAL_LD="$(which clang)"
+fi
+
+
 ARGS=("$@")
 
 # add for 32-bit arm builds
@@ -9,37 +19,33 @@ fi
 
 # exec early if we're not actually filtering anything
 if [[ -z "${VIAM_STATIC_BUILD}" ]]; then
-	exec gcc ${ARGS[@]}
+	exec "$REAL_LD" ${ARGS[@]}
 fi
 
-# List of linker arguments to ignore
+# list of linker arguments to ignore
 STRIPPED_ARGS="-g -O2"
 
-# List of arguments to prefix with -Bstatic
-STATIC_ARGS="-lx264 -lnlopt -ljpeg -ltensorflowlite_c"
+# list of arguments to prefix with -Bstatic
+STATIC_ARGS="-lx264 -lnlopt -ljpeg -ltensorflowlite_c -lpigpio"
 
 # add explicit static standard library flags
-FILTERED="-static-libgcc -static-libstdc++"
+FILTERED=("-static-libgcc" "-static-libstdc++")
 # Loop through the arguments and filter
 for ARG in "${ARGS[@]}"; do
-  if [[ "${STRIPPED_ARGS[@]}" =~ "${ARG}" ]]; then
-    # don't forward the arg
-  	:
-  elif [[ "${STATIC_ARGS[@]}" =~ "${ARG}" ]]; then
-    # wrap the arg as a static one
-    FILTERED+=("-Wl,-Bstatic ${ARG} -Wl,-Bdynamic")
-  else
-  	# pass through with no filtering
-    FILTERED+=("${ARG}")
-  fi
+	if [[ "${STRIPPED_ARGS[@]}" =~ "${ARG}" ]]; then
+		# don't forward the arg
+		:
+	elif [[ "${STATIC_ARGS[@]}" =~ "${ARG}" ]]; then
+		# wrap the arg as a static one
+		FILTERED+=("-Wl,-Bstatic" "${ARG}" "-Wl,-Bdynamic")
+	else
+		# pass through with no filtering
+		FILTERED+=("${ARG}")
+	fi
 done
 
 # add libstdc++ statically (and last)
-FILTERED+=("-Wl,-Bstatic -lstdc++ -Wl,-Bdynamic")
+FILTERED+=("-Wl,-Bstatic" "-lstdc++" "-Wl,-Bdynamic")
 
-if [[ -z "${LONG_PLT}" ]]; then
-	FILTERED+=("${LONG_PLT}")
-fi
-
-# Call GCC with the filtered arguments
-exec gcc ${FILTERED[@]}
+# call the real linker with the filtered arguments
+exec "$REAL_LD" "${FILTERED[@]}"
