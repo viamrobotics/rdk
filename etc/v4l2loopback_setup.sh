9b38c9edafc7bd89ab5baea70f8701f2d04f6b7f
Clyde Bazile <clyde@viam.com>
2023-04-06 12:40:10 -0400

    remove subshells

--- etc/v4l2loopback_setup.sh
+++ etc/v4l2loopback_setup.sh
@@ -19,16 +19,14 @@ if [[ ! "$(command -v apt-get)" ]]; then
 fi
 
 install_dependencies() {
-  sudo bash -c "apt-get install --assume-yes kmod git make gcc libelf-dev"
+  sudo apt-get install --assume-yes kmod git make gcc libelf-dev
 }
 
 install_kernel_headers() {
   echo "updating kernel module..."
-  sudo bash <<-EOS
 	sudo apt-get --assume-yes update
 	sudo apt-get --assume-yes dist-upgrade
 	sudo apt-get --assume-yes upgrade
-	EOS
 	echo "done"
 
 	if [[ -f /var/run/reboot-required ]]; then
@@ -48,9 +46,7 @@ install_v4l2loopback(){
     return
   fi
 
-  sudo bash <<-EOS
-	apt-get install --assume-yes v4l2loopback-dkms v4l2loopback-utils
-	EOS
+	sudo apt-get install --assume-yes v4l2loopback-dkms v4l2loopback-utils
 }
 
 install_gstreamer(){
@@ -59,12 +55,10 @@ install_gstreamer(){
     return
   fi
 
-  sudo bash <<-EOS
-	apt-get install --assume-yes libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev
-	apt-get install --assume-yes gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
-	apt-get install --assume-yes gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x
-	apt-get install --assume-yes gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
-	EOS
+	sudo apt-get install --assume-yes libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev
+	sudo apt-get install --assume-yes gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
+	sudo apt-get install --assume-yes gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x
+	sudo apt-get install --assume-yes gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 }
 
 allow_modprobe_as_superuser() {
