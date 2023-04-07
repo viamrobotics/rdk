viamrobotics/rdk

commit 6e139c6a3a6c4e9e871281256bacfc3487678d0c

[RSDK-2019] Hold onto the file descriptors for GPIO pins (#1896)
This is a follow-up to https://github.com/viamrobotics/rdk/pull/1810. Thanks to Pete G for noticing the issue! 

We've been acquiring file descriptors for each GPIO pin, setting their direction to input/output, and if output, setting their value, and then closing the files again. It turns out that when you close the file, the value of the output is undetermined: the pin I tried last time (pin 40) keeps its most recent value, but pins like 37 and 32 float instead.

So, this PR holds the file descriptors open for the remainder of the program (once they're opened for the first time). That way, the GPIO pins keep the same output all the way until the next time you change one of them.

components/board/genericlinux/gpio.go
@@ -12,8 +12,7 @@ import (
 
 	"github.com/mkch/gpio"
 	"github.com/pkg/errors"
-
-	"go.viam.com/rdk/components/board"
+	"go.viam.com/utils"
 )
 
 type gpioPin struct {
@@ -21,13 +20,41 @@ type gpioPin struct {
 	// the multiple calls to the gpio package don't have race conditions.
 	devicePath string
 	offset     uint32
+	line       *gpio.Line
 	mu         sync.Mutex
+}
+
+// This is a private helper function that should only be called when the mutex is locked. It sets
+// pin.line to a valid struct or returns an error.
+func (pin *gpioPin) openGpioFd() error {
+	if pin.line != nil {
+		return nil // If the pin is already opened, don't re-open it.
+	}
+
+	chip, err := gpio.OpenChip(pin.devicePath)
+	if err != nil {
+		return err
+	}
+	defer utils.UncheckedErrorFunc(chip.Close)
+
+	// The 0 just means the default value for this pin is off. We'll set it to the intended value
+	// in Set(), below.
+	line, err := chip.OpenLine(pin.offset, 0, gpio.Output, "viam-gpio")
+	if err != nil {
+		return err
+	}
+	pin.line = line
+	return nil
 }
 
 // This helps implement the board.GPIOPin interface for gpioPin.
 func (pin *gpioPin) Set(ctx context.Context, isHigh bool, extra map[string]interface{}) (err error) {
 	pin.mu.Lock()
 	defer pin.mu.Unlock()
+
+	if err := pin.openGpioFd(); err != nil {
+		return err
+	}
 
 	var value byte
 	if isHigh {
@@ -36,21 +63,7 @@ func (pin *gpioPin) Set(ctx context.Context, isHigh bool, extra map[string]inter
 		value = 0
 	}
 
-	chip, err := gpio.OpenChip(pin.devicePath)
-	if err != nil {
-		return err
-	}
-	defer func() { err = chip.Close() }()
-
-	// The line returned has its default value set to the intended GPIO output, so we don't need to
-	// do anything else with it but close the file descriptor.
-	line, err := chip.OpenLine(pin.offset, value, gpio.Output, "viam-gpio")
-	if err != nil {
-		return err
-	}
-	defer func() { err = line.Close() }()
-
-	return nil
+	return pin.line.SetValue(value)
 }
 
 // This helps implement the board.GPIOPin interface for gpioPin.
@@ -58,19 +71,11 @@ func (pin *gpioPin) Get(ctx context.Context, extra map[string]interface{}) (resu
 	pin.mu.Lock()
 	defer pin.mu.Unlock()
 
-	chip, err := gpio.OpenChip(pin.devicePath)
-	if err != nil {
+	if err := pin.openGpioFd(); err != nil {
 		return false, err
 	}
-	defer func() { err = chip.Close() }()
 
-	line, err := chip.OpenLine(pin.offset, 0, gpio.Input, "viam-gpio")
-	if err != nil {
-		return false, err
-	}
-	defer func() { err = line.Close() }()
-
-	value, err := line.Value()
+	value, err := pin.line.Value()
 	if err != nil {
 		return false, err
 	}
@@ -99,22 +104,29 @@ func (pin *gpioPin) SetPWMFreq(ctx context.Context, freqHz uint, extra map[strin
 	return errors.New("PWM stuff is not supported on ioctl GPIO pins yet")
 }
 
-var pins map[string]*gpioPin
+func (pin *gpioPin) Close() error {
+	// We keep the gpio.Line object open indefinitely, so it holds its state for as long as this
+	// struct is around. This function is a way to close it when we're about to go out of scope, so
+	// we don't leak file descriptors.
+	pin.mu.Lock()
+	defer pin.mu.Unlock()
 
-func gpioInitialize(gpioMappings map[int]GPIOBoardMapping) {
-	pins = make(map[string]*gpioPin)
+	if pin.line == nil {
+		return nil // Never opened, so no need to close
+	}
+
+	err := pin.line.Close()
+	pin.line = nil
+	return err
+}
+
+func gpioInitialize(gpioMappings map[int]GPIOBoardMapping) map[string]*gpioPin {
+	pins := make(map[string]*gpioPin)
 	for pin, mapping := range gpioMappings {
 		pins[fmt.Sprintf("%d", pin)] = &gpioPin{
 			devicePath: mapping.GPIOChipDev,
 			offset:     uint32(mapping.GPIO),
 		}
 	}
-}
-
-func gpioGetPin(pinName string) (board.GPIOPin, error) {
-	pin, ok := pins[pinName]
-	if !ok {
-		return nil, errors.Errorf("Cannot set GPIO for unknown pin: %s", pinName)
-	}
-	return pin, nil
+	return pins
 }

components/board/genericlinux/board.go
@@ -56,13 +56,6 @@ func RegisterBoard(modelName string, gpioMappings map[int]GPIOBoardMapping, useP
 				return nil, utils.NewUnexpectedTypeError(conf, config.ConvertedAttributes)
 			}
 
-			if !usePeriphGpio {
-				// We currently have two implementations of GPIO pins on these boards: one using
-				// libraries from periph.io and one using an ioctl approach. If we're using the
-				// latter, we need to initialize it here.
-				gpioInitialize(gpioMappings)
-			}
-
 			var spis map[string]*spiBus
 			if len(conf.SPIs) != 0 {
 				spis = make(map[string]*spiBus, len(conf.SPIs))
@@ -103,7 +96,7 @@ func RegisterBoard(modelName string, gpioMappings map[int]GPIOBoardMapping, useP
 			}
 
 			cancelCtx, cancelFunc := context.WithCancel(context.Background())
-			return &sysfsBoard{
+			b := sysfsBoard{
 				gpioMappings:  gpioMappings,
 				spis:          spis,
 				analogs:       analogs,
@@ -113,7 +106,14 @@ func RegisterBoard(modelName string, gpioMappings map[int]GPIOBoardMapping, useP
 				logger:        logger,
 				cancelCtx:     cancelCtx,
 				cancelFunc:    cancelFunc,
-			}, nil
+			}
+			if !usePeriphGpio {
+				// We currently have two implementations of GPIO pins on these boards: one using
+				// libraries from periph.io and one using an ioctl approach. If we're using the
+				// latter, we need to initialize it here.
+				b.gpios = gpioInitialize(gpioMappings) // Defined in gpio.go
+			}
+			return &b, nil
 		}})
 	config.RegisterComponentAttributeMapConverter(
 		board.Subtype,
@@ -155,14 +155,16 @@ func init() {
 
 type sysfsBoard struct {
 	generic.Unimplemented
-	mu            sync.RWMutex
-	gpioMappings  map[int]GPIOBoardMapping
-	spis          map[string]*spiBus
-	analogs       map[string]board.AnalogReader
-	pwms          map[string]pwmSetting
-	i2cs          map[string]board.I2C
+	mu           sync.RWMutex
+	gpioMappings map[int]GPIOBoardMapping
+	spis         map[string]*spiBus
+	analogs      map[string]board.AnalogReader
+	pwms         map[string]pwmSetting
+	i2cs         map[string]board.I2C
+	logger       golog.Logger
+
 	usePeriphGpio bool
-	logger        golog.Logger
+	gpios         map[string]*gpioPin // Only used for non-periph.io pins
 
 	cancelCtx               context.Context
 	cancelFunc              func()
@@ -317,7 +319,12 @@ func (b *sysfsBoard) GPIOPinByName(pinName string) (board.GPIOPin, error) {
 	if b.usePeriphGpio {
 		return b.periphGPIOPinByName(pinName)
 	}
-	return gpioGetPin(pinName) // implemented in gpio.go
+	// Otherwise, the pins are stored in b.gpios.
+	pin, ok := b.gpios[pinName]
+	if !ok {
+		return nil, errors.Errorf("Cannot find GPIO for unknown pin: %s", pinName)
+	}
+	return pin, nil
 }
 
 func (b *sysfsBoard) periphGPIOPinByName(pinName string) (board.GPIOPin, error) {
@@ -472,9 +479,20 @@ func (b *sysfsBoard) ModelAttributes() board.ModelAttributes {
 	return board.ModelAttributes{}
 }
 
-func (b *sysfsBoard) Close() {
+func (b *sysfsBoard) Close() error {
 	b.mu.Lock()
 	b.cancelFunc()
 	b.mu.Unlock()
 	b.activeBackgroundWorkers.Wait()
+
+	// For non-Periph boards, shut down all our open pins so we don't leak file descriptors
+	if b.usePeriphGpio {
+		return nil
+	}
+
+	var err error
+	for _, pin := range b.gpios {
+		err = multierr.Combine(err, pin.Close())
+	}
+	return err
 }

commit 485d56b3fe2ae95aaafd7807b05979d85c418ccb

[RSDK-2631] Allow GPIO pins on genericlinux boards to act as inputs again! :scream:  (#2180)
This broke way back in https://github.com/viamrobotics/rdk/pull/1896 and I didn't notice until yesterday!

Tried at my desk: at the very least pins 7 and 40 work properly again. You can set a pin high or low and its output value will be what you'd expect, but then you can connect it to either the 3.3V rail or ground and read it, and it will read the thing it's connected to, not the value it was most recently set to. I'll test the rest of the pins exhaustively when working on RSDK-1027 (making sure the pin definitions are all correct).

The new field is named `isInput` so that the default value (false) is the most commonly used one (output). and then the comments reminding you what the argument means are because it's super easy to forget and not recognize it.

and while I was looking at this, the `line` of the pin has been mutable for months, and should be documented as such.

components/board/genericlinux/gpio.go
@@ -24,9 +24,10 @@ type gpioPin struct {
 	// These values should both be considered immutable.
 	devicePath string
 	offset     uint32
-	line       *gpio.Line
 
 	// These values are mutable. Lock the mutex when interacting with them.
+	line            *gpio.Line
+	isInput         bool
 	swPwmRunning    bool
 	hwPwm           *pwmDevice // Defined in hw_pwm.go, will be nil for pins that don't support it.
 	pwmFreqHz       uint
@@ -40,7 +41,16 @@ type gpioPin struct {
 
 // This is a private helper function that should only be called when the mutex is locked. It sets
 // pin.line to a valid struct or returns an error.
-func (pin *gpioPin) openGpioFd() error {
+func (pin *gpioPin) openGpioFd(isInput bool) error {
+	if isInput != pin.isInput {
+		// We're switching from an input pin to an output one or vice versa. Close the line and
+		// repoen in the other mode.
+		if err := pin.closeGpioFd(); err != nil {
+			return err
+		}
+		pin.isInput = isInput
+	}
+
 	if pin.line != nil {
 		return nil // The pin is already opened, don't re-open it.
 	}
@@ -59,11 +69,16 @@ func (pin *gpioPin) openGpioFd() error {
 	}
 	defer utils.UncheckedErrorFunc(chip.Close)
 
-	// The 0 just means the default value for this pin is off. We'll set it to the intended value
-	// in Set(), below.
+	direction := gpio.Output
+	if pin.isInput {
+		direction = gpio.Input
+	}
+
+	// The 0 just means the default output value for this pin is off. We'll set it to the intended
+	// value in Set(), below, if this is an output pin.
 	// NOTE: we could pass in extra flags to configure the pin to be open-source or open-drain, but
-	// we haven't done that yet, and instead go with whatever the default on the board is.
-	line, err := chip.OpenLine(pin.offset, 0, gpio.Output, "viam-gpio")
+	// we haven't done that yet, and we instead go with whatever the default on the board is.
+	line, err := chip.OpenLine(pin.offset, 0, direction, "viam-gpio")
 	if err != nil {
 		return err
 	}
@@ -103,7 +118,7 @@ func (pin *gpioPin) setInternal(isHigh bool) (err error) {
 		value = 0
 	}
 
-	if err := pin.openGpioFd(); err != nil {
+	if err := pin.openGpioFd( /* isInput= */ false); err != nil {
 		return err
 	}
 
@@ -117,7 +132,7 @@ func (pin *gpioPin) Get(
 	pin.mu.Lock()
 	defer pin.mu.Unlock()
 
-	if err := pin.openGpioFd(); err != nil {
+	if err := pin.openGpioFd( /* isInput= */ true); err != nil {
 		return false, err
 	}
 