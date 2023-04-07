41868aa598a2217460174fbfde0b26d1e8097c90
Khari Jarrett <khari@viam.com>
2023-04-07 15:59:55 -0400

    some linting

--- services/mlmodel/tflite_cpu/tflite_cpu_test.go
+++ services/mlmodel/tflite_cpu/tflite_cpu_test.go
@@ -1,13 +1,13 @@
-package tfliteCPU
+package tflitecpu
 
 import (
 	"context"
-	"fmt"
-	"go.viam.com/rdk/rimage"
 	"testing"
 
 	"go.viam.com/test"
 	"go.viam.com/utils/artifact"
+
+	"go.viam.com/rdk/rimage"
 )
 
 func TestNewTFLiteCPUModel(t *testing.T) {
@@ -42,7 +42,7 @@ func TestNewTFLiteCPUModel(t *testing.T) {
 	gotMD, err := got.Metadata(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, gotMD, test.ShouldNotBeNil)
-	//fmt.Print(gotMD)
+
 	test.That(t, gotMD.Inputs[0].Name, test.ShouldResemble, "image")
 	test.That(t, gotMD.Outputs[0].Name, test.ShouldResemble, "location")
 	test.That(t, gotMD.Outputs[1].Name, test.ShouldResemble, "category")
@@ -56,12 +56,11 @@ func TestNewTFLiteCPUModel(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, gotOutput, test.ShouldNotBeNil)
 
-	fmt.Print(gotOutput)
 	test.That(t, gotOutput["number of detections"], test.ShouldResemble, []float32{25})
 	test.That(t, len(gotOutput["score"].([]float32)), test.ShouldResemble, 25)
 	test.That(t, len(gotOutput["location"].([]float32)), test.ShouldResemble, 100)
 	test.That(t, len(gotOutput["category"].([]float32)), test.ShouldResemble, 25)
 
 	// TODO: Khari. Make sure that the values are actually CORRECT, not just there.
-
+	// To also do: Test the tflite classifier and make sure it's vibing too
 }
