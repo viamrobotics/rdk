7e63f09da20fd85726b35fdb2cd6bf0acbab7081
Nick Franczak <nicholasgfranczak@gmail.com>
2023-04-10 15:07:32 -0400

    lints

--- web/frontend/src/components/slam-2d-render.vue
+++ web/frontend/src/components/slam-2d-render.vue
@@ -7,7 +7,7 @@ import * as THREE from 'three';
 import { MapControls } from 'three/examples/jsm/controls/OrbitControls';
 import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader';
 import type { commonApi } from '@viamrobotics/sdk';
-import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 
 const props = defineProps<{
   name: string
@@ -27,10 +27,32 @@ const props = defineProps<{
 }
 >();
 
-const baseUrl = "data:image/svg+xml,%3Csvg width='31' height='39' viewBox='0 0 31 39' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.55907 4.21112L1.70584 2.78033L1.95357 5.10847L4.71496 31.0594L5.08708 34.5565L6.61063 31.3869L11.7355 20.7249L23.5021 21.9458L27.0001 22.3088L24.2164 20.1596L3.55907 4.21112Z' fill='%23BE3536' stroke='%23FCECEA' stroke-width='2'/%3E%3C/svg%3E"
-const destUrl = "data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_2995_26851)'%3E%3Cpath d='M12 22L11.2579 22.6703L12 23.4919L12.7421 22.6703L12 22ZM12 22C12.7421 22.6703 12.7422 22.6701 12.7424 22.67L12.7429 22.6694L12.7443 22.6679L12.749 22.6627L12.7658 22.6439C12.7803 22.6277 12.8012 22.6042 12.8281 22.5737C12.8819 22.5127 12.9599 22.4238 13.0584 22.3094C13.2554 22.0808 13.5352 21.75 13.8702 21.3372C14.5393 20.5127 15.4332 19.3558 16.329 18.0281C17.2228 16.7032 18.1311 15.1902 18.8189 13.6549C19.5008 12.1328 20 10.5148 20 9C20 4.57772 16.4223 1 12 1C7.57772 1 4 4.57772 4 9C4 10.5148 4.49923 12.1328 5.18115 13.6549C5.86894 15.1902 6.77715 16.7032 7.67104 18.0281C8.56684 19.3558 9.46066 20.5127 10.1298 21.3372C10.4648 21.75 10.7446 22.0808 10.9416 22.3094C11.0401 22.4238 11.1181 22.5127 11.1719 22.5737C11.1988 22.6042 11.2197 22.6277 11.2342 22.6439L11.251 22.6627L11.2557 22.6679L11.2571 22.6694L11.2576 22.67C11.2578 22.6701 11.2579 22.6703 12 22ZM8 9C8 6.79228 9.79228 5 12 5C14.2077 5 16 6.79228 16 9C16 10.169 15.3927 11.7756 14.4208 13.5258C13.7032 14.818 12.8343 16.106 12.004 17.2276C11.1827 16.1085 10.3144 14.8168 9.59393 13.5214C8.61458 11.7606 8 10.1512 8 9Z' fill='%23BE3536' stroke='%23FCECEA' stroke-width='2'/%3E%3Cpath d='M12 12.5C13.933 12.5 15.5 10.933 15.5 9C15.5 7.067 13.933 5.5 12 5.5C10.067 5.5 8.5 7.067 8.5 9C8.5 10.933 10.067 12.5 12 12.5Z' fill='%23BE3536' stroke='%23FCECEA' stroke-width='2'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_2995_26851'%3E%3Crect width='24' height='24' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E"
+const baseUrl = `data:image/svg+xml,%3Csvg width='31' height='39' viewBox='0 0 31 39' fill='none' 
+                xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.55907 4.21112L1.70584 2.78033L1.95357
+                5.10847L4.71496 31.0594L5.08708 34.5565L6.61063 31.3869L11.7355 20.7249L23.5021 21.9458L27.0001
+                22.3088L24.2164 20.1596L3.55907 4.21112Z' fill='%23BE3536' stroke='%23FCECEA'
+                stroke-width='2'/%3E%3C/svg%3E`;
+const destUrl = `data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' 
+                xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_2995_26851)'%3E%3Cpath
+                d='M12 22L11.2579 22.6703L12 23.4919L12.7421 22.6703L12 22ZM12 22C12.7421 22.6703 12.7422 22.6701 
+                12.7424 22.67L12.7429 22.6694L12.7443 22.6679L12.749 22.6627L12.7658 22.6439C12.7803 22.6277 
+                12.8012 22.6042 12.8281 22.5737C12.8819 22.5127 12.9599 22.4238 13.0584 22.3094C13.2554 22.0808 
+                13.5352 21.75 13.8702 21.3372C14.5393 20.5127 15.4332 19.3558 16.329 18.0281C17.2228 16.7032 
+                18.1311 15.1902 18.8189 13.6549C19.5008 12.1328 20 10.5148 20 9C20 4.57772 16.4223 1 12 1C7.57772 
+                1 4 4.57772 4 9C4 10.5148 4.49923 12.1328 5.18115 13.6549C5.86894 15.1902 6.77715 16.7032 7.67104 
+                18.0281C8.56684 19.3558 9.46066 20.5127 10.1298 21.3372C10.4648 21.75 10.7446 22.0808 10.9416 
+                22.3094C11.0401 22.4238 11.1181 22.5127 11.1719 22.5737C11.1988 22.6042 11.2197 22.6277 11.2342 
+                22.6439L11.251 22.6627L11.2557 22.6679L11.2571 22.6694L11.2576 22.67C11.2578 22.6701 11.2579 
+                22.6703 12 22ZM8 9C8 6.79228 9.79228 5 12 5C14.2077 5 16 6.79228 16 9C16 10.169 15.3927 11.7756 
+                14.4208 13.5258C13.7032 14.818 12.8343 16.106 12.004 17.2276C11.1827 16.1085 10.3144 14.8168 
+                9.59393 13.5214C8.61458 11.7606 8 10.1512 8 9Z' fill='%23BE3536' stroke='%23FCECEA' 
+                stroke-width='2'/%3E%3Cpath d='M12 12.5C13.933 12.5 15.5 10.933 15.5 9C15.5 7.067 13.933 5.5 12 
+                5.5C10.067 5.5 8.5 7.067 8.5 9C8.5 10.933 10.067 12.5 12 12.5Z' fill='%23BE3536' 
+                stroke='%23FCECEA' stroke-width='2'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath 
+                id='clip0_2995_26851'%3E%3Crect width='24' height='24' 
+                fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E`;
 
-const emit = defineEmits<{(event: 'click', point: THREE.Vector3): void}>()
+const emit = defineEmits<{(event: 'click', point: THREE.Vector3): void}>();
 
 const loader = new PCDLoader();
 
@@ -53,13 +75,12 @@ controls.enableRotate = false;
 
 const raycaster = new MouseRaycaster({ camera, renderer, recursive: false });
 
-raycaster.on('click', async (event) => {
+raycaster.on('click', (event) => {
   const [intersection] = event.intersections as THREE.Intersection[];
   if (intersection && intersection.point) {
-    emit('click', intersection.point)
+    emit('click', intersection.point);
   }
 });
-
 
 const makeMarker = async (url : string, name: string, scalar: number) => {
   const guiData = {
@@ -67,45 +88,42 @@ const makeMarker = async (url : string, name: string, scalar: number) => {
     drawFillShapes: true,
     drawStrokes: true,
     fillShapesWireframe: false,
-    strokesWireframe: false
+    strokesWireframe: false,
   };
 
   const svgLoader = new SVGLoader();
-  const data = await svgLoader.loadAsync(guiData.currentURL)
+  const data = await svgLoader.loadAsync(guiData.currentURL);
 
   const { paths } = data!;
 
   const group = new THREE.Group();
   group.scale.multiplyScalar(scalar);
-  group.position.x = - 70;
+  group.position.x = -70;
   group.position.y = 70;
-  group.scale.y *= - 1;
+  group.scale.y *= -1;
 
-  for ( let i = 0; i < paths.length; i ++ ) {
-
-    const path = paths[ i ];
+  for (const path of paths) {
 
     const fillColor = path!.userData!.style.fill;
-    
-    if ( guiData.drawFillShapes && fillColor !== undefined && fillColor !== 'none' ) {
-      const material = new THREE.MeshBasicMaterial( {
-        color: new THREE.Color().setStyle( fillColor ).convertSRGBToLinear(),
+
+    if (guiData.drawFillShapes && fillColor !== undefined && fillColor !== 'none') {
+      const material = new THREE.MeshBasicMaterial({
+        color: new THREE.Color().setStyle(fillColor)
+          .convertSRGBToLinear(),
         opacity: path!.userData!.style.fillOpacity,
         transparent: true,
         side: THREE.DoubleSide,
         depthWrite: false,
-        wireframe: guiData.fillShapesWireframe
-      } );
+        wireframe: guiData.fillShapesWireframe,
+      });
 
-      const shapes = SVGLoader.createShapes( path! );
+      const shapes = SVGLoader.createShapes(path!);
 
-      for ( let j = 0; j < shapes.length; j ++ ) {
+      for (const shape of shapes) {
 
-        const shape = shapes[ j ];
-
-        const geometry = new THREE.ShapeGeometry( shape );
-        const mesh = new THREE.Mesh( geometry.rotateX(-Math.PI / 2).rotateY(Math.PI), material );
-        group.add( mesh );
+        const geometry = new THREE.ShapeGeometry(shape);
+        const mesh = new THREE.Mesh(geometry.rotateX(-Math.PI / 2).rotateY(Math.PI), material);
+        group.add(mesh);
 
       }
 
@@ -113,33 +131,34 @@ const makeMarker = async (url : string, name: string, scalar: number) => {
 
     const strokeColor = path!.userData!.style.stroke;
 
-    if ( guiData.drawStrokes && strokeColor !== undefined && strokeColor !== 'none' ) {
+    if (guiData.drawStrokes && strokeColor !== undefined && strokeColor !== 'none') {
 
-      const material = new THREE.MeshBasicMaterial( {
-        color: new THREE.Color().setStyle( strokeColor ).convertSRGBToLinear(),
+      const material = new THREE.MeshBasicMaterial({
+        color: new THREE.Color().setStyle(strokeColor)
+          .convertSRGBToLinear(),
         opacity: path!.userData!.style.strokeOpacity,
         transparent: true,
         side: THREE.DoubleSide,
         depthWrite: false,
-        wireframe: guiData.strokesWireframe
-      } );
+        wireframe: guiData.strokesWireframe,
+      });
 
-      for ( let j = 0, jl = path!.subPaths.length; j < jl; j ++ ) {
-        const subPath = path!.subPaths[ j ];
-        const geometry = SVGLoader.pointsToStroke( subPath.getPoints(), path!.userData!.style );
+      for (let j = 0, jl = path!.subPaths.length; j < jl; j = j + 1) {
+        const subPath = path!.subPaths[j];
+        const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path!.userData!.style);
 
-        if ( geometry ) {
-          const mesh = new THREE.Mesh( geometry.rotateX(-Math.PI / 2).rotateY(Math.PI), material );
-          group.add( mesh );
+        if (geometry) {
+          const mesh = new THREE.Mesh(geometry.rotateX(-Math.PI / 2).rotateY(Math.PI), material);
+          group.add(mesh);
         }
       }
     }
   }
 
-  group.name = name
-  scene.add(group)
-  return group
-}
+  group.name = name;
+  scene.add(group);
+  return group;
+};
 
 const disposeScene = () => {
   scene.traverse((object) => {
@@ -157,6 +176,20 @@ const disposeScene = () => {
   });
 
   scene.clear();
+};
+
+const updatePose = async (newPose: commonApi.Pose) => {
+  const x = newPose.getX();
+  const z = newPose.getZ();
+  const baseMarker = scene.getObjectByName('Base') ?? await makeMarker(baseUrl, 'Base', 0.04);
+  baseMarker.position.set(x + 0.35, 0, z - 0.55);
+
+  if (props.destExists) {
+    const { x } = props.destVector!;
+    const { z } = props.destVector!;
+    const destMarker = scene.getObjectByName('Marker') ?? await makeMarker(destUrl, 'Marker', 0.1);
+    destMarker.position.set(x + 0.6, 0, z - 1.24);
+  }
 };
 
 const updateCloud = (pointcloud: Uint8Array) => {
@@ -191,62 +224,40 @@ const updateCloud = (pointcloud: Uint8Array) => {
   intersectionPlane.position.set(center.x, 0, center.z);
   raycaster.objects = [intersectionPlane];
 
-  if (props.axes) {
-    addGrids(center.x, center.z)
-  }
-
-  scene.add(points);
-  scene.add(intersectionPlane);  
-  updatePose(props.pose!)
-};
-
-const addGrids = (x: number, z: number) => {
   // construct grids
-  const axesHelper1 = new THREE.AxesHelper( 5 );
-  axesHelper1.position.set(x, 0, z);
-  axesHelper1.rotateY(Math.PI/2)
-  axesHelper1.scale.x = 1e5
-  axesHelper1.scale.z = 1e5
-  axesHelper1.renderOrder = 998
-  axesHelper1.name = "Axes1"
-  axesHelper1.visible = props.axes
+  const axesHelper1 = new THREE.AxesHelper(5);
+  axesHelper1.position.set(center.x, 0, center.z);
+  axesHelper1.rotateY(Math.PI / 2);
+  axesHelper1.scale.x = 1e5;
+  axesHelper1.scale.z = 1e5;
+  axesHelper1.renderOrder = 998;
+  axesHelper1.name = 'Axes1';
+  axesHelper1.visible = props.axes;
 
-  
-  const axesHelper2 = new THREE.AxesHelper( 5 );
-  axesHelper2.position.set(x, 0, z);
-  axesHelper2.rotateY(-Math.PI/2)
-  axesHelper2.scale.x = 1e5
-  axesHelper2.scale.z = 1e5
-  axesHelper2.renderOrder = 997
-  axesHelper2.name = "Axes2"
-  axesHelper1.visible = props.axes
+  const axesHelper2 = new THREE.AxesHelper(5);
+  axesHelper2.position.set(center.x, 0, center.z);
+  axesHelper2.rotateY(-Math.PI / 2);
+  axesHelper2.scale.x = 1e5;
+  axesHelper2.scale.z = 1e5;
+  axesHelper2.renderOrder = 997;
+  axesHelper2.name = 'Axes2';
+  axesHelper1.visible = props.axes;
 
-  const gridHelper = new THREE.GridHelper( 1000, 100, 0xCACACA, 0xCACACA ); // this needs to be updated so it is set to 1m
-  gridHelper.position.set(x, 0, z);
+  // this needs to be updated so it is set to 1m
+  const gridHelper = new THREE.GridHelper(1000, 100, 0xCA_CA_CA, 0xCA_CA_CA);
+  gridHelper.position.set(center.x, 0, center.z);
   gridHelper.renderOrder = 996;
-  gridHelper.name = "Grid"
-  gridHelper.visible = props.axes
+  gridHelper.name = 'Grid';
+  gridHelper.visible = props.axes;
 
-  // add objects to scene  
+  // add objects to scene
   scene.add(axesHelper1);
   scene.add(axesHelper2);
   scene.add(gridHelper);
-}
 
-
-const updatePose = async (newPose: commonApi.Pose) => {
-  const x = newPose.getX();
-  const z = newPose.getZ();
-  const baseMarker = scene.getObjectByName('Base') ?? await makeMarker(baseUrl, "Base", 0.04)
-  baseMarker.position.set(x+.35, 0, z-.55)  
-
-  if (props.destExists) {
-    const x = props.destVector!.x
-    const z = props.destVector!.z
-    const destMarker = scene.getObjectByName('Marker') ?? await makeMarker(destUrl, "Marker", 0.1)
-    destMarker.position.set(x +.6, 0, z-1.24)
-  }
-  
+  scene.add(points);
+  scene.add(intersectionPlane);
+  updatePose(props.pose!);
 };
 
 onMounted(() => {
@@ -270,31 +281,31 @@ onUnmounted(() => {
 
 watch(() => [props.destVector?.x, props.destVector?.z, props.destExists], async () => {
   if (props.destVector && props.destExists) {
-    const marker =  scene.getObjectByName('Marker') ?? await makeMarker(destUrl, "Marker", 0.1)
-    marker.position.set(props.destVector.x +1.2, 0, props.destVector.z-2.44)
+    const marker = scene.getObjectByName('Marker') ?? await makeMarker(destUrl, 'Marker', 0.1);
+    marker.position.set(props.destVector.x + 1.2, 0, props.destVector.z - 2.44);
   }
   if (!props.destExists) {
-    const marker =  scene.getObjectByName('Marker')
-    if (marker !== undefined){
-      scene.remove(marker)
+    const marker = scene.getObjectByName('Marker');
+    if (marker !== undefined) {
+      scene.remove(marker);
     }
   }
-})
+});
 
 watch(() => props.axes, () => {
-  const ax1 =  scene.getObjectByName('Axes1')
-  if (ax1 != undefined) {
-    ax1.visible = props.axes
+  const ax1 = scene.getObjectByName('Axes1');
+  if (ax1 !== undefined) {
+    ax1.visible = props.axes;
   }
-  
-  const ax2 =  scene.getObjectByName('Axes2')
-  if (ax2 != undefined) {
-    ax2.visible = props.axes
+
+  const ax2 = scene.getObjectByName('Axes2');
+  if (ax2 !== undefined) {
+    ax2.visible = props.axes;
   }
-  
-  const grid = scene.getObjectByName('Grid')
-  if (grid != undefined) {
-    grid.visible = props.axes
+
+  const grid = scene.getObjectByName('Grid');
+  if (grid !== undefined) {
+    grid.visible = props.axes;
   }
 });
 
@@ -321,24 +332,75 @@ watch(() => props.pointCloudUpdateCount, () => {
 </script>
 
 <template>
-    <div
-      ref="container"
-      class="pcd-container relative w-full"
-    >
-      <p class="absolute left-3 top-3 bg-white text-xs">
-        Grid set to 1 meter
-      </p>
-      <div class="absolute right-3 top-3">
-        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="10" height="10" rx="5" fill="#BE3536"/>
-        <path d="M4.66278 6.032H4.51878L2.76678 2.4H3.51878L4.95078 5.456H5.04678L6.47878 2.4H7.23078L5.47878 6.032H5.33478V8H4.66278V6.032Z" fill="#FCECEA"/>
-        <rect x="20" y="20" width="10" height="10" rx="5" fill="#0066CC"/>
-        <path d="M23.6708 22.4L24.9268 24.88H25.0708L26.3268 22.4H27.0628L25.6628 25.144V25.24L27.0628 28H26.3268L25.0708 25.504H24.9268L23.6708 28H22.9348L24.3348 25.24V25.144L22.9348 22.4H23.6708Z" fill="#E1F3FF"/>
-        <rect x="4" y="9" width="2" height="17" fill="#BE3536"/>
-        <rect x="21" y="24" width="2" height="17" transform="rotate(90 21 24)" fill="#0066CC"/>
-        <rect x="0.5" y="20.5" width="9" height="9" rx="4.5" fill="#E0FAE3"/>
-        <rect x="0.5" y="20.5" width="9" height="9" rx="4.5" stroke="#3D7D3F"/>
-        </svg>
-      </div>
+  <div
+    ref="container"
+    class="pcd-container relative w-full"
+  >
+    <p class="absolute left-3 top-3 bg-white text-xs">
+      Grid set to 1 meter
+    </p>
+    <div class="absolute right-3 top-3">
+      <svg
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          width="10"
+          height="10"
+          rx="5"
+          fill="#BE3536"
+        />
+        <path
+          d="M4.66278 6.032H4.51878L2.76678 2.4H3.51878L4.95078 5.456H5.04678L6.47878 2.4H7.23078L5.47878 6.032H5.33478V8H4.66278V6.032Z"
+          fill="#FCECEA"
+        />
+        <rect
+          x="20"
+          y="20"
+          width="10"
+          height="10"
+          rx="5"
+          fill="#0066CC"
+        />
+        <path
+          d="M23.6708 22.4L24.9268 24.88H25.0708L26.3268 22.4H27.0628L25.6628 25.144V25.24L27.0628 28H26.3268L25.0708 25.504H24.9268L23.6708 28H22.9348L24.3348 25.24V25.144L22.9348 22.4H23.6708Z"
+          fill="#E1F3FF"
+        />
+        <rect
+          x="4"
+          y="9"
+          width="2"
+          height="17"
+          fill="#BE3536"
+        />
+        <rect
+          x="21"
+          y="24"
+          width="2"
+          height="17"
+          transform="rotate(90 21 24)"
+          fill="#0066CC"
+        />
+        <rect
+          x="0.5"
+          y="20.5"
+          width="9"
+          height="9"
+          rx="4.5"
+          fill="#E0FAE3"
+        />
+        <rect
+          x="0.5"
+          y="20.5"
+          width="9"
+          height="9"
+          rx="4.5"
+          stroke="#3D7D3F"
+        />
+      </svg>
     </div>
+  </div>
 </template>
