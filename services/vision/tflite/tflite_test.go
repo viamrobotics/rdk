602fa0d0633b532a4fdaf867099b8c9a845f8c1f
Bijan Haney <bijan@viam.com>
2023-04-10 18:35:48 -0400

    WIP adds tflite temporarily

--- services/vision/tflite/tflite_test.go
+++ services/vision/tflite/tflite_test.go
@@ -37,3 +37,28 @@ func TestNewTfLiteDetector(t *testing.T) {
 	test.That(t, goutils.TryClose(ctx, got2), test.ShouldBeNil)
 
 }
+
+func TestNewTfLiteClassifier(t *testing.T) {
+	ctx := context.Background()
+	// Test that a classifier would give an expected output on the lion image
+	pic, err := rimage.NewImageFromFile(artifact.MustPath("vision/tflite/lion.jpeg"))
+	test.That(t, err, test.ShouldBeNil)
+	modelLoc := artifact.MustPath("vision/tflite/effnet0.tflite")
+	cfg := &TFLiteClassifierConfig{
+		ModelPath:  modelLoc,
+		LabelPath:  nil,
+		NumThreads: 1,
+	}
+	r := &inject.Robot{}
+	got2, err := registerTFLiteClassifier(ctx, "tf_class", cfg, r, golog.NewTestLogger(t))
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, got2, test.ShouldNotBeNil)
+
+	outClass, err := got2.Classifications(ctx, pic, 5, nil)
+	test.That(t, err, test.ShouldBeNil)
+	bestOut, err := outClass.TopN(1)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, bestOut[0].Label(), test.ShouldResemble, "291")
+	test.That(t, bestOut[0].Score(), test.ShouldBeGreaterThan, 0.82)
+	test.That(t, goutils.TryClose(ctx, got2), test.ShouldBeNil)
+}
