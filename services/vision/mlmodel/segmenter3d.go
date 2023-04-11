de4a57c8184a358275ee8d515e3cde95147c5bd7
Bijan Haney <bijan@viam.com>
2023-04-11 18:30:01 -0400

    adds stub functions

---
+++ services/vision/mlmodel/segmenter3d.go
@@ -0,0 +1,10 @@
+package mlmodel
+
+import (
+	"go.viam.com/rdk/services/mlmodel"
+	"go.viam.com/rdk/vision/segmentation"
+)
+
+func attemptToBuild3DSegmenter(mlm mlmodel.Service) (segmentation.Segmenter, error) {
+	return nil, nil
+}
