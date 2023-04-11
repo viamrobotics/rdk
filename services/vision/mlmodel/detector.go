93f1a4b9fa28a1273e46aaa1ad2c81983aeb588d
Bijan Haney <bijan@viam.com>
2023-04-11 18:30:01 -0400

    adds stub functions

---
+++ services/vision/mlmodel/detector.go
@@ -0,0 +1,10 @@
+package mlmodel
+
+import (
+	"go.viam.com/rdk/services/mlmodel"
+	"go.viam.com/rdk/vision/objectdetection"
+)
+
+func attemptToBuildDetector(mlm mlmodel.Service) (objectdetection.Detector, error) {
+	return nil, nil
+}
