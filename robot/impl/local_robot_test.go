cba4c407222fea123c10e9dd785e3203520639e8
Cheuk Tse <cheuk@viam.com>
2023-04-10 20:06:36 -0400

    lint

--- robot/impl/local_robot_test.go
+++ robot/impl/local_robot_test.go
@@ -19,7 +19,6 @@ import (
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
-
 	// registers all components.
 	commonpb "go.viam.com/api/common/v1"
 	armpb "go.viam.com/api/component/arm/v1"
