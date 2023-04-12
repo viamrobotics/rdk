4b84ce8253953d4042d4c101f7f4f5132b301888
Bijan Haney <bijan@viam.com>
2023-04-12 10:51:54 -0400

    3d segmenters not supported

--- services/vision/mlvision/segmenter3d.go
+++ services/vision/mlvision/segmenter3d.go
@@ -1,10 +1,12 @@
 package mlvision
 
 import (
+	"errors"
+
 	"go.viam.com/rdk/services/mlmodel"
 	"go.viam.com/rdk/vision/segmentation"
 )
 
 func attemptToBuild3DSegmenter(mlm mlmodel.Service) (segmentation.Segmenter, error) {
-	return nil, nil
+	return nil, errors.New("vision 3D segmenters from ML models are currently not supported")
 }
