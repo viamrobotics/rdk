9fa0165c33fe57a498179a8fd5965c174d5a65c1
Peter LoVerso <biotinker@gmail.com>
2023-04-12 09:34:24 -0700

    Reorganize tests slightly

--- pointcloud/basic_octree_utils_test.go
+++ pointcloud/basic_octree_utils_test.go
@@ -358,45 +358,46 @@ func TestBasicOctreeCollision(t *testing.T) {
 
 	test.That(t, startPC.Size(), test.ShouldEqual, basicOct.Size())
 
-	// create a non-colliding obstacle far away from any octree point
-	far, err := spatialmath.NewBox(spatialmath.NewZeroPose(), r3.Vector{1, 2, 3}, "far")
-	test.That(t, err, test.ShouldBeNil)
-	// create a non-colliding obstacle near an octree point
-	near, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2443, 0, 3855}), r3.Vector{1, 2, 3}, "near")
-	test.That(t, err, test.ShouldBeNil)
-	// create a colliding obstacle overlapping an octree point
-	hit, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2443, 0, 3855}), r3.Vector{12, 2, 30}, "hit")
-	test.That(t, err, test.ShouldBeNil)
-	// create a colliding obstacle overlapping an octree point that has sub-threshold probability
-	lowprob, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2471, 0, 3790}), r3.Vector{3, 2, 3}, "lowprob")
-	test.That(t, err, test.ShouldBeNil)
-
 	t.Run("no collision with box far from octree points", func(t *testing.T) {
+		// create a non-colliding obstacle far away from any octree point
+		far, err := spatialmath.NewBox(spatialmath.NewZeroPose(), r3.Vector{1, 2, 3}, "far")
+		test.That(t, err, test.ShouldBeNil)
 		collides, err := basicOct.CollidesWithGeometry(far, 80, 1.0)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, collides, test.ShouldBeFalse)
 	})
 
 	t.Run("no collision with box near octree points", func(t *testing.T) {
+		// create a non-colliding obstacle near an octree point
+		near, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2443, 0, 3855}), r3.Vector{1, 2, 3}, "near")
+		test.That(t, err, test.ShouldBeNil)
 		collides, err := basicOct.CollidesWithGeometry(near, 80, 1.0)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, collides, test.ShouldBeFalse)
 	})
 
 	t.Run("collision with box near octree points when a large buffer is used", func(t *testing.T) {
-		// Test `near` again with a large buffer that should now collide
+		// create a non-colliding obstacle near an octree point
+		near, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2443, 0, 3855}), r3.Vector{1, 2, 3}, "near")
+		test.That(t, err, test.ShouldBeNil)
 		collides, err := basicOct.CollidesWithGeometry(near, 80, 10.0)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, collides, test.ShouldBeTrue)
 	})
 
 	t.Run("no collision with box overlapping low-probability octree points", func(t *testing.T) {
+		// create a colliding obstacle overlapping an octree point that has sub-threshold probability
+		lowprob, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2471, 0, 3790}), r3.Vector{3, 2, 3}, "lowprob")
+		test.That(t, err, test.ShouldBeNil)
 		collides, err := basicOct.CollidesWithGeometry(lowprob, 80, 1.0)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, collides, test.ShouldBeFalse)
 	})
 
 	t.Run("collision with box overlapping octree points", func(t *testing.T) {
+		// create a colliding obstacle overlapping an octree point
+		hit, err := spatialmath.NewBox(spatialmath.NewPoseFromPoint(r3.Vector{-2443, 0, 3855}), r3.Vector{12, 2, 30}, "hit")
+		test.That(t, err, test.ShouldBeNil)
 		collides, err := basicOct.CollidesWithGeometry(hit, 80, 1.0)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, collides, test.ShouldBeTrue)
