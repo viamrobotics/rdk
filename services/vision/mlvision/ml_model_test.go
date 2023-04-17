7795bcf868aa408d12b70bc83f3ae6637aeff326
Khari Jarrett <khari@viam.com>
2023-04-17 16:58:34 -0400

    add tflite ml classifier test for vision

--- services/vision/mlvision/ml_model_test.go
+++ services/vision/mlvision/ml_model_test.go
@@ -2,7 +2,6 @@ package mlvision
 
 import (
 	"context"
-	"github.com/nfnt/resize"
 	"go.viam.com/rdk/services/mlmodel/tflitecpu"
 	"testing"
 
@@ -17,22 +16,19 @@ func TestNewMLDetector(t *testing.T) {
 	// Set it up as a ML Model
 
 	ctx := context.Background()
-	// r := &inject.Robot{}
 	modelLoc := artifact.MustPath("vision/tflite/effdet0.tflite")
 	labelLoc := artifact.MustPath("vision/tflite/effdetlabels.txt")
-	// name := "myMLDet"
 	cfg := tflitecpu.TFLiteConfig{ // detector config
 		ModelPath:  modelLoc,
 		NumThreads: 2,
 		LabelPath:  &labelLoc,
 	}
-	// visCfg := MLModelConfig{ModelName: "myMLDet"}
+
 	pic, err := rimage.NewImageFromFile(artifact.MustPath("vision/tflite/dogscute.jpeg"))
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, pic, test.ShouldNotBeNil)
 
 	// Test that a detector would give the expected output on the dog image
-	// Creating the model should populate model and attrs, but not metadata
 	out, err := tflitecpu.NewTFLiteCPUModel(ctx, &cfg, "myMLDet")
 	check, err := out.Metadata(ctx)
 	test.That(t, check, test.ShouldNotBeNil)
@@ -41,14 +37,6 @@ func TestNewMLDetector(t *testing.T) {
 	test.That(t, check.Outputs[0].Name, test.ShouldResemble, "location")
 	test.That(t, check.Outputs[1].Name, test.ShouldResemble, "category")
 	test.That(t, check.Outputs[1].Extra["labels"], test.ShouldNotBeNil)
-
-	resized := resize.Resize(320, 320, pic, resize.Bilinear)
-
-	inMap := make(map[string]interface{})
-	inMap["image"] = rimage.ImageToUInt8Buffer(resized)
-	gotOut, err := out.Infer(ctx, inMap)
-	test.That(t, err, test.ShouldBeNil)
-	test.That(t, gotOut, test.ShouldNotBeNil)
 
 	gotDetector, err := attemptToBuildDetector(out)
 	test.That(t, err, test.ShouldBeNil)
@@ -60,5 +48,47 @@ func TestNewMLDetector(t *testing.T) {
 	test.That(t, gotDetections[1].Score(), test.ShouldBeGreaterThan, 0.7)
 	test.That(t, gotDetections[0].Label(), test.ShouldResemble, "Dog")
 	test.That(t, gotDetections[1].Label(), test.ShouldResemble, "Dog")
+
+}
+
+func TestNewMLClassifier(t *testing.T) {
+	// Test that a detector would give an expected output on the dog image
+	// Set it up as a ML Model
+
+	ctx := context.Background()
+	// r := &inject.Robot{}
+	modelLoc := artifact.MustPath("vision/tflite/effnet0.tflite")
+	labelLoc := artifact.MustPath("vision/tflite/imagenetlabels.txt")
+	// name := "myMLDet"
+	cfg := tflitecpu.TFLiteConfig{ // detector config
+		ModelPath:  modelLoc,
+		NumThreads: 2,
+		LabelPath:  &labelLoc,
+	}
+	pic, err := rimage.NewImageFromFile(artifact.MustPath("vision/tflite/lion.jpeg"))
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, pic, test.ShouldNotBeNil)
+
+	// Test that a classifier would give the expected result on the lion image
+	out, err := tflitecpu.NewTFLiteCPUModel(ctx, &cfg, "myMLClassif")
+	check, err := out.Metadata(ctx)
+	test.That(t, check, test.ShouldNotBeNil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, check.Inputs[0].Name, test.ShouldResemble, "image")
+	test.That(t, check.Outputs[0].Name, test.ShouldResemble, "probability")
+	test.That(t, check.Outputs[0].Extra["labels"], test.ShouldNotBeNil)
+
+	gotClassifier, err := attemptToBuildClassifier(out)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, gotClassifier, test.ShouldNotBeNil)
+
+	gotClassifications, err := gotClassifier(ctx, pic)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, gotClassifications, test.ShouldNotBeNil)
+	gotTop, err := gotClassifications.TopN(5)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, gotTop, test.ShouldNotBeNil)
+	test.That(t, gotTop[0].Label(), test.ShouldContainSubstring, "lion")
+	test.That(t, gotTop[0].Score(), test.ShouldBeGreaterThan, 200)
 
 }
