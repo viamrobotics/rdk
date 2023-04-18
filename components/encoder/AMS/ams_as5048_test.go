95122ea605611c806e743c607a4dbb2e9be742a6
martha-johnston <martha.johnston@viam.com>
2023-04-18 16:34:53 -0400

    test reset by checking byte map

--- components/encoder/AMS/ams_as5048_test.go
+++ components/encoder/AMS/ams_as5048_test.go
@@ -20,6 +20,8 @@ import (
 
 var model = resource.NewDefaultModel("AMS-AS5048")
 
+var writeData = make(map[byte]byte)
+
 func TestConvertBytesToAngle(t *testing.T) {
 	// 180 degrees
 	msB := byte(math.Pow(2.0, 7.0))
@@ -122,21 +124,75 @@ func TestAMSEncoder(t *testing.T) {
 		test.That(t, pos, test.ShouldAlmostEqual, 142, 0.1)
 		test.That(t, posType, test.ShouldEqual, 2)
 	})
+}
+
+func setupDependenciesWithWrite(mockData []byte) (config.Component, registry.Dependencies) {
+	testBoardName := "board"
+	i2cName := "i2c"
+
+	i2cConf := &I2CAttrConfig{
+		I2CBus:  i2cName,
+		I2CAddr: 64,
+	}
+
+	cfg := config.Component{
+		Name:  "encoder",
+		Model: model,
+		Type:  encoder.SubtypeName,
+		ConvertedAttributes: &AttrConfig{
+			BoardName:      testBoardName,
+			ConnectionType: "i2c",
+			I2CAttrConfig:  i2cConf,
+		},
+	}
+
+	i2cHandle := &inject.I2CHandle{}
+	i2cHandle.ReadByteDataFunc = func(ctx context.Context, register byte) (byte, error) {
+		return mockData[register], nil
+	}
+	i2cHandle.WriteByteDataFunc = func(ctx context.Context, b1, b2 byte) error {
+		writeData[b1] = b2
+		return nil
+	}
+	i2cHandle.CloseFunc = func() error { return nil }
+	mockBoard := &inject.Board{}
+	mockBoard.I2CByNameFunc = func(name string) (board.I2C, bool) {
+		i2c := &inject.I2C{}
+		i2c.OpenHandleFunc = func(addr byte) (board.I2CHandle, error) {
+			return i2cHandle, nil
+		}
+		return i2c, true
+	}
+	return cfg, registry.Dependencies{
+		resource.NameFromSubtype(board.Subtype, testBoardName): mockBoard,
+	}
+}
+
+func TestAMSEncoderReset(t *testing.T) {
+	ctx := context.Background()
+
+	positionMockData := make([]byte, 256)
+	positionMockData[0xFE] = 100
+	positionMockData[0xFF] = 60
+
+	logger := golog.NewTestLogger(t)
+	cfg, deps := setupDependenciesWithWrite(positionMockData)
+	enc, err := newAS5048Encoder(ctx, deps, cfg, logger)
+	test.That(t, err, test.ShouldBeNil)
+	defer utils.TryClose(context.Background(), enc)
+
 	t.Run("test reset", func(t *testing.T) {
 		testutils.WaitForAssertion(t, func(tb testing.TB) {
 			enc.ResetPosition(ctx, nil)
 		})
 
 		testutils.WaitForAssertion(t, func(tb testing.TB) {
-			pos, posType, _ := enc.GetPosition(ctx, encoder.PositionTypeDEGREES.Enum(), nil)
-			test.That(t, pos, test.ShouldAlmostEqual, 0, 0.1)
-			test.That(t, posType, test.ShouldEqual, 2)
-		})
-
-		testutils.WaitForAssertion(t, func(tb testing.TB) {
-			pos, posType, _ := enc.GetPosition(ctx, encoder.PositionTypeTICKS.Enum(), nil)
+			pos, posType, _ := enc.GetPosition(ctx, encoder.PositionTypeUNSPECIFIED.Enum(), nil)
 			test.That(t, pos, test.ShouldAlmostEqual, 0, 0.1)
 			test.That(t, posType, test.ShouldEqual, 1)
 		})
+
+		test.That(t, writeData[0x16], test.ShouldEqual, byte(100))
+		test.That(t, writeData[0x17], test.ShouldEqual, byte(60))
 	})
 }
