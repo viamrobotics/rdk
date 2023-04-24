362a7d5a79b0f997343f3649bb2c60321e7c43bb
Khari Jarrett <khari@viam.com>
2023-04-24 13:37:02 -0400

    edit comment

--- services/vision/mlvision/segmenter3d.go
+++ services/vision/mlvision/segmenter3d.go
@@ -7,6 +7,7 @@ import (
 	"go.viam.com/rdk/vision/segmentation"
 )
 
+// TODO: RSDK-2665, build 3D segmenter from ML models.
 func attemptToBuild3DSegmenter(mlm mlmodel.Service) (segmentation.Segmenter, error) {
 	return nil, errors.New("vision 3D segmenters from ML models are currently not supported")
 }
