9f68b90cb2dab5c01510f301426c9fc9260db41d
James Otting <james@viam.com>
2023-04-25 14:36:27 -0500

    Fix static release script

--- etc/packaging/static/package_release.sh
+++ etc/packaging/static/package_release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e
-cd $(realpath $0)
-source "../../utils.sh"
+SELF=$(realpath $0)
+source "$(dirname $SELF)/../../utils.sh"
 
 if get_version_tag > /dev/null
 then
-	BIN_SRC="../../../bin/$(uname -s)-$(uname -m)"
+	BIN_SRC="$(dirname $SELF)/../../../bin/$(uname -s)-$(uname -m)"
 	mkdir -p deploy
-	cp "${BIN_SRC}/viam-server" "deploy/viam-server-$(get_version_tag)-$(uname -m)"
-	cp "${BIN_SRC}/viam-server" "deploy/viam-server-stable-$(uname -m)"
+	cp "${BIN_SRC}/viam-server" "$(dirname $SELF)/deploy/viam-server-$(get_version_tag)-$(uname -m)"
+	cp "${BIN_SRC}/viam-server" "$(dirname $SELF)/deploy/viam-server-stable-$(uname -m)"
 fi
