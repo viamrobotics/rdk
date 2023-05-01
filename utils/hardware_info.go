94bf5dcc5f303cd3d80262963f5c830bad6905ac
susmitaSanyal <susmita@viam.com>
2023-05-01 17:13:14 -0400

    comments

--- utils/hardware_info.go
+++ utils/hardware_info.go
@@ -4,11 +4,13 @@ import (
 	"runtime"
 )
 
+// returns the architecture of the board
 func GetArchitectureInfo() string {
 	var arch string = runtime.GOARCH
 	return arch
 }
 
+// returns the OS of the board.
 func GetOSInfo() string {
 	var os string = runtime.GOOS
 	return os
