dd409286e67fd3bf2d3924429ac6dfc091b7e044
Ray Bjorkman <raymond@viam.com>
2023-05-02 12:18:45 -0400

    address erics comments

--- internal/resource.go
+++ internal/resource.go
@@ -21,9 +21,8 @@ func (c componentDependencyWildcardMatcher) notActuallyImplementedYet() {}
 // SLAMDependencyWildcardMatcher is used internally right now for lack of a better way to
 // "select" slam services that another resource is dependency on. Usage of this is an
 // anti-pattern and a better matcher system should exist.
-var SLAMDependencyWildcardMatcher = ResourceMatcher(slamDependencyWildcardMatcher("*:service:slam/*:*"))
+var SLAMDependencyWildcardMatcher = ResourceMatcher(slamDependencyWildcardMatcher("rdk:service:slam/*:*"))
 
 type slamDependencyWildcardMatcher string
 
-// TODO:  @erd is it time to do this now?
 func (s slamDependencyWildcardMatcher) notActuallyImplementedYet() {}
