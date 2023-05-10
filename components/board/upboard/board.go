627dd95cbfee66d46f79e10c8e3c4da8da7aaa67
susmitaSanyal <118220271+susmitaSanyal@users.noreply.github.com>
2023-05-10 15:17:12 -0400

    X86 support on genericlinux (#2340)

---
+++ components/board/upboard/board.go
@@ -0,0 +1,33 @@
+// Package upboard implements an Intel based board.
+package upboard
+
+// This is experimental
+/*
+	Datasheet: https://github.com/up-board/up-community/wiki/Pinout_UP4000
+	Supported board: UP4000
+*/
+
+import (
+	"github.com/edaniels/golog"
+	"github.com/pkg/errors"
+	"periph.io/x/host/v3"
+
+	"go.viam.com/rdk/components/board/genericlinux"
+)
+
+const modelName = "upboard"
+
+func init() {
+	if _, err := host.Init(); err != nil {
+		golog.Global().Debugw("error initializing host", "error", err)
+	}
+
+	gpioMappings, err := genericlinux.GetGPIOBoardMappings(modelName, boardInfoMappings)
+	var noBoardErr genericlinux.NoBoardFoundError
+	if errors.As(err, &noBoardErr) {
+		golog.Global().Debugw("error getting up board GPIO board mapping", "error", err)
+	}
+
+	// Not using Periph io for GPIO
+	genericlinux.RegisterBoard(modelName, gpioMappings, false)
+}
