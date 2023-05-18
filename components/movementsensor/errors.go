c4e7c02a2df9295f875ca240b02cc08f79af9d49
susmitaSanyal <susmita@viam.com>
2023-05-18 13:19:22 -0400

    Adding new error file

--- components/movementsensor/errors.go
+++ components/movementsensor/errors.go
@@ -6,13 +6,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-func addressReadError(err error, address byte, bus, board string) error {
+func AddressReadError(err error, address byte, bus, board string) error {
 	msg := fmt.Sprintf("can't read from I2C address %d on bus %s of board %s",
 		address, bus, board)
 	return errors.Wrap(err, msg)
 }
 
-func unexpectedDeviceError(address, defaultAddress byte, deviceName string) error {
+func UnexpectedDeviceError(address, defaultAddress byte, deviceName string) error {
 	return errors.Errorf("unexpected non-%s device at address %d: response '%d'",
 		deviceName, address, defaultAddress)
 }
