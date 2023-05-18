d3b6f7ee4de71044c63b4f10f1c8a15978a7c8cb
susmitaSanyal <susmita@viam.com>
2023-05-18 12:44:44 -0400

    adding errors to movement sensor.

---
+++ components/movementsensor/errors.go
@@ -0,0 +1,18 @@
+package movementsensor
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+func addressReadError(err error, address byte, bus, board string) error {
+	msg := fmt.Sprintf("can't read from I2C address %d on bus %s of board %s",
+		address, bus, board)
+	return errors.Wrap(err, msg)
+}
+
+func unexpectedDeviceError(address, defaultAddress byte, deviceName string) error {
+	return errors.Errorf("unexpected non-%s device at address %d: response '%d'",
+		deviceName, address, defaultAddress)
+}
