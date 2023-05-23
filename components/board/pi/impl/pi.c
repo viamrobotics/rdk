a34e57a0dcf8ccfc1c314a080d0a7cc6e8ec7b80
Alan Davidson <alan@viam.com>
2023-05-23 10:18:00 -0400

    use spaces, not tabs, in C

--- components/board/pi/impl/pi.c
+++ components/board/pi/impl/pi.c
@@ -13,19 +13,19 @@ void interruptCallback(int gpio, int level, uint32_t tick) {
 
 int setupInterrupt(int gpio) {
     int result = gpioSetMode(gpio, PI_INPUT);
-	if (result != 0) {
-		return result;
-	}
+    if (result != 0) {
+        return result;
+    }
     result = gpioSetPullUpDown(gpio, PI_PUD_UP); // should this be configurable?
-	if (result != 0) {
-		return result;
-	}
+    if (result != 0) {
+        return result;
+    }
     result = gpioSetAlertFunc(gpio, interruptCallback);
-	return result;
+    return result;
 }
 
 int teardownInterrupt(int gpio) {
     int result = gpioSetAlertFunc(gpio, NULL);
     // Do we need to unset the pullup resistors?
-	return result;
+    return result;
 }
