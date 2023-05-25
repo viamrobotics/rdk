872d4abd7fcb8e38a0a7694751bbf35f84533f44
Sierra Guequierre <sguequierre@gmail.com>
2023-05-25 16:22:13 +0200

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// The Robot Development Kit (RDK) is the open-source, on-robot portion of the Viam platform that provides viam-server and the Go SDK.
+// Package rdk is the Robot Development Kit, the open-source, on-robot portion of the Viam platform that provides viam-server and the Go SDK.
 package rdk
