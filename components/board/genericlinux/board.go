2aeae5892ce068855cf6acd3450654e40108dd39
Natalie Huang <natalie.huang@viam.com>
2023-06-05 12:14:40 -0400

    address comments

--- components/board/genericlinux/board.go
+++ components/board/genericlinux/board.go
@@ -586,18 +586,16 @@ func (b *sysfsBoard) GPIOPinByName(pinName string) (board.GPIOPin, error) {
 		return b.periphGPIOPinByName(pinName)
 	}
 	// Otherwise, the pins are stored in b.gpios.
-	pin, ok := b.gpios[pinName]
-	if !ok {
-		// check if pin is a digital interrupt
-		interrupt, interruptOk := b.interrupts[pinName]
+	if pin, ok := b.gpios[pinName]; ok {
+		return pin, nil
+	}
 
-		if !interruptOk {
-			return nil, errors.Errorf("cannot find GPIO for unknown pin: %s", pinName)
-		}
-
+	// check if pin is a digital interrupt
+	if interrupt, interruptOk := b.interrupts[pinName]; interruptOk {
 		return &gpioInterruptWrapperPin{*interrupt}, nil
 	}
-	return pin, nil
+
+	return nil, errors.Errorf("cannot find GPIO for unknown pin: %s", pinName)
 }
 
 func (b *sysfsBoard) periphGPIOPinByName(pinName string) (board.GPIOPin, error) {
