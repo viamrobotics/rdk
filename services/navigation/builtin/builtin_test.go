d38160014a1129fa66be05a3cf20f24ce7e04f38
Nick Franczak <nicholasgfranczak@gmail.com>
2023-06-06 11:39:32 -0400

    update test

--- services/navigation/builtin/builtin_test.go
+++ services/navigation/builtin/builtin_test.go
@@ -33,9 +33,9 @@ func setupNavigationServiceFromConfig(t *testing.T, configFilename string) (navi
 }
 
 func TestNavSetup(t *testing.T) {
-	setupNavigationServiceFromConfig(t, "../data/nav_cfg.json")
 	ns, teardown := setupNavigationServiceFromConfig(t, "../data/nav_cfg.json")
 	defer teardown()
+
 	ctx := context.Background()
 
 	navMode, err := ns.Mode(ctx, nil)
