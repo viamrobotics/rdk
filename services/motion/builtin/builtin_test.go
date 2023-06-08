add2327edd01af6eb95f05314cc6ffa8af5c7639
Nick Franczak <nicholasgfranczak@gmail.com>
2023-06-08 13:24:46 -0400

    get test to work + remove rotation from limits

--- services/motion/builtin/builtin_test.go
+++ services/motion/builtin/builtin_test.go
@@ -233,6 +233,7 @@ func TestMoveOnGlobe(t *testing.T) {
 
 	motionCfg := make(map[string]interface{})
 	motionCfg["motion_profile"] = "position_only"
+	motionCfg["timeout"] = 20.
 
 	t.Run("ensure success to a nearby geo point", func(t *testing.T) {
 		// TODO: This test should be updated such that we can navigate to an
@@ -280,17 +281,18 @@ func TestMoveOnGlobe(t *testing.T) {
 
 	t.Run("fail because of long wall", func(t *testing.T) {
 		boxPose := spatialmath.NewPoseFromPoint(r3.Vector{50, 0, 0})
-		boxDims := r3.Vector{2, 100000, 10}
+		boxDims := r3.Vector{2, 1000, 10}
 		geometries, err := spatialmath.NewBox(boxPose, boxDims, "wall")
 		test.That(t, err, test.ShouldBeNil)
 		geoObstacle := spatialmath.NewGeoObstacle(geo.NewPoint(40.7, -73.98), []spatialmath.Geometry{geometries})
 
-		success, _ := ms.MoveOnGlobe(
+		success, err := ms.MoveOnGlobe(
 			context.Background(),
 			base.Named("test-base"),
 			geo.NewPoint(40.7, -73.9800009),
 			math.NaN(),
 			movementsensor.Named("test-gps"),
+			// nil,
 			[]*spatialmath.GeoObstacle{geoObstacle},
 			math.NaN(),
 			math.NaN(),
