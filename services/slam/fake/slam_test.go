ece4859bd0a0baaf7dc1de8572c359eb9f270975
Nick Sanford <nick.sanford@viam.com>
2023-06-08 14:53:48 -0400

    RSDK-3341-shorten-TestFakeSLAMStateful-remove-skip

--- services/slam/fake/slam_test.go
+++ services/slam/fake/slam_test.go
@@ -43,10 +43,13 @@ func TestFakeSLAMGetPosition(t *testing.T) {
 }
 
 func TestFakeSLAMStateful(t *testing.T) {
-	t.Skip("Skipping this test because it takes too long. Re-enable in RSDK-3341")
 	t.Run("Test getting a PCD map via streaming APIs advances the test data", func(t *testing.T) {
+		orgMaxDataCount := maxDataCount
+		// maxDataCount lowered under test to reduce test runtime
+		maxDataCount = 5
 		slamSvc := &SLAM{Named: slam.Named("test").AsNamed(), logger: golog.NewTestLogger(t)}
 		verifyGetPointCloudMapStateful(t, slamSvc)
+		maxDataCount = orgMaxDataCount
 	})
 }
 
