d448cfe84017c18313b03e1b29211ccbae933387
Peter LoVerso <biotinker@gmail.com>
2023-06-12 13:06:54 -0700

    Update test

--- motionplan/tpspace/ptgGridSim_test.go
+++ motionplan/tpspace/ptgGridSim_test.go
@@ -1,50 +1,30 @@
 package tpspace
 
-// ~ import(
-// ~ "testing"
-// ~ "fmt"
+import(
+	"testing"
+	
+	"go.viam.com/test"
+)
 
-// ~ )
+var defaultPTGs = []func(float64, float64, float64) PrecomputePTG {
+	NewCirclePTG,
+	NewCCPTG,
+	NewCCSPTG,
+	NewCSPTG,
+	NewAlphaPTG,
+}
 
-// ~ var defaultPTGs = []func(float64, float64, float64) PrecomputePTG {
-// ~ NewCirclePTG,
-// ~ NewCCPTG,
-// ~ NewCCSPTG,
-// ~ NewCSPTG,
-// ~ NewAlphaPTG,
-// ~ }
+var defaultMps = 1.
+var defaultDps = 45.
 
-// ~ var defaultMps = 1.
-// ~ var defaultDps = 45.
-
-// ~ func TestSim(t *testing.T) {
-
-// ~ for _, ptg := range defaultPTGs {
-
-// ~ for _, x := range ptgPrecomp.Trajectory(i) {
-// ~ pt := x.Pose.Point()
-// ~ fmt.Printf("FINALPATH,%f,%f\n", pt.X, pt.Y)
-// ~ }
-// ~ }
-// ~ ptgGen = ptg(defaultMps, defaultDps, -1.)
-// ~ if ptgGen != nil {
-// ~ ptgPrecomp, _ := NewPTGGridSim(ptgGen, defaultAlphaCnt)
-
-// ~ for i := uint(0); i < defaultAlphaCnt; i++ {
-// ~ for _, x := range ptgPrecomp.Trajectory(i) {
-// ~ pt := x.Pose.Point()
-// ~ fmt.Printf("FINALPATH,%f,%f\n", pt.X, pt.Y)
-// ~ }
-// ~ }
-// ~ ptgGen = ptg(defaultMps, defaultDps, -1.)
-// ~ if ptgGen != nil {
-// ~ ptgPrecomp, _ := NewPTGGridSim(ptgGen, defaultAlphaCnt)
-
-// ~ for _, x := range ptgPrecomp.Trajectory(i) {
-// ~ pt := x.Pose.Point()
-// ~ fmt.Printf("FINALPATH,%f,%f\n", pt.X, pt.Y)
-// ~ }
-// ~ }
-// ~ }
-// ~ }
-// ~ }
+func TestSim(t *testing.T) {
+	
+	
+	for _, ptg := range defaultPTGs {
+		
+		ptgGen := ptg(defaultMps, defaultDps, 1.)
+		test.That(t, ptgGen, test.ShouldNotBeNil)
+		_, err := NewPTGGridSim(ptgGen, defaultAlphaCnt)
+		test.That(t, err, test.ShouldNotBeNil)
+	}
+}
