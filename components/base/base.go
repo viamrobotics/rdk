fce629acafbea1a30ebcd0cbe7666c61faa3d98b
JessamyT <jessamy.taylor@gmail.com>
2023-06-15 18:32:02 -0700

    Clarify base coordinate system comments

--- components/base/base.go
+++ components/base/base.go
@@ -49,13 +49,15 @@ type Base interface {
 
 	// Spin spins the robot by a given angle in degrees at a given speed.
 	// If a speed of 0 the base will stop.
+	// A positive angle is measured in degrees to the left.
+	// A positive speed is to the left.
 	// This method blocks until completed or cancelled
 	Spin(ctx context.Context, angleDeg, degsPerSec float64, extra map[string]interface{}) error
 
 	SetPower(ctx context.Context, linear, angular r3.Vector, extra map[string]interface{}) error
 
-	// linear is in mmPerSec
-	// angular is in degsPerSec
+	// linear is in mmPerSec (positive Y moves forwards)
+	// angular is in degsPerSec (positive Z turns to the left)
 	SetVelocity(ctx context.Context, linear, angular r3.Vector, extra map[string]interface{}) error
 
 	Properties(ctx context.Context, extra map[string]interface{}) (Properties, error)
