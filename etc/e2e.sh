0c4692699bd7b4c4b60b3139e8c01c265c4eef35
Ethan Look <ethan.look@viam.com>
2023-06-16 16:03:17 -0400

    APP-2044 Kill child process in test-e2e

--- etc/e2e.sh
+++ etc/e2e.sh
@@ -5,21 +5,34 @@ ROOT_DIR="$DIR/../"
 
 cd $ROOT_DIR
 
+FLAG_OPEN="open"
+FLAG_RUN="run"
+
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -o <open|run>"
+   echo "Usage: $0 -o <$FLAG_OPEN|$FLAG_RUN> [--no-kill]"
    echo -e "\t-o Whether or not to open the cypress UI or just run the tests"
+   echo -e "\t-k Does *not* try to pkill the child test-e2e process. Helpful to avoid pkill in CI."
    exit 1 # Exit script after printing help
 }
 
-while getopts "o:" opt
+KILL_CHILD=true
+
+while getopts "o:k" opt
 do
    case "$opt" in
       o ) OPEN="$OPTARG" ;;
+      k ) KILL_CHILD=false ;;
       ? ) helpFunction ;; 
    esac
 done
+
+if [ -z "$OPEN" ] ; then
+    helpFunction
+elif [ $OPEN != $FLAG_OPEN ] && [ $OPEN != $FLAG_RUN ] ; then
+    helpFunction
+fi
 
 # Run server with test config
 nohup ./bin/test-e2e/server --config web/frontend/cypress/data/test_robot_config.json &
@@ -34,8 +47,8 @@ if [ $OPEN == "open" ] ; then
     npm run cypress -- -c defaultCommandTimeout=30000
 elif [ $OPEN == "run" ] ; then
     npm run cypress:ci -- -c defaultCommandTimeout=30000
-else
-    helpFunction
 fi
 
-
+if [ $KILL_CHILD == true ] ; then
+    pkill -f test-e2e
+fi
