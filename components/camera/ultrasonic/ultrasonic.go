c4d871f5d348e17f7f1e3a7c08d6dd72f5911a06
vpandiarajan20 <vpandiarajan20@gmail.com>
2023-06-22 14:06:48 -0400

    removing extra comments

--- components/camera/ultrasonic/ultrasonic.go
+++ components/camera/ultrasonic/ultrasonic.go
@@ -18,19 +18,9 @@ import (
 
 var model = resource.DefaultModelFamily.WithModel("ultrasonic")
 
-// Config is used for converting config attributes.
-// type Config struct {
-// 	TriggerPin    string `json:"trigger_pin"`
-// 	EchoInterrupt string `json:"echo_interrupt_pin"`
-// 	Board         string `json:"board"`
-// 	TimeoutMs     uint   `json:"timeout_ms,omitempty"`
-// }
-
 type ultrasonicWrapper struct {
 	usSensor sensor.Sensor
 }
-
-// pointcloud and read on usCam struct
 
 func init() {
 	resource.RegisterComponent(
@@ -48,30 +38,26 @@ func init() {
 					return nil, err
 				}
 
-				// usSensor, err := sen
-
 				usSensor, err := ultrasense.NewSensor(ctx, deps, conf.ResourceName(), newConf)
 				if err != nil {
 					return nil, err
 				}
 				usWrapper := ultrasonicWrapper{usSensor: usSensor}
+
 				usVideoSource, err := camera.NewVideoSourceFromReader(ctx, &usWrapper, nil, camera.UnspecifiedStream)
+				if err != nil {
+					return nil, err
+				}
 
 				return camera.FromVideoSource(conf.ResourceName(), usVideoSource), nil
-				// struct embedding
 			},
 		})
 }
 
-// Stream returns a stream that makes a best effort to return consecutive images
-// that may have a MIME type hint dictated in the context via gostream.WithMIMETypeHint.
-// videosource from reader should make stream function
 func (usvs *ultrasonicWrapper) Stream(ctx context.Context, errHandlers ...gostream.ErrorHandler) (gostream.VideoStream, error) {
 	return nil, errors.New("Not yet implemented")
 }
 
-// NextPointCloud returns the next immediately available point cloud, not necessarily one
-// a part of a sequence. In the future, there could be streaming of point clouds.
 func (usvs *ultrasonicWrapper) NextPointCloud(ctx context.Context) (pointCloud.PointCloud, error) {
 
 	readings, err := usvs.usSensor.Readings(ctx, make(map[string]interface{}))
@@ -84,7 +70,7 @@ func (usvs *ultrasonicWrapper) NextPointCloud(ctx context.Context) (pointCloud.P
 		return nil, errors.New("unable to convert distance to float64")
 	}
 	basicData := pointCloud.NewBasicData()
-	basicData.SetValue(1)
+	// basicData.SetValue(1)
 	distVector := pointCloud.NewVector(0, 0, distFloat)
 	fmt.Println("Added point at ", distVector)
 	pcToReturn.Set(distVector, basicData)
@@ -92,8 +78,6 @@ func (usvs *ultrasonicWrapper) NextPointCloud(ctx context.Context) (pointCloud.P
 	return pcToReturn, nil
 }
 
-// Properties returns properties that are intrinsic to the particular
-// implementation of a camera
 func (usvs *ultrasonicWrapper) Properties(ctx context.Context) (camera.Properties, error) {
 	return camera.Properties{SupportsPCD: true, ImageType: camera.UnspecifiedStream}, nil
 }
@@ -104,22 +88,5 @@ func (usvs *ultrasonicWrapper) Close(ctx context.Context) error {
 }
 
 func (usvs *ultrasonicWrapper) Read(ctx context.Context) (image.Image, func(), error) {
-	return nil, nil, errors.New("Not yet implemented")
+	return nil, nil, errors.New("not yet implemented")
 }
-
-// func newCamera(ctx context.Context, deps resource.Dependencies, name resource.Name, config *ultrasense.Config) (camera.Camera, error) {
-// 	usCam := &ultraSonicCamera{
-// 		Named:  name.AsNamed(),
-// 		config: config,
-// 	}
-
-// 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
-// 	s.cancelCtx = cancelCtx
-// 	s.cancelFunc = cancelFunc
-
-// }
-
-// Validate function should already be defined in sensor/ultrasonic.go
-// func (conf *ultrasense.Config) Validate(path string) ([]string, error) {
-// 	return conf.Validate(path)
-// }
