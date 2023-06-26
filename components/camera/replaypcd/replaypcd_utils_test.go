db475d3ce1b7a6cbc5cd5da9d4b7464f14a6a64e
martha-johnston <106617924+martha-johnston@users.noreply.github.com>
2023-06-26 17:44:29 -0400

    Update replaypcd_utils_test.go

--- components/camera/replaypcd/replaypcd_utils_test.go
+++ components/camera/replaypcd/replaypcd_utils_test.go
@@ -46,7 +46,7 @@ type mockDataServiceServer struct {
 func (mDServer *mockDataServiceServer) BinaryDataByIDs(ctx context.Context, req *datapb.BinaryDataByIDsRequest,
 ) (*datapb.BinaryDataByIDsResponse, error) {
 	// Parse request
-	// adding nolint until the corresponding test is fixed to match the newest API version
+	// TODO(RSDK-3795): Update BinaryDataByIDs test to match newest API
 	//nolint: staticcheck
 	fileID := req.FileIds[0]
 
