5992431d053642bbd812976caf129e1ca3c33a86
randhid <rand@viam.com>
2023-06-27 17:59:32 -0400

    Fix merge conflicts

---
+++ components/movementsensor/merged/merged.go
@@ -0,0 +1,380 @@
+// Package merged implements a movementsensor combining movement data from other sensors
+package merged
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"sync"
+
+	"github.com/edaniels/golog"
+	"github.com/golang/geo/r3"
+	geo "github.com/kellydunn/golang-geo"
+	"go.uber.org/multierr"
+	"golang.org/x/exp/maps"
+
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/spatialmath"
+)
+
+const errStrAccuracy = "_accuracy_err"
+
+var model = resource.DefaultModelFamily.WithModel("merged")
+
+// Config is the config of the merged movement_sensor model.
+type Config struct {
+	Position           []string `json:"position,omitempty"`
+	Orientation        []string `json:"orientation,omitempty"`
+	CompassHeading     []string `json:"compass_heading,omitempty"`
+	LinearVelocity     []string `json:"linear_velocity,omitempty"`
+	AngularVelocity    []string `json:"angular_velocity,omitempty"`
+	LinearAcceleration []string `json:"angular_acceleration,omitempty"`
+}
+
+// Validate validates the merged model's configuration.
+func (cfg *Config) Validate(path string) ([]string, error) {
+	var deps []string
+	deps = append(deps, cfg.Position...)
+	deps = append(deps, cfg.Orientation...)
+	deps = append(deps, cfg.CompassHeading...)
+	deps = append(deps, cfg.LinearVelocity...)
+	deps = append(deps, cfg.AngularVelocity...)
+	deps = append(deps, cfg.LinearAcceleration...)
+	return deps, nil
+}
+
+type merged struct {
+	resource.Named
+	logger golog.Logger
+
+	mu sync.Mutex
+
+	ori     movementsensor.MovementSensor
+	pos     movementsensor.MovementSensor
+	compass movementsensor.MovementSensor
+	linVel  movementsensor.MovementSensor
+	angVel  movementsensor.MovementSensor
+	linAcc  movementsensor.MovementSensor
+}
+
+func init() {
+	resource.Register(
+		movementsensor.API, model,
+		resource.Registration[movementsensor.MovementSensor, *Config]{
+			Constructor: newMergedModel,
+		})
+}
+
+func newMergedModel(ctx context.Context, deps resource.Dependencies, conf resource.Config, logger golog.Logger) (
+	movementsensor.MovementSensor, error,
+) {
+	m := merged{
+		logger: logger,
+	}
+
+	if err := m.Reconfigure(ctx, deps, conf); err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func (m *merged) Reconfigure(ctx context.Context, deps resource.Dependencies, conf resource.Config) error {
+	newConf, err := resource.NativeConfig[*Config](conf)
+	if err != nil {
+		return err
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	firstGoodSensorWithProperties := func(
+		deps resource.Dependencies, names []string, logger golog.Logger,
+		mprops *movementsensor.Properties, propname string,
+	) (movementsensor.MovementSensor, error) {
+		// check if the config names and dependencies have been passed at all
+		if len(names) == 0 || deps == nil {
+			return nil, nil
+		}
+
+		for _, name := range names {
+			ms, err := movementsensor.FromDependencies(deps, name)
+			if err != nil {
+				logger.Debugf("error getting sensor %v from dependencies", ms.Name().ShortName())
+				continue
+			}
+
+			props, err := ms.Properties(ctx, nil)
+			if err != nil {
+				logger.Debugf("error in getting sensor %v properties", ms.Name().ShortName())
+				continue
+			}
+
+			// found check that the sensor matches the properties that  want
+			// if it doesn't, skip it and go on to the next sensor in the list
+			if mprops.OrientationSupported && !props.OrientationSupported {
+				continue
+			}
+
+			if mprops.PositionSupported && !props.PositionSupported {
+				continue
+			}
+
+			if mprops.CompassHeadingSupported && !props.CompassHeadingSupported {
+				continue
+			}
+
+			if mprops.LinearVelocitySupported && !props.LinearVelocitySupported {
+				continue
+			}
+
+			if mprops.AngularVelocitySupported && !props.AngularVelocitySupported {
+				continue
+			}
+
+			if mprops.LinearAccelerationSupported && !props.LinearAccelerationSupported {
+				continue
+			}
+
+			// we've found the sensor that does everythign we want
+			return ms, nil
+		}
+
+		return nil, fmt.Errorf("%v not supported by any sensor in list %#v", propname, names)
+	}
+
+	m.ori, err = firstGoodSensorWithProperties(
+		deps, newConf.Orientation, m.logger,
+		&movementsensor.Properties{OrientationSupported: true}, "orientation")
+	if err != nil {
+		return err
+	}
+
+	m.pos, err = firstGoodSensorWithProperties(
+		deps, newConf.Position, m.logger,
+		&movementsensor.Properties{PositionSupported: true}, "position")
+	if err != nil {
+		return err
+	}
+
+	m.compass, err = firstGoodSensorWithProperties(
+		deps, newConf.CompassHeading, m.logger,
+		&movementsensor.Properties{CompassHeadingSupported: true}, "compass_heading")
+	if err != nil {
+		return err
+	}
+
+	m.linVel, err = firstGoodSensorWithProperties(
+		deps, newConf.LinearVelocity, m.logger,
+		&movementsensor.Properties{LinearVelocitySupported: true}, "linear_velocity")
+	if err != nil {
+		return err
+	}
+
+	m.angVel, err = firstGoodSensorWithProperties(
+		deps, newConf.AngularVelocity, m.logger,
+		&movementsensor.Properties{AngularVelocitySupported: true}, "angular_velocity")
+	if err != nil {
+		return err
+	}
+
+	m.linAcc, err = firstGoodSensorWithProperties(
+		deps, newConf.LinearAcceleration, m.logger,
+		&movementsensor.Properties{LinearAccelerationSupported: true}, "linear_acceleration")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *merged) Position(ctx context.Context, extra map[string]interface{}) (*geo.Point, float64, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.pos == nil {
+		return geo.NewPoint(math.NaN(), math.NaN()), math.NaN(),
+			movementsensor.ErrMethodUnimplementedPosition
+	}
+	return m.pos.Position(ctx, extra)
+}
+
+func (m *merged) Orientation(ctx context.Context, extra map[string]interface{}) (spatialmath.Orientation, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.ori == nil {
+		nanOri := spatialmath.NewOrientationVector()
+		nanOri.OX = math.NaN()
+		nanOri.OY = math.NaN()
+		nanOri.OZ = math.NaN()
+		nanOri.Theta = math.NaN()
+		return nanOri,
+			movementsensor.ErrMethodUnimplementedOrientation
+	}
+	return m.ori.Orientation(ctx, extra)
+}
+
+func (m *merged) CompassHeading(ctx context.Context, extra map[string]interface{}) (float64, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.compass == nil {
+		return math.NaN(),
+			movementsensor.ErrMethodUnimplementedCompassHeading
+	}
+	return m.compass.CompassHeading(ctx, extra)
+}
+
+func (m *merged) LinearVelocity(ctx context.Context, extra map[string]interface{}) (r3.Vector, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.linVel == nil {
+		return r3.Vector{X: math.NaN(), Y: math.NaN(), Z: math.NaN()},
+			movementsensor.ErrMethodUnimplementedLinearVelocity
+	}
+	return m.linVel.LinearVelocity(ctx, extra)
+}
+
+func (m *merged) AngularVelocity(ctx context.Context, extra map[string]interface{}) (spatialmath.AngularVelocity, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.angVel == nil {
+		return spatialmath.AngularVelocity{X: math.NaN(), Y: math.NaN(), Z: math.NaN()},
+			movementsensor.ErrMethodUnimplementedAngularVelocity
+	}
+	return m.angVel.AngularVelocity(ctx, extra)
+}
+
+func (m *merged) LinearAcceleration(ctx context.Context, extra map[string]interface{}) (r3.Vector, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.linAcc == nil {
+		return r3.Vector{X: math.NaN(), Y: math.NaN(), Z: math.NaN()},
+			movementsensor.ErrMethodUnimplementedLinearAcceleration
+	}
+	return m.linAcc.LinearAcceleration(ctx, extra)
+}
+
+func mapWithSensorName(name string, accMap map[string]float32) map[string]float32 {
+	for k, v := range accMap {
+		// if map key hasn't been appended, delete
+		delete(accMap, k)
+		// if map key has already been appended, delete
+		delete(accMap, name+"_"+k)
+		// append map with the senosr name and accuracy field
+		accMap[name+"_"+k] = v
+	}
+	return accMap
+}
+
+func (m *merged) Accuracy(ctx context.Context, extra map[string]interface{}) (map[string]float32, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	accMap := make(map[string]float32)
+	var errs error
+
+	if m.ori != nil {
+		oriAcc, err := m.ori.Accuracy(ctx, extra)
+		if err != nil {
+			// replace entire map with a map that shows that it has errors
+			oriAcc = map[string]float32{
+				m.ori.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			maps.Copy(accMap, oriAcc)
+			errs = multierr.Combine(errs, err)
+		} else {
+			maps.Copy(accMap, mapWithSensorName(m.ori.Name().ShortName(), oriAcc))
+		}
+	}
+
+	if m.pos != nil {
+		posAcc, err := m.pos.Accuracy(ctx, extra)
+		if err != nil {
+			posAcc = map[string]float32{
+				m.pos.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			maps.Copy(accMap, posAcc)
+			errs = multierr.Combine(errs, err)
+		} else {
+			maps.Copy(accMap, mapWithSensorName(m.pos.Name().ShortName(), posAcc))
+		}
+	}
+
+	if m.compass != nil {
+		compassAcc, err := m.compass.Accuracy(ctx, extra)
+		if err != nil {
+			compassAcc = map[string]float32{
+				m.compass.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			errs = multierr.Combine(errs, err)
+		}
+		maps.Copy(accMap, mapWithSensorName(m.compass.Name().ShortName(), compassAcc))
+	}
+
+	if m.linVel != nil {
+		linvelAcc, err := m.linVel.Accuracy(ctx, extra)
+		if err != nil {
+			linvelAcc = map[string]float32{
+				m.linVel.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			errs = multierr.Combine(errs, err)
+		}
+		maps.Copy(accMap, mapWithSensorName(m.linVel.Name().ShortName(), linvelAcc))
+	}
+
+	if m.angVel != nil {
+		angvelAcc, err := m.angVel.Accuracy(ctx, extra)
+		if err != nil {
+			angvelAcc = map[string]float32{
+				m.angVel.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			errs = multierr.Combine(errs, err)
+		}
+		maps.Copy(accMap, mapWithSensorName(m.angVel.Name().ShortName(), angvelAcc))
+	}
+
+	if m.linAcc != nil {
+		linAccAcc, err := m.linAcc.Accuracy(ctx, extra)
+		if err != nil {
+			linAccAcc = map[string]float32{
+				m.linAcc.Name().ShortName() + errStrAccuracy: float32(math.NaN()),
+			}
+			errs = multierr.Combine(errs, err)
+		}
+		maps.Copy(accMap, mapWithSensorName(m.linAcc.Name().ShortName(), linAccAcc))
+	}
+
+	return accMap, errs
+}
+
+func (m *merged) Properties(ctx context.Context, extra map[string]interface{}) (*movementsensor.Properties, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return &movementsensor.Properties{
+		OrientationSupported:        m.ori != nil,
+		PositionSupported:           m.pos != nil,
+		CompassHeadingSupported:     m.compass != nil,
+		LinearVelocitySupported:     m.linVel != nil,
+		AngularVelocitySupported:    m.angVel != nil,
+		LinearAccelerationSupported: m.linAcc != nil,
+	}, nil
+}
+
+func (m *merged) Readings(ctx context.Context, extra map[string]interface{}) (map[string]interface{}, error) {
+	// we're already in lock in this driver
+	// don't lock the mutex again for the Readings call
+	return movementsensor.Readings(ctx, m, extra)
+}
+
+func (m *merged) Close(context.Context) error {
+	// we do not try to Close the movement sensors that this driver depends on
+	// we let their own drivers and modules close them
+	return nil
+}
---
+++ components/movementsensor/merged/merged_test.go
@@ -0,0 +1,351 @@
+package merged
+
+import (
+	"context"
+	"errors"
+	"math"
+	"strings"
+	"testing"
+
+	"github.com/edaniels/golog"
+	"github.com/golang/geo/r3"
+	geo "github.com/kellydunn/golang-geo"
+	"go.viam.com/test"
+
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/spatialmath"
+	"go.viam.com/rdk/testutils/inject"
+)
+
+const (
+	testName = "testSensor"
+)
+
+var (
+	testlinvel   = r3.Vector{X: 1, Y: 2, Z: 3}
+	testori      = &spatialmath.OrientationVector{OX: 0, OY: 0, OZ: -1, Theta: 75}
+	testgeopoint = geo.NewPoint(43.4, -72.9)
+	testalt      = 45.0
+	testcompass  = 75.0
+	testangvel   = spatialmath.AngularVelocity{X: 4, Y: 5, Z: 6}
+	testlinacc   = r3.Vector{X: 7, Y: 8, Z: 9}
+
+	errAccuracy = errors.New("no accuracy for you merged sensor")
+	errProps    = errors.New("no properties for you merged sensor")
+
+	emptySensors   = []string{}
+	posSensors     = []string{"goodPos", "unusedPos"}
+	oriSensors     = []string{"goodOri", "unusedOri"}
+	compassSensors = []string{"badCompass", "goodCompass", "unusedCompass"}
+	linvelSensors  = []string{"goodLinVel", "unusedLinVel"}
+	angvelSensors  = []string{"goodAngVel", "unusedAngVel"}
+	linaccSensors  = []string{"goodLinAcc", "unusedLinAcc"}
+
+	emptyProps   = movementsensor.Properties{}
+	oriProps     = movementsensor.Properties{OrientationSupported: true}
+	posProps     = movementsensor.Properties{PositionSupported: true}
+	compassProps = movementsensor.Properties{CompassHeadingSupported: true}
+	linvelProps  = movementsensor.Properties{LinearVelocitySupported: true}
+	angvelProps  = movementsensor.Properties{AngularVelocitySupported: true}
+	linaccProps  = movementsensor.Properties{LinearAccelerationSupported: true}
+)
+
+func setUpCfg(ori, pos, compass, linvel, angvel, linacc []string) resource.Config {
+	return resource.Config{
+		Name:  testName,
+		Model: model,
+		API:   movementsensor.API,
+		ConvertedAttributes: &Config{
+			Orientation:        ori,
+			Position:           pos,
+			CompassHeading:     compass,
+			LinearVelocity:     linvel,
+			AngularVelocity:    angvel,
+			LinearAcceleration: linacc,
+		},
+	}
+}
+
+func setupMovementSensor(
+	name string, prop movementsensor.Properties,
+	errAcc, oriPropsErr bool,
+) movementsensor.MovementSensor {
+	ms := inject.NewMovementSensor(name)
+	ms.PropertiesFunc = func(ctx context.Context, extra map[string]interface{},
+	) (*movementsensor.Properties, error) {
+		if oriPropsErr && strings.Contains(name, "goodOri") {
+			return &prop, errProps
+		}
+		return &prop, nil
+	}
+	ms.AccuracyFunc = func(ctx context.Context, exta map[string]interface{}) (map[string]float32, error) {
+		if errAcc {
+			return nil, errAccuracy
+		}
+		return map[string]float32{"accuracy": 32}, nil
+	}
+
+	switch {
+	case strings.Contains(name, "Ori"):
+		ms.OrientationFunc = func(ctx context.Context, extra map[string]interface{}) (spatialmath.Orientation, error) {
+			return testori, nil
+		}
+	case strings.Contains(name, "Pos"):
+		ms.PositionFunc = func(ctx context.Context, extra map[string]interface{}) (*geo.Point, float64, error) {
+			return testgeopoint, testalt, nil
+		}
+	case strings.Contains(name, "Compass"):
+		ms.CompassHeadingFunc = func(ctx context.Context, extra map[string]interface{}) (float64, error) {
+			return testcompass, nil
+		}
+	case strings.Contains(name, "AngVel"):
+		ms.AngularVelocityFunc = func(ctx context.Context, extra map[string]interface{}) (spatialmath.AngularVelocity, error) {
+			return testangvel, nil
+		}
+	case strings.Contains(name, "LinVel"):
+		ms.LinearVelocityFunc = func(ctx context.Context, extra map[string]interface{}) (r3.Vector, error) {
+			return testlinvel, nil
+		}
+	case strings.Contains(name, "LinAcc"):
+		ms.LinearAccelerationFunc = func(ctx context.Context, extra map[string]interface{}) (r3.Vector, error) {
+			return testlinacc, nil
+		}
+	}
+
+	return ms
+}
+
+func setupDependencies(t *testing.T,
+	depswithProps map[string]movementsensor.Properties,
+	errAcc, errProps bool,
+) resource.Dependencies {
+	t.Helper()
+	result := make(resource.Dependencies)
+	for name, prop := range depswithProps {
+		ms := setupMovementSensor(name, prop, errAcc, errProps)
+		result[movementsensor.Named(name)] = ms
+	}
+	return result
+}
+
+func TestValidate(t *testing.T) {
+	// doesn't pass validate
+	conf := setUpCfg(
+		emptySensors, emptySensors /*pos*/, emptySensors, /*compass*/
+		emptySensors /*linvel*/, emptySensors /*angvel*/, emptySensors /*linacc*/)
+	implicits, err := conf.Validate("somepath", movementsensor.API.Type.Name)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, implicits, test.ShouldBeNil)
+
+	// doesn't pass configuration
+	conf = setUpCfg(
+		oriSensors, emptySensors /*pos*/, emptySensors, /*compass*/
+		linvelSensors, emptySensors /*angvel*/, emptySensors /*linacc*/)
+	implicits, err = conf.Validate("somepath", movementsensor.API.Type.Name)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, implicits, test.ShouldResemble, append(oriSensors, linvelSensors...))
+
+	conf = setUpCfg(
+		/*ori*/ emptySensors, emptySensors /*pos*/, emptySensors, /*comapss*/
+		linvelSensors /*linval*/, angvelSensors /*angvel*/, emptySensors /*linacc*/)
+	implicits, err = conf.Validate("somepath", movementsensor.API.Type.Name)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, implicits, test.ShouldResemble, append(linvelSensors, angvelSensors...))
+}
+
+func TestCreation(t *testing.T) {
+	ctx := context.Background()
+	logger := golog.NewTestLogger(t)
+
+	// doesn't pass configuration
+	conf := setUpCfg(
+		oriSensors, emptySensors /*pos*/, emptySensors, /*compass*/
+		linvelSensors, emptySensors /*angvel*/, emptySensors /*linacc*/)
+
+	depmap := map[string]movementsensor.Properties{
+		linvelSensors[0]: emptyProps,                       // empty
+		linvelSensors[1]: {AngularVelocitySupported: true}, // wrong properties
+		oriSensors[0]:    {OrientationSupported: true},     // has correct properties but errors
+		oriSensors[1]:    {LinearVelocitySupported: true},  // second sensor in a list, but wrong property
+	}
+
+	deps := setupDependencies(t, depmap, false, true /* properties error */)
+	ms, err := newMergedModel(ctx, deps, conf, logger)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "orientation not supported")
+	test.That(t, ms, test.ShouldBeNil)
+
+	// first time passing configuration with two merged sensors
+	conf = setUpCfg(
+		/*ori*/ emptySensors, emptySensors /*pos*/, emptySensors, /*comapss*/
+		linvelSensors /*linval*/, angvelSensors /*angvel*/, emptySensors /*linacc*/)
+
+	depmap = map[string]movementsensor.Properties{
+		linvelSensors[0]: linvelProps,
+		linvelSensors[1]: linvelProps,
+		angvelSensors[0]: angvelProps,
+		angvelSensors[1]: angvelProps,
+	}
+
+	deps = setupDependencies(t, depmap, false, false)
+
+	ms, err = newMergedModel(ctx, deps, conf, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	compass, err := ms.CompassHeading(ctx, nil)
+	test.That(t, err, test.ShouldBeError, movementsensor.ErrMethodUnimplementedCompassHeading)
+	test.That(t, math.IsNaN(compass), test.ShouldBeTrue)
+
+	pos, alt, err := ms.Position(ctx, nil)
+	test.That(t, err, test.ShouldBeError, movementsensor.ErrMethodUnimplementedPosition)
+	test.That(t, math.IsNaN(pos.Lat()), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(pos.Lng()), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(alt), test.ShouldBeTrue)
+
+	linacc, err := ms.LinearAcceleration(ctx, nil)
+	test.That(t, err, test.ShouldBeError, movementsensor.ErrMethodUnimplementedLinearAcceleration)
+	test.That(t, math.IsNaN(linacc.X), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(linacc.Y), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(linacc.Z), test.ShouldBeTrue)
+
+	ori, err := ms.Orientation(ctx, nil)
+	test.That(t, err, test.ShouldBeError, movementsensor.ErrMethodUnimplementedOrientation)
+	test.That(t, math.IsNaN(ori.OrientationVectorRadians().OX), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(ori.OrientationVectorRadians().OY), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(ori.OrientationVectorRadians().OZ), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(ori.OrientationVectorRadians().Theta), test.ShouldBeTrue)
+
+	linvel, err := ms.LinearVelocity(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, linvel, test.ShouldResemble, testlinvel)
+
+	angvel, err := ms.AngularVelocity(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, angvel, test.ShouldResemble, testangvel)
+
+	linacc, err = ms.LinearAcceleration(ctx, nil)
+	test.That(t, err, test.ShouldBeError, movementsensor.ErrMethodUnimplementedLinearAcceleration)
+	test.That(t, math.IsNaN(linacc.X), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(linacc.Y), test.ShouldBeTrue)
+	test.That(t, math.IsNaN(linacc.Z), test.ShouldBeTrue)
+
+	properties, err := ms.Properties(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, properties.OrientationSupported, test.ShouldBeFalse)
+	test.That(t, properties.PositionSupported, test.ShouldBeFalse)
+	test.That(t, properties.CompassHeadingSupported, test.ShouldBeFalse)
+	test.That(t, properties.LinearAccelerationSupported, test.ShouldBeFalse)
+	test.That(t, properties.AngularVelocitySupported, test.ShouldBeTrue)
+	test.That(t, properties.LinearVelocitySupported, test.ShouldBeTrue)
+
+	accuracies, err := ms.Accuracy(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, accuracies, test.ShouldResemble,
+		map[string]float32{
+			"goodAngVel_accuracy": 32,
+			"goodLinVel_accuracy": 32,
+		})
+
+	readings, err := ms.Readings(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, readings, test.ShouldResemble, map[string]interface{}{
+		"linear_velocity":  linvel,
+		"angular_velocity": angvel,
+	})
+
+	conf = setUpCfg(oriSensors, posSensors, compassSensors, linvelSensors, angvelSensors, linaccSensors)
+
+	depmap[linvelSensors[0]] = linvelProps
+	depmap[linvelSensors[1]] = linvelProps
+	depmap[angvelSensors[0]] = angvelProps
+	depmap[angvelSensors[1]] = angvelProps
+	depmap[oriSensors[0]] = oriProps
+	depmap[oriSensors[1]] = angvelProps // wrong properties for first compass sensor
+	depmap[posSensors[0]] = posProps
+	depmap[posSensors[1]] = emptyProps
+	depmap[compassSensors[0]] = emptyProps
+	depmap[compassSensors[1]] = compassProps
+	depmap[compassSensors[2]] = emptyProps
+	depmap[linaccSensors[0]] = linaccProps
+	depmap[linaccSensors[1]] = emptyProps
+
+	deps = setupDependencies(t, depmap, false, false)
+
+	// first reconfiguration with six sensors and no function errors
+	err = ms.Reconfigure(ctx, deps, conf)
+	test.That(t, err, test.ShouldBeNil)
+
+	pos, alt, err = ms.Position(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, pos, test.ShouldEqual, testgeopoint)
+	test.That(t, alt, test.ShouldEqual, testalt)
+
+	ori, err = ms.Orientation(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, ori, test.ShouldEqual, testori)
+
+	compass, err = ms.CompassHeading(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, compass, test.ShouldEqual, testcompass)
+
+	linvel, err = ms.LinearVelocity(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, linvel, test.ShouldResemble, testlinvel)
+
+	angvel, err = ms.AngularVelocity(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, angvel, test.ShouldResemble, testangvel)
+
+	linacc, err = ms.LinearAcceleration(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, linacc, test.ShouldResemble, testlinacc)
+
+	properties, err = ms.Properties(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, properties.OrientationSupported, test.ShouldBeTrue)
+	test.That(t, properties.PositionSupported, test.ShouldBeTrue)
+	test.That(t, properties.CompassHeadingSupported, test.ShouldBeTrue)
+	test.That(t, properties.LinearAccelerationSupported, test.ShouldBeTrue)
+	test.That(t, properties.AngularVelocitySupported, test.ShouldBeTrue)
+	test.That(t, properties.LinearVelocitySupported, test.ShouldBeTrue)
+
+	accuracies, err = ms.Accuracy(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, accuracies, test.ShouldResemble,
+		map[string]float32{
+			"goodOri_accuracy":     32,
+			"goodPos_accuracy":     32,
+			"goodCompass_accuracy": 32,
+			"goodAngVel_accuracy":  32,
+			"goodLinVel_accuracy":  32,
+			"goodLinAcc_accuracy":  32,
+		})
+
+	readings, err = ms.Readings(ctx, nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, readings, test.ShouldResemble, map[string]interface{}{
+		"orientation":         ori,
+		"position":            pos,
+		"altitude":            alt,
+		"compass":             compass,
+		"linear_velocity":     linvel,
+		"angular_velocity":    angvel,
+		"linear_acceleration": linacc,
+	})
+
+	// second reconfiguration with six sensors but an error in accuracy
+	deps = setupDependencies(t, depmap, true /* accuracy error */, false)
+	err = ms.Reconfigure(ctx, deps, conf)
+	test.That(t, err, test.ShouldBeNil)
+
+	accuracies, err = ms.Accuracy(ctx, nil)
+	test.That(t, err, test.ShouldNotBeNil)
+
+	for k, v := range accuracies {
+		test.That(t, k, test.ShouldContainSubstring, errStrAccuracy)
+		test.That(t, math.IsNaN(float64(v)), test.ShouldBeTrue)
+	}
+
+	// close the sensor, this test is done
+	test.That(t, ms.Close(ctx), test.ShouldBeNil)
+}
