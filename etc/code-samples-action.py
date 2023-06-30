83f9efec3d97830c941687a051ce79c9e6ea88c4
Alexa Greenberg <alexa.greenberg@viam.com>
2023-06-30 11:17:28 -0400

    pull function name out of dict correctly (#2599)

--- etc/code-samples-action.py
+++ etc/code-samples-action.py
@@ -3,11 +3,11 @@ import json
 data = None
 with open("./examples/apis.json") as f:
     data = json.load(f)
-    data = data["functions"]
 with open("code-samples-warning.md", "w") as f:
     f.write("Warning your change may break code samples. ")
     f.write("If your change modifies any of the following functions please contact @viamrobotics/fleet-management. Thanks!\n")
     f.write("|component|function|\n")
     f.write("|-|-|\n")
     for k, v in data.items():
-        f.write(f"|{k}|{v}|\n")
+        func = v["func"]
+        f.write(f"|{k}|{func}|\n")
