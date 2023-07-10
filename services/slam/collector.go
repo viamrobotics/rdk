77e9e9bc36c3ed7263cd1167cf3c661d6e226ebb
Tahiya Salam <tahiyasalam@gmail.com>
2023-07-10 14:16:17 -0400

    Sketch out collector

---
+++ services/slam/collector.go
@@ -0,0 +1,59 @@
+package slam
+
+import (
+	"context"
+
+	"go.opencensus.io/trace"
+	"go.viam.com/rdk/data"
+	"go.viam.com/rdk/spatialmath"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+type method int64
+
+const (
+	getPosition      method = iota
+	getPointCloudMap method = iota
+)
+
+func (m method) String() string {
+	switch m {
+	case getPosition:
+		return "GetPosition"
+	case getPointCloudMap:
+		return "GetPointCloudMap"
+	}
+	return "Unknown"
+}
+
+type Position struct {
+	Pose               spatialmath.Pose
+	ComponentReference string
+}
+
+func newGetPositionCollector(resource interface{}, params data.CollectorParams) (data.Collector, error) {
+	slam, err := assertSLAM(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]*anypb.Any) (interface{}, error) {
+		_, span := trace.StartSpan(ctx, "slam::data::collector::CaptureFunc::GetPosition")
+		defer span.End()
+
+		pose, componentRef, err := slam.GetPosition(ctx)
+		if err != nil {
+			return nil, data.FailedToReadErr(params.ComponentName, getPosition.String(), err)
+		}
+		return Position{Pose: pose, ComponentReference: componentRef}, nil
+	})
+	return data.NewCollector(cFunc, params)
+}
+
+func assertSLAM(resource interface{}) (Service, error) {
+	slamService, ok := resource.(Service)
+	if !ok {
+		return nil, data.InvalidInterfaceErr(API)
+	}
+	return slamService, nil
+}
