1327b16dc28bd82fc4fd3b6818ce20b96cc4a94a
Susmita Sanyal <susmita@viam.com>
2023-07-20 10:47:05 -0400

    test readings

--- components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
+++ components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
@@ -5,9 +5,17 @@ import (
 	"testing"
 
 	"github.com/edaniels/golog"
+	geo "github.com/kellydunn/golang-geo"
+	"go.viam.com/rdk/components/movementsensor/fake"
 	rtk "go.viam.com/rdk/components/movementsensor/rtkutils"
 	"go.viam.com/test"
 	"go.viam.com/utils"
+)
+
+var (
+	alt   = 50.5
+	speed = 5.4
+	fix   = 1
 )
 
 // mock ntripinfo client.
@@ -68,4 +76,34 @@ func TestConnect(t *testing.T) {
 
 	err = g.getStream("", 10)
 	test.That(t, err, test.ShouldNotBeNil)
+}
+
+func TestReadings(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+	ctx := context.Background()
+	cancelCtx, cancelFunc := context.WithCancel(ctx)
+	g := rtkSerial{
+		cancelCtx:  cancelCtx,
+		cancelFunc: cancelFunc,
+		logger:     logger,
+	}
+
+	g.nmeamovementsensor = &fake.MovementSensor{}
+
+	status, err := g.getNtripConnectionStatus()
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, status, test.ShouldEqual, false)
+
+	loc1, alt1, err := g.Position(ctx, make(map[string]interface{}))
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, loc1, test.ShouldResemble, geo.NewPoint(40.7, -73.98))
+	test.That(t, alt1, test.ShouldEqual, alt)
+
+	speed1, err := g.LinearVelocity(ctx, make(map[string]interface{}))
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, speed1.Y, test.ShouldEqual, speed)
+
+	fix1, err := g.readFix(ctx)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, fix1, test.ShouldEqual, fix)
 }
