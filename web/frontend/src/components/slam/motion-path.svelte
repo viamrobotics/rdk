b687af1456b1f712a3a19be920762bb47e576b09
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2023-07-20 15:31:57 -0400

    Fix state updates for motion path visualization when multiple files are dropped (#2661)

--- web/frontend/src/components/slam/motion-path.svelte
+++ web/frontend/src/components/slam/motion-path.svelte
@@ -8,7 +8,6 @@ import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
 export let scene: THREE.Scene;
 export let path: string | undefined;
 
-const geometry = new LineGeometry();
 const material = new LineMaterial({
   color: 0xFF_00_47,
   linewidth: 0.005,
@@ -16,30 +15,43 @@ const material = new LineMaterial({
   alphaToCoverage: true,
 });
 
-const line = new Line2(geometry, material);
-line.visible = false;
-scene.add(line);
+let line: Line2;
 
-$: {
-  if (path === undefined) {
-    line.visible = false;
-  } else {
-    line.visible = true;
-
-    const points: number[] = [];
-
-    for (const xy of path.split('\n')) {
-      const [xString, yString] = xy.split(',');
-      if (xString !== undefined && yString !== undefined) {
-        const x = Number.parseFloat(xString) / 1000;
-        const y = Number.parseFloat(yString) / 1000;
-        points.push(x, y, 0);
-      }
-    }
-
-    const vertices = new Float32Array(points);
-    geometry.setPositions(vertices);
+const updatePath = (pathstr?: string) => {
+  if (line !== undefined) {
+    scene.remove(line);
+    line.geometry.dispose();
   }
-}
+
+  if (pathstr === undefined) {
+    return;
+  }
+
+  const geometry = new LineGeometry();
+
+  const points: number[] = [];
+
+  for (const xy of pathstr.split('\n')) {
+    const [xString, yString] = xy.split(',');
+    if (xString !== undefined && yString !== undefined) {
+      const x = Number.parseFloat(xString) / 1000;
+      const y = Number.parseFloat(yString) / 1000;
+      points.push(x, y, 0);
+    }
+  }
+
+  const vertices = new Float32Array(points);
+  geometry.setPositions(vertices);
+
+  line = new Line2(geometry, material);
+
+  // Render above pointcloud.
+  line.renderOrder = 3;
+
+  line.computeLineDistances();
+  scene.add(line);
+};
+
+$: updatePath(path);
 
 </script>
