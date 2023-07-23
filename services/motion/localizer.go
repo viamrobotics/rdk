e2b725ea6ef3fe51312e3810248fa2828d5d8963
Ray Bjorkman <raymond@viam.com>
2023-07-23 10:41:36 -0400

    Align localizer with planning frame (#2609)
    Co-authored-by: Nick Franczak <nicholasgfranczak@gmail.com>

--- services/motion/localizer.go
+++ services/motion/localizer.go
@@ -2,30 +2,20 @@ package motion
 
 import (
 	"context"
-	"fmt"
+	"math"
+	"strings"
+
+	geo "github.com/kellydunn/golang-geo"
 
 	"go.viam.com/rdk/components/movementsensor"
 	"go.viam.com/rdk/referenceframe"
-	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/slam"
 	"go.viam.com/rdk/spatialmath"
 )
 
 // Localizer is an interface which both slam and movementsensor can satisfy when wrapped respectively.
 type Localizer interface {
-	CurrentPosition(context.Context) (referenceframe.PoseInFrame, error)
-}
-
-// NewLocalizer constructs either a slamLocalizer or movementSensorLocalizer from the given resource.
-func NewLocalizer(ctx context.Context, res resource.Resource) (Localizer, error) {
-	switch res := res.(type) {
-	case slam.Service:
-		return &slamLocalizer{Service: res}, nil
-	case movementsensor.MovementSensor:
-		return &movementSensorLocalizer{MovementSensor: res}, nil
-	default:
-		return nil, fmt.Errorf("cannot localize on resource of type %T", res)
-	}
+	CurrentPosition(context.Context) (*referenceframe.PoseInFrame, error)
 }
 
 // slamLocalizer is a struct which only wraps an existing slam service.
@@ -33,28 +23,55 @@ type slamLocalizer struct {
 	slam.Service
 }
 
+// NewSLAMLocalizer creates a new Localizer that relies on a slam service to report Pose.
+func NewSLAMLocalizer(slam slam.Service) Localizer {
+	return &slamLocalizer{Service: slam}
+}
+
 // CurrentPosition returns slam's current position.
-func (s slamLocalizer) CurrentPosition(ctx context.Context) (referenceframe.PoseInFrame, error) {
-	var pif referenceframe.PoseInFrame
+func (s *slamLocalizer) CurrentPosition(ctx context.Context) (*referenceframe.PoseInFrame, error) {
 	pose, _, err := s.GetPosition(ctx)
 	if err != nil {
-		return pif, err
+		return nil, err
 	}
-	return *referenceframe.NewPoseInFrame(referenceframe.World, pose), err
+	return referenceframe.NewPoseInFrame(referenceframe.World, pose), err
 }
 
 // movementSensorLocalizer is a struct which only wraps an existing movementsensor.
 type movementSensorLocalizer struct {
 	movementsensor.MovementSensor
+	origin      *geo.Point
+	calibration spatialmath.Pose
+}
+
+// NewMovementSensorLocalizer creates a Localizer from a MovementSensor.
+// An origin point must be specified and the localizer will return Poses relative to this point.
+// A calibration pose can also be specified, which will adjust the location after it is calculated relative to the origin.
+func NewMovementSensorLocalizer(ms movementsensor.MovementSensor, origin *geo.Point, calibration spatialmath.Pose) Localizer {
+	return &movementSensorLocalizer{MovementSensor: ms, origin: origin, calibration: calibration}
 }
 
 // CurrentPosition returns a movementsensor's current position.
-func (m movementSensorLocalizer) CurrentPosition(ctx context.Context) (referenceframe.PoseInFrame, error) {
-	var pif referenceframe.PoseInFrame
+func (m *movementSensorLocalizer) CurrentPosition(ctx context.Context) (*referenceframe.PoseInFrame, error) {
 	gp, _, err := m.Position(ctx, nil)
 	if err != nil {
-		return pif, err
+		return nil, err
 	}
-	pose := spatialmath.GeoPointToPose(gp)
-	return *referenceframe.NewPoseInFrame(m.Name().Name, pose), nil
+	var o spatialmath.Orientation
+	compass, err := m.CompassHeading(ctx, nil)
+	if err != nil {
+		if !strings.Contains(err.Error(), movementsensor.ErrMethodUnimplementedCompassHeading.Error()) {
+			return nil, err
+		}
+		o, err = m.Orientation(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		o = &spatialmath.OrientationVectorDegrees{OZ: 1, Theta: compass}
+	}
+	pose := spatialmath.NewPose(spatialmath.GeoPointToPose(gp, m.origin).Point(), o)
+	alignEast := spatialmath.NewPoseFromOrientation(&spatialmath.OrientationVector{OZ: 1, Theta: -math.Pi / 2})
+	correction := spatialmath.Compose(m.calibration, alignEast)
+	return referenceframe.NewPoseInFrame(m.Name().Name, spatialmath.Compose(pose, correction)), nil
 }
