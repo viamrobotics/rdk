3582c69981ecf3aaec52a2c40e42ee3dae7f9476
mrloureed <mrloureed@gmail.com>
2023-07-24 13:08:36 -0400

    revert

--- web/frontend/src/lib/components/collapse.svelte
+++ web/frontend/src/lib/components/collapse.svelte
@@ -4,68 +4,71 @@
   Once we've completed the svelte migration it can be removed and replaced with the PRIME component.
 -->
 <script lang="ts">
-  import { onMount, createEventDispatcher, tick } from 'svelte';
 
+  import { onMount, createEventDispatcher, tick } from 'svelte';
+  
   export let title = '';
   export let open = Boolean(localStorage.getItem(`rc.collapse.${title}.open`));
-
+  
   const dispatch = createEventDispatcher();
-
+  
   const handleClick = async (event: Event) => {
     if ((event.target as HTMLElement).getAttribute('slot') === 'header') {
       return;
     }
-
+  
     open = !open;
-
+  
     if (open) {
       localStorage.setItem(`rc.collapse.${title}.open`, 'true');
     } else {
       localStorage.removeItem(`rc.collapse.${title}.open`);
     }
-
+  
     await tick();
-
+  
     dispatch('toggle', { open });
   };
-
+  
   onMount(() => {
     if (open) {
       dispatch('toggle', { open: true });
     }
   });
-</script>
-
-<div class="relative w-full">
-  <div
-    class="
-      border border-light bg-white w-full py-2 px-4
-      flex flex-reverse items-center justify-between text-default cursor-pointer
-    "
-    on:click={handleClick}
-    on:keyup|stopPropagation|preventDefault={handleClick}
-  >
-    <div class="flex flex-wrap gap-x-3 gap-y-1 items-center">
-      {#if title}
-        <h2 class="m-0 text-sm">{title}</h2>
-      {/if}
-
-      <slot name="title" />
+  
+  </script>
+  
+  <div class="relative w-full">
+    <div
+      class='
+        border border-light bg-white w-full py-2 px-4
+        flex flex-reverse items-center justify-between text-default cursor-pointer
+      '
+      on:click={handleClick}
+      on:keyup|stopPropagation|preventDefault={handleClick}
+    >
+      <div class="flex flex-wrap gap-x-3 gap-y-1 items-center">
+        {#if title}
+          <h2 class="m-0 text-sm">{title}</h2>
+        {/if}
+  
+        <slot name="title" />
+      </div>
+  
+      <div class="h-full flex items-center gap-3">
+        <slot name="header" />
+  
+        <v-icon
+          class:rotate-0={!open}
+          class:rotation-180={open}
+          name="chevron-down"
+          size="2xl"
+        />
+      </div>
     </div>
-
-    <div class="h-full flex items-center gap-3">
-      <slot name="header" />
-
-      <v-icon
-        class:rotate-0={!open}
-        class:rotation-180={open}
-        name="chevron-down"
-        size="2xl"
-      />
-    </div>
+  
+    {#if open}
+      <slot />
+    {/if}
   </div>
-
-  {#if open}
-    <slot />
-  {/if}
-</div>
+  
