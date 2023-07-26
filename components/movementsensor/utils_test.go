f18ad1081e56236bbca2de15bc7f62222c353a50
Susmita Sanyal <susmita@viam.com>
2023-07-26 14:16:28 -0400

    utils tests

--- components/movementsensor/utils_test.go
+++ components/movementsensor/utils_test.go
@@ -2,65 +2,68 @@ package movementsensor
 
 import (
 	"errors"
+	"math"
 	"testing"
 
 	geo "github.com/kellydunn/golang-geo"
 	"go.viam.com/test"
 )
 
+var (
+	testPos  = geo.NewPoint(65.35996, -17.03663)
+	testPos2 = geo.NewPoint(8.46696, -17.03663)
+	zeroPos  = geo.NewPoint(0, 0)
+	nanPos   = geo.NewPoint(math.NaN(), math.NaN())
+)
+
 func TestGetHeading(t *testing.T) {
 	// test case 1, standard bearing = 0, heading = 270
-	var (
-		GPS1 = geo.NewPoint(8.46696, -17.03663)
-		GPS2 = geo.NewPoint(65.35996, -17.03663)
-	)
-
-	bearing, heading, standardBearing := GetHeading(GPS1, GPS2, 90)
+	bearing, heading, standardBearing := GetHeading(testPos2, testPos, 90)
 	test.That(t, bearing, test.ShouldAlmostEqual, 0)
 	test.That(t, heading, test.ShouldAlmostEqual, 270)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, 0)
 
 	// test case 2, reversed test case 1.
-	GPS1 = geo.NewPoint(65.35996, -17.03663)
-	GPS2 = geo.NewPoint(8.46696, -17.03663)
+	testPos2 = geo.NewPoint(65.35996, -17.03663)
+	testPos = geo.NewPoint(8.46696, -17.03663)
 
-	bearing, heading, standardBearing = GetHeading(GPS1, GPS2, 90)
+	bearing, heading, standardBearing = GetHeading(testPos2, testPos, 90)
 	test.That(t, bearing, test.ShouldAlmostEqual, 180)
 	test.That(t, heading, test.ShouldAlmostEqual, 90)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, 180)
 
 	// test case 2.5, changed yaw offsets
-	GPS1 = geo.NewPoint(65.35996, -17.03663)
-	GPS2 = geo.NewPoint(8.46696, -17.03663)
+	testPos2 = geo.NewPoint(65.35996, -17.03663)
+	testPos = geo.NewPoint(8.46696, -17.03663)
 
-	bearing, heading, standardBearing = GetHeading(GPS1, GPS2, 270)
+	bearing, heading, standardBearing = GetHeading(testPos2, testPos, 270)
 	test.That(t, bearing, test.ShouldAlmostEqual, 180)
 	test.That(t, heading, test.ShouldAlmostEqual, 270)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, 180)
 
 	// test case 3
-	GPS1 = geo.NewPoint(8.46696, -17.03663)
-	GPS2 = geo.NewPoint(56.74367734077241, 29.369620000000015)
+	testPos2 = geo.NewPoint(8.46696, -17.03663)
+	testPos = geo.NewPoint(56.74367734077241, 29.369620000000015)
 
-	bearing, heading, standardBearing = GetHeading(GPS1, GPS2, 90)
+	bearing, heading, standardBearing = GetHeading(testPos2, testPos, 90)
 	test.That(t, bearing, test.ShouldAlmostEqual, 27.2412, 1e-3)
 	test.That(t, heading, test.ShouldAlmostEqual, 297.24126, 1e-3)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, 27.24126, 1e-3)
 
 	// test case 4, reversed coordinates
-	GPS1 = geo.NewPoint(56.74367734077241, 29.369620000000015)
-	GPS2 = geo.NewPoint(8.46696, -17.03663)
+	testPos2 = geo.NewPoint(56.74367734077241, 29.369620000000015)
+	testPos = geo.NewPoint(8.46696, -17.03663)
 
-	bearing, heading, standardBearing = GetHeading(GPS1, GPS2, 90)
+	bearing, heading, standardBearing = GetHeading(testPos2, testPos, 90)
 	test.That(t, bearing, test.ShouldAlmostEqual, 235.6498, 1e-3)
 	test.That(t, heading, test.ShouldAlmostEqual, 145.6498, 1e-3)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, -124.3501, 1e-3)
 
 	// test case 4.5, changed yaw Offset
-	GPS1 = geo.NewPoint(56.74367734077241, 29.369620000000015)
-	GPS2 = geo.NewPoint(8.46696, -17.03663)
+	testPos2 = geo.NewPoint(56.74367734077241, 29.369620000000015)
+	testPos = geo.NewPoint(8.46696, -17.03663)
 
-	bearing, heading, standardBearing = GetHeading(GPS1, GPS2, 270)
+	bearing, heading, standardBearing = GetHeading(testPos2, testPos, 270)
 	test.That(t, bearing, test.ShouldAlmostEqual, 235.6498, 1e-3)
 	test.That(t, heading, test.ShouldAlmostEqual, 325.6498, 1e-3)
 	test.That(t, standardBearing, test.ShouldAlmostEqual, -124.3501, 1e-3)
@@ -108,4 +111,37 @@ func TestSuppressRareErrors(t *testing.T) {
 	test.That(t, err.Error(), test.ShouldEqual, "three")
 	// and now that we've returned an error, the history is cleared out again.
 	test.That(t, le.Get(), test.ShouldBeNil)
+}
+
+func TestLastPosition(t *testing.T) {
+	lp := NewLastPosition()
+	lp.SetLastPosition(testPos)
+	test.That(t, lp.lastposition, test.ShouldEqual, testPos)
+
+	lp.SetLastPosition(testPos2)
+	get_pos := lp.GetLastPosition()
+	test.That(t, get_pos, test.ShouldEqual, testPos2)
+}
+
+func TestPositionLogic(t *testing.T) {
+	lp := NewLastPosition()
+
+	test.That(t, lp.ArePointsEqual(testPos, testPos), test.ShouldBeTrue)
+	test.That(t, lp.ArePointsEqual(testPos, testPos2), test.ShouldBeFalse)
+
+	test.That(t, lp.IsZeroPosition(zeroPos), test.ShouldBeTrue)
+	test.That(t, lp.IsZeroPosition(testPos), test.ShouldBeFalse)
+
+	test.That(t, lp.IsPositionNaN(nanPos), test.ShouldBeTrue)
+	test.That(t, lp.IsPositionNaN(testPos2), test.ShouldBeFalse)
+}
+
+func TestPMTKFunctions(t *testing.T) {
+	var (
+		expectedValue = ([]uint8{36, 80, 77, 84, 75, 50, 50, 48, 44, 49, 48, 48, 48, 42, 31})
+		testValue     = ([]byte("PMTK220,1000"))
+	)
+	test.That(t, PMTKChecksum(testValue), test.ShouldEqual, 31)
+	test.That(t, PMTKAddChk(testValue), test.ShouldResemble, expectedValue)
+
 }
