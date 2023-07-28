8b3f5c469087ee8d4b6e743074eb67a77fbb7d4a
AaronCasas <aaron@viam.com>
2023-07-28 16:08:28 -0400

    [DATA-1649]/[DATA-1647] Add GetPointCloudMap collector and use streaming sync rpc for large files. (#2703)

--- services/slam/collector.go
+++ services/slam/collector.go
@@ -14,11 +14,15 @@ type method int64
 
 const (
 	getPosition method = iota
+	getPointCloudMap
 )
 
 func (m method) String() string {
 	if m == getPosition {
 		return "GetPosition"
+	}
+	if m == getPointCloudMap {
+		return "GetPointCloudMap"
 	}
 	return "Unknown"
 }
@@ -35,6 +39,28 @@ func newGetPositionCollector(resource interface{}, params data.CollectorParams) 
 			return nil, data.FailedToReadErr(params.ComponentName, getPosition.String(), err)
 		}
 		return &pb.GetPositionResponse{Pose: spatialmath.PoseToProtobuf(pose), ComponentReference: componentRef}, nil
+	})
+	return data.NewCollector(cFunc, params)
+}
+
+func newGetPointCloudMapCollector(resource interface{}, params data.CollectorParams) (data.Collector, error) {
+	slam, err := assertSLAM(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]*anypb.Any) (interface{}, error) {
+		f, err := slam.GetPointCloudMap(ctx)
+		if err != nil {
+			return nil, data.FailedToReadErr(params.ComponentName, getPointCloudMap.String(), err)
+		}
+
+		pcd, err := HelperConcatenateChunksToFull(f)
+		if err != nil {
+			return nil, data.FailedToReadErr(params.ComponentName, getPointCloudMap.String(), err)
+		}
+
+		return pcd, nil
 	})
 	return data.NewCollector(cFunc, params)
 }
