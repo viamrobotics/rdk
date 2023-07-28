324b09d2b227e68f28d93548e08f7555f123d3d0
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-07-28 15:28:18 -0400

    [RSDK-4277] Fix flakey segmenter test (#2707)

--- components/camera/transformpipeline/segmenter.go
+++ components/camera/transformpipeline/segmenter.go
@@ -87,6 +87,9 @@ func (ss *segmenterSource) NextPointCloud(ctx context.Context) (pointcloud.Point
 	if err != nil {
 		return nil, fmt.Errorf("could not get point clouds: %w", err)
 	}
+	if clouds == nil {
+		return pointcloud.New(), nil
+	}
 
 	// merge pointclouds
 	cloudsWithOffset := make([]pointcloud.CloudAndOffsetFunc, 0, len(clouds))
