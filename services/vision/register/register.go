15c99ab3d16d26fff9ae47a18b39da6100b918cf
Khari Jarrett <khari@viam.com>
2023-07-31 16:08:12 -0400

    add to registry

--- services/vision/register/register.go
+++ services/vision/register/register.go
@@ -6,6 +6,7 @@ import (
 	_ "go.viam.com/rdk/services/vision/colordetector"
 	_ "go.viam.com/rdk/services/vision/detectionstosegments"
 	_ "go.viam.com/rdk/services/vision/mlvision"
+	_ "go.viam.com/rdk/services/vision/obstacledepth"
 	_ "go.viam.com/rdk/services/vision/obstacledistance"
 	_ "go.viam.com/rdk/services/vision/obstaclespointcloud"
 )
