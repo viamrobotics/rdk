7c72f44c6e9f7ddabc9615c81a3999f9d70a6160
sbal13 <sbal13@gmail.com>
2023-07-31 11:51:27 -0400

    [RSDK-3805] isolated rc override work; (#2675)

---
+++ web/frontend/src/types/overrides.ts
@@ -0,0 +1,43 @@
+import type { Pose } from '@viamrobotics/sdk';
+import type { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
+
+export interface MappingMetadata {
+  id: string;
+  orgId: string;
+  locationId: string;
+  robotId: string;
+  timeStartSubmitted?: Timestamp.AsObject;
+  timeCloudRunJobStarted?: Timestamp.AsObject;
+  timeEndSubmitted?: Timestamp.AsObject;
+  timeCloudRunJobEnded?: Timestamp.AsObject;
+  endStatus: string;
+  cloudRunJobId: string;
+  viamServerVersion: string;
+  mapName: string;
+  slamVersion: string;
+  config: string;
+}
+
+type MappingDetails = {
+  mode: 'localize' | 'create' | 'update';
+  name?: string;
+  version?: string;
+};
+
+export interface SLAMOverrides {
+  getMappingSessionPCD: (
+    sessionId: string
+  ) => Promise<{ map: Uint8Array; pose: Pose }>;
+  startMappingSession: (mapName: string) => Promise<string>;
+  getActiveMappingSession: () => Promise<MappingMetadata | undefined>;
+  endMappingSession: (
+    sessionId: string
+  ) => Promise<{ packageId: string; version: string }>;
+  viewMap: (sessionId: string) => void;
+  validateMapName: (mapName: string) => string;
+  mappingDetails: MappingDetails;
+  isCloudSlam: boolean;
+}
+export interface RCOverrides {
+  slam?: SLAMOverrides;
+}
