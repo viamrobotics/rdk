b876cf2ab86b3f88fdcfd9b80dbfe582eb0ecb3a
Micheal Parks <micheal.parks@outlook.com>
2023-08-08 10:49:24 -0400

    document helpers

--- web/frontend/src/components/slam/2d/helpers.svelte
+++ web/frontend/src/components/slam/2d/helpers.svelte
@@ -1,10 +1,20 @@
+<!--
+  @component
+  Helpers for orienting the user spacially within a map.
+  Contains a grid and axes, with small grid spaces placed at 1m.
+-->
 <script lang='ts'>
 
 import { T, extend } from '@threlte/core';
 import { AxesHelper, GridHelper } from 'trzy';
 import { renderOrder } from './constants';
 
-extend({ AxesHelper, GridHelper });
+extend({
+  // trzy.AxesHelper uses fat lines so that axes are more clearly displayed, and is dynamically resizable.
+  AxesHelper,
+  // trzy.GridHelper is more performant by drawing the grid in a shader, and can have small and large cells.
+  GridHelper
+});
 
 </script>
 
