a6212e00edc73d6447e5e2e1fe211150d1904c5d
randhid <rand@viam.com>
2023-08-16 19:14:56 -0400

    Unregister sensor base and lint

--- components/base/register/register.go
+++ components/base/register/register.go
@@ -5,6 +5,5 @@ import (
 	// register bases.
 	_ "go.viam.com/rdk/components/base/agilex"
 	_ "go.viam.com/rdk/components/base/fake"
-	_ "go.viam.com/rdk/components/base/sensorbase"
 	_ "go.viam.com/rdk/components/base/wheeled"
 )
