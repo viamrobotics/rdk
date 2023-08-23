716404db06733804bb3599507991bb4dda1e2058
Peter LoVerso <biotinker@gmail.com>
2023-08-23 10:06:08 -0700

    Fix function ordering

--- motionplan/constraint.go
+++ motionplan/constraint.go
@@ -14,27 +14,6 @@ import (
 )
 
 // Given a constraint input with only frames and input positions, calculates the corresponding poses as needed.
-func resolveStatesToPositions(state *ik.State) error {
-	if state.Position == nil {
-		if state.Frame != nil {
-			if state.Configuration != nil {
-				pos, err := state.Frame.Transform(state.Configuration)
-				if err == nil {
-					state.Position = pos
-				} else {
-					return err
-				}
-			} else {
-				return errors.New("invalid constraint input")
-			}
-		} else {
-			return errors.New("invalid constraint input")
-		}
-	}
-	return nil
-}
-
-// Given a constraint input with only frames and input positions, calculates the corresponding poses as needed.
 func resolveSegmentsToPositions(segment *ik.Segment) error {
 	if segment.StartPosition == nil {
 		if segment.Frame != nil {
@@ -58,6 +37,27 @@ func resolveSegmentsToPositions(segment *ik.Segment) error {
 				pos, err := segment.Frame.Transform(segment.EndConfiguration)
 				if err == nil {
 					segment.EndPosition = pos
+				} else {
+					return err
+				}
+			} else {
+				return errors.New("invalid constraint input")
+			}
+		} else {
+			return errors.New("invalid constraint input")
+		}
+	}
+	return nil
+}
+
+// Given a constraint input with only frames and input positions, calculates the corresponding poses as needed.
+func resolveStatesToPositions(state *ik.State) error {
+	if state.Position == nil {
+		if state.Frame != nil {
+			if state.Configuration != nil {
+				pos, err := state.Frame.Transform(state.Configuration)
+				if err == nil {
+					state.Position = pos
 				} else {
 					return err
 				}
