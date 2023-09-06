636f8d8bc8d549390a5416778df3f6dbeaeb115a
Ray Bjorkman <raymond@viam.com>
2023-09-06 13:31:48 -0400

    fix flushing

--- utils/channel.go
+++ utils/channel.go
@@ -2,7 +2,11 @@ package utils
 
 // FlushChan is a function that takes a generic chanel and completely empties it.
 func FlushChan[T any](c chan T) {
-	for i := 0; i < len(c); i++ {
-		<-c
+	for gotSomething := true; gotSomething; {
+		select {
+		case <-c:
+		default:
+			gotSomething = false
+		}
 	}
 }
