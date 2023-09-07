ce3ba564ef624b717a628f73872d9d655e47d34c
Alexa Greenberg <alexa.greenberg@viam.com>
2023-09-07 10:25:14 -0700

    RSDK-4502 implement error state helper function to return distance from current plan (#2854)

--- referenceframe/frame_system.go
+++ referenceframe/frame_system.go
@@ -410,7 +410,7 @@ func (sfs *simpleFrameSystem) transformFromParent(inputMap map[string][]Input, s
 	}
 
 	// transform from source to world, world to target parent
-	return NewPoseInFrame(dst.Name(), spatial.Compose(spatial.PoseInverse(dstToWorld), srcToWorld)), nil
+	return NewPoseInFrame(dst.Name(), spatial.PoseBetween(dstToWorld, srcToWorld)), nil
 }
 
 // compose the quaternions from the input frame to the world referenceframe.
