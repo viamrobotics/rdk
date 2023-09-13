5b97dbc6459aad84948fb7a4465857742a0a9007
randhid <rand@viam.com>
2023-09-13 17:27:09 -0400

    Remove spin block too

--- components/base/wheeled/wheeled_base_test.go
+++ components/base/wheeled/wheeled_base_test.go
@@ -197,25 +197,6 @@ func TestWheelBaseMath(t *testing.T) {
 		test.That(t, rpms, test.ShouldAlmostEqual, 0.523, 0.001)
 		test.That(t, rotations, test.ShouldAlmostEqual, 0.0261, 0.001)
 	})
-	t.Run("spin block", func(t *testing.T) {
-		go func() {
-			time.Sleep(time.Millisecond * 10)
-			err := wb.Stop(ctx, nil)
-			if err != nil {
-				panic(err)
-			}
-		}()
-
-		err := wb.Spin(ctx, 5, 5, nil)
-		test.That(t, err, test.ShouldBeNil)
-
-		for _, m := range wb.allMotors {
-			isOn, powerPct, err := m.IsPowered(ctx, nil)
-			test.That(t, err, test.ShouldBeNil)
-			test.That(t, isOn, test.ShouldBeFalse)
-			test.That(t, powerPct, test.ShouldEqual, 0.0)
-		}
-	})
 
 	// Velocity tests
 	t.Run("velocity math curved", func(t *testing.T) {
