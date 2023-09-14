bf74aa7137799578b40c72bae5cc555c7083b9ba
kharijarrett <khari@viam.com>
2023-09-14 12:46:40 -0400

    RSDK-4199 - Remove Pbstruct from infer signature (#2900)

--- services/mlmodel/server.go
+++ services/mlmodel/server.go
@@ -2,12 +2,8 @@ package mlmodel
 
 import (
 	"context"
-	"encoding/base64"
 
-	"github.com/pkg/errors"
 	pb "go.viam.com/api/service/mlmodel/v1"
-	vprotoutils "go.viam.com/utils/protoutils"
-	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/rdk/ml"
 	"go.viam.com/rdk/resource"
@@ -30,17 +26,7 @@ func (server *serviceServer) Infer(ctx context.Context, req *pb.InferRequest) (*
 	if err != nil {
 		return nil, err
 	}
-	if req.InputData != nil && req.InputTensors != nil {
-		return nil, errors.New("both input_data and input_tensors fields in the Infer request are not nil." +
-			"Server can only process one or the other")
-	}
-	var id map[string]interface{}
-	if req.InputData != nil {
-		id, err = asMap(req.InputData)
-		if err != nil {
-			return nil, err
-		}
-	}
+
 	var it ml.Tensors
 	if req.InputTensors != nil {
 		it, err = ProtoToTensors(req.InputTensors)
@@ -48,11 +34,7 @@ func (server *serviceServer) Infer(ctx context.Context, req *pb.InferRequest) (*
 			return nil, err
 		}
 	}
-	ot, od, err := svc.Infer(ctx, it, id)
-	if err != nil {
-		return nil, err
-	}
-	outputData, err := vprotoutils.StructToStructPb(od)
+	ot, err := svc.Infer(ctx, it)
 	if err != nil {
 		return nil, err
 	}
@@ -60,30 +42,7 @@ func (server *serviceServer) Infer(ctx context.Context, req *pb.InferRequest) (*
 	if err != nil {
 		return nil, err
 	}
-	return &pb.InferResponse{OutputData: outputData, OutputTensors: outputTensors}, nil
-}
-
-// AsMap converts x to a general-purpose Go map.
-// The map values are converted by calling Value.AsInterface.
-func asMap(x *structpb.Struct) (map[string]interface{}, error) {
-	if x == nil {
-		return nil, errors.New("input pb.Struct is nil")
-	}
-	f := x.GetFields()
-	vs := make(map[string]interface{}, len(f))
-	for k, in := range f {
-		switch in.GetKind().(type) {
-		case *structpb.Value_StringValue:
-			out, err := base64.StdEncoding.DecodeString(in.GetStringValue())
-			if err != nil {
-				return nil, err
-			}
-			vs[k] = out
-		default:
-			vs[k] = in.AsInterface()
-		}
-	}
-	return vs, nil
+	return &pb.InferResponse{OutputTensors: outputTensors}, nil
 }
 
 func (server *serviceServer) Metadata(
