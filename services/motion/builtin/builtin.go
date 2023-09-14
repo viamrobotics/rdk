487c54795c9adeed5c5756c3d4797d53355b2057
Nick Sanford <nick.sanford@viam.com>
2023-09-14 14:22:11 -0400

    fix bad merge conflict resolution

--- services/motion/builtin/builtin.go
+++ services/motion/builtin/builtin.go
@@ -331,7 +331,7 @@ func (ms *builtIn) MoveOnGlobe(
 			return resp.success, resp.err
 
 		// if the position poller hit an error return it, otherwise replan
-		case resp := <-moveRequest.position.responseChan:
+		case resp := <-ma.position.responseChan:
 			ms.logger.Debugf("position response: %s", resp)
 			ma.cancel()
 			if resp.err != nil {
@@ -339,7 +339,7 @@ func (ms *builtIn) MoveOnGlobe(
 			}
 
 		// if the obstacle poller hit an error return it, otherwise replan
-		case resp := <-moveRequest.obstacle.responseChan:
+		case resp := <-ma.obstacle.responseChan:
 			ms.logger.Debugf("obstacle response: %s", resp)
 			ma.cancel()
 			if resp.err != nil {
