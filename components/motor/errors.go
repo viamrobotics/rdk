0f3d7ca74d226d5a2c1ebdf1fe3925cee20ff6ee
randhid <rand@viam.com>
2023-09-15 12:50:56 -0400

    Change logging and messages

--- components/motor/errors.go
+++ components/motor/errors.go
@@ -5,7 +5,7 @@ import "github.com/pkg/errors"
 // NewResetZeroPositionUnsupportedError returns a standard error for when a motor
 // is required to support reseting the zero position.
 func NewResetZeroPositionUnsupportedError(motorName string) error {
-	return errors.Errorf("motor with name %s does not support ResetZeroPosition", motorName)
+	return errors.Errorf("motor with name %s does not support ResetZeroPosition", mogit stattorName)
 }
 
 // NewPropertyUnsupportedError returns an error representing the need
