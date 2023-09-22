92c7ebcb95940c4c4d4f52ec92bd18ccbc96b8d3
Cheuk Tse <cheuk@viam.com>
2023-09-22 14:23:59 -0400

    fix test

--- robot/impl/robot_reconfigure_test.go
+++ robot/impl/robot_reconfigure_test.go
@@ -3507,7 +3507,7 @@ func TestResourceConstructTimeout(t *testing.T) {
 	timeOutErrorCount := func() int {
 		return logs.Filter(func(o observer.LoggedEntry) bool {
 			for k, v := range o.ContextMap() {
-				if k == "error" && strings.Contains(fmt.Sprint(v), "timed out during reconfigure") {
+				if k == "error" && strings.Contains(fmt.Sprint(v), "timed out after") {
 					return true
 				}
 			}
