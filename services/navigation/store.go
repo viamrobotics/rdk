e445df7d9febeddc836f5a1a608e00da4eafa409
Ray Bjorkman <raymond@viam.com>
2023-09-27 12:47:49 -0400

    add infrastructure for default store

--- services/navigation/store.go
+++ services/navigation/store.go
@@ -54,7 +54,6 @@ func (config *StoreConfig) Validate(path string) error {
 	return nil
 }
 
-// NewStoreFromConfig builds a NavStore from the provided StoreConfig and returns it.
 func NewStoreFromConfig(ctx context.Context, conf StoreConfig) (NavStore, error) {
 	switch conf.Type {
 	case StoreTypeMemory:
