93fc1e3d805c5b517f9d2bd93b1a26cdc84d66ae
Alexa Greenberg <alexa.greenberg@viam.com>
2023-09-28 12:38:57 -0400

    [DATA-1985]: Add the ability to use "get_images()" in the data capture service configuration. (#2990)

--- components/camera/camera.go
+++ components/camera/camera.go
@@ -42,6 +42,10 @@ func init() {
 		API:        API,
 		MethodName: readImage.String(),
 	}, newReadImageCollector)
+	data.RegisterCollector(data.MethodMetadata{
+		API:        API,
+		MethodName: getImages.String(),
+	}, newGetImagesCollector)
 }
 
 // SubtypeName is a constant that identifies the camera resource subtype string.
