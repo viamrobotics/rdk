b192b7654037e908b3a36dc8b89a48a6b7e250e2
Nick Franczak <nicholasgfranczak@gmail.com>
2023-09-28 15:08:38 -0400

    initial commit

--- components/arm/universalrobots/ur5e_test.go
+++ components/arm/universalrobots/ur5e_test.go
@@ -48,7 +48,17 @@ func testUR5eInverseKinematics(t *testing.T, pos spatialmath.Pose) {
 
 	m, err := referenceframe.UnmarshalModelJSON(ur5modeljson, "")
 	test.That(t, err, test.ShouldBeNil)
-	steps, err := motionplan.PlanFrameMotion(ctx, logger, pos, m, referenceframe.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0}), nil, nil)
+	planRequest := motionplan.PlanRequest{
+		Logger:             logger,
+		Goal:               referenceframe.NewPoseInFrame(referenceframe.World, pos),
+		Frame:              m,
+		FrameSystem:        nil,
+		StartConfiguration: map[string][]referenceframe.Input{m.Name(): referenceframe.FloatsToInputs([]float64{0, 0, 0, 0, 0, 0})},
+	}
+	plan, err := motionplan.PlanMotion(ctx, &planRequest)
+	test.That(t, err, test.ShouldBeNil)
+	steps, err := plan.GetFrameSteps(m.Name())
+	test.That(t, err, test.ShouldBeNil)
 
 	test.That(t, err, test.ShouldBeNil)
 	solution := steps[len(steps)-1]
