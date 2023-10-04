510611ffe10e13bbb969d427132186d8760418b8
jeremyrhyde <jeremy@viam.com>
2023-10-04 14:48:34 -0400

    match logger pattern of waypoint

--- services/navigation/builtin/explore.go
+++ services/navigation/builtin/explore.go
@@ -6,5 +6,5 @@ import (
 
 //nolint:unparam
 func (svc *builtIn) startExploreMode(ctx context.Context) {
-	svc.logger.Warn("Explore mode is currently disabled")
+	svc.logger.Debug("startExploreMode called")
 }
