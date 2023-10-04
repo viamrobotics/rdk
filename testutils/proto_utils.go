f6974ad3fe1d58731b0aa914fd41dcda7696417d
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2023-10-04 11:31:41 -0400

    fix lint

--- testutils/proto_utils.go
+++ testutils/proto_utils.go
@@ -2,6 +2,8 @@ package testutils
 
 import "go.viam.com/utils/protoutils"
 
+// ToProtoMapIgnoreOmitEmpty is a helper to convert anything
+// to a map to compare against a structpb.
 func ToProtoMapIgnoreOmitEmpty(data any) map[string]any {
 	ret, err := protoutils.StructToStructPbIgnoreOmitEmpty(data)
 	if err != nil {
