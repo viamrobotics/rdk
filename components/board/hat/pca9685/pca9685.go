viamrobotics/rdk

commit 28978986f93aac8dcab2f4bfdbf69f0cd7c19a99

[RSDK-5240] Let the PCA9685 component talk directly to the I2C bus (#3030)
This has a couple changes:
- In the genericlinux package, there's now a `GetI2CBus` function. If you give it a bus number, it gets the raw I2C bus straight from the OS, and otherwise it tries to get the bus from the board component like we used to do. This way, you can either specify the I2C bus the old way (by naming the board and the entry in the "i2cs" map within the board) or the new way (just name the raw I2C bus on the machine). This function has a TODO to remove it again when all robot configs are converted from the old approach to the new one.
- In the PCA9685 component, make the `board_name` and `bus_name` entries in the config optional, and also add an optional `i2c_bus` int. In the `Validate` function, make sure that either the first two or that last one is present.
- In the PCA9685 component, call `GetI2CBus` to get the bus. It takes in the new `i2c_bus` int if present, and the `board_name` and `bus_name` if it's not present.
- Remove the "return early if nothing has changed" paragraph in the PCA9685's reconfiguration. It was "saving" a trivial amount of work (to the point that the if statement itself might have run slower than the work it saved). It would need adapting as the definition of "if nothing has changed" would need to be updated, but removing it was way easier.

Once this change is in, we can update all the robot configs to use the new approach instead of the old one, and afterwards stop using `GetI2CBus` in favor of just calling `NewI2cBus` ourselves. I can find 13 configs in the database that mention a PCA9685 board.

Tried on a raspberry pi with a PCA9685 with a servo hooked to it: works as normal with both this old config (with trivial parts removed to make it easier to read):
```
"components": [
    {
      "attributes": {
        "i2cs": [
          {
            "bus": "1",
            "name": "main"
          }
        ],
      },

components/board/hat/pca9685/pca9685.go
@@ -4,7 +4,6 @@ package pca9685
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"sync"
 	"time"
@@ -16,6 +15,7 @@ import (
 	"go.viam.com/utils"
 
 	"go.viam.com/rdk/components/board"
+	"go.viam.com/rdk/components/board/genericlinux"
 	"go.viam.com/rdk/grpc"
 	"go.viam.com/rdk/resource"
 )
@@ -29,8 +29,9 @@ var (
 
 // Config describes a PCA9685 board attached to some other board via I2C.
 type Config struct {
-	BoardName      string `json:"board_name"`
-	I2CName        string `json:"i2c_name"`
+	BoardName      string `json:"board_name,omitempty"`
+	I2CName        string `json:"i2c_name,omitempty"`
+	I2CBus         *int   `json:"i2c_bus,omitempty"`
 	I2CAddress     *int   `json:"i2c_address,omitempty"`
 	PWMFrequencyHz int    `json:"pwm_frequency_hz,omitempty"`
 }
@@ -38,19 +39,30 @@ type Config struct {
 // Validate ensures all parts of the config are valid.
 func (conf *Config) Validate(path string) ([]string, error) {
 	var deps []string
-	if conf.BoardName == "" {
-		return nil, utils.NewConfigValidationFieldRequiredError(path, "board_name")
+	// Either the i2c bus or both the board name and i2c name is required.
+	if conf.I2CBus == nil {
+		if conf.BoardName == "" && conf.I2CName == "" {
+			// If all 3 are missing, prefer the i2c_bus approach.
+			return nil, utils.NewConfigValidationFieldRequiredError(path, "i2c_bus")
+		}
+		// Otherwise, we're just missing either the board name or i2c name.
+		if conf.BoardName == "" {
+			return nil, utils.NewConfigValidationFieldRequiredError(path, "board_name")
+		}
+		if conf.I2CName == "" {
+			return nil, utils.NewConfigValidationFieldRequiredError(path, "i2c_name")
+		}
 	}
-	if conf.I2CName == "" {
-		return nil, utils.NewConfigValidationFieldRequiredError(path, "i2c_name")
-	}
+
 	if conf.I2CAddress == nil {
 		conf.I2CAddress = &defaultAddr
 	}
 	if *conf.I2CAddress < 0 || *conf.I2CAddress > 255 {
 		return nil, utils.NewConfigValidationError(path, errors.New("i2c_address must be an unsigned byte"))
 	}
-	deps = append(deps, conf.BoardName)
+	if conf.BoardName != "" {
+		deps = append(deps, conf.BoardName)
+	}
 	return deps, nil
 }
 
@@ -126,28 +138,19 @@ func (pca *PCA9685) Reconfigure(ctx context.Context, deps resource.Dependencies,
 		return err
 	}
 
-	b, err := board.FromDependencies(deps, newConf.BoardName)
-	if err != nil {
-		return err
-	}
-	localBoard, ok := b.(board.LocalBoard)
-	if !ok {
-		return fmt.Errorf("board %s is not local", newConf.BoardName)
+	busNum := 0
+	if newConf.I2CBus != nil {
+		busNum = *newConf.I2CBus
 	}
 
-	bus, ok := localBoard.I2CByName(newConf.I2CName)
-	if !ok {
-		return errors.Errorf("can't find I2C bus (%s) requested by Motor", newConf.I2CName)
+	bus, err := genericlinux.GetI2CBus(deps, newConf.BoardName, newConf.I2CName, busNum)
+	if err != nil {
+		return err
 	}
 	address := byte(*newConf.I2CAddress)
 
 	pca.mu.Lock()
 	defer pca.mu.Unlock()
-
-	needsReset := pca.boardName != newConf.BoardName || pca.i2cName != newConf.I2CName
-	if !needsReset {
-		return nil
-	}
 
 	pca.bus = bus
 	pca.address = address

components/board/genericlinux/i2c.go
@@ -5,6 +5,7 @@ package genericlinux
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/edaniels/golog"
@@ -13,6 +14,7 @@ import (
 	"periph.io/x/host/v3"
 
 	"go.viam.com/rdk/components/board"
+	"go.viam.com/rdk/resource"
 )
 
 func init() {
@@ -150,4 +152,30 @@ func (h *I2cHandle) Close() error {
 	h.device = nil
 	// Don't close the bus itself: it should remain open for other handles to use
 	return nil
+}
+
+// GetI2CBus retrieves an I2C interface. If the bus number is specified, it uses that on the local
+// machine, and otherwise it tries to get the named bus from the named board.
+// TODO(RSDK-5254): remove this once all I2C devices are talking directly to the bus without going
+// through the board.
+func GetI2CBus(deps resource.Dependencies, boardName, busName string, busNum int) (board.I2C, error) {
+	if busNum != 0 {
+		return NewI2cBus(fmt.Sprintf("%d", busNum))
+	}
+
+	// Otherwise, look things up through the board.
+	b, err := board.FromDependencies(deps, boardName)
+	if err != nil {
+		return nil, err
+	}
+	localBoard, ok := b.(board.LocalBoard)
+	if !ok {
+		return nil, fmt.Errorf("cannot get I2C bus '%s' from nonlocal board '%s'",
+			busName, boardName)
+	}
+	bus, success := localBoard.I2CByName(busName)
+	if !success {
+		return nil, fmt.Errorf("unknown I2C bus %s on board %s", busName, boardName)
+	}
+	return bus, nil
 }

commit b34d7db3573c3bb9e0d7dbeddaa1c16407bf1b05

The PCA9685 is Linux-only (#3038)
Sorry to break the Mac build!

Linking things up: the Mac build was broken in https://github.com/viamrobotics/rdk/pull/3030, and this PR is an attempt to fix it again. 

What happened: we're trying to get all components that use the I2C bus to talk to that bus directly, instead of going through a board component like a raspberry pi to do so. This will let you have boards in modular components while still using I2C components built into the RDK itself. However, we currently only support talking directly to the I2C bus on Linux. The PCA9685 used to build successfully on any platform because it would tell another component to talk to the I2C bus (and that other component didn't exist on Macs but things would still compile fine as long as you didn't try using it). Now that we have removed that indirection, though, this needs to be Linux-only.

components/board/hat/pca9685/pca9685.go
@@ -1,3 +1,5 @@
+//go:build linux
+
 // Package pca9685 implements a PCA9685 HAT. It's probably also a generic PCA9685
 // but that has not been verified yet.
 package pca9685

components/board/hat/pca9685/pca9685_nonlinux.go
@@ -1,0 +1,2 @@
+// Package pca9685 is unimplemented for Macs.
+package pca9685