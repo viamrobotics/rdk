79cd2f1b5d916a22384d0ff79a766d8442f46b04
Peter LoVerso <biotinker@gmail.com>
2023-10-12 08:16:54 -0700

    lint

--- services/motion/localizer_test.go
+++ services/motion/localizer_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 
 	geo "github.com/kellydunn/golang-geo"
+	"go.viam.com/test"
 
-	"go.viam.com/rdk/services/motion"
 	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/services/motion"
 	"go.viam.com/rdk/spatialmath"
 	"go.viam.com/rdk/testutils/inject"
-	"go.viam.com/test"
 )
 
 func createInjectedMovementSensor(name string, gpsPoint *geo.Point) *inject.MovementSensor {
@@ -30,30 +31,30 @@ func createInjectedMovementSensor(name string, gpsPoint *geo.Point) *inject.Move
 
 func TestLocalizerOrientation(t *testing.T) {
 	ctx := context.Background()
-	
+
 	origin := geo.NewPoint(-70, 40)
 	movementSensor := createInjectedMovementSensor("", origin)
 	localizer := motion.NewMovementSensorLocalizer(movementSensor, origin, spatialmath.NewZeroPose())
-	
+
 	heading, err := movementSensor.CompassHeading(ctx, nil)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, heading, test.ShouldEqual, 0)
-	
+
 	pif, err := localizer.CurrentPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
-	
+
 	// A compass heading of 0 means we are pointing north. If the base's 0 is pointing east, north is a +90 degree right-hand rotation.
 	test.That(t, spatialmath.OrientationAlmostEqual(
 		pif.Pose().Orientation(),
 		&spatialmath.OrientationVectorDegrees{OZ: 1, Theta: 90}),
 		test.ShouldBeTrue,
 	)
-	
+
 	// Update compass heading to point northwest
 	movementSensor.CompassHeadingFunc = func(ctx context.Context, extra map[string]interface{}) (float64, error) {
 		return 315, nil
 	}
-	
+
 	pif, err = localizer.CurrentPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	// A compass heading of 315 means we are pointing northwest.
@@ -63,14 +64,14 @@ func TestLocalizerOrientation(t *testing.T) {
 		&spatialmath.OrientationVectorDegrees{OZ: 1, Theta: 135}),
 		test.ShouldBeTrue,
 	)
-	
+
 	// Update compass heading to point east
 	movementSensor.CompassHeadingFunc = func(ctx context.Context, extra map[string]interface{}) (float64, error) {
 		return 90, nil
 	}
 	pif, err = localizer.CurrentPosition(ctx)
 	test.That(t, err, test.ShouldBeNil)
-	
+
 	// A compass heading of 90 means we are pointing east.
 	// If the base's 0 is pointing east, this should result in a theta of zero.
 	test.That(t, spatialmath.OrientationAlmostEqual(
