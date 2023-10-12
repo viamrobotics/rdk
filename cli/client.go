a120b99392b99bc7f6ba72361698527340835c43
zaporter-work <work-github@zackporter.com>
2023-10-12 01:35:12 -0400

    initial

--- cli/client.go
+++ cli/client.go
@@ -78,7 +78,11 @@ func (c *viamClient) listOrganizationsAction(cCtx *cli.Context) error {
 		if i == 0 {
 			printf(cCtx.App.Writer, "Organizations for %q:", c.conf.Auth)
 		}
-		printf(cCtx.App.Writer, "\t%s (id: %s)", org.Name, org.Id)
+		printf(cCtx.App.Writer, "\t%s", org.Name)
+		printf(cCtx.App.Writer, "\t\tid: %s", org.Id)
+		if org.PublicNamespace != "" {
+			printf(cCtx.App.Writer, "\t\tnamespace: %s", org.PublicNamespace)
+		}
 	}
 	return nil
 }
