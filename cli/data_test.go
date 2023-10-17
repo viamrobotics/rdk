c7a3895aaf1b41c0ef973241fe050b5e55c03e9d
Alexa Greenberg <alexa.greenberg@viam.com>
2023-10-17 12:22:10 -0400

    lint

--- cli/data_test.go
+++ cli/data_test.go
@@ -8,19 +8,18 @@ import (
 )
 
 func TestFilenameForDownload(t *testing.T) {
-	const utc0 = "1970-01-01T00:00:00Z"
-	const utcReplaced = "1970-01-01T00_00_00Z"
+	const expectedUTC = "1970-01-01T00_00_00Z"
 	noFilename := filenameForDownload(&datapb.BinaryMetadata{Id: "my-id"})
-	test.That(t, noFilename, test.ShouldEqual, utcReplaced+"_my-id")
+	test.That(t, noFilename, test.ShouldEqual, expectedUTC+"_my-id")
 
 	normalExt := filenameForDownload(&datapb.BinaryMetadata{FileName: "whatever.txt"})
-	test.That(t, normalExt, test.ShouldEqual, utcReplaced+"_whatever.txt")
+	test.That(t, normalExt, test.ShouldEqual, expectedUTC+"_whatever.txt")
 
 	inFolder := filenameForDownload(&datapb.BinaryMetadata{FileName: "dir/whatever.txt"})
 	test.That(t, inFolder, test.ShouldEqual, "dir/whatever.txt")
 
 	gzAtRoot := filenameForDownload(&datapb.BinaryMetadata{FileName: "whatever.gz"})
-	test.That(t, gzAtRoot, test.ShouldEqual, utcReplaced+"_whatever")
+	test.That(t, gzAtRoot, test.ShouldEqual, expectedUTC+"_whatever")
 
 	gzInFolder := filenameForDownload(&datapb.BinaryMetadata{FileName: "dir/whatever.gz"})
 	test.That(t, gzInFolder, test.ShouldEqual, "dir/whatever")
