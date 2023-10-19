16f65118e6c1c6218ca1188622fcadbc01cef57a
Abe Winter <abe-winter@users.noreply.github.com>
2023-10-19 15:43:09 -0400

    lint

--- resource/resource.go
+++ resource/resource.go
@@ -182,10 +182,10 @@ func (t TriviallyValidateConfig) Validate(path string) ([]string, error) {
 	return nil, nil
 }
 
-// This is a clone of TriviallyValidateConfig that fails on 32-bit ARM.
+// TriviallyValidateExcept32Bit is a clone of TriviallyValidateConfig that fails on 32-bit ARM.
 type TriviallyValidateExcept32Bit struct{}
 
-// Fail validation on ARM 32-bit.
+// Validate fails validation on ARM 32-bit.
 func (t TriviallyValidateExcept32Bit) Validate(path string) ([]string, error) {
 	if runtime.GOARCH == "arm" {
 		return nil, errors.Errorf("%s not available on 32-bit ARM", path)
