47ccd1992fbdf087884651131dbbbda3dc37d1bc
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2023-10-23 13:52:54 -0500

    lint

--- components/movementsensor/replay/replay.go
+++ components/movementsensor/replay/replay.go
@@ -12,11 +12,6 @@ import (
 	geo "github.com/kellydunn/golang-geo"
 	"github.com/pkg/errors"
 	datapb "go.viam.com/api/app/data/v1"
-	"go.viam.com/rdk/components/movementsensor"
-	"go.viam.com/rdk/internal/cloud"
-	"go.viam.com/rdk/resource"
-	"go.viam.com/rdk/spatialmath"
-	"go.viam.com/rdk/utils/contextutils"
 	goutils "go.viam.com/utils"
 	"go.viam.com/utils/rpc"
 	"golang.org/x/exp/maps"
@@ -25,6 +20,12 @@ import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/internal/cloud"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/spatialmath"
+	"go.viam.com/rdk/utils/contextutils"
 )
 
 const (
