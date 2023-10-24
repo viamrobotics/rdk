7dd250adccd4151bc22ac5c8e650a7b2acc21665
Nick Franczak <nicholasgfranczak@gmail.com>
2023-10-24 10:42:12 -0400

    change starting configuration value

--- motionplan/motionPlanner.go
+++ motionplan/motionPlanner.go
@@ -517,7 +517,7 @@ func CheckPlan(
 	if sfPlanner.planOpts, err = sfPlanner.plannerSetupFromMoveRequest(
 		currentPosition,                    // starting pose
 		planNodes[len(planNodes)-1].Pose(), // goalPose
-		plan[0],                            // starting configuration
+		sf.sliceToMap(currentInputs),       // starting configuration
 		worldState,
 		nil, // no pb.Constraints
 		nil, // no plannOpts
