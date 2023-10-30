2b0ef086e6a983fabcc8fb504dee776bddb58786
Benji Rewis <benji.rewis@viam.com>
2023-10-30 09:43:23 -0700

    RSDK-5537 Relax LastReconfigured test time assertions

--- resource/resource_graph_test.go
+++ resource/resource_graph_test.go
@@ -971,9 +971,9 @@ func TestResourceGraphLastReconfigured(t *testing.T) {
 	lr := node1.LastReconfigured()
 	test.That(t, lr, test.ShouldNotBeNil)
 	// Assert that after SwapResource, node's lastReconfigured time is between
-	// 50ms ago and now.
+	// 10s ago and now.
 	test.That(t, *lr, test.ShouldHappenBetween,
-		time.Now().Add(-50*time.Millisecond), time.Now())
+		time.Now().Add(-10*time.Second), time.Now())
 
 	// Mock a mutation with another SwapResource. Assert that lastReconfigured
 	// value changed.
@@ -981,10 +981,10 @@ func TestResourceGraphLastReconfigured(t *testing.T) {
 	newLR := node1.LastReconfigured()
 	test.That(t, newLR, test.ShouldNotBeNil)
 	// Assert that after another SwapResource, node's lastReconfigured time is
-	// after old lr value and between 50ms ago and now.
+	// after old lr value and between 10s ago and now.
 	test.That(t, *newLR, test.ShouldHappenAfter, *lr)
 	test.That(t, *newLR, test.ShouldHappenBetween,
-		time.Now().Add(-50*time.Millisecond), time.Now())
+		time.Now().Add(-10*time.Second), time.Now())
 }
 
 func TestResourceGraphResolveDependencies(t *testing.T) {
