b66095cd627d825896192a28be6786f8959fda02
jeremyrhyde <34897732+jeremyrhyde@users.noreply.github.com>
2023-10-31 19:39:32 -0400

    Explore Mode using Motion Service (#3065)

--- services/navigation/builtin/explore.go
+++ services/navigation/builtin/explore.go
@@ -2,9 +2,44 @@ package builtin
 
 import (
 	"context"
+	"math/rand"
+
+	"github.com/golang/geo/r3"
+	"go.viam.com/utils"
+
+	frame "go.viam.com/rdk/referenceframe"
+	"go.viam.com/rdk/spatialmath"
 )
 
-//nolint:unparam
+const defaultDistanceMM = 100 * 1000
+
 func (svc *builtIn) startExploreMode(ctx context.Context) {
 	svc.logger.Debug("startExploreMode called")
+
+	svc.activeBackgroundWorkers.Add(1)
+	utils.PanicCapturingGo(func() {
+		defer svc.activeBackgroundWorkers.Done()
+
+		// Send motionCfg parameters through extra until motionCfg can be added to Move()
+		extra := map[string]interface{}{"motionCfg": svc.motionCfg}
+
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+
+			//nolint:gosec
+			destination := frame.NewPoseInFrame(svc.base.Name().Name, spatialmath.NewPose(
+				r3.Vector{
+					X: defaultDistanceMM * (2*rand.Float64() - 1.0),
+					Y: defaultDistanceMM * (2*rand.Float64() - 1.0),
+					Z: 0.,
+				}, spatialmath.NewOrientationVector()))
+
+			_, err := svc.exploreMotionService.Move(ctx, svc.base.Name(), destination, nil, nil, extra)
+			if err != nil {
+				svc.logger.Debugf("error occurred when moving to point %v: %v", destination, err)
+			}
+		}
+	})
 }
---
+++ services/navigation/builtin/explore_test.go
@@ -0,0 +1,42 @@
+package builtin
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/geo/r3"
+	"github.com/pkg/errors"
+	v1 "go.viam.com/api/service/motion/v1"
+	"go.viam.com/test"
+
+	frame "go.viam.com/rdk/referenceframe"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/testutils/inject"
+)
+
+func TestExploreMode(t *testing.T) {
+	cancelCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ns, teardown := setupNavigationServiceFromConfig(t, "../data/nav_no_map_cfg.json")
+
+	var points []r3.Vector
+	mockExploreMotionService := &inject.MotionService{}
+	mockExploreMotionService.MoveFunc = func(ctx context.Context, componentName resource.Name,
+		destination *frame.PoseInFrame, worldState *frame.WorldState, constraints *v1.Constraints,
+		extra map[string]interface{},
+	) (bool, error) {
+		points = append(points, destination.Pose().Point())
+		return false, errors.New("expected error")
+	}
+
+	nsStruct := ns.(*builtIn)
+	nsStruct.exploreMotionService = mockExploreMotionService
+
+	ctxTimeout, cancelFunc := context.WithTimeout(cancelCtx, 50*time.Millisecond)
+	defer cancelFunc()
+	nsStruct.startExploreMode(ctxTimeout)
+	<-ctxTimeout.Done()
+	teardown()
+	test.That(t, len(points), test.ShouldBeGreaterThan, 100)
+}
