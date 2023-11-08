025940f5ca79ecf48883d57084ae62794e8920bc
zaporter-work <work-github@zackporter.com>
2023-11-08 12:01:27 -0500

    cleanup options

--- module/modmanager/options/options.go
+++ module/modmanager/options/options.go
@@ -10,13 +10,10 @@ import (
 // Options configures a modManager.
 type Options struct {
 	UntrustedEnv bool
-
 	// Root of Viam server configuration. This is ~/.viam for now but will be migrating to /opt/viam as part of viam-agent
 	ViamHomeDir string
-
 	// Cloud ID of the robot. Empty if this is a local-only robot
 	RobotCloudID string
-
 	// RemoveOrphanedResources is a function that the module manager can call to
 	// remove orphaned resources from the resource graph.
 	RemoveOrphanedResources func(ctx context.Context, rNames []resource.Name)
