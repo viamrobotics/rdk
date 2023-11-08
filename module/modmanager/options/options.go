951c300066ce424f480fe9c44aaa4f99ff552f34
zaporter-work <work-github@zackporter.com>
2023-11-08 12:30:05 -0500

    lint-fix

--- module/modmanager/options/options.go
+++ module/modmanager/options/options.go
@@ -10,9 +10,9 @@ import (
 // Options configures a modManager.
 type Options struct {
 	UntrustedEnv bool
-	// Root of Viam server configuration. This is ~/.viam for now but will be migrating to /opt/viam as part of viam-agent
+	// ViamHomeDir is the root of Viam server configuration. This is ~/.viam for now but will become /opt/viam as part of viam-agent
 	ViamHomeDir string
-	// Cloud ID of the robot. Empty if this is a local-only robot
+	// RobotCloudID is the ID of the robot in app.viam.com. Empty if this is a local-only robot
 	RobotCloudID string
 	// RemoveOrphanedResources is a function that the module manager can call to
 	// remove orphaned resources from the resource graph.
