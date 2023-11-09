cc45141f23b02ffa284847ded28baaff12ca85a9
Sean Pollock <seanpollock3344@gmail.com>
2023-11-09 17:38:07 -0500

    Hotfix to check if h264 encoder is supported by the OS (#3216)

--- web/server/entrypoint_arm64.go
+++ web/server/entrypoint_arm64.go
@@ -5,18 +5,23 @@ package server
 import (
 	"go.viam.com/rdk/gostream"
 	"go.viam.com/rdk/gostream/codec/h264"
-	"go.viam.com/rdk/gostream/codec/h264/ffmpeg/avcodec"
 	"go.viam.com/rdk/gostream/codec/opus"
 	"go.viam.com/rdk/gostream/codec/x264"
 )
 
 func makeStreamConfig() gostream.StreamConfig {
 	var streamConfig gostream.StreamConfig
-	if avcodec.FindEncoderByName(h264.V4l2m2m) != nil {
-		streamConfig.VideoEncoderFactory = h264.NewEncoderFactory()
-	} else {
+
+	// Attempt to create a new encoder with hardcoded parameters
+	// to check if V4l2m2m codec is supported.
+	width, height, keyFrameInterval := 1920, 1080, 30
+	_, err := h264.NewEncoder(width, height, keyFrameInterval, nil)
+	if err != nil {
 		streamConfig.VideoEncoderFactory = x264.NewEncoderFactory()
+	} else {
+		streamConfig.VideoEncoderFactory = h264.NewEncoderFactory()
 	}
+
 	streamConfig.AudioEncoderFactory = opus.NewEncoderFactory()
 	return streamConfig
 }
