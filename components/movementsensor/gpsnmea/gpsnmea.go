50368a1b609e259a0b0fed8021fd35ef6e1af348
Alan Davidson <alan@viam.com>
2023-11-09 16:59:20 -0500

    fix bad rebase?

--- components/movementsensor/gpsnmea/gpsnmea.go
+++ components/movementsensor/gpsnmea/gpsnmea.go
@@ -133,9 +133,9 @@ func newNMEAGPS(
 
 	switch strings.ToLower(newConf.ConnectionType) {
 	case serialStr:
-		return NewSerialGPSNMEA(ctx, conf, logger)
+		return NewSerialGPSNMEA(ctx, conf.ResourceName(), newConf, logger)
 	case i2cStr:
-		return NewPmtkI2CGPSNMEA(ctx, deps, conf, logger)
+		return NewPmtkI2CGPSNMEA(ctx, deps, conf.ResourceName(), newConf, logger)
 	default:
 		return nil, connectionTypeError(
 			newConf.ConnectionType,
