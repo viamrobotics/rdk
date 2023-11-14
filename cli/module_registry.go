4c893cf0a5859e2666622f087e53af1910c08191
Alexa Greenberg <alexa.greenberg@viam.com>
2023-11-14 15:33:55 -0500

    Add omitempty to fix bug (#3235)

--- cli/module_registry.go
+++ cli/module_registry.go
@@ -72,14 +72,14 @@ var defaultBuildInfo = manifestBuildInfo{
 // moduleManifest is used to create & parse manifest.json.
 type moduleManifest struct {
 	// for backward compatibility - DO NOT SET as will be deprecated
-	Name        string            `json:"name,omitempty"`
-	ModuleID    string            `json:"module_id"`
-	Visibility  moduleVisibility  `json:"visibility"`
-	URL         string            `json:"url"`
-	Description string            `json:"description"`
-	Models      []ModuleComponent `json:"models"`
-	Entrypoint  string            `json:"entrypoint"`
-	Build       manifestBuildInfo `json:"build"`
+	Name        string             `json:"name,omitempty"`
+	ModuleID    string             `json:"module_id"`
+	Visibility  moduleVisibility   `json:"visibility"`
+	URL         string             `json:"url"`
+	Description string             `json:"description"`
+	Models      []ModuleComponent  `json:"models"`
+	Entrypoint  string             `json:"entrypoint"`
+	Build       *manifestBuildInfo `json:"build,omitempty"`
 }
 
 const (
