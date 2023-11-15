d544f74810f8b307e5de6d2546ff3dbb78e3dd27
Susmita Sanyal <susmita@viam.com>
2023-11-15 15:37:17 -0500

    lint

--- components/movementsensor/gpsrtkpmtk/gpsrtkpmtk.go
+++ components/movementsensor/gpsrtkpmtk/gpsrtkpmtk.go
@@ -488,8 +488,8 @@ func (g *rtkI2C) receiveAndWriteI2C(ctx context.Context) {
 	}
 }
 
-//nolint
-// getNtripConnectionStatus returns true if connection to NTRIP stream is OK, false if not.
+// nolint
+// getNtripConnectionStatus returns true if connection to NTRIP stream is OK, false if not
 func (g *rtkI2C) getNtripConnectionStatus() (bool, error) {
 	g.ntripMu.Lock()
 	defer g.ntripMu.Unlock()
