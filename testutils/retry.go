cb43c78a71cd38c7dfd4bd2391116e903406bcaf
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2023-11-21 14:22:57 -0600

    Lint

--- testutils/retry.go
+++ testutils/retry.go
@@ -2,10 +2,10 @@ package testutils
 
 import "time"
 
-type Condition func() bool
+type condition func() bool
 
-// Retry sleeps until a condition is met or a max of numRetries times
-func Retry(condition Condition, numRetries int) {
+// Retry sleeps until a condition is met or a max of numRetries times.
+func Retry(condition condition, numRetries int) {
 	for i := 0; i < numRetries && !condition(); i++ {
 		time.Sleep(time.Second)
 	}
