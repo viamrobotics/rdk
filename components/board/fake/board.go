eb89469264b35f951ba88fbb79dc0f760d34fe25
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2023-11-22 10:27:42 -0500

    analogs

--- components/board/fake/board.go
+++ components/board/fake/board.go
@@ -272,8 +272,10 @@ func (b *Board) SetPowerMode(ctx context.Context, mode pb.PowerMode, duration *t
 	return grpc.UnimplementedError
 }
 
-// WriteAnalog writes the value to the given pin.
+// WriteAnalog writes the value to the given pin. Add to AnalogReaders so you can read the value back.
 func (b *Board) WriteAnalog(ctx context.Context, pin string, value int32, extra map[string]interface{}) error {
+	alg := &AnalogReader{pin: pin, Value: int(value)}
+	b.AnalogReaders[pin] = alg
 	return nil
 }
 
