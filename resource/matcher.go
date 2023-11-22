bfb135a1391ae6be62c5a121d9759345c08154cc
Ray Bjorkman <raymond@viam.com>
2023-11-22 15:33:16 -0500

    add comments

--- resource/matcher.go
+++ resource/matcher.go
@@ -1,22 +1,26 @@
 package resource
 
-// Matcher matches resource expressions against resources.
+// Matcher describes whether a given resource matches its specified criteria.
 type Matcher interface {
 	IsMatch(Name) bool
 }
 
+// TypeMatcher matches resource names that have the given Type.
 type TypeMatcher struct {
 	Type string
 }
 
-func (am TypeMatcher) IsMatch(name Name) bool {
-	return name.API.Type.Name == am.Type
+// IsMatch returns true if the given name has a Type that matches the TypeMatcher's Type.
+func (tm TypeMatcher) IsMatch(name Name) bool {
+	return name.API.Type.Name == tm.Type
 }
 
+// SubtypeMatcher matches resource names that have the given Subtype.
 type SubtypeMatcher struct {
 	Subtype string
 }
 
+// IsMatch returns true if the given name has a Subtype that matches the SubtypeMatcher's Subtype.
 func (sm SubtypeMatcher) IsMatch(name Name) bool {
 	return name.API.Type.Name == sm.Subtype
 }
