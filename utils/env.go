6d8bec9ea712dcff948a5842e050c8dc9ac88367
Cheuk Tse <cheuk@viam.com>
2023-12-01 13:30:57 -0500

    something

--- utils/env.go
+++ utils/env.go
@@ -18,7 +18,7 @@ const (
 	ResourceConfigurationTimeoutEnvVar = "VIAM_RESOURCE_CONFIGURATION_TIMEOUT"
 
 	// DefaultModuleStartupTimeout is the default module startup timeout.
-	DefaultModuleStartupTimeout = 5 * time.Minute
+	DefaultModuleStartupTimeout = time.Minute
 
 	// ModuleStartupTimeoutEnvVar is the environment variable that can
 	// be set to override DefaultModuleStartupTimeout as the duration
