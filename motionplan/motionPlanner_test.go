482b40f67f84fe3d7877774ce67406d8af8e4b86
Alexa Greenberg <alexa.greenberg@viam.com>
2023-12-05 15:54:22 -0500

    RSDK-5930: Adjust nlopt to work better with PTGs (#3291)

--- motionplan/motionPlanner_test.go
+++ motionplan/motionPlanner_test.go
@@ -452,11 +452,10 @@ func TestArmAndGantrySolve(t *testing.T) {
 }
 
 func TestMultiArmSolve(t *testing.T) {
-	t.Parallel()
 	fs := makeTestFS(t)
 	positions := frame.StartPositions(fs)
 	// Solve such that the ur5 and xArm are pointing at each other, 40mm from gripper to camera
-	goal2 := spatialmath.NewPose(r3.Vector{Z: 40}, &spatialmath.OrientationVectorDegrees{OZ: -1})
+	goal2 := spatialmath.NewPose(r3.Vector{Z: 60}, &spatialmath.OrientationVectorDegrees{OZ: -1})
 	plan, err := PlanMotion(context.Background(), &PlanRequest{
 		Logger:             logger,
 		Goal:               frame.NewPoseInFrame("urCamera", goal2),
