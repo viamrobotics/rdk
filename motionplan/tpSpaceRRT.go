96a6e8db5cac2baee10de239ce3a7d5c7add7130
Peter LoVerso <biotinker@gmail.com>
2023-12-12 14:15:49 -0800

    Update motionplan/tpSpaceRRT.go

--- motionplan/tpSpaceRRT.go
+++ motionplan/tpSpaceRRT.go
@@ -67,7 +67,7 @@ const (
 	defaultMinTrajectoryLength = 350
 
 	// Print very fine-grained debug info. Useful for observing the inner RRT tree structure directly.
-	pathdebug = true
+	pathdebug = false
 )
 
 var defaultGoalMetricConstructor = ik.NewPosWeightSquaredNormMetric
