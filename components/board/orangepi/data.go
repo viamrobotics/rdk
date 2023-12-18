3ab0482775a45145d9bcd56ae8658932ee9482c7
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2023-12-18 10:42:23 -0500

    add 3lts

--- components/board/orangepi/data.go
+++ components/board/orangepi/data.go
@@ -3,6 +3,7 @@ package orangepi
 import "go.viam.com/rdk/components/board/genericlinux"
 
 const opzero2 = "OrangePi Zero2"
+const op3lts = "OrangePi 3 LTS"
 
 var boardInfoMappings = map[string]genericlinux.BoardInformation{
 	opzero2: {
@@ -27,5 +28,28 @@ var boardInfoMappings = map[string]genericlinux.BoardInformation{
 			{Name: "26", DeviceName: "gpiochip0", LineNumber: 7},
 		},
 		Compats: []string{"xunlong,orangepi-zero2", "allwinner,sun50i-h616"},
+	},
+	op3lts: {
+		PinDefinitions: []genericlinux.PinDefinition{
+			{Name: "3", DeviceName: "gpiochip1", LineNumber: 122},
+			{Name: "5", DeviceName: "gpiochip1", LineNumber: 121},
+			// When we can switch between gpio and pwm, the line number would be 118.
+			{Name: "7", DeviceName: "gpiochip1", LineNumber: -1, PwmChipSysfsDir: "300a000.pwm", PwmID: 0},
+			{Name: "11", DeviceName: "gpiochip1", LineNumber: 120},
+			{Name: "13", DeviceName: "gpiochip1", LineNumber: 119},
+			{Name: "15", DeviceName: "gpiochip0", LineNumber: 10},
+			{Name: "19", DeviceName: "gpiochip1", LineNumber: 229},
+			{Name: "21", DeviceName: "gpiochip1", LineNumber: 230},
+			{Name: "23", DeviceName: "gpiochip1", LineNumber: 228},
+			{Name: "8", DeviceName: "gpiochip0", LineNumber: 2},
+			{Name: "10", DeviceName: "gpiochip0", LineNumber: 3},
+			{Name: "12", DeviceName: "gpiochip1", LineNumber: 114},
+			{Name: "16", DeviceName: "gpiochip1", LineNumber: 111},
+			{Name: "18", DeviceName: "gpiochip1", LineNumber: 112},
+			{Name: "22", DeviceName: "gpiochip1", LineNumber: 117},
+			{Name: "24", DeviceName: "gpiochip1", LineNumber: 227},
+			{Name: "26", DeviceName: "gpiochip0", LineNumber: 8},
+		},
+		Compats: []string{"xunlong,orangepi-3-lts", "allwinner,sun50i-h6"},
 	},
 }
