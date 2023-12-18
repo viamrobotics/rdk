ca838a0fb43f4a89d42cc69bf4328cf45747eb2c
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2023-12-18 13:36:41 -0500

    PR comments

--- components/board/orangepi/data.go
+++ components/board/orangepi/data.go
@@ -2,11 +2,15 @@ package orangepi
 
 import "go.viam.com/rdk/components/board/genericlinux"
 
-const opzero2 = "OrangePi Zero2"
-const op3lts = "OrangePi 3 LTS"
+const (
+	opzero2 = "OrangePi Zero2"
+	op3lts  = "OrangePi 3 LTS"
+)
 
 var boardInfoMappings = map[string]genericlinux.BoardInformation{
 	opzero2: {
+		// OP zero 2 user manual: https://drive.google.com/drive/folders/1ToDjWZQptABxfiRwaeYW1WzQILM5iwpb
+		// Gpio pins can be found on page 147.
 		PinDefinitions: []genericlinux.PinDefinition{
 			{Name: "3", DeviceName: "gpiochip0", LineNumber: 229},
 			{Name: "5", DeviceName: "gpiochip0", LineNumber: 228},
@@ -30,23 +34,25 @@ var boardInfoMappings = map[string]genericlinux.BoardInformation{
 		Compats: []string{"xunlong,orangepi-zero2", "allwinner,sun50i-h616"},
 	},
 	op3lts: {
+		// OP 3 LTS user manual: https://drive.google.com/file/d/1jka7avWnzNeTIQFkk78LoJdygWaGH2iu/view
+		// Gpio pins can be found on page 145.
 		PinDefinitions: []genericlinux.PinDefinition{
 			{Name: "3", DeviceName: "gpiochip1", LineNumber: 122},
 			{Name: "5", DeviceName: "gpiochip1", LineNumber: 121},
 			// When we can switch between gpio and pwm, the line number would be 118.
 			{Name: "7", DeviceName: "gpiochip1", LineNumber: -1, PwmChipSysfsDir: "300a000.pwm", PwmID: 0},
-			{Name: "11", DeviceName: "gpiochip1", LineNumber: 120},
-			{Name: "13", DeviceName: "gpiochip1", LineNumber: 119},
-			{Name: "15", DeviceName: "gpiochip0", LineNumber: 10},
-			{Name: "19", DeviceName: "gpiochip1", LineNumber: 229},
-			{Name: "21", DeviceName: "gpiochip1", LineNumber: 230},
-			{Name: "23", DeviceName: "gpiochip1", LineNumber: 228},
 			{Name: "8", DeviceName: "gpiochip0", LineNumber: 2},
 			{Name: "10", DeviceName: "gpiochip0", LineNumber: 3},
+			{Name: "11", DeviceName: "gpiochip1", LineNumber: 120},
 			{Name: "12", DeviceName: "gpiochip1", LineNumber: 114},
+			{Name: "13", DeviceName: "gpiochip1", LineNumber: 119},
+			{Name: "15", DeviceName: "gpiochip0", LineNumber: 10},
 			{Name: "16", DeviceName: "gpiochip1", LineNumber: 111},
 			{Name: "18", DeviceName: "gpiochip1", LineNumber: 112},
+			{Name: "19", DeviceName: "gpiochip1", LineNumber: 229},
+			{Name: "21", DeviceName: "gpiochip1", LineNumber: 230},
 			{Name: "22", DeviceName: "gpiochip1", LineNumber: 117},
+			{Name: "23", DeviceName: "gpiochip1", LineNumber: 228},
 			{Name: "24", DeviceName: "gpiochip1", LineNumber: 227},
 			{Name: "26", DeviceName: "gpiochip0", LineNumber: 8},
 		},
