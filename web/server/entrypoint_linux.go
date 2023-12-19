961cf561fe84a248016af948b19b98e04f778bdb
Alexa Greenberg <alexa.greenberg@viam.com>
2023-12-19 15:33:25 -0500

    RSDK-5830 Add static FFmpeg libraries for Linux x86_64 (#3325)

---
+++ web/server/entrypoint_linux.go
@@ -0,0 +1,17 @@
+//go:build cgo && !(arm || android)
+
+package server
+
+import (
+	"go.viam.com/rdk/gostream"
+	"go.viam.com/rdk/gostream/codec/opus"
+	"go.viam.com/rdk/gostream/codec/x264"
+)
+
+func makeStreamConfig() gostream.StreamConfig {
+	var streamConfig gostream.StreamConfig
+	// TODO(RSDK-5916): support v4l2m2m codec on arm64/rpi
+	streamConfig.VideoEncoderFactory = x264.NewEncoderFactory()
+	streamConfig.AudioEncoderFactory = opus.NewEncoderFactory()
+	return streamConfig
+}
