f7a4d08482ce7f920558b9dfa13bb6b478dd38dd
Alexa Greenberg <alexa.greenberg@viam.com>
2023-12-20 14:11:51 -0500

    RSDK-5105 enable hardware pwm on custom linux (#3363)

--- components/board/customlinux/board.go
+++ components/board/customlinux/board.go
@@ -87,13 +87,13 @@ func parseRawPinData(pinData []byte, filePath string) ([]genericlinux.PinDefinit
 	for name, pin := range parsedPinData.Pins {
 		err = multierr.Combine(err, pin.Validate(filePath))
 
-		// Until we get hardware PWM working reliably on lots of boards (likely by being able to
-		// set the pin mode directly), we disable all hardware PWM on these boards.
-		// The range operator makes a copy of the value, so to change the original, we need to look
-		// it up in the map again.
-		// TODO(RSDK-5105): Undo this when we're ready to support hardware PWM.
-		parsedPinData.Pins[name].PwmChipSysfsDir = ""
-		parsedPinData.Pins[name].PwmID = -1
+		// Until we can reliably switch between gpio and pwm on lots of boards, pins that have
+		// hardware pwm enabled will be hardware pwm only. Disabling gpio functionality on these
+		// pins.
+		if parsedPinData.Pins[name].PwmChipSysfsDir != "" && parsedPinData.Pins[name].LineNumber >= 0 {
+			logging.Global().Warnf("pin %s can be used for PWM only", parsedPinData.Pins[name].Name)
+			parsedPinData.Pins[name].LineNumber = -1
+		}
 	}
 	if err != nil {
 		return nil, err
