0e37d4fa3e159a6ef299dbbed5af4bc3b6301466
Alan Davidson <alan@viam.com>
2023-12-21 10:56:09 -0500

    [RSDK-6168] Plumb a logger through the customlinux ConfigConverter (#3370)
    The constructor already had a logger present, but the `ConfigConverter` was using the global logger. So, this PR adds a logger to the arguments to that function type. The only other `ConfigConverter` doesn't log anything, so nothing in it changed.

--- components/board/customlinux/setup_test.go
+++ components/board/customlinux/setup_test.go
@@ -9,27 +9,29 @@ import (
 	"go.viam.com/test"
 
 	"go.viam.com/rdk/components/board/genericlinux"
+	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/resource"
 )
 
 func TestConfigParse(t *testing.T) {
+	logger := logging.NewTestLogger(t)
 	emptyConfig := []byte(`{"pins": [{}]}`)
-	_, err := parseRawPinData(emptyConfig, "path")
+	_, err := parseRawPinData(emptyConfig, "path", logger)
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, resource.GetFieldFromFieldRequiredError(err), test.ShouldEqual, "name")
 
 	emptyPWMID := []byte(`{"pins": [{"name": "7", "device_name": "gpiochip1", "line_number": 71, "pwm_chip_sysfs_dir": "hi"}]}`)
-	_, err = parseRawPinData(emptyPWMID, "path")
+	_, err = parseRawPinData(emptyPWMID, "path", logger)
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, err.Error(), test.ShouldContainSubstring, "must supply pwm_id for the pwm chip")
 
 	invalidLineNumber := []byte(`{"pins": [{"name": "7", "device_name": "gpiochip1", "line_number": -2}]}`)
-	_, err = parseRawPinData(invalidLineNumber, "path")
+	_, err = parseRawPinData(invalidLineNumber, "path", logger)
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, err.Error(), test.ShouldContainSubstring, "line_number on gpio chip must be at least zero")
 
 	validConfig := []byte(`{"pins": [{"name": "7", "device_name": "gpiochip1", "line_number": 80}]}`)
-	data, err := parseRawPinData(validConfig, "path")
+	data, err := parseRawPinData(validConfig, "path", logger)
 	correctData := make([]genericlinux.PinDefinition, 1)
 	correctData[0] = genericlinux.PinDefinition{
 		Name:       "7",
