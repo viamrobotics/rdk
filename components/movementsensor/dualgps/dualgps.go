8415395a86fef297c58a9084afcd674427c7a9a8
Susmita Sanyal <susmita@viam.com>
2023-12-21 13:26:18 -0500

    dualgps

--- components/movementsensor/dualgps/dualgps.go
+++ components/movementsensor/dualgps/dualgps.go
@@ -225,8 +225,9 @@ func (dg *dualGPS) Orientation(ctx context.Context, extra map[string]interface{}
 	return spatialmath.NewZeroOrientation(), movementsensor.ErrMethodUnimplementedOrientation
 }
 
-func (dg *dualGPS) Accuracy(ctx context.Context, extra map[string]interface{}) (map[string]float32, error) {
-	return map[string]float32{}, movementsensor.ErrMethodUnimplementedAccuracy
+func (dg *dualGPS) Accuracy(ctx context.Context, extra map[string]interface{}) (map[string]float32,
+	float32, float32, movementsensor.NmeaGGAFixType, float32, error) {
+	return map[string]float32{}, 0, 0, 0, 0, movementsensor.ErrMethodUnimplementedAccuracy
 }
 
 func (dg *dualGPS) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
