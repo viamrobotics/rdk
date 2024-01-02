c0ab5e7e5584091a2158cee543504ddfefc27288
Susmita Sanyal <susmita@viam.com>
2024-01-02 13:10:04 -0500

    linter issue

--- components/movementsensor/gpsnmea/pmtkI2C.go
+++ components/movementsensor/gpsnmea/pmtkI2C.go
@@ -209,7 +209,6 @@ func (g *PmtkI2CNMEAMovementSensor) GetBusAddr() (buses.I2C, byte) {
 	return g.bus, g.addr
 }
 
-// nolint
 // Position returns the current geographic location of the MovementSensor.
 func (g *PmtkI2CNMEAMovementSensor) Position(ctx context.Context, extra map[string]interface{}) (*geo.Point, float64, error) {
 	lastPosition := g.lastPosition.GetLastPosition()
