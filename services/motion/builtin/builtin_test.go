2fcdc5431d1d662ba125fd2fa8fc2043a18ba500
Nick Franczak <nicholasgfranczak@gmail.com>
2024-01-08 16:27:06 -0500

    add back todo

--- services/motion/builtin/builtin_test.go
+++ services/motion/builtin/builtin_test.go
@@ -1621,7 +1621,7 @@ func TestMoveOnMapNew(t *testing.T) {
 	})
 
 	t.Run("Is able to reach the zero pose with an empty motion configuration", func(t *testing.T) {
-
+		// TODO(RSDK-6272): How to handle solving for zerpo poses
 		t.Skip()
 		_, ms := createMoveOnMapEnvironment(ctx, t, "pointcloud/octagonspace.pcd", 40, nil)
 		defer ms.Close(ctx)
