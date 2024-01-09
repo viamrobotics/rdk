be5274c3a7cb2ae76c5cfe5ad96e3b99bcc8feff
Susmita Sanyal <susmita@viam.com>
2024-01-09 14:16:26 -0500

    lint and mpu6050 update

--- components/movementsensor/accuracy.go
+++ components/movementsensor/accuracy.go
@@ -2,6 +2,31 @@ package movementsensor
 
 import pb "go.viam.com/api/component/movementsensor/v1"
 
+// Accuracy defines the precision and reliability metrics of a movement sensor.
+// It includes various parameters to assess the sensor's accuracy in different dimensions.
+//
+// Fields:
+//
+// AccuracyMap: A mapping of specific measurement parameters to their accuracy values.
+// The keys are string identifiers for each measurement (e.g., "Hdop", "Vdop"),
+// and the values are their corresponding accuracy levels as float32.
+//
+// Hdop: Horizontal Dilution of Precision (HDOP) value. It indicates the level of accuracy
+// of horizontal measurements. Lower values represent higher precision.
+//
+// Vdop: Vertical Dilution of Precision (VDOP) value. Similar to HDOP, it denotes the
+// accuracy level of vertical measurements. Lower VDOP values signify better precision.
+//
+// NmeaFix: An integer value representing the quality of the NMEA fix.
+// Higher values generally indicate a better quality fix, with specific meanings depending
+// on the sensor and context. Generally a fix of 1 or 2 lends to large position errors,
+// ideally we want a fix of 4-5. Other fixes are unsuitable for outdoor navigation.
+// The meaning of each fix value is documented here
+// https://docs.novatel.com/OEM7/Content/Logs/GPGGA.htm#GPSQualityIndicators
+//
+// CompassDegreeError: The estimated error in compass readings, measured in degrees.
+// It signifies the deviation or uncertainty in the sensor's compass measurements.
+// A lower value implies a more accurate compass direction.
 type Accuracy struct {
 	AccuracyMap        map[string]float32
 	Hdop               float32
@@ -10,6 +35,8 @@ type Accuracy struct {
 	CompassDegreeError float32
 }
 
+// ProtoFeaturesToAccuracy converts a GetAccuracyResponse from a protocol buffer (protobuf)
+// into an Accuracy struct.
 func ProtoFeaturesToAccuracy(resp *pb.GetAccuracyResponse) *Accuracy {
 	var hdop, vdop, compassError float32
 	var nmeaFix int32
@@ -36,6 +63,7 @@ func ProtoFeaturesToAccuracy(resp *pb.GetAccuracyResponse) *Accuracy {
 	}
 }
 
+// AccuracyToProtoResponse converts an Accuracy struct into a protobuf GetAccuracyResponse.
 func AccuracyToProtoResponse(acc *Accuracy) (*pb.GetAccuracyResponse, error) {
 	return &pb.GetAccuracyResponse{
 		Accuracy:            acc.AccuracyMap,
