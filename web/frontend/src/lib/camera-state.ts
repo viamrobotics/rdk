54b92cedbd045139065e0a8a46f8d032b2c7acf6
mrloureed <mrloureed@gmail.com>
2024-01-19 15:52:52 -0500

    fix text casing

--- web/frontend/src/lib/camera-state.ts
+++ web/frontend/src/lib/camera-state.ts
@@ -9,7 +9,7 @@ export const cameraStreamStates = new Map<string, StreamState>();
 export const selectedMap = {
   Live: -1,
   'Manual refresh': 0,
-  'Every 30 Seconds': 30,
-  'Every 10 Seconds': 10,
-  'Every Second': 1,
+  'Every 30 seconds': 30,
+  'Every 10 seconds': 10,
+  'Every second': 1,
 } as const;
