281c85cd6b912c99f5093b33ade578cc2a02ba6b
Alan Davidson <alan@viam.com>
2024-01-23 11:47:47 -0500

    [RSDK-1963] servo digital interrupts are only in the raspberry pi code (#3369)
    I had this much, much cleaner, where the only part left in the `board` package was the interface, and all the implementation details were left up to the boards. but then it turned out that a bunch of unit tests were using these as well, and I had to put a bunch of stuff back.
    
    Main changes:
    - The `PostProcessor` type is gone. The `Formula` field in the interrupt config is gone.
    - There is a copy of all the digital interrupt stuff (including the configs) in board/pi/impl/ now. That component uses this new copy.
    - The main (non-pi) version of digital interrupts no longer has a `ServoDigitalInterrupt` implementation.
    - The main (non-pi) version of digital interrupt configs no longer takes a Type.

--- components/board/pi/impl/digital_interrupts_test.go
+++ components/board/pi/impl/digital_interrupts_test.go
@@ -1,4 +1,6 @@
-package board
+//go:build linux && (arm64 || arm) && !no_pigpio && !no_cgo
+
+package piimpl
 
 import (
 	"context"
@@ -7,6 +9,8 @@ import (
 	"time"
 
 	"go.viam.com/test"
+
+	"go.viam.com/rdk/components/board"
 )
 
 func nowNanosecondsTest() uint64 {
@@ -33,7 +37,7 @@ func TestBasicDigitalInterrupt1(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, intVal, test.ShouldEqual, int64(1))
 
-	c := make(chan Tick)
+	c := make(chan board.Tick)
 	i.AddCallback(c)
 
 	timeNanoSec := nowNanosecondsTest()
@@ -51,7 +55,7 @@ func TestBasicDigitalInterrupt1(t *testing.T) {
 
 	i.RemoveCallback(c)
 
-	c = make(chan Tick, 2)
+	c = make(chan board.Tick, 2)
 	i.AddCallback(c)
 	go func() {
 		i.Tick(context.Background(), true, uint64(1))
@@ -78,7 +82,7 @@ func TestRemoveCallbackDigitalInterrupt(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, intVal, test.ShouldEqual, int64(1))
 
-	c1 := make(chan Tick)
+	c1 := make(chan board.Tick)
 	test.That(t, c1, test.ShouldNotBeNil)
 	i.AddCallback(c1)
 	var wg sync.WaitGroup
@@ -104,7 +108,7 @@ func TestRemoveCallbackDigitalInterrupt(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, intVal, test.ShouldEqual, int64(2))
 	wg.Wait()
-	c2 := make(chan Tick)
+	c2 := make(chan board.Tick)
 	test.That(t, c2, test.ShouldNotBeNil)
 	i.AddCallback(c2)
 	test.That(t, ret, test.ShouldBeTrue)
@@ -147,4 +151,48 @@ func TestRemoveCallbackDigitalInterrupt(t *testing.T) {
 	intVal, err = i.Value(context.Background(), nil)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, intVal, test.ShouldEqual, int64(3))
+}
+
+func TestServoInterrupt(t *testing.T) {
+	config := DigitalInterruptConfig{
+		Name: "s1",
+		Type: "servo",
+	}
+
+	s, err := CreateDigitalInterrupt(config)
+	test.That(t, err, test.ShouldBeNil)
+
+	now := uint64(0)
+	for i := 0; i < 20; i++ {
+		test.That(t, s.Tick(context.Background(), true, now), test.ShouldBeNil)
+		now += 1500 * 1000 // this is what we measure
+		test.That(t, s.Tick(context.Background(), false, now), test.ShouldBeNil)
+		now += 1000 * 1000 * 1000 // this is between measurements
+	}
+
+	intVal, err := s.Value(context.Background(), nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, intVal, test.ShouldEqual, int64(1500))
+}
+
+func TestServoInterruptWithPP(t *testing.T) {
+	config := DigitalInterruptConfig{
+		Name: "s1",
+		Type: "servo",
+	}
+
+	s, err := CreateDigitalInterrupt(config)
+	test.That(t, err, test.ShouldBeNil)
+
+	now := uint64(0)
+	for i := 0; i < 20; i++ {
+		test.That(t, s.Tick(context.Background(), true, now), test.ShouldBeNil)
+		now += 1500 * 1000 // this is what we measure
+		test.That(t, s.Tick(context.Background(), false, now), test.ShouldBeNil)
+		now += 1000 * 1000 * 1000 // this is between measurements
+	}
+
+	intVal, err := s.Value(context.Background(), nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, intVal, test.ShouldEqual, int64(1500))
 }
