064021f9fec8794127ab507bba5a6a6457377732
martha-johnston <martha.johnston@viam.com>
2024-01-26 11:33:41 -0500

    don't reqire exact position to move on

--- components/motor/gpio/motor_encoder.go
+++ components/motor/gpio/motor_encoder.go
@@ -434,8 +434,7 @@ func (m *EncodedMotor) GoFor(ctx context.Context, rpm, revolutions float64, extr
 			goal := m.state.goalPos
 			m.stateMu.Unlock()
 			pos, err := m.position(ctx, extra)
-			m.logger.Errorf("POS = %v, GOAL = %v", pos, goal)
-			return pos == goal, err
+			return rdkutils.Float64AlmostEqual(pos, goal, 1.0), err
 		}
 		return m.opMgr.WaitForSuccess(
 			ctx,
