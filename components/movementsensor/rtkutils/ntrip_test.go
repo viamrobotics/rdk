1e20a0b8fd2430db6d75deb893e381cdcde06367
Alan Davidson <alan@viam.com>
2024-02-06 18:27:56 -0500

    make lint

--- components/movementsensor/rtkutils/ntrip_test.go
+++ components/movementsensor/rtkutils/ntrip_test.go
@@ -11,7 +11,8 @@ import (
 
 func TestConnectInvalidURL(t *testing.T) {
 	logger := logging.NewTestLogger(t)
-	cancelCtx, _ := context.WithCancel(context.Background())
+	cancelCtx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
 
 	// Note: if MaxConnectAttempts is 0, we don't bother trying to connect.
 	ntripInfo := &NtripInfo{MaxConnectAttempts: 1}
@@ -22,14 +23,15 @@ func TestConnectInvalidURL(t *testing.T) {
 
 func TestConnectSucceeds(t *testing.T) {
 	logger := logging.NewTestLogger(t)
-	cancelCtx, _ := context.WithCancel(context.Background())
+	cancelCtx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
 
 	config := NtripConfig{
-		NtripURL: "http://fakeurl",
+		NtripURL:             "http://fakeurl",
 		NtripConnectAttempts: 10,
-		NtripMountpoint: "",
-		NtripUser: "user",
-		NtripPass: "pwd",
+		NtripMountpoint:      "",
+		NtripUser:            "user",
+		NtripPass:            "pwd",
 	}
 
 	ntripInfo, err := NewNtripInfo(&config, logger)
