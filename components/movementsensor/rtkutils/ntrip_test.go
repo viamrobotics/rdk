4ff53a2ec5bd67dc38d64c4849f8556223af15b0
Alan Davidson <alan@viam.com>
2024-02-06 17:35:32 -0500

    split test in two

--- components/movementsensor/rtkutils/ntrip_test.go
+++ components/movementsensor/rtkutils/ntrip_test.go
@@ -9,14 +9,20 @@ import (
 	"go.viam.com/rdk/logging"
 )
 
-func TestConnect(t *testing.T) {
+func TestConnectInvalidURL(t *testing.T) {
 	logger := logging.NewTestLogger(t)
 	cancelCtx, _ := context.WithCancel(context.Background())
 
+	// Note: if MaxConnectAttempts is 0, we don't bother trying to connect.
 	ntripInfo := &NtripInfo{MaxConnectAttempts: 1}
 	err := ntripInfo.Connect(cancelCtx, logger)
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, err.Error(), test.ShouldContainSubstring, `address must start with http://`)
+}
+
+func TestConnectSucceeds(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	cancelCtx, _ := context.WithCancel(context.Background())
 
 	config := NtripConfig{
 		NtripURL: "http://fakeurl",
@@ -26,7 +32,7 @@ func TestConnect(t *testing.T) {
 		NtripUser: "user",
 	}
 
-	ntripInfo, err = NewNtripInfo(&config, logger)
+	ntripInfo, err := NewNtripInfo(&config, logger)
 	test.That(t, err, test.ShouldBeNil)
 
 	err = ntripInfo.Connect(cancelCtx, logger)
