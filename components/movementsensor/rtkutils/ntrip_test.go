be53068a385ba1b4ea8c77a240908b0fddbb0b5a
Alan Davidson <alan@viam.com>
2024-02-06 17:02:56 -0500

    move test over, doesn't pass yet

---
+++ components/movementsensor/rtkutils/ntrip_test.go
@@ -0,0 +1,33 @@
+package rtkutils
+
+import (
+	"context"
+	"testing"
+
+	"go.viam.com/test"
+
+	"go.viam.com/rdk/logging"
+)
+
+func TestConnect(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	cancelCtx, _ := context.WithCancel(context.Background())
+
+	ntripInfo := &NtripInfo{}
+	err := ntripInfo.Connect(cancelCtx, logger)
+	test.That(t, err.Error(), test.ShouldContainSubstring, `address must start with http://`)
+
+	config := NtripConfig{
+		NtripURL: "http://fakeurl",
+		NtripConnectAttempts: 10,
+		NtripMountpoint: "",
+		NtripPass: "pwd",
+		NtripUser: "user",
+	}
+
+	ntripInfo, err = NewNtripInfo(&config, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	err = ntripInfo.Connect(cancelCtx, logger)
+	test.That(t, err, test.ShouldBeNil)
+}
