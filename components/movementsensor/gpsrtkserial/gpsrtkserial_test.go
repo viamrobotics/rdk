39375f0039f09b847567efbb8b8df37628986ec1
Alexa Greenberg <alexa.greenberg@viam.com>
2024-02-07 11:43:26 -0500

    [RSDK-5462] Remove duplicate functions when connecting to ntrip URLs (#3548)
    There's probably going to be another one or two of these, but this PR is already big enough.
    
    - The `rtkI2C.connect()` function used approximately nothing from the `rtkI2C` struct but a bunch of stuff from `rtkI2C.NtripInfo`.
    - Similarly, the `rtkSerial.connect()` function did roughly the same thing.
    - So, let's put that in the `NtripInfo` struct instead, and just call it from two places. Similarly, the two sets of tests can be de-duplicated into just one location.
    
    This PR should be easy to review commit-by-commit: the last few commits aren't exactly on the same topic as the rest.
    
    Tried on `piworld`, both with a bare NMEA component, and both an I2C and a serial RTK component: we've still got the race conditions from the double-start, resulting in a bunch of checksum errors, but it's nonetheless connecting and receiving data, which is the part this PR modifies.

--- components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
+++ components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
@@ -63,29 +63,6 @@ func TestValidateRTK(t *testing.T) {
 	})
 }
 
-func TestConnect(t *testing.T) {
-	logger := logging.NewTestLogger(t)
-	ctx := context.Background()
-	cancelCtx, cancelFunc := context.WithCancel(ctx)
-	g := rtkSerial{
-		cancelCtx:  cancelCtx,
-		cancelFunc: cancelFunc,
-		logger:     logger,
-	}
-
-	url := "http://fakeurl"
-	username := "user"
-	password := "pwd"
-
-	err := g.connect("invalidurl", username, password, 10)
-	test.That(t, err.Error(), test.ShouldContainSubstring, `address must start with http://`)
-
-	g.ntripClient = makeMockNtripClient()
-
-	err = g.connect(url, username, password, 10)
-	test.That(t, err, test.ShouldBeNil)
-}
-
 func TestReadings(t *testing.T) {
 	var (
 		alt   = 50.5
@@ -212,9 +189,4 @@ func (c *CustomMovementSensor) Position(ctx context.Context, extra map[string]in
 	}
 	// Fallback to the default implementation if PositionFunc is not set.
 	return c.MovementSensor.Position(ctx, extra)
-}
-
-// mock ntripinfo client.
-func makeMockNtripClient() *rtk.NtripInfo {
-	return &rtk.NtripInfo{}
 }
