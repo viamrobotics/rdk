a5ca01d872581d5c4fae74283225c28c1de43c06
Alexa Greenberg <alexa.greenberg@viam.com>
2024-02-09 12:22:10 -0500

    RSDK-6596 more module APIs in no_cgo (#3564)

--- services/register/all_cgo.go
+++ services/register/all_cgo.go
@@ -6,5 +6,4 @@ import (
 	// blank import registration pattern.
 	_ "go.viam.com/rdk/services/motion/register"
 	_ "go.viam.com/rdk/services/navigation/register"
-	_ "go.viam.com/rdk/services/vision/register"
 )
