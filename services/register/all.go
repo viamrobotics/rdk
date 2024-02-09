8dc034cf4d2a73d8772e49f4bb5207a7c211c024
abe-winter <abe-winter@users.noreply.github.com>
2024-02-09 12:22:10 -0500

    RSDK-6596 more module APIs in no_cgo (#3564)

--- services/register/all.go
+++ services/register/all.go
@@ -6,7 +6,9 @@ import (
 	_ "go.viam.com/rdk/services/baseremotecontrol/register"
 	_ "go.viam.com/rdk/services/datamanager/register"
 	_ "go.viam.com/rdk/services/generic/register"
+	_ "go.viam.com/rdk/services/mlmodel/register"
 	_ "go.viam.com/rdk/services/sensors/register"
 	_ "go.viam.com/rdk/services/shell/register"
 	_ "go.viam.com/rdk/services/slam/register"
+	_ "go.viam.com/rdk/services/vision/register"
 )
