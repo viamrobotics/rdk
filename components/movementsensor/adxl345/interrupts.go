57157787a3658b8b05278e4f792fb57d3e55d3ae
randhid <rand@viam.com>
2024-02-15 16:04:28 -0500

    unexport movement sensor constanta and functions

--- components/movementsensor/adxl345/interrupts.go
+++ components/movementsensor/adxl345/interrupts.go
@@ -8,39 +8,39 @@ package adxl345
 const (
 	// an unsigned time value representing the maximum time that an event must be above the
 	// THRESH_TAP threshold to qualify as a tap event [625 µs/LSB].
-	DurAddr byte = 0x21
+	durAddr byte = 0x21
 	// info on which interrupts have been enabled.
-	IntEnableAddr byte = 0x2E
+	intEnableAddr byte = 0x2E
 	// info on which interrupt pin to send each interrupt.
-	IntMapAddr byte = 0x2F
+	intMapAddr byte = 0x2F
 	// info on which interrupt has gone off since the last time this address has been read from.
-	IntSourceAddr byte = 0x30
+	intSourceAddr byte = 0x30
 	// an unsigned time value representing the wait time from the detection of a tap event to the
 	// start of the time window [1.25 ms/LSB].
-	LatentAddr byte = 0x22
+	latentAddr byte = 0x22
 	// info on which axes have been turned on for taps (X, Y, Z are bits 2, 1, 0 respectively).
-	TapAxesAddr byte = 0x2A
+	tapAxesAddr byte = 0x2A
 	// an unsigned threshold value for tap interrupts [62.5 mg/LSB ].
-	ThreshTapAddr byte = 0x1D
+	threshTapAddr byte = 0x1D
 	// che threshold value, in unsigned format, for free-fall detection.
-	ThreshFfAddr byte = 0x28
+	threshFfAddr byte = 0x28
 	// the  minimum time that the value of all axes must be less than THRESH_FF to generate a free-fall interrupt.
-	TimeFfAddr byte = 0x29
+	timeFfAddr byte = 0x29
 )
 
 // InterruptID is a type of interrupts available on ADXL345.
 type InterruptID = uint8
 
 const (
-	// SingleTap is a key value used to find various needs associated with this interrupt.
-	SingleTap InterruptID = iota
-	// FreeFall is a key value used to find various needs associated with this interrupt.
-	FreeFall InterruptID = iota
+	// singleTap is a key value used to find various needs associated with this interrupt.
+	singleTap InterruptID = iota
+	// freeFall is a key value used to find various needs associated with this interrupt.
+	freeFall InterruptID = iota
 )
 
 var interruptBitPosition = map[InterruptID]byte{
-	SingleTap: 1 << 6,
-	FreeFall:  1 << 2,
+	singleTap: 1 << 6,
+	freeFall:  1 << 2,
 }
 
 /*
@@ -53,29 +53,29 @@ From the data sheet:
 */
 var defaultRegisterValues = map[byte]byte{
 	// Interrupt Enabled
-	IntEnableAddr: 0x00,
-	IntMapAddr:    0x00,
+	intEnableAddr: 0x00,
+	intMapAddr:    0x00,
 
 	// Single Tap & Double Tap
-	TapAxesAddr:   0x07,
-	ThreshTapAddr: 0x30,
-	DurAddr:       0x10,
-	LatentAddr:    0x10,
+	tapAxesAddr:   0x07,
+	threshTapAddr: 0x30,
+	durAddr:       0x10,
+	latentAddr:    0x10,
 
 	// Free Fall
-	TimeFfAddr:   0x20, // 0x14 - 0x46 are recommended
-	ThreshFfAddr: 0x07, // 0x05 - 0x09 are recommended
+	timeFfAddr:   0x20, // 0x14 - 0x46 are recommended
+	threshFfAddr: 0x07, // 0x05 - 0x09 are recommended
 }
 
 const (
-	// ThreshTapScaleFactor is the scale factor for THRESH_TAP register.
-	ThreshTapScaleFactor float32 = 62.5
-	// DurScaleFactor is the scale factor for DUR register.
-	DurScaleFactor float32 = 625
-	// TimeFfScaleFactor is the scale factor for TIME_FF register.
-	TimeFfScaleFactor float32 = .5
-	// ThreshFfScaleFactor is the scale factor for THRESH_FF register.
-	ThreshFfScaleFactor float32 = 62.5
+	// threshTapScaleFactor is the scale factor for THRESH_TAP register.
+	threshTapScaleFactor float32 = 62.5
+	// durScaleFactor is the scale factor for DUR register.
+	durScaleFactor float32 = 625
+	// timeFfScaleFactor is the scale factor for TIME_FF register.
+	timeFfScaleFactor float32 = .5
+	// threshFfScaleFactor is the scale factor for THRESH_FF register.
+	threshFfScaleFactor float32 = 62.5
 )
 
 const (
