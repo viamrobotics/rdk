0a94a7b9b51de8ea9fc9e2c94fb1d21d3146e37a
Alexa Greenberg <alexa.greenberg@viam.com>
2024-02-21 18:19:18 -0500

    revert GIT_TAG to latest hash (#3602)

--- cli/client.go
+++ cli/client.go
@@ -491,7 +491,7 @@ func CheckUpdateAction(c *cli.Context) error {
 	}
 
 	appVersion := rconfig.Version
-	if appVersion == "(dev)" {
+	if appVersion == "" {
 		warningf(c.App.ErrWriter, "CLI Update Check: Your CLI is more than 6 weeks old. "+
 			"Consider updating to version: %s", latestVersion.Original())
 		return nil
@@ -540,6 +540,9 @@ func VersionAction(c *cli.Context) error {
 	}
 
 	appVersion := rconfig.Version
+	if appVersion == "" {
+		appVersion = "(dev)"
+	}
 	printf(c.App.Writer, "Version %s Git=%s API=%s", appVersion, version, apiVersion)
 	return nil
 }
