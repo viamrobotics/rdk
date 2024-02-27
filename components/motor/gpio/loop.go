ac036e951b9392381bd8b88dfa0ca69369b64306
martha-johnston <martha.johnston@viam.com>
2024-02-27 12:47:15 -0500

    use helper functions in encoded motor

--- components/motor/gpio/loop.go
+++ components/motor/gpio/loop.go
@@ -4,19 +4,10 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
-	"go.uber.org/multierr"
 
 	"go.viam.com/rdk/control"
 	rdkutils "go.viam.com/rdk/utils"
 )
-
-const (
-	blockNameEndpoint    = "endpoint"
-	blockNameConstant    = "constant"
-	blockNameTrapezoidal = "trapz"
-)
-
-// TODO: RSDK-5610 test the scaling factor with a non-pi board with hardware pwm.
 
 func errMissingBlock(blockType string) error {
 	return errors.Errorf("one block of type %s is required", blockType)
@@ -38,7 +29,7 @@ func (m *EncodedMotor) State(ctx context.Context) ([]float64, error) {
 func (m *EncodedMotor) updateControlBlock(ctx context.Context, setPoint, maxVel float64) error {
 	// Update the Trapezoidal Velocity Profile block with the given maxVel for velocity control
 	velConf := control.BlockConfig{
-		Name: m.blockNames[blockNameTrapezoidal],
+		Name: m.blockNames["trapezoidalVelocityProfile"][0],
 		Type: "trapezoidalVelocityProfile",
 		Attribute: rdkutils.AttributeMap{
 			"max_vel":    maxVel,
@@ -46,141 +37,72 @@ func (m *EncodedMotor) updateControlBlock(ctx context.Context, setPoint, maxVel 
 			"pos_window": 0.0,
 			"kpp_gain":   0.45,
 		},
-		DependsOn: []string{m.blockNames[blockNameConstant], m.blockNames[blockNameEndpoint]},
+		DependsOn: []string{m.blockNames["constant"][0], m.blockNames["endpoint"][0]},
 	}
-	if err := m.loop.SetConfigAt(ctx, m.blockNames[blockNameTrapezoidal], velConf); err != nil {
+	if err := m.loop.SetConfigAt(ctx, m.blockNames["trapezoidalVelocityProfile"][0], velConf); err != nil {
 		return err
 	}
 
 	// Update the Constant block with the given setPoint for position control
 	posConf := control.BlockConfig{
-		Name: m.blockNames[blockNameConstant],
+		Name: m.blockNames["trapezoidalVelocityProfile"][0],
 		Type: "constant",
 		Attribute: rdkutils.AttributeMap{
 			"constant_val": setPoint,
 		},
 		DependsOn: []string{},
 	}
-	if err := m.loop.SetConfigAt(ctx, m.blockNames[blockNameConstant], posConf); err != nil {
+	if err := m.loop.SetConfigAt(ctx, m.blockNames["constant"][0], posConf); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (m *EncodedMotor) storeBlockOfType(ctx context.Context, bType, bName string) error {
-	blocks := m.loop.ConfigsAtType(ctx, bType)
-	if len(blocks) != 1 {
-		return errMissingBlock(bType)
+func (m *EncodedMotor) setupControlLoop() error {
+	// set the necessary options for an encoded motor
+	options := control.Options{
+		PositionControlUsingTrapz: true,
+		LoopFrequency:             100.0,
 	}
-	m.blockNames[bName] = blocks[0].Name
+
+	// convert the motor config ControlParameters to the control.PIDConfig structure for use in setup_control.go
+	convertedControlParams := []control.PIDConfig{{
+		Type: "",
+		P:    m.cfg.ControlParameters.P,
+		I:    m.cfg.ControlParameters.I,
+		D:    m.cfg.ControlParameters.D,
+	}}
+
+	// auto tune motor if all ControlParameters are 0
+	// since there's only one set of PID values for a motor, they will always be at convertedControlParams[0]
+	if convertedControlParams[0].P == 0.0 &&
+		convertedControlParams[0].I == 0.0 &&
+		convertedControlParams[0].D == 0.0 {
+		options.NeedsAutoTuning = true
+	}
+
+	pl, err := control.SetupPIDControlConfig(convertedControlParams, m.Name().ShortName(), options, m, m.logger)
+	if err != nil {
+		return err
+	}
+
+	m.controlLoopConfig = pl.ControlConf
+	m.loop = pl.ControlLoop
+	m.blockNames = pl.BlockNames
+
 	return nil
 }
 
-// validateControlConfig ensures the programmatically edited blocks are named correctly.
-func (m *EncodedMotor) validateControlConfig(ctx context.Context) error {
-	m.blockNames = make(map[string]string)
-
-	// These three blocks are the only block names that are used by EncodedMotor
-	return multierr.Combine(
-		m.storeBlockOfType(ctx, "constant", blockNameConstant),
-		m.storeBlockOfType(ctx, "trapezoidalVelocityProfile", blockNameTrapezoidal),
-		m.storeBlockOfType(ctx, "endpoint", blockNameEndpoint),
-	)
-}
-
-// createControlLoopConfig creates a Control Loop Configuration that is embedded in this file so a user does not
-// have to configure the loop from within the attributes of the config file. It sets up a loop that takes a constant ->
-// trapezoidalVelocityProfile-> sum -> PID -> gain -> endpoint -> derivative back to sum, and endpoint
-// back to trapezoidalVelocityProfile structure. The gain is 0.0039 (1/255) to account for the PID range,
-// the PID values are experimental this structure can change as hardware experiments with an encoded motor require.
-func (m *EncodedMotor) createControlLoopConfig(p, i, d float64) control.Config {
-	conf := control.Config{
-		Blocks: []control.BlockConfig{
-			{
-				Name: "set_point",
-				Type: "constant",
-				Attribute: rdkutils.AttributeMap{
-					"constant_val": 0.0,
-				},
-			},
-			{
-				Name: "trapz",
-				Type: "trapezoidalVelocityProfile",
-				Attribute: rdkutils.AttributeMap{
-					"kpp_gain":   0.45,
-					"max_acc":    30000.0,
-					"max_vel":    4000.0,
-					"pos_window": 0.0,
-				},
-				DependsOn: []string{"set_point", "endpoint"},
-			},
-			{
-				Name: "PID",
-				Type: "PID",
-				Attribute: rdkutils.AttributeMap{
-					"int_sat_lim_lo": -255.0,
-					"int_sat_lim_up": 255.0,
-					"kD":             d,
-					"kI":             i,
-					"kP":             p,
-					"limit_lo":       -255.0,
-					"limit_up":       255.0,
-					"tune_method":    "ziegerNicholsPI",
-					"tune_ssr_value": 2.0,
-					"tune_step_pct":  0.35,
-				},
-				DependsOn: []string{"sum"},
-			},
-			{
-				Name: "gain",
-				Type: "gain",
-				Attribute: rdkutils.AttributeMap{
-					"gain": 0.00392156862,
-				},
-				DependsOn: []string{"PID"},
-			},
-			{
-				Name: "endpoint",
-				Type: "endpoint",
-				Attribute: rdkutils.AttributeMap{
-					"motor_name": m.Name().ShortName(),
-				},
-				DependsOn: []string{"gain"},
-			},
-			{
-				Name: "derivative",
-				Type: "derivative",
-				Attribute: rdkutils.AttributeMap{
-					"derive_type": "backward1st1",
-				},
-				DependsOn: []string{"endpoint"},
-			},
-		},
-		Frequency: 100.0,
+func (m *EncodedMotor) startControlLoop() error {
+	// create control loop
+	loop, err := control.NewLoop(m.logger, m.controlLoopConfig, m)
+	if err != nil {
+		return err
 	}
-
-	if p == 0.0 && i == 0.0 && d == 0.0 {
-		// when tuning (all PID are zero), the loop has to exclude the trapz block, so the sum block is
-		// temporarily changed to depend on the constant block instead of the trapz block to pass over trapz
-		conf = setSumBlock(conf, "set_point")
-	} else {
-		// if not tuning, set the sum block to once again depend
-		// on the trapz block instead of the constant bloc
-		conf = setSumBlock(conf, "trapz")
+	if err := loop.Start(); err != nil {
+		return err
 	}
+	m.loop = loop
 
-	return conf
-}
-
-func setSumBlock(conf control.Config, firstDependency string) control.Config {
-	sumBlock := control.BlockConfig{
-		Name: "sum",
-		Type: "sum",
-		Attribute: rdkutils.AttributeMap{
-			"sum_string": "+-",
-		},
-		DependsOn: []string{firstDependency, "derivative"},
-	}
-	conf.Blocks = append(conf.Blocks, sumBlock)
-	return conf
+	return nil
 }
--- components/motor/gpio/motor_encoder.go
+++ components/motor/gpio/motor_encoder.go
@@ -104,24 +104,7 @@ func newEncodedMotor(
 
 	// setup control loop
 	if motorConfig.ControlParameters != nil {
-		// create control loop config with PID values from config
-		em.controlLoopConfig = em.createControlLoopConfig(
-			motorConfig.ControlParameters.P,
-			motorConfig.ControlParameters.I,
-			motorConfig.ControlParameters.D,
-		)
-
-		cLoop, err := control.NewLoop(em.logger, em.controlLoopConfig, em)
-		if err != nil {
-			em.logger.Error(err)
-		}
-		if err = cLoop.Start(); err != nil {
-			em.logger.Error(err)
-		}
-		em.loop = cLoop
-
-		// validate control loop config
-		if err = em.validateControlConfig(cancelCtx); err != nil {
+		if err := em.startControlLoop(); err != nil {
 			return nil, err
 		}
 	} else {
@@ -178,7 +161,7 @@ type EncodedMotor struct {
 	opMgr     *operation.SingleOperationManager
 
 	controlLoopConfig control.Config
-	blockNames        map[string]string
+	blockNames        map[string][]string
 	loop              *control.Loop
 }
 
@@ -448,14 +431,9 @@ func (m *EncodedMotor) goForInternal(ctx context.Context, rpm, revolutions float
 	}
 	// create new control loop if control config exists
 	if m.cfg.ControlParameters != nil {
-		cLoop, err := control.NewLoop(m.logger, m.controlLoopConfig, m)
-		if err != nil {
-			m.logger.Error(err)
+		if err := m.setupControlLoop(); err != nil {
+			return err
 		}
-		if err = cLoop.Start(); err != nil {
-			m.logger.Error(err)
-		}
-		m.loop = cLoop
 	} else {
 		m.rpmMonitorStart()
 	}
