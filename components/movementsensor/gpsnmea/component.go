3e74fad1ff357e6303c253cecf7a4f6788870e16
Alexa Greenberg <alexa.greenberg@viam.com>
2024-03-07 18:47:03 -0500

    [RSDK-6525] Pull cached data out of the NMEAMovementSensor struct (#3652)
    This is mostly moving code around, rather than writing new stuff or deleting old stuff; it's not as big as it looks. I don't have a sense of whether it would be easier to read all at once or commit-by-commit.
    
    In this PR, we split the `NMEAMovementSensor` into a `CachedData` struct (which wraps `NmeaParser` and updates `lastCompassHeading` and other stateful stuff) and the remaining `NMEAMovementSensor` (which is a component which can be reconfigured, closed, etc.). In an upcoming PR, we'll make the `rtkSerial` and `rtkI2C` structs contain a `CachedData` instead of an `NMEAMovementSensor`, so that we're not embedding one component within another.

--- components/movementsensor/gpsnmea/component.go
+++ components/movementsensor/gpsnmea/component.go
@@ -3,12 +3,10 @@ package gpsnmea
 
 import (
 	"context"
-	"math"
 	"sync"
 
 	"github.com/golang/geo/r3"
 	geo "github.com/kellydunn/golang-geo"
-	"github.com/pkg/errors"
 	"go.uber.org/multierr"
 	"go.viam.com/utils"
 
@@ -18,8 +16,6 @@ import (
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/spatialmath"
 )
-
-var errNilLocation = errors.New("nil gps location, check nmea message parsing")
 
 // DataReader represents a way to get data from a GPS NMEA device. We can read data from it using
 // the channel in Messages, and we can close the device when we're done.
@@ -32,16 +28,11 @@ type DataReader interface {
 type NMEAMovementSensor struct {
 	resource.Named
 	resource.AlwaysRebuild
-	mu                      sync.RWMutex
 	cancelCtx               context.Context
 	cancelFunc              func()
 	logger                  logging.Logger
-	data                    rtkutils.GPSData
+	cachedData              rtkutils.CachedData
 	activeBackgroundWorkers sync.WaitGroup
-
-	err                movementsensor.LastError
-	lastPosition       movementsensor.LastPosition
-	lastCompassHeading movementsensor.LastCompassHeading
 
 	dev DataReader
 }
@@ -53,14 +44,12 @@ func NewNmeaMovementSensor(
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
 
 	g := &NMEAMovementSensor{
-		Named:              name.AsNamed(),
-		dev:                dev,
-		cancelCtx:          cancelCtx,
-		cancelFunc:         cancelFunc,
-		logger:             logger,
-		err:                movementsensor.NewLastError(1, 1),
-		lastPosition:       movementsensor.NewLastPosition(),
-		lastCompassHeading: movementsensor.NewLastCompassHeading(),
+		Named:      name.AsNamed(),
+		dev:        dev,
+		cancelCtx:  cancelCtx,
+		cancelFunc: cancelFunc,
+		logger:     logger,
+		cachedData: rtkutils.NewCachedData(),
 	}
 
 	if err := g.Start(ctx); err != nil {
@@ -70,7 +59,7 @@ func NewNmeaMovementSensor(
 }
 
 // Start begins reading nmea messages from module and updates gps data.
-func (g *NMEAMovementSensor) Start(ctx context.Context) error {
+func (g *NMEAMovementSensor) Start(_ context.Context) error {
 	g.activeBackgroundWorkers.Add(1)
 	utils.PanicCapturingGo(func() {
 		defer g.activeBackgroundWorkers.Done()
@@ -90,11 +79,9 @@ func (g *NMEAMovementSensor) Start(ctx context.Context) error {
 				return
 			case message := <-messages:
 				// Update our struct's gps data in-place
-				g.mu.Lock()
-				err := g.data.ParseAndUpdate(message)
-				g.mu.Unlock()
+				err := g.cachedData.ParseAndUpdate(message)
 				if err != nil {
-					g.logger.CWarnf(ctx, "can't parse nmea sentence: %#v", err)
+					g.logger.CWarnf(g.cancelCtx, "can't parse nmea sentence: %#v", err)
 					g.logger.Debug("Check: GPS requires clear sky view." +
 						"Ensure the antenna is outdoors if signal is weak or unavailable indoors.")
 				}
@@ -102,50 +89,21 @@ func (g *NMEAMovementSensor) Start(ctx context.Context) error {
 		}
 	})
 
-	return g.err.Get()
+	return nil
 }
 
 // Position returns the position and altitide of the sensor, or an error.
 func (g *NMEAMovementSensor) Position(
 	ctx context.Context, extra map[string]interface{},
 ) (*geo.Point, float64, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-
-	lastPosition := g.lastPosition.GetLastPosition()
-	currentPosition := g.data.Location
-
-	if currentPosition == nil {
-		return lastPosition, 0, errNilLocation
-	}
-
-	// if current position is (0,0) we will return the last non-zero position
-	if movementsensor.IsZeroPosition(currentPosition) && !movementsensor.IsZeroPosition(lastPosition) {
-		return lastPosition, g.data.Alt, g.err.Get()
-	}
-
-	// updating the last known valid position if the current position is non-zero
-	if !movementsensor.IsZeroPosition(currentPosition) && !movementsensor.IsPositionNaN(currentPosition) {
-		g.lastPosition.SetLastPosition(currentPosition)
-	}
-
-	return currentPosition, g.data.Alt, g.err.Get()
+	return g.cachedData.Position(ctx, extra)
 }
 
 // Accuracy returns the accuracy map, hDOP, vDOP, Fixquality and compass heading error.
 func (g *NMEAMovementSensor) Accuracy(
 	ctx context.Context, extra map[string]interface{},
 ) (*movementsensor.Accuracy, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-	acc := movementsensor.Accuracy{
-		AccuracyMap:        map[string]float32{"hDOP": float32(g.data.HDOP), "vDOP": float32(g.data.VDOP)},
-		Hdop:               float32(g.data.HDOP),
-		Vdop:               float32(g.data.VDOP),
-		NmeaFix:            int32(g.data.FixQuality),
-		CompassDegreeError: float32(math.NaN()),
-	}
-	return &acc, g.err.Get()
+	return g.cachedData.Accuracy(ctx, extra)
 }
 
 // LinearVelocity returns the sensor's linear velocity. It requires having a compass heading, so we
@@ -154,74 +112,45 @@ func (g *NMEAMovementSensor) Accuracy(
 func (g *NMEAMovementSensor) LinearVelocity(
 	ctx context.Context, extra map[string]interface{},
 ) (r3.Vector, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-
-	if math.IsNaN(g.data.CompassHeading) {
-		return r3.Vector{}, g.err.Get()
-	}
-
-	headingInRadians := g.data.CompassHeading * (math.Pi / 180)
-	xVelocity := g.data.Speed * math.Sin(headingInRadians)
-	yVelocity := g.data.Speed * math.Cos(headingInRadians)
-
-	return r3.Vector{X: xVelocity, Y: yVelocity, Z: 0}, g.err.Get()
+	return g.cachedData.LinearVelocity(ctx, extra)
 }
 
 // LinearAcceleration returns the sensor's linear acceleration.
 func (g *NMEAMovementSensor) LinearAcceleration(
 	ctx context.Context, extra map[string]interface{},
 ) (r3.Vector, error) {
-	return r3.Vector{}, movementsensor.ErrMethodUnimplementedLinearAcceleration
+	return g.cachedData.LinearAcceleration(ctx, extra)
 }
 
 // AngularVelocity returns the sensor's angular velocity.
 func (g *NMEAMovementSensor) AngularVelocity(
 	ctx context.Context, extra map[string]interface{},
 ) (spatialmath.AngularVelocity, error) {
-	return spatialmath.AngularVelocity{}, movementsensor.ErrMethodUnimplementedAngularVelocity
+	return g.cachedData.AngularVelocity(ctx, extra)
 }
 
 // Orientation returns the sensor's orientation.
 func (g *NMEAMovementSensor) Orientation(
 	ctx context.Context, extra map[string]interface{},
 ) (spatialmath.Orientation, error) {
-	return nil, movementsensor.ErrMethodUnimplementedOrientation
+	return g.cachedData.Orientation(ctx, extra)
 }
 
 // CompassHeading returns the heading, from the range 0->360.
 func (g *NMEAMovementSensor) CompassHeading(
 	ctx context.Context, extra map[string]interface{},
 ) (float64, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-
-	lastHeading := g.lastCompassHeading.GetLastCompassHeading()
-	currentHeading := g.data.CompassHeading
-
-	if !math.IsNaN(lastHeading) && math.IsNaN(currentHeading) {
-		return lastHeading, nil
-	}
-
-	if !math.IsNaN(currentHeading) && currentHeading != lastHeading {
-		g.lastCompassHeading.SetLastCompassHeading(currentHeading)
-	}
-
-	return currentHeading, nil
+	return g.cachedData.CompassHeading(ctx, extra)
 }
 
 // ReadFix returns Fix quality of MovementSensor measurements.
 func (g *NMEAMovementSensor) ReadFix(ctx context.Context) (int, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-	return g.data.FixQuality, nil
+	return g.cachedData.ReadFix(ctx)
 }
 
 // ReadSatsInView returns the number of satellites in view.
 func (g *NMEAMovementSensor) ReadSatsInView(ctx context.Context) (int, error) {
-	g.mu.RLock()
-	defer g.mu.RUnlock()
-	return g.data.SatsInView, nil
+	return g.cachedData.ReadSatsInView(ctx)
 }
 
 // Readings will use return all of the MovementSensor Readings.
@@ -251,11 +180,7 @@ func (g *NMEAMovementSensor) Readings(
 func (g *NMEAMovementSensor) Properties(
 	ctx context.Context, extra map[string]interface{},
 ) (*movementsensor.Properties, error) {
-	return &movementsensor.Properties{
-		LinearVelocitySupported: true,
-		PositionSupported:       true,
-		CompassHeadingSupported: true,
-	}, nil
+	return g.cachedData.Properties(ctx, extra)
 }
 
 // Close shuts down the NMEAMovementSensor.
@@ -264,8 +189,6 @@ func (g *NMEAMovementSensor) Close(ctx context.Context) error {
 	g.cancelFunc()
 	g.activeBackgroundWorkers.Wait()
 
-	g.mu.Lock()
-	defer g.mu.Unlock()
 	if g.dev != nil {
 		if err := g.dev.Close(); err != nil {
 			return err
