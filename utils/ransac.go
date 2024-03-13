207a2cef1d16b1b462529d90f097acecbb313072
Matthew J Perez <matt@mperez.io>
2024-03-13 12:11:11 -0400

    update deprecated rand.Seed

--- utils/ransac.go
+++ utils/ransac.go
@@ -16,7 +16,7 @@ func SelectNIndicesWithoutReplacement(nSamples, nMax int) ([]int, error) {
 		a[i] = i
 	}
 
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
 	return a[:nSamples], nil
 }
