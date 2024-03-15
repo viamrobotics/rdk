5836113b7738ff260ab6c9a62e915a4ccfe0ebdb
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-03-15 16:51:57 -0400

    make new file

--- components/board/client.go
+++ components/board/client.go
@@ -10,11 +10,9 @@ import (
 	"github.com/pkg/errors"
 	commonpb "go.viam.com/api/common/v1"
 	pb "go.viam.com/api/component/board/v1"
-	"go.viam.com/utils"
 	"go.viam.com/utils/protoutils"
 	"go.viam.com/utils/rpc"
 	"google.golang.org/protobuf/types/known/durationpb"
-	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/rdk/logging"
 	rprotoutils "go.viam.com/rdk/protoutils"
@@ -40,18 +38,6 @@ type client struct {
 	interruptStreams []*interruptStream
 
 	mu sync.Mutex
-}
-
-type interruptStream struct {
-	*client
-	streamCancel  context.CancelFunc
-	streamRunning bool
-	streamReady   chan bool
-	streamMu      sync.Mutex
-
-	activeBackgroundWorkers sync.WaitGroup
-	cancelBackgroundWorkers context.CancelFunc
-	extra                   *structpb.Struct
 }
 
 type boardInfo struct {
@@ -296,103 +282,6 @@ func (c *client) StreamTicks(ctx context.Context, interrupts []string, ch chan T
 // RemoveCallbacks removes the callbacks from the given interrupts.
 func (c *client) RemoveCallbacks(ctx context.Context, interrupts []string, ch chan Tick) error {
 	return nil
-}
-
-func (s *interruptStream) startStream(ctx context.Context, interrupts []string, ch chan Tick) error {
-	s.streamMu.Lock()
-	defer s.streamMu.Unlock()
-
-	s.streamRunning = true
-	s.streamReady = make(chan bool)
-	s.activeBackgroundWorkers.Add(1)
-	ctx, cancel := context.WithCancel(ctx)
-	s.cancelBackgroundWorkers = cancel
-
-	streamCtx, cancel := context.WithCancel(ctx)
-	s.streamCancel = cancel
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-	}
-
-	req := &pb.StreamTicksRequest{
-		Name:     s.client.info.name,
-		PinNames: interrupts,
-	}
-
-	// This call won't return any errors it had until the client tries to receive.
-	//nolint:errcheck
-	stream, _ := s.client.client.StreamTicks(streamCtx, req)
-	_, err := stream.Recv()
-	if err != nil {
-		s.client.logger.CError(ctx, err)
-		return err
-	}
-
-	// Create a background go routine to recive from the server stream.
-	utils.PanicCapturingGo(func() {
-		defer s.activeBackgroundWorkers.Done()
-		s.recieveFromStream(streamCtx, stream, ch)
-	})
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-s.streamReady:
-		return nil
-	}
-}
-
-func (s *interruptStream) recieveFromStream(ctx context.Context, stream pb.BoardService_StreamTicksClient, ch chan Tick) {
-	defer func() {
-		s.streamMu.Lock()
-		defer s.streamMu.Unlock()
-		s.streamRunning = false
-	}()
-	if s.streamReady != nil {
-		close(s.streamReady)
-	}
-	s.streamReady = nil
-	defer s.closeStream(s.streamCancel)
-
-	// repeatly receive from the stream
-	for {
-		select {
-		case <-ctx.Done():
-			s.client.logger.Debug(ctx.Err())
-			return
-		default:
-		}
-		streamResp, err := stream.Recv()
-		if err != nil {
-			// only debug log the context canceled error
-			s.client.logger.Debug(err)
-			return
-		}
-		// If there is a response, send to the tick channel.
-		tick := Tick{
-			Name:             streamResp.PinName,
-			High:             streamResp.High,
-			TimestampNanosec: streamResp.Time,
-		}
-		ch <- tick
-	}
-}
-
-func (s *interruptStream) closeStream(cancel func()) {
-	cancel()
-
-	for i := range s.interruptStreams {
-		if s.client.interruptStreams[i] == s {
-			// To remove this item, we replace it with the last item in the list, then truncate the
-			// list by 1.
-			s.client.interruptStreams[i] = s.client.interruptStreams[len(s.client.interruptStreams)-1]
-			s.client.interruptStreams = s.client.interruptStreams[:len(s.client.interruptStreams)-1]
-			break
-		}
-	}
 }
 
 // gpioPinClient satisfies a gRPC based board.GPIOPin. Refer to the interface
