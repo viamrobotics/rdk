63082a0609605d1008ad76d7dfc93d000a1efaa2
Alexa Greenberg <alexa.greenberg@viam.com>
2024-03-20 15:37:40 -0400

    [RSDK-6936] Remove RTSP cam (#3697)

--- components/camera/register/register_cgo.go
+++ components/camera/register/register_cgo.go
@@ -8,7 +8,6 @@ import (
 	_ "go.viam.com/rdk/components/camera/align"
 	_ "go.viam.com/rdk/components/camera/ffmpeg"
 	_ "go.viam.com/rdk/components/camera/replaypcd"
-	_ "go.viam.com/rdk/components/camera/rtsp"
 	_ "go.viam.com/rdk/components/camera/ultrasonic"
 	_ "go.viam.com/rdk/components/camera/velodyne"
 	_ "go.viam.com/rdk/components/camera/videosource"
