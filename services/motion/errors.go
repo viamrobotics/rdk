35c2a52174658baa93313a322844bf50d4f5a19d
Ray Bjorkman <raymond@viam.com>
2024-04-03 16:52:13 -0400

    lint

--- services/motion/errors.go
+++ services/motion/errors.go
@@ -2,5 +2,5 @@ package motion
 
 import "github.com/pkg/errors"
 
-// ErrGoalWithinPlanDeviation is an error describing when planning fails becuase there is nothing to be done.
+// ErrGoalWithinPlanDeviation is an error describing when planning fails because there is nothing to be done.
 var ErrGoalWithinPlanDeviation = errors.New("no need to move, already within planDeviationMM")
