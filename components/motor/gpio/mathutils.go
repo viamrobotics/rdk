57d9755d41108ccf82cf7a31440c3f991d990fdf
randhid <rand@viam.com>
2024-04-11 16:30:21 -0400

    move general functions to mathutils file in motor

---
+++ components/motor/gpio/mathutils.go
@@ -0,0 +1,40 @@
+package gpio
+
+import (
+	"math"
+	"time"
+)
+
+func fixPowerPct(powerPct, max float64) float64 {
+	powerPct = math.Min(powerPct, max)
+	powerPct = math.Max(powerPct, -1*max)
+	return powerPct
+}
+
+func sign(x float64) float64 { // A quick helper function
+	if math.Signbit(x) {
+		return -1.0
+	}
+	return 1.0
+}
+
+// If revolutions is 0, the returned wait duration will be 0 representing that
+// the motor should run indefinitely.
+func goForMath(maxRPM, rpm, revolutions float64) (float64, time.Duration) {
+	// need to do this so time is reasonable
+	if rpm > maxRPM {
+		rpm = maxRPM
+	} else if rpm < -1*maxRPM {
+		rpm = -1 * maxRPM
+	}
+
+	if revolutions == 0 {
+		powerPct := rpm / maxRPM
+		return powerPct, 0
+	}
+
+	dir := rpm * revolutions / math.Abs(revolutions*rpm)
+	powerPct := math.Abs(rpm) / maxRPM * dir
+	waitDur := time.Duration(math.Abs(revolutions/rpm)*60*1000) * time.Millisecond
+	return powerPct, waitDur
+}
