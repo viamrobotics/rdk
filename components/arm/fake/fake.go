338d39918b252ac9c8af9591710c2f8193010c66
randhid <rand@viam.com>
2024-04-17 17:11:29 -0400

    Fix test

--- components/arm/fake/fake.go
+++ components/arm/fake/fake.go
@@ -111,7 +111,8 @@ func (a *Arm) Reconfigure(ctx context.Context, deps resource.Dependencies, conf 
 		return err
 	}
 
-	if len(model.DoF()) == 0 {
+	dof := len(model.DoF())
+	if dof == 0 {
 		a.logger.Info("fake arm built with zero degrees-of-freedom, nothing will show up on the Control tab " +
 			"you have either given a kinematics file that resulted in a zero degrees-of-freedom arm or omitted both" +
 			"the arm-model and model-path from attributes")
@@ -119,7 +120,7 @@ func (a *Arm) Reconfigure(ctx context.Context, deps resource.Dependencies, conf 
 
 	a.mu.Lock()
 	defer a.mu.Unlock()
-	a.joints = &pb.JointPositions{Values: make([]float64, len(model.DoF()))}
+	a.joints = &pb.JointPositions{Values: make([]float64, dof)}
 	a.model = model
 
 	return nil
