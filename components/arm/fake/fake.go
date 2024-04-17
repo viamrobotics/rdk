b91bec385cfa2a6c846ba83fc4c12ecebba1d77a
randhid <rand@viam.com>
2024-04-16 21:16:10 -0400

    hyphen

--- components/arm/fake/fake.go
+++ components/arm/fake/fake.go
@@ -114,7 +114,7 @@ func (a *Arm) Reconfigure(ctx context.Context, deps resource.Dependencies, conf 
 	if len(model.DoF()) == 0 {
 		a.logger.Info("fake arm built with zero degrees-of-freedom, nothing will show up on the Control tab " +
 			"you have either given a kinematics file that resulted in a zero degrees-of-freedom arm or omitted both" +
-			"the arm-model and model path from attributes")
+			"the arm-model and model-path from attributes")
 	}
 
 	a.mu.Lock()
