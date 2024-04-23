33846df169e51579bacb1cdde5df1ce4ab34bd66
Tahiya Salam <tahiyasalam@gmail.com>
2024-04-23 16:46:54 -0400

    Revert "Add defaults for capture and sync"
    This reverts commit 37e3a8674622abd644eda911d478db1e6c3b6427.

--- services/datamanager/builtin/builtin.go
+++ services/datamanager/builtin/builtin.go
@@ -146,8 +146,6 @@ func NewBuiltIn(
 		captureDir:                 viamCaptureDotDir,
 		collectors:                 make(map[resourceMethodMetadata]*collectorAndConfig),
 		syncIntervalMins:           0,
-		syncDisabled:               true,
-		captureDisabled:            true,
 		additionalSyncPaths:        []string{},
 		tags:                       []string{},
 		fileLastModifiedMillis:     defaultFileLastModifiedMillis,
@@ -614,7 +612,7 @@ func (svc *builtIn) sync() {
 	}
 }
 
-// nolint
+//nolint
 func getAllFilesToSync(dir string, lastModifiedMillis int) []string {
 	var filePaths []string
 	_ = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
