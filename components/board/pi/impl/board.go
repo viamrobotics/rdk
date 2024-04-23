f7f7f74f5fd1cab54564f7a8d5d25551ae6475e9
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-04-23 17:13:58 -0400

    fix pi

--- components/board/pi/impl/board.go
+++ components/board/pi/impl/board.go
@@ -788,9 +788,18 @@ func pigpioInterruptCallback(gpio, level int, rawTick uint32) {
 		}
 		// this should *not* block for long otherwise the lock
 		// will be held
-		err := Tick(instance.cancelCtx, i.(*BasicDigitalInterrupt), high, tick*1000)
-		if err != nil {
-			instance.logger.Error(err)
+		switch di := i.(type) {
+
+		case *BasicDigitalInterrupt:
+			err := Tick(instance.cancelCtx, di, high, tick*1000)
+			if err != nil {
+				instance.logger.Error(err)
+			}
+		case *ServoDigitalInterrupt:
+			err := ServoTick(instance.cancelCtx, di, high, tick*1000)
+			if err != nil {
+				instance.logger.Error(err)
+			}
 		}
 	}
 }
