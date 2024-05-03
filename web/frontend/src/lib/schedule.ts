0a3e8765c5b7cfd2bd48b5db461756c0fe469a20
Abe Winter <abe-winter@users.noreply.github.com>
2024-05-03 13:01:26 -0400

    document setAsyncInterval, add ignoreErrors param

--- web/frontend/src/lib/schedule.ts
+++ web/frontend/src/lib/schedule.ts
@@ -1,12 +1,26 @@
+/** provides setInterval-like functionality for an async function.
+ * @param interval - time to wait in between calls to `callback`. We wait for callback to complete, then wait interval, so callback calls won't overlap.
+ * @param ignoreErrors - if false (the default), the first error in `callback` will stop the interval.
+ * @returns a 'cancel' function you can call to stop the interval. It is safe to call this multiple times.
+ */
 export const setAsyncInterval = (
   callback: () => Promise<void>,
-  interval: number
-) => {
+  interval: number,
+  ignoreErrors: boolean = false,
+): () => void => {
   let cancelled = false;
   let timeoutId = -1;
 
   const refreshAndScheduleNext = async () => {
-    await callback();
+    try {
+      await callback();
+    } catch (err) {
+      if (ignoreErrors) {
+        console.warn("ignoring error in setAsyncInterval", err);
+      } else {
+        throw err;
+      }
+    }
 
     if (cancelled) {
       return;
