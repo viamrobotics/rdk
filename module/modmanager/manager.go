087a089c5ef974990cbac26ff61e983c7daea0f5
Abe Winter <abe-winter@users.noreply.github.com>
2024-05-07 19:31:34 -0400

    start process from new location for local tarball

--- module/modmanager/manager.go
+++ module/modmanager/manager.go
@@ -1013,7 +1013,7 @@ func (m *module) startProcess(
 
 	// We evaluate the Module's ExePath absolutely in the viam-server process so that
 	// setting the CWD does not cause issues with relative process names
-	absoluteExePath, err := filepath.Abs(m.cfg.ExePath)
+	absoluteExePath, err := m.cfg.EvaluateExePath()
 	if err != nil {
 		return err
 	}
