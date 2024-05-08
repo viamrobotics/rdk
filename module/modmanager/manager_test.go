b06439deb1a75a3afd450e0a6af0ebb3ff3e1f64
Dan Gottlieb <dgottlieb@users.noreply.github.com>
2024-05-08 13:26:45 -0400

    RSDK-7514: Have module connections cope with pion leaky close. (#3922)

--- module/modmanager/manager_test.go
+++ module/modmanager/manager_test.go
@@ -1068,6 +1068,7 @@ func TestRTPPassthrough(t *testing.T) {
 
 	err = mod.startProcess(ctx, parentAddr, nil, logger, viamHomeTemp)
 	test.That(t, err, test.ShouldBeNil)
+	defer mod.stopProcess()
 
 	err = mod.dial()
 	test.That(t, err, test.ShouldBeNil)
