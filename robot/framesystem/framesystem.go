20ba88902eabd912815d6d26c17de7d21bae9677
sky leilani <45158875+skyleilani@users.noreply.github.com>
2024-05-13 12:14:54 -0400

    revert whitespace due to test failure

--- robot/framesystem/framesystem.go
+++ robot/framesystem/framesystem.go
@@ -56,7 +56,6 @@ var InternalServiceName = resource.NewName(API, "builtin")
 //	myCurrentInputs, err := fsService.CurrentInputs(context.Background())
 //
 //	frameSystem, err := fsService.FrameSystem(context.Background(), nil)
-//
 type Service interface {
 	resource.Resource
 
