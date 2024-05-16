c2a40c462189f81f9e1636104c0876f32758d89e
JessamyT <jessamy.taylor@gmail.com>
2024-05-16 16:06:58 -0700

    lint

--- services/slam/slam.go
+++ services/slam/slam.go
@@ -100,21 +100,28 @@ func FromDependencies(deps resource.Dependencies, name string) (Service, error) 
 // helper methods instead of interface methods.
 //
 // Position example:
-//     // Get the current position of the specified source component
-//     // in the SLAM map as a Pose.
-//     pos, name, err := mySLAMService.Position(context.Background())
+//
+//	// Get the current position of the specified source component
+//	// in the SLAM map as a Pose.
+//	pos, name, err := mySLAMService.Position(context.Background())
+//
 // PointCloudMap example (using PointCloudMapFull helper method):
-//     // Get the point cloud map in standard PCD format.
-//     pcdMapBytes, err := PointCloudMapFull(
-//         context.Background(), mySLAMService, true)
+//
+//	// Get the point cloud map in standard PCD format.
+//	pcdMapBytes, err := PointCloudMapFull(
+//	    context.Background(), mySLAMService, true)
+//
 // InternalState example (using InternalStateFull helper method):
-//     // Get the internal state of the SLAM algorithm required
-//     // to continue mapping/localization.
-//     internalStateBytes, err := InternalStateFull(
-//         context.Background(), mySLAMService)
+//
+//	// Get the internal state of the SLAM algorithm required
+//	// to continue mapping/localization.
+//	internalStateBytes, err := InternalStateFull(
+//	    context.Background(), mySLAMService)
+//
 // Properties example:
-//     // Get the properties of your current SLAM session
-//     properties, err := mySLAMService.Properties(context.Background())
+//
+//	// Get the properties of your current SLAM session
+//	properties, err := mySLAMService.Properties(context.Background())
 type Service interface {
 	resource.Resource
 	Position(ctx context.Context) (spatialmath.Pose, string, error)
