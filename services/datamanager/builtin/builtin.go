460373c0bae7451da8f26097fdfd839ad6fd6831
Alexa Greenberg <alexa.greenberg@viam.com>
2024-05-24 12:15:56 -0400

    [DATA-2652] Do not attempt sync if offline

--- services/datamanager/builtin/builtin.go
+++ services/datamanager/builtin/builtin.go
@@ -4,6 +4,7 @@ package builtin
 import (
 	"context"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -637,7 +638,7 @@ func (svc *builtIn) uploadData(cancelCtx context.Context, intervalMins float64) 
 					}
 					svc.lock.Unlock()
 
-					if shouldSync {
+					if !isOffline() && shouldSync {
 						svc.sync()
 					}
 				} else {
@@ -646,6 +647,13 @@ func (svc *builtIn) uploadData(cancelCtx context.Context, intervalMins float64) 
 			}
 		}
 	})
+}
+
+func isOffline() bool {
+	timeout := 5 * time.Second
+	_, err := net.DialTimeout("tcp", "viam.com:443", timeout)
+	// If there's an error, the system is likely offline.
+	return err != nil
 }
 
 func (svc *builtIn) sync() {
@@ -663,7 +671,7 @@ func (svc *builtIn) sync() {
 	}
 }
 
-//nolint
+// nolint
 func getAllFilesToSync(dir string, lastModifiedMillis int) []string {
 	var filePaths []string
 	_ = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
