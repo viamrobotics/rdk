433468b30073cc857c960f036d33f5d2cf92717a
Alexa Greenberg <alexa.greenberg@viam.com>
2024-05-24 16:35:08 -0400

    [DATA-2652] Use app.viam.com for offline check instead of viam.com (#4005)

--- services/datamanager/builtin/builtin.go
+++ services/datamanager/builtin/builtin.go
@@ -669,7 +669,7 @@ func (svc *builtIn) uploadData(cancelCtx context.Context, intervalMins float64) 
 
 func isOffline() bool {
 	timeout := 5 * time.Second
-	_, err := net.DialTimeout("tcp", "viam.com:443", timeout)
+	_, err := net.DialTimeout("tcp", "app.viam.com:443", timeout)
 	// If there's an error, the system is likely offline.
 	return err != nil
 }
