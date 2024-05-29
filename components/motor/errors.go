054fb12c123192cda5ea3d876694e1470b731a21
martha-johnston <martha.johnston@viam.com>
2024-05-29 12:12:17 -0400

    add SetRPM to test and dummy motors

--- components/motor/errors.go
+++ components/motor/errors.go
@@ -31,6 +31,7 @@ func NewControlParametersUnimplementedError() error {
 	return errors.New("control parameters must be configured to setup a motor with controls")
 }
 
+// NewSetRPMUnsupportedError returns an error when a motor is required to support SetRPM.
 func NewSetRPMUnsupportedError(motorName string) error {
 	return errors.Errorf("motor named %s does not support SetRPM", motorName)
 }
