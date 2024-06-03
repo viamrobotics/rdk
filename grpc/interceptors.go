e19ebd24e0a76e89386e9fbd52835a2ed04c6ef5
Daniel Botros <93689852+danielbotros@users.noreply.github.com>
2024-06-03 13:29:47 -0400

    RSDK-7489 Client Request Timeout To Modules (#4029)
    Co-authored-by: Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
    Co-authored-by: Maxim Pertsov <maxim.pertsov@gmail.com>

--- grpc/interceptors.go
+++ grpc/interceptors.go
@@ -7,20 +7,39 @@ import (
 	"google.golang.org/grpc"
 )
 
-// defaultMethodTimeout is the default context timeout for all inbound gRPC
-// methods used when no deadline is set on the context.
-var defaultMethodTimeout = 10 * time.Minute
+// DefaultMethodTimeout is the default context timeout for all inbound gRPC
+// methods and all outbound gRPC methods to modules, only used when no
+// deadline is set on the context.
+var DefaultMethodTimeout = 10 * time.Minute
 
-// EnsureTimeoutUnaryInterceptor sets a default timeout on the context if one is
+// EnsureTimeoutUnaryServerInterceptor sets a default timeout on the context if one is
 // not already set. To be called as the first unary server interceptor.
-func EnsureTimeoutUnaryInterceptor(ctx context.Context, req interface{},
+func EnsureTimeoutUnaryServerInterceptor(ctx context.Context, req interface{},
 	info *grpc.UnaryServerInfo, handler grpc.UnaryHandler,
 ) (interface{}, error) {
 	if _, deadlineSet := ctx.Deadline(); !deadlineSet {
 		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeout(ctx, defaultMethodTimeout)
+		ctx, cancel = context.WithTimeout(ctx, DefaultMethodTimeout)
 		defer cancel()
 	}
 
 	return handler(ctx, req)
+}
+
+// EnsureTimeoutUnaryClientInterceptor sets a default timeout on the context if one is
+// not already set. To be called as the first unary client interceptor.
+func EnsureTimeoutUnaryClientInterceptor(
+	ctx context.Context,
+	method string, req, reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption,
+) error {
+	if _, deadlineSet := ctx.Deadline(); !deadlineSet {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, DefaultMethodTimeout)
+		defer cancel()
+	}
+
+	return invoker(ctx, method, req, reply, cc, opts...)
 }
