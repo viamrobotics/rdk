87b7defb82ea309316b6b1990747a5180ae6334b
Maxim Pertsov <maxim@viam.com>
2024-06-04 11:17:34 -0400

    revert workflow changes

--- etc/test.sh
+++ etc/test.sh
@@ -18,11 +18,8 @@ if [[ "$1" == "race" ]]; then
 	LOGFILE="--jsonfile json.log"
 fi
 
-# TODO: better filter?
-BENCH="-bench=BenchmarkConcurrentReconfiguration"
-
 # We run analyzetests on every run, pass or fail. We only run analyzecoverage when all tests passed.
-PION_LOG_WARN=webrtc,datachannel,sctp gotestsum --format standard-verbose $LOGFILE -- -tags=no_skip $BENCH $RACE $COVER $TEST_TARGET
+PION_LOG_WARN=webrtc,datachannel,sctp gotestsum --format standard-verbose $LOGFILE -- -tags=no_skip $RACE $COVER $TEST_TARGET
 SUCCESS=$?
 
 if [[ $RACE != "" ]]; then
