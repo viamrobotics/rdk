efa497b03aa7994cc740c4351bbf86fbf330f400
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-06-04 09:50:23 -0400

    arm nil checks

--- components/arm/client.go
+++ components/arm/client.go
@@ -78,6 +78,10 @@ func (c *client) MoveToPosition(ctx context.Context, pose spatialmath.Pose, extr
 	if err != nil {
 		return err
 	}
+
+	if pose == nil {
+		c.logger.Warnf("%s MoveToPosition: pose parameter is nil")
+	}
 	_, err = c.client.MoveToPosition(ctx, &pb.MoveToPositionRequest{
 		Name:  c.name,
 		To:    spatialmath.PoseToProtobuf(pose),
@@ -91,6 +95,11 @@ func (c *client) MoveToJointPositions(ctx context.Context, positions *pb.JointPo
 	if err != nil {
 		return err
 	}
+
+	if positions == nil {
+		c.logger.Warnf("%s MoveToJointPositions: position parameter is nil")
+	}
+
 	_, err = c.client.MoveToJointPositions(ctx, &pb.MoveToJointPositionsRequest{
 		Name:      c.name,
 		Positions: positions,
