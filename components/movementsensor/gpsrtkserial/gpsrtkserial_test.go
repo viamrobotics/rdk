10b4abdd4477a295d03085e125991083446fafe6
cloudui <eric.chen@viam.com>
2024-06-10 17:28:47 -0400

    Add position serial testing gpsrtk

--- components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
+++ components/movementsensor/gpsrtkserial/gpsrtkserial_test.go
@@ -2,10 +2,14 @@ package gpsrtkserial
 
 import (
 	"context"
+	"errors"
+	"math"
 	"testing"
 
 	"go.viam.com/test"
 
+	geo "github.com/kellydunn/golang-geo"
+	"go.viam.com/rdk/components/movementsensor"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/resource"
 )
@@ -84,4 +88,35 @@ func TestReconfigure(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, g.writePath, test.ShouldResemble, "/dev/ttyUSB1")
 	test.That(t, g.wbaud, test.ShouldEqual, 115200)
+}
+
+func TestPosition(t *testing.T) {
+
+	t.Run("position test with last error and no last position", func(t *testing.T) {
+		g := &rtkSerial{
+			err: movementsensor.NewLastError(1, 1),
+		}
+		g.err.Set(errors.New("last error test"))
+
+		point, flt, err := g.Position(context.Background(), nil)
+		test.That(t, movementsensor.IsPositionNaN(point), test.ShouldBeTrue)
+		test.That(t, math.IsNaN(flt), test.ShouldBeTrue)
+		test.That(t, err, test.ShouldBeError, "last error test")
+	})
+
+	t.Run("position test with last error and last position", func(t *testing.T) {
+		g := &rtkSerial{
+			err:          movementsensor.NewLastError(1, 1),
+			lastposition: movementsensor.NewLastPosition(),
+		}
+		g.lastposition.SetLastPosition(geo.NewPoint(42.1, 123))
+		g.err.Set(errors.New("last position"))
+		expectedPoint := geo.NewPoint(42.1, 123.)
+
+		point, flt, err := g.Position(context.Background(), nil)
+		test.That(t, movementsensor.ArePointsEqual(point, expectedPoint), test.ShouldBeTrue)
+		test.That(t, flt, test.ShouldEqual, 0.0)
+		test.That(t, err, test.ShouldBeNil)
+	})
+
 }
