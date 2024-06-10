2f808d0e63e47ee79f19117ecec2361df24e74eb
cloudui <eric.chen@viam.com>
2024-06-10 15:46:04 -0400

    Add linear velocity test to cachedData

--- components/movementsensor/gpsutils/cachedData_test.go
+++ components/movementsensor/gpsutils/cachedData_test.go
@@ -2,6 +2,7 @@ package gpsutils
 
 import (
 	"context"
+	"math"
 	"testing"
 
 	geo "github.com/kellydunn/golang-geo"
@@ -59,4 +60,50 @@ func TestReadingsSerial(t *testing.T) {
 	fix1, err := g.ReadFix(ctx)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, fix1, test.ShouldEqual, fix)
+}
+
+func TestLinearVelocity(t *testing.T) {
+	ctx := context.Background()
+	logger := logging.NewTestLogger(t)
+	g := NewCachedData(&mockDataReader{}, logger)
+
+	t.Run("no compass heading", func(t *testing.T) {
+		g.nmeaData = NmeaParser{
+			CompassHeading: math.NaN(),
+		}
+
+		speed, err := g.LinearVelocity(ctx, make(map[string]interface{}))
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, speed.X, test.ShouldEqual, 0.0)
+		test.That(t, speed.Y, test.ShouldEqual, 0.0)
+	})
+
+	t.Run("test sample velocity", func(t *testing.T) {
+		// Two quandrants of the compass
+		// Enough to verify quadrant signs are correct
+
+		g.nmeaData = NmeaParser{
+			Speed:          speed,
+			CompassHeading: 60.,
+		}
+
+		expectedX := speed * math.Sqrt(3) / 2
+		expectedY := speed / 2
+
+		speed1, err := g.LinearVelocity(ctx, make(map[string]interface{}))
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, speed1.X, test.ShouldAlmostEqual, expectedX)
+		test.That(t, speed1.Y, test.ShouldAlmostEqual, expectedY)
+
+		g.nmeaData.CompassHeading = 150.
+
+		expectedX = speed / 2.
+		expectedY = speed * -math.Sqrt(3) / 2.
+
+		speed2, err := g.LinearVelocity(ctx, make(map[string]interface{}))
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, speed2.X, test.ShouldAlmostEqual, expectedX)
+		test.That(t, speed2.Y, test.ShouldAlmostEqual, expectedY)
+	})
+
 }
