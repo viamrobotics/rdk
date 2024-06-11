5d354b8c50e31518737b1805a381a51feb5b8e79
danielbotros <daniel.botros@viam.com>
2024-06-11 15:48:09 -0400

    Refactor shutdown test to use actual robot client to sync error handling

--- web/server/entrypoint_test.go
+++ web/server/entrypoint_test.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"testing"
 	"time"
 
@@ -158,15 +159,14 @@ func TestShutdown(t *testing.T) {
 		}
 		test.That(t, success, test.ShouldBeTrue)
 
-		conn, err := robottestutils.Connect(port)
-		test.That(t, err, test.ShouldBeNil)
+		addr := "localhost:" + strconv.Itoa(port)
+		rc := robottestutils.NewRobotClient(t, testLogger, addr, time.Second)
 		defer func() {
-			test.That(t, conn.Close(), test.ShouldBeNil)
+			test.That(t, rc.Close(context.Background()), test.ShouldBeNil)
 		}()
-		rc := robotpb.NewRobotServiceClient(conn)
 
 		testLogger.Info("Issuing shutdown.")
-		_, err = rc.Shutdown(context.Background(), &robotpb.ShutdownRequest{})
+		err = rc.Shutdown(context.Background())
 
 		gtestutils.WaitForAssertionWithSleep(t, 50*time.Millisecond, 50, func(tb testing.TB) {
 			tb.Helper()
