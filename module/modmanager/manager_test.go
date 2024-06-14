addabdfe417ff3a40cf4c2fd52da4608c94e84a0
Nick Sanford <nick.sanford@viam.com>
2024-06-14 11:06:34 -0400

    RSDK-7667 - bump test SubscribeRTP timeout

--- module/modmanager/manager_test.go
+++ module/modmanager/manager_test.go
@@ -1052,7 +1052,7 @@ func TestRTPPassthrough(t *testing.T) {
 	noPassSource, ok := noPassCam.(rtppassthrough.Source)
 	test.That(t, ok, test.ShouldBeTrue)
 
-	subscribeRTPTimeout := time.Second * 30
+	subscribeRTPTimeout := time.Second * 60
 	subCtx, subCancelFn := context.WithTimeout(context.Background(), subscribeRTPTimeout)
 	sub, err := noPassSource.SubscribeRTP(subCtx, 512, func(pkts []*rtp.Packet) {
 		t.Log("should not happen")
@@ -1290,7 +1290,7 @@ func TestAddStreamMaxTrackErr(t *testing.T) {
 	test.That(t, len(first9Sources), test.ShouldEqual, 9)
 
 	greenLog(t, "the first 9's SubscribeRTP calls succeed")
-	subscribeRTPTimeout := time.Second * 30
+	subscribeRTPTimeout := time.Second * 60
 	for _, source := range first9Sources {
 		calledCtx, calledFn := context.WithCancel(context.Background())
 		// SubscribeRTP succeeds
