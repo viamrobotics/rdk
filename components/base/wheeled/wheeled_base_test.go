e1441f9fedb9285fc90422070f50f4c6fcd129dd
martha-johnston <martha.johnston@viam.com>
2024-06-17 17:24:48 -0400

    fix wheeled base test

--- components/base/wheeled/wheeled_base_test.go
+++ components/base/wheeled/wheeled_base_test.go
@@ -90,8 +90,8 @@ func TestWheelBaseMath(t *testing.T) {
 		test.That(t, len(geometries), test.ShouldBeZeroValue)
 		test.That(t, err, test.ShouldBeNil)
 
-		err = wb.SetVelocity(ctx, r3.Vector{X: 0, Y: 10, Z: 0}, r3.Vector{X: 0, Y: 0, Z: 10}, nil)
-		test.That(t, err.Error(), test.ShouldContainSubstring, "RPM that is nearly 0")
+		err = wb.SetVelocity(ctx, r3.Vector{X: 0, Y: 0, Z: 0}, r3.Vector{X: 0, Y: 0, Z: 0}, nil)
+		test.That(t, err, test.ShouldBeNil)
 
 		err = wb.SetVelocity(ctx, r3.Vector{X: 0, Y: 100, Z: 0}, r3.Vector{X: 0, Y: 0, Z: 100}, nil)
 		test.That(t, err, test.ShouldBeNil)
