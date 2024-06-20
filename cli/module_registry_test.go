6374e9b35be49135e99f5f869c7aa49bf80da42b
Pranav <pranav.puranam@gmail.com>
2024-06-20 12:28:12 -0400

    verify with expectedmeta.json

--- cli/module_registry_test.go
+++ cli/module_registry_test.go
@@ -12,23 +12,23 @@ import (
 
 func TestUpdateModelsAction(t *testing.T) {
 	_, filename, _, ok := runtime.Caller(0)
-    test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, ok, test.ShouldBeTrue)
 
 	dir := filepath.Dir(filename)
 	binaryPath := testutils.BuildTempModule(t, "./module/testmodule")
-	modulePath := dir + "/../module/testmodule/module.json"
 	metaPath := dir + "/../module/testmodule/meta.json"
+	expectedMetaPath := dir + "/../module/testmodule/expectedmeta.json"
 
 	flags := map[string]any{"binary": binaryPath, "module": metaPath}
 	cCtx, _, _, _ := setup(&inject.AppServiceClient{}, nil, nil, nil, flags, "")
-    test.That(t, UpdateModelsAction(cCtx), test.ShouldBeNil)
+	test.That(t, UpdateModelsAction(cCtx), test.ShouldBeNil)
 
-	// verify that models added to meta.json are equivalent to those defined in module.json
-	moduleModels, err := loadManifest(modulePath)
-    test.That(t, err, test.ShouldBeNil)
-
+	// verify that models added to meta.json are equivalent to those defined in expectedmeta.json
 	metaModels, err := loadManifest(metaPath)
-    test.That(t, err, test.ShouldBeNil)
+	test.That(t, err, test.ShouldBeNil)
 
-	test.That(t, sameModels(moduleModels.Models, metaModels.Models), test.ShouldBeTrue)
+	expectedMetaModels, err := loadManifest(expectedMetaPath)
+	test.That(t, err, test.ShouldBeNil)
+
+	test.That(t, sameModels(metaModels.Models, expectedMetaModels.Models), test.ShouldBeTrue)
 }
