41d5d7527fb592e34dd155c988fef01110695014
Pranav <pranav.puranam@gmail.com>
2024-06-25 13:22:12 -0400

    nolint on deprecated endpoints + bump api version

--- robot/client/client.go
+++ robot/client/client.go
@@ -886,6 +886,7 @@ func (rc *RobotClient) Status(ctx context.Context, resourceNames []resource.Name
 		names = append(names, rprotoutils.ResourceNameToProto(name))
 	}
 
+	//nolint:staticcheck
 	resp, err := rc.client.GetStatus(ctx, &pb.GetStatusRequest{ResourceNames: names})
 	if err != nil {
 		return nil, err
