e03e757c2fe0238e7228c688113f2de00ca2d7b7
Pranav <pranav.puranam@gmail.com>
2024-07-01 10:29:22 -0400

    return foreign resource when client registration doesnt exist

--- robot/client/client.go
+++ robot/client/client.go
@@ -559,10 +559,7 @@ func (rc *RobotClient) ResourceByName(name resource.Name) (resource.Resource, er
 func (rc *RobotClient) createClient(name resource.Name) (resource.Resource, error) {
 	apiInfo, ok := resource.LookupGenericAPIRegistration(name.API)
 	if !ok || apiInfo.RPCClient == nil {
-		if name.API.Type.Namespace != resource.APINamespaceRDK {
-			return grpc.NewForeignResource(name, &rc.conn), nil
-		}
-		return nil, ErrMissingClientRegistration
+		return grpc.NewForeignResource(name, &rc.conn), nil
 	}
 	return apiInfo.RPCClient(rc.backgroundCtx, &rc.conn, rc.remoteName, name, rc.Logger())
 }
