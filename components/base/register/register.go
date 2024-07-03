987cc1fc24d094178fe1d6f11f3cd7011c29bfdb
Abe Winter <abe-winter@users.noreply.github.com>
2024-07-03 16:06:26 -0400

    actually sensorcontrolled base was fine

--- components/base/register/register.go
+++ components/base/register/register.go
@@ -4,5 +4,6 @@ package register
 import (
 	// register bases.
 	_ "go.viam.com/rdk/components/base/fake"
+	_ "go.viam.com/rdk/components/base/sensorcontrolled"
 	_ "go.viam.com/rdk/components/base/wheeled"
 )
--- components/base/register/register_cgo.go
+++
@@ -1,9 +0,0 @@
-//go:build !no_cgo
-
-// Package register registers all relevant bases
-package register
-
-import (
-	// register bases.
-	_ "go.viam.com/rdk/components/base/sensorcontrolled"
-)
