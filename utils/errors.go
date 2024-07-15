066b87b70f06ef7a1ccd1e910e8a1e152876e7e7
Pranav <pranav.puranam@gmail.com>
2024-07-15 15:01:00 -0400

    remove FilterOutError

--- utils/errors.go
+++ utils/errors.go
@@ -3,11 +3,9 @@ package utils
 import (
 	"fmt"
 	"reflect"
-	"strings"
 	"time"
 
 	"github.com/pkg/errors"
-	"go.uber.org/multierr"
 
 	"go.viam.com/rdk/logging"
 )
@@ -69,36 +67,4 @@ func NewConfigValidationError(path string, err error) error {
 // copied from goutils.
 func NewConfigValidationFieldRequiredError(path, field string) error {
 	return NewConfigValidationError(path, errors.Errorf("%q is required", field))
-}
-
-// FilterOutError filters out an error based on the given target. For
-// example, if err was context.Canceled and so was the target, this
-// would return nil. Furthermore, if err was a multierr containing
-// a context.Canceled, it would also be filtered out from a new
-// multierr.
-// copied from goutils.
-func FilterOutError(err, target error) error {
-	if err == nil {
-		return nil
-	}
-	if target == nil {
-		return err
-	}
-	errs := multierr.Errors(err)
-	if len(errs) == 1 {
-		// multierr flattens errors so we can assume this
-		// is not a multierr
-		if errors.Is(err, target) || strings.Contains(err.Error(), target.Error()) {
-			return nil
-		}
-		return err
-	}
-	newErrs := make([]error, 0, len(errs))
-	for _, e := range errs {
-		if FilterOutError(e, target) == nil {
-			continue
-		}
-		newErrs = append(newErrs, e)
-	}
-	return multierr.Combine(newErrs...)
 }
