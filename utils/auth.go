2ba29c2485c8d9fc7613495c88c6b4d237ee61af
Pranav <pranav.puranam@gmail.com>
2024-07-15 15:18:13 -0400

    copy over credential code, refer to goutils version

--- utils/auth.go
+++ utils/auth.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	goutils "go.viam.com/utils/rpc"
+)
+
 // CredentialsType signifies a means of representing a credential. For example,
 // an API key.
 type CredentialsType string
@@ -10,6 +14,14 @@ const (
 
 	// CredentialsTypeRobotLocationSecret is for credentials used against the cloud managing this robot's location.
 	CredentialsTypeRobotLocationSecret = "robot-location-secret"
+
+	// CredentialsTypeAPIKey is intended for by external users, human and computer.
+	CredentialsTypeAPIKey = goutils.CredentialsTypeAPIKey
+
+	// CredentialsTypeExternal is for credentials that are to be produced by some
+	// external authentication endpoint (see ExternalAuthService#AuthenticateTo) intended
+	// for another, different consumer at a different endpoint.
+	CredentialsTypeExternal = goutils.CredentialsTypeExternal
 )
 
 // Credentials packages up both a type of credential along with its payload which
@@ -17,4 +29,11 @@ const (
 type Credentials struct {
 	Type    CredentialsType `json:"type"`
 	Payload string          `json:"payload"`
+}
+
+// WithEntityCredentials returns a DialOption which sets the entity credentials
+// to use for authenticating the request. This is mutually exclusive with
+// WithCredentials.
+func WithEntityCredentials(entity string, creds goutils.Credentials) goutils.DialOption {
+	return goutils.WithEntityCredentials(entity, creds)
 }
