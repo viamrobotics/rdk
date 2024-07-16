aaef15207164aaaf57af905b394813b2f15f3fbe
Pranav <pranav.puranam@gmail.com>
2024-07-16 15:37:09 -0400

    make logger manager functions exported

--- logging/logger_manager.go
+++ logging/logger_manager.go
@@ -41,3 +41,28 @@ func (lr *loggerRegistry) updateLoggerLevel(name string, level Level) error {
 	logger.SetLevel(level)
 	return nil
 }
+
+// Exported Functions specifically for use on global logger manager
+func RegisterLogger(name string, logger Logger) {
+	loggerManager.mu.Lock()
+	defer loggerManager.mu.Unlock()
+	loggerManager.loggers[name] = logger
+}
+
+func LoggerNamed(name string) (logger Logger, ok bool) {
+	loggerManager.mu.RLock()
+	defer loggerManager.mu.RUnlock()
+	logger, ok = loggerManager.loggers[name]
+	return logger, ok
+}
+
+func UpdateLoggerLevel(name string, level Level) error {
+	loggerManager.mu.RLock()
+	defer loggerManager.mu.RUnlock()
+	logger, ok := loggerManager.loggers[name]
+	if !ok {
+		return fmt.Errorf("logger named %s not recognized", name)
+	}
+	logger.SetLevel(level)
+	return nil
+}
