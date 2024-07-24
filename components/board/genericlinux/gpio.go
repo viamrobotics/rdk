0506b85e3a36de440ff0957c22b8ed89e5879834
Alan Davidson <alan@viam.com>
2024-07-24 13:21:41 -0400

    undo grasping at straws

--- components/board/genericlinux/gpio.go
+++ components/board/genericlinux/gpio.go
@@ -322,9 +322,11 @@ func (pin *gpioPin) softwarePwmLoop(ctx context.Context) {
 		// it, and we might be running on a board with a small enough CPU that pointer assignment
 		// is not atomic. Lock the mutex when getting a copy of the channel, so we don't
 		// accidentally get half a pointer to an old one and half a pointer to a new one.
-		pin.mu.Lock()
-		startSoftwarePWMChan := pin.startSoftwarePWMChan
-		pin.mu.Unlock()
+		startSoftwarePWMChan := func() chan any {
+			pin.mu.Lock()
+			defer pin.mu.Unlock()
+			return pin.startSoftwarePWMChan
+		}()
 
 		select {
 		case <-ctx.Done():
