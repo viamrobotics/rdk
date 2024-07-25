d56f80e462834477053d2f681d46214ba898e524
Maxim Pertsov <maxim@viam.com>
2024-07-25 10:47:59 -0400

    remove unnecessary test change

--- resource/graph_node_test.go
+++ resource/graph_node_test.go
@@ -307,15 +307,6 @@ type anotherResource struct {
 }
 
 // Close calls the injected Close or the real version.
-func (a *anotherResource) Name() resource.Name {
-	return resource.Name{
-		Name:   "anotherOne",
-		API:    resource.APINamespaceRDK.WithComponentType("mock"),
-		Remote: "",
-	}
-}
-
-// Close calls the injected Close or the real version.
 func (a *anotherResource) Close(ctx context.Context) error {
 	if a.CloseFunc == nil {
 		return errors.New("oops")
