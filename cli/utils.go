fe81be501493035315ae8013c1d7d4a7daf86e3b
Abe Winter <abe-winter@users.noreply.github.com>
2024-07-26 22:59:57 -0400

    replaceInTarGz

--- cli/utils.go
+++ cli/utils.go
@@ -1,6 +1,14 @@
 package cli
 
-import "path/filepath"
+import (
+	"archive/tar"
+	"compress/gzip"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
 
 // samePath returns true if abs(path1) and abs(path2) are the same.
 func samePath(path1, path2 string) (bool, error) {
@@ -28,4 +36,87 @@ func getMapString(m map[string]any, key string) string {
 		}
 	}
 	return ""
+}
+
+// replaceInTarGz adds entries to a .tar.gz file by the awkward method of copying
+// its contents to a new file.
+func replaceInTarGz(path string, newEntries map[string][]byte) error {
+	tmpPath := path + ".tmp"
+	// nested function so the Close() calls finish before we rename.
+	err := func() error {
+		// open reader
+		sourceFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer sourceFile.Close()
+		gzRead, err := gzip.NewReader(sourceFile)
+		if err != nil {
+			return err
+		}
+		defer gzRead.Close()
+		tarRead := tar.NewReader(gzRead)
+
+		// open writer
+		destFile, err := os.Create(tmpPath)
+		if err != nil {
+			return err
+		}
+		defer destFile.Close()
+		gzw := gzip.NewWriter(destFile)
+		defer gzw.Close()
+		tarWrite := tar.NewWriter(gzw)
+		defer tarWrite.Close()
+
+		// copy
+		consumed := make(map[string]bool)
+		for {
+			head, err := tarRead.Next()
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			if entry, ok := newEntries[head.Name]; ok {
+				head.Size = int64(len(entry))
+				if err := tarWrite.WriteHeader(head); err != nil {
+					return err
+				}
+				if _, err := tarWrite.Write(entry); err != nil {
+					return err
+				}
+				consumed[head.Name] = true
+			} else {
+				if err := tarWrite.WriteHeader(head); err != nil {
+					return err
+				}
+				if _, err := io.Copy(tarWrite, tarRead); err != nil {
+					return err
+				}
+			}
+		}
+		for name, entry := range newEntries {
+			if consumed[name] {
+				continue
+			}
+			if err := tarWrite.WriteHeader(&tar.Header{
+				Typeflag: tar.TypeReg,
+				Name:     name,
+				Size:     int64(len(entry)),
+				ModTime:  time.Now(),
+			}); err != nil {
+				return err
+			}
+			if _, err := tarWrite.Write(entry); err != nil {
+				return err
+			}
+		}
+		return nil
+	}()
+	if err != nil {
+		return err
+	}
+	return os.Rename(tmpPath, path)
 }
--- cli/utils_test.go
+++ cli/utils_test.go
@@ -1,6 +1,12 @@
 package cli
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"errors"
+	"io"
+	"os"
+	"os/exec"
 	"testing"
 
 	"go.viam.com/test"
@@ -21,4 +27,66 @@ func TestGetMapString(t *testing.T) {
 	test.That(t, getMapString(m, "x"), test.ShouldEqual, "x")
 	test.That(t, getMapString(m, "y"), test.ShouldEqual, "")
 	test.That(t, getMapString(m, "z"), test.ShouldEqual, "")
+}
+
+func chdir(t *testing.T, path string) {
+	wd, err := os.Getwd()
+	test.That(t, err, test.ShouldBeNil)
+	err = os.Chdir(path)
+	test.That(t, err, test.ShouldBeNil)
+	t.Cleanup(func() { os.Chdir(wd) })
+}
+
+// helper; starts and waits for an exec.Cmd.
+func runCommand(t *testing.T, cmd *exec.Cmd) {
+	t.Helper()
+	err := errors.Join(cmd.Start(), cmd.Wait())
+	test.That(t, err, test.ShouldBeNil)
+}
+
+// helper; returns map of {name: length} for tarball contents.
+func tarContents(t *testing.T, path string) map[string]int64 {
+	r, err := os.Open(path)
+	test.That(t, err, test.ShouldBeNil)
+	gz, err := gzip.NewReader(r)
+	test.That(t, err, test.ShouldBeNil)
+	tr := tar.NewReader(gz)
+	ret := make(map[string]int64)
+	for {
+		head, err := tr.Next()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		test.That(t, err, test.ShouldBeNil)
+		ret[head.Name] = head.Size
+	}
+	return ret
+}
+
+func TestReplaceInTarGz(t *testing.T) {
+	tmp := t.TempDir()
+	chdir(t, tmp)
+
+	// setup
+	runCommand(t, exec.Command("touch", "hello.txt"))
+	runCommand(t, exec.Command("touch", "replaceme.txt"))
+	// add a symlink to make sure this doesn't choke on symlinks
+	runCommand(t, exec.Command("ln", "-s", "hello.txt", "linkhello"))
+	runCommand(t, exec.Command("tar", "czf", "archive.tar.gz", "hello.txt", "replaceme.txt", "linkhello"))
+
+	err := replaceInTarGz("archive.tar.gz", map[string][]byte{
+		// replace an entry
+		"replaceme.txt": []byte("hi"),
+		// add an entry
+		"new.txt": []byte("hey"),
+	})
+	test.That(t, err, test.ShouldBeNil)
+
+	// verify
+	test.That(t, tarContents(t, "archive.tar.gz"), test.ShouldResemble, map[string]int64{
+		"hello.txt":     0,
+		"linkhello":     0,
+		"replaceme.txt": 2,
+		"new.txt":       3,
+	})
 }
