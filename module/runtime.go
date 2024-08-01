3b0c458677da3559b25f7d6e2c28ab07dd824126
Alan Davidson <alan@viam.com>
2024-08-01 19:13:10 -0400

    make a standardized ModularMain function

---
+++ module/runtime.go
@@ -0,0 +1,42 @@
+package module
+
+import (
+	"context"
+
+	"go.viam.com/utils"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+)
+
+// APIModel is a glorified (resource.API, resource.Model) pair. Pass as many of these as you want
+// to ModularMain, to have this module support those models.
+type APIModel = resource.APIModel
+
+// ModularMain can be called as the main function from a module. It will start up a module with all
+// the provided APIModels added to it.
+func ModularMain(loggerName string, models ...resource.APIModel) {
+	mainWithArgs := func(ctx context.Context, args []string, logger logging.Logger) error {
+		mod, err := NewModuleFromArgs(ctx, logger)
+		if err != nil {
+			return err
+		}
+
+		for _, apiModel := range models {
+			if err = mod.AddModelFromRegistry(ctx, apiModel.API, apiModel.Model); err != nil {
+				return err
+			}
+		}
+
+		err = mod.Start(ctx)
+		defer mod.Close(ctx)
+		if err != nil {
+			return err
+		}
+
+		<-ctx.Done()
+		return nil
+	}
+
+	utils.ContextualMain(mainWithArgs, NewLoggerFromArgs(loggerName))
+}
