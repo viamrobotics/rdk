19a4cad2c1d301bd74aaf5b8038a9d786bc5504d
Maxim Pertsov <maxim@viam.com>
2024-08-06 19:37:37 -0400

    get resource names with resource statuses instead
    removed mutex usage to prevent deadlock

--- robot/impl/local_robot.go
+++ robot/impl/local_robot.go
@@ -1398,9 +1398,9 @@ func (r *localRobot) Shutdown(ctx context.Context) error {
 func (r *localRobot) MachineStatus(ctx context.Context) (robot.MachineStatus, error) {
 	var result robot.MachineStatus
 
-	r.manager.resourceGraphLock.Lock()
-	result.Resources = append(result.Resources, r.manager.resources.Status()...)
-	r.manager.resourceGraphLock.Unlock()
+	// r.manager.resourceGraphLock.Lock()
+	result.Resources = r.manager.ResourceStatuses()
+	// r.manager.resourceGraphLock.Unlock()
 
 	r.configRevisionMu.RLock()
 	result.Config = r.configRevision
