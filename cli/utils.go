f52fa0cfe9cdfa109ce8ea84278c0970f9329564
Abe Winter <abe-winter@users.noreply.github.com>
2024-08-07 16:06:34 -0400

    ParseFileType passing

--- cli/utils.go
+++ cli/utils.go
@@ -1,6 +1,10 @@
 package cli
 
-import "path/filepath"
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+)
 
 // samePath returns true if abs(path1) and abs(path2) are the same.
 func samePath(path1, path2 string) (bool, error) {
@@ -28,4 +32,27 @@ func getMapString(m map[string]any, key string) string {
 		}
 	}
 	return ""
+}
+
+var fileTypeRegex = regexp.MustCompile(`^((ELF) [^,]+, ([^,]+),|(Mach-O) [^\ ]+ ([^\ ]+) executable)`)
+
+// ParseFileType parses output from the `file` command. Returns a platform string like "linux/amd64".
+// Empty string means failed to parse.
+func ParseFileType(raw string) string {
+	groups := fileTypeRegex.FindStringSubmatch(raw)
+	if len(groups) == 0 {
+		return ""
+	}
+	var rawOs, rawArch string
+	if groups[2] != "" {
+		rawOs = groups[2]
+		rawArch = groups[3]
+	} else {
+		rawOs = groups[4]
+		rawArch = groups[5]
+	}
+	osLookup := map[string]string{"ELF": "linux", "Mach-O": "darwin"}
+	// todo: double-check 'arm' is right for arm32
+	archLookup := map[string]string{"x86-64": "amd64", "ARM aarch64": "arm64", "ARM": "arm", "x86_64": "amd64", "arm64": "arm64"}
+	return fmt.Sprintf("%s/%s", osLookup[rawOs], archLookup[rawArch])
 }
