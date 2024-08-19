5529876ba2ae26b0bc01543ce4194bee91718bbb
Dan Gottlieb <dan.gottlieb@viam.com>
2024-08-18 23:45:40 -0400

    a single lint error

--- robot/web/stream/state/state.go
+++ robot/web/stream/state/state.go
@@ -295,7 +295,7 @@ func (state *StreamState) streamH264Passthrough() error {
 	// Get the camera and see if it implements the rtp passthrough API of SubscribeRTP + Unsubscribe
 	rtpPassthroughSource, ok := cam.(rtppassthrough.Source)
 	if !ok {
-		return fmt.Errorf("Stream does not support RTP passthrough")
+		return fmt.Error("Stream does not support RTP passthrough")
 	}
 
 	var count atomic.Uint64
