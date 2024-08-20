e0fe63f1cf41f6824f790ca863eec9c6376cf755
Alan Davidson <alan@viam.com>
2024-08-20 15:25:21 -0400

    use ModularMain in the example modules (#4293)
    All examples compile, and if you edit the JSON files to use absolute paths, all the demos still work (and if you don't use absolute paths, they fail on the `main` branch, too!). 
    
    The intention here is that I've made a `ModularMain()` function (see https://github.com/viamrobotics/rdk/pull/4256) to avoid copying and pasting so much boilerplate when running modules, and we should encourage users to use it instead of copying and pasting boilerplate, too.

--- examples/customresources/demos/multiplemodules/gizmomodule/gizmomodule.go
+++ examples/customresources/demos/multiplemodules/gizmomodule/gizmomodule.go
@@ -2,41 +2,14 @@
 package main
 
 import (
-	"context"
-
-	"go.viam.com/utils"
-
 	"go.viam.com/rdk/examples/customresources/apis/gizmoapi"
 	"go.viam.com/rdk/examples/customresources/models/mygizmosummer"
-	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/resource"
 )
 
 func main() {
-	// NewLoggerFromArgs will create a logging.Logger at "DebugLevel" if
-	// "--log-level=debug" is the third argument in os.Args and at "InfoLevel"
-	// otherwise.
-	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("gizmomodule"))
-}
-
-func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
-	myMod, err := module.NewModuleFromArgs(ctx, logger)
-	if err != nil {
-		return err
-	}
-
-	// Models and APIs add helpers to the registry during their init().
-	// They can then be added to the module here.
-	err = myMod.AddModelFromRegistry(ctx, gizmoapi.API, mygizmosummer.Model)
-	if err != nil {
-		return err
-	}
-
-	err = myMod.Start(ctx)
-	defer myMod.Close(ctx)
-	if err != nil {
-		return err
-	}
-	<-ctx.Done()
-	return nil
+	// ModularMain will create a logger named "gizmomodule", and will stand up a module which will
+	// host our gizmo.
+	module.ModularMain("gizmomodule", resource.APIModel{gizmoapi.API, mygizmosummer.Model})
 }
