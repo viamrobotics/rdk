79b6fd0c059b5a24157a25ea26bca7a643244f2a
Benji Rewis <benji.rewis@viam.com>
2024-09-17 12:41:46 -0400

    remove extraneous logger param

--- robot/web/web_c.go
+++ robot/web/web_c.go
@@ -334,7 +334,7 @@ func (svc *webService) closeStreamServer() {
 	}
 }
 
-func (svc *webService) initStreamServer(ctx context.Context, options *weboptions.Options, logger logging.Logger) error {
+func (svc *webService) initStreamServer(ctx context.Context, options *weboptions.Options) error {
 	var err error
 	svc.streamServer, err = svc.makeStreamServer(ctx)
 	if err != nil {
