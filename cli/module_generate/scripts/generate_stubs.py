1a5ad6357ef46138a981f86aa05af7deacff2a0e
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-10-03 13:03:16 -0400

    import viam

--- cli/module_generate/scripts/generate_stubs.py
+++ cli/module_generate/scripts/generate_stubs.py
@@ -23,7 +23,7 @@ def main(
     resource = getattr(module, resource_name)
     methods = inspect.getmembers(resource, predicate=inspect.isfunction)
 
-    imports = []
+    imports = ["import viam"]
     abstract_methods = []
     for name, method in methods:
         if getattr(method, "__isabstractmethod__", False):
