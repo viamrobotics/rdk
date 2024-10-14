0fb5c442723173263c9b51e900ab2f0e266f137f
Maxim Pertsov <maxim@viam.com>
2024-10-14 15:48:29 -0400

    TODO: FirstRun before removing

--- robot/impl/resource_manager.go
+++ robot/impl/resource_manager.go
@@ -1274,6 +1274,7 @@ func (manager *resourceManager) markRemoved(
 
 	var resourcesToMark []resource.Name
 	for _, conf := range conf.Modules {
+		// TODO: call FirstRun for new modules before removing.
 		orphanedResourceNames, err := manager.moduleManager.Remove(conf.Name)
 		if err != nil {
 			manager.logger.CErrorw(ctx, "error removing module", "module", conf.Name, "error", err)
