9bf866b47108b2e830366b65eda2294c8e8b8fc0
Naveed Jooma <naveed@viam.com>
2024-10-15 17:37:02 -0400

    Add instaleld to gitignored

--- cli/module_generate.go
+++ cli/module_generate.go
@@ -282,7 +282,7 @@ func promptUser() (*moduleInputs, error) {
 					if s == "" {
 						return errors.New("model name must not be empty")
 					}
-					match, err := regexp.MatchString("^[a-z0-9]+(?:[_-][a-z0-9]+)*$", s)
+					match, err := regexp.MatchString("^[a-zA-Z0-9]+(?:[_-][a-zA-Z0-9]+)*$", s)
 					if !match || err != nil {
 						return errors.New("module names can only contain alphanumeric characters, dashes, and underscores")
 					}
