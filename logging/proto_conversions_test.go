53acac929b538b9cf490ef58fa5d09e70ed4aa6b
Dan Gottlieb <dan.gottlieb@viam.com>
2024-10-22 15:17:22 -0400

    RSDK-9097: Fix zapcore.Field <-> Proto roundtrip for error types.

--- logging/proto_conversions_test.go
+++ logging/proto_conversions_test.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"errors"
 	"math"
 	"testing"
 	"time"
@@ -142,4 +143,68 @@ func TestFieldConversion(t *testing.T) {
 			test.That(t, val, test.ShouldResemble, tc.expectedVal)
 		})
 	}
+}
+
+type writeParams struct {
+	Entry  zapcore.Entry
+	Fields []zapcore.Field
+}
+
+type replayAppender struct {
+	logs []writeParams
+}
+
+func (app *replayAppender) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	app.logs = append(app.logs, writeParams{entry, fields})
+	return nil
+}
+
+func (app *replayAppender) Sync() error {
+	return nil
+}
+
+func TestErrorTypeRoundtrip(t *testing.T) {
+	testLogger := NewTestLogger(t)
+	_ = testLogger
+
+	// Create a logger with a special appender to capture the exact `zapcore.Field` objects being
+	// passed around.
+	replayLogger := NewBlankLogger("proto")
+
+	appender := &replayAppender{}
+	replayLogger.AddAppender(appender)
+
+	// Log a `w` line with an error value.
+	replayLogger.Infow("log message", "err", errors.New("error message"))
+	testLogger.Infof("LogEntry: %#v", appender.logs)
+
+	test.That(t, len(appender.logs), test.ShouldEqual, 1)
+	test.That(t, len(appender.logs[0].Fields), test.ShouldEqual, 1)
+
+	// Grab the `err: error` field off the replay appender.
+	field := appender.logs[0].Fields[0]
+	testLogger.Infof("Field: %#v", field)
+
+	// Demonstrate that the encoder is happy with this raw field.
+	jsonEncoder := zapcore.NewJSONEncoder(zapcore.EncoderConfig{SkipLineEnding: true})
+	_, err := jsonEncoder.EncodeEntry(zapcore.Entry{}, []zapcore.Field{field})
+	test.That(t, err, test.ShouldBeNil)
+
+	// Serialize the field to proto.
+	proto, err := FieldToProto(field)
+	test.That(t, err, test.ShouldBeNil)
+
+	// Deserialize the proto back into a zapcore.Field.
+	testLogger.Infof("Proto: %#v", proto)
+	roundTrip, err := FieldFromProto(proto)
+	test.That(t, err, test.ShouldBeNil)
+	testLogger.Infof("Roundtrip: %#v", roundTrip)
+
+	// Prior to RSDK-9097, this encoder step would fail. As we've created a zapcore.Field.Type ==
+	// error. But with an empty zapcore.Field.Interface.
+	_, err = jsonEncoder.EncodeEntry(zapcore.Entry{}, []zapcore.Field{roundTrip})
+	test.That(t, err, test.ShouldBeNil)
+
+	// TODO: Do better by preserving the error type
+	// test.That(t, roundTrip.Type, test.ShouldEqual, zapcore.ErrorType)
 }
