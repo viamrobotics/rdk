61b7428caefa6fd0cc5ab73642f6407ba8ebe93b
Julie Krasnick <julie.krasnick@viam.com>
2024-10-24 13:45:02 -0400

    changed default value

--- cli/module_generate.go
+++ cli/module_generate.go
@@ -668,7 +668,7 @@ func getLatestSDKTag(c *cli.Context, language string) (string, error) {
 	case golang:
 		repo = "rdk"
 	default:
-		repo = "viam-python-sdk"
+		return "", errors.Errorf("Unexpected language was selected. Cannot produce template.")
 	}
 	debugf(c.App.Writer, c.Bool(debugFlag), "Getting the latest release tag for %s", repo)
 	url := fmt.Sprintf("https://api.github.com/repos/viamrobotics/%s/releases", repo)
