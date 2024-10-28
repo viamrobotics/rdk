dc1fabb05d5b6f290e93e54c0f90d7ae267e0b7b
Dan Gottlieb <dan.gottlieb@viam.com>
2024-10-28 09:33:19 -0400

    Add parser code.

---
+++ ftdc/cmd/parser.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"go.viam.com/rdk/ftdc"
+)
+
+type gnuplotWriter struct {
+	// metricFiles contain the actual data points to be graphed. A "top level" gnuplot will
+	// reference them.
+	metricFiles map[string]*os.File
+
+	tempdir string
+}
+
+func writeln(toWrite io.Writer, str string) {
+	_, err := toWrite.Write([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = toWrite.Write([]byte{byte('\n')})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writelnf(toWrite io.Writer, formatStr string, args ...any) {
+	writeln(toWrite, fmt.Sprintf(formatStr, args...))
+}
+
+func newGnuPlotWriter() *gnuplotWriter {
+	tempdir, err := os.MkdirTemp("", "ftdc_parser")
+	if err != nil {
+		panic(err)
+	}
+
+	return &gnuplotWriter{
+		metricFiles: make(map[string]*os.File),
+		tempdir:     tempdir,
+	}
+}
+
+func (gpw *gnuplotWriter) getDatafile(metricName string) io.Writer {
+	if datafile, created := gpw.metricFiles[metricName]; created {
+		return datafile
+	}
+
+	datafile, err := os.CreateTemp(gpw.tempdir, "")
+	if err != nil {
+		panic(err)
+	}
+	gpw.metricFiles[metricName] = datafile
+
+	return datafile
+}
+
+func (gpw *gnuplotWriter) addPoint(timeInt int64, metricName string, metricValue float32) {
+	writelnf(gpw.getDatafile(metricName), "%v %.2f", timeInt, metricValue)
+}
+
+func (gpw *gnuplotWriter) addFlatDatum(datum ftdc.FlatDatum) {
+	for _, reading := range datum.Readings {
+		gpw.addPoint(datum.Time, reading.MetricName, reading.Value)
+	}
+}
+
+func (gpw *gnuplotWriter) RenderAndClose() {
+	gnuFile, err := os.CreateTemp(gpw.tempdir, "main")
+	if err != nil {
+		panic(err)
+	}
+	defer gnuFile.Close()
+
+	fmt.Println("GNUPlot File:", gnuFile.Name())
+	writelnf(gnuFile, "set term png size %d, %d", 1000, 200*len(gpw.metricFiles))
+	writeln(gnuFile, "set output 'plot.png'")
+	writelnf(gnuFile, "set multiplot layout %v,1 margins 0.05,0.9, 0.05,0.9 spacing screen 0, char 5", len(gpw.metricFiles))
+	writeln(gnuFile, "set timefmt '%s'")
+	writeln(gnuFile, "set format x '%H:%M:%S'")
+	writeln(gnuFile, "set xlabel 'Time'")
+	writeln(gnuFile, "set xdata time")
+	writeln(gnuFile, "set yrange [0:*]")
+
+	for metricName, file := range gpw.metricFiles {
+		writelnf(gnuFile, "plot '%v' using 1:2 with lines linestyle 7 lw 4 title '%v'", file.Name(), strings.ReplaceAll(metricName, "_", "\\_"))
+		file.Close()
+	}
+}
+
+func main() {
+	fmt.Println("Args:", os.Args)
+	ftdcFile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := ftdc.Parse(ftdcFile)
+	if err != nil {
+		panic(err)
+	}
+
+	gpw := newGnuPlotWriter()
+	for _, flatDatum := range data {
+		gpw.addFlatDatum(flatDatum)
+	}
+
+	gpw.RenderAndClose()
+}
