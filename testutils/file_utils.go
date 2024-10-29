b59505ef1185876557ee5add04b611d660d41359
Maxim Pertsov <maxim@viam.com>
2024-10-29 10:00:44 -0400

    debug: check what files exist

--- testutils/file_utils.go
+++ testutils/file_utils.go
@@ -67,7 +67,24 @@ func BuildTempModuleWithFirstRun(tb testing.TB, modDir string) string {
 		{"test_meta.json", "meta.json"},
 		{"first_run.sh", "first_run.sh"},
 	} {
-		srcPath := utils.ResolveFile(filepath.Join(modDir, cp.src))
+		resolvedModDir := utils.ResolveFile(modDir)
+
+		//nolint:gosec
+		dir, err := os.Open(resolvedModDir)
+		if err != nil {
+			tb.Fatal(err)
+		}
+		files, err := dir.ReadDir(-1)
+		if err != nil {
+			tb.Fatal(err)
+		}
+
+		tb.Log("files in mod directory:")
+		for _, file := range files {
+			tb.Log(file)
+		}
+
+		srcPath := filepath.Join(resolvedModDir, cp.src)
 		//nolint:gosec
 		src, err := os.Open(srcPath)
 		if err != nil {
