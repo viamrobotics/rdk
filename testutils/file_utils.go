be6f5958a71ffc92b92d85c3cce4e435f50f1b2c
Maxim Pertsov <maxim@viam.com>
2024-10-30 13:55:19 -0400

    RSDK-9087 Test setup phase (#4483)

--- testutils/file_utils.go
+++ testutils/file_utils.go
@@ -1,6 +1,9 @@
 package testutils
 
 import (
+	"errors"
+	"io"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
@@ -12,15 +15,16 @@ import (
 	"go.viam.com/rdk/utils"
 )
 
-// BuildTempModule will run "go build ." in the provided RDK directory and return the
-// path to the built temporary file. This function will fail the current test if there
-// are any build-related errors.
-func BuildTempModule(tb testing.TB, dir string) string {
+// BuildTempModule will attempt to build the module in the provided directory and put the
+// resulting executable binary into a temporary directory. If successful, this function will
+// return the path to the executable binary.
+func BuildTempModule(tb testing.TB, modDir string) string {
 	tb.Helper()
-	modPath := filepath.Join(tb.TempDir(), filepath.Base(dir))
+
+	exePath := filepath.Join(tb.TempDir(), filepath.Base(modDir))
 	//nolint:gosec
-	builder := exec.Command("go", "build", "-o", modPath, ".")
-	builder.Dir = utils.ResolveFile(dir)
+	builder := exec.Command("go", "build", "-o", exePath, ".")
+	builder.Dir = utils.ResolveFile(modDir)
 	out, err := builder.CombinedOutput()
 	// NOTE (Nick S): Workaround for the tickets below:
 	// https://viam.atlassian.net/browse/RSDK-7145
@@ -37,7 +41,60 @@ func BuildTempModule(tb testing.TB, dir string) string {
 	if tb.Failed() {
 		tb.Fatalf("failed to build temporary module for testing")
 	}
-	return modPath
+	return exePath
+}
+
+// BuildTempModuleWithFirstRun will attempt to build the module in the provided directory and put the
+// resulting executable binary into a temporary directory. After building, it will also copy "meta.json"
+// and "first_run.sh" into the same temporary directory. It is assumed that these files are in the
+// provided module directory. If successful, this function will return the path to the executable binary.
+func BuildTempModuleWithFirstRun(tb testing.TB, modDir string) string {
+	tb.Helper()
+
+	exePath := BuildTempModule(tb, modDir)
+	exeDir := filepath.Dir(exePath)
+
+	type copyOp struct {
+		src string
+		dst string
+	}
+
+	for _, cp := range []copyOp{
+		// TODO(RSDK-9151): Having a `meta.json` in the testmodule directory results in
+		// unintended behavior changes across our test suite. To avoid this, we name the
+		// file `first_run_meta.json` in the source test module directory and rename it in the
+		// destination directory that contains the built executable. This is fine for now
+		// but we should investigate and understand why this happens.
+		{"first_run_meta.json", "meta.json"},
+		{"first_run.sh", "first_run.sh"},
+	} {
+		srcPath := utils.ResolveFile(filepath.Join(modDir, cp.src))
+		//nolint:gosec
+		src, err := os.Open(srcPath)
+		if err != nil {
+			tb.Fatal(err)
+		}
+		dstPath := filepath.Join(exeDir, cp.dst)
+		//nolint:gosec
+		dst, err := os.OpenFile(dstPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o500)
+		if err != nil {
+			tb.Fatal(err)
+		}
+		closeFiles := func() {
+			if err := errors.Join(src.Close(), dst.Close()); err != nil {
+				tb.Error(err)
+			}
+		}
+		if _, err = io.Copy(dst, src); err != nil {
+			closeFiles()
+			tb.Fatal(err)
+		}
+		closeFiles()
+		if tb.Failed() {
+			tb.FailNow()
+		}
+	}
+	return exePath
 }
 
 // MockBuffer is a buffered writer that just appends data to an array to read
