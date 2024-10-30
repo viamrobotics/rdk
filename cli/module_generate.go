8ea1bcd0b5149e2f054affa5f2b089441e078a2f
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-10-30 11:11:02 -0400

    add language tag

--- cli/module_generate.go
+++ cli/module_generate.go
@@ -65,7 +65,7 @@ func (c *viamClient) generateModuleAction(cCtx *cli.Context) error {
 			ModuleName:       "my-module",
 			IsPublic:         false,
 			Namespace:        "my-org",
-			Language:         python,
+			Language:         cCtx.String(moduleFlagLanguage),
 			Resource:         resourceSubtype + " " + resourceType,
 			ResourceType:     resourceType,
 			ResourceSubtype:  resourceSubtype,
