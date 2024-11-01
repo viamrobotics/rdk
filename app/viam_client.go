e40cdaf77557732012249a94aa24163d71cdf358
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-11-01 17:12:45 -0400

    change name of url

--- app/viam_client.go
+++ app/viam_client.go
@@ -12,16 +12,16 @@ type viamClient struct {
 	conn rpc.ClientConn
 }
 
-func createViamClient(ctx context.Context, viamBaseURL string, opts rpc.DialOption, logger logging.Logger) (*viamClient, error) {
-	if viamBaseURL == "" {
-		viamBaseURL = "https://app.viam.com"
+func createViamClient(ctx context.Context, baseURL string, opts rpc.DialOption, logger logging.Logger) (*viamClient, error) {
+	if baseURL == "" {
+		baseURL = "https://app.viam.com"
 	}
-	viamURL, err := url.Parse(viamBaseURL + ":443")
+	serviceHost, err := url.Parse(baseURL + ":443")
 	if err != nil {
 		return nil, err
 	}
 
-	conn, err := rpc.DialDirectGRPC(ctx, viamURL.Host, logger, opts)
+	conn, err := rpc.DialDirectGRPC(ctx, serviceHost.Host, logger, opts)
 	if err != nil {
 		return nil, err
 	}
