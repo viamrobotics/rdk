e542e721c93104e0fc963ebde53948fb6e40e3e1
martha-johnston <martha.johnston@viam.com>
2024-11-04 12:37:26 -0500

    remove jetson and upboard from builtins

--- components/board/register/register.go
+++ components/board/register/register.go
@@ -8,11 +8,9 @@ import (
 	_ "go.viam.com/rdk/components/board/esp32"
 	_ "go.viam.com/rdk/components/board/fake"
 	_ "go.viam.com/rdk/components/board/hat/pca9685"
-	_ "go.viam.com/rdk/components/board/jetson"
 	_ "go.viam.com/rdk/components/board/numato"
 	_ "go.viam.com/rdk/components/board/odroid"
 	_ "go.viam.com/rdk/components/board/orangepi"
 	_ "go.viam.com/rdk/components/board/pi5"
 	_ "go.viam.com/rdk/components/board/ti"
-	_ "go.viam.com/rdk/components/board/upboard"
 )
