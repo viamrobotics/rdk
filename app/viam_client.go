c5b41f527b98c4996a3b3917befbc400baa2931d
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-11-04 12:25:56 -0500

    add checks for api key and key id formatting

--- app/viam_client.go
+++ app/viam_client.go
@@ -2,17 +2,19 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"net/url"
+	"regexp"
 
 	"go.viam.com/rdk/logging"
 	"go.viam.com/utils/rpc"
 )
 
-type viamClient struct {
+type ViamClient struct {
 	conn rpc.ClientConn
 }
 
-func createViamClient(ctx context.Context, baseURL string, opts rpc.DialOption, logger logging.Logger) (*viamClient, error) {
+func CreateViamClient(ctx context.Context, baseURL string, apiKey string, apiKeyID string, logger logging.Logger) (*ViamClient, error) {
 	if baseURL == "" {
 		baseURL = "https://app.viam.com"
 	}
@@ -21,9 +23,35 @@ func createViamClient(ctx context.Context, baseURL string, opts rpc.DialOption, 
 		return nil, err
 	}
 
+	switch {
+	case apiKey == "" || apiKeyID == "":
+		return nil, fmt.Errorf("API key and API key ID cannot be empty.")
+	case !validateApiKeyFormat(apiKey):
+		return nil, fmt.Errorf("API key should be a 32-char all-lowercase alphanumeric string")
+	case !validateApiKeyIDFormat(apiKeyID):
+		return nil, fmt.Errorf("API key ID should be an all-lowercase alphanumeric string with this format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
+	}
+	opts := rpc.WithEntityCredentials(
+		apiKeyID,
+		rpc.Credentials{
+			Type: rpc.CredentialsTypeAPIKey,
+			Payload: apiKey,
+		},
+	)
+
 	conn, err := rpc.DialDirectGRPC(ctx, serviceHost.Host, logger, opts)
 	if err != nil {
 		return nil, err
 	}
-	return &viamClient{conn: conn}, nil
+	return &ViamClient{conn: conn}, nil
+}
+
+func validateApiKeyFormat(apiKey string) (bool) {
+	var regex = regexp.MustCompile("^[a-z0-9]{32}$")
+	return regex.MatchString(apiKey)
+}
+
+func validateApiKeyIDFormat(apiKeyID string) (bool) {
+	var regex = regexp.MustCompile("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}")
+	return regex.MatchString(apiKeyID)
 }
