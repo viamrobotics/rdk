2a8cf919091c37635e589421d7545b4c466e83c7
Maxim Pertsov <maxim@viam.com>
2024-11-04 13:15:09 -0500

    fix docstring
    Co-authored-by: Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>

--- config/module.go
+++ config/module.go
@@ -43,7 +43,8 @@ type Module struct {
 
 	// FirstRunTimeout is the timeout duration for the first run script.
 	//
-	// Setting this field to a zero value (e.g. 0 * [time.Second]) will // set the first run timeout to the default value of 1 hour, which
+	// Setting this field to a zero value (e.g. 0 * [time.Second]) will 
+	// set the first run timeout to the default value of 1 hour, which
 	// is equivalent to leaving this field unset. If you wish to set an
 	// immediate timeout you can set this field to a negative value.
 	FirstRunTimeout goutils.Duration `json:"first_run_timeout,omitempty"`
