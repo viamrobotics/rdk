be1ad0646ce7ca5c5f64c054d828f8a4737e0033
martha-johnston <martha.johnston@viam.com>
2024-11-05 09:42:46 -0500

    remove beaglebone from builtin

--- components/board/register/register.go
+++ components/board/register/register.go
@@ -3,7 +3,6 @@ package register
 
 import (
 	// for boards.
-	_ "go.viam.com/rdk/components/board/beaglebone"
 	_ "go.viam.com/rdk/components/board/customlinux"
 	_ "go.viam.com/rdk/components/board/esp32"
 	_ "go.viam.com/rdk/components/board/fake"
