3d9a72f84887aa20dc8d542cea397c5c22b70174
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-11-06 12:59:48 -0500

    add location wrappers

--- app/app_client.go
+++ app/app_client.go
@@ -189,3 +189,120 @@ func (c *AppClient) ResendOrganizationInvite(ctx context.Context, orgId string, 
 	return resp.Invite, nil
 }
 
+// CreateLocation creates a location.
+func (c *AppClient) CreateLocation(ctx context.Context, orgId string, name string, parentLocationId *string) (*pb.Location, error) {
+	resp, err := c.client.CreateLocation(ctx, &pb.CreateLocationRequest{
+		OrganizationId: orgId,
+		Name: name,
+		ParentLocationId: parentLocationId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Location, nil
+}
+
+// GetLocation gets a location.
+func (c *AppClient) GetLocation(ctx context.Context, locationId string) (*pb.Location, error) {
+	resp, err := c.client.GetLocation(ctx, &pb.GetLocationRequest{
+		LocationId: locationId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Location, nil
+}
+
+// UpdateLocation updates a location.
+func (c *AppClient) UpdateLocation(ctx context.Context, locationId string, name *string, parentLocationId *string, region *string) (*pb.Location, error) {
+	resp, err := c.client.UpdateLocation(ctx, &pb.UpdateLocationRequest{
+		LocationId: locationId,
+		Name: name,
+		ParentLocationId: parentLocationId,
+		Region: region,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Location, nil
+}
+
+// DeleteLocation deletes a location.
+func (c *AppClient) DeleteLocation(ctx context.Context, locationId string) error {
+	_, err := c.client.DeleteLocation(ctx, &pb.DeleteLocationRequest{
+		LocationId: locationId,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListLocations gets a list of locations under the specified organization.
+func (c *AppClient) ListLocations(ctx context.Context, orgId string) ([]*pb.Location, error) {
+	resp, err := c.client.ListLocations(ctx, &pb.ListLocationsRequest{
+		OrganizationId: orgId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Locations, nil
+}
+
+// ShareLocation shares a location with an organization.
+func (c *AppClient) ShareLocation(ctx context.Context, locationId string, orgId string) error {
+	_, err := c.client.ShareLocation(ctx, &pb.ShareLocationRequest{
+		LocationId: locationId,
+		OrganizationId: orgId,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UnshareLocation stops sharing a location with an organization.
+func (c *AppClient) UnshareLocation(ctx context.Context, locationId string, orgId string) error {
+	_, err := c.client.UnshareLocation(ctx, &pb.UnshareLocationRequest{
+		LocationId: locationId,
+		OrganizationId: orgId,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// LocationAuth gets a location's authorization secrets.
+func (c *AppClient) LocationAuth(ctx context.Context, locationId string) (*pb.LocationAuth, error) {
+	resp, err := c.client.LocationAuth(ctx, &pb.LocationAuthRequest{
+		LocationId: locationId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Auth, nil
+}
+
+// CreateLocationSecret creates a new generated secret in the location. Succeeds if there are no more than 2 active secrets after creation.
+func (c *AppClient) CreateLocationSecret(ctx context.Context, locationId string) (*pb.LocationAuth, error) {
+	resp, err := c.client.CreateLocationSecret(ctx, &pb.CreateLocationSecretRequest{
+		LocationId: locationId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Auth, nil
+}
+
+// Delete a secret from the location.
+func (c *AppClient) DeleteLocationSecret(ctx context.Context, locationId string, secretId string) error {
+	_, err := c.client.DeleteLocationSecret(ctx, &pb.DeleteLocationSecretRequest{
+		LocationId: locationId,
+		SecretId: secretId,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
