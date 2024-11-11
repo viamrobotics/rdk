fd50881ed898e13ed2250958db27a98abb2748e2
hexbabe <sean.xiaochen.yu@gmail.com>
2024-11-11 14:39:04 -0500

    Remove jpeg default in vision

--- services/vision/vision.go
+++ services/vision/vision.go
@@ -18,7 +18,6 @@ import (
 	"go.viam.com/rdk/gostream"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/robot"
-	"go.viam.com/rdk/utils"
 	viz "go.viam.com/rdk/vision"
 	"go.viam.com/rdk/vision/classification"
 	"go.viam.com/rdk/vision/objectdetection"
@@ -310,7 +309,7 @@ func (vm *vizModel) DetectionsFromCamera(
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not find camera named %s", cameraName)
 	}
-	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, utils.MimeTypeJPEG), extra, cam)
+	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, ""), extra, cam)
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not get go image from %s", cameraName)
 	}
@@ -352,7 +351,7 @@ func (vm *vizModel) ClassificationsFromCamera(
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not find camera named %s", cameraName)
 	}
-	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, utils.MimeTypeJPEG), extra, cam)
+	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, ""), extra, cam)
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not get go image from %s", cameraName)
 	}
@@ -401,7 +400,7 @@ func (vm *vizModel) CaptureAllFromCamera(
 	if err != nil {
 		return viscapture.VisCapture{}, errors.Wrapf(err, "could not find camera named %s", cameraName)
 	}
-	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, utils.MimeTypeJPEG), extra, cam)
+	img, err := camera.GetGoImage(ctx, gostream.MIMETypeHint(ctx, ""), extra, cam)
 	if err != nil {
 		return viscapture.VisCapture{}, errors.Wrapf(err, "could not get image from %s", cameraName)
 	}
