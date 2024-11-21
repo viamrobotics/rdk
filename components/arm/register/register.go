d13d58a0ab54455609d0f642307ba7ce2c82de20
Nick Franczak <nicholasgfranczak@gmail.com>
2024-11-21 14:05:06 -0500

    remove xArm from being registered within RDK

--- components/arm/register/register.go
+++ components/arm/register/register.go
@@ -3,8 +3,6 @@ package register
 
 import (
 	// register arms.
-	_ "github.com/viam-modules/viam-ufactory-xarm/arm"
-
 	_ "go.viam.com/rdk/components/arm/eva"
 	_ "go.viam.com/rdk/components/arm/fake"
 	_ "go.viam.com/rdk/components/arm/universalrobots"
