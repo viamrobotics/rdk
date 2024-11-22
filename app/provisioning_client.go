4a1f463d8a9e47467b968cad2e008b3436e072a3
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-11-22 15:52:49 -0500

    nit

--- app/provisioning_client.go
+++ app/provisioning_client.go
@@ -82,7 +82,7 @@ type ProvisioningClient struct {
 	client pb.ProvisioningServiceClient
 }
 
-// NewProvisioningClient constructs a new ProvisioningClient using the connection passed in by the Viam client.
+// NewProvisioningClient constructs a new ProvisioningClient using the connection passed in by the ViamClient.
 func NewProvisioningClient(conn rpc.ClientConn) *ProvisioningClient {
 	return &ProvisioningClient{client: pb.NewProvisioningServiceClient(conn)}
 }
