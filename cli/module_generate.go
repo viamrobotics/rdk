9cde52f249a7903f26514b13cf07afa29a8f4943
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-12-10 11:17:18 -0500

    erase repetitive steps

--- cli/module_generate.go
+++ cli/module_generate.go
@@ -72,16 +72,6 @@ func (c *viamClient) generateModuleAction(cCtx *cli.Context) error {
 			ModelName:        "my-model",
 			EnableCloudBuild: false,
 			RegisterOnApp:    false,
-			GeneratorVersion: "0.1.0",
-			GeneratedOn:      time.Now().UTC(),
-
-			ModulePascal:          "MyModule",
-			API:                   fmt.Sprintf("rdk:%s:%s", resourceType, resourceSubtype),
-			ResourceSubtypePascal: strings.ToUpper(string(resourceSubtype[0])) + resourceSubtype[1:],
-			ModelPascal:           "MyModel",
-			ModelTriple:           "my-org:my-module:my-model",
-
-			SDKVersion: "0.0.0",
 		}
 		populateAdditionalInfo(newModule)
 	} else {
