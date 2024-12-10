c1e816cf37e495069a6d8e69fe66c3e932a9631a
Benji Rewis <benji.rewis@viam.com>
2024-12-10 16:07:12 -0500

    atomic

--- robot/impl/local_robot.go
+++ robot/impl/local_robot.go
@@ -93,7 +93,7 @@ type localRobot struct {
 
 	// whether the robot is still initializing (first reconfigure after initial
 	// construction has not yet completed.)
-	initializing bool
+	initializing atomic.Bool
 }
 
 // ExportResourcesAsDot exports the resource graph as a DOT representation for
@@ -503,7 +503,7 @@ func newWithResources(
 
 	successful = true
 	// Robot is "initializing" until first reconfigure after initial creation completes.
-	r.initializing = true
+	r.initializing.Store(true)
 	return r, nil
 }
 
@@ -1097,7 +1097,7 @@ func (r *localRobot) Reconfigure(ctx context.Context, newConfig *config.Config) 
 	// got a chance to attempt configuration.
 	//
 	// On initial construction, this value will get set back to true.
-	r.initializing = false
+	r.initializing.Store(false)
 }
 
 // set Module.LocalVersion on Type=local modules. Call this before localPackages.Sync and in RestartModule.
@@ -1297,7 +1297,7 @@ func (r *localRobot) reconfigure(ctx context.Context, newConfig *config.Config, 
 	// If initializing, machine will be starting with no modules, but may
 	// immediately reconfigure to start modules that have already been
 	// downloaded. Do not cleanup packages/module dirs in that case.
-	if !r.initializing {
+	if !r.initializing.Load() {
 		allErrs = multierr.Combine(allErrs, r.packageManager.Cleanup(ctx))
 		allErrs = multierr.Combine(allErrs, r.localPackages.Cleanup(ctx))
 		// Cleanup extra dirs from previous modules or rogue scripts.
@@ -1309,7 +1309,6 @@ func (r *localRobot) reconfigure(ctx context.Context, newConfig *config.Config, 
 	} else {
 		r.logger.CInfow(ctx, "Robot (re)configured")
 	}
-
 }
 
 // checkMaxInstance checks to see if the local robot has reached the maximum number of a specific resource type that are local.
@@ -1418,7 +1417,7 @@ func (r *localRobot) MachineStatus(ctx context.Context) (robot.MachineStatus, er
 	r.configRevisionMu.RUnlock()
 
 	result.State = robot.StateRunning
-	if r.initializing {
+	if r.initializing.Load() {
 		result.State = robot.StateInitializing
 	}
 	return result, nil
