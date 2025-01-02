45d59d8541bff99378a710a7137c5d210c7acc97
Alan Davidson <alan@viam.com>
2025-01-02 13:37:33 -0500

    tweak comment so it still makes sense after gofmt

--- cli/module_generate/_templates/go/tmpl-main.go
+++ cli/module_generate/_templates/go/tmpl-main.go
@@ -5,11 +5,9 @@ import (
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/{{.ResourceType}}s/{{.ResourceSubtype}}"
-
 )
 
 func main() {
-	module.ModularMain(resource.APIModel{ {{.ResourceSubtype}}.API, models.{{.ModelPascal}} },
-	                   // If your module implements multiple models, add the rest here
-				       )
+	// ModularMain can take multiple arguments, if your module implements multiple models.
+	module.ModularMain(resource.APIModel{ {{.ResourceSubtype}}.API, models.{{.ModelPascal}}})
 }
