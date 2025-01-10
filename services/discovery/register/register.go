27ae6f5f186aceb0c4e18d09e9e9d3c3ac0c46ce
JohnN193 <92045055+JohnN193@users.noreply.github.com>
2025-01-10 11:49:16 -0500

    [RSDK-9655] skip reflection for discovery service and add fake discovery (#4697)

---
+++ services/discovery/register/register.go
@@ -0,0 +1,7 @@
+// Package register registers all relevant discovery models and also API specific functions
+package register
+
+import (
+	// for discovery models.
+	_ "go.viam.com/rdk/services/discovery/fake"
+)
