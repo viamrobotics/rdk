0ac5ac6115c275b3827b5f592ab4cd5a958fdbd3
Ray Bjorkman <raymond@viam.com>
2025-01-27 18:47:33 -0500

    remove some triangle functions

--- spatialmath/triangle.go
+++ spatialmath/triangle.go
@@ -1,8 +1,6 @@
 package spatialmath
 
 import (
-	"math"
-
 	"github.com/golang/geo/r3"
 )
 
@@ -25,62 +23,14 @@ func NewTriangle(p0, p1, p2 r3.Vector) *Triangle {
 	}
 }
 
-// ClosestPointTriangleCoplanarPoint takes a point, and returns the closest point on the triangle to the given point
-// The given point *MUST* be coplanar with the triangle. If it is known ahead of time that the point is coplanar, this is faster.
-func ClosestPointTriangleCoplanarPoint(t *Triangle, pt r3.Vector) r3.Vector {
-	// Determine whether point is inside all triangle edges:
-	c0 := pt.Sub(t.p0).Cross(t.p1.Sub(t.p0))
-	c1 := pt.Sub(t.p1).Cross(t.p2.Sub(t.p1))
-	c2 := pt.Sub(t.p2).Cross(t.p0.Sub(t.p2))
-	inside := c0.Dot(t.normal) <= 0 && c1.Dot(t.normal) <= 0 && c2.Dot(t.normal) <= 0
-
-	if inside {
-		return pt
-	}
-
-	// Edge 1:
-	refPt := ClosestPointSegmentPoint(t.p0, t.p1, pt)
-	bestDist := pt.Sub(refPt).Norm2()
-
-	// Edge 2:
-	point2 := ClosestPointSegmentPoint(t.p1, t.p2, pt)
-	if distsq := pt.Sub(point2).Norm2(); distsq < bestDist {
-		refPt = point2
-		bestDist = distsq
-	}
-
-	// Edge 3:
-	point3 := ClosestPointSegmentPoint(t.p2, t.p0, pt)
-	if distsq := pt.Sub(point3).Norm2(); distsq < bestDist {
-		return point3
-	}
-	return refPt
+// Points returns the three points associated with the triangle.
+func (t *Triangle) Points() []r3.Vector {
+	return []r3.Vector{t.p0, t.p1, t.p2}
 }
 
-// ClosestPointTrianglePoint takes a point, and returns the closest point on the triangle to the given point.
-// This is slower than ClosestPointTriangleCoplanarPoint.
-func ClosestPointTrianglePoint(t *Triangle, point r3.Vector) r3.Vector {
-	closestPtInside, inside := ClosestTriangleInsidePoint(t, point)
-	if inside {
-		return closestPtInside
-	}
-
-	// If the closest point is outside the triangle, it must be on an edge, so we
-	// check each triangle edge for a closest point to the point pt.
-	closestPt := ClosestPointSegmentPoint(t.p0, t.p1, point)
-	bestDist := point.Sub(closestPt).Norm2()
-
-	newPt := ClosestPointSegmentPoint(t.p1, t.p2, point)
-	if newDist := point.Sub(newPt).Norm2(); newDist < bestDist {
-		closestPt = newPt
-		bestDist = newDist
-	}
-
-	newPt = ClosestPointSegmentPoint(t.p2, t.p0, point)
-	if newDist := point.Sub(newPt).Norm2(); newDist < bestDist {
-		return newPt
-	}
-	return closestPt
+// Normal returns the triangle's normal vector.
+func (t *Triangle) Normal() r3.Vector {
+	return t.normal
 }
 
 // ClosestTriangleInsidePoint returns the closest point on a triangle IF AND ONLY IF the query point's projection overlaps the triangle.
@@ -107,95 +57,4 @@ func ClosestTriangleInsidePoint(t *Triangle, point r3.Vector) (r3.Vector, bool) 
 	v := (-b*e0.Dot(d) + a*e1.Dot(d)) / det
 	inside := (0 <= u+eps) && (u <= 1+eps) && (0 <= v+eps) && (v <= 1+eps) && (u+v <= 1+eps)
 	return t.p0.Add(e0.Mul(u)).Add(e1.Mul(v)), inside
-}
-
-// Points returns the three points associated with the triangle.
-func (t *Triangle) Points() []r3.Vector {
-	return []r3.Vector{t.p0, t.p1, t.p2}
-}
-
-// Normal returns the triangle's normal vector.
-func (t *Triangle) Normal() r3.Vector {
-	return t.normal
-}
-
-// TriangleIntersectsPlane determines if the triangle intersects with a plane defined by a point and normal vector.
-// Returns true if the triangle intersects with or lies on the plane.
-func TriangleIntersectsPlane(t *Triangle, planePt, planeNormal r3.Vector) bool {
-	// Calculate signed distances from each triangle vertex to the plane
-	d0 := planeNormal.Dot(t.p0.Sub(planePt))
-	d1 := planeNormal.Dot(t.p1.Sub(planePt))
-	d2 := planeNormal.Dot(t.p2.Sub(planePt))
-
-	// If all points are on the same side of the plane (all distances positive or all negative),
-	// there is no intersection
-	if (d0 > floatEpsilon && d1 > floatEpsilon && d2 > floatEpsilon) ||
-		(d0 < -floatEpsilon && d1 < -floatEpsilon && d2 < -floatEpsilon) {
-		return false
-	}
-
-	// If any point is very close to the plane or points lie on different sides,
-	// there is an intersection
-	return true
-}
-
-// TrianglePlaneIntersectingSegment determines the line segment where a triangle intersects with a plane.
-// Returns the two points defining the intersection line segment and whether an intersection exists.
-// If the triangle only touches the plane at a point, both returned points will be the same.
-// If the triangle lies in the plane, it returns two points representing the longest edge of the triangle.
-func TrianglePlaneIntersectingSegment(t *Triangle, planePt, planeNormal r3.Vector) (r3.Vector, r3.Vector, bool) {
-	// First check if there's an intersection
-	if !TriangleIntersectsPlane(t, planePt, planeNormal) {
-		return r3.Vector{}, r3.Vector{}, false
-	}
-
-	// Calculate signed distances from each vertex to the plane
-	d0 := planeNormal.Dot(t.p0.Sub(planePt))
-	d1 := planeNormal.Dot(t.p1.Sub(planePt))
-	d2 := planeNormal.Dot(t.p2.Sub(planePt))
-
-	// If triangle lies in plane (all distances are approximately zero)
-	if math.Abs(d0) < floatEpsilon && math.Abs(d1) < floatEpsilon && math.Abs(d2) < floatEpsilon {
-		// Return the longest edge of the triangle
-		e1 := t.p1.Sub(t.p0).Norm2()
-		e2 := t.p2.Sub(t.p1).Norm2()
-		e3 := t.p0.Sub(t.p2).Norm2()
-		if e1 >= e2 && e1 >= e3 {
-			return t.p0, t.p1, true
-		} else if e2 >= e1 && e2 >= e3 {
-			return t.p1, t.p2, true
-		}
-		return t.p2, t.p0, true
-	}
-
-	// Find the two edges that intersect with the plane
-	var intersections []r3.Vector
-	edges := [][2]r3.Vector{
-		{t.p0, t.p1},
-		{t.p1, t.p2},
-		{t.p2, t.p0},
-	}
-	dists := []float64{d0, d1, d2}
-
-	for i := 0; i < 3; i++ {
-		j := (i + 1) % 3
-		// If edge crosses plane (distances have different signs)
-		if (dists[i] * dists[j]) < 0 {
-			// Calculate intersection point using linear interpolation
-			t := dists[i] / (dists[i] - dists[j])
-			edge := edges[i]
-			intersection := edge[0].Add(edge[1].Sub(edge[0]).Mul(t))
-			intersections = append(intersections, intersection)
-		} else if math.Abs(dists[i]) < floatEpsilon {
-			// Vertex lies on plane
-			intersections = append(intersections, edges[i][0])
-		}
-	}
-
-	// We should have exactly two intersection points
-	if len(intersections) < 2 {
-		// Handle degenerate case where triangle only touches plane at a point
-		return intersections[0], intersections[0], true
-	}
-	return intersections[0], intersections[1], true
 }
