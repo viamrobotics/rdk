da4721b7cbf11e321d562d554576ec6c2094901d
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-02-06 14:37:36 -0500

    add generator class

---
+++ cli/module_generate/cpp-gen/generator.cpp
@@ -0,0 +1,148 @@
+#include "generator.hpp"
+
+#include "compilation_db.hpp"
+#include "compiler_info.hpp"
+#include <clang/AST/PrettyPrinter.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/JSONCompilationDatabase.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/ADT/StringMap.h>
+#include <llvm/Support/Path.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <string_view>
+#include <unordered_map>
+
+namespace viam::gen {
+
+Generator Generator::create(llvm::StringRef className,
+                            llvm::StringRef buildDir,
+                            llvm::StringRef sourceDir,
+                            llvm::raw_ostream& moduleFile) {
+    std::string error;
+    auto jsonDb = clang::tooling::JSONCompilationDatabase::autoDetectFromDirectory(buildDir, error);
+    if (!jsonDb) {
+        throw std::runtime_error(error);
+    }
+
+    return Generator(GeneratorCompDB(*jsonDb, getCompilersDefaultIncludeDir(*jsonDb, true)),
+                     className.str(),
+                     (sourceDir + classNameToSource(className)).str(),
+                     moduleFile);
+}
+
+Generator Generator::createFromCommandLine(const clang::tooling::CompilationDatabase& compDb,
+                                           llvm::StringRef sourceFile,
+                                           llvm::raw_ostream& moduleFile) {
+    llvm::SmallVector<llvm::StringRef, 3> splits;
+    auto classFilename = llvm::sys::path::filename(sourceFile);
+
+    classFilename.substr(0, classFilename.find('.')).split(splits, '_');
+
+    std::string className;
+
+    llvm::raw_string_ostream os(className);
+
+    for (llvm::StringRef component : splits) {
+        os << static_cast<char>(std::toupper(*component.bytes_begin())) << component.drop_front();
+    }
+
+    return Generator(GeneratorCompDB(compDb, getCompilersDefaultIncludeDir(compDb, true)),
+                     className,
+                     sourceFile.str(),
+                     moduleFile);
+}
+
+int Generator::run() {
+    clang::tooling::ClangTool tool(db_, classPath_);
+
+    using namespace clang::ast_matchers;
+
+    std::string qualName = ("viam::sdk::" + className_);
+
+    DeclarationMatcher methodMatcher =
+        cxxMethodDecl(isPure(), hasParent(cxxRecordDecl(hasName(qualName)))).bind("method");
+
+    struct MethodPrinter : MatchFinder::MatchCallback {
+        MethodPrinter(llvm::raw_ostream& os_) : os(os_) {}
+
+        llvm::raw_ostream& os;
+
+        void printParm(const clang::ParmVarDecl& parm) {
+            os << parm.getType().getAsString({parm.getASTContext().getLangOpts()}) << " "
+               << parm.getName();
+        }
+
+        void run(const MatchFinder::MatchResult& result) override {
+            if (const auto* method = result.Nodes.getNodeAs<clang::CXXMethodDecl>("method")) {
+                clang::PrintingPolicy printPolicy(method->getASTContext().getLangOpts());
+
+                os << method->getReturnType().getAsString(printPolicy) << " " << method->getName()
+                   << "(";
+
+                if (method->getNumParams() > 0) {
+                    auto param_begin = method->param_begin();
+                    printParm(**param_begin);
+
+                    if (method->getNumParams() > 1) {
+                        for (const clang::ParmVarDecl* parm :
+                             llvm::makeArrayRef(++param_begin, method->param_end())) {
+                            os << ", ";
+                            printParm(*parm);
+                        }
+                    }
+                }
+
+                os << ")";
+
+                method->getMethodQualifiers().print(os, printPolicy, false);
+
+                os << " override\n";
+
+                os << "{\n\tthrow std::logic_error(\"" << method->getName()
+                   << "\" not implemented\");\n}\n\n";
+            }
+        }
+    };
+
+    MethodPrinter printer(moduleFile_);
+    MatchFinder finder;
+
+    finder.addMatcher(methodMatcher, &printer);
+
+    return tool.run(clang::tooling::newFrontendActionFactory(&finder).get());
+}
+
+Generator::Generator(GeneratorCompDB db,
+                     std::string className,
+                     std::string classPath,
+                     llvm::raw_ostream& moduleFile)
+    : db_(std::move(db)),
+      className_(std::move(className)),
+      classPath_(std::move(classPath)),
+      moduleFile_(moduleFile) {}
+
+llvm::StringRef Generator::classNameToSource(llvm::StringRef className) {
+    static std::unordered_map<std::string_view, std::string_view> correspondence{
+        {"Arm", "arm.cpp"},
+        {"Base", "base.cpp"},
+        {"Board", "board.cpp"},
+        {"Camera", "camera.cpp"},
+        {"Component", "component.cpp"},
+        {"Encoder", "encoder.cpp"},
+        {"Gantry", "gantry.cpp"},
+        {"Generic", "generic.cpp"},
+        {"Gripper", "gripper.cpp"},
+        {"Motor", "motor.cpp"},
+        {"MovementSensor", "movement_sensor.cpp"},
+        {"PoseTracker", "pose_tracker.cpp"},
+        {"PowerSensor", "power_sensor.cpp"},
+        {"Sensor", "sensor.cpp"},
+        {"Servo", "servo.cpp"}};
+
+    return correspondence.at(className);
+}
+
+}  // namespace viam::gen
---
+++ cli/module_generate/cpp-gen/generator.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "compilation_db.hpp"
+
+#include <clang/Tooling/Tooling.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/raw_ostream.h>
+
+namespace viam::gen {
+
+class Generator {
+   public:
+    static Generator create(llvm::StringRef className,
+                            llvm::StringRef buildDir,
+                            llvm::StringRef sourceDir,
+                            llvm::raw_ostream& moduleFile);
+
+    static Generator createFromCommandLine(const clang::tooling::CompilationDatabase& compDb,
+                                           llvm::StringRef sourceFile,
+                                           llvm::raw_ostream& moduleFile);
+
+    int run();
+
+   private:
+    Generator(GeneratorCompDB db,
+              std::string className,
+              std::string classPath,
+              llvm::raw_ostream& moduleFile);
+
+    static llvm::StringRef classNameToSource(llvm::StringRef className);
+
+    GeneratorCompDB db_;
+
+    std::string className_;
+
+    std::string classPath_;
+
+    llvm::raw_ostream& moduleFile_;
+};
+
+}  // namespace viam::gen
