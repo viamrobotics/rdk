61996a63638e31d575897d4874ec1534f65e39b5
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2025-02-14 14:13:24 -0500

    Fix no browser warning in the cli (#4793)

--- cli/auth.go
+++ cli/auth.go
@@ -715,9 +715,9 @@ func (a *authFlow) loginAsUser(c *cli.Context) (*token, error) {
 
 	err = a.directUser(deviceCode)
 	if err != nil {
-		warningf(c.App.ErrWriter, "unable to open the browser to complete the login flow due to %w. "+
+		warningf(c.App.ErrWriter, "unable to open the browser to complete the login flow due to %q. "+
 			"Please go to the provided URL to log in; you can use the --%s flag to skip this warning in the future",
-			err, loginFlagDisableBrowser)
+			err.Error(), loginFlagDisableBrowser)
 	}
 
 	token, err := a.waitForUser(ctx, deviceCode, discovery)
