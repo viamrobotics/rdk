962b21a83a138a32bb1d2516782c205be64a57c1
Bashar Eid <bashar.eid@viam.com>
2025-02-18 15:26:13 -0500

    modify auth tests

--- internal/cloud/service_test.go
+++ internal/cloud/service_test.go
@@ -2,7 +2,6 @@ package cloud_test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"testing"
 
@@ -76,6 +75,7 @@ func TestCloudManaged(t *testing.T) {
 	})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, resp.Message, test.ShouldEqual, "hello")
+
 	resp, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
@@ -148,9 +148,8 @@ func TestCloudManagedWithAuth(t *testing.T) {
 
 	appConn, err := grpc.NewAppConn(context.Background(), conf.AppAddress, "", "", logger)
 	test.That(t, err, test.ShouldBeNil)
-	defer test.That(t, appConn.Close(), test.ShouldBeNil)
 
-	svc := cloud.NewCloudConnectionService(conf, nil, logger)
+	svc := cloud.NewCloudConnectionService(conf, appConn, logger)
 	id, conn1, err := svc.AcquireConnection(context.Background())
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, id, test.ShouldBeEmpty)
@@ -162,13 +161,15 @@ func TestCloudManagedWithAuth(t *testing.T) {
 	test.That(t, conn2, test.ShouldEqual, appConn)
 
 	echoClient := echopb.NewEchoServiceClient(conn1)
-	_, err = echoClient.Echo(context.Background(), &echopb.EchoRequest{
+	resp, err := echoClient.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
+	test.That(t, resp, test.ShouldBeNil)
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, status.Code(err), test.ShouldEqual, codes.Unauthenticated)
 
 	test.That(t, svc.Close(context.Background()), test.ShouldBeNil)
+	test.That(t, appConn.Close(), test.ShouldBeNil)
 
 	conf = &config.Cloud{
 		AppAddress: fmt.Sprintf("http://%s", addr),
@@ -176,7 +177,10 @@ func TestCloudManagedWithAuth(t *testing.T) {
 		Secret:     "bar",
 	}
 
-	svc = cloud.NewCloudConnectionService(conf, nil, logger)
+	appConn, err = grpc.NewAppConn(context.Background(), conf.AppAddress, conf.Secret, conf.ID, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	svc = cloud.NewCloudConnectionService(conf, appConn, logger)
 	id, conn1, err = svc.AcquireConnection(context.Background())
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, id, test.ShouldEqual, "foo")
@@ -185,82 +189,53 @@ func TestCloudManagedWithAuth(t *testing.T) {
 	id2, conn2, err = svc.AcquireConnection(context.Background())
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, id2, test.ShouldEqual, "foo")
-	test.That(t, conn2, test.ShouldNotBeNil)
-	test.That(t, conn2, test.ShouldNotEqual, conn1)
+	test.That(t, conn2, test.ShouldEqual, appConn)
 
 	echoClient1 := echopb.NewEchoServiceClient(conn1)
 	echoClient2 := echopb.NewEchoServiceClient(conn2)
 
-	resp, err := echoClient1.Echo(context.Background(), &echopb.EchoRequest{
+	resp, err = echoClient1.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, resp.Message, test.ShouldEqual, "hello")
 
-	conn2.Close()
-
-	// technically conn2 is open via ref count
 	resp, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, resp.Message, test.ShouldEqual, "hello")
-	test.That(t, conn1.Close(), test.ShouldBeNil)
 
-	// now both are closed
-	_, err = echoClient1.Echo(context.Background(), &echopb.EchoRequest{
+	test.That(t, appConn.Close(), test.ShouldBeNil)
+
+	// now "both" connections are closed
+	resp, err = echoClient1.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
+	test.That(t, resp, test.ShouldBeNil)
 	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
-	_, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
+	test.That(t, err, test.ShouldEqual, grpc.ErrNotConnected)
+
+	resp, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
+	test.That(t, resp, test.ShouldBeNil)
 	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
+	test.That(t, err, test.ShouldEqual, grpc.ErrNotConnected)
 
 	id3, conn3, err := svc.AcquireConnection(context.Background())
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, id3, test.ShouldEqual, "foo")
 	test.That(t, conn3, test.ShouldNotBeNil)
-	test.That(t, conn3, test.ShouldNotEqual, conn2)
-
-	_, err = echoClient1.Echo(context.Background(), &echopb.EchoRequest{
-		Message: "hello",
-	})
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
-	_, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
-		Message: "hello",
-	})
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
+	test.That(t, conn3, test.ShouldEqual, conn2)
 
 	echoClient3 := echopb.NewEchoServiceClient(conn3)
 	resp, err = echoClient3.Echo(context.Background(), &echopb.EchoRequest{
 		Message: "hello",
 	})
-	test.That(t, err, test.ShouldBeNil)
-	test.That(t, resp.Message, test.ShouldEqual, "hello")
+	test.That(t, resp, test.ShouldBeNil)
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err, test.ShouldEqual, grpc.ErrNotConnected)
 
 	test.That(t, svc.Close(context.Background()), test.ShouldBeNil)
-	_, err = echoClient1.Echo(context.Background(), &echopb.EchoRequest{
-		Message: "hello",
-	})
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
-	_, err = echoClient2.Echo(context.Background(), &echopb.EchoRequest{
-		Message: "hello",
-	})
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
-	_, err = echoClient3.Echo(context.Background(), &echopb.EchoRequest{
-		Message: "hello",
-	})
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, status.Code(err), test.ShouldEqual, codes.Canceled)
-	test.That(t, conn2.Close(), test.ShouldBeNil)
-
-	_, _, err = svc.AcquireConnection(context.Background())
-	test.That(t, err, test.ShouldBeError, errors.New("service closed"))
 }
