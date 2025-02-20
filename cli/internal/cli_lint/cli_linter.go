adecad4d64e3e0c22d7d3161af7d56949d17f708
Ethan Rodkin <ethan@viam.com>
2025-02-20 11:50:59 -0500

    a bit of cleanup

--- cli/internal/cli_lint/cli_linter.go
+++ cli/internal/cli_lint/cli_linter.go
@@ -5,7 +5,6 @@ import (
 	"go/types"
 	"strings"
 
-	_ "go.viam.com/rdk"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
@@ -24,7 +23,7 @@ func enforceCreateCommandWithTRun(pass *analysis.Pass) (interface{}, error) {
 		if pkg.Name() == "cli" && !strings.Contains(pkg.Path(), "viam") {
 
 			commandType = pkg.Scope().Lookup("Command").Type()
-			commandType = types.NewPointer(commandType)
+			commandType = types.NewPointer(commandType) // actual `Commands` in the app are pointers
 		}
 	}
 	if commandType == nil { // no CLI imports so we can skip
@@ -34,52 +33,62 @@ func enforceCreateCommandWithTRun(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
 		ast.Inspect(file, func(node ast.Node) bool {
 			composite, isComposite := node.(*ast.CompositeLit)
+
+			// we aren't looking at a struct, so no need to evaluate further
 			if !isComposite {
 				return true
 			}
 			compositeType := pass.TypesInfo.TypeOf(composite)
+
+			// we aren't looking at a CLI command, so no need to evaluate further
 			if compositeType.String() != commandType.String() {
 				return true
 			}
+
 			for _, elt := range composite.Elts {
-				if ident, isIdent := elt.(*ast.KeyValueExpr); isIdent {
-					key := ident.Key.(*ast.Ident)
+				keyValue, isKeyValue := elt.(*ast.KeyValueExpr)
 
-					// "Action", "Before", and "After" are the three types of CLI actions for which
-					// `createCommandWithT` was designed
-					if key.Name == "Action" || key.Name == "Before" ||
-						key.Name == "After" {
-						callExpr, isCallExpr := ident.Value.(*ast.CallExpr)
+				// we aren't looking at assignment of a struct param, so no need to evaluate further
+				if !isKeyValue {
+					return true
+				}
+				key := keyValue.Key.(*ast.Ident)
 
-						// `Action` was assigned a literal value, rather than a function call.
-						if !isCallExpr {
-							pass.Report(analysis.Diagnostic{
-								Pos:     ident.Pos(),
-								End:     ident.End(),
-								Message: "must use createCommandWithT when constructing a CLI action",
-							})
-							return true
-						}
-						callExprFunc, isCallExprFunc := callExpr.Fun.(*ast.IndexExpr)
-						if !isCallExprFunc { // this should never happen, but just for explicitness
-							return true
-						}
-						funcIdent, isFuncIdent := callExprFunc.X.(*ast.Ident)
-						if !isFuncIdent { // as above, this should never happen
-							return true
-						}
-						if funcIdent.Name != "createCommandWithT" {
-							// some other func was used to generate the action
-							pass.Report(analysis.Diagnostic{
-								Pos:     funcIdent.Pos(),
-								End:     funcIdent.End(),
-								Message: "must use createCommandWithT when constructing a CLI action",
-							})
-						}
+				// "Action", "Before", and "After" are the three types of CLI actions for which
+				// `createCommandWithT` was designed
+				if key.Name == "Action" || key.Name == "Before" ||
+					key.Name == "After" {
+					callExpr, isCallExpr := keyValue.Value.(*ast.CallExpr)
+
+					// `Action` was assigned a literal value, rather than a function call.
+					if !isCallExpr {
+						pass.Report(analysis.Diagnostic{
+							Pos:     keyValue.Pos(),
+							End:     keyValue.End(),
+							Message: "must use createCommandWithT when constructing a CLI action",
+						})
+						return true
+					}
+
+					callExprFunc, isCallExprFunc := callExpr.Fun.(*ast.IndexExpr)
+					if !isCallExprFunc { // this should never happen, but just for explicitness
+						return true
+					}
+					funcIdent, isFuncIdent := callExprFunc.X.(*ast.Ident)
+					if !isFuncIdent { // as above, this should never happen
+						return true
+					}
+
+					if funcIdent.Name != "createCommandWithT" {
+						// some other func was used to generate the action
+						pass.Report(analysis.Diagnostic{
+							Pos:     funcIdent.Pos(),
+							End:     funcIdent.End(),
+							Message: "must use createCommandWithT when constructing a CLI action",
+						})
 					}
 				}
 			}
-
 			return true
 		})
 	}
