6cbaba3c8d93a6b98e2010d24ab36fe95c0bdabd
Ray Bjorkman <raymond@viam.com>
2025-02-21 14:42:20 -0500

    cleanup ancient pose tracker code

--- components/posetracker/pose_tracker.go
+++ components/posetracker/pose_tracker.go
@@ -7,7 +7,6 @@ import (
 
 	pb "go.viam.com/api/component/posetracker/v1"
 
-	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/referenceframe"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/robot"
@@ -33,31 +32,15 @@ func Named(name string) resource.Name {
 	return resource.NewName(API, name)
 }
 
-// BodyToPoseInFrame represents a map of body names to PoseInFrames.
-type BodyToPoseInFrame map[string]*referenceframe.PoseInFrame
-
 // A PoseTracker represents a robot component that can observe bodies in an
 // environment and provide their respective poses in space. These poses are
 // given in the context of the PoseTracker's frame of reference.
 type PoseTracker interface {
-	sensor.Sensor
-	Poses(ctx context.Context, bodyNames []string, extra map[string]interface{}) (BodyToPoseInFrame, error)
+	resource.Resource
+	Poses(ctx context.Context, bodyNames []string, extra map[string]interface{}) (referenceframe.FrameSystemPoses, error)
 }
 
 // FromRobot is a helper for getting the named force matrix sensor from the given Robot.
 func FromRobot(r robot.Robot, name string) (PoseTracker, error) {
 	return robot.ResourceFromRobot[PoseTracker](r, Named(name))
-}
-
-// Readings is a helper for getting all readings from a PoseTracker.
-func Readings(ctx context.Context, poseTracker PoseTracker) (map[string]interface{}, error) {
-	poseLookup, err := poseTracker.Poses(ctx, []string{}, map[string]interface{}{})
-	if err != nil {
-		return nil, err
-	}
-	result := map[string]interface{}{}
-	for bodyName, poseInFrame := range poseLookup {
-		result[bodyName] = poseInFrame
-	}
-	return result, nil
 }
