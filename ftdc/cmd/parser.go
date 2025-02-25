5050187048f7739fb5ce73c486184f1ab8449f47
Dan Gottlieb <dgottlieb@users.noreply.github.com>
2025-02-25 15:28:37 -0500

    RSDK-10079: Add event command to FTDC parsing. (#4813)
    Co-authored-by: Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>

--- ftdc/cmd/parser.go
+++ ftdc/cmd/parser.go
@@ -10,6 +10,7 @@ import (
 	"math"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"slices"
 	"strings"
 	"time"
@@ -19,6 +20,14 @@ import (
 	"go.viam.com/rdk/ftdc"
 	"go.viam.com/rdk/logging"
 )
+
+// graphInfo points to an OS file containing the data points for a single graph. We also record the
+// min/max values for scaling purposes when generating plots.
+type graphInfo struct {
+	file   *os.File
+	minVal int64
+	maxVal int64
+}
 
 // gnuplotWriter organizes all of the output for `gnuplot` to create a graph from FTDC
 // data. Notably:
@@ -30,12 +39,18 @@ import (
 //   - Each graph will have the same bounds on the X (Time) axis. Scanning vertically through the
 //     graphs at the same horizontal position will show readings as of a common point in time.
 type gnuplotWriter struct {
-	// metricFiles contain the actual data points to be graphed. A "top level" gnuplot will
+	// metricFiles contains the actual data points to be graphed. The map key is a metric name and
+	// the value encapsulates the file with that metrics' datapoints. In addition to other metadata
+	// for plotting that data (e.g: min/max values for scaling). A "top level" gnuplot will
 	// reference them.
-	metricFiles map[string]*os.File
+	metricFiles map[string]*graphInfo
 
+	// tempdir is a temporary directory for writing out all of the files that gnuplot will use to
+	// create a graph. This is expected to be of the form `/tmp/ftdc<random digits>`.
 	tempdir string
 
+	// options is a set of user-input graphOptions. Such as when they ask for a specific time slice
+	// of data.
 	options graphOptions
 }
 
@@ -69,12 +84,25 @@ type graphOptions struct {
 	// timestamp. The default is all datapoints (minTimeSeconds: 0, maxTimeSeconds: MaxInt64).
 	minTimeSeconds int64
 	maxTimeSeconds int64
+
+	// hideAllZeroes will omit graphs where all values are 0. Generally speaking, graphs that only
+	// contain zeroes won't correlate to anything interesting. A lot of graphs (e.g: component
+	// `State`s) are all zeroes. Avoid graphing them to help users navigate the data that's more
+	// interesting.
+	hideAllZeroes bool
+
+	// vertLinesAtSeconds will draw a vertical line for each element. The items are expected to be
+	// in units of seconds since the epoch. These are to represent "events" that are of interest to a
+	// user (where the user would like to find correlations in other metrics.)
+	vertLinesAtSeconds []int64
 }
 
 func defaultGraphOptions() graphOptions {
 	return graphOptions{
-		minTimeSeconds: 0,
-		maxTimeSeconds: math.MaxInt64,
+		minTimeSeconds:     0,
+		maxTimeSeconds:     math.MaxInt64,
+		hideAllZeroes:      true,
+		vertLinesAtSeconds: make([]int64, 0),
 	}
 }
 
@@ -82,6 +110,14 @@ func nolintPrintln(str ...any) {
 	// This is a CLI. It's acceptable to output to stdout.
 	//nolint:forbidigo
 	fmt.Println(str...)
+}
+
+// write is a wrapper for Fprint that panics on any error.
+func write(toWrite io.Writer, str string) {
+	_, err := fmt.Fprint(toWrite, str)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // writeln is a wrapper for Fprintln that panics on any error.
@@ -97,6 +133,11 @@ func writelnf(toWrite io.Writer, formatStr string, args ...any) {
 	writeln(toWrite, fmt.Sprintf(formatStr, args...))
 }
 
+// writef will string format the latter arguments and call write.
+func writef(toWrite io.Writer, formatStr string, args ...any) {
+	write(toWrite, fmt.Sprintf(formatStr, args...))
+}
+
 func newGnuPlotWriter(graphOptions graphOptions) *gnuplotWriter {
 	tempdir, err := os.MkdirTemp("", "ftdc_parser")
 	if err != nil {
@@ -104,24 +145,28 @@ func newGnuPlotWriter(graphOptions graphOptions) *gnuplotWriter {
 	}
 
 	return &gnuplotWriter{
-		metricFiles: make(map[string]*os.File),
+		metricFiles: make(map[string]*graphInfo),
 		tempdir:     tempdir,
 		options:     graphOptions,
 	}
 }
 
-func (gpw *gnuplotWriter) getDatafile(metricName string) io.Writer {
-	if datafile, created := gpw.metricFiles[metricName]; created {
-		return datafile
+func (gpw *gnuplotWriter) getGraphInfo(metricName string) *graphInfo {
+	if gi, created := gpw.metricFiles[metricName]; created {
+		return gi
 	}
 
 	datafile, err := os.CreateTemp(gpw.tempdir, "")
 	if err != nil {
 		panic(err)
 	}
-	gpw.metricFiles[metricName] = datafile
 
-	return datafile
+	ret := &graphInfo{
+		file: datafile,
+	}
+	gpw.metricFiles[metricName] = ret
+
+	return ret
 }
 
 func (gpw *gnuplotWriter) addPoint(timeSeconds int64, metricName string, metricValue float32) {
@@ -129,7 +174,12 @@ func (gpw *gnuplotWriter) addPoint(timeSeconds int64, metricName string, metricV
 		return
 	}
 
-	writelnf(gpw.getDatafile(metricName), "%v %.5f", timeSeconds, metricValue)
+	// While we're adding points, track the min/max values we saw. This can be used to better scale
+	// graphs. As we've found gnuplots auto scaling to be a bit clunky.
+	gi := gpw.getGraphInfo(metricName)
+	gi.minVal = min(gi.minVal, int64(metricValue))
+	gi.maxVal = max(gi.maxVal, int64(metricValue))
+	writelnf(gi.file, "%v %.5f", timeSeconds, metricValue)
 }
 
 // ratioMetric describes which two FTDC metrics that should be combined to create a computed
@@ -160,6 +210,13 @@ var ratioMetricToFields = map[string]ratioMetric{
 	"RxPacketsPerSec":        {"RxPackets", ""},
 	"TxBytesPerSec":          {"TxBytes", ""},
 	"RxBytesPerSec":          {"RxBytes", ""},
+
+	// Dan: Just tacking these on -- omitted metrics from this list does not mean they shouldn't* be
+	// here. Also, personally, sometimes I think not* doing PerSec for these can also be
+	// useful. Maybe we should consider including both the raw and rate graphs. Instead of replacing
+	// the raw values with a rate graph.
+	"GetImagePerSec":    {"GetImage", ""},
+	"GetReadingsPerSec": {"GetReadings", ""},
 }
 
 // ratioReading is a reading of two metrics described by `ratioMetric`. This is what will be graphed.
@@ -353,9 +410,15 @@ func (gpw *gnuplotWriter) CompileAndClose() string {
 	}
 	defer utils.UncheckedErrorFunc(gnuFile.Close)
 
-	// Write a png with width of 1000 pixels and 200 pixels of height per metric/graph.
-	writelnf(gnuFile, "set term png size %d, %d", 1000, 200*len(gpw.metricFiles))
+	// Write a png with width of 1000 pixels and 200 pixels of height per metric/graph. This works
+	// well for small numbers of graphs, but for large numbers, we get a bunch of extra whitespace
+	// at the top/bottom. Adding `crop` will trim that whitespace.
+	writelnf(gnuFile, "set term png size %d, %d crop", 1000, 200*len(gpw.metricFiles))
 
+	// Log the tempdir in case one wants to go back and see/edit how the graph was generated. A user
+	// can rerun `gnuplot /<tmpdir>/main<unique value>` to recreate `plot.png` with the new
+	// settings/data.
+	nolintPrintln("Gnuplot dir:", gpw.tempdir)
 	nolintPrintln("Output file: `plot.png`")
 	// The output filename
 	writeln(gnuFile, "set output 'plot.png'")
@@ -370,22 +433,107 @@ func (gpw *gnuplotWriter) CompileAndClose() string {
 
 	//  Axis labeling/formatting/type information.
 	writeln(gnuFile, "set timefmt '%s'")
+
+	// This omits the month-day-year. Probably worth adding in for whoever researches how to do it.
 	writeln(gnuFile, "set format x '%H:%M:%S'")
 	writeln(gnuFile, "set xlabel 'Time'")
 	writeln(gnuFile, "set xdata time")
 
-	// FTDC does not have negative numbers, so start the Y-axis at 0. Except that some metrics may
-	// want to be negative like position or voltages? Revisit if this can be more granular as a
-	// per-graph setting rather than a global.
-	writeln(gnuFile, "set yrange [0:*]")
+	allZeroesHidden := 0
+	var minY int64
+	var maxY int64
 
+	// For each metric file, we will output a single gnuplot `plot` directive that will generate a
+	// single graph.
 	for _, nameFilePair := range sorted(gpw.metricFiles) {
-		metricName, file := nameFilePair.Key, nameFilePair.Val
-		writelnf(gnuFile, "plot '%v' using 1:2 with lines linestyle 7 lw 4 title '%v'", file.Name(), strings.ReplaceAll(metricName, "_", "\\_"))
-		utils.UncheckedErrorFunc(file.Close)
+		metricName, graphInfo := nameFilePair.Key, nameFilePair.Val
+		if gpw.options.hideAllZeroes && graphInfo.minVal == 0 && graphInfo.maxVal == 0 {
+			allZeroesHidden++
+			continue
+		}
+
+		// The minY/maxY values start at 0. So this the range from minY -> maxY will fall into one
+		// of the three buckets (in order of likelihood):
+		//
+		// - [0, positive number]
+		// - [negative number, positive number]
+		// - [negative number, 0]
+		minY = min(minY, graphInfo.minVal)
+		maxY = max(maxY, graphInfo.maxVal)
+
+		// If we're graphing something that has the same value for all readings, bump the yrange to
+		// avoid gnuplot complaints. E.g: `set yrange [0:0]` turns into `set yrange [0:1]`.
+		if graphInfo.minVal == graphInfo.maxVal {
+			graphInfo.maxVal = graphInfo.minVal + 1
+		}
+
+		// Set the lower/upper limits on the Y-axis for the graph. We use 2* maxVal to allow extra
+		// headroom for writing out the legend.
+		writelnf(gnuFile, "set yrange [%v:%v]", graphInfo.minVal, 2*float64(graphInfo.maxVal))
+
+		//nolint
+		// We write the plot line without a trailing newline. In case we want to add the vertical
+		// lines. An example output for the following writes might be:
+		// plot '/tmp/ftdc_parser1500397090/3060093295' using 1:2 with lines linestyle 7 lw 4 title 'rdk-internal:service:web/builtin.ResStats.RPCServer.WebRTCGrpcStats.CallTicketsAvailable',\
+		// 		'/tmp/ftdc_parser1500397090/vert-0.txt' using 1:2 with lines linestyle 6 lw 4 title '2025-02-22 21:13:00 +0000 UTC',\
+		// 		'/tmp/ftdc_parser1500397090/vert-1.txt' using 1:2 with lines linestyle 6 lw 4 title '2025-02-22 21:13:40 +0000 UTC'
+		//
+		//
+		// linestyle 7 is red, 6 is blue, lw is line-width (or weight) -- makes it thicker. The
+		// title is what's used in the legend.
+		writef(gnuFile, "plot '%v' using 1:2 with lines linestyle 7 lw 4 title '%v'", graphInfo.file.Name(), strings.ReplaceAll(metricName, "_", "\\_"))
+
+		// "vertical lines" for events are rendered as another set of data points for a
+		// `plot`. Because the vertical lines are at the same x-value/time for each graph, we can
+		// re-use the same file at a pre-determined name. These files will be written out next after
+		// we've accumulated all of the min/max Y values.
+		for idx, vertLineX := range gpw.options.vertLinesAtSeconds {
+			writeln(gnuFile, ",\\")
+			writef(gnuFile,
+				"\t'%v' using 1:2 with lines linestyle 6 lw 4 title '%v'",
+				filepath.Join(gpw.tempdir, fmt.Sprintf("vert-%d.txt", idx)),
+				time.Unix(vertLineX, 0).UTC())
+		}
+
+		// The trailing newline for the above calls to write out a single plot.
+		writeln(gnuFile, "")
+
+		utils.UncheckedErrorFunc(graphInfo.file.Close)
+	}
+	if allZeroesHidden > 0 {
+		nolintPrintln("Hid metrics that only had 0s for data. Cnt:", allZeroesHidden)
+		// Dan: perhaps add a command to disable this. E.g:
+		//   nolintPrintln("Use `set show-all-zeroes 1` to show them.")
+	}
+
+	// Actually write out the `vert-<number>.txt` plots.
+	for idx, vertLineX := range gpw.options.vertLinesAtSeconds {
+		vertFile, err := os.Create(filepath.Join(gpw.tempdir, fmt.Sprintf("vert-%v.txt", idx)))
+		defer utils.UncheckedErrorFunc(vertFile.Close)
+		if err != nil {
+			panic(err)
+		}
+
+		// For same reason, trying to graph two points (time, -inf) -> (time, +inf) does not
+		// render. So we instead write (time, -inf) -> (time, 0) -> (time, +inf). Which does work...
+		writelnf(vertFile, "%v %d", vertLineX, minY)
+		writelnf(vertFile, "%v 0", vertLineX)
+		writelnf(vertFile, "%v %d", vertLineX, maxY)
 	}
 
 	return gnuFile.Name()
+}
+
+func parseStringAsTime(inp string) (time.Time, error) {
+	goTime, err := time.Parse("2006-01-02T15:04:05", inp)
+	if err != nil {
+		// This is a CLI. It's acceptable to output to stdout.
+		//nolint:forbidigo
+		fmt.Printf("Error parsing start time. Working example: `2024-09-24T18:00:00` Inp: %q Err: %v\n", inp, err)
+		return time.Time{}, err
+	}
+
+	return goTime, nil
 }
 
 func main() {
@@ -460,31 +608,29 @@ func main() {
 			if start == "start" {
 				graphOptions.minTimeSeconds = 0
 			} else {
-				goTime, err := time.Parse("2006-01-02T15:04:05", start)
-				if err != nil {
-					// This is a CLI. It's acceptable to output to stdout.
-					//nolint:forbidigo
-					fmt.Printf("Error parsing start time. Working example: `2024-09-24T18:00:00` Inp: %q Err: %v\n", start, err)
-					continue
+				if goTime, err := parseStringAsTime(start); err == nil {
+					// parseStringAsTime outputs an error message for us.
+					graphOptions.minTimeSeconds = goTime.Unix()
 				}
-				graphOptions.minTimeSeconds = goTime.Unix()
 			}
 
 			if end == "end" {
 				graphOptions.maxTimeSeconds = math.MaxInt64
 			} else {
-				goTime, err := time.Parse("2006-01-02T15:04:05", end)
-				if err != nil {
-					// This is a CLI. It's acceptable to output to stdout.
-					//nolint:forbidigo
-					fmt.Printf("Error parsing end time. Working example: `2024-09-24T18:00:00` Inp: %q Err: %v\n", end, err)
-					continue
+				if goTime, err := parseStringAsTime(end); err == nil {
+					// parseStringAsTime outputs an error message for us.
+					graphOptions.maxTimeSeconds = goTime.Unix()
 				}
-				graphOptions.maxTimeSeconds = goTime.Unix()
 			}
 		case strings.HasPrefix(cmd, "reset range"):
 			graphOptions.minTimeSeconds = 0
 			graphOptions.maxTimeSeconds = math.MaxInt64
+		case strings.HasPrefix(cmd, "ev ") || strings.HasPrefix(cmd, "event "):
+			pieces := strings.SplitN(cmd, " ", 2)
+			if goTime, err := parseStringAsTime(pieces[1]); err == nil {
+				// parseStringAsTime outputs an error message for us.
+				graphOptions.vertLinesAtSeconds = append(graphOptions.vertLinesAtSeconds, goTime.Unix())
+			}
 		case cmd == "refresh" || cmd == "r":
 			nolintPrintln("Refreshing graphs with new data")
 		case len(cmd) == 0:
