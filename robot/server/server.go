926d74336b5ebc20b5a765c73322c820e7db71bd
Benji Rewis <benji.rewis@viam.com>
2025-02-26 11:29:41 -0500

    block comment to satisy all-mighty lint

--- robot/server/server.go
+++ robot/server/server.go
@@ -75,21 +75,22 @@ func (s *Server) Tunnel(srv pb.RobotService_TunnelServer) error {
 
 	// TODO(RSDK-5763): Start rejecting requests to unavailable ports once `app` has been
 	// updated to propagate `traffic_tunnel_endpoints` configs.
-	//
-	// var destAllowed bool
-	//// Ensure destination port is available; otherwise error.
-	// for _, tte := range s.robot.ListTunnels() {
-	// if int(req.DestinationPort) == tte.Port {
-	// destAllowed = true
-	// if tte.ConnectionTimeout != 0 {
-	// dialTimeout = tte.ConnectionTimeout
-	//}
-	//break
-	//}
-	//}
-	//if !destAllowed {
-	//return fmt.Errorf("tunnel not available at port %d", req.DestinationPort)
-	//}
+	/*
+	   var destAllowed bool
+	  // Ensure destination port is available; otherwise error.
+	   for _, tte := range s.robot.ListTunnels() {
+	   if int(req.DestinationPort) == tte.Port {
+	   destAllowed = true
+	   if tte.ConnectionTimeout != 0 {
+	   dialTimeout = tte.ConnectionTimeout
+	  }
+	   break
+	  }
+	  }
+	   if !destAllowed {
+	  return fmt.Errorf("tunnel not available at port %d", req.DestinationPort)
+	  }
+	*/
 
 	dest := strconv.Itoa(int(req.DestinationPort))
 
