eec09b7b8a8cf92db0731cc3e0d4c5a790a60732
purplenicole730 <jihyun.nicole.jung@gmail.com>
2025-03-05 12:39:33 -0500

    use try except

--- cli/module_generate/_templates/python/src/tmpl-main.py
+++ cli/module_generate/_templates/python/src/tmpl-main.py
@@ -2,8 +2,10 @@ import asyncio
 import os
 import sys
 from viam.module.module import Module
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from models.{{ .ModelSnake }} import {{ .ModelPascal }}
+try:
+    from models.hello_sensor import HelloSensor
+except ModuleNotFoundError:
+    from .models.hello_sensor import HelloSensor
 
 
 if __name__ == '__main__':
