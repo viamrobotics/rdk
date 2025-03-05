90407f9dde58d31ff1a529dee94429eda455d1a0
purplenicole730 <jihyun.nicole.jung@gmail.com>
2025-03-05 12:41:33 -0500

    add comment

--- cli/module_generate/_templates/python/src/tmpl-main.py
+++ cli/module_generate/_templates/python/src/tmpl-main.py
@@ -5,6 +5,7 @@ from viam.module.module import Module
 try:
     from models.hello_sensor import HelloSensor
 except ModuleNotFoundError:
+    # when running as local module with run.sh
     from .models.hello_sensor import HelloSensor
 
 
