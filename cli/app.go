6ba4d87a04defd58ebe96c85e64f614b4ebad63d
JosephBorodach <josephborodach@gmail.com>
2025-03-14 13:23:43 -0400

    Resolve generalFlagStart usage lint length

--- cli/app.go
+++ cli/app.go
@@ -1909,8 +1909,9 @@ var app = &cli.App{
 							Usage: "filter logs by levels (e.g., info, warn, error)",
 						},
 						&cli.StringFlag{
-							Name:  generalFlagStart,
-							Usage: "ISO-8601 timestamp in RFC3339 format indicating the start of the interval filter (e.g., 2025-01-15T14:00:00Z). Defaults to 12 hours ago if not specified",
+							Name: generalFlagStart,
+							Usage: "ISO-8601 timestamp in RFC3339 format indicating the start of the interval filter (e.g., 2025-01-15T14:00:00Z). " +
+								"Defaults to 12 hours ago if not specified",
 						},
 						&cli.StringFlag{
 							Name:  generalFlagEnd,
