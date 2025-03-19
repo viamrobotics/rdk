3961b0f11a483ad2720c4819d4f314a97523f484
Abe Winter <abe-winter@users.noreply.github.com>
2025-03-19 13:48:38 -0400

    enable image_file camera on no_cgo builds

--- components/camera/register/register_cgo_android.go
+++ components/camera/register/register_cgo_android.go
@@ -1,7 +1,10 @@
+//go:build !no_cgo || android
+
 // Package register registers all relevant cameras and also API specific functions
 package register
 
 import (
 	// for cameras.
 	_ "go.viam.com/rdk/components/camera/fake"
+	_ "go.viam.com/rdk/components/camera/transformpipeline"
 )
