0d2cbf6824ade20e949f7ec70c250a004f0d718d
JosephBorodach <josephborodach@gmail.com>
2025-03-19 09:20:21 -0400

    Move 'Defaults to 1 day ago if not specified' from Usage to DefaultText

--- cli/app.go
+++ cli/app.go
@@ -1909,9 +1909,9 @@ var app = &cli.App{
 							Usage: "filter logs by levels (e.g., info, warn, error)",
 						},
 						&cli.StringFlag{
-							Name: generalFlagStart,
-							Usage: "ISO-8601 timestamp in RFC3339 format indicating the start of the interval filter (e.g., 2025-01-15T14:00:00Z). " +
-								"Defaults to 1 day ago if not specified",
+							Name:        generalFlagStart,
+							Usage:       "ISO-8601 timestamp in RFC3339 format indicating the start of the interval filter (e.g., 2025-01-15T14:00:00Z)",
+							DefaultText: "1 day ago",
 						},
 						&cli.StringFlag{
 							Name:  generalFlagEnd,
