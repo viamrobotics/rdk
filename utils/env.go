055c1d91a4b4436bec8e90c166d22ba5f7ba39a2
Abe Winter <abe.winter@viam.com>
2025-03-28 14:27:17 -0400

    restore VIAM_TCP_SOCKETS but only when requested (no longer mandated on goos=win)

--- utils/env.go
+++ utils/env.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"slices"
 	"strings"
 	"time"
 
@@ -121,9 +122,7 @@ func ViamTCPSockets() bool {
 	// note: unix sockets have been supported on windows for a while, but go-grpc does not support them.
 	// 2017 support announcement: https://devblogs.microsoft.com/commandline/af_unix-comes-to-windows/
 	// go grpc client bug on win: https://github.com/dotnet/aspnetcore/issues/47043
-	return false
-	// return runtime.GOOS == "windows" ||
-	// 	slices.Contains(EnvTrueValues, os.Getenv("VIAM_TCP_SOCKETS"))
+	return slices.Contains(EnvTrueValues, os.Getenv("VIAM_TCP_SOCKETS"))
 }
 
 // LogViamEnvVariables logs the list of viam environment variables in [os.Environ] along with the env passed in.
