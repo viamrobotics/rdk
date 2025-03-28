6a9c88afde7473689023c68f5e78eb4239276170
Cheuk Tse <cheuk@viam.com>
2025-03-28 18:05:15 -0400

    reduce

--- robot/impl/local_robot.go
+++ robot/impl/local_robot.go
@@ -1058,6 +1058,9 @@ func dialRobotClient(
 		rOpts = append(rOpts, client.WithReconnectEvery(config.ReconnectInterval))
 	}
 
+	// only dial once per reconfiguration cycle, any failures will be retried on a ticker anyway
+	rOpts = append(rOpts, client.WithInitialDialAttempts(1))
+
 	robotClient, err := client.New(
 		ctx,
 		config.Address,
