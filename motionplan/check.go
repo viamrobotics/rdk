e4eed569cab4432ef524057c684e8ddf13aa3cb4
Ray Bjorkman <raymond@viam.com>
2025-04-15 12:34:27 -0400

    RSDK-10371 provide more helpful error messages when IK fails to produce a solution (#4904)

--- motionplan/check.go
+++ motionplan/check.go
@@ -270,9 +270,10 @@ func checkSegmentsFS(sfPlanner *planManager, segments []*ik.SegmentFS, lookAhead
 			if lastValid != nil {
 				checkConf = lastValid.EndConfiguration
 			}
-			ok, reason := sfPlanner.planOpts.CheckStateFSConstraints(&ik.StateFS{Configuration: checkConf, FS: sfPlanner.fs})
-			if !ok {
-				reason = " reason: " + reason
+			var reason string
+			err := sfPlanner.planOpts.CheckStateFSConstraints(&ik.StateFS{Configuration: checkConf, FS: sfPlanner.fs})
+			if err != nil {
+				reason = " reason: " + err.Error()
 			} else {
 				reason = ""
 			}
@@ -354,12 +355,12 @@ func checkSegments(sfPlanner *planManager, segments []*ik.Segment, lookAheadDist
 			}
 
 			// Checks for collision along the interpolated route and returns a the first interpolated pose where a collision is detected.
-			if isValid, err := sfPlanner.planOpts.CheckStateConstraints(interpolatedState); !isValid {
-				return fmt.Errorf("found constraint violation or collision in segment between %v and %v at %v: %s",
+			if err := sfPlanner.planOpts.CheckStateConstraints(interpolatedState); err != nil {
+				return errors.Wrapf(err,
+					"found constraint violation or collision in segment between %v and %v at %v",
 					segment.StartPosition.Point(),
 					segment.EndPosition.Point(),
 					poseInPath.Point(),
-					err,
 				)
 			}
 		}
