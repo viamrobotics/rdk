d67e659de5957f86b886785738b15623ef5fe786
Ray Bjorkman <raymond@viam.com>
2025-05-13 14:12:15 -0400

    add back some very necessary code

--- motionplan/constraint.go
+++ motionplan/constraint.go
@@ -222,7 +222,7 @@ func NewCollisionConstraint(
 	reportDistances bool,
 	collisionBufferMM float64,
 ) (StateConstraint, error) {
-	zeroCG, err := setupZeroCG(moving, static, collisionSpecifications, reportDistances, collisionBufferMM)
+	zeroCG, err := setupZeroCG(moving, static, collisionSpecifications, true, collisionBufferMM)
 	if err != nil {
 		return nil, err
 	}
@@ -277,7 +277,7 @@ func NewCollisionConstraintFS(
 	reportDistances bool,
 	collisionBufferMM float64,
 ) (StateFSConstraint, error) {
-	zeroCG, err := setupZeroCG(moving, static, collisionSpecifications, reportDistances, collisionBufferMM)
+	zeroCG, err := setupZeroCG(moving, static, collisionSpecifications, true, collisionBufferMM)
 	if err != nil {
 		return nil, err
 	}
