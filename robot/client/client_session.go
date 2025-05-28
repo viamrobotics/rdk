fe524d4f3995e07ebb5215d0235c71d864a78ec0
Alden Hsu <alden.hsu@viam.com>
2025-05-28 13:19:59 +0000

    add isSafetyHeartbeatMonitored to check at runtime for safety_heartbeat_monitored RPC option

--- robot/client/client_session.go
+++ robot/client/client_session.go
@@ -2,16 +2,21 @@ package client
 
 import (
 	"context"
+	"strings"
 	"sync"
 	"time"
 
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	common "go.viam.com/api/common/v1"
 	pb "go.viam.com/api/robot/v1"
 	"go.viam.com/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
 
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/session"
@@ -165,8 +170,26 @@ func (rc *RobotClient) safetyMonitorFromHeaders(ctx context.Context, hdr metadat
 	}
 }
 
+// isSafetyHeartbeatMonitored checks if the provided RPC method has the safety_heartbeat_monitored option,
+// and if so, returns its bool value.
+func isSafetyHeartbeatMonitored(method string) bool {
+	// reformat "/viam.component.base.v1.BaseService/MoveStraight" -> "viam.component.base.v1.BaseService.MoveStraight"
+	method = strings.TrimPrefix(method, "/")
+	method = strings.ReplaceAll(method, "/", ".")
+	// err is NotFound if not present. We just need to return false in this case.
+	desc, err := protoregistry.GlobalFiles.FindDescriptorByName(protoreflect.FullName(method))
+	if err == nil {
+		methodOpts := desc.(protoreflect.MethodDescriptor).Options()
+		if proto.HasExtension(methodOpts, common.E_SafetyHeartbeatMonitored) {
+			return proto.GetExtension(methodOpts, common.E_SafetyHeartbeatMonitored).(bool)
+		}
+	}
+	return false
+}
+
 func (rc *RobotClient) useSessionInRequest(ctx context.Context, method string) bool {
-	return !rc.sessionsDisabled && !exemptFromSession[method] && ctx.Value(ctxKeyInSessionMDReq) == nil
+	return !rc.sessionsDisabled && !exemptFromSession[method] &&
+		ctx.Value(ctxKeyInSessionMDReq) == nil && isSafetyHeartbeatMonitored(method)
 }
 
 func (rc *RobotClient) sessionUnaryClientInterceptor(
