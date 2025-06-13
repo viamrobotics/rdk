209fcd70878e61452c2be6429ce4ea1d6227fc2a
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-13 15:38:32 -0400

    change to StoppableWorkers implementation, include timer reads based on frame rate

--- components/camera/videosource/webcam_buffer.go
+++ components/camera/videosource/webcam_buffer.go
@@ -1,36 +1,41 @@
 package videosource
 
 import (
+	"context"
 	"image"
 	"sync"
+	"time"
 
 	"github.com/pion/mediadevices/pkg/io/video"
+	goutils "go.viam.com/utils"
 
 	"go.viam.com/rdk/logging"
-	"go.viam.com/rdk/utils"
 )
 
-const SIZEOFBUFFER = 1
+const sizeOfBuffer = 1
 
 // WebcamBuffer is a buffer for webcam frames.
 type WebcamBuffer struct {
-	frames           []image.Image	// Holds the frames in the buffer
-	mu               sync.RWMutex	// Mutex to synchronize access to the buffer
-	currentIndex     int			// Index of the current frame we are accessing
-	stopChan         chan struct{}	// Channel to stop the buffer collection process
-	currentlyRunning bool 			// Checks if the buffer collection process is running
-	reader           video.Reader	// Reader to read frames from the webcam
-	logger           logging.Logger	// Logger for errors or debugging
+	frames           []image.Image  // Holds the frames in the buffer
+	mu               sync.RWMutex   // Mutex to synchronize access to the buffer
+	currentIndex     int            // Index of the current frame we are accessing
+	stopChan         chan struct{}  // Channel to stop the buffer collection process
+	currentlyRunning bool           // Checks if the buffer collection process is running
+	reader           video.Reader   // Reader to read frames from the webcam
+	logger           logging.Logger // Logger for errors or debugging
+	workers          *goutils.StoppableWorkers
+	frameRate        float32 // Frame rate in frames per second
 }
 
 // NewWebcamBuffer creates a new WebcamBuffer struct, for now, we make the buffer a size of 50 frames.
-func NewWebcamBuffer(reader video.Reader, logger logging.Logger) *WebcamBuffer {
+func NewWebcamBuffer(reader video.Reader, logger logging.Logger, frameRate float32) *WebcamBuffer {
 	return &WebcamBuffer{
-		frames:       make([]image.Image, SIZEOFBUFFER),
+		frames:       make([]image.Image, sizeOfBuffer),
 		currentIndex: 0,
 		stopChan:     make(chan struct{}),
 		reader:       reader,
 		logger:       logger,
+		frameRate:    frameRate,
 	}
 }
 
@@ -45,7 +50,35 @@ func (wb *WebcamBuffer) StartBuffer() {
 	}
 
 	wb.currentlyRunning = true
-	go wb.CollectFrames()
+
+	// Calculate ticker duration based on frame rate
+	duration := time.Duration(float64(time.Second) / float64(wb.frameRate))
+	ticker := time.NewTicker(duration)
+	defer ticker.Stop()
+
+	wb.workers.Add(func(ctx context.Context) {
+		for {
+			select {
+			case <-wb.stopChan:
+				return
+			case <-ticker.C:
+				img, release, err := wb.reader.Read()
+				if err != nil {
+					wb.logger.Errorf("error reading frame: %v", err)
+					continue
+				}
+
+				wb.mu.Lock()
+				wb.frames[wb.currentIndex] = img
+				wb.currentIndex = (wb.currentIndex + 1) % sizeOfBuffer
+				wb.mu.Unlock()
+
+				if release != nil {
+					release()
+				}
+			}
+		}
+	})
 }
 
 // StopBuffer stops the buffer collection process.
@@ -53,36 +86,9 @@ func (wb *WebcamBuffer) StopBuffer() {
 	wb.mu.Lock()
 	defer wb.mu.Unlock()
 
-	if !wb.currentlyRunning {
-		return
-	}
-
-	close(wb.stopChan)
-	wb.currentlyRunning = false
-}
-
-// CollectFrames collects frames from the reader and adds them to the buffer. This function is called in a goroutine.
-func (wb *WebcamBuffer) CollectFrames() {
-	for {
-		select {
-		case <-wb.stopChan:
-			return
-		default:
-			img, release, err := wb.reader.Read()
-			if err != nil {
-				wb.logger.Errorf("error reading frame: %v", err)
-				continue
-			}
-
-			wb.mu.Lock()
-			wb.frames[wb.currentIndex] = img
-			wb.currentIndex = (wb.currentIndex + 1) % wb.sizeOfBuffer
-			wb.mu.Unlock()
-
-			if release != nil {
-				release()
-			}
-		}
+	if wb.workers != nil {
+		wb.workers.Stop()
+		wb.currentlyRunning = false
 	}
 }
 
@@ -92,7 +98,7 @@ func (wb *WebcamBuffer) GetLatestFrame() image.Image {
 	defer wb.mu.Unlock()
 
 	if wb.currentIndex == 0 {
-		return wb.frames[wb.sizeOfBuffer-1]
+		return wb.frames[sizeOfBuffer-1]
 	}
 
 	return wb.frames[wb.currentIndex-1]
