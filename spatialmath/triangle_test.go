9713aa61a456a5e629c61ed0342ad3c4a51e64b8
Emiyare Ikwut-Ukwa <138727724+e-ikwut@users.noreply.github.com>
2025-06-18 15:00:16 -0400

    closest inside point tests

--- spatialmath/triangle_test.go
+++ spatialmath/triangle_test.go
@@ -37,4 +37,36 @@ func TestBasicTriangleFunctions(t *testing.T) {
 			test.That(t, tri2.Points()[i], test.ShouldResemble, NewPoint(expectedPts[i], "").Transform(tf).Pose().Point())
 		}
 	})
+
+	t.Run("closest triangle inside point", func(t *testing.T) {
+		// interior
+		closestPoint, isInside := closestTriangleInsidePoint(tri, r3.Vector{1, 1, 1})
+		test.That(t, closestPoint, test.ShouldResemble, r3.Vector{1, 1, 0})
+		test.That(t, isInside, test.ShouldResemble, true)
+
+		// directly above edge
+		closestPoint, isInside = closestTriangleInsidePoint(tri, r3.Vector{2, 0, 1})
+		test.That(t, closestPoint, test.ShouldResemble, r3.Vector{2, 0, 0})
+		test.That(t, isInside, test.ShouldResemble, true)
+
+		// directly above vertex
+		closestPoint, isInside = closestTriangleInsidePoint(tri, r3.Vector{0, 3, 1})
+		test.That(t, closestPoint, test.ShouldResemble, r3.Vector{0, 3, 0})
+		test.That(t, isInside, test.ShouldResemble, true)
+
+		// outside (obtuse)
+		closestPoint, isInside = closestTriangleInsidePoint(tri, r3.Vector{1, -1, 1})
+		test.That(t, isInside, test.ShouldResemble, false)
+
+		// outside (straight)
+		closestPoint, isInside = closestTriangleInsidePoint(tri, r3.Vector{0, 4, 0})
+		test.That(t, isInside, test.ShouldResemble, false)
+
+		// interior, an isoceles right triangle rotated off the xy-plane; numbers large to keep integers (floats hit rounding error)
+		rotatedPts := []r3.Vector{{0, 0, 0}, {50, 0, 0}, {0, 30, 40}}
+		rotatedTri := NewTriangle(rotatedPts[0], rotatedPts[1], rotatedPts[2])
+		closestPoint, isInside = closestTriangleInsidePoint(rotatedTri, r3.Vector{1, 3 + 4, 4 - 3})
+		test.That(t, closestPoint, test.ShouldResembleProto, r3.Vector{1, 3, 4})
+		test.That(t, isInside, test.ShouldResemble, true)
+	})
 }
