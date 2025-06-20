d0dc4fa1ad8256b9f28bdb8ecbdcfd377848db28
Emerald <emerald.zhang@viam.com>
2025-06-20 16:33:51 -0400

    powersensor code

--- components/powersensor/collectors.go
+++ components/powersensor/collectors.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	pb "go.viam.com/api/component/powersensor/v1"
-	"google.golang.org/protobuf/types/known/anypb"
-
 	"go.viam.com/rdk/data"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type method int64
@@ -155,6 +155,50 @@ func newReadingsCollector(resource interface{}, params data.CollectorParams) (da
 		}
 		ts := data.Timestamps{TimeRequested: timeRequested, TimeReceived: time.Now()}
 		return data.NewTabularCaptureResultReadings(ts, values)
+	})
+	return data.NewCollector(cFunc, params)
+}
+
+// newDoCommandCollector returns a collector to register a doCommand action. If one is already registered
+// with the same MethodMetadata it will panic.
+func newDoCommandCollector(resource interface{}, params data.CollectorParams) (data.Collector, error) {
+	ps, err := assertPowerSensor(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]*anypb.Any) (data.CaptureResult, error) {
+		timeRequested := time.Now()
+		var res data.CaptureResult
+
+		var payload map[string]interface{}
+
+		if payloadAny, exists := params.MethodParams["docommand_input"]; exists && payloadAny != nil {
+			if payloadAny.MessageIs(&structpb.Struct{}) {
+				var s structpb.Struct
+				if err := payloadAny.UnmarshalTo(&s); err != nil {
+					return res, err
+				}
+				payload = s.AsMap()
+			} else {
+				// handle empty payload
+				payload = make(map[string]interface{})
+			}
+		} else {
+			// key does not exist
+			return res, errors.New("missing payload")
+		}
+
+		values, err := ps.DoCommand(ctx, payload)
+
+		if err != nil {
+			if errors.Is(err, data.ErrNoCaptureToStore) {
+				return res, err
+			}
+			return res, data.NewFailedToReadError(params.ComponentName, "DoCommand", err)
+		}
+		ts := data.Timestamps{TimeRequested: timeRequested, TimeReceived: time.Now()}
+		return data.NewTabularCaptureResult(ts, values)
 	})
 	return data.NewCollector(cFunc, params)
 }
