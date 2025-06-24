f543f1bc6817aae1f8c8c6f4d8a8139a3b173ec1
Emerald <emerald.zhang@viam.com>
2025-06-24 12:19:16 -0400

    slam

--- services/slam/collectors.go
+++ services/slam/collectors.go
@@ -2,13 +2,14 @@ package slam
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	pb "go.viam.com/api/service/slam/v1"
-	"google.golang.org/protobuf/types/known/anypb"
-
 	"go.viam.com/rdk/data"
 	"go.viam.com/rdk/spatialmath"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type method int64
@@ -16,6 +17,7 @@ type method int64
 const (
 	position method = iota
 	pointCloudMap
+	doCommand
 )
 
 func (m method) String() string {
@@ -24,6 +26,9 @@ func (m method) String() string {
 	}
 	if m == pointCloudMap {
 		return "PointCloudMap"
+	}
+	if m == doCommand {
+		return "DoCommand"
 	}
 	return "Unknown"
 }
@@ -75,6 +80,49 @@ func newPointCloudMapCollector(resource interface{}, params data.CollectorParams
 			Payload:  pcd,
 			MimeType: data.MimeTypeApplicationPcd,
 		}}), nil
+	})
+	return data.NewCollector(cFunc, params)
+}
+
+// newDoCommandCollector returns a collector to register a doCommand action. If one is already registered
+// with the same MethodMetadata it will panic.
+func newDoCommandCollector(resource interface{}, params data.CollectorParams) (data.Collector, error) {
+	slam, err := assertSLAM(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	cFunc := data.CaptureFunc(func(ctx context.Context, _ map[string]*anypb.Any) (data.CaptureResult, error) {
+		timeRequested := time.Now()
+		var res data.CaptureResult
+
+		var payload map[string]interface{}
+
+		if payloadAny, exists := params.MethodParams["docommand_input"]; exists && payloadAny != nil {
+			if payloadAny.MessageIs(&structpb.Struct{}) {
+				var s structpb.Struct
+				if err := payloadAny.UnmarshalTo(&s); err != nil {
+					return res, err
+				}
+				payload = s.AsMap()
+			} else {
+				// handle empty payload
+				payload = make(map[string]interface{})
+			}
+		} else {
+			// key does not exist
+			return res, errors.New("missing payload")
+		}
+
+		values, err := slam.DoCommand(ctx, payload)
+		if err != nil {
+			if errors.Is(err, data.ErrNoCaptureToStore) {
+				return res, err
+			}
+			return res, data.NewFailedToReadError(params.ComponentName, "DoCommand", err)
+		}
+		ts := data.Timestamps{TimeRequested: timeRequested, TimeReceived: time.Now()}
+		return data.NewTabularCaptureResult(ts, values)
 	})
 	return data.NewCollector(cFunc, params)
 }
--- services/slam/collectors_test.go
+++ services/slam/collectors_test.go
@@ -13,6 +13,8 @@ import (
 	"go.viam.com/test"
 	"go.viam.com/utils"
 	"go.viam.com/utils/artifact"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/rdk/data"
 	"go.viam.com/rdk/logging"
@@ -26,6 +28,8 @@ const (
 	serviceName     = "slam"
 	captureInterval = time.Millisecond
 )
+
+var doCommandMap = map[string]any{"readings": "random-test"}
 
 func TestCollectors(t *testing.T) {
 	pcdPath := filepath.Clean(artifact.MustPath("pointcloud/octagonspace.pcd"))
@@ -99,6 +103,93 @@ func TestCollectors(t *testing.T) {
 	}
 }
 
+func TestDoCommandCollector(t *testing.T) {
+	tests := []struct {
+		name         string
+		collector    data.CollectorConstructor
+		methodParams map[string]*anypb.Any
+		expectError  bool
+	}{
+		{
+			name:      "DoCommand collector should write a list of values",
+			collector: slam.NewDoCommandCollector,
+			methodParams: map[string]*anypb.Any{
+				"docommand_input": func() *anypb.Any {
+					structVal := tu.ToStructPBStruct(t, map[string]any{
+						"command": "random",
+					})
+					anyVal, _ := anypb.New(structVal)
+					return anyVal
+				}(),
+			},
+		},
+		{
+			name:      "DoCommand collector should handle empty struct payload",
+			collector: slam.NewDoCommandCollector,
+			methodParams: map[string]*anypb.Any{
+				"docommand_input": func() *anypb.Any {
+					emptyStruct := &structpb.Struct{
+						Fields: make(map[string]*structpb.Value),
+					}
+					anyVal, _ := anypb.New(emptyStruct)
+					return anyVal
+				}(),
+			},
+		},
+		{
+			name:      "DoCommand collector should handle empty payload",
+			collector: slam.NewDoCommandCollector,
+			methodParams: map[string]*anypb.Any{
+				"docommand_input": {},
+			},
+		},
+		{
+			name:         "DoCommand collector should error on missing payload",
+			collector:    slam.NewDoCommandCollector,
+			methodParams: map[string]*anypb.Any{},
+			expectError:  true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			start := time.Now()
+			buf := tu.NewMockBuffer(t)
+			params := data.CollectorParams{
+				DataType:      data.CaptureTypeTabular,
+				ComponentName: serviceName,
+				Interval:      captureInterval,
+				Logger:        logging.NewTestLogger(t),
+				Clock:         clock.New(),
+				Target:        buf,
+				MethodParams:  tc.methodParams,
+			}
+
+			pcdPath := filepath.Clean(artifact.MustPath("pointcloud/octagonspace.pcd"))
+			slam := newSlamService(pcdPath)
+			col, err := tc.collector(slam, params)
+			test.That(t, err, test.ShouldBeNil)
+
+			defer col.Close()
+			col.Collect()
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			if tc.expectError {
+				test.That(t, len(buf.Writes), test.ShouldEqual, 0)
+			} else {
+				tu.CheckMockBufferWrites(t, ctx, start, buf.Writes, []*datasyncpb.SensorData{{
+					Metadata: &datasyncpb.SensorMetadata{},
+					Data: &datasyncpb.SensorData_Struct{
+						Struct: tu.ToStructPBStruct(t, doCommandMap),
+					},
+				}})
+			}
+			buf.Close()
+		})
+	}
+}
+
 func getPointCloudMap(path string) (func() ([]byte, error), error) {
 	const chunkSizeBytes = 1 * 1024 * 1024
 	file, err := os.Open(path)
@@ -125,6 +216,10 @@ func newSlamService(pcdPath string) slam.Service {
 
 	v.PointCloudMapFunc = func(ctx context.Context, returnEditedMap bool) (func() ([]byte, error), error) {
 		return getPointCloudMap(pcdPath)
+	}
+
+	v.DoCommandFunc = func(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
+		return doCommandMap, nil
 	}
 
 	return v
