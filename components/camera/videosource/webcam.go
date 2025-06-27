9c23e155fad8d3924e156238026419938caec00a
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-27 15:20:09 -0400

    moved over webcambuffer to webcam.go

--- components/camera/videosource/webcam.go
+++ components/camera/videosource/webcam.go
@@ -514,6 +514,37 @@ func (c *webcam) Geometries(ctx context.Context, extra map[string]interface{}) (
 	return make([]spatialmath.Geometry, 0), nil
 }
 
+// NewWebcamBuffer creates a new WebcamBuffer struct.
+func NewWebcamBuffer(frameRate float32) *WebcamBuffer {
+	return &WebcamBuffer{
+		frames:       make([]FrameStruct, sizeOfBuffer),
+		currentIndex: 0,
+		frameRate:    frameRate,
+	}
+}
+
+// GetLatestFrame gets the latest frame from the buffer.
+func (wb *WebcamBuffer) GetLatestFrame() (image.Image, error) {
+	wb.mu.Lock()
+	defer wb.mu.Unlock()
+
+	var latestFrame FrameStruct
+	if wb.currentIndex == 0 {
+		latestFrame = wb.frames[sizeOfBuffer-1]
+	} else {
+		latestFrame = wb.frames[wb.currentIndex-1]
+	}
+
+	if latestFrame.img == nil {
+		if latestFrame.err != nil {
+			return nil, latestFrame.err
+		}
+		return nil, errors.New("no frames available to read")
+	}
+
+	return latestFrame.img, nil
+}
+
 // StartBuffer initiates the buffer collection process.
 func (c *webcam) startBuffer() {
 	c.mu.Lock()
