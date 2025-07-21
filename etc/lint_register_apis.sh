818ccd3d021f0611e2cecf9c4727d1283dd826a1
Josh Matthews <josh.matthews@viam.com>
2025-07-21 17:39:35 -0400

    APP-8910: Remove automatic imports of cgo libraries in modules (#5159)

--- etc/lint_register_apis.sh
+++ etc/lint_register_apis.sh
@@ -2,16 +2,25 @@
 # Ensures that all component and service APIs have a corresponding import line in 'rdk/[services|components]/register_apis/all.go'.
 
 pkgs=(components services)
+cgo_paths=(services/motion services/vision components/camera components/audioinput)
 
 for p in "${pkgs[@]}"; do
   pushd $p > /dev/null
   relevantDirs=$(grep -rl 'resource\.RegisterAPI' * | cut -d/ -f1)
   for d in $relevantDirs; do
     expectedImport="_ \"go.viam.com/rdk/$p/$d\""
-    if ! grep -q "\\s\\+$expectedImport" register_apis/*; then
-      echo "Missing expected import '$expectedImport' in 'rdk/$p/register_apis'"
-      exit 1
+    regexp="\\s\\+$expectedImport"
+    if [[ ${cgo_paths[@]} =~ "$p/$d" ]]; then
+      if grep -q "$regexp" register_apis/*; then
+        echo "Detected restricted cgo import in 'rdk/$p/register_apis'"
+        exit 1
+      fi
+      continue
     fi
+    # if ! grep -q "$regexp" register_apis/*; then
+    #   echo "Missing expected import '$expectedImport' in 'rdk/$p/register_apis'"
+    #   exit 1
+    # fi
   done
   popd > /dev/null
 done
