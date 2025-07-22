3092ecaad92500f51f1145b80fdbb6c05700b1d5
Bohdan Karavan <bohdan.karavan@viam.com>
2025-07-22 15:36:44 -0400

    moving the windows if check

--- robot/impl/local_robot.go
+++ robot/impl/local_robot.go
@@ -6,6 +6,7 @@ package robotimpl
 
 import (
 	"context"
+	"runtime"
 	"slices"
 	"sync"
 	"sync/atomic"
@@ -372,11 +373,16 @@ func newWithResources(
 		//   the web service has not been "started".
 		ftdcDir := ftdc.DefaultDirectory(utils.ViamDotDir, partID)
 		ftdcWorker = ftdc.NewWithUploader(ftdcDir, conn, partID, logger.Sublogger("ftdc"))
-		if statser, err := sys.NewSelfSysUsageStatser(); err == nil {
-			ftdcWorker.Add("proc.viam-server", statser)
-		}
-		if statser, err := sys.NewNetUsage(); err == nil {
-			ftdcWorker.Add("net", statser)
+		if runtime.GOOS == "windows" {
+			// note: this logs a panic on RDK start on windows.
+			logger.Debug("System FTDC metrics are not implemented on windows")
+		} else {
+			if statser, err := sys.NewSelfSysUsageStatser(); err == nil {
+				ftdcWorker.Add("proc.viam-server", statser)
+			}
+			if statser, err := sys.NewNetUsage(); err == nil {
+				ftdcWorker.Add("net", statser)
+			}
 		}
 	}
 
