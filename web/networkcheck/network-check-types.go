6ba6bbbba6121b652972eb7a6c7053b9ee9b5e7f
Benji Rewis <benji.rewis@viam.com>
2025-07-25 19:07:12 -0400

    Implement basic DNS testing for network checks (broken)

--- web/networkcheck/network-check-types.go
+++ web/networkcheck/network-check-types.go
@@ -2,32 +2,188 @@ package networkcheck
 
 import (
 	"fmt"
+	"strings"
 
 	"go.viam.com/rdk/logging"
 )
 
-// STUNResponse represents a response from a STUN server.
-type STUNResponse struct {
-	// STUNServerURL is the URL of the STUN server.
-	STUNServerURL string
+type (
+	// DNSTestType is an enumeration of test types
+	DNSTestType int
 
-	// TCPSourceAddress is the source address for the bind request if this was a TCP test.
-	// If it was a UDP test, it will be the same UDP source address for all UDP tests, and
-	// that value will be passed to `logSTUNResults`.
-	TCPSourceAddress *string
+	// STUNResponse represents a response from a STUN server.
+	STUNResponse struct {
+		// STUNServerURL is the URL of the STUN server.
+		STUNServerURL string
 
-	// STUNServerAddr is the resolved address of the STUN server.
-	STUNServerAddr *string
+		// TCPSourceAddress is the source address for the bind request if this was a TCP test.
+		// If it was a UDP test, it will be the same UDP source address for all UDP tests, and
+		// that value will be passed to `logSTUNResults`.
+		TCPSourceAddress *string
 
-	// BindResponseAddr is our address as reported by the STUN server.
-	BindResponseAddr *string
+		// STUNServerAddr is the resolved address of the STUN server.
+		STUNServerAddr *string
 
-	// Time taken to send bind request, receive bind response, and extract address. A vague
-	// measurement of RTT to the STUN server.
-	TimeToBindResponseMS *int64
+		// BindResponseAddr is our address as reported by the STUN server.
+		BindResponseAddr *string
 
-	// Any error received during STUN interactions.
-	ErrorString *string
+		// Time taken to send bind request, receive bind response, and extract address. A vague
+		// measurement of RTT to the STUN server.
+		TimeToBindResponseMS *int64
+
+		// Any error received during STUN interactions.
+		ErrorString *string
+	}
+
+	// DNSResult represents the result of a DNS resolution test.
+	DNSResult struct {
+		// TestType indicates the type of DNS test.
+		TestType DNSTestType
+
+		// Any error encountered during the test.
+		ErrorString *string
+
+		/* Fields populated in Connection tests below */
+
+		// DNS server being tested.
+		DNSServer *string
+
+		// Time taken to connect to DNS server.
+		ConnectTimeMS *int64
+
+		// Time taken to send query and receive response.
+		QueryTimeMS *int64
+
+		// Size of DNS response in bytes.
+		ResponseSize *int64
+
+		/* Fields populated in Resolution tests below */
+
+		// Hostname being resolved.
+		Hostname *string
+
+		// Resolved IP addresses (comma-separated).
+		ResolvedIPs *string
+
+		// Time taken to resolve the hostname.
+		ResolutionTimeMS *int64
+	}
+)
+
+const (
+	ConnectionDNSTestType DNSTestType = iota
+	ResolutionDNSTestType
+)
+
+// String stringifies a DNS test type.
+func (dtt DNSTestType) String() string {
+	switch dtt {
+	case ConnectionDNSTestType:
+		return "connection"
+	case ResolutionDNSTestType:
+		return "resolution"
+	default:
+		return "unknown"
+	}
+}
+
+func stringifyDNSResults(dnsResults []*DNSResult) string {
+	ret := "["
+
+	for i, dr := range dnsResults {
+		comma := ","
+		if i == 0 {
+			comma = ""
+		}
+
+		ret += fmt.Sprintf("%v{test_type: %s", comma, dr.TestType)
+		if dr.ErrorString != nil {
+			ret += fmt.Sprintf(", error_string: %v", *dr.ErrorString)
+		}
+
+		// Connection fields.
+		if dr.DNSServer != nil {
+			ret += fmt.Sprintf(", dns_server: %v", *dr.DNSServer)
+		}
+		if dr.ConnectTimeMS != nil {
+			ret += fmt.Sprintf(", connect_time_ms: %d", *dr.ConnectTimeMS)
+		}
+		if dr.QueryTimeMS != nil {
+			ret += fmt.Sprintf(", query_time_ms: %d", *dr.QueryTimeMS)
+		}
+		if dr.ResponseSize != nil {
+			ret += fmt.Sprintf(", response_size: %d", *dr.ResponseSize)
+		}
+
+		// Resolution fields.
+		if dr.Hostname != nil {
+			ret += fmt.Sprintf(", hostname: %v", *dr.Hostname)
+		}
+		if dr.ResolutionTimeMS != nil {
+			ret += fmt.Sprintf(", resolution_time_ms: %d", *dr.ResolutionTimeMS)
+		}
+		if dr.ResolvedIPs != nil {
+			ret += fmt.Sprintf(", resolved_ips: %v", *dr.ResolvedIPs)
+		}
+
+		ret += "}"
+	}
+
+	return ret + "]"
+}
+
+// Logs DNS test results.
+func logDNSResults(logger logging.Logger, dnsResults []*DNSResult) {
+	var successfulConnectionTests, totalConnectionTests int
+	var successfulResolutionTests, totalResolutionTests int
+	var slowResolutions []string
+
+	for _, dr := range dnsResults {
+		switch dr.TestType {
+		case ConnectionDNSTestType:
+			totalConnectionTests++
+			if dr.ErrorString == nil {
+				successfulConnectionTests++
+			}
+		case ResolutionDNSTestType:
+			totalResolutionTests++
+			if dr.ErrorString == nil {
+				successfulResolutionTests++
+				// Flag slow DNS resolutions (>1s).
+				if dr.ResolutionTimeMS != nil && *dr.ResolutionTimeMS > 1000 {
+					if dr.Hostname != nil /* should be non-nil */ {
+						slowResolutions = append(slowResolutions, *dr.Hostname)
+					}
+				}
+			}
+		default:
+			logger.Warnf("Unknown DNS test type; cannot handle %s", dr.TestType)
+		}
+	}
+
+	systemMsg := fmt.Sprintf(
+		"%d/%d dns connection and %d/%d dns resolution tests succeeded",
+		successfulConnectionTests,
+		totalConnectionTests,
+		successfulResolutionTests,
+		totalResolutionTests,
+	)
+	keysAndValues := []any{"dns_tests", stringifyDNSResults(dnsResults)}
+
+	if successfulConnectionTests < totalConnectionTests ||
+		successfulResolutionTests < totalResolutionTests {
+		logger.Warnw(systemMsg, keysAndValues...)
+	} else {
+		logger.Infow(systemMsg, keysAndValues...)
+	}
+
+	// Warn about slow DNS resolutions
+	if len(slowResolutions) > 0 {
+		logger.Warnw(
+			"Slow DNS resolutions detected (>1000ms)",
+			"slow_hostnames", strings.Join(slowResolutions, ", "),
+		)
+	}
 }
 
 func stringifySTUNResponses(stunResponses []*STUNResponse) string {
