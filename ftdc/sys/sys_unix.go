21b392a73716fc1abfc7ee42a73f4669a14fece2
Bohdan Karavan <bohdan.karavan@viam.com>
2025-07-28 17:36:02 -0400

    fix for unix

--- ftdc/sys/sys_unix.go
+++ ftdc/sys/sys_unix.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/prometheus/procfs"
-	//"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/logging"
 )
 
 // On linux, getting the page size is a system call. Cache the page size for the entirety of the
@@ -36,17 +36,19 @@ func init() {
 
 // UsageStatser can be used to get system metrics for a process.
 type UsageStatser struct {
-	proc procfs.Proc
+	proc   procfs.Proc
+	logger logging.Logger
 }
 
 // newSysUsageStatser will return a `SysUsageStatser` for the given process id.
-func newUsageStatser(pid int) (Statser, error) {
+func newUsageStatser(pid int, logger logging.Logger) (Statser, error) {
+	lloger := logger.Sublogger("unix stuff")
 	process, err := procfs.NewProc(pid)
 	if err != nil {
 		return nil, err
 	}
 
-	return &UsageStatser{process}, nil
+	return &UsageStatser{process, lloger}, nil
 }
 
 // Stats returns Stats.
