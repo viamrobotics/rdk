29a6a8346bcbe191ca11de5d0234a9852e570cd8
Bohdan Karavan <bohdan.karavan@viam.com>
2025-07-29 15:44:03 -0400

    trying the net package for windows net

--- ftdc/sys/net_windows.go
+++ ftdc/sys/net_windows.go
@@ -3,16 +3,54 @@
 package sys
 
 import (
-	"net"
-	"unsafe" // probably not needed? double check
+	//"net"
+	//"unsafe" // probably not needed? double check
 
+	windows_net "github.com/shirou/gopsutil/v3/net"
 	"go.viam.com/rdk/logging"
-	"golang.org/x/sys/windows"
+	//"golang.org/x/sys/windows"
 )
 
+func (netStatser *netStatser) Stats() any {
+	ret := networkStats{
+		Ifaces: make(map[string]netDevLine),
+	}
+
+	// Interface stats
+	if netIOs, err := windows_net.IOCounters(true); err == nil {
+		for _, stat := range netIOs {
+			ret.Ifaces[stat.Name] = netDevLine{
+				RxBytes:   stat.BytesRecv,
+				RxPackets: stat.PacketsRecv,
+				RxErrors:  stat.Errin,
+				RxDropped: stat.Dropin,
+				TxBytes:   stat.BytesSent,
+				TxPackets: stat.PacketsSent,
+				TxErrors:  stat.Errout,
+				TxDropped: stat.Dropout,
+			}
+		}
+	}
+
+	// TCP stats
+	if conns, err := windows_net.Connections("tcp"); err == nil {
+		ret.TCP.UsedSockets = uint64(len(conns))
+		// Note: TX/RX queue lengths not exposed here
+	}
+
+	// UDP stats
+	if conns, err := windows_net.Connections("udp"); err == nil {
+		ret.UDP.UsedSockets = uint64(len(conns))
+	}
+
+	netStatser.logger.Info("logging windows info")
+
+	return ret
+}
+
 type netStatser struct {
-	iphlpapi *windows.LazyDLL
-	logger   logging.Logger
+	//iphlpapi *windows.LazyDLL
+	logger logging.Logger
 }
 
 // NewNetUsage returns an object that can interpreted as an `ftdc.Statser`.
@@ -20,168 +58,168 @@ func newNetUsage(logger logging.Logger) (*netStatser, error) {
 	statLogger := logger.Sublogger("windows stats")
 	statLogger.Info("I got created")
 	return &netStatser{
-		iphlpapi: windows.NewLazySystemDLL("iphlpapi.dll"),
-		logger:   statLogger,
+		//iphlpapi: windows.NewLazySystemDLL("iphlpapi.dll"),
+		logger: statLogger,
 	}, nil
 }
 
-// MIB_IFROW structure for GetIfTable
-type mibIfRow struct {
-	Name        [256]uint16
-	Index       uint32
-	Type        uint32
-	Mtu         uint32
-	Speed       uint32
-	PhysAddrLen uint32
-	// replace name with physAddr
-	// 4 bytes ipv4, or 8 bytes => check toString of this
-	// only parse it one time
-	PhysAddr        [8]byte
-	AdminStatus     uint32
-	OperStatus      uint32
-	LastChange      uint32
-	InOctets        uint32
-	InUcastPkts     uint32
-	InNUcastPkts    uint32
-	InDiscards      uint32
-	InErrors        uint32
-	InUnknownProtos uint32
-	OutOctets       uint32
-	OutUcastPkts    uint32
-	OutNUcastPkts   uint32
-	OutDiscards     uint32
-	OutErrors       uint32
-	OutQLen         uint32
-	DescrLen        uint32
-	Descr           [256]byte
-}
+//// MIB_IFROW structure for GetIfTable
+//type mibIfRow struct {
+//Name        [256]uint16
+//Index       uint32
+//Type        uint32
+//Mtu         uint32
+//Speed       uint32
+//PhysAddrLen uint32
+//// replace name with physAddr
+//// 4 bytes ipv4, or 8 bytes => check toString of this
+//// only parse it one time
+//PhysAddr        [8]byte
+//AdminStatus     uint32
+//OperStatus      uint32
+//LastChange      uint32
+//InOctets        uint32
+//InUcastPkts     uint32
+//InNUcastPkts    uint32
+//InDiscards      uint32
+//InErrors        uint32
+//InUnknownProtos uint32
+//OutOctets       uint32
+//OutUcastPkts    uint32
+//OutNUcastPkts   uint32
+//OutDiscards     uint32
+//OutErrors       uint32
+//OutQLen         uint32
+//DescrLen        uint32
+//Descr           [256]byte
+//}
 
-// TCP connection stats from GetTcpStatistics
-type mibTcpStats struct {
-	RtoAlgorithm uint32
-	RtoMin       uint32
-	RtoMax       uint32
-	MaxConn      uint32
-	ActiveOpens  uint32
-	PassiveOpens uint32
-	AttemptFails uint32
-	EstabResets  uint32
-	CurrEstab    uint32
-	InSegs       uint32
-	OutSegs      uint32
-	RetransSegs  uint32
-	InErrs       uint32
-	OutRsts      uint32
-}
+//// TCP connection stats from GetTcpStatistics
+//type mibTcpStats struct {
+//RtoAlgorithm uint32
+//RtoMin       uint32
+//RtoMax       uint32
+//MaxConn      uint32
+//ActiveOpens  uint32
+//PassiveOpens uint32
+//AttemptFails uint32
+//EstabResets  uint32
+//CurrEstab    uint32
+//InSegs       uint32
+//OutSegs      uint32
+//RetransSegs  uint32
+//InErrs       uint32
+//OutRsts      uint32
+//}
 
-// UDP stats from GetUdpStatistics
-type mibUdpStats struct {
-	InDatagrams  uint32
-	NoPorts      uint32
-	InErrors     uint32
-	OutDatagrams uint32
-	NumAddrs     uint32
-}
+//// UDP stats from GetUdpStatistics
+//type mibUdpStats struct {
+//InDatagrams  uint32
+//NoPorts      uint32
+//InErrors     uint32
+//OutDatagrams uint32
+//NumAddrs     uint32
+//}
 
-func (n *netStatser) Stats() any {
-	ret := networkStats{
-		Ifaces: make(map[string]netDevLine),
-	}
+//func (n *netStatser) Stats() any {
+//ret := networkStats{
+//Ifaces: make(map[string]netDevLine),
+//}
 
-	n.logger.Info("Doing stats")
+//n.logger.Info("Doing stats")
 
-	// Get network interface statistics using GetIfTable
-	n.getInterfaceStats(&ret)
+//// Get network interface statistics using GetIfTable
+//n.getInterfaceStats(&ret)
 
-	// Get TCP statistics
-	n.getTCPStats(&ret)
+//// Get TCP statistics
+//n.getTCPStats(&ret)
 
-	// Get UDP statistics
-	n.getUDPStats(&ret)
+//// Get UDP statistics
+//n.getUDPStats(&ret)
 
-	return ret
-}
+//return ret
+//}
 
-func (n *netStatser) getInterfaceStats(ret *networkStats) {
-	getIfTable := n.iphlpapi.NewProc("GetIfTable")
+//func (n *netStatser) getInterfaceStats(ret *networkStats) {
+//getIfTable := n.iphlpapi.NewProc("GetIfTable")
 
-	var size uint32
-	// First call to get required buffer size
-	getIfTable.Call(0, uintptr(unsafe.Pointer(&size)), 0)
+//var size uint32
+//// First call to get required buffer size
+//getIfTable.Call(0, uintptr(unsafe.Pointer(&size)), 0)
 
-	if size == 0 {
-		return
-	}
+//if size == 0 {
+//return
+//}
 
-	buffer := make([]byte, size)
-	r1, _, _ := getIfTable.Call(
-		uintptr(unsafe.Pointer(&buffer[0])),
-		uintptr(unsafe.Pointer(&size)),
-		0,
-	)
+//buffer := make([]byte, size)
+//r1, _, _ := getIfTable.Call(
+//uintptr(unsafe.Pointer(&buffer[0])),
+//uintptr(unsafe.Pointer(&size)),
+//0,
+//)
 
-	if r1 != 0 { // NO_ERROR is 0
-		return
-	}
+//if r1 != 0 { // NO_ERROR is 0
+//return
+//}
 
-	// Parse the MIB_IFTABLE structure
-	numEntries := *(*uint32)(unsafe.Pointer(&buffer[0]))
-	offset := unsafe.Sizeof(uint32(0))
+//// Parse the MIB_IFTABLE structure
+//numEntries := *(*uint32)(unsafe.Pointer(&buffer[0]))
+//offset := unsafe.Sizeof(uint32(0))
 
-	for i := uint32(0); i < numEntries; i++ {
-		row := (*mibIfRow)(unsafe.Pointer(&buffer[offset]))
+//for i := uint32(0); i < numEntries; i++ {
+//row := (*mibIfRow)(unsafe.Pointer(&buffer[offset]))
 
-		// Convert interface name from UTF-16
-		name := windows.UTF16ToString(row.Name[:])
-		n.logger.Info(name)
-		if name == "" {
-			// Fallback to description if name is empty
-			name = string(row.Descr[:row.DescrLen])
-			n.logger.Info("name from descr: ", name)
-		}
+//// Convert interface name from UTF-16
+//name := windows.UTF16ToString(row.Name[:])
+//n.logger.Info(name)
+//if name == "" {
+//// Fallback to description if name is empty
+//name = string(row.Descr[:row.DescrLen])
+//n.logger.Info("name from descr: ", name)
+//}
 
-		n.logger.Info(net.IP(row.PhysAddr[:]))
+//n.logger.Info(net.IP(row.PhysAddr[:]))
 
-		ret.Ifaces[name] = netDevLine{
-			RxBytes:   uint64(row.InOctets),
-			RxPackets: uint64(row.InUcastPkts + row.InNUcastPkts),
-			RxErrors:  uint64(row.InErrors),
-			RxDropped: uint64(row.InDiscards),
-			TxBytes:   uint64(row.OutOctets),
-			TxPackets: uint64(row.OutUcastPkts + row.OutNUcastPkts),
-			TxErrors:  uint64(row.OutErrors),
-			TxDropped: uint64(row.OutDiscards),
-		}
+//ret.Ifaces[name] = netDevLine{
+//RxBytes:   uint64(row.InOctets),
+//RxPackets: uint64(row.InUcastPkts + row.InNUcastPkts),
+//RxErrors:  uint64(row.InErrors),
+//RxDropped: uint64(row.InDiscards),
+//TxBytes:   uint64(row.OutOctets),
+//TxPackets: uint64(row.OutUcastPkts + row.OutNUcastPkts),
+//TxErrors:  uint64(row.OutErrors),
+//TxDropped: uint64(row.OutDiscards),
+//}
 
-		offset += unsafe.Sizeof(mibIfRow{})
-	}
-}
+//offset += unsafe.Sizeof(mibIfRow{})
+//}
+//}
 
-func (n *netStatser) getTCPStats(ret *networkStats) {
-	getTcpStatistics := n.iphlpapi.NewProc("GetTcpStatistics")
+//func (n *netStatser) getTCPStats(ret *networkStats) {
+//getTcpStatistics := n.iphlpapi.NewProc("GetTcpStatistics")
 
-	var stats mibTcpStats
-	r1, _, _ := getTcpStatistics.Call(uintptr(unsafe.Pointer(&stats)))
+//var stats mibTcpStats
+//r1, _, _ := getTcpStatistics.Call(uintptr(unsafe.Pointer(&stats)))
 
-	if r1 == 0 { // NO_ERROR
-		ret.TCP.UsedSockets = uint64(stats.CurrEstab)
-		// Windows doesn't directly expose TX/RX queue lengths like Linux /proc
-		// These values would need additional API calls to GetTcpTable for per-connection data
-		ret.TCP.TxQueueLength = 0
-		ret.TCP.RxQueueLength = 0
-	}
-}
+//if r1 == 0 { // NO_ERROR
+//ret.TCP.UsedSockets = uint64(stats.CurrEstab)
+//// Windows doesn't directly expose TX/RX queue lengths like Linux /proc
+//// These values would need additional API calls to GetTcpTable for per-connection data
+//ret.TCP.TxQueueLength = 0
+//ret.TCP.RxQueueLength = 0
+//}
+//}
 
-func (n *netStatser) getUDPStats(ret *networkStats) {
-	getUdpStatistics := n.iphlpapi.NewProc("GetUdpStatistics")
+//func (n *netStatser) getUDPStats(ret *networkStats) {
+//getUdpStatistics := n.iphlpapi.NewProc("GetUdpStatistics")
 
-	var stats mibUdpStats
-	r1, _, _ := getUdpStatistics.Call(uintptr(unsafe.Pointer(&stats)))
+//var stats mibUdpStats
+//r1, _, _ := getUdpStatistics.Call(uintptr(unsafe.Pointer(&stats)))
 
-	if r1 == 0 { // NO_ERROR
-		ret.UDP.UsedSockets = uint64(stats.NumAddrs)
-		ret.UDP.Drops = uint64(stats.InErrors)
-		ret.UDP.TxQueueLength = 0
-		ret.UDP.RxQueueLength = 0
-	}
-}
+//if r1 == 0 { // NO_ERROR
+//ret.UDP.UsedSockets = uint64(stats.NumAddrs)
+//ret.UDP.Drops = uint64(stats.InErrors)
+//ret.UDP.TxQueueLength = 0
+//ret.UDP.RxQueueLength = 0
+//}
+//}
