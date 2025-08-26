319e306d7f8d1b34dee1104e1294423b335f4a38
hexbabe <sean.xiaochen.yu@gmail.com>
2025-08-26 15:03:05 -0400

    Add get images list order test

--- components/camera/client_test.go
+++ components/camera/client_test.go
@@ -508,6 +508,22 @@ func TestClient(t *testing.T) {
 			test.That(t, err, test.ShouldBeError)
 			test.That(t, err.Error(), test.ShouldContainSubstring, "duplicate source name in filter: color")
 		})
+
+		t.Run("order should matter", func(t *testing.T) {
+			// NOTE: order matters bc of the way we set up our inject camera, order of images is not guaranteed.
+			// we're at the mercy of the camera implementation, but we should test that client/server do not mess up the order
+			images, _, err := camClient.Images(ctx, []string{"color", "depth"}, nil)
+			test.That(t, err, test.ShouldBeNil)
+			test.That(t, len(images), test.ShouldEqual, 2)
+			test.That(t, images[0].SourceName, test.ShouldEqual, "color")
+			test.That(t, images[1].SourceName, test.ShouldEqual, "depth")
+
+			images, _, err = camClient.Images(ctx, []string{"depth", "color"}, nil)
+			test.That(t, err, test.ShouldBeNil)
+			test.That(t, len(images), test.ShouldEqual, 2)
+			test.That(t, images[0].SourceName, test.ShouldEqual, "depth")
+			test.That(t, images[1].SourceName, test.ShouldEqual, "color")
+		})
 	})
 }
 
@@ -1002,7 +1018,7 @@ func TestMultiplexOverMultiHopRemoteConnection(t *testing.T) {
 	test.That(t, cameraClient.(rtppassthrough.Source).Unsubscribe(mainCtx, sub.ID), test.ShouldBeNil)
 }
 
-// nolint
+//nolint
 // NOTE: These tests fail when this condition occurs:
 //
 //	logger.go:130: 2024-06-17T16:56:14.097-0400 DEBUG   TestGrandRemoteRebooting.remote-1.rdk:remote:/remote-2.webrtc   rpc/wrtc_client_channel.go:299  no stream for id; discarding    {"ch": 0, "id": 11}
