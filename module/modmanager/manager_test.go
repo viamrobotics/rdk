85ac08f23c67eed840d509bf83fe063f0f1e4c45
Daniel Botros <93689852+danielbotros@users.noreply.github.com>
2025-09-05 15:05:09 -0400

    Revert "[APP-8439] Ensure VIAM_MODULE_ROOT is set for local modules (#5246)"
    This reverts commit 308a3d74b8d9e7c2b1ca83b0f12ddcbf891d96d2.

--- module/modmanager/manager_test.go
+++ module/modmanager/manager_test.go
@@ -160,18 +160,17 @@ func TestModManagerFunctions(t *testing.T) {
 
 			test.That(t, mod.process.Stop(), test.ShouldBeNil)
 
-			modEnv := mod.getFullEnvironment(viamHomeTemp, filepath.Join(viamHomeTemp, "packages"))
+			modEnv := mod.getFullEnvironment(viamHomeTemp)
 			test.That(t, modEnv["VIAM_HOME"], test.ShouldEqual, viamHomeTemp)
 			test.That(t, modEnv["VIAM_MODULE_DATA"], test.ShouldEqual, "module-data-dir")
 			test.That(t, modEnv["VIAM_MODULE_ID"], test.ShouldEqual, "new:york")
 			test.That(t, modEnv["SMART"], test.ShouldEqual, "MACHINES")
 
-			// Test that VIAM_MODULE_ID is unset and VIAM_MODULE_ROOT is set correctly for local modules
+			// Test that VIAM_MODULE_ID is unset for local modules
 			mod.cfg.Type = config.ModuleTypeLocal
-			modEnv = mod.getFullEnvironment(viamHomeTemp, filepath.Join(viamHomeTemp, "packages"))
+			modEnv = mod.getFullEnvironment(viamHomeTemp)
 			_, ok = modEnv["VIAM_MODULE_ID"]
 			test.That(t, ok, test.ShouldBeFalse)
-			test.That(t, modEnv["VIAM_MODULE_ROOT"], test.ShouldNotBeEmpty)
 
 			// Make a copy of addr and client to test that connections are properly remade
 			oldAddr := mod.addr
