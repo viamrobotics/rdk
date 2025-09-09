79cf7a50e4c34667fee1d53d1368ec245d53084a
Devin C <devin@viam.com>
2025-09-09 13:19:02 -0400

    Cleanup fake tests

--- services/worldstatestore/fake/fake_test.go
+++ services/worldstatestore/fake/fake_test.go
@@ -28,17 +28,6 @@ func TestFakeWorldStateStore(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, boxTransform, test.ShouldNotBeNil)
 	test.That(t, boxTransform.Uuid, test.ShouldResemble, []byte("box-001"))
-	test.That(t, boxTransform.Metadata, test.ShouldNotBeNil)
-
-	// Test color metadata - should be a structpb.Value containing a StructValue
-	colorField := boxTransform.Metadata.Fields["color"]
-	test.That(t, colorField, test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue(), test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue().Fields["r"].GetNumberValue(), test.ShouldEqual, 255)
-	test.That(t, colorField.GetStructValue().Fields["g"].GetNumberValue(), test.ShouldEqual, 0)
-	test.That(t, colorField.GetStructValue().Fields["b"].GetNumberValue(), test.ShouldEqual, 0)
-
-	test.That(t, boxTransform.Metadata.Fields["opacity"].GetNumberValue(), test.ShouldEqual, 0.3)
 
 	sphereTransform, err := fake.GetTransform(context.Background(), []byte("sphere-001"), nil)
 	test.That(t, err, test.ShouldBeNil)
@@ -46,31 +35,11 @@ func TestFakeWorldStateStore(t *testing.T) {
 	test.That(t, sphereTransform.Uuid, test.ShouldResemble, []byte("sphere-001"))
 	test.That(t, sphereTransform.Metadata, test.ShouldNotBeNil)
 
-	// Test color metadata for sphere
-	colorField = sphereTransform.Metadata.Fields["color"]
-	test.That(t, colorField, test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue(), test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue().Fields["r"].GetNumberValue(), test.ShouldEqual, 0)
-	test.That(t, colorField.GetStructValue().Fields["g"].GetNumberValue(), test.ShouldEqual, 0)
-	test.That(t, colorField.GetStructValue().Fields["b"].GetNumberValue(), test.ShouldEqual, 255)
-
-	test.That(t, sphereTransform.Metadata.Fields["opacity"].GetNumberValue(), test.ShouldEqual, 0.7)
-
 	capsuleTransform, err := fake.GetTransform(context.Background(), []byte("capsule-001"), nil)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, capsuleTransform, test.ShouldNotBeNil)
 	test.That(t, capsuleTransform.Uuid, test.ShouldResemble, []byte("capsule-001"))
 	test.That(t, capsuleTransform.Metadata, test.ShouldNotBeNil)
-
-	// Test color metadata for capsule
-	colorField = capsuleTransform.Metadata.Fields["color"]
-	test.That(t, colorField, test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue(), test.ShouldNotBeNil)
-	test.That(t, colorField.GetStructValue().Fields["r"].GetNumberValue(), test.ShouldEqual, 0)
-	test.That(t, colorField.GetStructValue().Fields["g"].GetNumberValue(), test.ShouldEqual, 255)
-	test.That(t, colorField.GetStructValue().Fields["b"].GetNumberValue(), test.ShouldEqual, 0)
-
-	test.That(t, capsuleTransform.Metadata.Fields["opacity"].GetNumberValue(), test.ShouldEqual, 1.0)
 
 	// Test StreamTransformChanges
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
@@ -78,18 +47,6 @@ func TestFakeWorldStateStore(t *testing.T) {
 	stream, err := fake.StreamTransformChanges(ctx, nil)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, stream, test.ShouldNotBeNil)
-
-	// Wait a bit for some changes to occur
-	time.Sleep(200 * time.Millisecond)
-
-	// Check that we've received some changes
-	changeCount := 0
-	if _, err := stream.Next(); err == nil {
-		changeCount++
-	}
-
-	// We should have at least some changes after 200ms
-	test.That(t, changeCount, test.ShouldBeGreaterThanOrEqualTo, 0)
 }
 
 func TestFakeWorldStateStoreClose(t *testing.T) {
@@ -98,12 +55,6 @@ func TestFakeWorldStateStoreClose(t *testing.T) {
 	// Test that Close works
 	err := fake.Close(context.Background())
 	test.That(t, err, test.ShouldBeNil)
-
-	// Test that ListUUIDs still works after close (should return empty)
-	uuids, err := fake.ListUUIDs(context.Background(), nil)
-	test.That(t, err, test.ShouldBeNil)
-	test.That(t, len(uuids), test.ShouldBeGreaterThanOrEqualTo, 3) // Static transforms are still available
-	test.That(t, len(uuids), test.ShouldBeLessThanOrEqualTo, 4)    // Dynamic transform may be available
 }
 
 func TestDoCommandSetFPS(t *testing.T) {
