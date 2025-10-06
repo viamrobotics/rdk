3558a6743fa45afa76fe9da499c18f96b3389937
Allison Chiang <allisonsc123@gmail.com>
2025-10-06 15:23:51 -0400

    lint

--- robot/packages/local_package_manager.go
+++ robot/packages/local_package_manager.go
@@ -53,7 +53,7 @@ func NewLocalManager(conf *config.Config, logger logging.Logger) (ManagerSyncer,
 		if err := os.MkdirAll(packagesDir, 0o700); err != nil {
 			return nil, err
 		}
-	
+
 		if err := os.MkdirAll(packagesDataDir, 0o700); err != nil {
 			return nil, err
 		}
