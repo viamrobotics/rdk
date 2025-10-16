b73304cd65c636451d3230ff8e77e09785654bcb
Eliot Horowitz <eliothorowitz@gmail.com>
2025-10-16 16:52:30 -0400

    wip

--- motionplan/armplanning/plan_manager.go
+++ motionplan/armplanning/plan_manager.go
@@ -150,7 +150,26 @@ func (pm *planManager) planToDirectJoints(
 	if err != nil {
 		return nil, fmt.Errorf("want to go to specific joint config but it is invalid: %w", err)
 	}
+	
+	{ // true cartesian half
+		startPoses, err := start.ComputePoses(pm.pc.fs)
+		if err != nil {
+			return nil, err
+		}
 
+		mid := interp(startPoses, goalPoses, .5)
+
+		err = pm.foo(ctx, start, mid)
+		if err != nil {
+			pm.logger.Infof("foo failed: %v", err)
+		} else {
+			panic(2)
+		}
+		
+		
+		panic(1)
+	}
+	
 	pathPlanner, err := newCBiRRTMotionPlanner(ctx, pm.pc, psc)
 	if err != nil {
 		return nil, err
@@ -306,4 +325,50 @@ func initRRTSolutions(ctx context.Context, psc *planSegmentContext) (*rrtSolutio
 	rrt.maps.startMap[&node{inputs: seed.inputs}] = nil
 
 	return rrt, nil
+}
+
+	
+func interp(start, end referenceframe.FrameSystemPoses, delta float64) referenceframe.FrameSystemPoses {
+	mid := referenceframe.FrameSystemPoses{}
+
+	for k, s := range start {
+		e, ok := end[k]
+		if !ok {
+			mid[k] = s
+			continue
+		}
+		if s.Parent() != e.Parent() {
+			panic("eliottttt")
+		}
+		m := spatialmath.Interpolate(s.Pose(), e.Pose(), .5)
+		mid[k] = referenceframe.NewPoseInFrame(s.Parent(), m)
+	}
+	return mid
+}
+
+func (pm *planManager) foo(ctx context.Context, start referenceframe.FrameSystemInputs, goal referenceframe.FrameSystemPoses) error {
+	psc, err := newPlanSegmentContext(ctx, pm.pc, start, goal)
+	if err != nil {
+		return err
+	}		
+
+	planSeed, err := initRRTSolutions(ctx, psc)
+	if err != nil {
+		return err
+	}
+	
+	if planSeed.steps == nil {
+		return fmt.Errorf("no steps")
+	}
+
+	if len(planSeed.steps) != 1 {
+		return fmt.Errorf("steps odd %d", len(planSeed.steps))
+	}
+
+	err = psc.checkPath(ctx, start, planSeed.steps[0])
+	if err != nil {
+		return err
+	}
+
+	panic(5)
 }
