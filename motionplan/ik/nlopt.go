fcbdd0a6d54355aa400e58bfcc4164594e9cfe1d
Eliot Horowitz <eliothorowitz@gmail.com>
2025-10-24 09:50:31 -0400

    Revert "min vs max"
    This reverts commit 02efd69baae4ca15f87a1ddfddb84efda92a9593.

--- motionplan/ik/nlopt.go
+++ motionplan/ik/nlopt.go
@@ -105,7 +105,7 @@ func (ik *NloptIK) newSeedState(ctx context.Context, minFunc CostFunc,
 	} else {
 		travelPercent = make([]float64, len(travelPercentIn))
 		for i, in := range travelPercentIn {
-			travelPercent[i] = min(travelPercentMultiplier, in)
+			travelPercent[i] = max(travelPercentMultiplier, in)
 		}
 	}
 
