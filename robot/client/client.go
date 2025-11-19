f3a956aefc588f0861ecf42040ce524b07a6b12b
Allison Chiang <allisonsc123@gmail.com>
2025-11-19 10:56:05 -0500

    update

--- robot/client/client.go
+++ robot/client/client.go
@@ -533,9 +533,8 @@ func (rc *RobotClient) connectWithLock(ctx context.Context) error {
 			err = nil
 		} else {
 			if statusErr := status.Convert(err); statusErr != nil &&
-				statusErr.Code() == codes.Unavailable &&
-				strings.Contains(statusErr.Message(), rpc.ErrOffline.Error()) &&
-				errors.Is(grpcErr, rpc.ErrMDNSQuery) &&
+				statusErr.Code() == codes.NotFound &&
+				errors.Is(grpcErr, rpc.ErrMDNSNoCandidatesFound) &&
 				errors.Is(grpcErr, context.DeadlineExceeded) {
 				return rpc.ErrOffline
 			}
