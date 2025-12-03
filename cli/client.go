1e1db56c0252520661e1fa25bf9880f1168ca0bd
Joshua Matthews <josh.matthews@viam.com>
2025-12-03 16:57:06 -0500

    Split trace cli implementation into separate file

--- cli/client.go
+++ cli/client.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"context"
 	"encoding/json"
-	stderrors "errors"
 	"fmt"
 	"io"
 	"io/fs"
@@ -31,8 +30,6 @@ import (
 	"github.com/nathan-fiscaletti/consolesize-go"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	tracepb "go.opentelemetry.io/proto/otlp/trace/v1"
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
 	buildpb "go.viam.com/api/app/build/v1"
@@ -45,7 +42,6 @@ import (
 	apppb "go.viam.com/api/app/v1"
 	commonpb "go.viam.com/api/common/v1"
 	"go.viam.com/utils"
-	"go.viam.com/utils/perf"
 	"go.viam.com/utils/rpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -56,7 +52,6 @@ import (
 	rconfig "go.viam.com/rdk/config"
 	"go.viam.com/rdk/grpc"
 	"go.viam.com/rdk/logging"
-	"go.viam.com/rdk/protoutils"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/robot/client"
 	"go.viam.com/rdk/services/shell"
@@ -86,7 +81,6 @@ const (
 var (
 	errNoShellService = errors.New("shell service is not enabled on this machine part")
 	ftdcPath          = path.Join("~", ".viam", "diagnostics.data")
-	tracesPath        = path.Join("~", ".viam", "trace")
 )
 
 // viamClient wraps a cli.Context and provides all the CLI command functionality
@@ -1328,31 +1322,6 @@ type machinesPartGetFTDCArgs struct {
 	Part         string
 }
 
-type traceFetchRemoteArgs struct {
-	Organization string
-	Location     string
-	Machine      string
-	Part         string
-	Destination  string
-}
-
-type traceImportRemoteArgs struct {
-	Organization string
-	Location     string
-	Machine      string
-	Part         string
-	Endpoint     string
-}
-
-type traceImportLocalArgs struct {
-	Path     string
-	Endpoint string
-}
-
-type tracePrintLocalArgs struct {
-	Path string
-}
-
 // MachinesPartGetFTDCAction is the corresponding Action for 'machines part get-ftdc'.
 func MachinesPartGetFTDCAction(c *cli.Context, args machinesPartGetFTDCArgs) error {
 	client, err := newViamClient(c)
@@ -1367,44 +1336,6 @@ func MachinesPartGetFTDCAction(c *cli.Context, args machinesPartGetFTDCArgs) err
 	logger := globalArgs.createLogger()
 
 	return client.machinesPartGetFTDCAction(c, args, globalArgs.Debug, logger)
-}
-
-func traceImportRemoteAction(ctx *cli.Context, args traceImportRemoteArgs) error {
-	client, err := newViamClient(ctx)
-	if err != nil {
-		return err
-	}
-
-	globalArgs, err := getGlobalArgs(ctx)
-	if err != nil {
-		return err
-	}
-	logger := globalArgs.createLogger()
-	tmp, err := os.MkdirTemp("", "viamtraceimport")
-	if err != nil {
-		return err
-	}
-	//nolint: errcheck
-	defer os.RemoveAll(tmp)
-	if err := client.tracesFetchRemoteAction(
-		ctx,
-		traceFetchRemoteArgs{
-			Organization: args.Organization,
-			Location:     args.Location,
-			Machine:      args.Machine,
-			Part:         args.Part,
-			Destination:  tmp,
-		},
-		globalArgs.Debug,
-		logger,
-	); err != nil {
-		return err
-	}
-
-	return traceImportLocalAction(ctx, traceImportLocalArgs{
-		Path:     filepath.Join(tmp, "traces"),
-		Endpoint: args.Endpoint,
-	})
 }
 
 // MachinesPartCopyFilesAction is the corresponding Action for 'machines part cp'.
@@ -1582,173 +1513,6 @@ func (c *viamClient) machinesPartGetFTDCAction(
 	}
 	if !quiet {
 		printf(ctx.App.Writer, "Done in %s.", time.Since(startTime))
-	}
-	return nil
-}
-
-func (c *viamClient) tracesFetchRemoteAction(
-	ctx *cli.Context,
-	flagArgs traceFetchRemoteArgs,
-	debug bool,
-	logger logging.Logger,
-) error {
-	part, err := c.robotPart(flagArgs.Organization, flagArgs.Location, flagArgs.Machine, flagArgs.Part)
-	if err != nil {
-		return err
-	}
-	// Intentional use of path instead of filepath: Windows understands both / and
-	// \ as path separators, and we don't want a cli running on Windows to send
-	// a path using \ to a *NIX machine.
-	src := path.Join(tracesPath, part.Id, "traces")
-	gArgs, err := getGlobalArgs(ctx)
-	quiet := err == nil && gArgs != nil && gArgs.Quiet
-	var startTime time.Time
-	if !quiet {
-		startTime = time.Now()
-		printf(ctx.App.Writer, "Saving to %s ...", path.Join(flagArgs.Destination, part.GetId()))
-	}
-	if err := c.copyFilesFromMachine(
-		flagArgs.Organization,
-		flagArgs.Location,
-		flagArgs.Machine,
-		flagArgs.Part,
-		debug,
-		true,
-		false,
-		[]string{src},
-		flagArgs.Destination,
-		logger,
-	); err != nil {
-		if statusErr := status.Convert(err); statusErr != nil &&
-			statusErr.Code() == codes.InvalidArgument &&
-			statusErr.Message() == shell.ErrMsgDirectoryCopyRequestNoRecursion {
-			return errDirectoryCopyRequestNoRecursion
-		}
-		return err
-	}
-	if !quiet {
-		printf(ctx.App.Writer, "Download finished in %s.", time.Since(startTime))
-	}
-	return nil
-}
-
-func tracePrintRemoteAction(
-	ctx *cli.Context,
-	args machinesPartGetFTDCArgs,
-) error {
-	client, err := newViamClient(ctx)
-	if err != nil {
-		return err
-	}
-
-	globalArgs, err := getGlobalArgs(ctx)
-	if err != nil {
-		return err
-	}
-	logger := globalArgs.createLogger()
-	tmp, err := os.MkdirTemp("", "viamtraceimport")
-	if err != nil {
-		return err
-	}
-	//nolint: errcheck
-	defer os.RemoveAll(tmp)
-	if err := client.tracesFetchRemoteAction(
-		ctx,
-		traceFetchRemoteArgs{
-			Organization: args.Organization,
-			Location:     args.Location,
-			Machine:      args.Machine,
-			Part:         args.Part,
-			Destination:  tmp,
-		},
-		globalArgs.Debug,
-		logger,
-	); err != nil {
-		return err
-	}
-	return tracePrintLocalAction(ctx, tracePrintLocalArgs{Path: filepath.Join(tmp, "traces")})
-}
-
-func traceFetchRemoteAction(ctx *cli.Context, args traceFetchRemoteArgs) error {
-	client, err := newViamClient(ctx)
-	if err != nil {
-		return err
-	}
-
-	globalArgs, err := getGlobalArgs(ctx)
-	if err != nil {
-		return err
-	}
-	logger := globalArgs.createLogger()
-
-	return client.tracesFetchRemoteAction(ctx, args, globalArgs.Debug, logger)
-}
-
-func tracePrintLocalAction(
-	ctx *cli.Context,
-	args tracePrintLocalArgs,
-) error {
-	traceFile, err := os.Open(args.Path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			printf(ctx.App.Writer, "No traces found")
-			return nil
-		}
-		return errors.Wrap(err, "failed to open trace file")
-	}
-	traceReader := protoutils.NewDelimitedProtoReader[tracepb.ResourceSpans](traceFile)
-	//nolint: errcheck
-	defer traceReader.Close()
-
-	devExporter := perf.NewOtelDevelopmentExporter()
-	if err := devExporter.Start(); err != nil {
-		return err
-	}
-	defer devExporter.Stop()
-	var msg tracepb.ResourceSpans
-	err = nil
-	for resource := range traceReader.AllWithMemory(&msg) {
-		for _, scope := range resource.ScopeSpans {
-			err = stderrors.Join(err, devExporter.ExportOTLPSpans(ctx.Context, scope.Spans))
-		}
-	}
-	return err
-}
-
-func traceImportLocalAction(
-	ctx *cli.Context,
-	args traceImportLocalArgs,
-) error {
-	traceFile, err := os.Open(args.Path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			printf(ctx.App.Writer, "No traces found")
-			return nil
-		}
-		return errors.Wrap(err, "failed to open trace file")
-	}
-	traceReader := protoutils.NewDelimitedProtoReader[tracepb.ResourceSpans](traceFile)
-	//nolint: errcheck
-	defer traceReader.Close()
-	endpoint := args.Endpoint
-	if endpoint == "" {
-		endpoint = "localhost:4317"
-	}
-	otlpClient := otlptracegrpc.NewClient(
-		otlptracegrpc.WithEndpoint(endpoint),
-		otlptracegrpc.WithInsecure(),
-	)
-	if err := otlpClient.Start(ctx.Context); err != nil {
-		return err
-	}
-	//nolint: errcheck
-	defer otlpClient.Stop(ctx.Context)
-	var msg tracepb.ResourceSpans
-	for span := range traceReader.AllWithMemory(&msg) {
-		err := otlpClient.UploadTraces(ctx.Context, []*tracepb.ResourceSpans{span})
-		if err != nil {
-			printf(ctx.App.Writer, "Error uploading trace: %v", err)
-		}
 	}
 	return nil
 }
---
+++ cli/traces.go
@@ -0,0 +1,253 @@
+package cli
+
+import (
+	stderrors "errors"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	tracepb "go.opentelemetry.io/proto/otlp/trace/v1"
+	"go.viam.com/utils/perf"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/protoutils"
+	"go.viam.com/rdk/services/shell"
+)
+
+var tracesPath = path.Join("~", ".viam", "trace")
+
+type traceFetchRemoteArgs struct {
+	Organization string
+	Location     string
+	Machine      string
+	Part         string
+	Destination  string
+}
+
+type traceImportRemoteArgs struct {
+	Organization string
+	Location     string
+	Machine      string
+	Part         string
+	Endpoint     string
+}
+
+type traceImportLocalArgs struct {
+	Path     string
+	Endpoint string
+}
+
+type tracePrintLocalArgs struct {
+	Path string
+}
+
+func traceImportRemoteAction(ctx *cli.Context, args traceImportRemoteArgs) error {
+	client, err := newViamClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	globalArgs, err := getGlobalArgs(ctx)
+	if err != nil {
+		return err
+	}
+	logger := globalArgs.createLogger()
+	tmp, err := os.MkdirTemp("", "viamtraceimport")
+	if err != nil {
+		return err
+	}
+	//nolint: errcheck
+	defer os.RemoveAll(tmp)
+	if err := client.tracesFetchRemoteAction(
+		ctx,
+		traceFetchRemoteArgs{
+			Organization: args.Organization,
+			Location:     args.Location,
+			Machine:      args.Machine,
+			Part:         args.Part,
+			Destination:  tmp,
+		},
+		globalArgs.Debug,
+		logger,
+	); err != nil {
+		return err
+	}
+
+	return traceImportLocalAction(ctx, traceImportLocalArgs{
+		Path:     filepath.Join(tmp, "traces"),
+		Endpoint: args.Endpoint,
+	})
+}
+
+func (c *viamClient) tracesFetchRemoteAction(
+	ctx *cli.Context,
+	flagArgs traceFetchRemoteArgs,
+	debug bool,
+	logger logging.Logger,
+) error {
+	part, err := c.robotPart(flagArgs.Organization, flagArgs.Location, flagArgs.Machine, flagArgs.Part)
+	if err != nil {
+		return err
+	}
+	// Intentional use of path instead of filepath: Windows understands both / and
+	// \ as path separators, and we don't want a cli running on Windows to send
+	// a path using \ to a *NIX machine.
+	src := path.Join(tracesPath, part.Id, "traces")
+	gArgs, err := getGlobalArgs(ctx)
+	quiet := err == nil && gArgs != nil && gArgs.Quiet
+	var startTime time.Time
+	if !quiet {
+		startTime = time.Now()
+		printf(ctx.App.Writer, "Saving to %s ...", path.Join(flagArgs.Destination, part.GetId()))
+	}
+	if err := c.copyFilesFromMachine(
+		flagArgs.Organization,
+		flagArgs.Location,
+		flagArgs.Machine,
+		flagArgs.Part,
+		debug,
+		true,
+		false,
+		[]string{src},
+		flagArgs.Destination,
+		logger,
+	); err != nil {
+		if statusErr := status.Convert(err); statusErr != nil &&
+			statusErr.Code() == codes.InvalidArgument &&
+			statusErr.Message() == shell.ErrMsgDirectoryCopyRequestNoRecursion {
+			return errDirectoryCopyRequestNoRecursion
+		}
+		return err
+	}
+	if !quiet {
+		printf(ctx.App.Writer, "Download finished in %s.", time.Since(startTime))
+	}
+	return nil
+}
+
+func tracePrintRemoteAction(
+	ctx *cli.Context,
+	args machinesPartGetFTDCArgs,
+) error {
+	client, err := newViamClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	globalArgs, err := getGlobalArgs(ctx)
+	if err != nil {
+		return err
+	}
+	logger := globalArgs.createLogger()
+	tmp, err := os.MkdirTemp("", "viamtraceimport")
+	if err != nil {
+		return err
+	}
+	//nolint: errcheck
+	defer os.RemoveAll(tmp)
+	if err := client.tracesFetchRemoteAction(
+		ctx,
+		traceFetchRemoteArgs{
+			Organization: args.Organization,
+			Location:     args.Location,
+			Machine:      args.Machine,
+			Part:         args.Part,
+			Destination:  tmp,
+		},
+		globalArgs.Debug,
+		logger,
+	); err != nil {
+		return err
+	}
+	return tracePrintLocalAction(ctx, tracePrintLocalArgs{Path: filepath.Join(tmp, "traces")})
+}
+
+func traceFetchRemoteAction(ctx *cli.Context, args traceFetchRemoteArgs) error {
+	client, err := newViamClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	globalArgs, err := getGlobalArgs(ctx)
+	if err != nil {
+		return err
+	}
+	logger := globalArgs.createLogger()
+
+	return client.tracesFetchRemoteAction(ctx, args, globalArgs.Debug, logger)
+}
+
+func tracePrintLocalAction(
+	ctx *cli.Context,
+	args tracePrintLocalArgs,
+) error {
+	traceFile, err := os.Open(args.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			printf(ctx.App.Writer, "No traces found")
+			return nil
+		}
+		return errors.Wrap(err, "failed to open trace file")
+	}
+	traceReader := protoutils.NewDelimitedProtoReader[tracepb.ResourceSpans](traceFile)
+	//nolint: errcheck
+	defer traceReader.Close()
+
+	devExporter := perf.NewOtelDevelopmentExporter()
+	if err := devExporter.Start(); err != nil {
+		return err
+	}
+	defer devExporter.Stop()
+	var msg tracepb.ResourceSpans
+	err = nil
+	for resource := range traceReader.AllWithMemory(&msg) {
+		for _, scope := range resource.ScopeSpans {
+			err = stderrors.Join(err, devExporter.ExportOTLPSpans(ctx.Context, scope.Spans))
+		}
+	}
+	return err
+}
+
+func traceImportLocalAction(
+	ctx *cli.Context,
+	args traceImportLocalArgs,
+) error {
+	traceFile, err := os.Open(args.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			printf(ctx.App.Writer, "No traces found")
+			return nil
+		}
+		return errors.Wrap(err, "failed to open trace file")
+	}
+	traceReader := protoutils.NewDelimitedProtoReader[tracepb.ResourceSpans](traceFile)
+	//nolint: errcheck
+	defer traceReader.Close()
+	endpoint := args.Endpoint
+	if endpoint == "" {
+		endpoint = "localhost:4317"
+	}
+	otlpClient := otlptracegrpc.NewClient(
+		otlptracegrpc.WithEndpoint(endpoint),
+		otlptracegrpc.WithInsecure(),
+	)
+	if err := otlpClient.Start(ctx.Context); err != nil {
+		return err
+	}
+	//nolint: errcheck
+	defer otlpClient.Stop(ctx.Context)
+	var msg tracepb.ResourceSpans
+	for span := range traceReader.AllWithMemory(&msg) {
+		err := otlpClient.UploadTraces(ctx.Context, []*tracepb.ResourceSpans{span})
+		if err != nil {
+			printf(ctx.App.Writer, "Error uploading trace: %v", err)
+		}
+	}
+	return nil
+}
