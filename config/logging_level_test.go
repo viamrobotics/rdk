0157690f7afab1083ab62f830371e2b3a7bf64bf
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2025-12-10 13:46:48 -0500

    RSDK-12690 Reorganize/name Subloggers for diagnostic logs (#5565)

--- config/logging_level_test.go
+++ config/logging_level_test.go
@@ -11,20 +11,25 @@ import (
 
 func TestConfigDebugFlag(t *testing.T) {
 	logConfig := logging.NewZapLoggerConfig()
-	logger := logging.FromZapCompatible(zap.Must(logConfig.Build()).Sugar())
+	globalLogger := logging.FromZapCompatible(zap.Must(logConfig.Build()).Sugar())
+	levelChangeLogger, logs := logging.NewObservedTestLogger(t)
 
 	for _, cmdLineValue := range []bool{true, false} {
 		for _, fileDebugValue := range []bool{true, false} {
 			for _, cloudDebugValue := range []bool{true, false} {
-				InitLoggingSettings(logger, cmdLineValue)
+				logs.TakeAll()
+
+				InitLoggingSettings(globalLogger, levelChangeLogger, cmdLineValue)
+				test.That(t, logs.FilterMessageSnippet("Log level initialized:").Len(), test.ShouldEqual, 1)
+
 				UpdateFileConfigDebug(fileDebugValue)
 				UpdateCloudConfigDebug(cloudDebugValue)
 
 				expectedDebugEnabled := cmdLineValue || fileDebugValue || cloudDebugValue
 				if expectedDebugEnabled {
-					test.That(t, logger.Level().Enabled(zap.DebugLevel), test.ShouldBeTrue)
+					test.That(t, globalLogger.Level().Enabled(zap.DebugLevel), test.ShouldBeTrue)
 				}
-				test.That(t, logger.Level().Enabled(zap.InfoLevel), test.ShouldBeTrue)
+				test.That(t, globalLogger.Level().Enabled(zap.InfoLevel), test.ShouldBeTrue)
 			}
 		}
 	}
