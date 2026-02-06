6acaf67f833ec7c584284d629dde48fa21c18dbe
Lillian Hwang-Geddes <34491386+lvhg@users.noreply.github.com>
2026-02-06 15:15:45 -0500

    APP-11769 - Add visibility to training script upload CLI (#5680)

--- cli/app.go
+++ cli/app.go
@@ -3775,6 +3775,11 @@ This won't work unless you have an existing installation of our GitHub app on yo
 							Name:  mlTrainingFlagURL,
 							Usage: "url of Github repository associated with the training scripts",
 						},
+						&cli.StringFlag{
+							Name:     mlTrainingFlagVisibility,
+							Usage:    formatAcceptedValues("visibility of the training script", "public", "private"),
+							Required: false,
+						},
 					},
 					Action: createCommandWithT[mlTrainingUploadArgs](MLTrainingUploadAction),
 				},
