6d3770262af6aa536a517d7bd0d5ff2d8693381e
Eliot Horowitz <eliothorowitz@gmail.com>
2026-03-02 11:17:58 -0500

    fix minRatio computation (#5802)
    Co-authored-by: Dan Gottlieb <dan.gottlieb@viam.com>

--- motionplan/armplanning/node.go
+++ motionplan/armplanning/node.go
@@ -154,19 +154,21 @@ func newSolutionSolvingState(ctx context.Context, psc *planSegmentContext, logge
 		sss.maxSolutions = defaultSolutionsToSeed
 	}
 
-	sss.linearSeeds = [][]float64{psc.start.GetLinearizedInputs()}
+	sss.linearSeeds = [][]float64{psc.start.GetLinearizedInputs()} // s:0
 	sss.seedLimits = [][]referenceframe.Limit{psc.pc.lis.GetLimits()}
 
+	// For multi-arm systems, `rawRatios` elements will be -1 for non-moving arms. `minRatio` will
+	// be the smallest (currently bumped to a 0.03 minimum) ratio value for joints in moving arms.
 	rawRatios, minRatio, err := sss.computeGoodCost(psc.goal)
 	if err != nil {
 		return nil, err
 	}
 
-	sss.linearSeeds = append(sss.linearSeeds, sss.linearSeeds[0])
+	sss.linearSeeds = append(sss.linearSeeds, sss.linearSeeds[0]) // s:1
 	sss.seedLimits = append(sss.seedLimits,
 		ik.ComputeAdjustLimitsArray(sss.linearSeeds[0], sss.seedLimits[0], clampSensitivities(rawRatios, .03)))
 
-	sss.linearSeeds = append(sss.linearSeeds, sss.linearSeeds[0])
+	sss.linearSeeds = append(sss.linearSeeds, sss.linearSeeds[0]) // s:2
 	sss.seedLimits = append(sss.seedLimits,
 		ik.ComputeAdjustLimitsArray(sss.linearSeeds[0], sss.seedLimits[0], clampSensitivities(rawRatios, .25)))
 
@@ -219,13 +221,14 @@ func (sss *solutionSolvingState) computeGoodCost(goal referenceframe.FrameSystem
 	}
 
 	ratios := clampSensitivities(rawRatios, .03)
-
 	minRatio := 1.0
 
 	adjusted := []float64{}
 	for idx, r := range ratios {
 		adjusted = append(adjusted, sss.psc.pc.lis.Jog(idx, sss.linearSeeds[0][idx], r))
-		minRatio = min(minRatio, r)
+		if r > 0 { // if a joint is part of a non-moving arm, don't consider it
+			minRatio = min(minRatio, r)
+		}
 	}
 
 	step, err := sss.psc.pc.lis.FloatsToInputs(adjusted)
@@ -342,16 +345,16 @@ func (sss *solutionSolvingState) shouldStopEarly() bool {
 		minMillis = 100
 	}
 
-	if sss.bestScoreNoProblem < sss.goodCost/20 {
+	if sss.bestScoreNoProblem < sss.goodCost/20 { // .05
 		multiple = 0
 		minMillis = 5
-	} else if sss.bestScoreNoProblem < sss.goodCost/15 {
+	} else if sss.bestScoreNoProblem < sss.goodCost/15 { // .06
 		multiple = 1
 		minMillis = 10
-	} else if sss.bestScoreNoProblem < sss.goodCost/10 {
+	} else if sss.bestScoreNoProblem < sss.goodCost/10 { // .1
 		multiple = 0
 		minMillis = 15
-	} else if sss.bestScoreNoProblem < sss.goodCost/5 {
+	} else if sss.bestScoreNoProblem < sss.goodCost/5 { // .2
 		multiple = 2
 		minMillis = 15
 	} else if sss.bestScoreNoProblem < sss.goodCost/3.5 {
