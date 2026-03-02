77b2e1457563e6435b6ef2584571c7f838132bdb
Eliot Horowitz <eliothorowitz@gmail.com>
2026-03-02 18:26:44 -0500

    fix test

--- motionplan/armplanning/functional_test.go
+++ motionplan/armplanning/functional_test.go
@@ -469,7 +469,11 @@ func TestArmOOBSolve(t *testing.T) {
 		PlannerOptions: NewBasicPlannerOptions(),
 	})
 	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, err.Error(), test.ShouldEqual, errIKSolve.Error())
+	if IsTooSmallForCache() {
+		test.That(t, err.Error(), test.ShouldEqual, errIKSolve.Error())
+	} else {
+		test.That(t, err.Error(), test.ShouldContainSubstring, "too far")
+	}
 }
 
 func TestArmObstacleSolve(t *testing.T) {
