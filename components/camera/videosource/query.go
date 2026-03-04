6967b14f95ba9c984aa7fbe6347c171f2a63abcd
sean yu <sean.xiaochen.yu@gmail.com>
2026-03-04 18:36:56 -0500

    Improve error message when webcam config constraints don't match
    Add actionable guidance to the error message when no driver matches
    the configured constraints, suggesting users try relaxing width/height
    or changing frame format.

--- components/camera/videosource/query.go
+++ components/camera/videosource/query.go
@@ -313,7 +313,9 @@ func selectBestDriver(
 			labels = append(labels, d.Info().Label)
 		}
 		return nil, prop.Media{}, errors.Errorf(
-			"failed to find a queryable driver that matches the config constraints. Devices tried: %s",
+			"failed to find a queryable driver that matches the config constraints. "+
+				"You can try tweaking or relaxing the constraints, e.g. removing or changing the height/width, "+
+				"changing to a valid frame format, etc. Devices tried: %s",
 			strings.Join(labels, ", "))
 	}
 
