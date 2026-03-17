75d57a42c1f1ee39565d268ee68b72b1961bb5c8
Dan Gottlieb <dan.gottlieb@viam.com>
2026-03-17 15:50:53 -0400

    Hook up to api

--- referenceframe/pose_cloud.go
+++ referenceframe/pose_cloud.go
@@ -15,11 +15,12 @@ import (
 // leeway might work with a smaller difference in the candidate's theta (the cup stays mostly facing
 // up), but not necessarily a larger one (the cup starts to tip and spill).
 type PoseCloud struct {
-	// ReferenceFrame represents which reference frame the following leeways will be applied
-	// to. Consider a case where an arm with a hand is pushing a block up a hill. It would be
-	// important for the arm to follow the slope of the incline (the hand stays close to the
-	// incline). But it might be okay if the hand, at any given waypoint, moves a little bit more up
-	// or down the hill:
+	// A note about translational leeways (X, Y, Z) and orientations:
+	//
+	// Consider a case where an arm with a hand is pushing a block up a hill. It would be important
+	// for the arm to follow the slope of the incline (the hand stays close to the incline). But it
+	// might be okay if the hand, at any given waypoint, moves a little bit more up or down the
+	// hill:
 	//
 	//     +------+
 	//     |     /|
@@ -35,20 +36,22 @@ type PoseCloud struct {
 	// also lower the hand closer to the incline. If we try to calculate this in the world reference
 	// frame, this means that the leeway for Z is in terms of a specific IK solution's leeway for X.
 	//
-	// To solve this, we allow the user to specify a reference frame that the leeways will be
-	// applied to. If the box's orientation vector is perpendicular to incline, the user can specify
-	// that, in the box's reference frame, the leeway of Z' is 0. While the leeway for X' (in the
-	// box reference frame) can be a wider range.
+	// To solve this, we always evaluate in the reference frame of the object being asked to
+	// move. In the above example, a movement request would ask the box to move, assuming it's
+	// parented to the hand. Hence, the leeways would be applied to the box. For this to work, we
+	// would want the box's orientation vector to be perpendicular to the incline. Such that we
+	// declare the leeway of Z' to be 0. While the leeway for X' (in the box reference frame) can be
+	// a wider range.
 	ReferenceFrame string
 
 	// The following X, Y and Z are translational leeways. They are all in units of millimeters, the
 	// same as a goal pose. The value represents a leeway in the range of [-Value, +Value].
-
-	// The X leeway where any X (with respect to the reference frame's orientation).
+	//
+	// The X leeway where any X (with respect to the movement object's orientation).
 	X float64 `json:"x"`
-	// The Y leeway where any Y (with respect to the reference frame's orientation).
+	// The Y leeway where any Y (with respect to the movement object's orientation).
 	Y float64 `json:"y"`
-	// The Z leeway where any Z (with respect to the reference frame's orientation).
+	// The Z leeway where any Z (with respect to the movement object's orientation).
 	Z float64 `json:"z"`
 
 	// The following orientation leeway values represents a leeway in the range of [-Value,
@@ -70,13 +73,12 @@ type PoseCloud struct {
 
 func (pc *PoseCloud) ToProto() *commonpb.PoseCloud {
 	return &commonpb.PoseCloud{
-		ReferenceFrame: pc.ReferenceFrame,
-		X:              pc.X,
-		Y:              pc.Y,
-		Z:              pc.Z,
-		OX:             pc.OX,
-		OY:             pc.OY,
-		OZ:             pc.OZ,
-		Theta:          pc.Theta,
+		X:     pc.X,
+		Y:     pc.Y,
+		Z:     pc.Z,
+		OX:    pc.OX,
+		OY:    pc.OY,
+		OZ:    pc.OZ,
+		Theta: pc.Theta,
 	}
 }
