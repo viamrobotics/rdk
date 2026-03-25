bc95cddb4562b6fea0205b2f941bbac7028c679b
Alden Hsu <alden.hsu@viam.com>
2026-03-25 10:57:59 -0400

    allow overriding VIAM_HOME. log that it's experimental

--- web/server/entrypoint.go
+++ web/server/entrypoint.go
@@ -71,15 +71,6 @@ func logViamEnvVariables(logger logging.Logger) {
 	if rutils.PlatformHomeDir() != "" {
 		viamEnvVariables["HOME"] = rutils.PlatformHomeDir()
 	}
-	// Always attempt to overwrite VIAM_HOME because we do not currently support user-defined home directories.
-	value, alreadySet := os.LookupEnv(rutils.HomeEnvVar)
-	err := os.Setenv(rutils.HomeEnvVar, rutils.ViamDotDir)
-	// if we successfully overwrite VIAM_HOME, log
-	if err == nil && alreadySet {
-		logger.Infof("Environment variable %v was overwritten from %v to %v", rutils.HomeEnvVar, value, rutils.ViamDotDir)
-	} else if err != nil && !alreadySet {
-		logger.Infof("Unable to set %v environment variable, continuing with startup", rutils.HomeEnvVar)
-	}
 	rutils.LogViamEnvVariables("Started with the following Viam environment variables", viamEnvVariables, logger)
 }
 
@@ -159,6 +150,14 @@ func RunServer(ctx context.Context, args []string, _ logging.Logger) (err error)
 		// used. Otherwise run network checks asynchronously.
 		nc.RunNetworkChecks(ctx, rootLogger, false /* !continueRunningTestDNS */)
 		return
+	}
+
+	// If user has set a custom VIAM_HOME, note that it is experimental.
+	if value, isSet := os.LookupEnv(rutils.HomeEnvVar); isSet {
+		rootLogger.Infof("Environment variable %v is set to %v. Support for custom %v is currently experimental. "+
+			"Continuing with startup.", rutils.HomeEnvVar, value, rutils.HomeEnvVar)
+		rootLogger.Infof("Updating .viam dir from %v to %v", rutils.ViamDotDir, value)
+		rutils.ViamDotDir = value
 	}
 
 	// log startup info locally if server fails and exits while attempting to start up
