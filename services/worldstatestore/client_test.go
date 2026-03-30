1d5d5dcc2e429f584ecb2f8fdca4667c374f9270
Ethan Rodkin <ethan@viam.com>
2026-03-30 11:56:37 -0400

    make worldstatestore tests more like docommand tests pt2

--- services/worldstatestore/client_test.go
+++ services/worldstatestore/client_test.go
@@ -198,17 +198,12 @@ func TestClient(t *testing.T) {
 		)
 		test.That(t, err, test.ShouldBeNil)
 
-		// Status - default empty status
-		statusResult, err := client.Status(context.Background())
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, statusResult, test.ShouldBeEmpty)
-
 		// Status - custom status
 		expectedStatus := map[string]interface{}{"key": "value", "count": float64(42)}
 		srv.StatusFunc = func(ctx context.Context) (map[string]interface{}, error) {
 			return expectedStatus, nil
 		}
-		statusResult, err = client.Status(context.Background())
+		statusResult, err := client.Status(context.Background())
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, statusResult, test.ShouldResemble, expectedStatus)
 		srv.StatusFunc = nil
