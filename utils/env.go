39ba24e5ba2ff00ec648ffe8bdfe6a1bcb29a73a
Abe Winter <abe-winter@users.noreply.github.com>
2026-03-31 19:12:36 -0400

    lint

--- utils/env.go
+++ utils/env.go
@@ -176,15 +176,13 @@ func PlatformMkdirTemp(dir, pattern string) (string, error) {
 	if runtime.GOOS == "windows" {
 		if wd, err := os.Getwd(); err != nil {
 			return "", err
-		} else {
-			if filepath.VolumeName(wd) != "C:" {
-				// because we put socket paths in the temp dir, and because socket paths are
-				// posix paths (leading slash) for grpc-go reasons, we can't use the normal
-				// temp dir (which is on C:) when working directory is not on C:.
-				dir = filepath.Join(ViamDotDir, "tmp")
-				if err := os.MkdirAll(dir, 0o700); err != nil {
-					return "", err
-				}
+		} else if filepath.VolumeName(wd) != "C:" {
+			// because we put socket paths in the temp dir, and because socket paths are
+			// posix paths (leading slash) for grpc-go reasons, we can't use the normal
+			// temp dir (which is on C:) when working directory is not on C:.
+			dir = filepath.Join(ViamDotDir, "tmp")
+			if err := os.MkdirAll(dir, 0o700); err != nil {
+				return "", err
 			}
 		}
 	}
