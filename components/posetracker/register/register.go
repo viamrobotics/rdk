1a771a6e2c9b1b3680c89db532faf13fb1eb91e7
Katie Peters <katiepeters@users.noreply.github.com>
2026-05-04 12:15:06 -0400

    Add fake posetracker

---
+++ components/posetracker/register/register.go
@@ -0,0 +1,7 @@
+// Package register registers all relevant pose trackers
+package register
+
+import (
+	// for pose trackers.
+	_ "go.viam.com/rdk/components/posetracker/fake"
+)
--- components/register/all.go
+++ components/register/all.go
@@ -16,8 +16,7 @@ import (
 	_ "go.viam.com/rdk/components/input/register"
 	_ "go.viam.com/rdk/components/motor/register"
 	_ "go.viam.com/rdk/components/movementsensor/register"
-	// register APIs without implementations directly.
-	_ "go.viam.com/rdk/components/posetracker"
+	_ "go.viam.com/rdk/components/posetracker/register"
 	_ "go.viam.com/rdk/components/powersensor/register"
 	_ "go.viam.com/rdk/components/sensor/register"
 	_ "go.viam.com/rdk/components/servo/register"
