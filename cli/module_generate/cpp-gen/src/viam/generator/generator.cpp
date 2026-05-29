7f287486200dfc287a738bc281db69c012597c7d
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-05-29 16:15:50 -0400

    fix test

--- cli/module_generate/cpp-gen/src/viam/generator/generator.cpp
+++ cli/module_generate/cpp-gen/src/viam/generator/generator.cpp
@@ -81,12 +81,11 @@ Generator::Generator(GeneratorCompDB db,
       resourcePath_(std::move(resourcePath)),
       headerOut_(std::move(headerOut)),
       srcOut_(std::move(srcOut)) {
-    if (llvm::StringRef(resourceSubtypeSnake_).startswith("generic_")) {
-        resourceSubtypeSnake_ = "generic";
-
+    if (resourceSubtypeSnake_ == "generic") {
         resourceSubtypePascal_ +=
             (resourceType_ == ResourceType::component) ? "Component" : "Service";
     }
+    llvm::errs() << "Appended " << resourceSubtypePascal_ << "\n";
 }
 
 void Generator::header_prefix() {
