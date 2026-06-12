c42e96b4a46b53d3621b50391d7eda05fbdc86cb
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-06-12 11:53:47 -0400

    feat: update response metadata struct

--- resource/response_metadata.go
+++ resource/response_metadata.go
@@ -4,18 +4,29 @@ import (
 	"time"
 
 	commonpb "go.viam.com/api/common/v1"
+	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // ResponseMetadata contains extra info associated with a Resource's standard response.
 type ResponseMetadata struct {
 	CapturedAt time.Time
+	Attributes map[string]interface{}
 }
 
 // AsProto turns the ResponseMetadata struct into a protobuf message.
 func (rm ResponseMetadata) AsProto() *commonpb.ResponseMetadata {
 	metadata := &commonpb.ResponseMetadata{}
 	metadata.CapturedAt = timestamppb.New(rm.CapturedAt)
+	attributes := make(map[string]*structpb.Value)
+	for k, v := range rm.Attributes {
+		value, err := structpb.NewValue(v)
+		if err != nil {
+			continue
+		}
+		attributes[k] = value
+	}
+	metadata.Attributes = &structpb.Struct{Fields: attributes}
 	return metadata
 }
 
@@ -23,5 +34,6 @@ func (rm ResponseMetadata) AsProto() *commonpb.ResponseMetadata {
 func ResponseMetadataFromProto(proto *commonpb.ResponseMetadata) ResponseMetadata {
 	metadata := ResponseMetadata{}
 	metadata.CapturedAt = proto.CapturedAt.AsTime()
+	metadata.Attributes = proto.Attributes.AsMap()
 	return metadata
 }
--- resource/response_metadata_test.go
+++ resource/response_metadata_test.go
@@ -6,19 +6,22 @@ import (
 
 	commonpb "go.viam.com/api/common/v1"
 	"go.viam.com/test"
+	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestResponseToProto(t *testing.T) {
 	ts := time.UnixMilli(12345)
-	metadata := ResponseMetadata{CapturedAt: ts}
+	metadata := ResponseMetadata{CapturedAt: ts, Attributes: map[string]interface{}{"key": "value"}}
 	proto := metadata.AsProto()
 	test.That(t, proto.CapturedAt.AsTime(), test.ShouldEqual, ts)
+	test.That(t, proto.Attributes.Fields["key"].GetStringValue(), test.ShouldEqual, "value")
 }
 
 func TestResponseFromProto(t *testing.T) {
 	ts := &timestamppb.Timestamp{Seconds: 12, Nanos: 345000000}
-	proto := &commonpb.ResponseMetadata{CapturedAt: ts}
+	proto := &commonpb.ResponseMetadata{CapturedAt: ts, Attributes: &structpb.Struct{Fields: map[string]*structpb.Value{"key": {Kind: &structpb.Value_StringValue{StringValue: "value"}}}}}
 	metadata := ResponseMetadataFromProto(proto)
 	test.That(t, metadata.CapturedAt, test.ShouldEqual, time.UnixMilli(12345))
+	test.That(t, metadata.Attributes["key"], test.ShouldEqual, "value")
 }
