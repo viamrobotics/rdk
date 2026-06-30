viamrobotics/rdk

commit cf61af336a70250c74ba17737d96872f4b401879

Reconfiguration Refactor and system-wide QoL Improvements (#2225)
With these changes, there is a new way to simply reconfigure resources. All resources (a resource.Resource) must implement a Reconfigure method that takes in the component/service's configuration and dependencies. If a resource does not wish to implement this because config changes do not matter, embed resource.TriviallyReconfigurable. If they do matter but writing the reconfiguration is infeasible, embed resource.AlwaysRebuild. Otherwise, reconfigurations should be done atomically and in place. It's best to write a resource model with an initial construction phase of minimal setup and then immediately calling Reconfigure for the first time. Writing it in this way eases implementation and also has one consider if any past state exists while doing reconfiguration. If reconfiguration fails, the resource should put itself into a failure state until it is asked to reconfigure again (potentially with a new config). Anything that depends on it will get closed down but during the in between time, this resource should error out on all of its exposed methods. The resource can also choose to close itself until the next reconfigure.

- Remove reconfigurable and various update handlers
- Remove proxy/wrapping concepts
- Reify resource.Resource with Name and Reconfigure; all resources must handle Reconfigure in some way now
- Unify component and service configs into resource.Config
- Be consistent with use of Config over Attrs
- Make getting converted attributes easier
- Resource Graph only supports GraphNodes now which contains the state/lifecycle of a resource/remote
- Resource Graph always represents desired state of Config + Active Remotes
- Registry (resource and config) simplified with unified constructors and generics
- Internal services introduced to improve reconfiguration and decoupling; web, package, cloud connection, and framesystem

resource/graph_node.go
@@ -1,0 +1,310 @@
+package resource
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+
+	"github.com/pkg/errors"
+)
+
+// A GraphNode contains the current state of a resource.
+// It starts out as either uninitialized, unconfigured, or configured.
+// Based on these states, the underlying Resource may or may not be available.
+// Additionally, the node can be informed that the resource either needs to be
+// updated or eventually removed. During its life, errors may be set on the
+// node to indicate that the resource is no longer available to external users.
+type GraphNode struct {
+	// all fields are protected by this mutex right now, including
+	// the pointer to the atomic int.
+	mu                        sync.RWMutex
+	updatedAt                 int64
+	clock                     *atomic.Int64
+	current                   Resource
+	currentModel              Model
+	config                    Config
+	needsReconfigure          bool
+	lastErr                   error
+	markedForRemoval          bool
+	unresolvedDependencies    []string
+	needsDependencyResolution bool
+}
+
+var (
+	errNotInitalized  = errors.New("resource not initialized yet")
+	errPendingRemoval = errors.New("resource is pending removal")
+)
+
+// NewUninitializedNode returns a node that is brand new and not yet initialized.
+func NewUninitializedNode() *GraphNode {
+	return &GraphNode{}
+}
+
+// NewUnconfiguredGraphNode returns a node that contains enough information to
+// construct the underlying resource.
+func NewUnconfiguredGraphNode(config Config, dependencies []string) *GraphNode {
+	node := &GraphNode{}
+	node.SetNewConfig(config, dependencies)
+	return node
+}
+
+// NewConfiguredGraphNode returns a node that is already configured with
+// the supplied config and resource.
+func NewConfiguredGraphNode(config Config, res Resource, resModel Model) *GraphNode {
+	node := &GraphNode{}
+	node.SetNewConfig(config, nil)
+	node.setDependenciesResolved()
+	node.SwapResource(res, resModel)
+	return node
+}
+
+// UpdatedAt returns the logical time this node was added at.
+func (w *GraphNode) UpdatedAt() int64 {
+	return w.updatedAt
+}
+
+func (w *GraphNode) setClock(clock *atomic.Int64) {
+	w.clock = clock
+}
+
+// Resource returns the underlying resource if it is not pending removal,
+// has no error on it, and is initialized.
+func (w *GraphNode) Resource() (Resource, error) {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	if w.markedForRemoval {
+		return nil, errPendingRemoval
+	}
+	if w.lastErr != nil {
+		return nil, w.lastErr
+	}
+	if w.current == nil {
+		return nil, errNotInitalized
+	}
+	return w.current, nil
+}
+
+// UnsafeResource always returns the underlying resource, if
+// initialized, even if it is in an error state. This should
+// only be called during reconfiguration.
+func (w *GraphNode) UnsafeResource() (Resource, error) {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	if w.current == nil {
+		return nil, errNotInitalized
+	}
+	return w.current, nil
+}
+
+// ResourceModel returns the current model that this resource is.
+// This value should only be assumed to be associated with the current
+// resource.
+func (w *GraphNode) ResourceModel() Model {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.currentModel
+}
+
+// HasResource returns if calling Resource would result in no error.
+func (w *GraphNode) HasResource() bool {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return !w.markedForRemoval && w.lastErr == nil && w.current != nil
+}
+
+// IsUninitialized returns if this resource is in an uninitialized state.
+func (w *GraphNode) IsUninitialized() bool {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.current == nil
+}
+
+// SwapResource emplaces the new resource. It may be the same as before
+// and expects the caller to close the old one. This is considered
+// to be a working resource and as such we unmark it for removal
+// and indicate it no longer needs reconfiguration.
+func (w *GraphNode) SwapResource(newRes Resource, newModel Model) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.current = newRes
+	w.currentModel = newModel
+	w.lastErr = nil
+	w.needsReconfigure = false
+	w.markedForRemoval = false
+
+	// these should already be set
+	w.unresolvedDependencies = nil
+	w.needsDependencyResolution = false
+	if w.clock != nil {
+		w.updatedAt = w.clock.Add(1)
+	}
+}
+
+// MarkForRemoval marks this node for removal at a later time.
+func (w *GraphNode) MarkForRemoval() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.markedForRemoval = true
+}
+
+// MarkedForRemoval returns if this node is marked for removal.
+func (w *GraphNode) MarkedForRemoval() bool {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return w.markedForRemoval
+}
+
+// SetLastError sets the latest error on this node. This will
+// cause the resource to become unavailable to external users of
+// the graph. The resource manager may still access the
+// underlying resource via UnsafeResource.
+func (w *GraphNode) SetLastError(err error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.lastErr = err
+}
+
+// Config returns the current config that this resource is using.
+// This value should only be assumed to be associated with the current
+// resource.
+func (w *GraphNode) Config() Config {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.config
+}
+
+// NeedsReconfigure returns whether or not this node needs reconfiguration
+// performed on its underlying resource.
+func (w *GraphNode) NeedsReconfigure() bool {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return !w.markedForRemoval && w.needsReconfigure
+}
+
+// hasUnresolvedDependencies returns whether or not this node has any
+// dependencies to be resolved (even if they are empty).
+func (w *GraphNode) hasUnresolvedDependencies() bool {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.needsDependencyResolution
+}
+
+func (w *GraphNode) setNeedsReconfigure(newConfig Config, mustReconfigure bool, dependencies []string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if !mustReconfigure && w.markedForRemoval {
+		// This is the case where the node is being asked to update
+		// with no new config but it was marked for removal otherwise.
+		// The current system enforces us to remove since dependencies
+		// have not changed.
+		return
+	}
+	if mustReconfigure {
+		w.needsDependencyResolution = true
+	}
+	w.config = newConfig
+	w.needsReconfigure = true
+	w.markedForRemoval = false
+	w.unresolvedDependencies = dependencies
+}
+
+// SetNewConfig is used to inform the node that it has been modified
+// and requires a reconfiguration. If the node was previously marked for removal,
+// this unmarks it.
+func (w *GraphNode) SetNewConfig(newConfig Config, dependencies []string) {
+	w.setNeedsReconfigure(newConfig, true, dependencies)
+}
+
+// SetNeedsUpdate is used to inform the node that it should
+// reconfigure itself with the same config in order to process
+// dependency updates. If the node was previously marked for removal,
+// this makes no changes.
+func (w *GraphNode) SetNeedsUpdate() {
+	// doing two mutex ops here but we assume there's only one caller.
+	w.setNeedsReconfigure(w.Config(), false, w.UnresolvedDependencies())
+}
+
+// setUnresolvedDependencies sets names that are yet to be resolved as
+// dependencies for the node. Note that even an empty list will still
+// set needsDependencyResolution to true. If no resolution is needed,
+// you would call setDependenciesResolved. The resource graph calls
+// these internally.
+func (w *GraphNode) setUnresolvedDependencies(names ...string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.unresolvedDependencies = names
+	w.needsDependencyResolution = true
+}
+
+// setDependenciesResolved sets that all unresolved dependencies have been
+// resolved and linked/unlinked from the calling resource graph.
+func (w *GraphNode) setDependenciesResolved() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.needsDependencyResolution = false
+}
+
+// UnresolvedDependencies returns the set of names that are yet to be resolved as
+// dependencies for the node.
+func (w *GraphNode) UnresolvedDependencies() []string {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	if len(w.unresolvedDependencies) == 0 {
+		return nil
+	}
+	unresolvedDependencies := make([]string, 0, len(w.unresolvedDependencies))
+	unresolvedDependencies = append(unresolvedDependencies, w.unresolvedDependencies...)
+	return unresolvedDependencies
+}
+
+// Close closes the underlying resource of this node.
+func (w *GraphNode) Close(ctx context.Context) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.current == nil {
+		return nil
+	}
+	current := w.current
+	w.current = nil
+	return current.Close(ctx)
+}
+
+func (w *GraphNode) replace(other *GraphNode) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.current != nil {
+		return errors.New("may only replace an uninitialized node")
+	}
+	if w == other {
+		return nil
+	}
+
+	other.mu.Lock()
+	w.updatedAt = other.updatedAt
+	if other.clock != nil {
+		w.clock = other.clock
+	}
+	w.current = other.current
+	w.currentModel = other.currentModel
+	w.config = other.config
+	w.needsReconfigure = other.needsReconfigure
+	w.lastErr = other.lastErr
+	w.markedForRemoval = other.markedForRemoval
+	w.unresolvedDependencies = other.unresolvedDependencies
+	w.needsDependencyResolution = other.needsDependencyResolution
+
+	// other is now owned by the graph/node and is invalidated
+	other.updatedAt = 0
+	other.clock = nil
+	other.current = nil
+	other.currentModel = Model{}
+	other.config = Config{}
+	other.needsReconfigure = false
+	other.lastErr = nil
+	other.markedForRemoval = false
+	other.unresolvedDependencies = nil
+	other.needsDependencyResolution = false
+	other.mu.Unlock()
+	return nil
+}

commit 40818a3ecbe9d00e836a8960ad33baaca34e7df2

resource: re-resolve dependent edges when a removed dependency is re-added
When a resource node is removed, Graph.remove strips it from every
dependent's parent set but never re-arms those dependents for dependency
resolution. Since ResolveDependencies builds edges only from a node's
`unresolvedDependencies` list (emptied once the node resolves) and the
dependency-changed notify paths (SetNeedsUpdate/SetNeedsRebuild) re-seed
that list from the now-empty set, a surviving dependent's edge is never
rebuilt. The dependency can come back healthy and the dependent still
fails with "Resource missing from dependencies" until its config is
reprocessed (e.g. its module is restarted).

Regressed in #2225 (Apr 2023), which replaced re-deriving edges from
conf.Dependencies() each completeConfig pass with the lazy
resolve-once-then-clear model. The Graph.remove edge-strip dates to #434
(2022) but was benign before #2225.

Fix: in Graph.remove, re-mark the removed dependency as unresolved on each
surviving dependent so the next ResolveDependencies rebuilds the edge once
the dependency reappears. Adds a regression test that fails without the fix.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

resource/graph_node.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"context"
 	"errors"
+	"slices"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -448,6 +449,19 @@ func (w *GraphNode) setUnresolvedDependencies(names ...string) {
 	defer w.mu.Unlock()
 	w.unresolvedDependencies = names
 	w.needsDependencyResolution = true
+}
+
+// addUnresolvedDependency re-marks a single dependency name as unresolved and flags the
+// node for resolution, without clobbering any deps already pending. Used when a dependency
+// edge is force-dropped (e.g. the dependency node is removed) so the dependent re-resolves
+// it once it reappears.
+func (w *GraphNode) addUnresolvedDependency(name string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.needsDependencyResolution = true
+	if !slices.Contains(w.unresolvedDependencies, name) {
+		w.unresolvedDependencies = append(w.unresolvedDependencies, name)
+	}
 }
 
 // setDependenciesResolved sets that all unresolved dependencies have been

resource/resource_graph.go
@@ -637,6 +637,12 @@ func (g *Graph) remove(node Name) {
 	for k, vertice := range g.parents {
 		if _, ok := vertice[node]; ok {
 			removeNodeFromNodeMap(g.parents, k, node)
+			// The dependent `k` just lost an edge to a dependency it still declares.
+			// Re-arm it for resolution so the edge is rebuilt if `node` reappears,
+			// instead of being silently stranded until `k`'s config is reprocessed.
+			if depNode, ok := g.nodes.Get(k); ok {
+				depNode.addUnresolvedDependency(node.String())
+			}
 		}
 	}
 	delete(g.transitiveClosureMatrix, node)

resource/resource_graph_test.go
@@ -1279,3 +1279,70 @@ func shouldMatchMultipleNodesErr(actual interface{}, expected ...interface{}) st
 	}
 	return ""
 }
+
+// TestResourceGraphRebuildEdgeAfterDependencyReadded reproduces the dependency-edge
+// stranding bug: once a node has resolved its dependencies, removing one of those
+// dependency nodes strips the edge but does NOT re-arm the dependent for resolution,
+// so re-adding the dependency never rebuilds the edge.
+//
+// Models: A (dependent) depends-by-name on B (dependency), as a modular resource would
+// after Validate() returns B in its required deps.
+func TestResourceGraphRebuildEdgeAfterDependencyReadded(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	g := NewGraph(logger)
+
+	nameA := NewName(apiA, "A")
+	nameB := NewName(apiA, "B")
+
+	parentsOfA := func() []Name { return g.GetAllParentsOf(nameA) }
+
+	// B exists (a built dependency). A is unconfigured and declares it depends on B by name,
+	// exactly as NewUnconfiguredGraphNode(conf, conf.Dependencies()) would seed it.
+	test.That(t, g.AddNode(nameB, &GraphNode{}), test.ShouldBeNil)
+	test.That(t, g.AddNode(nameA, NewUnconfiguredGraphNode(Config{API: apiA, Name: "A"}, []string{nameB.String()})), test.ShouldBeNil)
+
+	// First resolution: the A->B edge is created (this is "A resolved B once at construction").
+	test.That(t, g.ResolveDependencies(logger), test.ShouldBeNil)
+	t.Logf("after initial resolve, parents of A = %v", parentsOfA())
+	test.That(t, parentsOfA(), test.ShouldResemble, []Name{nameB})
+
+	// The dependency's node is removed (collision / removed-from-config). This strips the edge.
+	g.remove(nameB)
+	t.Logf("after remove(B),     parents of A = %v", parentsOfA())
+	test.That(t, parentsOfA(), test.ShouldBeEmpty) // edge correctly dropped
+
+	// The dependency comes back as a fresh node, and the resolver runs again (as completeConfig
+	// does every pass). EXPECTED: the A->B edge is rebuilt. (This is the assertion that fails today.)
+	test.That(t, g.AddNode(nameB, &GraphNode{}), test.ShouldBeNil)
+	test.That(t, g.ResolveDependencies(logger), test.ShouldBeNil)
+	t.Logf("after readd + resolve, parents of A = %v  (want [%v])", parentsOfA(), nameB)
+	test.That(t, parentsOfA(), test.ShouldResemble, []Name{nameB})
+}
+
+// TestResourceGraphRebuildEdgeViaFullReseed shows the "cure" path: when the dependent is
+// re-seeded with its full declared dependency list (what markResourceForUpdate /
+// ResolveImplicitDependencies do on a config reprocess / module restart), the edge is rebuilt.
+func TestResourceGraphRebuildEdgeViaFullReseed(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	g := NewGraph(logger)
+
+	nameA := NewName(apiA, "A")
+	nameB := NewName(apiA, "B")
+	parentsOfA := func() []Name { return g.GetAllParentsOf(nameA) }
+
+	test.That(t, g.AddNode(nameB, &GraphNode{}), test.ShouldBeNil)
+	nodeA := NewUnconfiguredGraphNode(Config{API: apiA, Name: "A"}, []string{nameB.String()})
+	test.That(t, g.AddNode(nameA, nodeA), test.ShouldBeNil)
+	test.That(t, g.ResolveDependencies(logger), test.ShouldBeNil)
+	test.That(t, parentsOfA(), test.ShouldResemble, []Name{nameB})
+
+	g.remove(nameB)
+	test.That(t, parentsOfA(), test.ShouldBeEmpty)
+
+	// Re-add B, and re-seed A's full declared deps (the config-reprocess path).
+	test.That(t, g.AddNode(nameB, &GraphNode{}), test.ShouldBeNil)
+	nodeA.SetNewConfig(Config{API: apiA, Name: "A"}, []string{nameB.String()})
+	test.That(t, g.ResolveDependencies(logger), test.ShouldBeNil)
+	t.Logf("after full re-seed + resolve, parents of A = %v", parentsOfA())
+	test.That(t, parentsOfA(), test.ShouldResemble, []Name{nameB})
+}