12bb428b0e50f84578f872b125e67844da37d825
Benjamin Rewis <benji.rewis@viam.com>
2026-07-15 12:24:24 -0400

    Revert "Log in NetAppender when we lose and regain connection app"
    This reverts commit 90ace0542b2583ddd3f9c677f0ca5466089f648c.

--- grpc/app_conn.go
+++ grpc/app_conn.go
@@ -111,8 +111,6 @@ func NewAppConn(ctx context.Context, appAddress, partID string, cloudCreds rpc.D
 
 // GetState returns the current state of the connection.
 func (ac *AppConn) GetState() connectivity.State {
-	ac.connMu.RLock()
-	defer ac.connMu.RUnlock()
 	if ac.conn == nil {
 		return connectivity.Connecting
 	}
