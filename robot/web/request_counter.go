6a2641ed40362f36a4f7ff07321a666542462844
Evan Dorsky <evan.dorsky@icloud.com>
2026-07-27 12:16:23 -0400

    log on liveness check expiry

--- robot/web/request_counter.go
+++ robot/web/request_counter.go
@@ -547,7 +547,6 @@ func (rc *RequestCounter) setClientMetadataForPC(ctx context.Context, pc *webrtc
 	}
 
 	rc.pcToClientMetadata.Store(pc, clientMetadata)
-	rc.logger.Activity("connection", "connect", "client", clientMetadata)
 }
 
 // incrInFlight attempts to increment the in-flight request counters for a given
