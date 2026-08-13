1cffe9e14b3769fffb254a31437ddce2db7c8c38
Dan Gottlieb <dan.gottlieb@viam.com>
2026-08-13 10:56:46 -0400

    omit empty frames when joint position goals were provided

--- referenceframe/linear_inputs.go
+++ referenceframe/linear_inputs.go
@@ -285,6 +285,11 @@ func (li *LinearInputs) GetFrameInputs(frame Frame) ([]Input, error) {
 func (li *LinearInputs) ComputePoses(fs *FrameSystem) (FrameSystemPoses, error) {
 	computedPoses := make(FrameSystemPoses)
 	for _, frameName := range fs.FrameNames() {
+		frame := fs.Frame(frameName)
+		if len(frame.DoF()) == 0 {
+			continue
+		}
+
 		pif, err := fs.Transform(li, NewZeroPoseInFrame(frameName), World)
 		if err != nil {
 			return nil, err
