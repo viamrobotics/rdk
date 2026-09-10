338db7233ab2ef996a4ada1d5062bc5745e1ab93
Nandini <nandini.swami@viam.com>
2026-09-10 14:49:12 -0400

    Collect FTDC process stats on darwin without /proc

---
+++ ftdc/sys/sys_darwin.go
@@ -0,0 +1,51 @@
+//go:build darwin
+
+package sys
+
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
+
+// UsageStatser can be used to get system metrics for a process.
+type UsageStatser struct {
+	proc *process.Process
+}
+
+// newSysUsageStatser will return a `UsageStatser` for the given process id.
+func newSysUsageStatser(pid int) (*UsageStatser, error) {
+	proc, err := process.NewProcess(int32(pid))
+	if err != nil {
+		return nil, err
+	}
+
+	return &UsageStatser{proc}, nil
+}
+
+// Stats returns Stats.
+func (sys *UsageStatser) Stats() any {
+	cpuTimes, err := sys.proc.Times()
+	if err != nil {
+		return stats{}
+	}
+
+	memInfo, err := sys.proc.MemoryInfo()
+	if err != nil {
+		return stats{}
+	}
+
+	// CreateTime returns process creation time in milliseconds since the epoch.
+	createTime, err := sys.proc.CreateTime()
+	if err != nil {
+		return stats{}
+	}
+
+	return stats{
+		UserCPUSecs:     cpuTimes.User,
+		SystemCPUSecs:   cpuTimes.System,
+		ElapsedTimeSecs: float64(time.Now().UnixMilli()-createTime) / 1000.0,
+		VssMB:           float64(memInfo.VMS) / 1_000_000.0,
+		RssMB:           float64(memInfo.RSS) / 1_000_000.0,
+	}
+}
