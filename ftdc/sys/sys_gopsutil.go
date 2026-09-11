089366a7810a797c8793149b5e7f2a9fee4f3365
Nandini <nandini.swami@viam.com>
2026-09-11 14:32:49 -0400

    merge sys_windows.go and sys_darwin.go

--- ftdc/sys/sys_gopsutil.go
+++ ftdc/sys/sys_gopsutil.go
@@ -1,25 +1,37 @@
-//go:build darwin
+//go:build darwin || windows
 
 package sys
 
 import (
 	"time"
 
+	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/process"
 )
+
+var machineBootTimeSecsSinceEpoch float64
+
+func init() {
+	bootTime, err := host.BootTime()
+	if err != nil {
+		machineBootTimeSecsSinceEpoch = 0
+	} else {
+		machineBootTimeSecsSinceEpoch = float64(bootTime)
+	}
+}
 
 // UsageStatser can be used to get system metrics for a process.
 type UsageStatser struct {
 	proc *process.Process
 }
 
-// newSysUsageStatser will return a `UsageStatser` for the given process id.
+// NewPidSysUsageStatser will return a `SysUsageStatser` for the given process id.
+// just leave this one
 func newSysUsageStatser(pid int) (*UsageStatser, error) {
 	proc, err := process.NewProcess(int32(pid))
 	if err != nil {
 		return nil, err
 	}
-
 	return &UsageStatser{proc}, nil
 }
 
@@ -35,16 +47,18 @@ func (sys *UsageStatser) Stats() any {
 		return stats{}
 	}
 
-	// CreateTime returns process creation time in milliseconds since the epoch.
+	// CreateTime returns process creations time, in ms.
 	createTime, err := sys.proc.CreateTime()
 	if err != nil {
 		return stats{}
 	}
 
+	elapsedTimeSecs := float64(time.Now().UnixMilli()-createTime) / 1000.0
+
 	return stats{
 		UserCPUSecs:     cpuTimes.User,
 		SystemCPUSecs:   cpuTimes.System,
-		ElapsedTimeSecs: float64(time.Now().UnixMilli()-createTime) / 1000.0,
+		ElapsedTimeSecs: elapsedTimeSecs,
 		VssMB:           float64(memInfo.VMS) / 1_000_000.0,
 		RssMB:           float64(memInfo.RSS) / 1_000_000.0,
 	}
