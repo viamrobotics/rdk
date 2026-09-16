0aa734c2fff599dd67bbfb01bc39815f3f0bbbf8
Gabe <gabe.gottlob@viam.com>
2026-09-16 16:52:32 -0400

    fix register comment

--- components/camera/register/register_webcam_cgo.go
+++ components/camera/register/register_webcam_cgo.go
@@ -1,11 +1,9 @@
 //go:build (windows && cgo) || viam_windows_resource_dump
 
 // Package register registers all relevant cameras and also API specific functions
+// This registers the webcam driver on Windows builds that have cgo enabled
 package register
 
-// Package register registers the webcam driver on Windows builds that have cgo enabled,
-// independent of the no_cgo tag so other cgo-gated features (graphviz, nlopt, x264
-// streaming, etc.) keep using their existing no_cgo stubs on Windows.
 import (
 	// for cameras.
 	_ "go.viam.com/rdk/components/camera/videosource"
