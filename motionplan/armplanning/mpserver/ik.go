92ecea7a5f627e6eb31d6cd95268354700f56a8b
Bohdan Karavan <bohdan.m.karavan@gmail.com>
2026-09-18 10:58:58 -0400

    call randSeed as a function argument

--- motionplan/armplanning/mpserver/ik.go
+++ motionplan/armplanning/mpserver/ik.go
@@ -90,7 +90,6 @@ func InspectIK(ctx context.Context, logger logging.Logger,
 	for seedIdx, seed := range sss.LinearSeeds {
 		seeds := [][]float64{seed}
 		limits := [][]referenceframe.Limit{sss.SeedLimits[seedIdx]}
-		randInt := randSeed.Int()
 
 		ctxWithCancel, cancel := context.WithCancel(ctx)
 		wg := sync.WaitGroup{}
@@ -98,7 +97,7 @@ func InspectIK(ctx context.Context, logger logging.Logger,
 		go func() {
 			//nolint: errcheck
 			_, _, _ = solver.Solve(ctxWithCancel, retChan, nil,
-				seeds, limits, ikMinimizingFunc, randInt)
+				seeds, limits, ikMinimizingFunc, randSeed.Int())
 			cancel()
 			wg.Done()
 		}()
