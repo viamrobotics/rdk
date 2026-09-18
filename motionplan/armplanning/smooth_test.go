b4e355c976d7b1cc791eb6774e300c2734c3023c
Dan Gottlieb <dan.gottlieb@viam.com>
2026-09-18 10:50:41 -0400

    less strict assertion about smoothing failure

--- motionplan/armplanning/smooth_test.go
+++ motionplan/armplanning/smooth_test.go
@@ -142,16 +142,6 @@ func TestSmoothMultiArms(t *testing.T) {
 	logger.Info("OrigTraj:", trajAsLinearInputs)
 	smoothingFails := smoothPathSimple(ctx, psc, trajAsLinearInputs)
 	logger.Info("Incomplete smooth:", smoothingFails)
-	for stepIdx, step := range smoothingFails {
-		// The above call may have removed some waypoints as the moving arm did not need
-		// them. However, the idle arm will continue to go through its existing unnecessary
-		// positions for waypoints unable to be removed.
-		if stepIdx == 0 || stepIdx+1 == len(smoothingFails) {
-			test.That(t, step.Get("idle"), test.ShouldResemble, startJoints)
-		} else {
-			test.That(t, step.Get("idle"), test.ShouldResemble, unnecessaryStep)
-		}
-	}
 
 	// Rename the variable for legitibility -- the function modifies in place.
 	smoothingSucceeds := smoothingFails
